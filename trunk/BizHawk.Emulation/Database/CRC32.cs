afbdf7d4208fe14c744e00b142742e510c1b1b5e
beirich <beirich@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-01-17 07:41:01 +0000

    SMS: Implement TMS9918 legacy Mode 0

--- trunk/BizHawk.Emulation/Database/CRC32.cs
+++ trunk/BizHawk.Emulation/Database/CRC32.cs
@@ -7,31 +7,27 @@
 
 		static CRC32() 
         {
-			//unchecked {
-				CRC32Table = new uint[256];
-			    for (uint i = 0; i < 256; ++i) 
+			CRC32Table = new uint[256];
+		    for (uint i = 0; i < 256; ++i) 
+            {
+				uint crc = i;
+				for (int j = 8; j > 0; --j) 
                 {
-					uint crc = i;
-					for (int j = 8; j > 0; --j) 
-                    {
-						if ((crc & 1) == 1)
-							crc = ((crc >> 1) ^ 0xEDB88320);
-					    else
-							crc >>= 1;
-					}
-					CRC32Table[i] = crc;
+					if ((crc & 1) == 1)
+						crc = ((crc >> 1) ^ 0xEDB88320);
+				    else
+						crc >>= 1;
 				}
-			//}
+				CRC32Table[i] = crc;
+			}
 		}
 
         public static int Calculate(byte[] data) 
         {
-			//unchecked {
-			    uint Result = 0xFFFFFFFF;
-				foreach (var b in data) 
-                    Result = (((Result) >> 8) ^ CRC32Table[b ^ ((Result) & 0xFF)]);				
-				return (int)~Result;
-			//}
+		    uint Result = 0xFFFFFFFF;
+			foreach (var b in data) 
+                Result = (((Result) >> 8) ^ CRC32Table[b ^ ((Result) & 0xFF)]);				
+			return (int)~Result;
 		}
 	}
 }
