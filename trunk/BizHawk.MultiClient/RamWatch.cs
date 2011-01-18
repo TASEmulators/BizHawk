4dd019ffcac3bade307af815941f90ec324816c7
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-01-18 03:36:43 +0000

    Add a very bare Ram Watch dialog

---
+++ trunk/BizHawk.MultiClient/RamWatch.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BizHawk.MultiClient
+{
+    public partial class RamWatch : Form
+    {
+        public RamWatch()
+        {
+            InitializeComponent();
+        }
+
+        void AddNewWatch()
+        {
+        }
+
+        void EditWatch()
+        {
+        }
+
+        void RemoveWatch()
+        {
+        }
+
+        void DuplicateWatch()
+        {
+        }
+
+        void MoveUp()
+        {
+        }
+
+        void MoveDown()
+        {
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void newListToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void appendFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void autoLoadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveWindowPositionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newWatchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AddNewWatch();
+        }
+
+        private void editWatchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            EditWatch();
+        }
+
+        private void removeWatchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RemoveWatch();
+        }
+
+        private void duplicateWatchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DuplicateWatch();
+        }
+
+        private void moveUpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MoveUp();
+        }
+
+        private void moveDownToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MoveDown();
+        }
+    }
+}
