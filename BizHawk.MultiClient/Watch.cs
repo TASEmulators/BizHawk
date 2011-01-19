49ea0515d814366b65f91c6230bb39e0a7b2dd52
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-01-19 02:49:47 +0000

    Ram Watch - more progress on Open Watch function, reads .wch files in the format of other emulators.  Currently the results are garbled, need to fix bugs

--- BizHawk.MultiClient/Watch.cs
+++ BizHawk.MultiClient/Watch.cs
@@ -34,5 +34,41 @@ namespace BizHawk.MultiClient
         public asigned signed { get; set; }    //Signed/Unsigned?
         public bool bigendian { get; set; }
         public string notes { get; set; }      //User notes
+
+        public bool SetTypeByChar(char c)     //b = byte, w = word, d = dword
+        {
+            switch (c)
+            {
+                case 'b':
+                    type = atype.BYTE;
+                    return true;
+                case 'w':
+                    type = atype.WORD;
+                    return true;
+                case 'd':
+                    type = atype.DWORD;
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        public bool SetSignedByChar(char c) //s = signed, u = unsigned, h = hex
+        {
+            switch (c)
+            {
+                case 's':
+                    signed = asigned.SIGNED;
+                    return true;
+                case 'u':
+                    signed = asigned.UNSIGNED;
+                    return true;
+                case 'h':
+                    signed = asigned.HEX;
+                    return true;
+                default:
+                    return false;
+            }
+        }
     }
 }
