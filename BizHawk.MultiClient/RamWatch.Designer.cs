887d82770c40b736ba92bfc3f44cf303ba42738f
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-01-20 01:10:05 +0000

    Ram Watch - fix saving of window position

--- BizHawk.MultiClient/RamWatch.Designer.cs
+++ BizHawk.MultiClient/RamWatch.Designer.cs
@@ -334,6 +334,7 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "RamWatch";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Ram Watch";
             this.Load += new System.EventHandler(this.RamWatch_Load);
             this.LocationChanged += new System.EventHandler(this.RamWatch_LocationChanged);
