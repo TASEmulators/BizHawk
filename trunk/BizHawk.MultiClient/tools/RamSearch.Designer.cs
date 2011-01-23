fff99fbcf0182735666d1a14528811a5a18ba805
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-01-23 20:15:54 +0000

    Some small Ram Search progress

--- trunk/BizHawk.MultiClient/tools/RamSearch.Designer.cs
+++ trunk/BizHawk.MultiClient/tools/RamSearch.Designer.cs
@@ -30,16 +30,20 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RamSearch));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.toolStripSeparator = new System.Windows.Forms.ToolStripSeparator();
-            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-            this.SearchListBox = new System.Windows.Forms.ListBox();
-            this.TotalSearchLabel = new System.Windows.Forms.Label();
             this.newToolStripButton = new System.Windows.Forms.ToolStripButton();
             this.openToolStripButton = new System.Windows.Forms.ToolStripButton();
             this.saveToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator = new System.Windows.Forms.ToolStripSeparator();
             this.cutToolStripButton = new System.Windows.Forms.ToolStripButton();
             this.WatchtoolStripButton1 = new System.Windows.Forms.ToolStripButton();
             this.PoketoolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.TotalSearchLabel = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Address = new System.Windows.Forms.ColumnHeader();
+            this.Value = new System.Windows.Forms.ColumnHeader();
+            this.Previous = new System.Windows.Forms.ColumnHeader();
+            this.Changes = new System.Windows.Forms.ColumnHeader();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -59,33 +63,6 @@
             this.toolStrip1.Size = new System.Drawing.Size(378, 25);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
-            // 
-            // toolStripSeparator
-            // 
-            this.toolStripSeparator.Name = "toolStripSeparator";
-            this.toolStripSeparator.Size = new System.Drawing.Size(6, 25);
-            // 
-            // toolStripSeparator1
-            // 
-            this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
-            // 
-            // SearchListBox
-            // 
-            this.SearchListBox.FormattingEnabled = true;
-            this.SearchListBox.Location = new System.Drawing.Point(12, 52);
-            this.SearchListBox.Name = "SearchListBox";
-            this.SearchListBox.Size = new System.Drawing.Size(150, 394);
-            this.SearchListBox.TabIndex = 1;
-            // 
-            // TotalSearchLabel
-            // 
-            this.TotalSearchLabel.AutoSize = true;
-            this.TotalSearchLabel.Location = new System.Drawing.Point(13, 33);
-            this.TotalSearchLabel.Name = "TotalSearchLabel";
-            this.TotalSearchLabel.Size = new System.Drawing.Size(64, 13);
-            this.TotalSearchLabel.TabIndex = 2;
-            this.TotalSearchLabel.Text = "0 addresses";
             // 
             // newToolStripButton
             // 
@@ -117,6 +94,11 @@
             this.saveToolStripButton.Text = "&Save";
             this.saveToolStripButton.ToolTipText = "Save Watch List";
             // 
+            // toolStripSeparator
+            // 
+            this.toolStripSeparator.Name = "toolStripSeparator";
+            this.toolStripSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
             // cutToolStripButton
             // 
             this.cutToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
@@ -145,13 +127,65 @@
             this.PoketoolStripButton1.Size = new System.Drawing.Size(23, 22);
             this.PoketoolStripButton1.Text = "Poke";
             // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // TotalSearchLabel
+            // 
+            this.TotalSearchLabel.AutoSize = true;
+            this.TotalSearchLabel.Location = new System.Drawing.Point(13, 33);
+            this.TotalSearchLabel.Name = "TotalSearchLabel";
+            this.TotalSearchLabel.Size = new System.Drawing.Size(64, 13);
+            this.TotalSearchLabel.TabIndex = 2;
+            this.TotalSearchLabel.Text = "0 addresses";
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Address,
+            this.Value,
+            this.Previous,
+            this.Changes});
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(16, 58);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(221, 391);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Address
+            // 
+            this.Address.Text = "Address";
+            this.Address.Width = 66;
+            // 
+            // Value
+            // 
+            this.Value.Text = "Value";
+            this.Value.Width = 48;
+            // 
+            // Previous
+            // 
+            this.Previous.Text = "Prev";
+            this.Previous.Width = 48;
+            // 
+            // Changes
+            // 
+            this.Changes.Text = "Changes";
+            this.Changes.Width = 55;
+            // 
             // RamSearch
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(378, 461);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.TotalSearchLabel);
-            this.Controls.Add(this.SearchListBox);
             this.Controls.Add(this.toolStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "RamSearch";
@@ -175,7 +209,11 @@
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripButton WatchtoolStripButton1;
         private System.Windows.Forms.ToolStripButton PoketoolStripButton1;
-        private System.Windows.Forms.ListBox SearchListBox;
         private System.Windows.Forms.Label TotalSearchLabel;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Address;
+        private System.Windows.Forms.ColumnHeader Value;
+        private System.Windows.Forms.ColumnHeader Previous;
+        private System.Windows.Forms.ColumnHeader Changes;
     }
 }
