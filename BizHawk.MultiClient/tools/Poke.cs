b1dadc44a0d463e87d123410624a121e87e17e7f
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-02-16 16:35:51 +0000

    Progress on implementing a Memory Poke object (for tools such as Ram Watch, Ram Search, and eventually things like a Hex Editor)

---
+++ BizHawk.MultiClient/tools/Poke.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BizHawk.MultiClient
+{
+    /// <summary>
+    /// This class prompts the user for an address & value and writes to memory
+    /// If supplied a Watch object it will use it to generate default values
+    /// </summary>
+    class Poke
+    {
+        Watch w = new Watch();
+
+        public bool PokeAddress(Watch watch)
+        {
+            w = watch;
+            return PokeWatch();
+        }
+
+        public bool PokeAddress()
+        {
+            return PokeWatch();
+        }
+
+        private bool PokeWatch()
+        {
+            InputPrompt i = new InputPrompt();
+            i.Text = "Poke address";
+            i.ShowDialog();
+            //Prompt user
+            //Attempt to poke
+            //If use cancels or some failure, return false, else turn true
+            return true;
+        }
+    }
+}
