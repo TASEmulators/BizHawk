5c3c69c2ee31e9e08435bdcfa506a9687c732752
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-02-18 03:48:42 +0000

    Ram Watch - attempt to validate when a user types an address directly into the list view.  Also, added a static InputValidate class that will contain various input validation functions to use on text boxes & such.

---
+++ trunk/BizHawk.Util/InputValidate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BizHawk
+{
+    /// <summary>
+    /// Includes helper functions to validate user input
+    /// </summary>
+    public static class InputValidate
+    {
+        public static bool IsValidUnsignedNumber(char[] input)
+        {
+            ASCIIEncoding AE = new ASCIIEncoding();
+            // Check each character in the new label to determine if it is a number.
+            for (int x = 0; x < input.Length; x++)
+            {
+                // Encode the character from the character array to its ASCII code.
+                byte[] bc = AE.GetBytes(input[x].ToString());
+
+                // Determine if the ASCII code is within the valid range of numerical values.
+                if (bc[0] < 47 || bc[0] > 58)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
