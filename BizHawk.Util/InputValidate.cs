5a0d477dba6a12c78571d9c78073586dbcd406c7
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-02-18 05:52:09 +0000

    Ram Watch - directly editing an address in the list view validates user input, and if a valid hex number, will pull up an edit watch dialog

--- BizHawk.Util/InputValidate.cs
+++ BizHawk.Util/InputValidate.cs
@@ -10,8 +10,9 @@ namespace BizHawk
     /// </summary>
     public static class InputValidate
     {
-        public static bool IsValidUnsignedNumber(char[] input)
+        public static bool IsValidUnsignedNumber(string Str)
         {
+            char[] input = (Str.ToCharArray());
             ASCIIEncoding AE = new ASCIIEncoding();
             // Check each character in the new label to determine if it is a number.
             for (int x = 0; x < input.Length; x++)
@@ -22,6 +23,33 @@ namespace BizHawk
                 // Determine if the ASCII code is within the valid range of numerical values.
                 if (bc[0] < 47 || bc[0] > 58)
                     return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// validates is a Hex number 0-9, A-F (must be capital letters)
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static bool IsValidHexNumber(string Str)
+        {
+            char[] input = (Str.ToCharArray());
+            ASCIIEncoding AE = new ASCIIEncoding();
+            // Check each character in the new label to determine if it is a number.
+            for (int x = 0; x < input.Length; x++)
+            {
+                // Encode the character from the character array to its ASCII code.
+                byte[] bc = AE.GetBytes(input[x].ToString());
+
+                // Determine if the ASCII code is within the valid range of numerical values.
+                if (bc[0] < 47)
+                    return false;
+                if (bc[0] > 58)
+                {
+                    if (bc[0] < 65 || bc[0] > 70) //A-F capital letters only!
+                        return false;
+                }
             }
             return true;
         }
