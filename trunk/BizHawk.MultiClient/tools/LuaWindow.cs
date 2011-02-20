1c0dd9c47eb3ea601a1ca71cf5db76391e1b8850
kylethomson@gmail.com <kylethomson@gmail.com@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-02-20 01:27:16 +0000

    Adding C# lua projects to BizHawk. One refuses to compile.

--- trunk/BizHawk.MultiClient/tools/LuaWindow.cs
+++ trunk/BizHawk.MultiClient/tools/LuaWindow.cs
@@ -6,9 +6,11 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+ 
 
 namespace BizHawk.MultiClient.tools
 {
+    
     public partial class LuaWindow : Form
     {        
         public LuaWindow()
