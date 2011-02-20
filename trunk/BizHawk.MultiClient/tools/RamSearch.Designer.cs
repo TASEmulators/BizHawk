7b5f72a3ff8220611644c44318dceb37bb90d97e
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-02-20 23:01:43 +0000

    Ram Search - implement searching by number of changes

--- trunk/BizHawk.MultiClient/tools/RamSearch.Designer.cs
+++ trunk/BizHawk.MultiClient/tools/RamSearch.Designer.cs
@@ -392,7 +392,7 @@
             this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton1.Name = "toolStripButton1";
             this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
-            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Text = "Search";
             this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
             // 
             // ClearChangeCountstoolStripButton
