ba1c985a66155ee749669180bf5b74de6244c8dc
zeromus@zeromus.org <zeromus@zeromus.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-02-20 01:35:58 +0000

    zeromus uses the sandglass of regression!

--- trunk/BizHawk.MultiClient/tools/LuaWindow.cs
+++ trunk/BizHawk.MultiClient/tools/LuaWindow.cs
@@ -6,11 +6,9 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
- 
 
 namespace BizHawk.MultiClient.tools
 {
-    
     public partial class LuaWindow : Form
     {        
         public LuaWindow()
