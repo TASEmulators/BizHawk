cbdcea13413c0721ef3312c45b91fb4f2c8412fe
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-02-24 22:25:53 +0000

    Get started on a Movie class

---
+++ BizHawk.MultiClient/movie/Movie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BizHawk.MultiClient
+{
+    class Movie
+    {
+        private MovieHeader Header = new MovieHeader();
+        private MovieLog Log = new MovieLog();
+
+        public Movie()
+        {
+
+        }
+
+        public void AddMovieRecord()
+        {
+
+        }
+
+        public void WriteMovie()
+        {
+
+        }
+
+        public int GetMovieLength()
+        {
+            return Log.GetMovieLength();
+        }
+    }
+}
