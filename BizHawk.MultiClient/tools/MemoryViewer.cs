a8474b0a177e38752a45cf0a71b680a30a13debe
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-03-06 21:41:41 +0000

    Hex Editor - reduce flickering by creating a custom control for the memory viewer and setting useful flags

---
+++ BizHawk.MultiClient/tools/MemoryViewer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BizHawk.MultiClient
+{
+    public class MemoryViewer : GroupBox
+    {
+        public MemoryViewer()
+        {
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.DoubleBuffer, true);
+            //SetStyle(ControlStyles.Opaque, true);
+        }
+    }
+}
