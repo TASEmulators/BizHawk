0a61cc63a993b71a176641baeb87ba58bb884f61
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-03-07 02:47:13 +0000

    Hex Editor - refactor so scrollbar & paint logic are part of the MemoryViewer custom control.  Still a few bugs like no border or text display

--- trunk/BizHawk.MultiClient/tools/HexEditor.cs
+++ trunk/BizHawk.MultiClient/tools/HexEditor.cs
@@ -25,15 +25,12 @@ namespace BizHawk.MultiClient
         //Show num addresses in group box title (show "address" if 1 address)
         //big font for currently mouse over'ed value?
 
-        Font font = new Font("Courier New", 10);
-        Brush regBrush = Brushes.Black;
-        int RowsVisible = 0;
+        
 
-        const string HEADER = "       0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F";
+        
 
         int defaultWidth;
         int defaultHeight;
-        MemoryDomain Domain = new MemoryDomain("NULL", 1, Endian.Little, addr => 0, (a, v) => { });
         
         public HexEditor()
         {
@@ -70,34 +67,6 @@ namespace BizHawk.MultiClient
             this.Close();
         }
 
-        private void MemoryViewer_Paint(object sender, PaintEventArgs e)
-        {
-            unchecked
-            {
-                int row = 0;
-                int rowX = 8; 
-                int rowY = 16;
-                int rowYoffset = 20;
-                string rowStr;
-
-                e.Graphics.DrawString(HEADER, font, regBrush, new Point(rowX, rowY));
-                e.Graphics.DrawLine(new Pen(regBrush), MemoryViewer.Left + 38, MemoryViewer.Top, MemoryViewer.Left + 38, MemoryViewer.Bottom - 40);
-                e.Graphics.DrawLine(new Pen(regBrush), MemoryViewer.Left, 34, MemoryViewer.Right - 16, 34);
-
-                for (int i = 0; i < RowsVisible; i++)
-                {
-                    row = i+vScrollBar1.Value;
-                    rowStr = String.Format("{0:X4}", row*16) + "  "; //TODO: num digits based on size of domain
-                    for (int j = 0; j < 16; j++)
-                    {
-                        rowStr += String.Format("{0:X2}", Domain.PeekByte((row*16)+j)) + " "; //TODO: format based on data size
-                    }
-
-                    e.Graphics.DrawString(rowStr, font, regBrush, new Point(rowX, (rowY*(i+1))+rowYoffset));
-                }
-            }
-        }
-
         public void UpdateValues()
         {
             if (!this.IsHandleCreated || this.IsDisposed) return;
@@ -107,7 +76,8 @@ namespace BizHawk.MultiClient
         public void Restart()
         {
             SetMemoryDomainMenu(); //Calls update routines
-            SetUpScrollBar();
+            MemoryViewer.ResetScrollBar();
+            MemoryViewer.SetUpScrollBar();
             MemoryViewer.Refresh();
         }
 
@@ -130,17 +100,17 @@ namespace BizHawk.MultiClient
         {
             if (pos < Global.Emulator.MemoryDomains.Count)  //Sanity check
             {
-                Domain = Global.Emulator.MemoryDomains[pos];
+                MemoryViewer.SetMemoryDomain(Global.Emulator.MemoryDomains[pos]);
             }
             UpdateDomainString();
-            SetUpScrollBar();
-            vScrollBar1.Value = 0;
+            MemoryViewer.ResetScrollBar();
+            MemoryViewer.SetUpScrollBar();
             MemoryViewer.Refresh();
         }
 
         private void UpdateDomainString()
         {
-            string memoryDomain = Domain.ToString();
+            string memoryDomain = MemoryViewer.GetMemoryDomainStr();
             string systemID = Global.Emulator.SystemId;
             MemoryViewer.Text = systemID + " " + memoryDomain;
         }
@@ -169,10 +139,6 @@ namespace BizHawk.MultiClient
             }
             else
                 memoryDomainsToolStripMenuItem.Enabled = false;
-
-            SetUpScrollBar();
-            vScrollBar1.Value = 0;
-            MemoryViewer.Refresh();
         }
 
         private void goToAddressToolStripMenuItem_Click(object sender, EventArgs e)
@@ -180,34 +146,14 @@ namespace BizHawk.MultiClient
             //TODO
         }
 
-        private void SetUpScrollBar()
-        {
-            RowsVisible = MemoryViewer.Height / 16;
-            int totalRows = Domain.Size / 16;
-            int MaxRows = (totalRows - RowsVisible) + 17;
-
-            if (MaxRows > 0)
-            {
-                vScrollBar1.Visible = true;
-                if (vScrollBar1.Value > MaxRows)
-                    vScrollBar1.Value = MaxRows;
-                vScrollBar1.Maximum = MaxRows;
-            }
-            else
-                vScrollBar1.Visible = false;
-
-        }
+        
 
         private void HexEditor_Resize(object sender, EventArgs e)
         {
-            SetUpScrollBar();
+            MemoryViewer.SetUpScrollBar();
             MemoryViewer.Refresh();
         }
 
-        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
-        {
-            SetUpScrollBar();
-            MemoryViewer.Refresh();
-        }
+        
     }
 }
