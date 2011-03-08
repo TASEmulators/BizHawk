c51928364951291cfdc9054dc57c7f9cded4495a
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-03-08 19:05:23 +0000

    NESPPU - hook up windows position saving & autoload, and hook up UpdateValues to frame loop

--- trunk/BizHawk.MultiClient/NEStools/NESPPU.cs
+++ trunk/BizHawk.MultiClient/NEStools/NESPPU.cs
@@ -12,19 +12,41 @@ namespace BizHawk.MultiClient
 {
     public partial class NESPPU : Form
     {
+        int defaultWidth;     //For saving the default size of the dialog, so the user can restore if desired
+        int defaultHeight;
+
         public NESPPU()
         {
             InitializeComponent();
+            Closing += (o, e) => SaveConfigSettings(); 
+        }
+
+        private void SaveConfigSettings()
+        {
+            Global.Config.NESPPUWndx = this.Location.X;
+            Global.Config.NESPPUWndy = this.Location.Y;
+        }
+
+        private void LoadConfigSettings()
+        {
+            defaultWidth = Size.Width;     //Save these first so that the user can restore to its original size
+            defaultHeight = Size.Height;
+
+            if (Global.Config.NESPPUWndx >= 0 && Global.Config.NESPPUWndy >= 0)
+                Location = new Point(Global.Config.NESPPUWndx, Global.Config.NESPPUWndy);
+
         }
 
         public void UpdateValues()
         {
             if (!(Global.Emulator is NES)) return;
+            if (!this.IsHandleCreated || this.IsDisposed) return;
+            PaletteView.Refresh();
         }
 
         private void NESPPU_Load(object sender, EventArgs e)
         {
-            
+            LoadConfigSettings();
         }
     }
 }
