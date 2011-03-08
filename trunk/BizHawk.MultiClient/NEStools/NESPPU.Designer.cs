75895793265399fc7b470b3565dfa7d06f4f9c0f
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-03-08 16:43:08 +0000

    Start a PaletteViewer control for the NES PPU Viewer dialog

--- trunk/BizHawk.MultiClient/NEStools/NESPPU.Designer.cs
+++ trunk/BizHawk.MultiClient/NEStools/NESPPU.Designer.cs
@@ -29,7 +29,9 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.PalettesGroups = new System.Windows.Forms.GroupBox();
+            this.PalettesGroup = new System.Windows.Forms.GroupBox();
+            this.PaletteView = new PaletteViewer();
+            this.PalettesGroup.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -41,25 +43,34 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Pattern Tables";
             // 
-            // PalettesGroups
+            // PalettesGroup
             // 
-            this.PalettesGroups.Location = new System.Drawing.Point(12, 262);
-            this.PalettesGroups.Name = "PalettesGroups";
-            this.PalettesGroups.Size = new System.Drawing.Size(415, 84);
-            this.PalettesGroups.TabIndex = 1;
-            this.PalettesGroups.TabStop = false;
-            this.PalettesGroups.Text = "Palettes";
+            this.PalettesGroup.Controls.Add(this.PaletteView);
+            this.PalettesGroup.Location = new System.Drawing.Point(12, 262);
+            this.PalettesGroup.Name = "PalettesGroup";
+            this.PalettesGroup.Size = new System.Drawing.Size(415, 84);
+            this.PalettesGroup.TabIndex = 1;
+            this.PalettesGroup.TabStop = false;
+            this.PalettesGroup.Text = "Palettes";
+            // 
+            // PaletteView
+            // 
+            this.PaletteView.Location = new System.Drawing.Point(69, 35);
+            this.PaletteView.Name = "PaletteView";
+            this.PaletteView.TabIndex = 0;
             // 
             // NESPPU
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(443, 359);
-            this.Controls.Add(this.PalettesGroups);
+            this.Controls.Add(this.PalettesGroup);
             this.Controls.Add(this.groupBox1);
             this.Name = "NESPPU";
             this.Text = "PPU Viewer";
             this.Load += new System.EventHandler(this.NESPPU_Load);
+            this.PalettesGroup.ResumeLayout(false);
+            this.PalettesGroup.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -67,6 +78,7 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.GroupBox PalettesGroups;
+        private System.Windows.Forms.GroupBox PalettesGroup;
+        private PaletteViewer PaletteView;
     }
 }
