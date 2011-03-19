759fc88eae6e4d09aed898c39352b35494996321
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-03-19 01:44:16 +0000

    Cheat Window - cheats works now!

--- BizHawk.MultiClient/tools/RamWatch.cs
+++ BizHawk.MultiClient/tools/RamWatch.cs
@@ -22,7 +22,6 @@ namespace BizHawk.MultiClient
         //address num digits based on domain size
         //Restore window size should restore column order as well
         //When receiving a watch from a different domain, should something be done?
-        //Save should default to SaveAs if no watch file
 
         int defaultWidth;     //For saving the default size of the dialog, so the user can restore if desired
         int defaultHeight;
