ae8b4a3576c6567ffbfa29c213094b8daec1ffdd
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-04-07 01:30:42 +0000

    Basic functionality of the Tool Box, binded to the T key

--- BizHawk.MultiClient/tools/ToolBox.Designer.cs
+++ BizHawk.MultiClient/tools/ToolBox.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace BizHawk.MultiClient.tools
+﻿namespace BizHawk.MultiClient
 {
     partial class ToolBox
     {
@@ -29,20 +29,88 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ToolBox));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.toolStripButton4});
+            this.toolStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.Table;
+            this.toolStrip1.Location = new System.Drawing.Point(9, 27);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(85, 111);
+            this.toolStrip1.TabIndex = 0;
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Image = global::BizHawk.MultiClient.Properties.Resources.cheat;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(74, 20);
+            this.toolStripButton1.Text = "Debugger";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.Image = global::BizHawk.MultiClient.Properties.Resources.FindHS;
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(82, 20);
+            this.toolStripButton2.Text = "Ram Watch";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.Image = global::BizHawk.MultiClient.Properties.Resources.search;
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(84, 20);
+            this.toolStripButton3.Text = "Ram Search";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.Image = global::BizHawk.MultiClient.Properties.Resources.poke;
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(74, 20);
+            this.toolStripButton4.Text = "Ram Poke";
+            this.toolStripButton4.Click += new System.EventHandler(this.toolStripButton4_Click);
             // 
             // ToolBox
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.ClientSize = new System.Drawing.Size(123, 297);
+            this.Controls.Add(this.toolStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ToolBox";
             this.Text = "Tool Box";
+            this.Load += new System.EventHandler(this.ToolBox_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+
     }
 }
--- BizHawk.MultiClient/tools/ToolBox.cs
+++ BizHawk.MultiClient/tools/ToolBox.cs
@@ -7,13 +7,41 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
-namespace BizHawk.MultiClient.tools
+namespace BizHawk.MultiClient
 {
     public partial class ToolBox : Form
     {
         public ToolBox()
         {
             InitializeComponent();
+        }
+
+        private void ToolBox_Load(object sender, EventArgs e)
+        {
+            int x = Global.MainForm.Location.X + Global.MainForm.Size.Width;
+            int y = Global.MainForm.Location.Y;
+            Location = new Point(x, y);
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            Global.MainForm.LoadCheatsWindow();
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            Global.MainForm.LoadRamWatch();
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            Global.MainForm.LoadRamSearch();
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+            RamPoke r = new RamPoke();
+            r.Show();
         }
     }
 }
