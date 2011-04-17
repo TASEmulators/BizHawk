40cd085509d7d4e8e5bf417e97ae1375fa61b381
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-04-17 21:39:35 +0000

    Initial checkin of Jaleco-JF_11_14 (mapper 140)

---
+++ BizHawk.Emulation/Consoles/Nintendo/NES/Boards/Jaleco-JF_11_14.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace BizHawk.Emulation.Consoles.Nintendo
+{
+    /*
+     * Life Span: October 1986 - April 1987
+PCB Class: Jaleco-JF-11
+           Jaleco-JF-14
+iNES Mapper #140
+
+JF-11
+PRG-ROM: 128kb
+CHR-ROM: 32kb
+Battery is not available
+Uses vertical mirroring
+No CIC present
+Other chips used: Sunsoft-1
+     * 
+     * Games:
+     * Bio Senshi Dan - Increaser Tono Tatakai
+     */
+
+    class Jaleco_JF_11_14 : NES.NESBoardBase
+    {
+        
+        public override bool Configure(NES.EDetectionOrigin origin)
+        {
+            //configure
+			switch (Cart.board_type)
+			{
+				case "JALECO-JF-14":
+					break;
+				default:
+					return false;
+			}
+            SetMirrorType(Cart.pad_h, Cart.pad_v);
+            return true;
+        }
+
+        public override byte ReadPPU(int addr)
+        {
+            return base.ReadPPU(addr);
+        }
+
+        public override void SyncStateBinary(BinarySerializer ser)
+        {
+            base.SyncStateBinary(ser);
+        }
+    }
+}
