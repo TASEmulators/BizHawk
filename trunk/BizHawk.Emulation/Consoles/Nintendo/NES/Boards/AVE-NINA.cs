37588f40dc5855ef12171e3ffddc4b4959ce03e2
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-04-18 23:55:47 +0000

    NES - Start AVE-NINA class

---
+++ trunk/BizHawk.Emulation/Consoles/Nintendo/NES/Boards/AVE-NINA.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace BizHawk.Emulation.Consoles.Nintendo
+{
+    /*
+    
+     */
+
+    class AVI_NINA_001 : NES.NESBoardBase
+    {
+        public override bool Configure(NES.EDetectionOrigin origin)
+        {
+            //configure
+			switch (Cart.board_type)
+			{
+				case "AVI_NINA_001":
+					break;
+				default:
+					return false;
+			}
+            SetMirrorType(Cart.pad_h, Cart.pad_v);
+            return true;
+        }
+
+        public override byte ReadPPU(int addr)
+        {
+            return base.ReadPPU(addr);
+        }
+
+		public override void SyncState(Serializer ser)
+        {
+			base.SyncState(ser);
+        }
+    }
+    
+    /*
+    */
+    class AVI_Misc : NES.NESBoardBase
+    {
+        public override bool Configure(NES.EDetectionOrigin origin)
+        {
+            //configure
+            switch (Cart.board_type)
+            {
+                case "AVI_Misc":
+                    break;
+                default:
+                    return false;
+            }
+            SetMirrorType(Cart.pad_h, Cart.pad_v);
+            return true;
+        }
+
+        public override byte ReadPPU(int addr)
+        {
+            return base.ReadPPU(addr);
+        }
+
+        public override void SyncState(Serializer ser)
+        {
+            base.SyncState(ser);
+        }
+    }
+}
