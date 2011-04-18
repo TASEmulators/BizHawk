e51f2efbc6dfeaa9620d87c914ca586da534987d
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-04-18 02:26:42 +0000

    Start mapper 242

---
+++ trunk/BizHawk.Emulation/Consoles/Nintendo/NES/Boards/Mapper242.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace BizHawk.Emulation.Consoles.Nintendo
+{
+    /*
+PCB Class: Unknown
+iNES Mapper #242
+PRG-ROM: 32KB
+PRG-RAM: None
+CHR-ROM: 16KB
+CHR-RAM: None
+Battery is not available
+mirroring - both
+     * 
+     * Games:
+     * Wai Xing Zhan Shi (Ch)
+     */
+
+    class Mapper242 : NES.NESBoardBase
+    {
+
+        public override bool Configure(NES.EDetectionOrigin origin)
+        {
+            //configure
+			switch (Cart.board_type)
+			{
+				case "Mapper242":
+					break;
+				default:
+					return false;
+			}
+            return true;
+        }
+
+        public override byte ReadPPU(int addr)
+        {
+            return base.ReadPPU(addr);
+        }
+
+        public override byte ReadPRG(int addr)
+        {
+            return base.ReadPRG(addr);
+        }
+
+        public override void WriteWRAM(int addr, byte value)
+        {
+
+            base.WriteWRAM(addr, value);
+        }
+
+		public override void SyncState(Serializer ser)
+        {
+			base.SyncState(ser);
+        }
+    }
+}
