a066d6e03d74d541c1dcfe3be8179d24c15b3aac
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-04-18 22:33:06 +0000

    Mapper 242 stuff

--- BizHawk.Emulation/Consoles/Nintendo/NES/Boards/Mapper242.cs
+++ BizHawk.Emulation/Consoles/Nintendo/NES/Boards/Mapper242.cs
@@ -21,6 +21,15 @@ mirroring - both
     class Mapper242 : NES.NESBoardBase
     {
         int prg, mirror;
+        
+        public enum EMirrorType
+        {
+            Vertical, Horizontal,
+            OneScreenA, OneScreenB,
+            //unknown or controlled by the board
+            External
+        }
+
         public override bool Configure(NES.EDetectionOrigin origin)
         {
             //configure
@@ -36,19 +45,22 @@ mirroring - both
 
         public override byte ReadPPU(int addr)
         {
-            //SetMirroring(mirror, mirror, 0, 0); //?? TODO
+            if (mirror == 0)
+                SetMirrorType(0, 1);
+            else
+                SetMirrorType(1, 0);
+            SetMirrorType(Cart.pad_h, Cart.pad_v);
             return base.ReadPPU(addr);
         }
 
         public override byte ReadPRG(int addr)
         {
-            return VROM[addr + (prg * 0x8000)];
-            //return base.ReadPRG(addr);
+            return ROM[addr + (prg * 0x8000)];
         }
 
         public override void WriteWRAM(int addr, byte value)
         {
-            mirror = (addr & 0x01);
+            int mirror = (addr & 0x01);
             prg = (addr & 0x7F) >> 3;
             base.WriteWRAM(addr, value);
         }
