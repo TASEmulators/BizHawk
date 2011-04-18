ded0d906d7110d52f920f834db3b66a756c33f3e
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-04-18 11:02:07 +0000

    Checking in some mapper 242 stuff

--- BizHawk.Emulation/Consoles/Nintendo/NES/Boards/Mapper242.cs
+++ BizHawk.Emulation/Consoles/Nintendo/NES/Boards/Mapper242.cs
@@ -20,7 +20,7 @@ mirroring - both
 
     class Mapper242 : NES.NESBoardBase
     {
-
+        int prg, mirror;
         public override bool Configure(NES.EDetectionOrigin origin)
         {
             //configure
@@ -36,23 +36,28 @@ mirroring - both
 
         public override byte ReadPPU(int addr)
         {
+            //SetMirroring(mirror, mirror, 0, 0); //?? TODO
             return base.ReadPPU(addr);
         }
 
         public override byte ReadPRG(int addr)
         {
-            return base.ReadPRG(addr);
+            return VROM[addr + (prg * 0x8000)];
+            //return base.ReadPRG(addr);
         }
 
         public override void WriteWRAM(int addr, byte value)
         {
-
+            mirror = (addr & 0x01);
+            prg = (addr & 0x7F) >> 3;
             base.WriteWRAM(addr, value);
         }
 
 		public override void SyncState(Serializer ser)
         {
 			base.SyncState(ser);
+            ser.Sync("prg", ref prg);
+            ser.Sync("mirror", ref mirror);
         }
     }
 }
