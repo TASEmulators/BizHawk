ff0e7194e441fcf68ecf88b8a90ebf01c388af7c
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-04-30 21:19:11 +0000

    Toolbox - add emulator dependent tools to toolbox.

--- trunk/BizHawk.MultiClient/tools/ToolBox.cs
+++ trunk/BizHawk.MultiClient/tools/ToolBox.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using BizHawk.Emulation.Consoles.Nintendo;
+using BizHawk.Emulation.Consoles.Calculator;
 
 namespace BizHawk.MultiClient
 {
@@ -21,6 +23,28 @@ namespace BizHawk.MultiClient
             int x = Global.MainForm.Location.X + Global.MainForm.Size.Width;
             int y = Global.MainForm.Location.Y;
             Location = new Point(x, y);
+            if (Global.Emulator is NES)
+            {
+                NESPPU.Visible = true;
+                NESDebugger.Visible = true;
+                NESGameGenie.Visible = true;
+                NESNameTable.Visible = true;
+            }
+            else
+            {
+                NESPPU.Visible = false;
+                NESDebugger.Visible = false;
+                NESGameGenie.Visible = false;
+                NESNameTable.Visible = false;
+            }
+            if (Global.Emulator is TI83)
+            {
+
+            }
+            else
+            {
+
+            }
         }
 
         private void toolStripButton1_Click(object sender, EventArgs e)
@@ -53,6 +77,33 @@ namespace BizHawk.MultiClient
         {
             var window = new BizHawk.MultiClient.tools.LuaWindow();
             window.Show();
+        }
+
+        private void NESPPU_Click(object sender, EventArgs e)
+        {
+            Global.MainForm.LoadNESPPU();
+        }
+
+        private void NESDebugger_Click(object sender, EventArgs e)
+        {
+            Global.MainForm.LoadNESDebugger();
+        }
+
+        private void NESGameGenie_Click(object sender, EventArgs e)
+        {
+            Global.MainForm.LoadGameGenieEC();
+        }
+
+        private void NESNameTable_Click(object sender, EventArgs e)
+        {
+            Global.MainForm.LoadNESNameTable();
+        }
+
+        private void toolStripButton6_Click(object sender, EventArgs e)
+        {
+            if (!(Global.Emulator is TI83))
+                return;
+            Global.MainForm.LoadTI83KeyPad();
         }
     }
 }
