99ade3b3df9ae42de7594f65b243f5cf0655bf70
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-05-06 23:08:50 +0000

    Progress on lua console window

---
+++ trunk/BizHawk.MultiClient/tools/LuaFiles.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BizHawk.MultiClient
+{
+    class LuaFiles
+    {
+        public string Name;
+        public string Path;
+        public bool Enabled;
+
+        public LuaFiles(string path)
+        {
+            Name = "";
+            Path = path;
+            Enabled = true;
+        }
+
+        public LuaFiles(string name, string path, bool enabled)
+        {
+            Name = name;
+            Path = path;
+            Enabled = enabled;
+        }
+
+        public LuaFiles(LuaFiles l)
+        {
+            Name = l.Name;
+            Path = l.Path;
+            Enabled = l.Enabled;
+        }
+
+        public void Toggle()
+        {
+            Enabled ^= true;
+        }
+    }
+}
