41e836f9a77606d4da9c58e69bbead0545dd08c3
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-05-07 01:17:19 +0000

    start a LuaText object

---
+++ trunk/BizHawk.MultiClient/tools/LuaText.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using Font = SlimDX.Direct3D9.Font;
+using System.Drawing;
+using SlimDX;
+using SlimDX.Direct3D9;
+
+namespace BizHawk.MultiClient.tools
+{
+    class LuaText
+    {
+        public int x;
+        public int y;
+//        public Font font;
+        public Color color;
+        public Color outline;
+        public String message;
+        private Device device;
+
+        public LuaText()
+        {
+            x = 0;
+            y = 0;
+//            font = new Font(device, 16, 0, FontWeight.Bold, 1, false, CharacterSet.Default, Precision.Default, FontQuality.Default, PitchAndFamily.Default | PitchAndFamily.DontCare, "Arial"); 
+            color = Color.White;
+            outline = Color.Black;
+            message = "";
+        }
+    }
+}
