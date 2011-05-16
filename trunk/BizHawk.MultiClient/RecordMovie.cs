536b1d89db85f62cf2c526aacdeafa699baaf1aa
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-05-16 00:56:16 +0000

    Implement record movie dialog enough to choose a movie and begin recording.  Needs a way to set header info & settings

--- trunk/BizHawk.MultiClient/RecordMovie.cs
+++ trunk/BizHawk.MultiClient/RecordMovie.cs
@@ -6,14 +6,53 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
 
 namespace BizHawk.MultiClient
 {
     public partial class RecordMovie : Form
     {
+        Movie MovieToRecord;
+
         public RecordMovie()
         {
             InitializeComponent();
+        }
+
+        private void OK_Click(object sender, EventArgs e)
+        {
+            Global.MainForm.UserMovie = MovieToRecord;
+            Global.MainForm.UserMovie.StartNewRecording();
+            this.Close();
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.InitialDirectory = PathManager.MakeAbsolutePath(Global.Config.MoviesPath, "");
+            sfd.DefaultExt = ".tas";
+            sfd.FileName = Global.Game.Name;
+            sfd.Filter = "Movie files (*.tas)|*.TAS";
+
+            Global.Sound.StopSound();
+            var result = sfd.ShowDialog();
+            Global.Sound.StartSound();
+            if (result == DialogResult.OK)
+            {
+                var file = new FileInfo(sfd.FileName);
+                MovieToRecord = new Movie(sfd.FileName, MOVIEMODE.RECORD);
+                RecordBox.Text = sfd.FileName;
+            }
+        }
+
+        private void RecordMovie_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
