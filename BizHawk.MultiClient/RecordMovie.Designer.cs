24a713ee1ff8c468a122762f526985c6b7afb17b
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-05-16 00:56:16 +0000

    Implement record movie dialog enough to choose a movie and begin recording.  Needs a way to set header info & settings

--- BizHawk.MultiClient/RecordMovie.Designer.cs
+++ BizHawk.MultiClient/RecordMovie.Designer.cs
@@ -28,30 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RecordMovie));
             this.Cancel = new System.Windows.Forms.Button();
             this.OK = new System.Windows.Forms.Button();
+            this.Browse = new System.Windows.Forms.Button();
+            this.RecordBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // Cancel
             // 
             this.Cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.Cancel.Location = new System.Drawing.Point(200, 238);
+            this.Cancel.Location = new System.Drawing.Point(282, 238);
             this.Cancel.Name = "Cancel";
             this.Cancel.Size = new System.Drawing.Size(75, 23);
             this.Cancel.TabIndex = 0;
             this.Cancel.Text = "&Cancel";
             this.Cancel.UseVisualStyleBackColor = true;
+            this.Cancel.Click += new System.EventHandler(this.Cancel_Click);
             // 
             // OK
             // 
             this.OK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.OK.Location = new System.Drawing.Point(108, 238);
+            this.OK.Location = new System.Drawing.Point(190, 238);
             this.OK.Name = "OK";
             this.OK.Size = new System.Drawing.Size(75, 23);
             this.OK.TabIndex = 1;
             this.OK.Text = "&Ok";
             this.OK.UseVisualStyleBackColor = true;
+            this.OK.Click += new System.EventHandler(this.OK_Click);
+            // 
+            // Browse
+            // 
+            this.Browse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Browse.Location = new System.Drawing.Point(300, 40);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(62, 23);
+            this.Browse.TabIndex = 2;
+            this.Browse.Text = "&Browse...";
+            this.Browse.UseVisualStyleBackColor = true;
+            this.Browse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // RecordBox
+            // 
+            this.RecordBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.RecordBox.Location = new System.Drawing.Point(13, 40);
+            this.RecordBox.Name = "RecordBox";
+            this.RecordBox.Size = new System.Drawing.Size(277, 20);
+            this.RecordBox.TabIndex = 3;
             // 
             // RecordMovie
             // 
@@ -59,14 +84,19 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.Cancel;
-            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.ClientSize = new System.Drawing.Size(374, 273);
+            this.Controls.Add(this.RecordBox);
+            this.Controls.Add(this.Browse);
             this.Controls.Add(this.OK);
             this.Controls.Add(this.Cancel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "RecordMovie";
             this.Text = "Record Movie";
+            this.Load += new System.EventHandler(this.RecordMovie_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -74,5 +104,7 @@
 
         private System.Windows.Forms.Button Cancel;
         private System.Windows.Forms.Button OK;
+        private System.Windows.Forms.Button Browse;
+        private System.Windows.Forms.TextBox RecordBox;
     }
 }
