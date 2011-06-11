f052b0bd714a6b8f84565a952295de4d6bb3d3da
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-06-11 16:02:28 +0000

    Add palette files to MakeRelease.bat

--- BizHawk.MultiClient/output/MakeRelease.bat
+++ BizHawk.MultiClient/output/MakeRelease.bat
@@ -1,2 +1,2 @@
 del /s BizHawk.zip
-zip -X -9 -r BizHawk.zip BizHawk.MultiClient.exe DiscoHawk.exe *.dll  NesCarts.7z gamedb.txt gamedb_neshomebrew.txt
+zip -X -9 -r BizHawk.zip BizHawk.MultiClient.exe DiscoHawk.exe *.dll  NesCarts.7z gamedb.txt gamedb_neshomebrew.txt NES\Palettes
