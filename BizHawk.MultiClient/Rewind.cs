38ca651aab2521ac2a4f09f323e3f68d60cdc303
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-06-12 00:14:19 +0000

    TAStudio - more misc stuff

--- BizHawk.MultiClient/Rewind.cs
+++ BizHawk.MultiClient/Rewind.cs
@@ -2,173 +2,173 @@
 
 namespace BizHawk.MultiClient
 {
-    public partial class MainForm
-    {
-        private MruStack<MemoryStream> RewindBuf = new MruStack<MemoryStream>(15000);
-        private byte[] LastState;
+	public partial class MainForm
+	{
+		private MruStack<MemoryStream> RewindBuf = new MruStack<MemoryStream>(15000);
+		private byte[] LastState;
 
-        private void CaptureRewindState()
-        {
-            if (LastState == null)
-            {
-                // This is the first frame. Capture the state, and put it in LastState for future deltas to be compared against.
-                LastState = Global.Emulator.SaveStateBinary();
-                //System.Console.WriteLine(LastState.Length);
-                return;
-            }
+		private void CaptureRewindState()
+		{
+			if (LastState == null)
+			{
+				// This is the first frame. Capture the state, and put it in LastState for future deltas to be compared against.
+				LastState = Global.Emulator.SaveStateBinary();
+				//System.Console.WriteLine(LastState.Length);
+				return;
+			}
 
-            // Otherwise, it's not the first frame, so build a delta.
-            if (LastState.Length <= 0x10000)
-                CaptureRewindState64K();
-            else
-                CaptureRewindStateLarge();
-        }
+			// Otherwise, it's not the first frame, so build a delta.
+			if (LastState.Length <= 0x10000)
+				CaptureRewindState64K();
+			else
+				CaptureRewindStateLarge();
+		}
 
-        /// <summary>
-        /// Builds a delta for states that are <= 64K in size.
-        /// </summary>
-        private void CaptureRewindState64K()
-        {
-            byte[] CurrentState = Global.Emulator.SaveStateBinary();
-            int beginChangeSequence = -1;
-            bool inChangeSequence = false;
-            var ms = new MemoryStream();
-            var writer = new BinaryWriter(ms);
-            for (int i = 0; i < CurrentState.Length; i++)
-            {
-                if (inChangeSequence == false)
-                {
+		/// <summary>
+		/// Builds a delta for states that are <= 64K in size.
+		/// </summary>
+		private void CaptureRewindState64K()
+		{
+			byte[] CurrentState = Global.Emulator.SaveStateBinary();
+			int beginChangeSequence = -1;
+			bool inChangeSequence = false;
+			var ms = new MemoryStream();
+			var writer = new BinaryWriter(ms);
+			for (int i = 0; i < CurrentState.Length; i++)
+			{
+				if (inChangeSequence == false)
+				{
 					if (i >= LastState.Length)
 						continue;
-                    if (CurrentState[i] == LastState[i])
-                        continue;
+					if (CurrentState[i] == LastState[i])
+						continue;
 
-                    inChangeSequence = true;
-                    beginChangeSequence = i;
-                    continue;
-                }
+					inChangeSequence = true;
+					beginChangeSequence = i;
+					continue;
+				}
 
-                if (i - beginChangeSequence == 254 || i == CurrentState.Length - 1)
-                {
-                    writer.Write((byte)(i - beginChangeSequence + 1));
-                    writer.Write((ushort)beginChangeSequence);
-                    writer.Write(LastState, beginChangeSequence, i - beginChangeSequence + 1);
-                    inChangeSequence = false;
-                    continue;
-                }
+				if (i - beginChangeSequence == 254 || i == CurrentState.Length - 1)
+				{
+					writer.Write((byte)(i - beginChangeSequence + 1));
+					writer.Write((ushort)beginChangeSequence);
+					writer.Write(LastState, beginChangeSequence, i - beginChangeSequence + 1);
+					inChangeSequence = false;
+					continue;
+				}
 
-                if (CurrentState[i] == LastState[i])
-                {
-                    writer.Write((byte)(i - beginChangeSequence));
-                    writer.Write((ushort)beginChangeSequence);
-                    writer.Write(LastState, beginChangeSequence, i - beginChangeSequence);
-                    inChangeSequence = false;
-                }
-            }
-            LastState = CurrentState;
-            ms.Position = 0;
-            RewindBuf.Push(ms);
-        }
+				if (CurrentState[i] == LastState[i])
+				{
+					writer.Write((byte)(i - beginChangeSequence));
+					writer.Write((ushort)beginChangeSequence);
+					writer.Write(LastState, beginChangeSequence, i - beginChangeSequence);
+					inChangeSequence = false;
+				}
+			}
+			LastState = CurrentState;
+			ms.Position = 0;
+			RewindBuf.Push(ms);
+		}
 
-        /// <summary>
-        /// Builds a delta for states that are > 64K in size.
-        /// </summary>
-        private void CaptureRewindStateLarge()
-        {
-            byte[] CurrentState = Global.Emulator.SaveStateBinary();
-            int beginChangeSequence = -1;
-            bool inChangeSequence = false;
-            var ms = new MemoryStream();
-            var writer = new BinaryWriter(ms);
-            for (int i = 0; i < CurrentState.Length; i++)
-            {
-                if (inChangeSequence == false)
-                {
+		/// <summary>
+		/// Builds a delta for states that are > 64K in size.
+		/// </summary>
+		private void CaptureRewindStateLarge()
+		{
+			byte[] CurrentState = Global.Emulator.SaveStateBinary();
+			int beginChangeSequence = -1;
+			bool inChangeSequence = false;
+			var ms = new MemoryStream();
+			var writer = new BinaryWriter(ms);
+			for (int i = 0; i < CurrentState.Length; i++)
+			{
+				if (inChangeSequence == false)
+				{
 					if (i >= LastState.Length)
 						continue;
-                    if (CurrentState[i] == LastState[i])
-                        continue;
+					if (CurrentState[i] == LastState[i])
+						continue;
 
-                    inChangeSequence = true;
-                    beginChangeSequence = i;
-                    continue;
-                }
+					inChangeSequence = true;
+					beginChangeSequence = i;
+					continue;
+				}
 
-                if (i - beginChangeSequence == 254 || i == CurrentState.Length - 1)
-                {
-                    writer.Write((byte)(i - beginChangeSequence + 1));
-                    writer.Write(beginChangeSequence);
-                    writer.Write(LastState, beginChangeSequence, i - beginChangeSequence + 1);
-                    inChangeSequence = false;
-                    continue;
-                }
+				if (i - beginChangeSequence == 254 || i == CurrentState.Length - 1)
+				{
+					writer.Write((byte)(i - beginChangeSequence + 1));
+					writer.Write(beginChangeSequence);
+					writer.Write(LastState, beginChangeSequence, i - beginChangeSequence + 1);
+					inChangeSequence = false;
+					continue;
+				}
 
-                if (CurrentState[i] == LastState[i])
-                {
-                    writer.Write((byte)(i - beginChangeSequence));
-                    writer.Write(beginChangeSequence);
-                    writer.Write(LastState, beginChangeSequence, i - beginChangeSequence);
-                    inChangeSequence = false;
-                }
-            }
-            LastState = CurrentState;
-            ms.Position = 0;
-            RewindBuf.Push(ms);
-        }
+				if (CurrentState[i] == LastState[i])
+				{
+					writer.Write((byte)(i - beginChangeSequence));
+					writer.Write(beginChangeSequence);
+					writer.Write(LastState, beginChangeSequence, i - beginChangeSequence);
+					inChangeSequence = false;
+				}
+			}
+			LastState = CurrentState;
+			ms.Position = 0;
+			RewindBuf.Push(ms);
+		}
 
-        private void Rewind64K()
-        {
-            var ms = RewindBuf.Pop();
-            var reader = new BinaryReader(ms);
-            var output = new MemoryStream(LastState);
-            while (ms.Position < ms.Length - 1)
-            {
-                byte len = reader.ReadByte();
-                ushort offset = reader.ReadUInt16();
-                output.Position = offset;
-                output.Write(ms.GetBuffer(), (int)ms.Position, len);
-                ms.Position += len;
-            }
-            reader.Close();
-            output.Position = 0;
-            Global.Emulator.LoadStateBinary(new BinaryReader(output));
-        }
+		private void Rewind64K()
+		{
+			var ms = RewindBuf.Pop();
+			var reader = new BinaryReader(ms);
+			var output = new MemoryStream(LastState);
+			while (ms.Position < ms.Length - 1)
+			{
+				byte len = reader.ReadByte();
+				ushort offset = reader.ReadUInt16();
+				output.Position = offset;
+				output.Write(ms.GetBuffer(), (int)ms.Position, len);
+				ms.Position += len;
+			}
+			reader.Close();
+			output.Position = 0;
+			Global.Emulator.LoadStateBinary(new BinaryReader(output));
+		}
 
-        private void RewindLarge()
-        {
-            var ms = RewindBuf.Pop();
-            var reader = new BinaryReader(ms);
-            var output = new MemoryStream(LastState);
-            while (ms.Position < ms.Length - 1)
-            {
-                byte len = reader.ReadByte();
-                int offset = reader.ReadInt32();
-                output.Position = offset;
-                output.Write(ms.GetBuffer(), (int)ms.Position, len);
-                ms.Position += len;
-            }
-            reader.Close();
-            output.Position = 0;
-            Global.Emulator.LoadStateBinary(new BinaryReader(output));
-        }
+		private void RewindLarge()
+		{
+			var ms = RewindBuf.Pop();
+			var reader = new BinaryReader(ms);
+			var output = new MemoryStream(LastState);
+			while (ms.Position < ms.Length - 1)
+			{
+				byte len = reader.ReadByte();
+				int offset = reader.ReadInt32();
+				output.Position = offset;
+				output.Write(ms.GetBuffer(), (int)ms.Position, len);
+				ms.Position += len;
+			}
+			reader.Close();
+			output.Position = 0;
+			Global.Emulator.LoadStateBinary(new BinaryReader(output));
+		}
 
-        private void Rewind(int frames)
-        {
-            for (int i = 0; i < frames; i++)
-            {
-                if (RewindBuf.Count == 0)
-                    return;
-                if (LastState.Length < 0x10000)
-                    Rewind64K();
-                else
-                    RewindLarge();
-            }
-        }
+		public void Rewind(int frames)
+		{
+			for (int i = 0; i < frames; i++)
+			{
+				if (RewindBuf.Count == 0)
+					return;
+				if (LastState.Length < 0x10000)
+					Rewind64K();
+				else
+					RewindLarge();
+			}
+		}
 
-        public void ResetRewindBuffer()
-        {
-            RewindBuf.Clear();
-            LastState = null;
-        }
-    }
+		public void ResetRewindBuffer()
+		{
+			RewindBuf.Clear();
+			LastState = null;
+		}
+	}
 }
--- BizHawk.MultiClient/config/SoundConfig.cs
+++ BizHawk.MultiClient/config/SoundConfig.cs
@@ -9,65 +9,65 @@ using System.Windows.Forms;
 
 namespace BizHawk.MultiClient
 {
-    public partial class SoundConfig : Form
-    {
-        public SoundConfig()
-        {
-            InitializeComponent();
-        }
+	public partial class SoundConfig : Form
+	{
+		public SoundConfig()
+		{
+			InitializeComponent();
+		}
 
-        private void SoundConfig_Load(object sender, EventArgs e)
-        {
-            SoundOnCheckBox.Checked = Global.Config.SoundEnabled;
-            MuteFrameAdvance.Checked = Global.Config.MuteFrameAdvance;
-            SoundVolBar.Value = Global.Config.SoundVolume;
-            SoundVolNumeric.Value = Global.Config.SoundVolume;
-            UpdateSoundDialog();
-        }
+		private void SoundConfig_Load(object sender, EventArgs e)
+		{
+			SoundOnCheckBox.Checked = Global.Config.SoundEnabled;
+			MuteFrameAdvance.Checked = Global.Config.MuteFrameAdvance;
+			SoundVolBar.Value = Global.Config.SoundVolume;
+			SoundVolNumeric.Value = Global.Config.SoundVolume;
+			UpdateSoundDialog();
+		}
 
-        private void OK_Click(object sender, EventArgs e)
-        {
-            Global.Config.SoundEnabled = SoundOnCheckBox.Checked;
-            Global.Config.MuteFrameAdvance = MuteFrameAdvance.Checked;
-            Global.Config.SoundVolume = SoundVolBar.Value;
-            Global.Sound.ChangeVolume(Global.Config.SoundVolume);
-            Global.Sound.UpdateSoundSettings();
-            Global.Sound.StartSound();
-            this.Close();
-        }
+		private void OK_Click(object sender, EventArgs e)
+		{
+			Global.Config.SoundEnabled = SoundOnCheckBox.Checked;
+			Global.Config.MuteFrameAdvance = MuteFrameAdvance.Checked;
+			Global.Config.SoundVolume = SoundVolBar.Value;
+			Global.Sound.ChangeVolume(Global.Config.SoundVolume);
+			Global.Sound.UpdateSoundSettings();
+			Global.Sound.StartSound();
+			this.Close();
+		}
 
-        private void Cancel_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
+		private void Cancel_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
 
-        private void trackBar1_Scroll(object sender, EventArgs e)
-        {
-            SoundVolNumeric.Value = SoundVolBar.Value;
-        }
+		private void trackBar1_Scroll(object sender, EventArgs e)
+		{
+			SoundVolNumeric.Value = SoundVolBar.Value;
+		}
 
-        private void SoundVolNumeric_ValueChanged(object sender, EventArgs e)
-        {
-            SoundVolBar.Value = (int)SoundVolNumeric.Value;
-        }
+		private void SoundVolNumeric_ValueChanged(object sender, EventArgs e)
+		{
+			SoundVolBar.Value = (int)SoundVolNumeric.Value;
+		}
 
-        private void SoundOnCheckBox_CheckedChanged(object sender, EventArgs e)
-        {
-            UpdateSoundDialog();
-        }
+		private void SoundOnCheckBox_CheckedChanged(object sender, EventArgs e)
+		{
+			UpdateSoundDialog();
+		}
 
-        private void UpdateSoundDialog()
-        {
-            if (SoundOnCheckBox.Checked)
-            {
-                SoundVolGroup.Enabled = true;
-                MuteFrameAdvance.Enabled = true;
-            }
-            else
-            {
-                SoundVolGroup.Enabled = false;
-                MuteFrameAdvance.Enabled = false;
-            }
-        }
-    }
+		private void UpdateSoundDialog()
+		{
+			if (SoundOnCheckBox.Checked)
+			{
+				SoundVolGroup.Enabled = true;
+				MuteFrameAdvance.Enabled = true;
+			}
+			else
+			{
+				SoundVolGroup.Enabled = false;
+				MuteFrameAdvance.Enabled = false;
+			}
+		}
+	}
 }
