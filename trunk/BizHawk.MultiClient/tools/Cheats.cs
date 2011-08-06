8256aa832e15641e533a5431e12f5c530eba32e1
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-08-06 02:34:24 +0000

    Pull the cheat list and some related functions out of the Cheats winform and into a CheatList object.  Still some bugs to work out, but at least the cheat window doesn't crash when opened a 2nd time

--- trunk/BizHawk.MultiClient/tools/Cheats.cs
+++ trunk/BizHawk.MultiClient/tools/Cheats.cs
@@ -29,6 +29,7 @@ namespace BizHawk.MultiClient
 		string currentCheatFile = "";
 		bool changes = false;
 
+		//TODO: Delete me
 		public List<Cheat> GetCheatList()
 		{
 			List<Cheat> c = new List<Cheat>();
@@ -51,7 +52,7 @@ namespace BizHawk.MultiClient
 				return false;
 			else
 			{
-				LoadCheatFile(CheatFile, false);
+				Global.MainForm.CheatList.LoadCheatFile(CheatFile, false);
 				return true;
 			}
 		}
@@ -585,92 +586,18 @@ namespace BizHawk.MultiClient
 			return file;
 		}
 
-		private MemoryDomain SetDomain(string name)
-		{
-			//Attempts to find the memory domain by name, if it fails, it defaults to index 0
-			for (int x = 0; x < Global.Emulator.MemoryDomains.Count; x++)
-			{
-				if (Global.Emulator.MemoryDomains[x].Name == name)
-					return Global.Emulator.MemoryDomains[x];
-			}
-			return Global.Emulator.MemoryDomains[0];
-		}
+		
 
 		public bool LoadCheatFile(string path, bool append)
 		{
-			int y;
-			var file = new FileInfo(path);
-			if (file.Exists == false) return false;
-
-			using (StreamReader sr = file.OpenText())
-			{
-				if (!append) currentCheatFile = path;
-
-				string s = "";
-				string temp = "";
-
-				if (append == false)
-					cheatList.Clear();  //Wipe existing list and read from file
-
-				while ((s = sr.ReadLine()) != null)
-				{
-					if (s.Length < 6) continue;
-					Cheat c = new Cheat();
-					temp = s.Substring(0, s.IndexOf('\t'));   //Address
-					c.address = int.Parse(temp, NumberStyles.HexNumber);
-
-					y = s.IndexOf('\t') + 1;
-					s = s.Substring(y, s.Length - y);   //Value
-					temp = s.Substring(0, 2);
-					c.value = byte.Parse(temp, NumberStyles.HexNumber);
-
-					y = s.IndexOf('\t') + 1;
-					s = s.Substring(y, s.Length - y); //Memory Domain
-					temp = s.Substring(0, s.IndexOf('\t'));
-					c.domain = SetDomain(temp);
-
-					y = s.IndexOf('\t') + 1;
-					s = s.Substring(y, s.Length - y); //Enabled
-					y = int.Parse(s[0].ToString());
-
-					try
-					{
-						if (y == 0)
-							c.Disable();
-						else
-							c.Enable();
-					}
-					catch
-					{
-						NotSupportedError();
-					}
-
-					y = s.IndexOf('\t') + 1;
-					s = s.Substring(y, s.Length - y); //Name
-					c.name = s;
-
-					cheatList.Add(c);
-				}
-
-				Global.Config.RecentCheats.Add(file.FullName);
-				changes = false;
-				MessageLabel.Text = Path.GetFileName(file.FullName);
-				UpdateNumberOfCheats();
-			}
-
-			if (Global.Config.DisableCheatsOnLoad)
-			{
-				for (int x = 0; x < cheatList.Count; x++)
-					cheatList[x].Disable();
-			}
+			Global.MainForm.CheatList.LoadCheatFile(path, append);
+			UpdateNumberOfCheats();
+			MessageLabel.Text = Path.GetFileName(Global.MainForm.CheatList.currentCheatFile);
+			DisplayCheatsList();
 			return true; //TODO
 		}
 
-		private void NotSupportedError()
-		{
-			MessageBox.Show("Unable to enable cheat for this platform, cheats are not supported for " + Global.Emulator.SystemId, "Cheat error",
-							MessageBoxButtons.OK, MessageBoxIcon.Error);
-		}
+		
 
 		private void OpenCheatFile()
 		{
@@ -682,7 +609,6 @@ namespace BizHawk.MultiClient
 				if (r)
 				{
 					LoadCheatFile(file.FullName, false);
-					DisplayCheatsList();
 				}
 			}
 		}
@@ -737,7 +663,7 @@ namespace BizHawk.MultiClient
 				c.Enable();
 			} 
 			catch {
-				NotSupportedError();
+				Global.MainForm.CheatList.NotSupportedError();
 			}
 			return c;
 		}
@@ -855,7 +781,7 @@ namespace BizHawk.MultiClient
 						}
 						catch
 						{
-							NotSupportedError();
+							Global.MainForm.CheatList.NotSupportedError();
 						}
 					}
 				}
@@ -969,17 +895,7 @@ namespace BizHawk.MultiClient
 			CheatListView.Columns[4].Width = defaultOnWidth;
 		}
 
-		public bool IsActiveCheat(MemoryDomain d, int address)
-		{
-			for (int x = 0; x < cheatList.Count; x++)
-			{
-				if (cheatList[x].address == address && cheatList[x].domain.Name == d.Name)
-				{
-					return true;
-				}
-			}
-			return false;
-		}
+		
 
 		private void exitToolStripMenuItem_Click(object sender, EventArgs e)
 		{
