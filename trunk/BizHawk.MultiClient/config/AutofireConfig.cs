2906514c3022093f2486277c8fea4af043a42419
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-08-09 23:42:30 +0000

    and as usual, I forgot the new winform files

---
+++ trunk/BizHawk.MultiClient/config/AutofireConfig.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BizHawk.MultiClient
+{
+	public partial class AutofireConfig : Form
+	{
+		public AutofireConfig()
+		{
+			InitializeComponent();
+		}
+
+		private void AutofireConfig_Load(object sender, EventArgs e)
+		{
+			if (Global.Config.AutofireOn < OnNumeric.Minimum)
+				OnNumeric.Value = OnNumeric.Minimum;
+			else if (Global.Config.AutofireOn > OnNumeric.Maximum)
+				OnNumeric.Value = OnNumeric.Maximum;
+			else
+				OnNumeric.Value = Global.Config.AutofireOn;
+
+			if (Global.Config.AutofireOff < OffNumeric.Minimum)
+				OffNumeric.Value = OffNumeric.Minimum;
+			else if (Global.Config.AutofireOff > OffNumeric.Maximum)
+				OffNumeric.Value = OffNumeric.Maximum;
+			else
+				OffNumeric.Value = Global.Config.AutofireOff;
+		}
+
+		private void Ok_Click(object sender, EventArgs e)
+		{
+			Global.AutoFireController.On = Global.Config.AutofireOn = (int)OnNumeric.Value;
+			Global.AutoFireController.Off = Global.Config.AutofireOff = (int)OffNumeric.Value;
+			this.Close();
+		}
+
+		private void Cancel_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+	}
+}
