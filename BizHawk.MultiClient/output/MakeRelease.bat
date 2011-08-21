cfb2f912925ea986827fc4ace08b703c44d264b8
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-08-21 00:22:19 +0000

    add directx web installer and add to MakRelease.bat

--- BizHawk.MultiClient/output/MakeRelease.bat
+++ BizHawk.MultiClient/output/MakeRelease.bat
@@ -1,3 +1,3 @@
 del /s BizHawk.zip
 copy ..\..\SlimDx.dll
-zip -X -9 -r BizHawk.zip BizHawk.MultiClient.exe DiscoHawk.exe *.dll  NesCarts.7z gamedb.txt gamedb_neshomebrew.txt NES\Palettes
+zip -X -9 -r BizHawk.zip BizHawk.MultiClient.exe DiscoHawk.exe *.dll  NesCarts.7z gamedb.txt gamedb_neshomebrew.txt dxwebsetup.exe NES\Palettes
