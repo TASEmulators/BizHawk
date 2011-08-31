cfa6f5b48b5b86a4c2c6d88f1b6a148ac67c8c59
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-08-31 02:23:27 +0000

    Oops, forgot to check in new discohawk files

---
+++ trunk/DiscoHawk/MainDiscoForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace BizHawk
+{
+	partial class MainDiscoForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainDiscoForm));
+			this.ExitButton = new System.Windows.Forms.Button();
+			this.lblMagicDragArea = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.lblMagicDragArea.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// ExitButton
+			// 
+			this.ExitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.ExitButton.Location = new System.Drawing.Point(281, 316);
+			this.ExitButton.Name = "ExitButton";
+			this.ExitButton.Size = new System.Drawing.Size(75, 23);
+			this.ExitButton.TabIndex = 0;
+			this.ExitButton.Text = "E&xit";
+			this.ExitButton.UseVisualStyleBackColor = true;
+			this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+			// 
+			// lblMagicDragArea
+			// 
+			this.lblMagicDragArea.Controls.Add(this.label1);
+			this.lblMagicDragArea.Location = new System.Drawing.Point(84, 12);
+			this.lblMagicDragArea.Name = "lblMagicDragArea";
+			this.lblMagicDragArea.Size = new System.Drawing.Size(200, 100);
+			this.lblMagicDragArea.TabIndex = 1;
+			this.lblMagicDragArea.DragDrop += new System.Windows.Forms.DragEventHandler(this.lblMagicDragArea_DragDrop);
+			this.lblMagicDragArea.DragEnter += new System.Windows.Forms.DragEventHandler(this.lblMagicDragArea_DragEnter);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(47, 10);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(106, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Drag here for MAGIC";
+			// 
+			// MainDiscoForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(368, 351);
+			this.Controls.Add(this.lblMagicDragArea);
+			this.Controls.Add(this.ExitButton);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "MainDiscoForm";
+			this.Text = "DiscoHawk";
+			this.Load += new System.EventHandler(this.MainDiscoForm_Load);
+			this.lblMagicDragArea.ResumeLayout(false);
+			this.lblMagicDragArea.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button ExitButton;
+		private System.Windows.Forms.Panel lblMagicDragArea;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ trunk/DiscoHawk/MainDiscoForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using BizHawk.DiscSystem;
+
+namespace BizHawk
+{
+	public partial class MainDiscoForm : Form
+	{
+		public MainDiscoForm()
+		{
+			InitializeComponent();
+		}
+
+		private class DiscRecord
+		{
+			public Disc Disc;
+			public string BaseName;
+		}
+
+		private void MainDiscoForm_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void ExitButton_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void lblMagicDragArea_DragDrop(object sender, DragEventArgs e)
+		{
+
+		}
+
+		private void lblMagicDragArea_DragEnter(object sender, DragEventArgs e)
+		{
+
+		}
+	}
+}
