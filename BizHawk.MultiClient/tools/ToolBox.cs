79e5a399f7fa66ad0cf7151d463a043e6064bc50
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-09-10 21:48:27 +0000

    Toolbox - resize slightly, set minimum size, change bg color of toolstrip, make ti83 keypad visible only if ti83 is loaded

--- BizHawk.MultiClient/tools/ToolBox.cs
+++ BizHawk.MultiClient/tools/ToolBox.cs
@@ -11,99 +11,99 @@ using BizHawk.Emulation.Consoles.Calculator;
 
 namespace BizHawk.MultiClient
 {
-    public partial class ToolBox : Form
-    {
-        public ToolBox()
-        {
-            InitializeComponent();
-        }
+	public partial class ToolBox : Form
+	{
+		public ToolBox()
+		{
+			InitializeComponent();
+		}
 
-        private void ToolBox_Load(object sender, EventArgs e)
-        {
-            int x = Global.MainForm.Location.X + Global.MainForm.Size.Width;
-            int y = Global.MainForm.Location.Y;
-            Location = new Point(x, y);
-            if (Global.Emulator is NES)
-            {
-                NESPPU.Visible = true;
-                NESDebugger.Visible = true;
-                NESGameGenie.Visible = true;
-                NESNameTable.Visible = true;
-            }
-            else
-            {
-                NESPPU.Visible = false;
-                NESDebugger.Visible = false;
-                NESGameGenie.Visible = false;
-                NESNameTable.Visible = false;
-            }
-            if (Global.Emulator is TI83)
-            {
+		private void ToolBox_Load(object sender, EventArgs e)
+		{
+			int x = Global.MainForm.Location.X + Global.MainForm.Size.Width;
+			int y = Global.MainForm.Location.Y;
+			Location = new Point(x, y);
+			if (Global.Emulator is NES)
+			{
+				NESPPU.Visible = true;
+				NESDebugger.Visible = true;
+				NESGameGenie.Visible = true;
+				NESNameTable.Visible = true;
+			}
+			else
+			{
+				NESPPU.Visible = false;
+				NESDebugger.Visible = false;
+				NESGameGenie.Visible = false;
+				NESNameTable.Visible = false;
+			}
+			if (Global.Emulator is TI83)
+			{
+				KeypadTool.Visible = true;
+			}
+			else
+			{
+				KeypadTool.Visible = false;
+			}
+		}
 
-            }
-            else
-            {
+		private void toolStripButton1_Click(object sender, EventArgs e)
+		{
+			Global.MainForm.LoadCheatsWindow();
+		}
 
-            }
-        }
+		private void toolStripButton2_Click(object sender, EventArgs e)
+		{
+			Global.MainForm.LoadRamWatch();
+		}
 
-        private void toolStripButton1_Click(object sender, EventArgs e)
-        {
-            Global.MainForm.LoadCheatsWindow();
-        }
+		private void toolStripButton3_Click(object sender, EventArgs e)
+		{
+			Global.MainForm.LoadRamSearch();
+		}
 
-        private void toolStripButton2_Click(object sender, EventArgs e)
-        {
-            Global.MainForm.LoadRamWatch();
-        }
+		private void toolStripButton4_Click(object sender, EventArgs e)
+		{
+			RamPoke r = new RamPoke();
+			r.Show();
+		}
 
-        private void toolStripButton3_Click(object sender, EventArgs e)
-        {
-            Global.MainForm.LoadRamSearch();
-        }
+		private void HexEditor_Click(object sender, EventArgs e)
+		{
+			Global.MainForm.LoadHexEditor();
+		}
 
-        private void toolStripButton4_Click(object sender, EventArgs e)
-        {
-            RamPoke r = new RamPoke();
-            r.Show();
-        }
+		private void toolStripButton5_Click(object sender, EventArgs e)
+		{
+			var window = new BizHawk.MultiClient.tools.LuaWindow();
+			window.Show();
+		}
 
-        private void HexEditor_Click(object sender, EventArgs e)
-        {
-            Global.MainForm.LoadHexEditor();
-        }
+		private void NESPPU_Click(object sender, EventArgs e)
+		{
+			Global.MainForm.LoadNESPPU();
+		}
 
-        private void toolStripButton5_Click(object sender, EventArgs e)
-        {
-            var window = new BizHawk.MultiClient.tools.LuaWindow();
-            window.Show();
-        }
+		private void NESDebugger_Click(object sender, EventArgs e)
+		{
+			Global.MainForm.LoadNESDebugger();
+		}
 
-        private void NESPPU_Click(object sender, EventArgs e)
-        {
-            Global.MainForm.LoadNESPPU();
-        }
+		private void NESGameGenie_Click(object sender, EventArgs e)
+		{
+			Global.MainForm.LoadGameGenieEC();
+		}
 
-        private void NESDebugger_Click(object sender, EventArgs e)
-        {
-            Global.MainForm.LoadNESDebugger();
-        }
+		private void NESNameTable_Click(object sender, EventArgs e)
+		{
+			Global.MainForm.LoadNESNameTable();
+		}
 
-        private void NESGameGenie_Click(object sender, EventArgs e)
-        {
-            Global.MainForm.LoadGameGenieEC();
-        }
-
-        private void NESNameTable_Click(object sender, EventArgs e)
-        {
-            Global.MainForm.LoadNESNameTable();
-        }
-
-        private void toolStripButton6_Click(object sender, EventArgs e)
-        {
-            if (!(Global.Emulator is TI83))
-                return;
-            Global.MainForm.LoadTI83KeyPad();
-        }
-    }
+		private void KeyPadTool_Click(object sender, EventArgs e)
+		{
+			if (!(Global.Emulator is TI83))
+				return;
+			Global.MainForm.LoadTI83KeyPad();
+		}
+	}
 }
