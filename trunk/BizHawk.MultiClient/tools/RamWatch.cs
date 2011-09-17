0b4671ad9a0048fe46ee3281d0cf6901abbff756
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-09-17 15:35:39 +0000

    Hex Editor - use Ram Poke dialog for the Poke address function.  Ram Watch - Successfully stop/start sound on ram poke

--- trunk/BizHawk.MultiClient/tools/RamWatch.cs
+++ trunk/BizHawk.MultiClient/tools/RamWatch.cs
@@ -925,11 +925,11 @@ namespace BizHawk.MultiClient
 			ListView.SelectedIndexCollection indexes = WatchListView.SelectedIndices;
 			Global.Sound.StopSound();
 			RamPoke p = new RamPoke();
-			Global.Sound.StartSound();
 			if (indexes.Count > 0)
 				p.SetWatchObject(watchList[indexes[0]], Domain);
 			p.location = GetPromptPoint();
 			p.ShowDialog();
+			Global.Sound.StartSound();
 		}
 
 		private void pokeAddressToolStripMenuItem_Click(object sender, EventArgs e)
