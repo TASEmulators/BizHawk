0c95a3d07c9327d5e6bc43bd0f92e7a3493b2677
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-09-24 19:48:18 +0000

    NES - Started JALECO-JF-13 board (Mapper 86).  The only copy of Moero!! Pro Yakyuu (Red) I could find doesn't work and doesn't match bootgod's db.  However, it worked in FCEUX

---
+++ trunk/BizHawk.Emulation/Consoles/Nintendo/NES/Boards/JALECO_JF_13.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BizHawk.Emulation.Consoles.Nintendo
+{
+	//Mapper 86
+	
+	//Example Games:
+	//--------------------------
+	//Moero!! Pro Yakyuu (Black)
+	//Moero!! Pro Yakyuu (Red)
+
+	class JALECO_JF_13 : NES.NESBoardBase
+	{
+		int chr;
+		int prg;
+		int soundon;
+		int soundid;
+
+		public override bool Configure(NES.EDetectionOrigin origin)
+		{
+			switch (Cart.board_type)
+			{
+				case "JALECO-JF-13":
+					break;
+				default:
+					return false;
+			}
+			SetMirrorType(Cart.pad_h, Cart.pad_v);
+			return true;
+		}
+
+		public override byte ReadPRG(int addr)
+		{
+			if (addr < 0x8000)
+				return ROM[addr + (prg * 0x8000)];
+			else
+				return base.ReadPRG(addr);
+		}
+
+		public override byte ReadPPU(int addr)
+		{
+			if (addr < 0x2000)
+				return VROM[(addr & 0x1FFF) + (chr * 0x2000)];
+			else
+				return base.ReadPPU(addr);
+		}
+
+		public override void WriteWRAM(int addr, byte value)
+		{
+			prg = (value >> 4) & 3;
+			chr = (value & 3) + ((value >> 6) & 1);
+		}
+
+		public override void SyncState(Serializer ser)
+		{
+			base.SyncState(ser);
+			ser.Sync("chr", ref chr);
+			ser.Sync("prg", ref prg);
+			ser.Sync("soundon", ref soundon);
+			ser.Sync("soundid", ref soundid);
+		}
+	}
+}
