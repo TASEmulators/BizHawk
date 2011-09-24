db859ca9ae7fda8dba950ce2067f5ce6e9d11908
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-09-24 21:21:27 +0000

    Add Board IREM-74*161/161/21/138 (Mapper 77), Napoleon Senki now works

---
+++ trunk/BizHawk.Emulation/Consoles/Nintendo/NES/Boards/IREM-74_161_161_21_138.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BizHawk.Emulation.Consoles.Nintendo
+{
+	//Mapper 77
+	//Napoleon Senki
+
+	class IREM_74_161_161_21_138 : NES.NESBoardBase
+	{
+		int chr, prg;
+		public override bool Configure(NES.EDetectionOrigin origin)
+		{
+			//configure
+			switch (Cart.board_type)
+			{
+				case "IREM-74*161/161/21/138":
+					break;
+				default:
+					return false;
+			}
+
+			SetMirrorType(Cart.pad_h, Cart.pad_v);
+			return true;
+		}
+
+		public override void SyncState(Serializer ser)
+		{
+			base.SyncState(ser);
+			ser.Sync("chr", ref chr);
+			ser.Sync("prg", ref prg);
+		}
+
+		public override void WritePRG(int addr, byte value)
+		{
+			chr = (value >> 4) & 0x0F;
+			prg = value & 0x0F;
+		}
+
+		public override byte ReadPPU(int addr)
+		{
+			if (addr < 0x0800)
+			{
+				return VROM[addr + (chr * 0x0800)];
+			}
+			else if (addr < 0x2000)
+				return VRAM[addr];
+			else return base.ReadPPU(addr);
+		}
+
+		public override byte ReadPRG(int addr)
+		{
+			if (addr < 0x8000)
+				return ROM[addr + (prg * 0x8000)];
+			else
+				return base.ReadPRG(addr); 
+		}
+	}
+}
