a6afe4f3559633896b092d0d911beb0d0b279921
zeromus <zeromus@zeromus.org>
2011-09-25 09:44:13 +0000

    nes-add namcot mappers 019 and 210

--- BizHawk.Emulation/Consoles/Nintendo/NES/Boards/JALECO_JF_05_06_07.cs
+++ BizHawk.Emulation/Consoles/Nintendo/NES/Boards/JALECO_JF_05_06_07.cs
@@ -17,7 +17,7 @@ namespace BizHawk.Emulation.Consoles.Nintendo
 	Ninja Jajamaru Kun - JF_06
 	Argus (J) - JF_07
 	*/
-	class JALECO_JF_05 : NES.NESBoardBase
+	class JALECO_JF_05_06_07 : NES.NESBoardBase
 	{
 		bool hibit, lowbit;
 
@@ -26,66 +26,12 @@ namespace BizHawk.Emulation.Consoles.Nintendo
 			switch (Cart.board_type)
 			{
 				case "JALECO-JF-05":
-					AssertPrg(16); AssertChr(16); AssertVram(0); AssertWram(0);
-					break;
-				default:
-					return false;
-			}
-			SetMirrorType(NES.NESBoardBase.EMirrorType.Vertical);
-			return true;
-		}
-
-		public override void SyncState(Serializer ser)
-		{
-			base.SyncState(ser);
-			ser.Sync("hibit", ref hibit);
-			ser.Sync("lowbit", ref lowbit);
-		}
-
-		public override void WriteWRAM(int addr, byte value)
-		{
-			hibit = value.Bit(0);
-			lowbit = value.Bit(1);
-		}
-
-		public override byte ReadPPU(int addr)
-		{
-			if (addr < 0x2000)
-			{
-				if (lowbit)
-				{
-					if (hibit)
-						return VROM[addr + 0x6000];
-					return VROM[addr + 0x2000];
-				}
-				else
-				{
-					if (hibit)
-						return VROM[addr + 0x4000];
-					return VROM[addr];
-				}
-			}
-			return base.ReadPPU(addr);
-		}
-
-		public override byte ReadPRG(int addr)
-		{
-			if (addr > 0x4000) addr -= 0x4000;
-			return base.ReadPRG(addr);
-		}
-	}
-
-	class JALECO_JF_06 : NES.NESBoardBase
-	{
-		bool hibit, lowbit;
-
-		public override bool Configure(NES.EDetectionOrigin origin)
-		{
-			switch (Cart.board_type)
-			{
 				case "JALECO-JF-06":
 					AssertPrg(16); AssertChr(16); AssertVram(0); AssertWram(0);
 					break;
+				case "JALECO-JF-07":
+					AssertPrg(32); AssertChr(16); AssertVram(0); AssertWram(0);
+					break;
 				default:
 					return false;
 			}
@@ -132,57 +78,4 @@ namespace BizHawk.Emulation.Consoles.Nintendo
 			return base.ReadPRG(addr);
 		}
 	}
-
-	class JALECO_JF_07: NES.NESBoardBase
-	{
-		bool hibit, lowbit;
-
-		public override bool Configure(NES.EDetectionOrigin origin)
-		{
-			switch (Cart.board_type)
-			{
-				case "JALECO-JF-07":
-					break;
-				default:
-					return false;
-			}
-			SetMirrorType(NES.NESBoardBase.EMirrorType.Vertical);
-			return true;
-		}
-
-		public override void SyncState(Serializer ser)
-		{
-			base.SyncState(ser);
-			ser.Sync("hibit", ref hibit);
-			ser.Sync("lowbit", ref lowbit);
-		}
-
-		public override void WriteWRAM(int addr, byte value)
-		{
-			hibit = value.Bit(0);
-			lowbit = value.Bit(1);
-		}
-
-		public override byte ReadPPU(int addr)
-		{
-			if (addr < 0x2000)
-			{
-				if (lowbit)
-				{
-					if (hibit)
-						return VROM[addr + 0x6000];
-					return VROM[addr + 0x2000];
-				}
-				else
-				{
-					if (hibit)
-						return VROM[addr + 0x4000];
-					return VROM[addr];
-				}
-			}
-			return base.ReadPPU(addr);
-		}
-	}
-
-	
 }
