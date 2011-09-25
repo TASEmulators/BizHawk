fece5fd7e28a6f2c75e182ff798280c1584a76aa
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-09-25 01:20:32 +0000

    NES - Start HVC-CNROM-256K-01 board (for mapper 185).  Finishing will require finding a rom with the right sha1.

---
+++ BizHawk.Emulation/Consoles/Nintendo/NES/Boards/HVC_CNROM_256K_01.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BizHawk.Emulation.Consoles.Nintendo
+{
+	class HVC_CNROM_256K_01 : NES.NESBoardBase 
+	{
+		//Mapper 185
+		//Spy Vs. Spy (J)
+		//Mighty Bomb Jack (J)
+
+		int chr;
+
+		public override bool Configure(NES.EDetectionOrigin origin)
+		{
+			//configure
+			switch (Cart.board_type)
+			{
+				case "HVC-CNROM-256K-01": 
+					break;
+				default:
+					return false;
+			}
+			SetMirrorType(Cart.pad_h, Cart.pad_v);
+			return true;
+		}
+
+		public override void SyncState(Serializer ser)
+		{
+			base.SyncState(ser);
+			ser.Sync("chr", ref chr);
+		}
+
+		public override void WritePRG(int addr, byte value)
+		{
+			chr = value;
+		}
+
+		public override byte ReadPPU(int addr)
+		{
+			if (addr < 0x2000)
+				return VROM[addr + (chr * 0x2000)];
+			return base.ReadPPU(addr);
+		}
+	}
+}
