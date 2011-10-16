d1cbff9db3e30186f8112a5e5634586def33064a
beirich <beirich@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2011-10-16 06:23:15 +0000

    GEN: Preliminary sprite rendering. hook up MemoryDomains.

--- trunk/BizHawk.Emulation/Consoles/Sega/Genesis/GenVDP.DMA.cs
+++ trunk/BizHawk.Emulation/Consoles/Sega/Genesis/GenVDP.DMA.cs
@@ -8,6 +8,7 @@ namespace BizHawk.Emulation.Consoles.Sega
         public Func<int, short> DmaReadFrom68000; // TODO make ushort
 
         public int DmaLength { get { return Registers[19] | (Registers[20] << 8); } }
+        public int DmaMode   { get { return (Registers[23] >> 6) & 3; } }
 
         public int DmaSource
         {
@@ -21,28 +22,26 @@ namespace BizHawk.Emulation.Consoles.Sega
             }
         }
 
-
         bool DmaFillModePending;
 
         void ExecuteDmaFill(ushort data)
         {
-            Console.WriteLine("DMA FILL REQD, WRITE {0:X4}, {1:X4} times, at {2:X4}", data, DmaLength, VdpDataAddr);
+            Log.Note("VDP","DMA FILL REQD, WRITE {0:X4}, {1:X4} times, at {2:X4}", data, DmaLength, VdpDataAddr);
 
-            // TODO: Is the address really VdpDataAddr and not DMA source? I guess that makes sense.
             // TODO: It should spread this out, not do it all at once.
-            // TODO: DMA can go to places besides just VRAM (eg CRAM, VSRAM)
-            // TODO: Does DMA fill really use the actual increment register value?
+            // TODO: DMA can go to places besides just VRAM (eg CRAM, VSRAM) ??? can it?
+            // TODO: what is this genvdp.txt comment about accurate vdp fill emulation writing some other byte first?
 
             int length = DmaLength;
             if (length == 0)
-                length = 0x10000; // Really necessary?
+                length = 0x10000;
 
             byte fillByte = (byte)(data >> 8);
 
             do
             {
-                VRAM[VdpDataAddr & 0xFFFF] = fillByte;
-                UpdatePatternBuffer(VdpDataAddr & 0xFFFF);
+                VRAM[VdpDataAddr] = fillByte;
+                UpdatePatternBuffer(VdpDataAddr);
                 VdpDataAddr += Registers[15];
             } while (--length > 0);
 
@@ -51,7 +50,7 @@ namespace BizHawk.Emulation.Consoles.Sega
 
         void Execute68000VramCopy()
         {
-            Console.WriteLine("DMA 68000 -> VRAM COPY REQ'D. LENGTH {0:X4}, SOURCE {1:X4}", DmaLength, DmaSource);
+            Log.Note("VDP", "DMA 68000 -> VRAM COPY REQ'D. LENGTH {0:X4}, SOURCE {1:X4}", DmaLength, DmaSource);
             
             int length = DmaLength;
             if (length == 0)
--- trunk/BizHawk.Emulation/Consoles/Sega/Genesis/MemoryMap.Z80.cs
+++ trunk/BizHawk.Emulation/Consoles/Sega/Genesis/MemoryMap.Z80.cs
@@ -16,13 +16,15 @@ namespace BizHawk.Emulation.Consoles.Sega
             if (address >= 0x4000 && address < 0x6000)
             {
                 //Console.WriteLine(" === Z80 READS FM STATUS ===");
-                return YM2612.ReadStatus();
+                return YM2612.ReadStatus(); // TODO: more than 1 read port probably?
             }
             if (address >= 0x8000)
             {
                 // 68000 Bank region
                 return (byte) ReadByte(BankRegion | (address & 0x7FFF));
             }
+            if (address <= 0x6100) // read from bank address register - returns FF
+                return 0xFF; 
             Console.WriteLine("UNHANDLED Z80 READ {0:X4}",address);
             return 0xCD;
         }
@@ -37,17 +39,39 @@ namespace BizHawk.Emulation.Consoles.Sega
             }
             if (address >= 0x4000 && address < 0x6000)
             {
-                //Console.WriteLine(" === Z80 WRITES Z80 {0:X4}:{1:X2} ===",address, value);
+                //Console.WriteLine(" === Z80 WRITES YM2612 {0:X4}:{1:X2} ===",address, value);
                 YM2612.Write(address & 3, value);
                 return;
             }
-            if (address == 0x6000)
+            if (address < 0x6100)
             {
                 BankRegion >>= 1;
                 BankRegion |= (value & 1) << 23;
                 BankRegion &= 0x00FF8000;
-                Console.WriteLine("Bank pointing at {0:X8}",BankRegion);
+                //Console.WriteLine("Bank pointing at {0:X8}",BankRegion);
                 return;
+            }
+            if (address >= 0x7F00 && address < 0x7F20)
+            {
+                switch (address & 0x1F)
+                {
+                    case 0x00:
+                    case 0x02:
+                        VDP.WriteVdpData((ushort) ((value<<8) | value));
+                        return;
+                    
+                    case 0x04:
+                    case 0x06:
+                        VDP.WriteVdpControl((ushort)((value << 8) | value));
+                        return;
+
+                    case 0x11:
+                    case 0x13:
+                    case 0x15:
+                    case 0x17:
+                        PSG.WritePsgData(value, SoundCPU.TotalExecutedCycles);
+                        return;
+                }
             }
             if (address >= 0x8000)
             {
