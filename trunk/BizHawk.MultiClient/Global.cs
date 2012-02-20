d13317dc47ebec9ac1919c8115fe19fddc65c972
brandman211@gmail.com <brandman211@gmail.com@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-02-20 23:25:26 +0000

    -Finished my simplification of InputAdapters for everything but TI-83 (I don't remember how to test it). Saved 183 lines from the last revision, and this number is probably going to grow tremendously after TI-83 is handled. -Inadvertently fixed the seemingly broken Genesis 3-Button Controller mnemonics...the controllers weren't responding at all before. I guess refactoring and working with simplified code IS worth it. <_< --That said, this is still really broken: ---Mnemonics don't show up while recording. ---Input goes past the frame length, and it's never labeled finished. ---Doesn't seem to sync at all. ---This exception: --------------------------- Oh, no, a terrible thing happened!
    System.Exception: unhandled opcode at pc=013648
       at BizHawk.Emulation.CPUs.M68000.MC68000.ExecuteCycles(Int32 cycles) in C:\Users\Administrator\Repo\bizhawk\BizHawk.Emulation\CPUs\68000\MC68000.cs:line 147
       at BizHawk.Emulation.Consoles.Sega.Genesis.FrameAdvance(Boolean render) in C:\Users\Administrator\Repo\bizhawk\BizHawk.Emulation\Consoles\Sega\Genesis\Genesis.cs:line 106
       at BizHawk.MultiClient.MainForm.StepRunLoop_Core() in C:\Users\Administrator\Repo\bizhawk\BizHawk.MultiClient\MainForm.cs:line 1676
       at BizHawk.MultiClient.MainForm.ProgramRunLoop() in C:\Users\Administrator\Repo\bizhawk\BizHawk.MultiClient\MainForm.cs:line 346
       at BizHawk.MultiClient.Program.Main(String[] args) in C:\Users\Administrator\Repo\bizhawk\BizHawk.MultiClient\Program.cs:line 47
    ---------------------------
    System.Exception: unhandled opcode at pc=013648
    
       at BizHawk.Emulation.CPUs.M68000.MC68000.ExecuteCycles(Int32 cycles) in C:\Users\Administrator\Repo\bizhawk\BizHawk.Emulation\CPUs\68000\MC68000.cs:line 147
    
       at BizHawk.Emulation.Consoles.Sega.Genesis.FrameAdvance(Boolean render) in C:\Users\Administrator\Repo\bizhawk\BizHawk.Emulation\Consoles\Sega\Genesis\Genesis.cs:line 106
    
       at BizHawk.MultiClient.MainForm.StepRunLoop_Core() in C:\Users\Administrator\Repo\bizhawk\BizHawk.MultiClient\MainForm.cs:line 1676
    
       at BizHawk.MultiClient.MainForm.ProgramRunLoop() in C:\Users\Administrator\Repo\bizhawk\BizHawk.MultiClient\MainForm.cs:line 346
    
       at BizHawk.MultiClient.Program.Main(String[] args) in C:\Users\Administrator\Repo\bizhawk\BizHawk.MultiClient\Program.cs:line 47
    ---------------------------
    OK   
    ---------------------------
    --I don't think I broke anything...if anything, I fixed it slightly so that input is at least recognized in some way.
    --Question: Is Genesis going to be properly supported by release time? If so, I might hold off a Genesis TAS until this is done. I hate Gens.
    -Moved BUTTONS, COMMANDS, and new variable PLAYERS, which indicates how many players are for a given system, to Global.cs.
    --If there's a better place for this, please let me know.
    --That said, if there are places in the code that refer to the mnemonics of button names of a given system, we should definitely apply these variables for easier modification. Please let me know where these places might be.
    -Fixed ImportText(). It now only reads as many controllers as the given system supports.
    
    TODO:
    -Merge the TI-83 stuff.
    -Mnemonic header.
    -Comment!
    -Fix the TI-83 mnemonics with the IRCs blessings.
    -Port the new mnemonics to the wiki.
    -Fix the importers.
    --I don't think ImportMMV handles the Pause and Reset commands (Or buttons, I don't even know) covert.

--- trunk/BizHawk.MultiClient/Global.cs
+++ trunk/BizHawk.MultiClient/Global.cs
@@ -1,4 +1,5 @@
 ﻿using BizHawk.DiscSystem;
+using System.Collections.Generic;
 using SlimDX.Direct3D9;
 using SlimDX.DirectSound;
 
@@ -31,6 +32,72 @@ namespace BizHawk.MultiClient
 		public static AutofireController AutofireGBControls;
 		public static AutofireController AutofireGenControls;
 
+		public static readonly Dictionary<string, Dictionary<string, string>> BUTTONS = new Dictionary<string, Dictionary<string, string>>()
+		{
+			{
+				"Gameboy Controller", new Dictionary<string, string>()
+				{
+					{"Up", "U"}, {"Down", "D"}, {"Left", "L"}, {"Right", "R"}, {"Select", "s"}, {"Start", "S"}, {"B", "B"},
+					{"A", "A"}
+				}
+			},
+			{
+				"Genesis 3-Button Controller", new Dictionary<string, string>()
+				{
+					{"Up", "U"}, {"Down", "D"}, {"Left", "L"}, {"Right", "R"}, {"Start", "S"}, {"A", "A"}, {"B", "B"},
+					{"C", "C"}
+				}
+			},
+			{
+				"NES Controller", new Dictionary<string, string>()
+				{
+					{"Up", "U"}, {"Down", "D"}, {"Left", "L"}, {"Right", "R"}, {"Select", "s"}, {"Start", "S"}, {"B", "B"},
+					{"A", "A"}
+				}
+			},
+			{
+				"PC Engine Controller", new Dictionary<string, string>()
+				{
+					{"Up", "U"}, {"Down", "D"}, {"Left", "L"}, {"Right", "R"}, {"Select", "s"}, {"Run", "r"}, {"B2", "2"},
+					{"B1", "1"}
+				}
+			},
+			{
+				"SMS Controller", new Dictionary<string, string>()
+				{
+					{"Up", "U"}, {"Down", "D"}, {"Left", "L"}, {"Right", "R"}, {"B1", "1"}, {"B2", "2"}
+				}
+			},
+			{
+				"TI83 Controller", new Dictionary<string, string>()
+				{
+					{"0", "0"}, {"1", "1"}, {"2", "2"}, {"3", "3"}, {"4", "4"}, {"5", "5"}, {"6", "6"}, {"7", "7"},
+					{"8", "8"}, {"9", "9"}, {"DOT", "`"}, {"ON", "O"}, {"ENTER", "="}, {"UP", "U"}, {"DOWN", "D"},
+					{"LEFT", "L"}, {"RIGHT", "R"}, {"PLUS", "+"}, {"MINUS", "_"}, {"MULTIPLY", "*"}, {"DIVIDE", "/"},
+ 					{"CLEAR", "c"}, {"EXP", "^"}, {"DASH", "-"}, {"PARAOPEN", "("}, {"PARACLOSE", ")"}, {"TAN", "T"},
+					{"VARS", "V"}, {"COS", "C"}, {"PRGM", "P"}, {"STAT", "s"}, {"MATRIX", "m"}, {"X", "X"}, {"STO", ">"},
+					{"LN", "n"}, {"LOG", "L"}, {"SQUARED", "2"}, {"NEG1", "1"}, {"MATH", "H"}, {"ALPHA", "A"},
+					{"GRAPH", "G"}, {"TRACE", "t"}, {"ZOOM", "Z"}, {"WINDOW", "W"}, {"Y", "Y"}, {"2ND", "&"}, {"MODE", "O"},
+					{"DEL", "D"}, {"COMMA", ","}, {"SIN", "S"}
+				}
+			}
+		};
+		public static readonly Dictionary<string, Dictionary<string, string>> COMMANDS = new Dictionary<string, Dictionary<string, string>>()
+		{
+			{"Gameboy Controller", new Dictionary<string, string>() {}},
+			{"Genesis 3-Button Controller", new Dictionary<string, string>() {}},
+			{"NES Controller", new Dictionary<string, string>() {{"Reset", "r"}}},
+			{"PC Engine Controller", new Dictionary<string, string>() {}},
+			{"SMS Controller", new Dictionary<string, string>() {{"Pause", "p"}, {"Reset", "r"}}},
+			{"TI83 Controller", new Dictionary<string, string>() {}}
+		};
+
+		public static readonly Dictionary<string, int> PLAYERS = new Dictionary<string, int>()
+		{
+			{"Gameboy Controller", 1}, {"Genesis 3-Button Controller", 2}, {"NES Controller", 4},
+			{"PC Engine Controller", 5}, {"SMS Controller", 2}, {"TI83 Controller", 1}
+		};
+
 		/// <summary>
 		/// whether vsync is force-disabled by use of fast forward
 		/// </summary>
@@ -45,22 +112,20 @@ namespace BizHawk.MultiClient
 		/// </summary>
 		public static MultitrackRewiringControllerAdapter MultitrackRewiringControllerAdapter = new MultitrackRewiringControllerAdapter();
 
-
 		public static MovieSession MovieSession = new MovieSession();
-
 
 		//dont take my word for it, since the final word is actually in RewireInputChain, but here is a guide...
 		//user -> Input -> ActiveController -> UDLR -> StickyXORPlayerInputAdapter -> TurboAdapter(TBD) -> Lua(?TBD?) -> ..
 		//.. -> MultitrackRewiringControllerAdapter -> MovieInputSourceAdapter -> (MovieSession) -> MovieOutputAdapter -> ControllerOutput(1) -> Game
 		//(1)->Input Display
-		
+
 		//the original source controller, bound to the user, sort of the "input" port for the chain, i think
 		public static Controller ActiveController;
 
 		//rapid fire version on the user controller, has its own key bindings and is OR'ed against ActiveController
 		public static AutofireController AutoFireController;
-		
-		//the "output" port for the controller chain. 
+
+		//the "output" port for the controller chain.
 		public static CopyControllerAdapter ControllerOutput = new CopyControllerAdapter();
 
 		//input state which has been destined for game controller inputs are coalesced here
@@ -98,8 +163,6 @@ namespace BizHawk.MultiClient
 
 		public static DiscHopper DiscHopper = new DiscHopper();
 
-
 		public static CoreAccessor PsxCoreLibrary = new CoreAccessor(new Win32LibAccessor("PsxHawk.Core.dll"));
-
 	}
 }
