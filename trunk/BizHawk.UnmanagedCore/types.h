70dbecb5abc12999d013fa99c0d509f540cdba25
zeromus@zeromus.org <zeromus@zeromus.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-02-24 20:42:11 +0000

    continuing cleanup.. sorry, this is like a month's worth of work

--- trunk/BizHawk.UnmanagedCore/types.h
+++
@@ -1,8 +0,0 @@
-typedef unsigned char u8;
-typedef char s8;
-typedef unsigned short u16;
-typedef short s16;
-typedef unsigned int u32;
-typedef int s32;
-typedef unsigned __int64 u64;
-typedef __int64 s64;
