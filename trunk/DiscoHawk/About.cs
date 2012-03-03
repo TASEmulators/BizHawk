e7cdc8a51b6c625b0952df3b7b041534c889034e
zeromus@zeromus.org <zeromus@zeromus.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-03-03 18:04:11 +0000

    polish discohawk

---
+++ trunk/DiscoHawk/About.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BizHawk
+{
+	public partial class About : Form
+	{
+		public About()
+		{
+			InitializeComponent();
+		}
+
+		private void richTextBox1_LinkClicked(object sender, LinkClickedEventArgs e)
+		{
+			 System.Diagnostics.Process.Start(e.LinkText);
+		}
+	}
+}
