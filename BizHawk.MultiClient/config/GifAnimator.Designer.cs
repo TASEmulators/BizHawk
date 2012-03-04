1def3a82922e90af84a12709aaa801f60e842451
adelikat <adelikat@tasvideos.org>
2012-03-04 21:00:58 +0000

    Minor fixups to gif animator dialog

--- BizHawk.MultiClient/config/GifAnimator.Designer.cs
+++ BizHawk.MultiClient/config/GifAnimator.Designer.cs
@@ -28,7 +28,8 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.Exit = new System.Windows.Forms.Button();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GifAnimator));
+			this.Save = new System.Windows.Forms.Button();
 			this.checkBox1 = new System.Windows.Forms.CheckBox();
 			this.TB_Frame_Skip = new System.Windows.Forms.TextBox();
 			this.comboBox1 = new System.Windows.Forms.ComboBox();
@@ -36,21 +37,23 @@
 			this.label2 = new System.Windows.Forms.Label();
 			this.label3 = new System.Windows.Forms.Label();
 			this.TB_Num_Frames = new System.Windows.Forms.TextBox();
-			this.button1 = new System.Windows.Forms.Button();
+			this.Cancel = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
-			// Exit
+			// Save
 			// 
-			this.Exit.Location = new System.Drawing.Point(12, 108);
-			this.Exit.Name = "Exit";
-			this.Exit.Size = new System.Drawing.Size(75, 23);
-			this.Exit.TabIndex = 5;
-			this.Exit.Text = "&Save";
-			this.Exit.UseVisualStyleBackColor = true;
-			this.Exit.Click += new System.EventHandler(this.Exit_Click);
+			this.Save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.Save.Location = new System.Drawing.Point(12, 108);
+			this.Save.Name = "Save";
+			this.Save.Size = new System.Drawing.Size(75, 23);
+			this.Save.TabIndex = 5;
+			this.Save.Text = "&Save";
+			this.Save.UseVisualStyleBackColor = true;
+			this.Save.Click += new System.EventHandler(this.Exit_Click);
 			// 
 			// checkBox1
 			// 
+			this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
 			this.checkBox1.AutoSize = true;
 			this.checkBox1.Location = new System.Drawing.Point(73, 85);
 			this.checkBox1.Name = "checkBox1";
@@ -61,13 +64,16 @@
 			// 
 			// TB_Frame_Skip
 			// 
+			this.TB_Frame_Skip.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
 			this.TB_Frame_Skip.Location = new System.Drawing.Point(108, 32);
+			this.TB_Frame_Skip.MaxLength = 1024;
 			this.TB_Frame_Skip.Name = "TB_Frame_Skip";
 			this.TB_Frame_Skip.Size = new System.Drawing.Size(58, 20);
 			this.TB_Frame_Skip.TabIndex = 2;
 			// 
 			// comboBox1
 			// 
+			this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
 			this.comboBox1.FormattingEnabled = true;
 			this.comboBox1.Location = new System.Drawing.Point(108, 58);
 			this.comboBox1.Name = "comboBox1";
@@ -103,28 +109,32 @@
 			// 
 			// TB_Num_Frames
 			// 
+			this.TB_Num_Frames.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
 			this.TB_Num_Frames.Location = new System.Drawing.Point(108, 6);
 			this.TB_Num_Frames.Name = "TB_Num_Frames";
 			this.TB_Num_Frames.Size = new System.Drawing.Size(58, 20);
 			this.TB_Num_Frames.TabIndex = 1;
 			// 
-			// button1
+			// Cancel
 			// 
-			this.button1.Location = new System.Drawing.Point(93, 108);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 6;
-			this.button1.Text = "&Cancel";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click);
+			this.Cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.Cancel.Location = new System.Drawing.Point(93, 108);
+			this.Cancel.Name = "Cancel";
+			this.Cancel.Size = new System.Drawing.Size(75, 23);
+			this.Cancel.TabIndex = 6;
+			this.Cancel.Text = "&Cancel";
+			this.Cancel.UseVisualStyleBackColor = true;
+			this.Cancel.Click += new System.EventHandler(this.button1_Click);
 			// 
 			// GifAnimator
 			// 
-			this.AcceptButton = this.Exit;
+			this.AcceptButton = this.Save;
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(184, 140);
-			this.Controls.Add(this.button1);
+			this.CancelButton = this.Cancel;
+			this.ClientSize = new System.Drawing.Size(183, 146);
+			this.Controls.Add(this.Cancel);
 			this.Controls.Add(this.label3);
 			this.Controls.Add(this.TB_Num_Frames);
 			this.Controls.Add(this.label2);
@@ -132,8 +142,11 @@
 			this.Controls.Add(this.comboBox1);
 			this.Controls.Add(this.TB_Frame_Skip);
 			this.Controls.Add(this.checkBox1);
-			this.Controls.Add(this.Exit);
+			this.Controls.Add(this.Save);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MinimumSize = new System.Drawing.Size(199, 184);
 			this.Name = "GifAnimator";
+			this.ShowIcon = false;
 			this.Text = "GifAnimator";
 			this.Load += new System.EventHandler(this.GifAnimator_Load);
 			this.ResumeLayout(false);
@@ -143,7 +156,7 @@
 
 		#endregion
 
-		private System.Windows.Forms.Button Exit;
+		private System.Windows.Forms.Button Save;
 		private System.Windows.Forms.CheckBox checkBox1;
 		private System.Windows.Forms.TextBox TB_Frame_Skip;
 		private System.Windows.Forms.ComboBox comboBox1;
@@ -151,6 +164,6 @@
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.TextBox TB_Num_Frames;
-		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button Cancel;
 	}
 }
