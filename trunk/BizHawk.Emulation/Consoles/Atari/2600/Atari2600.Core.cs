a1cd2098e2bee6f48b92808690e991069f3f322d
pjgat09@gmail.com <pjgat09@gmail.com@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-03-08 08:12:44 +0000

    Started work on Atari 2600 core. Added stubs for the TIA and M6532 to be filled in later. Memory map is hard coded, but will only work for basic 4K carts like pitfall. That'll have to be changed later.

--- trunk/BizHawk.Emulation/Consoles/Atari/2600/Atari2600.Core.cs
+++ trunk/BizHawk.Emulation/Consoles/Atari/2600/Atari2600.Core.cs
@@ -1,40 +1,142 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using BizHawk.Emulation.CPUs.M6502;
+using BizHawk.Emulation.Consoles.Atari;
 
 namespace BizHawk
 {
 	partial class Atari2600
 	{
-		public byte[] ram = new byte[128];
 		public byte[] rom;
-		public BizHawk.Emulation.CPUs.M6502.MOS6502 cpu;
+		public MOS6502 cpu;
+		public M6532 m6532;
+		public TIA tia;
 
+		// The Atari 2600 memory mapper looks something like this...usually
+
+		// N/A  Page #  
+		// 000  0000000 000000
+
+		// 0x0000-0x003F - TIA Registers
+		// 0x0040-0x007F - TIA Registers (mirror)
+		// 0x0080-0x00FF - 6532 RAM
+
+		// 0x0100-0x01FF - Mirror of 0x00FF
+
+		// 0x0200-0x023F - TIA Registers (mirror)
+		// 0x0240-0x027F - TIA Registers (mirror)
+
+		// 0x0280-0x029F - 6532 Registers
+		// 0x02A0-0x02BF - 6532 Registers (mirror)
+		// 0x02C0-0x02DF - 6532 Registers (mirror)
+		// 0x02E0-0x02FF - 6532 Registers (mirror)
+
+		// 0x0300-0x033F - TIA Registers (mirror)
+		// 0x0340-0x037F - TIA Registers (mirror)
+
+		// 0x0380-0x039F - 6532 Registers (mirror)
+		// 0x03A0-0x03BF - 6532 Registers (mirror)
+		// 0x03C0-0x03DF - 6532 Registers (mirror)
+		// 0x03E0-0x03FF - 6532 Registers (mirror)
+
+		// 0x0400-0x07FF - Mirror of 0x0000-0x03FF
+		// 0x0800-0x0BFF - Mirror of 0x0000-0x03FF
+		// 0x0C00-0x0FFF - Mirror of 0x0000-0x03FF
+
+		// 0x1000-0x1FFF - ROM
+
+		// If page# % 4 == 0 or 1, TIA
+		// If page# % 4 == 2 or 3, 6532
+		//   if (addr & 0x0200 == 0x0000 && addr & 0x1080 == 0x0080)
+		//     RAM
+		//   else
+		//     registers
+		// else
+		//   ROM
+					
 		public byte ReadMemory(ushort addr)
 		{
-			return 0xFF;
+			ushort maskedAddr = (ushort)(addr & 0x1FFF);
+			ushort pageNum = (ushort)(maskedAddr >> 6);
+			if (pageNum < 0x40)
+			{
+				// if Page 0x02, or a mirror
+				if (pageNum % 4 > 1)
+				{
+					return m6532.ReadMemory(maskedAddr);
+				}
+				// if Page 0x01 or 0x02, or a mirror
+				else
+				{
+					return tia.ReadMemory(maskedAddr);
+				}
+			}
+			// ROM data
+			else
+			{
+				Console.WriteLine("ROM read");
+				return rom[maskedAddr & 0x0FFF];
+			}
 		}
 
 		public void WriteMemory(ushort addr, byte value)
 		{
+			ushort maskedAddr = (ushort)(addr & 0x1FFF);
+			ushort pageNum = (ushort)(maskedAddr >> 6);
+			if (pageNum < 0x40)
+			{
+				if (pageNum % 4 > 1)
+				{
+					m6532.WriteMemory(maskedAddr, value);
+				}
+				else
+				{
+					tia.WriteMemory(maskedAddr, value);
+				}
+			}
+			// ROM data
+			else
+			{
+				Console.WriteLine("ROM write(?):  " + addr.ToString("x"));
+			}
 		}
 
 		public void HardReset()
 		{
 			cpu = new Emulation.CPUs.M6502.MOS6502();
+			cpu.debug = true;
 			cpu.ReadMemory = ReadMemory;
 			cpu.WriteMemory = WriteMemory;
 
+			// Setup TIA
+			tia = new TIA(cpu);
+			// Setup 6532
+			m6532 = new M6532(cpu, ram);
+
 			//setup the system state here. for instance..
-			cpu.PC = 0x0123; //set the initial PC
+			// Read from the reset vector for where to start
+			cpu.PC = (ushort)(ReadMemory(0xFFFC) + (ReadMemory(0xFFFD) << 8)); //set the initial PC
+			//cpu.PC = 0x0000; //set the initial PC
 		}
 
 		public void FrameAdvance(bool render)
 		{
+			Frame++;
+			//cpu.Execute(228);
+			cpu.Execute(1);
 			//clear the framebuffer (hack code)
 			if (render == false) return;
 			for (int i = 0; i < 256 * 192; i++)
-				frameBuffer[i] = 0; //black
+			{
+				if (i < 64*256)
+					frameBuffer[i] = i % 256; //black
+				if (i >= 64*256 && i < 128*256)
+					frameBuffer[i] = (i % 256) << 8; //black
+				if (i >= 128*256)
+					frameBuffer[i] = (i % 256) << 16; //black
+			}
+				
 
 			//run one frame's worth of cpu cyclees (i.e. do the emulation!)
 			//this should generate the framebuffer as it goes.
---
+++ trunk/BizHawk.Emulation/Consoles/Atari/2600/M6532.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using BizHawk.Emulation.CPUs.M6502;
+
+namespace BizHawk.Emulation.Consoles.Atari
+{
+	// Emulates the M6532 RIOT Chip
+	public partial class M6532
+	{
+		MOS6502 Cpu;
+		public byte[] ram;
+
+		public M6532(MOS6502 cpu, byte[] ram)
+		{
+			Cpu = cpu;
+			this.ram = ram;
+		}
+
+		public byte ReadMemory(ushort addr)
+		{
+			ushort maskedAddr;
+
+			if ((addr & 0x1080) == 0x0080 && (addr & 0x0200) == 0x0000)
+			{
+				maskedAddr = (ushort)(addr & 0x007f);
+				Console.WriteLine("6532 ram read: " + maskedAddr.ToString("x"));
+				return ram[maskedAddr];
+			}
+			else
+			{
+				maskedAddr = (ushort)(addr & 0x0007);
+				Console.WriteLine("6532 register read: " + maskedAddr.ToString("x"));
+			}
+
+			return 0x3A;
+		}
+
+		public void WriteMemory(ushort addr, byte value)
+		{
+			ushort maskedAddr;
+
+			if ((addr & 0x1080) == 0x0080 && (addr & 0x0200) == 0x0000)
+			{
+				maskedAddr = (ushort)(addr & 0x007f);
+				Console.WriteLine("6532 ram write: " + maskedAddr.ToString("x"));
+				ram[maskedAddr] = value;
+			}
+			else
+			{
+				maskedAddr = (ushort)(addr & 0x0007);
+				Console.WriteLine("6532 register write: " + maskedAddr.ToString("x"));
+			}
+		}
+	}
+}
