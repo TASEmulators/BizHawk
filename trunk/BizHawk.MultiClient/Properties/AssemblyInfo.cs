e439bbeee294677f8e777f35935070ada86c3a45
zeromus@zeromus.org <zeromus@zeromus.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-03-10 00:21:40 +0000

    fix versions stuff

--- trunk/BizHawk.MultiClient/Properties/AssemblyInfo.cs
+++ trunk/BizHawk.MultiClient/Properties/AssemblyInfo.cs
@@ -6,13 +6,8 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BizHawk.MultiClient")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("BizHawk.MultiClient")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyDescription("http://code.google.com/p/bizhawk")]
+[assembly: AssemblyCompany("BizHawk")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,5 +27,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1" + SubWCRev.SVN_REV)]
-[assembly: AssemblyFileVersion("1.0.1" + SubWCRev.SVN_REV)]
+[assembly: AssemblyVersion("1.0.1." + SubWCRev.SVN_REV)]
+[assembly: AssemblyFileVersion("1.0.1." + SubWCRev.SVN_REV)]
