ef28378dcd281aac07fcecb46fc3af3a02eed2a6
pjgat09 <pjgat09@gmail.com>
2012-03-15 21:08:53 +0000

    Atari 2600: added stub for the new TIA

---
+++ BizHawk.Emulation/Consoles/Atari/2600/TIA.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using BizHawk.Emulation.CPUs.M6507;
+using System.Collections.Generic;
+
+namespace BizHawk.Emulation.Consoles.Atari
+{
+	// Emulates the TIA
+	public partial class TIA
+	{
+		Atari2600 core;
+
+		byte hsyncCnt = 0;
+
+		struct playerData
+		{
+			public byte grp;
+			public byte dgrp;
+			public byte color;
+			public byte hPosCnt;
+			public byte scanCnt;
+			public byte HM;
+			public bool reflect;
+			public bool delay;
+			public byte nusiz;
+		};
+
+		playerData player0;
+		playerData player1;
+
+		public TIA(Atari2600 core)
+		{
+			this.core = core;
+		}
+
+		// Execute TIA cycles
+
+		// Every 4 cycles, increment the hsync counter
+		// if in visible part of screen, parse playfield
+
+	}
+}
