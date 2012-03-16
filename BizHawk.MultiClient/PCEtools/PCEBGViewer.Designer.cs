871e6df12fca0ce6d223d046711497bd888f88c5
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-03-16 15:29:05 +0000

    PCE BG Viewer - GUI overhaul

--- BizHawk.MultiClient/PCEtools/PCEBGViewer.Designer.cs
+++ BizHawk.MultiClient/PCEtools/PCEBGViewer.Designer.cs
@@ -28,25 +28,18 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.vdcComboBox = new System.Windows.Forms.ComboBox();
 			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
 			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.autoloadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.saveWindowPositionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.vDC1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.vCD2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
 			this.canvas = new BizHawk.MultiClient.PCEBGCanvas();
 			this.menuStrip1.SuspendLayout();
 			this.SuspendLayout();
-			// 
-			// vdcComboBox
-			// 
-			this.vdcComboBox.FormattingEnabled = true;
-			this.vdcComboBox.Location = new System.Drawing.Point(12, 27);
-			this.vdcComboBox.Name = "vdcComboBox";
-			this.vdcComboBox.Size = new System.Drawing.Size(121, 21);
-			this.vdcComboBox.TabIndex = 1;
-			this.vdcComboBox.SelectedIndexChanged += new System.EventHandler(this.vdcComboBox_SelectedIndexChanged);
 			// 
 			// menuStrip1
 			// 
@@ -55,17 +48,21 @@
             this.optionsToolStripMenuItem});
 			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
 			this.menuStrip1.Name = "menuStrip1";
-			this.menuStrip1.Size = new System.Drawing.Size(1056, 24);
+			this.menuStrip1.Size = new System.Drawing.Size(536, 24);
 			this.menuStrip1.TabIndex = 2;
 			this.menuStrip1.Text = "menuStrip1";
 			// 
 			// fileToolStripMenuItem
 			// 
 			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.vDC1ToolStripMenuItem,
+            this.vCD2ToolStripMenuItem,
+            this.toolStripSeparator1,
             this.exitToolStripMenuItem});
 			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
 			this.fileToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
 			this.fileToolStripMenuItem.Text = "&Viewer";
+			this.fileToolStripMenuItem.DropDownOpened += new System.EventHandler(this.fileToolStripMenuItem_DropDownOpened);
 			// 
 			// exitToolStripMenuItem
 			// 
@@ -99,25 +96,43 @@
 			this.saveWindowPositionToolStripMenuItem.Text = "&Save Window position";
 			this.saveWindowPositionToolStripMenuItem.Click += new System.EventHandler(this.saveWindowPositionToolStripMenuItem_Click);
 			// 
+			// vDC1ToolStripMenuItem
+			// 
+			this.vDC1ToolStripMenuItem.Name = "vDC1ToolStripMenuItem";
+			this.vDC1ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.vDC1ToolStripMenuItem.Text = "VDC&1";
+			this.vDC1ToolStripMenuItem.Click += new System.EventHandler(this.vDC1ToolStripMenuItem_Click);
+			// 
+			// vCD2ToolStripMenuItem
+			// 
+			this.vCD2ToolStripMenuItem.Name = "vCD2ToolStripMenuItem";
+			this.vCD2ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.vCD2ToolStripMenuItem.Text = "VCD&2";
+			this.vCD2ToolStripMenuItem.Click += new System.EventHandler(this.vCD2ToolStripMenuItem_Click);
+			// 
+			// toolStripSeparator1
+			// 
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+			// 
 			// canvas
 			// 
-			this.canvas.Location = new System.Drawing.Point(12, 65);
+			this.canvas.Location = new System.Drawing.Point(12, 27);
 			this.canvas.Name = "canvas";
-			this.canvas.Size = new System.Drawing.Size(1024, 543);
+			this.canvas.Size = new System.Drawing.Size(512, 543);
 			this.canvas.TabIndex = 0;
 			// 
 			// PCEBGViewer
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(1056, 649);
-			this.Controls.Add(this.vdcComboBox);
+			this.ClientSize = new System.Drawing.Size(536, 582);
 			this.Controls.Add(this.canvas);
 			this.Controls.Add(this.menuStrip1);
 			this.MainMenuStrip = this.menuStrip1;
 			this.Name = "PCEBGViewer";
 			this.ShowIcon = false;
-			this.Text = "PCE BG Viewer (interim)";
+			this.Text = "PCE BG Viewer";
 			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.PCEBGViewer_FormClosed);
 			this.Load += new System.EventHandler(this.PCEBGViewer_Load);
 			this.menuStrip1.ResumeLayout(false);
@@ -130,12 +145,14 @@
 		#endregion
 
 		private PCEBGCanvas canvas;
-		private System.Windows.Forms.ComboBox vdcComboBox;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem autoloadToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem saveWindowPositionToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem vDC1ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem vCD2ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
 	}
 }
