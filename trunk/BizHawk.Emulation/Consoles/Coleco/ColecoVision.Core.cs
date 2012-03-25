87a2a3cc3e2639e3d8929286d7f6754994c783de
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-03-25 01:33:05 +0000

    Start colecohawk (skeleton)

---
+++ trunk/BizHawk.Emulation/Consoles/Coleco/ColecoVision.Core.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BizHawk.Emulation.CPUs.Z80;
+using BizHawk.Emulation.Sound;
+
+namespace BizHawk.Emulation.Consoles.Coleco
+{
+	public partial class ColecoVision : IEmulator
+	{
+		public byte[] rom;
+		public Z80A cpu;
+
+		public byte ReadMemory(ushort addr)
+		{
+			return 0xFF;
+		}
+
+		public void WriteMemory(ushort addr, byte value)
+		{
+			return;
+		}
+
+		public void HardReset()
+		{
+
+		}
+
+		public void FrameAdvance(bool render)
+		{
+
+		}
+	}
+}
