d64b41829c09fe6e542451f0ca9d4bfab90afde3
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-03-31 20:53:14 +0000

    Atari - go ahead and hook up empty mappers for all 20 mappers

---
+++ trunk/BizHawk.Emulation/Consoles/Atari/2600/Mappers/mF0.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BizHawk.Emulation.Consoles.Atari._2600
+{
+	class mF0 : MapperBase 
+	{
+
+	}
+}
