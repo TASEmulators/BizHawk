6c1fc6259b7370bf8d25cf98214581715ba0132b
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-04-02 16:51:41 +0000

    Atari - F0 Mapper implemented (probably, needs testing)

--- trunk/BizHawk.Emulation/Consoles/Atari/2600/Mappers/mF0.cs
+++ trunk/BizHawk.Emulation/Consoles/Atari/2600/Mappers/mF0.cs
@@ -5,8 +5,50 @@ using System.Text;
 
 namespace BizHawk.Emulation.Consoles.Atari._2600
 {
+	/*
+	* F0 (Megaboy)
+	-----
+
+	This was used on one game, "megaboy".. Some kind of educational cartridge.  It supports
+	64K of ROM making it the biggest single production game made during the original run
+	of the 2600.
+
+	Bankswitching is very simple. There's 16 4K banks, and accessing 1FF0 causes the bank 
+	number to increment.
+
+	This means that you must keep accessing 1FF0 until the bank you want is selected.  Each
+	bank is numbered by means of one of the ROM locations, and the code simply keeps accessing
+	1FF0 until the bank it is looking for comes up.
+	*/
+
 	class mF0 : MapperBase 
 	{
+		int bank = 0;
 
+		public override byte ReadMemory(ushort addr)
+		{
+			if (addr == 0x0220)
+				Increment();
+			
+			if (addr < 0x1000) return base.ReadMemory(addr);
+			else return core.rom[bank * 4 * 1024 + (addr & 0xFFF)];
+		}
+		public override void WriteMemory(ushort addr, byte value)
+		{
+			if (addr < 0x1000) base.WriteMemory(addr, value);
+		}
+
+		public override void SyncState(Serializer ser)
+		{
+			base.SyncState(ser);
+			ser.Sync("bank", ref bank);
+		}
+
+		void Increment()
+		{
+			bank++;
+			if (bank > 15) 
+				bank = 0;
+		}
 	}
 }
