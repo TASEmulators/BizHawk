b3e79aa06f6b33f5830f19ffc3183b749b314d0a
beirich <beirich@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-04-14 16:02:45 +0000

    Fix input chain stuff so that genesis roms can be loaded without crashing client

--- BizHawk.MultiClient/MainForm.cs
+++ BizHawk.MultiClient/MainForm.cs
@@ -683,6 +683,17 @@ namespace BizHawk.MultiClient
 			genControls.BindMulti("P1 Start", Global.Config.GenesisController[0].Start);
 			Global.GenControls = genControls;
 
+            var agenControls = new AutofireController(Genesis.GenesisController);
+            agenControls.BindMulti("P1 Up", Global.Config.GenesisAutoController[0].Up);
+            agenControls.BindMulti("P1 Left", Global.Config.GenesisAutoController[0].Left);
+            agenControls.BindMulti("P1 Right", Global.Config.GenesisAutoController[0].Right);
+            agenControls.BindMulti("P1 Down", Global.Config.GenesisAutoController[0].Down);
+            agenControls.BindMulti("P1 A", Global.Config.GenesisAutoController[0].A);
+            agenControls.BindMulti("P1 B", Global.Config.GenesisAutoController[0].B);
+            agenControls.BindMulti("P1 C", Global.Config.GenesisAutoController[0].C);
+            agenControls.BindMulti("P1 Start", Global.Config.GenesisAutoController[0].Start);
+            Global.AutofireGenControls = agenControls;
+
 			var a2600Controls = new Controller(Atari2600.Atari2600ControllerDefinition);
 			a2600Controls.BindMulti("P1 Up", Global.Config.Atari2600Controller[0].Up);
 			a2600Controls.BindMulti("P1 Left", Global.Config.Atari2600Controller[0].Left);
