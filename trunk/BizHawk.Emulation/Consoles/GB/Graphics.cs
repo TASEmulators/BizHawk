5b2d0deb4a805ed4dcb0c84c205fedf338f60fdd
brandman211@gmail.com <brandman211@gmail.com@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-05-27 04:06:48 +0000

    -As I'm not going to know what I'm doing if I keep hacking away at zeromus' old core blindly, I'm writing a new one. --It's going to be based heavily based on Imran Nazar's "GameBoy Emulation in Javascript" series. I figure it's better that I learn by emulation (Har har) instead of spending more time reading references than programming. --Finished the memory management (Part 2). --adelikat: ---Do I implement the required functions for IEmulator and IVideoProvider now, do it later, or is this something you or zeromus would do (Like for the API)? ---At what point should we have the emulator actually use this core instead of zeromus'? As terrible as he says his is, this one doesn't do anything yet. Still, I need some mechanism of testing it.

---
+++ trunk/BizHawk.Emulation/Consoles/GB/Graphics.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BizHawk.Emulation.Consoles.GB
+{
+	public partial class GB
+	{
+		private byte[] OAM;
+		private byte[] VRAM;
+	}
+}
