2553ac50bb5fb663528a2531b694420a72fccaeb
brandman211 <brandman211@gmail.com>
2012-05-27 04:06:48 +0000

    -As I'm not going to know what I'm doing if I keep hacking away at zeromus' old core blindly, I'm writing a new one. --It's going to be based heavily based on Imran Nazar's "GameBoy Emulation in Javascript" series. I figure it's better that I learn by emulation (Har har) instead of spending more time reading references than programming. --Finished the memory management (Part 2). --adelikat: ---Do I implement the required functions for IEmulator and IVideoProvider now, do it later, or is this something you or zeromus would do (Like for the API)? ---At what point should we have the emulator actually use this core instead of zeromus'? As terrible as he says his is, this one doesn't do anything yet. Still, I need some mechanism of testing it.

---
+++ BizHawk.Emulation/Consoles/GB/GB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BizHawk.Emulation.CPUs.Z80GB;
+
+/*
+This Game Boy core was written using Imran Nazar's "GameBoy Emulation in
+Javascript" series (http://imrannazar.com/GameBoy-Emulation-in-JavaScript) and
+contains several comments from the articles.
+*/
+namespace BizHawk.Emulation.Consoles.GB
+{
+	public partial class GB/* : IEmulator, IVideoProvider */
+	{
+		private Z80 CPU;
+
+		public GB(GameInfo game, byte[] rom, bool skipBIOS)
+		{
+			inBIOS = !skipBIOS;
+			HardReset();
+		}
+
+		public void HardReset()
+		{
+			CPU = new CPUs.Z80GB.Z80();
+			CPU.ReadMemory = ReadMemory;
+			CPU.WriteMemory = WriteMemory;
+		}
+	}
+}
