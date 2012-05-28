e01c808906ff67dfd778429da37d9a5e63d14b5d
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-05-28 03:09:47 +0000

    Slight UI fixes - show mapped hotkey for Open TAStudio on TAStudio menu item, fix alignment of a few boxes in the hotkey config

--- trunk/BizHawk.MultiClient/MainForm.MenuItems.cs
+++ trunk/BizHawk.MultiClient/MainForm.MenuItems.cs
@@ -533,6 +533,7 @@ namespace BizHawk.MultiClient
 			hexEditorToolStripMenuItem.ShortcutKeyDisplayString = Global.Config.HexEditor;
 			luaConsoleToolStripMenuItem.ShortcutKeyDisplayString = Global.Config.LuaConsole;
 			cheatsToolStripMenuItem.ShortcutKeyDisplayString = Global.Config.Cheats;
+			tAStudioToolStripMenuItem.ShortcutKeyDisplayString = Global.Config.TASTudio;
 		}
 
 		private void saveSlotToolStripMenuItem_DropDownOpened(object sender, EventArgs e)
