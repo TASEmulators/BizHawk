b7adcf213737bf4ecc139ee4e0c134a499251ad0
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-06-24 03:45:56 +0000

    As usual, I forgot to check in new winform files

---
+++ trunk/BizHawk.MultiClient/tools/HexFind.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BizHawk.MultiClient
+{
+	public partial class HexFind : Form
+	{
+		private Point location;
+		public HexFind()
+		{
+			InitializeComponent();
+		}
+
+		public void SetInitialValue(string value)
+		{
+			FindBox.Text = value;
+		}
+
+		public void SetLocation(Point p)
+		{
+			location = p;
+			
+		}
+
+		private void HexFind_Load(object sender, EventArgs e)
+		{
+			if (location.X > 0 && location.Y > 0)
+				this.Location = location;
+		}
+
+		private void Find_Prev_Click(object sender, EventArgs e)
+		{
+			Global.MainForm.HexEditor1.FindPrev(FindBox.Text);
+		}
+
+		private void Find_Next_Click(object sender, EventArgs e)
+		{
+			Global.MainForm.HexEditor1.FindNext(FindBox.Text);
+		}
+	}
+}
