2457c68ed7537a2e22d9821ddf80d4e644229ce0
brandman211 <brandman211@gmail.com>
2012-07-08 00:36:16 +0000

    Starting IntelliHawk with the CP1610, using the other processors in BizHawk, BlissJ, and this (http://wiki.intellivision.us/index.php?title=CP1610) page as references. -Definitions. --Registers, Flags, TotalExecutedCycles, PendingCycles, ReadMemory, and WriteMemory. -Execute. --Implemented opcodes 0x001-0x027 with the exception of 0x004 and 0x005.

---
+++ BizHawk.Emulation/CPUs/CP1610/CP1610.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BizHawk.Emulation.CPUs.CP1610
+{
+	public sealed partial class CP1610
+	{
+		private bool FlagS, FlagC, FlagZ, FlagO, FlagI, FlagD;
+		private ushort[] Register = new ushort[8];
+		public ushort RegisterSP { get { return Register[6]; } set { Register[6] = value; } }
+		public ushort RegisterPC { get { return Register[7]; } set { Register[7] = value; } }
+
+		public int TotalExecutedCycles;
+		public int PendingCycles;
+
+		public Func<ushort, byte> ReadMemory;
+		public Action<ushort, byte> WriteMemory;
+	}
+}
