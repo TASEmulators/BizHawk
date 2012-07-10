df9caf738b29f1d03b2848a37dadce3b3980cbda
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-07-10 19:04:35 +0000

    Lua - start forms creation library, implement forms.create(), forms.destroy(), and forms.destroyall().  Still TODO: make the forms do something!  Also added console.log as an alias to console.output

---
+++ trunk/BizHawk.MultiClient/tools/LuaWinform.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using LuaInterface;
+
+namespace BizHawk.MultiClient.tools
+{
+	public partial class LuaWinform : Form
+	{
+		public List<LuaFunction> Events = new List<LuaFunction>();
+
+		public LuaWinform()
+		{
+			InitializeComponent();
+			Closing += (o, e) => CloseThis();
+		}
+
+		private void LuaWinform_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		public void CloseThis()
+		{
+			Global.MainForm.LuaConsole1.LuaImp.WindowClosed(Handle);
+		}
+	}
+}
