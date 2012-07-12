45227b1b11adde368169a70d8f0453a4c7e14f56
andres.delikat <andres.delikat@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-07-12 02:03:46 +0000

    Lua - Lua Function List - column sorting (including descending)

--- BizHawk.MultiClient/tools/LuaFunctionList.Designer.cs
+++ BizHawk.MultiClient/tools/LuaFunctionList.Designer.cs
@@ -32,8 +32,8 @@
 			this.OK = new System.Windows.Forms.Button();
 			this.directoryEntry1 = new System.DirectoryServices.DirectoryEntry();
 			this.FunctionView = new System.Windows.Forms.ListView();
-			this.LibraryHead = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
 			this.LibraryReturn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.LibraryHead = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
 			this.LibraryName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
 			this.LibraryParameters = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
 			this.SuspendLayout();
@@ -41,7 +41,7 @@
 			// OK
 			// 
 			this.OK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.OK.Location = new System.Drawing.Point(423, 287);
+			this.OK.Location = new System.Drawing.Point(423, 284);
 			this.OK.Name = "OK";
 			this.OK.Size = new System.Drawing.Size(75, 23);
 			this.OK.TabIndex = 0;
@@ -62,20 +62,21 @@
 			this.FunctionView.GridLines = true;
 			this.FunctionView.Location = new System.Drawing.Point(12, 12);
 			this.FunctionView.Name = "FunctionView";
-			this.FunctionView.Size = new System.Drawing.Size(486, 253);
+			this.FunctionView.Size = new System.Drawing.Size(486, 266);
 			this.FunctionView.TabIndex = 1;
 			this.FunctionView.UseCompatibleStateImageBehavior = false;
 			this.FunctionView.View = System.Windows.Forms.View.Details;
+			this.FunctionView.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.FunctionView_ColumnClick);
+			// 
+			// LibraryReturn
+			// 
+			this.LibraryReturn.Text = "Return";
 			// 
 			// LibraryHead
 			// 
 			this.LibraryHead.Text = "Library";
 			this.LibraryHead.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
 			this.LibraryHead.Width = 75;
-			// 
-			// LibraryReturn
-			// 
-			this.LibraryReturn.Text = "Return";
 			// 
 			// LibraryName
 			// 
@@ -92,7 +93,7 @@
 			this.AcceptButton = this.OK;
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(510, 322);
+			this.ClientSize = new System.Drawing.Size(510, 319);
 			this.Controls.Add(this.FunctionView);
 			this.Controls.Add(this.OK);
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
