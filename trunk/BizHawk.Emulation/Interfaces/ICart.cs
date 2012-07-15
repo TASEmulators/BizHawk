eb8c08585e1871b5bd875ba3d6c2279de9204665
brandman211@gmail.com <brandman211@gmail.com@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-07-15 08:38:50 +0000

    -Made Executive ROM and Graphics ROM read-only. I still haven't made the memory map accessibility limited by the VBlank Period, but I'm assuming that should come way later. -Initialized the memory devices with a tentative size that ignores the unofficial ranges. -Masked addresses to match those sizes (That's my understanding of what the memory map needs to do based on other examples). -Added the ICart interface. -Started the Intellicart parser; got far enough to know that the files I'm working with are not Intellicarts. ^_^

---
+++ trunk/BizHawk.Emulation/Interfaces/ICart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BizHawk
+{
+	public interface ICart
+	{
+		ushort ReadMemory(ushort addr, out bool responded);
+		void WriteMemory(ushort addr, ushort value, out bool responded);
+	}
+}
