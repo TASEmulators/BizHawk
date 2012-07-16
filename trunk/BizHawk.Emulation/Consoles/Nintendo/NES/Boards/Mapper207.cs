3bb0a3d3a2d42bf9be027f8cb36c633fd425482e
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-07-16 01:51:16 +0000

    Implement Mapper 207, Fudou Myouou Den now playable but graphical issues.

--- trunk/BizHawk.Emulation/Consoles/Nintendo/NES/Boards/Mapper207.cs
+++ trunk/BizHawk.Emulation/Consoles/Nintendo/NES/Boards/Mapper207.cs
@@ -1,24 +1,37 @@
 ﻿using System;
-using System.IO;
-using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace BizHawk.Emulation.Consoles.Nintendo
 {
-	//AKA mapper 80
-
-	//Minelvaton Saga
-	//Taito Grand Prix - Eikou heno License
-	//Kyuukyoku Harikiri Stadium
-
 	/*
-	* Registers:
+	*  Here are Disch's original notes:  
+	========================
+	=  Mapper 207          =
+	========================
+
+	Example Game:
+	--------------------------
+	Fudou Myouou Den
+
+	Notes:
+	---------------------------
+	Regs appear at $7EFx, I'm unsure whether or not PRG-RAM can exist at $6000-7FFF
+
+	Registers:
 	---------------------------
 
-	$7EF0-7EF5:  CHR Regs
+	$7EF0:  [MCCC CCCC]
+	M = Mirroring 0
+	C = CHR Reg 0
 
-	$7EF6:  [.... ...M]  Mirroring
-	0 = Horz
-	1 = Vert
+	$7EF1:  [MCCC CCCC]
+	M = Mirroring 1
+	C = CHR Reg 1
+
+	$7EF2-7EF5:  CHR Regs 2-5
+
 
 	$7EFA,7EFB:  PRG Reg 0 (8k @ $8000)
 	$7EFC,7EFD:  PRG Reg 1 (8k @ $A000)
@@ -27,32 +40,44 @@ namespace BizHawk.Emulation.Consoles.Nintendo
 
 	CHR Setup:
 	---------------------------
-
+ 
 	$0000   $0400   $0800   $0C00   $1000   $1400   $1800   $1C00 
 	+---------------+---------------+-------+-------+-------+-------+
 	|    <$7EF0>    |    <$7EF1>    | $7EF2 | $7EF3 | $7EF4 | $7EF5 |
 	+---------------+---------------+-------+-------+-------+-------+
-
+ 
 	PRG Setup:
 	---------------------------
-
+ 
 	$8000   $A000   $C000   $E000  
 	+-------+-------+-------+-------+
 	| $7EFA | $7EFC | $7EFE | { -1} |
 	+-------+-------+-------+-------+
-	*/
 
-	class TAITO_X1_005 : NES.NESBoardBase
+	Mirroring:
+	---------------------------
+
+	[ $7EF0 ][ $7EF0 ]
+	[ $7EF1 ][ $7EF1 ]
+
+	Mirroring bit of appropriate reg selects NTA or NTB
+	*/
+	class Mapper207 : NES.NESBoardBase
 	{
 		int prg_bank_mask, chr_bank_mask;
 		ByteBuffer chr_regs_1k = new ByteBuffer(8);
 		ByteBuffer prg_regs_8k = new ByteBuffer(4);
+		bool mirror0 = false;
+		bool mirror1 = false;
 
 		public override void SyncState(Serializer ser)
 		{
 			base.SyncState(ser);
 			ser.Sync("chr_regs_1k", ref chr_regs_1k);
 			ser.Sync("prg_regs_8k", ref prg_regs_8k);
+			ser.Sync("mirror0", ref mirror0);
+			ser.Sync("mirror1", ref mirror1);
+			DoMirroring();
 		}
 
 		public override void Dispose()
@@ -67,19 +92,19 @@ namespace BizHawk.Emulation.Consoles.Nintendo
 			//configure
 			switch (Cart.board_type)
 			{
-				case "MAPPER080":
+				case "MAPPER207":
 					break;
 				case "TAITO-X1-005":
 					if (Cart.mapper == 207)
 					{
-						return false;
+						break;
 					}
-					break;
+					return false;
 				default:
 					return false;
 			}
 
-			SetMirrorType(EMirrorType.Vertical);
+			SetMirrorType(EMirrorType.Horizontal);
 			chr_bank_mask = Cart.chr_size / 1 - 1;
 			prg_bank_mask = Cart.prg_size / 8 - 1;
 			prg_regs_8k[3] = 0xFF;
@@ -90,20 +115,17 @@ namespace BizHawk.Emulation.Consoles.Nintendo
 		{
 			switch (addr)
 			{
-				case 0x1EF6:
-					if (value.Bit(0))
-						SetMirrorType(EMirrorType.Vertical);
-					else
-						SetMirrorType(EMirrorType.Horizontal);
-					break;
-
 				case 0x1EF0:
-					chr_regs_1k[0] = (byte)(value/2*2);
-					chr_regs_1k[1] = (byte)(value/2*2+1);
+					mirror0 = value.Bit(7);
+					int val1 = value & 0x7F;
+					chr_regs_1k[0] = (byte)(val1/2*2);
+					chr_regs_1k[1] = (byte)(val1/2*2+1);
 					break;
 				case 0x1EF1:
-					chr_regs_1k[2] = (byte)(value/2*2);
-					chr_regs_1k[3] = (byte)(value/2*2+1);
+					mirror1 = value.Bit(7);
+					int val2 = value & 0x7F;
+					chr_regs_1k[2] = (byte)(val2/2*2);
+					chr_regs_1k[3] = (byte)(val2/2*2+1);
 					break;
 				
 				case 0x1EF2:
@@ -132,6 +154,28 @@ namespace BizHawk.Emulation.Consoles.Nintendo
 					prg_regs_8k[2] = value;
 					break;
 			}
+			DoMirroring();
+		}
+
+		private void DoMirroring()
+		{
+			if (mirror0 == false && mirror1 == false)
+			{
+				SetMirrorType(EMirrorType.Horizontal);
+			}
+			else if (mirror0 == true && mirror1 == false)
+			{
+				SetMirrorType(EMirrorType.Vertical);
+			}
+			else if (mirror0 == false && mirror1 == true)
+			{
+				SetMirrorType(EMirrorType.OneScreenA);
+			}
+			else
+			{
+				SetMirrorType(EMirrorType.OneScreenB);
+			}
+			
 		}
 
 		public override byte ReadPRG(int addr)
@@ -148,6 +192,7 @@ namespace BizHawk.Emulation.Consoles.Nintendo
 		{
 			if (addr < 0x2000)
 			{
+				
 				int bank_1k = addr >> 10;
 				int ofs = addr & ((1 << 10) - 1);
 				bank_1k = chr_regs_1k[bank_1k];
@@ -159,20 +204,3 @@ namespace BizHawk.Emulation.Consoles.Nintendo
 		}
 	}
 }
-
-
-
-
-
-
-/*
-		 CHR Setup:
-		 ---------------------------
- 
-						 $0000   $0400   $0800   $0C00   $1000   $1400   $1800   $1C00 
-					   +---------------+---------------+-------+-------+-------+-------+
-		 CHR Mode 0:   |    <$7EF0>    |    <$7EF1>    | $7EF2 | $7EF3 | $7EF4 | $7EF5 |
-					   +---------------+---------------+---------------+---------------+
-		 CHR Mode 1:   | $7EF2 | $7EF3 | $7EF4 | $7EF5 |    <$7EF0>    |    <$7EF1>    |
-					   +-------+-------+-------+-------+---------------+---------------+
- */
