9b97a951841113c99bf441316a5aee42206903b3
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-07-22 22:24:02 +0000

    Lua Writer - implement saving and fix so that the text only redraws when it needs to.

--- trunk/BizHawk.MultiClient/tools/LuaWriter.Designer.cs
+++ trunk/BizHawk.MultiClient/tools/LuaWriter.Designer.cs
@@ -29,20 +29,21 @@
         private void InitializeComponent()
         {
 			this.components = new System.ComponentModel.Container();
-			this.LuaText = new BizHawk.MultiClient.LuaWriterBox();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LuaWriter));
 			this.timer = new System.Windows.Forms.Timer(this.components);
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.MessageLabel = new System.Windows.Forms.Label();
+			this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.configToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.syntaxHighlightingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.fontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.LuaText = new BizHawk.MultiClient.LuaWriterBox();
+			this.menuStrip1.SuspendLayout();
 			this.SuspendLayout();
-			// 
-			// LuaText
-			// 
-			this.LuaText.AcceptsTab = true;
-			this.LuaText.Location = new System.Drawing.Point(12, 12);
-			this.LuaText.Name = "LuaText";
-			this.LuaText.Size = new System.Drawing.Size(819, 417);
-			this.LuaText.TabIndex = 0;
-			this.LuaText.Text = "";
-			this.LuaText.WordWrap = false;
-			this.LuaText.ZoomFactor = 2F;
 			// 
 			// timer
 			// 
@@ -50,19 +51,124 @@
 			this.timer.Interval = 1000;
 			this.timer.Tick += new System.EventHandler(this.timer_Tick);
 			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.configToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(843, 24);
+			this.menuStrip1.TabIndex = 1;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+			this.fileToolStripMenuItem.Text = "&File";
+			// 
+			// saveToolStripMenuItem
+			// 
+			this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+			this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+			this.saveToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+			this.saveToolStripMenuItem.Text = "&Save";
+			this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+			// 
+			// toolStripSeparator1
+			// 
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			this.toolStripSeparator1.Size = new System.Drawing.Size(192, 6);
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.ShortcutKeyDisplayString = "Alt+F4";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+			this.exitToolStripMenuItem.Text = "E&xit";
+			// 
+			// MessageLabel
+			// 
+			this.MessageLabel.AutoSize = true;
+			this.MessageLabel.Location = new System.Drawing.Point(16, 424);
+			this.MessageLabel.Name = "MessageLabel";
+			this.MessageLabel.Size = new System.Drawing.Size(91, 13);
+			this.MessageLabel.TabIndex = 2;
+			this.MessageLabel.Text = "                            ";
+			// 
+			// saveAsToolStripMenuItem
+			// 
+			this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+			this.saveAsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.S)));
+			this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+			this.saveAsToolStripMenuItem.Text = "Save &As...";
+			this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+			// 
+			// configToolStripMenuItem
+			// 
+			this.configToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fontToolStripMenuItem,
+            this.syntaxHighlightingToolStripMenuItem});
+			this.configToolStripMenuItem.Name = "configToolStripMenuItem";
+			this.configToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+			this.configToolStripMenuItem.Text = "&Config";
+			// 
+			// syntaxHighlightingToolStripMenuItem
+			// 
+			this.syntaxHighlightingToolStripMenuItem.Enabled = false;
+			this.syntaxHighlightingToolStripMenuItem.Name = "syntaxHighlightingToolStripMenuItem";
+			this.syntaxHighlightingToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+			this.syntaxHighlightingToolStripMenuItem.Text = "Syntax Highlighting";
+			// 
+			// fontToolStripMenuItem
+			// 
+			this.fontToolStripMenuItem.Enabled = false;
+			this.fontToolStripMenuItem.Name = "fontToolStripMenuItem";
+			this.fontToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+			this.fontToolStripMenuItem.Text = "Font";
+			// 
+			// LuaText
+			// 
+			this.LuaText.AcceptsTab = true;
+			this.LuaText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.LuaText.EnableAutoDragDrop = true;
+			this.LuaText.Location = new System.Drawing.Point(24, 27);
+			this.LuaText.Name = "LuaText";
+			this.LuaText.Size = new System.Drawing.Size(819, 392);
+			this.LuaText.TabIndex = 0;
+			this.LuaText.Text = "";
+			this.LuaText.WordWrap = false;
+			this.LuaText.ZoomFactor = 2F;
+			this.LuaText.TextChanged += new System.EventHandler(this.LuaText_TextChanged);
+			this.LuaText.KeyUp += new System.Windows.Forms.KeyEventHandler(this.LuaText_KeyUp);
+			// 
 			// LuaWriter
 			// 
 			this.AllowDrop = true;
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(843, 441);
+			this.Controls.Add(this.MessageLabel);
 			this.Controls.Add(this.LuaText);
+			this.Controls.Add(this.menuStrip1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MainMenuStrip = this.menuStrip1;
 			this.Name = "LuaWriter";
 			this.Text = "LuaWriter";
 			this.Load += new System.EventHandler(this.LuaWriter_Load);
 			this.DragDrop += new System.Windows.Forms.DragEventHandler(this.LuaWriter_DragDrop);
 			this.DragEnter += new System.Windows.Forms.DragEventHandler(this.LuaWriter_DragEnter);
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -70,5 +176,15 @@
 
         private LuaWriterBox LuaText;
 		private System.Windows.Forms.Timer timer;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+		private System.Windows.Forms.Label MessageLabel;
+		private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem configToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem fontToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem syntaxHighlightingToolStripMenuItem;
     }
 }
--- trunk/BizHawk.MultiClient/tools/LuaWriter.cs
+++ trunk/BizHawk.MultiClient/tools/LuaWriter.cs
@@ -14,9 +14,14 @@ namespace BizHawk.MultiClient
 	public partial class LuaWriter : Form
 	{
 		public string CurrentFile = "";
+
+		bool changes = false;
+		bool hasChanged;
+		public Regex keyWords = new Regex("and|break|do|else|if|end|false|for|function|in|local|nil|not|or|repeat|return|then|true|until|while|elseif");
 		
         Regex keyWords = new Regex("and|break|do|else|if|end|false|for|function|in|local|nil|not|or|repeat|return|then|true|until|while|elseif");
         char[] Symbols = { '+','-','*','/','%','^','#','=','<','>','(',')','{','}','[',']',';',':',',','.' };
+
 
 		public LuaWriter()
 		{
@@ -25,6 +30,18 @@ namespace BizHawk.MultiClient
 
 		private void timer_Tick(object sender, EventArgs e)
 		{
+			if (!hasChanged)
+			{
+				return;
+			}
+
+			ProcessText();
+
+			hasChanged = false;
+		}
+
+		private void ProcessText()
+		{
 			int selPos = LuaText.SelectionStart;
 			int selChars = LuaText.SelectedText.Length;
 
@@ -32,11 +49,8 @@ namespace BizHawk.MultiClient
 			LuaText.SelectionColor = Color.FromArgb(Global.Config.LuaDefaultTextColor);
 
 			ColorReservedWords();
-
 			ColorComments();
-
 			ColorStrings();
-
             ColorSymbols();
 
 			LuaText.Select(selPos, selChars);
@@ -167,7 +181,7 @@ namespace BizHawk.MultiClient
 				}
 			}
 		}
-
+		
 		private void ColorReservedWords()
 		{
 			foreach (Match keyWordMatch in keyWords.Matches(LuaText.Text))
@@ -195,7 +209,15 @@ namespace BizHawk.MultiClient
 			if (!String.IsNullOrWhiteSpace(CurrentFile))
 			{
 				LoadCurrentFile();
+				ProcessText();
+				NoChanges();
 			}
+		}
+
+		private void NoChanges()
+		{
+			changes = false;
+			MessageLabel.Text = CurrentFile;
 		}
 
 		private void LoadCurrentFile()
@@ -221,6 +243,8 @@ namespace BizHawk.MultiClient
 					LuaText.Text = luaText.ToString();
 				}
 			}
+
+			MessageLabel.Text = CurrentFile;
 		}
 
 		private void LuaWriter_DragEnter(object sender, DragEventArgs e)
@@ -237,6 +261,96 @@ namespace BizHawk.MultiClient
 				CurrentFile = filePaths[0];
 				LoadCurrentFile();
 			}
+		}
+
+		private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			if (!String.IsNullOrWhiteSpace(CurrentFile))
+			{
+				SaveScript();
+			}
+			else if (changes)
+			{
+				SaveScriptAs();
+				MessageLabel.Text = Path.GetFileName(CurrentFile) + " saved.";
+			}
+		}
+
+		private void SaveScript()
+		{
+			var file = new FileInfo(CurrentFile);
+
+			using (StreamWriter sw = new StreamWriter(CurrentFile))
+			{
+				foreach (string s in LuaText.Lines)
+				{
+					sw.WriteLine(s + '\n');
+				}
+			}
+
+			NoChanges();
+		}
+
+		private void SaveScriptAs()
+		{
+			var file = GetSaveFileFromUser(CurrentFile);
+			if (file != null)
+			{
+				CurrentFile = file.FullName;
+				SaveScript();
+				MessageLabel.Text = Path.GetFileName(CurrentFile) + " saved.";
+				Global.Config.RecentWatches.Add(file.FullName);
+			}
+		}
+
+		public static FileInfo GetSaveFileFromUser(string currentFile)
+		{
+			var sfd = new SaveFileDialog();
+			if (currentFile.Length > 0)
+			{
+				sfd.FileName = Path.GetFileNameWithoutExtension(currentFile);
+				sfd.InitialDirectory = Path.GetDirectoryName(currentFile);
+			}
+			else if (!(Global.Emulator is NullEmulator))
+			{
+				sfd.FileName = PathManager.FilesystemSafeName(Global.Game);
+				sfd.InitialDirectory = PathManager.MakeAbsolutePath(Global.Config.LuaPath, "");
+			}
+			else
+			{
+				sfd.FileName = "NULL";
+				sfd.InitialDirectory = PathManager.MakeAbsolutePath(Global.Config.LuaPath, "");
+			}
+			sfd.Filter = "Watch Files (*.lua)|*.lua|All Files|*.*";
+			sfd.RestoreDirectory = true;
+			Global.Sound.StopSound();
+			var result = sfd.ShowDialog();
+			Global.Sound.StartSound();
+			if (result != DialogResult.OK)
+				return null;
+			var file = new FileInfo(sfd.FileName);
+			return file;
+		}
+
+		private void LuaText_KeyUp(object sender, KeyEventArgs e)
+		{
+			hasChanged = true;
+		}
+
+		private void Changes()
+		{
+			changes = true;
+			MessageLabel.Text = CurrentFile + " *";
+		}
+
+		private void LuaText_TextChanged(object sender, EventArgs e)
+		{
+			Changes();
+		}
+
+		private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			SaveScriptAs();
 		}
 	}
 }
