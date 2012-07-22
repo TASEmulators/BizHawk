e56de584e397d6b117781d98882a19caa0e710f5
adelikat <adelikat@tasvideos.org>
2012-07-22 19:54:40 +0000

    Lua Writer - faster drawing

--- BizHawk.MultiClient/tools/LuaWriter.Designer.cs
+++ BizHawk.MultiClient/tools/LuaWriter.Designer.cs
@@ -28,43 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.LuaText = new System.Windows.Forms.RichTextBox();
-            this.timer = new System.Windows.Forms.Timer(this.components);
-            this.SuspendLayout();
-            // 
-            // LuaText
-            // 
-            this.LuaText.AcceptsTab = true;
-            this.LuaText.Location = new System.Drawing.Point(12, 12);
-            this.LuaText.Name = "LuaText";
-            this.LuaText.Size = new System.Drawing.Size(819, 417);
-            this.LuaText.TabIndex = 0;
-            this.LuaText.Text = "";
-            this.LuaText.WordWrap = false;
-            this.LuaText.ZoomFactor = 2F;
-            // 
-            // timer
-            // 
-            this.timer.Enabled = true;
-            this.timer.Interval = 1000;
-            this.timer.Tick += new System.EventHandler(this.timer_Tick);
-            // 
-            // LuaWriter
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(843, 441);
-            this.Controls.Add(this.LuaText);
-            this.Name = "LuaWriter";
-            this.Text = "LuaWriter";
-            this.ResumeLayout(false);
+			this.components = new System.ComponentModel.Container();
+			this.LuaText = new LuaWriterBox();
+			this.timer = new System.Windows.Forms.Timer(this.components);
+			this.SuspendLayout();
+			// 
+			// LuaText
+			// 
+			this.LuaText.AcceptsTab = true;
+			this.LuaText.Location = new System.Drawing.Point(12, 12);
+			this.LuaText.Name = "LuaText";
+			this.LuaText.Size = new System.Drawing.Size(819, 417);
+			this.LuaText.TabIndex = 0;
+			this.LuaText.Text = "";
+			this.LuaText.WordWrap = false;
+			this.LuaText.ZoomFactor = 2F;
+			// 
+			// timer
+			// 
+			this.timer.Enabled = true;
+			this.timer.Interval = 1000;
+			this.timer.Tick += new System.EventHandler(this.timer_Tick);
+			// 
+			// LuaWriter
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(843, 441);
+			this.Controls.Add(this.LuaText);
+			this.Name = "LuaWriter";
+			this.Text = "LuaWriter";
+			this.Load += new System.EventHandler(this.LuaWriter_Load);
+			this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.RichTextBox LuaText;
+        private LuaWriterBox LuaText;
 		private System.Windows.Forms.Timer timer;
     }
 }
