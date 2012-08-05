4cc242c571b38f5b26074cb847a145b00c9bc48e
brandman211@gmail.com <brandman211@gmail.com@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-08-05 06:47:00 +0000

    -Throw exceptions for TCI, SIN, and BEXT. -Added and logged INTRM, BUSRQ, BUSAK, and MSYNC.

--- trunk/BizHawk.Emulation/CPUs/CP1610/CP1610.cs
+++ trunk/BizHawk.Emulation/CPUs/CP1610/CP1610.cs
@@ -5,7 +5,7 @@ namespace BizHawk.Emulation.CPUs.CP1610
 {
 	public sealed partial class CP1610
 	{
-		private bool FlagS, FlagC, FlagZ, FlagO, FlagI, FlagD;
+		private bool FlagS, FlagC, FlagZ, FlagO, FlagI, FlagD, IntRM, BusRq, BusAk, MSync;
 		private ushort[] Register = new ushort[8];
 		public ushort RegisterSP { get { return Register[6]; } set { Register[6] = value; } }
 		public ushort RegisterPC { get { return Register[7]; } set { Register[7] = value; } }
@@ -33,12 +33,16 @@ namespace BizHawk.Emulation.CPUs.CP1610
 				log.WriteLine("R{0:d} = {1:X4}", register, Register[register]);
 			log.WriteLine("SP = {0:X4}", RegisterSP);
 			log.WriteLine("PC = {0:X4}", RegisterPC);
-			log.WriteLine("S = {0:X4}", FlagS);
-			log.WriteLine("C = {0:X4}", FlagC);
-			log.WriteLine("Z = {0:X4}", FlagZ);
-			log.WriteLine("O = {0:X4}", FlagO);
-			log.WriteLine("I = {0:X4}", FlagI);
-			log.WriteLine("D = {0:X4}", FlagD);
+			log.WriteLine("S = {0}", FlagS);
+			log.WriteLine("C = {0}", FlagC);
+			log.WriteLine("Z = {0}", FlagZ);
+			log.WriteLine("O = {0}", FlagO);
+			log.WriteLine("I = {0}", FlagI);
+			log.WriteLine("D = {0}", FlagD);
+			log.WriteLine("INTRM = {0}", IntRM);
+			log.WriteLine("BUSRQ = {0}", BusRq);
+			log.WriteLine("BUSAK = {0}", BusAk);
+			log.WriteLine("MSYNC = {0}", MSync);
 			log.Flush();
 		}
 	}
