9cb825418fae3758e013b72f81e772082698d275
adelikat <adelikat@tasvideos.org>
2012-08-11 01:50:27 +0000

    Lua Writer - take over paint event and only paint once per ProcessText().  Still todo: better management of when and what to draw

--- BizHawk.MultiClient/tools/LuaWriterBox.cs
+++ BizHawk.MultiClient/tools/LuaWriterBox.cs
@@ -3,15 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+
+
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
 
 namespace BizHawk.MultiClient
 {
 	class LuaWriterBox : RichTextBox
 	{
+		public bool InhibitPaint = false;
+
 		public LuaWriterBox()
 		{
 			SetStyle(ControlStyles.AllPaintingInWmPaint, true);
 			SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
 		}
+
+		protected override void OnPaintBackground(PaintEventArgs pevent)
+		{
+			base.OnPaintBackground(pevent);
+		}
+
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			base.OnPaint(e);
+		}
+
+		#region win32interop
+
+		protected override void WndProc(ref System.Windows.Forms.Message m)
+		{
+			base.WndProc(ref m);
+			if (m.Msg == 0x000F && !InhibitPaint) //WM_PAINT
+			{
+				// raise the paint event
+				using (Graphics graphic = base.CreateGraphics())
+					OnPaint(new PaintEventArgs(graphic,
+					 base.ClientRectangle));
+			}
+
+		}
+
+		#endregion
+
+		
 	}
 }
