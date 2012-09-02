5c49a14344d54342dd5a9d1a88c662de50481e2a
adelikat <adelikat@tasvideos.org>
2012-09-02 14:33:30 +0000

    Revert the moving of lua running in the frame loop

--- BizHawk.MultiClient/MainForm.cs
+++ BizHawk.MultiClient/MainForm.cs
@@ -1898,6 +1898,9 @@ namespace BizHawk.MultiClient
 				if (!fff)
 				{
 					UpdateToolsBefore();
+#if WINDOWS
+					LuaConsole1.ResumeScripts(true);
+#endif
 				}
 
 				if ((DateTime.Now - runloop_second).TotalSeconds > 1)
@@ -1967,9 +1970,6 @@ namespace BizHawk.MultiClient
 				if (!fff)
 				{
 					UpdateToolsAfter();
-#if WINDOWS
-					LuaConsole1.ResumeScripts(true);
-#endif
 				}
 			}
 
