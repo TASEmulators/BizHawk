76c68bf6d549e72eeecda5770c755e4138c45a7e
goyuken <goyuken@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-09-12 23:45:42 +0000

    add "interpolate" button to dmg color select idea shamelessly ripped from bgb

--- BizHawk.Emulation/Consoles/Nintendo/Gameboy/ColorChooserForm.Designer.cs
+++ BizHawk.Emulation/Consoles/Nintendo/Gameboy/ColorChooserForm.Designer.cs
@@ -53,6 +53,9 @@
 			this.button1 = new System.Windows.Forms.Button();
 			this.button2 = new System.Windows.Forms.Button();
 			this.panel13 = new System.Windows.Forms.Panel();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
@@ -295,11 +298,44 @@
 			this.panel13.Size = new System.Drawing.Size(52, 70);
 			this.panel13.TabIndex = 24;
 			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(226, 38);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 25;
+			this.button3.Text = "Interpolate";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(226, 77);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 23);
+			this.button4.TabIndex = 26;
+			this.button4.Text = "Interpolate";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(226, 114);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(75, 23);
+			this.button5.TabIndex = 27;
+			this.button5.Text = "Interpolate";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.button5_Click);
+			// 
 			// ColorChooserForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(231, 259);
+			this.ClientSize = new System.Drawing.Size(315, 259);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
 			this.Controls.Add(this.panel13);
 			this.Controls.Add(this.button2);
 			this.Controls.Add(this.button1);
@@ -362,5 +398,8 @@
 		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.Button button2;
 		private System.Windows.Forms.Panel panel13;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
 	}
 }
