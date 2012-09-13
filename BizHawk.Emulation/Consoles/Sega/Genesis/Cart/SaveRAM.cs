2d2af1d14324e56d9efe2e880f3505873a25c9b2
beirich <beirich@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-09-13 04:15:25 +0000

    oops

---
+++ BizHawk.Emulation/Consoles/Sega/Genesis/Cart/SaveRAM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BizHawk.Emulation.Consoles.Sega
+{
+    partial class Genesis
+    {
+        bool SaveRamEnabled = false;
+        int SaveRamStartOffset;
+        int SaveRamEndOffset;
+        int SaveRamLength;
+
+        byte[] SaveRAM = new byte[0];
+
+        void InitializeSaveRam(GameInfo game)
+        {
+            if (game["SaveRamOffset"])
+            {
+                SaveRamEnabled = true;
+                SaveRamStartOffset = game.GetHexValue("SaveRamOffset");
+                SaveRamLength = game.GetHexValue("SaveRamLength");
+                SaveRamEndOffset = SaveRamStartOffset + SaveRamLength;
+                SaveRAM = new byte[SaveRamLength];
+            }
+        }
+
+        public byte[] ReadSaveRam // TODO if you're going to rename this to ReadSaveRam, refactor it to be a method, not a property.
+		{
+            get { return SaveRAM; }
+		}
+
+		public bool SaveRamModified { get; set; }
+    }
+}
