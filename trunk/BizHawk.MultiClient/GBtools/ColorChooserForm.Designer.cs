f1e75bc4532d9c3246c63ed50abc68fa392f540e
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-09-16 02:59:07 +0000

    GB Palette config - add a "Default" button

--- trunk/BizHawk.MultiClient/GBtools/ColorChooserForm.Designer.cs
+++ trunk/BizHawk.MultiClient/GBtools/ColorChooserForm.Designer.cs
@@ -52,6 +52,7 @@
 			this.buttonSave = new System.Windows.Forms.Button();
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.label4 = new System.Windows.Forms.Label();
+			this.DefaultButton = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// panel1
@@ -248,24 +249,25 @@
 			this.buttonLoad.Name = "buttonLoad";
 			this.buttonLoad.Size = new System.Drawing.Size(60, 23);
 			this.buttonLoad.TabIndex = 28;
-			this.buttonLoad.Text = "Load...";
+			this.buttonLoad.Text = "&Load...";
 			this.buttonLoad.UseVisualStyleBackColor = true;
 			this.buttonLoad.Click += new System.EventHandler(this.button6_Click);
 			// 
 			// buttonSave
 			// 
-			this.buttonSave.Location = new System.Drawing.Point(114, 137);
+			this.buttonSave.Location = new System.Drawing.Point(112, 137);
 			this.buttonSave.Name = "buttonSave";
 			this.buttonSave.Size = new System.Drawing.Size(60, 23);
 			this.buttonSave.TabIndex = 29;
-			this.buttonSave.Text = "Save...";
+			this.buttonSave.Text = "&Save...";
 			this.buttonSave.UseVisualStyleBackColor = true;
 			this.buttonSave.Click += new System.EventHandler(this.button7_Click);
 			// 
 			// textBox1
 			// 
-			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
-						| System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox1.BackColor = System.Drawing.SystemColors.Control;
 			this.textBox1.Location = new System.Drawing.Point(17, 195);
 			this.textBox1.Name = "textBox1";
 			this.textBox1.ReadOnly = true;
@@ -282,6 +284,16 @@
 			this.label4.TabIndex = 31;
 			this.label4.Text = "Current palette file:";
 			// 
+			// DefaultButton
+			// 
+			this.DefaultButton.Location = new System.Drawing.Point(178, 137);
+			this.DefaultButton.Name = "DefaultButton";
+			this.DefaultButton.Size = new System.Drawing.Size(60, 23);
+			this.DefaultButton.TabIndex = 32;
+			this.DefaultButton.Text = "&Default";
+			this.DefaultButton.UseVisualStyleBackColor = true;
+			this.DefaultButton.Click += new System.EventHandler(this.DefaultButton_Click);
+			// 
 			// ColorChooserForm
 			// 
 			this.AcceptButton = this.OK;
@@ -290,6 +302,7 @@
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.CancelButton = this.Cancel;
 			this.ClientSize = new System.Drawing.Size(310, 254);
+			this.Controls.Add(this.DefaultButton);
 			this.Controls.Add(this.label4);
 			this.Controls.Add(this.textBox1);
 			this.Controls.Add(this.buttonSave);
@@ -350,5 +363,6 @@
 		private System.Windows.Forms.Button buttonSave;
 		private System.Windows.Forms.TextBox textBox1;
 		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button DefaultButton;
 	}
 }
