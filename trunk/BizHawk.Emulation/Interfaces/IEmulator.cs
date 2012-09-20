439e9e31b3bdfa1160c8738fa25fcc74d9b1c594
goyuken <goyuken@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-09-20 20:00:14 +0000

    change previous revision to use default value param

--- trunk/BizHawk.Emulation/Interfaces/IEmulator.cs
+++ trunk/BizHawk.Emulation/Interfaces/IEmulator.cs
@@ -14,7 +14,7 @@ namespace BizHawk
 
 		// note that most cores expect you to call SoundProvider.GetSamples() after each FrameAdvance()
 		// please do this, even when rendersound = false
-		void FrameAdvance(bool render, bool rendersound);
+		void FrameAdvance(bool render, bool rendersound = true);
 
 		int Frame { get; }
 		int LagCount { get; set; }
