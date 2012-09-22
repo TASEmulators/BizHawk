a348acc1f2195a4abf05935a00747812125cd7be
goyuken <goyuken@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-09-22 00:44:59 +0000

    add GifWriter, an implementation of AnimatedGif as an IVideoWriter main advantage is that the emulator can be controlled while it records, like the others the parameters for it are a bit different though...

---
+++ BizHawk.MultiClient/AVOut/GifWriterForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BizHawk.MultiClient.AVOut
+{
+	public partial class GifWriterForm : Form
+	{
+		public GifWriterForm()
+		{
+			InitializeComponent();
+		}
+
+		public static GifWriter.GifToken DoTokenForm(IWin32Window parent)
+		{
+			using (var dlg = new GifWriterForm())
+			{
+				dlg.numericUpDown1.Value = Global.Config.GifWriterFrameskip;
+
+				var result = dlg.ShowDialog(parent);
+				if (result == DialogResult.OK)
+				{
+					Global.Config.GifWriterFrameskip = (int)dlg.numericUpDown1.Value;
+					return GifWriter.GifToken.LoadFromConfig();
+				}
+				else
+					return null;
+			}
+		}
+	}
+}
