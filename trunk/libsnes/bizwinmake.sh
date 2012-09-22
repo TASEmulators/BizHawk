61566f8e2000ab09aefe59b166a095095dafe9b3
zeromus@zeromus.org <zeromus@zeromus.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-09-22 05:02:43 +0000

    rename snes.dll to libsneshawk.dll so nobody confuses it with a stock dll; static link some libs, eliminating need for libstdc++-6.dll and libgcc_s_dw2-1.dll

--- trunk/libsnes/bizwinmake.sh
+++ trunk/libsnes/bizwinmake.sh
@@ -1,6 +1,6 @@
 cd bsnes
-export BIZWINCFLAGS="-I. -O3 -masm=intel -DLIBCO_IMPORT -DLIBCO_MSVC"
-export TARGET_LIBSNES_LIBDEPS="-L ../libco_msvc_win32/release/ -llibco_msvc_win32"
+export BIZWINCFLAGS="-I. -O3 -masm=intel -DLIBCO_IMPORT -DLIBCO_MSVC -static-libgcc -static-libstdc++"
+export TARGET_LIBSNES_LIBDEPS="-L ../libco_msvc_win32/release/ -llibco_msvc_win32  -static-libgcc -static-libstdc++"
 profile=compatibility platform=win target=libsnes make -e -j
 cd ..
-cp bsnes/out/snes.dll ../BizHawk.MultiClient/output
+cp bsnes/out/snes.dll ../BizHawk.MultiClient/output/libsneshawk.dll
