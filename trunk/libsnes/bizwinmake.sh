6bf00dc1a68bc8f31d096f6e8e840618f95678ed
zeromus@zeromus.org <zeromus@zeromus.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-09-30 22:27:12 +0000

    at some point i accidentally removed multicore -j flag from libsnes's bizwinmake.sh

--- trunk/libsnes/bizwinmake.sh
+++ trunk/libsnes/bizwinmake.sh
@@ -3,6 +3,6 @@ mkdir obj
 mkdir out
 export BIZWINCFLAGS="-I. -O3 -masm=intel -DLIBCO_IMPORT -DLIBCO_MSVC -static-libgcc -static-libstdc++"
 export TARGET_LIBSNES_LIBDEPS="-L ../libco_msvc_win32/release/ -llibco_msvc_win32  -static-libgcc -static-libstdc++"
-profile=compatibility platform=win target=libsnes make -e
+profile=compatibility platform=win target=libsnes make -e -j
 cd ..
 cp bsnes/out/snes.dll ../BizHawk.MultiClient/output/libsneshawk.dll
