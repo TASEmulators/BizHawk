108162a9444ef0136f246abb8c6f14bb2b936f74
adelikat <adelikat@tasvideos.org>
2012-09-30 23:20:50 +0000

    Controller Config - NES Pad proof of concept

--- BizHawk.MultiClient/config/ControllerConfig.cs
+++ BizHawk.MultiClient/config/ControllerConfig.cs
@@ -18,7 +18,48 @@ namespace BizHawk.MultiClient
 
 		private void ControllerConfig_Load(object sender, EventArgs e)
 		{
+			NESController1Panel.ControllerNumber = 1;
+			NESController1Panel.Autofire = false;
+			NESController2Panel.ControllerNumber = 2;
+			NESController2Panel.Autofire = false;
+			NESController3Panel.ControllerNumber = 3;
+			NESController3Panel.Autofire = false;
+			NESController4Panel.ControllerNumber = 4;
+			NESController4Panel.Autofire = false;
+			NESAutofire1Panel.ControllerNumber = 1;
+			NESAutofire1Panel.Autofire = false;
+			NESAutofire2Panel.ControllerNumber = 2;
+			NESAutofire2Panel.Autofire = false;
+			NESAutofire3Panel.ControllerNumber = 3;
+			NESAutofire3Panel.Autofire = false;
+			NESAutofire4Panel.ControllerNumber = 4;
+			NESAutofire4Panel.Autofire = false;
 
+			SetAutoTab(true);
+		}
+
+		private void SetAutoTab(bool setting)
+		{
+			foreach (Control control1 in tabControl1.TabPages)
+			{
+				if (control1 is TabControl)
+				{
+					foreach (Control control2 in (control1 as TabControl).TabPages)
+					{
+						if (control2 is InputWidget)
+						{
+							(control2 as InputWidget).AutoTab = setting;
+						}
+					}
+				}
+				else
+				{
+					if (control1 is InputWidget)
+					{
+						(control1 as InputWidget).AutoTab = setting;
+					}
+				}
+			}
 		}
 	}
 }
--- BizHawk.MultiClient/config/ControllerConfig/NESGamePad.cs
+++ BizHawk.MultiClient/config/ControllerConfig/NESGamePad.cs
@@ -5,11 +5,28 @@ using System.Text;
 using System.Windows.Forms;
 using System.Drawing;
 
-namespace BizHawk.MultiClient.config.ControllerConfig
+namespace BizHawk.MultiClient
 {
 	class NESGamePad : Panel
 	{
-		InputWidget UpBox = new InputWidget();
+		public InputWidget UpBox = new InputWidget();
+		public InputWidget DownBox = new InputWidget();
+		public InputWidget LeftBox = new InputWidget();
+		public InputWidget RightBox = new InputWidget();
+		public InputWidget ABox = new InputWidget();
+		public InputWidget BBox = new InputWidget();
+		public InputWidget SelectBox = new InputWidget();
+		public InputWidget StartBox = new InputWidget();
+
+		public Label UpLabel = new Label();
+		public Label DownLabel = new Label();
+		public Label LeftLabel = new Label();
+		public Label RightLabel = new Label();
+		public Label ALabel = new Label();
+		public Label BLabel = new Label();
+		public Label SelectLabel = new Label();
+		public Label StartLabel = new Label();
+
 		public int ControllerNumber = 1;
 		public bool Autofire = false;
 
@@ -20,6 +37,72 @@ namespace BizHawk.MultiClient.config.ControllerConfig
 			ControllerNumber = 1;
 
 			UpBox.Location = new Point(15, 15);
+			UpBox.Size = new Size(200, 23);
+
+			DownBox.Location = new Point(15, 45);
+			DownBox.Size = new Size(200, 23);
+
+			LeftBox.Location = new Point(15, 75);
+			LeftBox.Size = new Size(200, 23);
+
+			RightBox.Location = new Point(15, 105);
+			RightBox.Size = new Size(200, 23);
+
+			ABox.Location = new Point(15, 135);
+			ABox.Size = new Size(200, 23);
+
+			BBox.Location = new Point(15, 165);
+			BBox.Size = new Size(200, 23);
+
+			SelectBox.Location = new Point(15, 195);
+			SelectBox.Size = new Size(200, 23);
+
+			StartBox.Location = new Point(15, 225);
+			StartBox.Size = new Size(200, 23);
+
+			UpLabel.Text = "Up";
+			UpLabel.Location = new Point(220, 18);
+
+			DownLabel.Text = "Down";
+			DownLabel.Location = new Point(220, 48);
+
+			LeftLabel.Text = "Left";
+			LeftLabel.Location = new Point(220, 78);
+			
+			RightLabel.Text = "Right";
+			RightLabel.Location = new Point(220, 108);
+
+			ALabel.Text = "A";
+			ALabel.Location = new Point(220, 138);
+
+			BLabel.Text = "B";
+			BLabel.Location = new Point(220, 168);
+
+			SelectLabel.Text = "Select";
+			SelectLabel.Location = new Point(220, 198);
+
+			StartLabel.Text = "Start";
+			StartLabel.Location = new Point(220, 228);
+
+			this.Controls.Add(this.UpBox);
+			this.Controls.Add(this.DownBox);
+			this.Controls.Add(this.LeftBox);
+			this.Controls.Add(this.RightBox);
+			this.Controls.Add(this.ABox);
+			this.Controls.Add(this.BBox);
+			this.Controls.Add(this.SelectBox);
+			this.Controls.Add(this.StartBox);
+
+			this.Controls.Add(this.UpLabel);
+			this.Controls.Add(this.DownLabel);
+			this.Controls.Add(this.LeftLabel);
+			this.Controls.Add(this.RightLabel);
+			this.Controls.Add(this.ALabel);
+			this.Controls.Add(this.BLabel);
+			this.Controls.Add(this.SelectLabel);
+			this.Controls.Add(this.StartLabel);
+
+			this.BorderStyle = BorderStyle.None;
 		}
 
 
