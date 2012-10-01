d7a5c87e0af21fb0db19ae4fbc2d8ffd780cf525
adelikat <adelikat@tasvideos.org>
2012-10-01 00:08:52 +0000

    Oops, didn't mean to check that file in

--- BizHawk.MultiClient/MainForm.MenuItems.cs
+++ BizHawk.MultiClient/MainForm.MenuItems.cs
@@ -410,7 +410,7 @@ namespace BizHawk.MultiClient
 
 		private void controllersToolStripMenuItem_Click(object sender, EventArgs e)
 		{
-			if (true)
+			if (false)
 			{
 				ControllerConfig c = new ControllerConfig();
 				c.ShowDialog();
