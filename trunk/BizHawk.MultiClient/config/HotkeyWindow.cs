f8e1085d9d409684e3ac6751385211bec68ec59d
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-10-02 00:23:37 +0000

    Controller Config - make input widgets work, also consistent namespace on hotkey dialog (no ((intended)) functional change)

--- trunk/BizHawk.MultiClient/config/HotkeyWindow.cs
+++ trunk/BizHawk.MultiClient/config/HotkeyWindow.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
-namespace BizHawk.MultiClient.tools
+namespace BizHawk.MultiClient
 {
 	public partial class HotkeyWindow : Form
 	{
