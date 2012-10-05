b3a42e7dbaa25ca85772681ff3fdcb6c14164f2e
beirich <beirich@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-10-05 03:12:42 +0000

    GG: Add emulator support for showing clipped regions of GG games Note, not yet hooked up to UI.

--- trunk/BizHawk.Emulation/Interfaces/CoreComms.cs
+++ trunk/BizHawk.Emulation/Interfaces/CoreComms.cs
@@ -8,6 +8,7 @@ namespace BizHawk
 		public bool NES_ShowBG, NES_ShowOBJ;
 		public bool PCE_ShowBG1, PCE_ShowOBJ1, PCE_ShowBG2, PCE_ShowOBJ2;
 		public bool SMS_ShowBG, SMS_ShowOBJ;
+        public bool GG_ShowClippedRegions;
 
 		public string SNES_FirmwaresPath;
 		public bool SNES_ShowBG1_0, SNES_ShowBG2_0, SNES_ShowBG3_0, SNES_ShowBG4_0;
