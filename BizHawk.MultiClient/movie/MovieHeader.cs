0ca3fc316744976d1efd6cd790b12e5ccdad4935
adelikat <adelikat@tasvideos.org>
2012-10-06 17:22:32 +0000

    Movies - add PAL flag to movie (if recording in pal mode!)

--- BizHawk.MultiClient/movie/MovieHeader.cs
+++ BizHawk.MultiClient/movie/MovieHeader.cs
@@ -29,6 +29,7 @@ namespace BizHawk.MultiClient
 		public const string FOURSCORE = "FourScore";
 		public const string SHA1 = "SHA1";
 		public const string FIRMWARESHA1 = "FirmwareSHA1";
+		public const string PAL = "PAL";
 
 		//Gameboy Settings that affect sync
 		public const string GB_FORCEDMG = "Force_DMG_Mode";
