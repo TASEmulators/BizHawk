00e534e923519afb2e8e82c853807b1fa282740f
goyuken <goyuken@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-10-09 01:34:21 +0000

    gamedb stuffed into gamedb subfolder

--- trunk/BizHawk.MultiClient/output/MakeRelease.bat
+++ trunk/BizHawk.MultiClient/output/MakeRelease.bat
@@ -1,3 +1,3 @@
 del /s BizHawk.zip
 copy ..\..\SlimDx.dll
-zip -X -9 -r BizHawk.zip BizHawk.MultiClient.exe DiscoHawk.exe *.dll ffmpeg.exe NesCarts.7z gamedb.txt gamedb*.txt NES\Palettes Lua Gameboy\Palettes
+zip -X -9 -r BizHawk.zip BizHawk.MultiClient.exe DiscoHawk.exe *.dll ffmpeg.exe NesCarts.7z gamedb NES\Palettes Lua Gameboy\Palettes
