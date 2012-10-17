a5a3f7d9eec2c34141d8591e22c13c9dae76c507
goyuken <goyuken@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-10-17 16:48:28 +0000

    nes: unif mapper "BMC-NovelDiamond9999999in1".  possibly also INES mapper 54, but i've only seen this game in unif form.  One game: "Multi-Game Pirate Carts.7z|Novel Diamond 999999-in-1 [U][p1][!].unf"

---
+++ trunk/BizHawk.Emulation/Consoles/Nintendo/NES/Boards/NovelDiamond.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BizHawk.Emulation.Consoles.Nintendo
+{
+	public class NovelDiamond : NES.NESBoardBase
+	{
+		int prg;
+		int chr;
+
+		public override bool Configure(NES.EDetectionOrigin origin)
+		{
+			switch (Cart.board_type)
+			{
+				case "MAPPER054": // ??
+				case "UNIF_BMC-NovelDiamond9999999in1": // works
+					break;
+				default:
+					return false;
+			}
+			AssertPrg(128);
+			AssertChr(64);
+			SetMirrorType(Cart.pad_h, Cart.pad_v);
+			return true;
+		}
+
+		public override byte ReadPPU(int addr)
+		{
+			if (addr < 0x2000)
+				return VROM[addr | chr << 13];
+			else
+				return base.ReadPPU(addr);
+		}
+
+		public override byte ReadPRG(int addr)
+		{
+			return ROM[addr | prg << 15];
+		}
+
+		public override void WritePRG(int addr, byte value)
+		{
+			prg = addr & 3;
+			chr = addr & 7;
+		}
+
+		public override void SyncState(Serializer ser)
+		{
+			base.SyncState(ser);
+			ser.Sync("prg", ref prg);
+			ser.Sync("chr", ref chr);
+		}
+	}
+}
