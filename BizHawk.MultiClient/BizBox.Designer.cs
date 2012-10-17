279b6980fe4df46fb532c1f35ac7ce8e26988dfd
adelikat <adelikat@tasvideos.org>
2012-10-17 02:10:13 +0000

    Add authors, etc, for each core to the about box

--- BizHawk.MultiClient/BizBox.Designer.cs
+++ BizHawk.MultiClient/BizBox.Designer.cs
@@ -40,13 +40,34 @@
 			this.label5 = new System.Windows.Forms.Label();
 			this.label6 = new System.Windows.Forms.Label();
 			this.label7 = new System.Windows.Forms.Label();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label10 = new System.Windows.Forms.Label();
+			this.label11 = new System.Windows.Forms.Label();
+			this.label13 = new System.Windows.Forms.Label();
+			this.label14 = new System.Windows.Forms.Label();
+			this.label16 = new System.Windows.Forms.Label();
+			this.label18 = new System.Windows.Forms.Label();
+			this.label19 = new System.Windows.Forms.Label();
+			this.label17 = new System.Windows.Forms.Label();
+			this.label20 = new System.Windows.Forms.Label();
+			this.label21 = new System.Windows.Forms.Label();
+			this.label22 = new System.Windows.Forms.Label();
+			this.label23 = new System.Windows.Forms.Label();
+			this.label24 = new System.Windows.Forms.Label();
+			this.label25 = new System.Windows.Forms.Label();
+			this.label26 = new System.Windows.Forms.Label();
+			this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+			this.linkLabel3 = new System.Windows.Forms.LinkLabel();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.panel1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// OK
 			// 
 			this.OK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.OK.Location = new System.Drawing.Point(390, 180);
+			this.OK.Location = new System.Drawing.Point(390, 412);
 			this.OK.Name = "OK";
 			this.OK.Size = new System.Drawing.Size(75, 23);
 			this.OK.TabIndex = 0;
@@ -68,7 +89,7 @@
 			// 
 			this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.linkLabel1.AutoSize = true;
-			this.linkLabel1.Location = new System.Drawing.Point(198, 179);
+			this.linkLabel1.Location = new System.Drawing.Point(198, 411);
 			this.linkLabel1.Name = "linkLabel1";
 			this.linkLabel1.Size = new System.Drawing.Size(180, 13);
 			this.linkLabel1.TabIndex = 2;
@@ -78,7 +99,6 @@
 			// 
 			// label1
 			// 
-			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
 			this.label1.AutoSize = true;
 			this.label1.Location = new System.Drawing.Point(198, 84);
 			this.label1.Name = "label1";
@@ -88,7 +108,6 @@
 			// 
 			// label2
 			// 
-			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
 			this.label2.AutoSize = true;
 			this.label2.Location = new System.Drawing.Point(198, 98);
 			this.label2.Name = "label2";
@@ -126,9 +145,8 @@
 			// 
 			// label5
 			// 
-			this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.label5.AutoSize = true;
-			this.label5.Location = new System.Drawing.Point(198, 125);
+			this.label5.Location = new System.Drawing.Point(198, 131);
 			this.label5.Name = "label5";
 			this.label5.Size = new System.Drawing.Size(66, 13);
 			this.label5.TabIndex = 8;
@@ -136,9 +154,8 @@
 			// 
 			// label6
 			// 
-			this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.label6.AutoSize = true;
-			this.label6.Location = new System.Drawing.Point(198, 139);
+			this.label6.Location = new System.Drawing.Point(198, 145);
 			this.label6.Name = "label6";
 			this.label6.Size = new System.Drawing.Size(223, 13);
 			this.label6.TabIndex = 9;
@@ -146,20 +163,225 @@
 			// 
 			// label7
 			// 
-			this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.label7.AutoSize = true;
-			this.label7.Location = new System.Drawing.Point(198, 153);
+			this.label7.Location = new System.Drawing.Point(198, 159);
 			this.label7.Name = "label7";
 			this.label7.Size = new System.Drawing.Size(237, 13);
 			this.label7.TabIndex = 10;
 			this.label7.Text = "Deign, Brandon Evans, Pasky13, link_7777, natt";
+			// 
+			// panel1
+			// 
+			this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.panel1.Controls.Add(this.linkLabel3);
+			this.panel1.Controls.Add(this.linkLabel2);
+			this.panel1.Controls.Add(this.label24);
+			this.panel1.Controls.Add(this.label25);
+			this.panel1.Controls.Add(this.label26);
+			this.panel1.Controls.Add(this.label21);
+			this.panel1.Controls.Add(this.label22);
+			this.panel1.Controls.Add(this.label23);
+			this.panel1.Controls.Add(this.label17);
+			this.panel1.Controls.Add(this.label20);
+			this.panel1.Controls.Add(this.label18);
+			this.panel1.Controls.Add(this.label19);
+			this.panel1.Controls.Add(this.label14);
+			this.panel1.Controls.Add(this.label16);
+			this.panel1.Controls.Add(this.label11);
+			this.panel1.Controls.Add(this.label13);
+			this.panel1.Controls.Add(this.label10);
+			this.panel1.Controls.Add(this.label9);
+			this.panel1.Controls.Add(this.label8);
+			this.panel1.Location = new System.Drawing.Point(12, 182);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(423, 224);
+			this.panel1.TabIndex = 11;
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(13, 11);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(57, 13);
+			this.label8.TabIndex = 0;
+			this.label8.Text = "NESHawk";
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(21, 28);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(105, 13);
+			this.label9.TabIndex = 1;
+			this.label9.Text = "Created by: Zeromus";
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Location = new System.Drawing.Point(21, 42);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(166, 13);
+			this.label10.TabIndex = 2;
+			this.label10.Text = "Contributers: adelikat, natt, taotao";
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.Location = new System.Drawing.Point(21, 102);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(66, 13);
+			this.label11.TabIndex = 5;
+			this.label11.Text = "author: byuu";
+			// 
+			// label13
+			// 
+			this.label13.AutoSize = true;
+			this.label13.Location = new System.Drawing.Point(13, 71);
+			this.label13.Name = "label13";
+			this.label13.Size = new System.Drawing.Size(64, 13);
+			this.label13.TabIndex = 3;
+			this.label13.Text = "SNESHawk";
+			// 
+			// label14
+			// 
+			this.label14.AutoSize = true;
+			this.label14.Location = new System.Drawing.Point(21, 169);
+			this.label14.Name = "label14";
+			this.label14.Size = new System.Drawing.Size(81, 13);
+			this.label14.TabIndex = 8;
+			this.label14.Text = "author: sinamas";
+			// 
+			// label16
+			// 
+			this.label16.AutoSize = true;
+			this.label16.Location = new System.Drawing.Point(13, 138);
+			this.label16.Name = "label16";
+			this.label16.Size = new System.Drawing.Size(50, 13);
+			this.label16.TabIndex = 6;
+			this.label16.Text = "GBHawk";
+			// 
+			// label18
+			// 
+			this.label18.AutoSize = true;
+			this.label18.Location = new System.Drawing.Point(212, 28);
+			this.label18.Name = "label18";
+			this.label18.Size = new System.Drawing.Size(94, 13);
+			this.label18.TabIndex = 10;
+			this.label18.Text = "Created by: vecna";
+			// 
+			// label19
+			// 
+			this.label19.AutoSize = true;
+			this.label19.Location = new System.Drawing.Point(204, 11);
+			this.label19.Name = "label19";
+			this.label19.Size = new System.Drawing.Size(56, 13);
+			this.label19.TabIndex = 9;
+			this.label19.Text = "PCEHawk";
+			// 
+			// label17
+			// 
+			this.label17.AutoSize = true;
+			this.label17.Location = new System.Drawing.Point(209, 70);
+			this.label17.Name = "label17";
+			this.label17.Size = new System.Drawing.Size(94, 13);
+			this.label17.TabIndex = 12;
+			this.label17.Text = "Created by: vecna";
+			// 
+			// label20
+			// 
+			this.label20.AutoSize = true;
+			this.label20.Location = new System.Drawing.Point(201, 53);
+			this.label20.Name = "label20";
+			this.label20.Size = new System.Drawing.Size(58, 13);
+			this.label20.TabIndex = 11;
+			this.label20.Text = "SMSHawk";
+			// 
+			// label21
+			// 
+			this.label21.AutoSize = true;
+			this.label21.Location = new System.Drawing.Point(209, 127);
+			this.label21.Name = "label21";
+			this.label21.Size = new System.Drawing.Size(175, 13);
+			this.label21.TabIndex = 15;
+			this.label21.Text = "Contributers: adelikat, zeromus, natt";
+			// 
+			// label22
+			// 
+			this.label22.AutoSize = true;
+			this.label22.Location = new System.Drawing.Point(209, 113);
+			this.label22.Name = "label22";
+			this.label22.Size = new System.Drawing.Size(107, 13);
+			this.label22.TabIndex = 14;
+			this.label22.Text = "Created by: micro500";
+			// 
+			// label23
+			// 
+			this.label23.AutoSize = true;
+			this.label23.Location = new System.Drawing.Point(201, 96);
+			this.label23.Name = "label23";
+			this.label23.Size = new System.Drawing.Size(56, 13);
+			this.label23.TabIndex = 13;
+			this.label23.Text = "AtariHawk";
+			// 
+			// label24
+			// 
+			this.label24.AutoSize = true;
+			this.label24.Location = new System.Drawing.Point(212, 182);
+			this.label24.Name = "label24";
+			this.label24.Size = new System.Drawing.Size(106, 13);
+			this.label24.TabIndex = 18;
+			this.label24.Text = "Contributers: adelikat";
+			// 
+			// label25
+			// 
+			this.label25.AutoSize = true;
+			this.label25.Location = new System.Drawing.Point(212, 168);
+			this.label25.Name = "label25";
+			this.label25.Size = new System.Drawing.Size(105, 13);
+			this.label25.TabIndex = 17;
+			this.label25.Text = "Created by: Zeromus";
+			// 
+			// label26
+			// 
+			this.label26.AutoSize = true;
+			this.label26.Location = new System.Drawing.Point(204, 151);
+			this.label26.Name = "label26";
+			this.label26.Size = new System.Drawing.Size(57, 13);
+			this.label26.TabIndex = 16;
+			this.label26.Text = "TI83Hawk";
+			// 
+			// linkLabel2
+			// 
+			this.linkLabel2.AutoSize = true;
+			this.linkLabel2.Location = new System.Drawing.Point(20, 154);
+			this.linkLabel2.Name = "linkLabel2";
+			this.linkLabel2.Size = new System.Drawing.Size(75, 13);
+			this.linkLabel2.TabIndex = 19;
+			this.linkLabel2.TabStop = true;
+			this.linkLabel2.Text = "gambatte r344";
+			this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+			// 
+			// linkLabel3
+			// 
+			this.linkLabel3.AutoSize = true;
+			this.linkLabel3.Location = new System.Drawing.Point(21, 84);
+			this.linkLabel3.Name = "linkLabel3";
+			this.linkLabel3.Size = new System.Drawing.Size(56, 13);
+			this.linkLabel3.TabIndex = 20;
+			this.linkLabel3.TabStop = true;
+			this.linkLabel3.Text = "bsnes v87";
+			this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
 			// 
 			// BizBox
 			// 
 			this.AcceptButton = this.OK;
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(477, 215);
+			this.ClientSize = new System.Drawing.Size(477, 447);
+			this.Controls.Add(this.panel1);
 			this.Controls.Add(this.label7);
 			this.Controls.Add(this.label6);
 			this.Controls.Add(this.label5);
@@ -172,12 +394,14 @@
 			this.Controls.Add(this.pictureBox1);
 			this.Controls.Add(this.OK);
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.MaximumSize = new System.Drawing.Size(900, 438);
+			this.MaximumSize = new System.Drawing.Size(900, 638);
 			this.MinimumSize = new System.Drawing.Size(493, 253);
 			this.Name = "BizBox";
 			this.Text = " BizHawk";
 			this.Load += new System.EventHandler(this.BizBox_Load);
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -196,5 +420,25 @@
 		private System.Windows.Forms.Label label5;
 		private System.Windows.Forms.Label label6;
 		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label24;
+		private System.Windows.Forms.Label label25;
+		private System.Windows.Forms.Label label26;
+		private System.Windows.Forms.Label label21;
+		private System.Windows.Forms.Label label22;
+		private System.Windows.Forms.Label label23;
+		private System.Windows.Forms.Label label17;
+		private System.Windows.Forms.Label label20;
+		private System.Windows.Forms.Label label18;
+		private System.Windows.Forms.Label label19;
+		private System.Windows.Forms.Label label14;
+		private System.Windows.Forms.Label label16;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.Label label13;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.LinkLabel linkLabel2;
+		private System.Windows.Forms.LinkLabel linkLabel3;
 	}
 }
