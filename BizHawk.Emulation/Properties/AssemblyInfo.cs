a34337691a7d66ac833fb38d999e75ac95c6b497
zeromus <zeromus@zeromus.org>
2012-10-21 05:21:59 +0000

    run all version numbers through one place so that gui version numbers and assembly/dll version resources can be the same

--- BizHawk.Emulation/Properties/AssemblyInfo.cs
+++ BizHawk.Emulation/Properties/AssemblyInfo.cs
@@ -27,5 +27,10 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1." + SubWCRev.SVN_REV)]
-[assembly: AssemblyFileVersion("1.0.1." + SubWCRev.SVN_REV)]
+[assembly: AssemblyVersion(VersionInfo.MAINVERSION + "." + SubWCRev.SVN_REV)]
+[assembly: AssemblyFileVersion(VersionInfo.MAINVERSION + "." + SubWCRev.SVN_REV)]
+
+public static class VersionInfo
+{
+	public const string MAINVERSION = "1.2.0";
+}
--- BizHawk.MultiClient/MainForm.cs
+++ BizHawk.MultiClient/MainForm.cs
@@ -24,7 +24,7 @@ namespace BizHawk.MultiClient
 	public partial class MainForm : Form
 	{
 		public bool INTERIM = true;
-		public const string EMUVERSION = "Version 1.2.0";
+		public const string EMUVERSION = "Version " + VersionInfo.MAINVERSION;
 		public const string RELEASEDATE = "October 20, 2012";
 		private Control renderTarget;
 		private RetainedViewportPanel retainedPanel;
