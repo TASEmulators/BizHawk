fb2bbc43fc6ce1dcc7db503eb0256bcd71529cab
goyuken <goyuken@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-10-28 17:34:47 +0000

    nes: mapper 250: "Time Diver Avenger (Unl)".  mmc3 clone

---
+++ trunk/BizHawk.Emulation/Consoles/Nintendo/NES/Boards/MMC3_family/Mapper250.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BizHawk.Emulation.Consoles.Nintendo
+{
+	// Time Diver Avenger (Unl)
+	// MMC3 with slightly different write scheme
+	// presumably the board contains an MMC3 clone with some unique edge case behavior; unknown
+	public class Mapper250 : MMC3Board_Base
+	{
+		public override bool Configure(NES.EDetectionOrigin origin)
+		{
+			switch (Cart.board_type)
+			{
+				case "MAPPER250":
+					break;
+				default:
+					return false;
+			}
+			BaseSetup();
+			return true;
+		}
+
+		public override void WritePRG(int addr, byte value)
+		{
+			base.WritePRG(addr & 0x6000 | addr >> 10 & 1, (byte)(addr & 0xff));
+		}
+	}
+}
