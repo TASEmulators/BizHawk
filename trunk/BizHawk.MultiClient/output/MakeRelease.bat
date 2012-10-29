66d12410a8ba734c6f348ee1c3c8e193f97b78f4
zeromus@zeromus.org <zeromus@zeromus.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-10-29 08:37:22 +0000

    support moving all dlls to the dll directory by adding assembly binding resolve redirection

--- trunk/BizHawk.MultiClient/output/MakeRelease.bat
+++ trunk/BizHawk.MultiClient/output/MakeRelease.bat
@@ -1,3 +1,12 @@
 del /s BizHawk.zip
+
+rem slimdx has a way of not making it into the output directory, so this is a good time to make sure
 copy ..\..\SlimDx.dll
-zip -X -9 -r BizHawk.zip BizHawk.MultiClient.exe DiscoHawk.exe *.dll dll ffmpeg.exe gamedb NES\Palettes Lua Gameboy\Palettes
+
+rem at the present we support moving all these dlls into the dll subdirectory
+rem that might be troublesome some day..... if it does get troublesome, then we'll have to 
+rem explicitly list the OK ones here as individual copies. until then....
+
+copy *.dll dll
+
+zip -X -9 -r BizHawk.zip BizHawk.MultiClient.exe DiscoHawk.exe dll gamedb NES\Palettes Lua Gameboy\Palettes
