e19f39e08c20d333660cc54242a631fff6974381
zeromus <zeromus@zeromus.org>
2012-10-29 08:37:22 +0000

    support moving all dlls to the dll directory by adding assembly binding resolve redirection

--- BizHawk.Util/Win32.cs
+++ BizHawk.Util/Win32.cs
@@ -439,9 +439,6 @@ namespace BizHawk
 
 		[DllImport("user32.dll", CharSet = CharSet.Auto)]
 		public static extern int SendMessage(IntPtr hWnd, int msg, int wParam, int lParam);
-
-		[DllImport("kernel32.dll", SetLastError = true)]
-		public static extern bool SetDllDirectory(string lpPathName);
 	}
 
 }
