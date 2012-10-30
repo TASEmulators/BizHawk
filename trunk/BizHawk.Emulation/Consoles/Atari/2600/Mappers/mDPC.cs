0aa8535a2acb88bf48af8418d2ca3dd2af16210c
goyuken <goyuken@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-10-30 22:14:31 +0000

    2600: add fake mDPC.cs to fix compile.

---
+++ trunk/BizHawk.Emulation/Consoles/Atari/2600/Mappers/mDPC.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BizHawk.Emulation.Consoles.Atari._2600
+{
+	partial class Atari2600
+	{
+		class mDPC : MapperBase
+		{
+			public override byte ReadMemory(ushort addr)
+			{
+				if (addr < 0x1000) return base.ReadMemory(addr);
+				return core.rom[addr & 0x7FF];
+			}
+		}
+	}
+}
