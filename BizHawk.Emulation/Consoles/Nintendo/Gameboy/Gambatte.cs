944c8e412f8893e2c07af55a8bc4b4aeaf2f0cac
goyuken <goyuken@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-11-02 23:19:16 +0000

    gbhawk: tracelogger shows disassembly now

--- BizHawk.Emulation/Consoles/Nintendo/Gameboy/Gambatte.cs
+++ BizHawk.Emulation/Consoles/Nintendo/Gameboy/Gambatte.cs
@@ -429,9 +429,10 @@ namespace BizHawk.Emulation.Consoles.GB
 		{
 			int[] s = new int[13];
 			System.Runtime.InteropServices.Marshal.Copy(_s, s, 0, 13);
+			ushort unused;
 
 			CoreInputComm.Tracer.Put(string.Format(
-				"{1:x4} {12:x2} SP:{2:x2} A:{3:x2} B:{4:x2} C:{5:x2} D:{6:x2} E:{7:x2} F:{8:x2} H:{9:x2} L:{10:x2} {11} Cy:{0}",
+				"{13} SP:{2:x2} A:{3:x2} B:{4:x2} C:{5:x2} D:{6:x2} E:{7:x2} F:{8:x2} H:{9:x2} L:{10:x2} {11} Cy:{0}",
 				s[0],
 				s[1] & 0xffff,
 				s[2] & 0xffff,
@@ -444,7 +445,8 @@ namespace BizHawk.Emulation.Consoles.GB
 				s[9] & 0xff,
 				s[10] & 0xff,
 				s[11] != 0 ? "skip" : "",
-				s[12] & 0xff
+				s[12] & 0xff,
+				CPUs.Z80GB.Disassembler.DAsm((ushort)s[1], (addr) => LibGambatte.gambatte_cpuread(GambatteState, addr), out unused).PadRight(30)
 			));
 		}
 
