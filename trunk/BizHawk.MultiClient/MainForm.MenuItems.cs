94ac380bf61780840e8e32b2469076a15d6f19d2
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-11-06 13:23:56 +0000

    oops

--- trunk/BizHawk.MultiClient/MainForm.MenuItems.cs
+++ trunk/BizHawk.MultiClient/MainForm.MenuItems.cs
@@ -412,7 +412,7 @@ namespace BizHawk.MultiClient
 
 		private void controllersToolStripMenuItem_Click(object sender, EventArgs e)
 		{
-			if (true)
+			if (false)
 			{
 				ControllerConfig c = new ControllerConfig();
 				c.ShowDialog();
