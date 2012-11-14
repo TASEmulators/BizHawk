4c6bf741e91962becc24e32e281b05b9d86078c1
saxxonpike@gmail.com <saxxonpike@gmail.com@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-11-14 01:50:17 +0000

    commodore64: G64 disk format support

---
+++ trunk/BizHawk.Emulation/Computers/Commodore64/Disk.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BizHawk.Emulation.Computers.Commodore64
+{
+	public class Disk
+	{
+		public class Track
+		{
+			public int bits;
+			public byte[] data;
+			public int density;
+			public int index;
+		}
+
+		public List<Track> tracks = new List<Track>();
+		public bool valid;
+	}
+}
