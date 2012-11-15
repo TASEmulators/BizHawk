bd3912939abbbc0e0d95496091b9f57a61056a3e
saxxonpike <saxxonpike@gmail.com>
2012-11-15 20:49:54 +0000

    commodore64: implement serial shift register in CIA chip to prepare for disk drive I/O

--- BizHawk.Emulation/Computers/Commodore64/Via.cs
+++ BizHawk.Emulation/Computers/Commodore64/Via.cs
@@ -37,6 +37,23 @@ namespace BizHawk.Emulation.Computers.Commodore64
 		}
 	}
 
+	// 0x0: port B
+	// 0x1: port A
+	// 0x2: port B data direction
+	// 0x3: port A data direction
+	// 0x4: timer lo
+	// 0x5: timer hi
+	// 0x6: timer latch lo
+	// 0x7: timer latch hi
+	// 0x8: unused
+	// 0x9: unused
+	// 0xA: unused
+	// 0xB: timer control
+	// 0xC: auxilary control
+	// 0xD: interrupt status
+	// 0xE: interrupt control
+	// 0xF: unused
+
 	public class Via
 	{
 		public ViaRegs regs;
