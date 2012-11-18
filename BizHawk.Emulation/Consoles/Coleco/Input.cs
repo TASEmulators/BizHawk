57abaad07e6ae7114bbb613288de74bfa7bd33fc
beirich <beirich@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-11-18 02:32:07 +0000

    Coleco: improve VDP interrupt handling, implement sprite collision status bits Fixes Frenzy and Carnival at least

--- BizHawk.Emulation/Consoles/Coleco/Input.cs
+++ BizHawk.Emulation/Consoles/Coleco/Input.cs
@@ -8,12 +8,12 @@
             BoolButtons = 
 			{
 				"P1 Up", "P1 Down", "P1 Left", "P1 Right",
-				"P1 L1", "P1 L2", "P1 R1", "P1 R2",
+				"P1 L", "P1 R",
 				"P1 Key0", "P1 Key1", "P1 Key2", "P1 Key3", "P1 Key4", "P1 Key5",
 				"P1 Key6", "P1 Key7", "P1 Key8", "P1 Key9", "P1 Star", "P1 Pound",
 
 				"P2 Up", "P2 Down", "P2 Left", "P2 Right",
-				"P2 L1", "P2 L2", "P2 R1", "P2 R2",
+				"P2 L", "P2 R",
 				"P1 Key0", "P2 Key1", "P2 Key2", "P2 Key3", "P2 Key4", "P2 Key5",
 				"P2 Key6", "P2 Key7", "P2 Key8", "P2 Key9", "P2 Star", "P2 Pound"
 			}
