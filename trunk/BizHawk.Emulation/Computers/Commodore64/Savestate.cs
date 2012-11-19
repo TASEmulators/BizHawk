434881766f51db3bd868477f79edee4024672f01
saxxonpike@gmail.com <saxxonpike@gmail.com@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-11-19 20:35:27 +0000

    commodore64: fix build error in savestate code

--- trunk/BizHawk.Emulation/Computers/Commodore64/Savestate.cs
+++ trunk/BizHawk.Emulation/Computers/Commodore64/Savestate.cs
@@ -47,7 +47,6 @@ namespace BizHawk.Emulation.Computers.Commodore64
 	public class StateParameters
 	{
 		private Dictionary<string, int> integerList = new Dictionary<string, int>();
-		private Dictionary<string, string> stringList = new Dictionary<string, string>();
 
 		public int this[string key]
 		{
@@ -65,35 +64,12 @@ namespace BizHawk.Emulation.Computers.Commodore64
 			}
 		}
 
-		public string this[string key]
-		{
-			get
-			{
-				if (stringList.ContainsKey(key))
-				{
-					return stringList[key];
-				}
-				return "";
-			}
-			set
-			{
-				stringList[key] = value;
-			}
-		}
-
 		public void ExportBinary(Stream target)
 		{
 			BinaryWriter writer = new BinaryWriter(target);
 
 			writer.Write((Int32)integerList.Count);
 			foreach (KeyValuePair<string, int> kv in integerList)
-			{
-				writer.Write(kv.Key);
-				writer.Write(kv.Value);
-			}
-
-			writer.Write((Int32)stringList.Count);
-			foreach (KeyValuePair<string, string> kv in stringList)
 			{
 				writer.Write(kv.Key);
 				writer.Write(kv.Value);
@@ -108,13 +84,7 @@ namespace BizHawk.Emulation.Computers.Commodore64
 
 			foreach (KeyValuePair<string, int> kv in integerList)
 			{
-				sb.Append("i" + kv.Key + "=");
-				sb.AppendLine(kv.Value.ToString());
-			}
-
-			foreach (KeyValuePair<string, string> kv in stringList)
-			{
-				sb.Append("s" + kv.Key + "=");
+				sb.Append(kv.Key + "=");
 				sb.AppendLine(kv.Value.ToString());
 			}
 
@@ -134,14 +104,6 @@ namespace BizHawk.Emulation.Computers.Commodore64
 				int val = reader.ReadInt32();
 				integerList[key] = val;
 			}
-
-			count = reader.ReadInt32();
-			for (int i = 0; i < count; i++)
-			{
-				string key = reader.ReadString();
-				string val = reader.ReadString();
-				stringList[key] = val;
-			}
 		}
 
 		public void ImportText(Stream source)
@@ -160,17 +122,7 @@ namespace BizHawk.Emulation.Computers.Commodore64
 
 					if (val.Length > 0 && key.Length > 0)
 					{
-						string valType = val.Substring(0, 1);
-						val = val.Substring(1);
-						switch (valType)
-						{
-							case @"i":
-								integerList[key] = int.Parse(val);
-								break;
-							case @"s":
-								stringList[key] = val;
-								break;
-						}
+						integerList[key] = int.Parse(val);
 					}
 				}
 			}
