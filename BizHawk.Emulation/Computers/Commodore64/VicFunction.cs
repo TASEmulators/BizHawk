484545fef67fded926928f77a76f44d33ffa918a
saxxonpike <saxxonpike@gmail.com>
2012-11-21 06:55:53 +0000

    commodore64: restructuring of VIC code for video glitch fixes

---
+++ BizHawk.Emulation/Computers/Commodore64/VicFunction.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BizHawk.Emulation.Computers.Commodore64
+{
+	public partial class VicII
+	{
+		private void BASprite(uint index0)
+		{
+		}
+
+		private void BASprite(uint index0, uint index1)
+		{
+		}
+
+		private void BASprite(uint index0, uint index1, uint index2)
+		{
+		}
+
+		private void CheckBorderLeft0()
+		{
+		}
+
+		private void CheckBorderLeft1()
+		{
+		}
+
+		private void CheckBorderRight0()
+		{
+		}
+
+		private void CheckBorderRight1()
+		{
+		}
+
+		private void CheckSpriteCrunch()
+		{
+		}
+
+		private void CheckSpriteDisplay()
+		{
+		}
+
+		private void CheckSpriteDma()
+		{
+		}
+
+		private void CheckSpriteExpansion()
+		{
+		}
+
+		private void FetchC()
+		{
+		}
+
+		private void FetchG()
+		{
+		}
+
+		private void Idle()
+		{
+		}
+
+		private void Phase0()
+		{
+		}
+
+		private void Phase1()
+		{
+		}
+
+		private void Refresh()
+		{
+		}
+
+		private void SpritePointer()
+		{
+		}
+
+		private void SpriteDma0()
+		{
+		}
+
+		private void SpriteDma1()
+		{
+		}
+
+		private void SpriteDma2()
+		{
+		}
+
+		private void UpdateMCBASE()
+		{
+		}
+
+		private void UpdateRC()
+		{
+		}
+
+		private void UpdateVC()
+		{
+		}
+
+		private void Vis(uint index)
+		{
+		}
+	}
+}
---
+++ BizHawk.Emulation/Computers/Commodore64/VicMem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BizHawk.Emulation.Computers.Commodore64
+{
+	public partial class VicII
+	{
+		public byte Peek(int addr)
+		{
+			addr &= 0x3F;
+			return this[(uint)addr];
+		}
+
+		public void Poke(int addr, byte val)
+		{
+			addr &= 0x3F;
+			this[(uint)addr] = val;
+		}
+
+		public byte Read(ushort addr)
+		{
+			byte result = 0xFF;
+
+			addr &= 0x3F;
+			switch (addr)
+			{
+				default:
+					result = this[addr];
+					break;
+			}
+
+			return result;
+		}
+
+		public void Write(ushort addr, byte val)
+		{
+			addr &= 0x3F;
+			switch (addr)
+			{
+				default:
+					this[addr] = val;
+					break;
+			}
+		}
+	}
+}
