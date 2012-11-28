22f4a03d1173416f5a4df5f4f67d815cbc149f6e
saxxonpike@gmail.com <saxxonpike@gmail.com@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-11-28 17:49:42 +0000

    commodore64: PRG loading uses a more robust method

---
+++ trunk/BizHawk.Emulation/Computers/Commodore64/Media/PRG.cs
@@ -0,0 +1,30 @@
+﻿using BizHawk.Emulation.Computers.Commodore64.MOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BizHawk.Emulation.Computers.Commodore64.Media
+{
+	public static class PRG
+	{
+		static public void Load(MOSPLA pla, byte[] prgFile)
+		{
+			uint length = (uint)prgFile.Length;
+			if (length > 2)
+			{
+				ushort addr = (ushort)(prgFile[0] | (prgFile[1] << 8));
+				uint offset = 2;
+				unchecked
+				{
+					while (offset < length)
+					{
+						pla.Write(addr, prgFile[offset]);
+						offset++;
+						addr++;
+					}
+				}
+			}
+		}
+	}
+}
