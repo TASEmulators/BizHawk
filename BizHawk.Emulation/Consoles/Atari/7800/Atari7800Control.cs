d205d68b95fe68505f6407448c6b19528ded9881
goyuken <goyuken@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-12-12 18:17:30 +0000

    move our 7800 code into Bizhawk.Emulation namespace (oops)

---
+++ BizHawk.Emulation/Consoles/Atari/7800/Atari7800Control.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EMU7800.Core;
+
+namespace BizHawk.Emulation
+{
+	public class Atari7800Control
+	{
+
+
+	}
+}
