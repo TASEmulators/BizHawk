867c6ef4b97422ca806cf9a9fa74d24ac88ede3f
adelikat <adelikat@tasvideos.org>
2012-12-16 18:31:49 +0000

    Add Atari 7800 info to about box

--- BizHawk.MultiClient/BizBox.cs
+++ BizHawk.MultiClient/BizBox.cs
@@ -35,14 +35,35 @@ namespace BizHawk.MultiClient
 
 		private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
 		{
-			this.linkLabel1.LinkVisited = true;
+			this.linkLabel3.LinkVisited = true;
 			System.Diagnostics.Process.Start("http://byuu.org/bsnes/");
 		}
 
 		private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
 		{
-			this.linkLabel1.LinkVisited = true;
+			this.linkLabel2.LinkVisited = true;
 			System.Diagnostics.Process.Start("http://gambatte.sourceforge.net/");
+		}
+
+		private void label30_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label28_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label29_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void linkLabel4_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+		{
+			this.linkLabel4.LinkVisited = true;
+			System.Diagnostics.Process.Start("http://emu7800.sourceforge.net/");
 		}
 	}
 }
