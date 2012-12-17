1a4928205e39a615d92a940071d5961cb6786438
brandman211@gmail.com <brandman211@gmail.com@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2012-12-17 04:23:59 +0000

    IntelliHawk: -Cleanup. -Added "Total Executed Cycles" to the log. -By observing the aforementioned data, I realized that the docs probably meant to say 14934 instead of 14394. --By adjusting this...TITLE SCREEN! --Still, there are definitely discrepancies with the log that imply that I'm far from done. -Enabled ANDR and XOR because they were executed during the title sequence, though it's hard to tell if it should at this point.

--- trunk/BizHawk.Emulation/Consoles/Intellivision/Intellivision.cs
+++ trunk/BizHawk.Emulation/Consoles/Intellivision/Intellivision.cs
@@ -25,17 +25,25 @@ namespace BizHawk.Emulation.Consoles.Intellivision
 		public void LoadExecutiveRom(string path)
 		{
 			var erom = File.ReadAllBytes(path);
-			if (erom.Length != 8192) throw new ApplicationException("EROM file is wrong size - expected 8192 bytes");
+			if (erom.Length != 8192)
+			{
+				throw new ApplicationException("EROM file is wrong size - expected 8192 bytes");
+			}
 			int index = 0;
 			// Combine every two bytes into a word.
 			while (index + 1 < erom.Length)
+			{
 				ExecutiveRom[index / 2] = (ushort)((erom[index++] << 8) | erom[index++]);
+			}
 		}
 
 		public void LoadGraphicsRom(string path)
 		{
 			GraphicsRom = File.ReadAllBytes(path);
-			if (GraphicsRom.Length != 2048) throw new ApplicationException("GROM file is wrong size - expected 2048 bytes");
+			if (GraphicsRom.Length != 2048)
+			{
+				throw new ApplicationException("GROM file is wrong size - expected 2048 bytes");
+			}
 		}
 
 		public Intellivision(CoreComm comm, GameInfo game, byte[] rom)
@@ -73,7 +81,7 @@ namespace BizHawk.Emulation.Consoles.Intellivision
 		public void FrameAdvance(bool render, bool rendersound)
 		{
 			Frame++;
-			Cpu.AddPendingCycles(14394 + 3791);
+			Cpu.AddPendingCycles(14934);
 			while (Cpu.GetPendingCycles() > 0)
 			{
 				int cycles = Cpu.Execute();
