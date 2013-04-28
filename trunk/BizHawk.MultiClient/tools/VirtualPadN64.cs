a9561dda471d0a8c61bb2d9e0ca9c9c5ea7a7a90
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2013-04-28 13:56:29 +0000

    Start a new virtual pad, and update 2012 sln file with new files from past few commits

---
+++ trunk/BizHawk.MultiClient/tools/VirtualPadN64.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BizHawk.MultiClient.tools
+{
+	public partial class VirtualPadN64 : UserControl
+	{
+		public VirtualPadN64()
+		{
+			InitializeComponent();
+		}
+
+		private void UserControl1_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
