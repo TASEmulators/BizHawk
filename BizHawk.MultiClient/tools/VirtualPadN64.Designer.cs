669edc55b36c6f8dd36ae618f1f7aec0991ae742
adelikat <adelikat@tasvideos.org>
2013-04-28 13:56:29 +0000

    Start a new virtual pad, and update 2012 sln file with new files from past few commits

---
+++ BizHawk.MultiClient/tools/VirtualPadN64.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace BizHawk.MultiClient.tools
+{
+	partial class VirtualPadN64
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.AnalogControl1 = new AnalogControlPanel();
+			this.PL = new System.Windows.Forms.CheckBox();
+			this.PD = new System.Windows.Forms.CheckBox();
+			this.PR = new System.Windows.Forms.CheckBox();
+			this.PU = new System.Windows.Forms.CheckBox();
+			this.KeyLeft = new System.Windows.Forms.CheckBox();
+			this.KeyRight = new System.Windows.Forms.CheckBox();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.checkBox2 = new System.Windows.Forms.CheckBox();
+			this.checkBox3 = new System.Windows.Forms.CheckBox();
+			this.checkBox4 = new System.Windows.Forms.CheckBox();
+			this.checkBox5 = new System.Windows.Forms.CheckBox();
+			this.checkBox6 = new System.Windows.Forms.CheckBox();
+			this.checkBox7 = new System.Windows.Forms.CheckBox();
+			this.checkBox8 = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// AnalogControl1
+			// 
+			this.AnalogControl1.Location = new System.Drawing.Point(14, 27);
+			this.AnalogControl1.Name = "AnalogControl1";
+			this.AnalogControl1.Size = new System.Drawing.Size(205, 147);
+			this.AnalogControl1.TabIndex = 0;
+			// 
+			// PL
+			// 
+			this.PL.Appearance = System.Windows.Forms.Appearance.Button;
+			this.PL.AutoSize = true;
+			this.PL.Image = global::BizHawk.MultiClient.Properties.Resources.Back;
+			this.PL.Location = new System.Drawing.Point(20, 229);
+			this.PL.Name = "PL";
+			this.PL.Size = new System.Drawing.Size(22, 22);
+			this.PL.TabIndex = 7;
+			this.PL.UseVisualStyleBackColor = true;
+			// 
+			// PD
+			// 
+			this.PD.Appearance = System.Windows.Forms.Appearance.Button;
+			this.PD.AutoSize = true;
+			this.PD.Image = global::BizHawk.MultiClient.Properties.Resources.BlueDown;
+			this.PD.Location = new System.Drawing.Point(41, 238);
+			this.PD.Name = "PD";
+			this.PD.Size = new System.Drawing.Size(22, 22);
+			this.PD.TabIndex = 6;
+			this.PD.UseVisualStyleBackColor = true;
+			// 
+			// PR
+			// 
+			this.PR.Appearance = System.Windows.Forms.Appearance.Button;
+			this.PR.AutoSize = true;
+			this.PR.Image = global::BizHawk.MultiClient.Properties.Resources.Forward;
+			this.PR.Location = new System.Drawing.Point(62, 229);
+			this.PR.Name = "PR";
+			this.PR.Size = new System.Drawing.Size(22, 22);
+			this.PR.TabIndex = 5;
+			this.PR.UseVisualStyleBackColor = true;
+			// 
+			// PU
+			// 
+			this.PU.Appearance = System.Windows.Forms.Appearance.Button;
+			this.PU.AutoSize = true;
+			this.PU.Image = global::BizHawk.MultiClient.Properties.Resources.BlueUp;
+			this.PU.Location = new System.Drawing.Point(41, 217);
+			this.PU.Name = "PU";
+			this.PU.Size = new System.Drawing.Size(22, 22);
+			this.PU.TabIndex = 4;
+			this.PU.UseVisualStyleBackColor = true;
+			// 
+			// KeyLeft
+			// 
+			this.KeyLeft.Appearance = System.Windows.Forms.Appearance.Button;
+			this.KeyLeft.AutoSize = true;
+			this.KeyLeft.Location = new System.Drawing.Point(14, 180);
+			this.KeyLeft.Name = "KeyLeft";
+			this.KeyLeft.Size = new System.Drawing.Size(23, 23);
+			this.KeyLeft.TabIndex = 8;
+			this.KeyLeft.Text = "L";
+			this.KeyLeft.UseVisualStyleBackColor = true;
+			// 
+			// KeyRight
+			// 
+			this.KeyRight.Appearance = System.Windows.Forms.Appearance.Button;
+			this.KeyRight.AutoSize = true;
+			this.KeyRight.Location = new System.Drawing.Point(177, 180);
+			this.KeyRight.Name = "KeyRight";
+			this.KeyRight.Size = new System.Drawing.Size(25, 23);
+			this.KeyRight.TabIndex = 9;
+			this.KeyRight.Text = "R";
+			this.KeyRight.UseVisualStyleBackColor = true;
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.Appearance = System.Windows.Forms.Appearance.Button;
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(98, 191);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(24, 23);
+			this.checkBox1.TabIndex = 10;
+			this.checkBox1.Text = "S";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// checkBox2
+			// 
+			this.checkBox2.Appearance = System.Windows.Forms.Appearance.Button;
+			this.checkBox2.AutoSize = true;
+			this.checkBox2.Location = new System.Drawing.Point(98, 270);
+			this.checkBox2.Name = "checkBox2";
+			this.checkBox2.Size = new System.Drawing.Size(24, 23);
+			this.checkBox2.TabIndex = 11;
+			this.checkBox2.Text = "Z";
+			this.checkBox2.UseVisualStyleBackColor = true;
+			// 
+			// checkBox3
+			// 
+			this.checkBox3.Appearance = System.Windows.Forms.Appearance.Button;
+			this.checkBox3.AutoSize = true;
+			this.checkBox3.Location = new System.Drawing.Point(98, 220);
+			this.checkBox3.Name = "checkBox3";
+			this.checkBox3.Size = new System.Drawing.Size(24, 23);
+			this.checkBox3.TabIndex = 12;
+			this.checkBox3.Text = "B";
+			this.checkBox3.UseVisualStyleBackColor = true;
+			// 
+			// checkBox4
+			// 
+			this.checkBox4.Appearance = System.Windows.Forms.Appearance.Button;
+			this.checkBox4.AutoSize = true;
+			this.checkBox4.Location = new System.Drawing.Point(128, 229);
+			this.checkBox4.Name = "checkBox4";
+			this.checkBox4.Size = new System.Drawing.Size(24, 23);
+			this.checkBox4.TabIndex = 13;
+			this.checkBox4.Text = "A";
+			this.checkBox4.UseVisualStyleBackColor = true;
+			// 
+			// checkBox5
+			// 
+			this.checkBox5.Appearance = System.Windows.Forms.Appearance.Button;
+			this.checkBox5.AutoSize = true;
+			this.checkBox5.Location = new System.Drawing.Point(212, 216);
+			this.checkBox5.Name = "checkBox5";
+			this.checkBox5.Size = new System.Drawing.Size(31, 23);
+			this.checkBox5.TabIndex = 14;
+			this.checkBox5.Text = "cU";
+			this.checkBox5.UseVisualStyleBackColor = true;
+			// 
+			// checkBox6
+			// 
+			this.checkBox6.Appearance = System.Windows.Forms.Appearance.Button;
+			this.checkBox6.AutoSize = true;
+			this.checkBox6.Location = new System.Drawing.Point(190, 245);
+			this.checkBox6.Name = "checkBox6";
+			this.checkBox6.Size = new System.Drawing.Size(29, 23);
+			this.checkBox6.TabIndex = 15;
+			this.checkBox6.Text = "cL";
+			this.checkBox6.UseVisualStyleBackColor = true;
+			// 
+			// checkBox7
+			// 
+			this.checkBox7.Appearance = System.Windows.Forms.Appearance.Button;
+			this.checkBox7.AutoSize = true;
+			this.checkBox7.Location = new System.Drawing.Point(225, 245);
+			this.checkBox7.Name = "checkBox7";
+			this.checkBox7.Size = new System.Drawing.Size(31, 23);
+			this.checkBox7.TabIndex = 16;
+			this.checkBox7.Text = "cR";
+			this.checkBox7.UseVisualStyleBackColor = true;
+			// 
+			// checkBox8
+			// 
+			this.checkBox8.Appearance = System.Windows.Forms.Appearance.Button;
+			this.checkBox8.AutoSize = true;
+			this.checkBox8.Location = new System.Drawing.Point(212, 274);
+			this.checkBox8.Name = "checkBox8";
+			this.checkBox8.Size = new System.Drawing.Size(31, 23);
+			this.checkBox8.TabIndex = 17;
+			this.checkBox8.Text = "cD";
+			this.checkBox8.UseVisualStyleBackColor = true;
+			// 
+			// VirtualPadN64
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.checkBox8);
+			this.Controls.Add(this.checkBox7);
+			this.Controls.Add(this.checkBox6);
+			this.Controls.Add(this.checkBox5);
+			this.Controls.Add(this.checkBox4);
+			this.Controls.Add(this.checkBox3);
+			this.Controls.Add(this.checkBox2);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.KeyRight);
+			this.Controls.Add(this.KeyLeft);
+			this.Controls.Add(this.PL);
+			this.Controls.Add(this.PD);
+			this.Controls.Add(this.PR);
+			this.Controls.Add(this.PU);
+			this.Controls.Add(this.AnalogControl1);
+			this.Name = "VirtualPadN64";
+			this.Size = new System.Drawing.Size(463, 332);
+			this.Load += new System.EventHandler(this.UserControl1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private AnalogControlPanel AnalogControl1;
+		private System.Windows.Forms.CheckBox PL;
+		private System.Windows.Forms.CheckBox PD;
+		private System.Windows.Forms.CheckBox PR;
+		private System.Windows.Forms.CheckBox PU;
+		private System.Windows.Forms.CheckBox KeyLeft;
+		private System.Windows.Forms.CheckBox KeyRight;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.CheckBox checkBox2;
+		private System.Windows.Forms.CheckBox checkBox3;
+		private System.Windows.Forms.CheckBox checkBox4;
+		private System.Windows.Forms.CheckBox checkBox5;
+		private System.Windows.Forms.CheckBox checkBox6;
+		private System.Windows.Forms.CheckBox checkBox7;
+		private System.Windows.Forms.CheckBox checkBox8;
+	}
+}
