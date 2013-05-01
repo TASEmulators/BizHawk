f96a37857d6a54f0f7769b2f6ea35b75c21c516a
goyuken <goyuken@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2013-05-01 15:48:23 +0000

    dual gameboy: xml creator dialog thingy.  not finished yet

---
+++ BizHawk.MultiClient/GBtools/DualGBXMLCreator.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BizHawk.MultiClient.GBtools
+{
+	public partial class DualGBXMLCreator : Form
+	{
+		public DualGBXMLCreator()
+		{
+			InitializeComponent();
+		}
+
+		// http://stackoverflow.com/questions/275689/how-to-get-relative-path-from-absolute-path
+		public static string GetRelativePath(string fromPath, string toPath)
+		{
+			Win32.FileAttributes fromAttr = GetPathAttribute(fromPath);
+			Win32.FileAttributes toAttr = GetPathAttribute(toPath);
+
+			StringBuilder path = new StringBuilder(260); // MAX_PATH
+			if (Win32.PathRelativePathTo(
+				path,
+				fromPath,
+				fromAttr,
+				toPath,
+				toAttr) == false)
+			{
+				throw new ArgumentException("Paths must have a common prefix");
+			}
+			return path.ToString();
+		}
+
+		private static Win32.FileAttributes GetPathAttribute(string path)
+		{
+			DirectoryInfo di = new DirectoryInfo(path);
+			if (di.Exists)
+			{
+				return Win32.FileAttributes.Directory;
+			}
+
+			FileInfo fi = new FileInfo(path);
+			if (fi.Exists)
+			{
+				return Win32.FileAttributes.Normal;
+			}
+
+			throw new FileNotFoundException();
+		}
+
+		bool Recalculate()
+		{
+			string PathLeft = dualGBFileSelector1.GetName();
+			string PathRight = dualGBFileSelector2.GetName();
+			string Name = textBoxName.Name;
+
+			if (string.IsNullOrWhiteSpace(PathLeft) || string.IsNullOrWhiteSpace(PathRight) || string.IsNullOrWhiteSpace(Name))
+				return false;
+
+			List<char> NewPathL = new List<char>();
+
+			for (int i = 0; i < PathLeft.Length && i < PathRight.Length; i++)
+			{
+				if (PathLeft[i] == PathRight[i])
+					NewPathL.Add(PathLeft[i]);
+				else
+					break;
+			}
+			string BasePath = new string(NewPathL.ToArray());
+			if (string.IsNullOrWhiteSpace(BasePath))
+				return false;
+			BasePath = System.IO.Path.GetDirectoryName(BasePath);
+			PathLeft = GetRelativePath(BasePath, PathLeft);
+			PathRight = GetRelativePath(BasePath, PathRight);
+
+			StringWriter XML = new StringWriter();
+			XML.WriteLine("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>");
+			XML.WriteLine("<BizHawk-XMLGame System=\"DGB\" Name=\"{0}\">", Name);
+			XML.WriteLine("  <LoadAssets>");
+			XML.WriteLine("    <LeftRom FileName=\"{0}\"/>", PathLeft);
+			XML.WriteLine("    <RightRom FileName=\"{0}\"/>", PathRight);
+			XML.WriteLine("  </LoadAssets>");
+			XML.WriteLine("</BizHawk-XMLGame>");
+
+			textBoxOutputDir.Text = BasePath;
+			textBoxXML.Text = XML.ToString();
+			return true;
+		}
+	}
+}
--- BizHawk.Util/Win32.cs
+++ BizHawk.Util/Win32.cs
@@ -477,6 +477,98 @@ namespace BizHawk
 
 		[DllImport("msvcrt.dll", EntryPoint = "memset", CallingConvention = CallingConvention.Cdecl, SetLastError = false)]
 		public static extern IntPtr MemSet(IntPtr dest, int c, uint count);
+
+		[DllImport("shlwapi.dll", CharSet = CharSet.Auto)]
+		public static extern bool PathRelativePathTo(
+			 [Out] System.Text.StringBuilder pszPath,
+			 [In] string pszFrom,
+			 [In] FileAttributes dwAttrFrom,
+			 [In] string pszTo,
+			 [In] FileAttributes dwAttrTo
+		);
+
+		/// <summary>
+		/// File attributes are metadata values stored by the file system on disk and are used by the system and are available to developers via various file I/O APIs.
+		/// </summary>
+		[Flags]
+		[CLSCompliant(false)]
+		public enum FileAttributes : uint
+		{
+			/// <summary>
+			/// A file that is read-only. Applications can read the file, but cannot write to it or delete it. This attribute is not honored on directories. For more information, see "You cannot view or change the Read-only or the System attributes of folders in Windows Server 2003, in Windows XP, or in Windows Vista".
+			/// </summary>
+			Readonly = 0x00000001,
+
+			/// <summary>
+			/// The file or directory is hidden. It is not included in an ordinary directory listing.
+			/// </summary>
+			Hidden = 0x00000002,
+
+			/// <summary>
+			/// A file or directory that the operating system uses a part of, or uses exclusively.
+			/// </summary>
+			System = 0x00000004,
+
+			/// <summary>
+			/// The handle that identifies a directory.
+			/// </summary>
+			Directory = 0x00000010,
+
+			/// <summary>
+			/// A file or directory that is an archive file or directory. Applications typically use this attribute to mark files for backup or removal.
+			/// </summary>
+			Archive = 0x00000020,
+
+			/// <summary>
+			/// This value is reserved for system use.
+			/// </summary>
+			Device = 0x00000040,
+
+			/// <summary>
+			/// A file that does not have other attributes set. This attribute is valid only when used alone.
+			/// </summary>
+			Normal = 0x00000080,
+
+			/// <summary>
+			/// A file that is being used for temporary storage. File systems avoid writing data back to mass storage if sufficient cache memory is available, because typically, an application deletes a temporary file after the handle is closed. In that scenario, the system can entirely avoid writing the data. Otherwise, the data is written after the handle is closed.
+			/// </summary>
+			Temporary = 0x00000100,
+
+			/// <summary>
+			/// A file that is a sparse file.
+			/// </summary>
+			SparseFile = 0x00000200,
+
+			/// <summary>
+			/// A file or directory that has an associated reparse point, or a file that is a symbolic link.
+			/// </summary>
+			ReparsePoint = 0x00000400,
+
+			/// <summary>
+			/// A file or directory that is compressed. For a file, all of the data in the file is compressed. For a directory, compression is the default for newly created files and subdirectories.
+			/// </summary>
+			Compressed = 0x00000800,
+
+			/// <summary>
+			/// The data of a file is not available immediately. This attribute indicates that the file data is physically moved to offline storage. This attribute is used by Remote Storage, which is the hierarchical storage management software. Applications should not arbitrarily change this attribute.
+			/// </summary>
+			Offline = 0x00001000,
+
+			/// <summary>
+			/// The file or directory is not to be indexed by the content indexing service.
+			/// </summary>
+			NotContentIndexed = 0x00002000,
+
+			/// <summary>
+			/// A file or directory that is encrypted. For a file, all data streams in the file are encrypted. For a directory, encryption is the default for newly created files and subdirectories.
+			/// </summary>
+			Encrypted = 0x00004000,
+
+			/// <summary>
+			/// This value is reserved for system use.
+			/// </summary>
+			Virtual = 0x00010000
+		}
 	}
 
 }
