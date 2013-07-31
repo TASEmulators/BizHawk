3dc4611263c48153c00855442bb686bb59c6b5d1
adelikat <adelikat@tasvideos.org>
2013-07-31 15:54:08 +0000

    Start a firmwares dialog, interim only

---
+++ BizHawk.MultiClient/config/FirmwaresConfig.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BizHawk.MultiClient
+{
+    public partial class FirmwaresConfig : Form
+    {
+        //TODO: commodore 64, intellivision
+
+
+
+        public FirmwaresConfig()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseBtn_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void FirmwaresConfig_Load(object sender, EventArgs e)
+        {
+            DoScan();
+        }
+
+        private void ScanBtn_Click(object sender, EventArgs e)
+        {
+            DoScan();
+        }
+
+        private void DoScan()
+        {
+            string p = Global.Config.FirmwaresPath;
+            FileInfo file;
+
+            file = new FileInfo(Path.Combine(p, Global.Config.FilenameFDSBios));
+            if (file.Exists) Disksys_ROM_PicBox.Image = MultiClient.Properties.Resources.GreenCheck; else Disksys_ROM_PicBox.Image = MultiClient.Properties.Resources.ExclamationRed;
+        }
+    }
+}
