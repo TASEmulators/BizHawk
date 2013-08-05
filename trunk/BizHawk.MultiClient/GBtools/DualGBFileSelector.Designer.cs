467abadf07a917e81cd416bdbcf0e47c367acbfa
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2013-08-05 22:36:18 +0000

    Oops, do show dual gb xml menu item since that feature is ready for release, also some misc dialog cleanup on it

--- trunk/BizHawk.MultiClient/GBtools/DualGBFileSelector.Designer.cs
+++ trunk/BizHawk.MultiClient/GBtools/DualGBFileSelector.Designer.cs
@@ -38,7 +38,7 @@
 			this.button1.Location = new System.Drawing.Point(279, 3);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 0;
+			this.button1.TabIndex = 2;
 			this.button1.Text = "Browse...";
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
