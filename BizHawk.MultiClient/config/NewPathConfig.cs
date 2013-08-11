5f6f1e29b991041b201682b6067dc4507009e454
adelikat <adelikat@tasvideos.org>
2013-08-11 14:28:14 +0000

    Some progress on new path config

--- BizHawk.MultiClient/config/NewPathConfig.cs
+++ BizHawk.MultiClient/config/NewPathConfig.cs
@@ -12,7 +12,6 @@ namespace BizHawk.MultiClient
 	public partial class NewPathConfig : Form
 	{
 		//TODO:
-		//System ID and preferred folder name do not match, needs new param
 		//FInish config entries
 		//Set tab focus based on system ID
 		//Remaining GUI items
@@ -46,6 +45,53 @@ namespace BizHawk.MultiClient
 
 		private void LoadSettings()
 		{
+			RecentForROMs.Checked = Global.Config.UseRecentForROMs;
+			BasePathBox.Text = Global.Config.BasePath;
+			DoTabs();
+			SetDefaultFocusedTab();
+		}
+
+		private void SetDefaultFocusedTab()
+		{
+			switch (Global.Game.System)
+			{
+				case "NULL":
+					PathTabControl.SelectTab(FindTabByName("Global"));
+					break;
+				default:
+					PathTabControl.SelectTab(FindTabByName(Global.Game.System));
+					break;
+
+				//"Sub" Systems and other exceptions go here
+				case "PCECD":
+				case "SGX":
+					PathTabControl.SelectTab(FindTabByName("PCE"));
+					break;
+				case "GBC":
+					PathTabControl.SelectTab(FindTabByName("GB"));
+					break;
+				case "SGB":
+					PathTabControl.SelectTab(FindTabByName("SNES"));
+					break;
+			}
+		}
+
+		private TabPage FindTabByName(string name)
+		{
+			IEnumerable<TabPage> query = from p in PathTabControl.TabPages.OfType<TabPage>() select p;
+			var tab = query.FirstOrDefault(x => x.Name.ToUpper() == name.ToUpper());
+			if (tab == null)
+			{
+				return new TabPage();
+			}
+			else
+			{
+				return tab;
+			}
+		}
+
+		private void DoTabs()
+		{
 			//Separate by system
 			List<string> systems = Global.Config.PathEntries.Select(x => x.System).Distinct().ToList();
 			systems.Sort();
@@ -53,11 +99,12 @@ namespace BizHawk.MultiClient
 
 			//TODO: fix anchoring
 			//TODO: fix logic of when to pass in the system (global and base do not want this)
-			foreach(string tab in systems)
+			foreach (string tab in systems)
 			{
 				TabPage t = new TabPage()
 				{
 					Text = tab,
+					Name = tab,
 				};
 				List<PathEntry> paths = Global.Config.PathEntries.Where(x => x.System == tab).OrderBy(x => x.Ordinal).ThenBy(x => x.Type).ToList();
 
@@ -66,9 +113,9 @@ namespace BizHawk.MultiClient
 				int textbox_width = 150;
 				int padding = 10;
 				int button_width = 26;
-				foreach(var path in paths)
+				foreach (var path in paths)
 				{
-					
+
 					TextBox box = new TextBox()
 					{
 						Text = path.Path,
@@ -85,7 +132,8 @@ namespace BizHawk.MultiClient
 						Width = button_width,
 						//Anchor = AnchorStyles.Top | AnchorStyles.Right,
 					};
-					btn.Click += new System.EventHandler(delegate {
+					btn.Click += new System.EventHandler(delegate
+					{
 						BrowseFolder(box, path.Type, path.System);
 					});
 
@@ -123,7 +171,33 @@ namespace BizHawk.MultiClient
 
 		private void SaveSettings()
 		{
+			Global.Config.UseRecentForROMs = RecentForROMs.Checked;
+			Global.Config.BasePath = BasePathBox.Text;
+			//TODO
+		}
 
+		private void BrowseBase_Click(object sender, EventArgs e)
+		{
+			FolderBrowserEx f = new FolderBrowserEx
+			{
+				Description = "Set the directory for the base global path",
+				SelectedPath = PathManager.MakeAbsolutePath(BasePathBox.Text)
+			};
+			DialogResult result = f.ShowDialog();
+			if (result == DialogResult.OK)
+			{
+				BasePathBox.Text = f.SelectedPath;
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			new PathInfo().Show();
+		}
+
+		private void RecentForROMs_CheckedChanged(object sender, EventArgs e)
+		{
+			//TODO
 		}
 	}
 }
