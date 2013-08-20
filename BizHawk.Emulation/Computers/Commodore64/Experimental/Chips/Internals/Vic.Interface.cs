5c762638c1c1a90c1f1b67273cedb749f5c38b62
saxxonpike <saxxonpike@gmail.com>
2013-08-20 08:27:36 +0000

    Commodore64: Video registers updated in experimental framework.

--- BizHawk.Emulation/Computers/Commodore64/Experimental/Chips/Internals/Vic.Interface.cs
+++ BizHawk.Emulation/Computers/Commodore64/Experimental/Chips/Internals/Vic.Interface.cs
@@ -11,12 +11,12 @@ namespace BizHawk.Emulation.Computers.Commodore64.Experimental.Chips.Internals
         public Func<int, int> ReadColorRam;
         public Func<int, int> ReadRam;
 
-        public bool AEC { get { return cachedAEC; } }
-        public bool BA { get { return cachedBA; } }
-        public bool IRQ { get { return cachedIRQ; } }
+        public bool AEC { get { return aec; } }
+        public bool BA { get { return ba; } }
+        public bool IRQ { get { return irq; } }
 
-        public bool OutputAEC() { return AEC; }
-        public bool OutputBA() { return BA; }
-        public bool OutputIRQ() { return IRQ; }
+        public bool OutputAEC() { return aec; }
+        public bool OutputBA() { return ba; }
+        public bool OutputIRQ() { return irq; }
     }
 }
