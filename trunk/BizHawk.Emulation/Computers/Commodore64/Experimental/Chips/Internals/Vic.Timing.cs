c5f6f3697d5f6936031734c014dc1fe3e9a42b5e
saxxonpike@gmail.com <saxxonpike@gmail.com@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2013-08-21 09:38:22 +0000

    Commodore64: Experimental VIC updated.

--- trunk/BizHawk.Emulation/Computers/Commodore64/Experimental/Chips/Internals/Vic.Timing.cs
+++ trunk/BizHawk.Emulation/Computers/Commodore64/Experimental/Chips/Internals/Vic.Timing.cs
@@ -7,44 +7,74 @@ namespace BizHawk.Emulation.Computers.Commodore64.Experimental.Chips.Internals
 {
     sealed public partial class Vic
     {
-        protected struct CycleTiming
-        {
-            public int CharacterCycle;
-            public int RefreshCycle;
-            public int SpriteCycle;
-        }
-        
-        protected struct DisplayTiming
-        {
-            public int CyclesPerLine;
-            public int HBlankStart;
-            public int HBlankEnd;
-            public int LeftXCoordinate;
-            public int LinesPerFrame;
-            public int VBlankStart;
-            public int VBlankEnd;
-            public int VisibleLines;
-            public int VisiblePixels;
-        }
-
-        protected class VicTiming
-        {
-            public CycleTiming Cycle;
-            public DisplayTiming Display;
-        }
-
         enum FetchState
         {
+            None,
             Idle,
             Graphics,
-            Color,
+            Character,
             Refresh,
             Sprite,
             Pointer
         }
+        FetchState fetchState;
 
-        int characterCycleCount;
-        int refreshCycleCount;
-        int spriteCycleCount;
+        bool characterBA;
+        int characterBAEnd;
+        int characterBAStart;
+        bool characterFetch;
+        int characterFetchStart;
+        bool graphicsFetch;
+        bool hBlank;
+        int hBlankDelay;
+        int rasterAdvance;
+        int rasterCount;
+        int rasterDelay;
+        int rasterWidth;
+        bool refreshFetch;
+        int refreshStart;
+        int screenXEnd;
+        int screenXStart;
+        int screenYEnd;
+        int screenYStart;
+        bool vBlank;
+
+        void InitTiming(VicTiming timing)
+        {
+            int spriteBAStart = timing.SpriteBAStart;
+
+            for (int i = 0; i < 8; i++)
+            {
+                sprites[i].BAStart = spriteBAStart % timing.HSize;
+                sprites[i].BAEnd = (spriteBAStart + 40) % timing.HSize;
+                sprites[i].FetchStart = (spriteBAStart + 24) % timing.HSize;
+            }
+
+            characterBAStart = timing.CharacterBAStart % timing.HSize;
+            characterBAEnd = (characterBAStart + 344) % timing.HSize;
+            characterFetchStart = (characterFetchStart + 24) % timing.HSize;
+            screenXStart = timing.HBlankEnd;
+            screenXEnd = timing.HBlankStart;
+            screenYStart = timing.VBlankEnd;
+            screenYEnd = timing.VBlankStart;
+            rasterWidth = timing.HSize;
+            rasterAdvance = timing.LineStart;
+            rasterCount = timing.VSize;
+        }
+    }
+
+    sealed public class VicTiming
+    {
+        public int CharacterBAStart; //VMBA
+        public int HBlankDelay;
+        public int HBlankEnd; //HBLANK
+        public int HBlankStart; //HBLANK
+        public int HSize; //BOL
+        public int LineStart; //VINC
+        public int RefreshStart; //REFW
+        public int SpriteBAStart; //SPBA
+        public int VBlankEnd; //VBLANK
+        public int VBlankStart; //VBLANK
+        public int VSize; //VRESET
     }
 }
--- trunk/BizHawk.Emulation/Computers/Commodore64/Experimental/Chips/Internals/Vic.VideoProvider.cs
+++ trunk/BizHawk.Emulation/Computers/Commodore64/Experimental/Chips/Internals/Vic.VideoProvider.cs
@@ -7,31 +7,59 @@ namespace BizHawk.Emulation.Computers.Commodore64.Experimental.Chips.Internals
 {
     sealed public partial class Vic : IVideoProvider
     {
-        protected int[] videoBuffer;
+        int borderPixelBufferIndex;
+        int[] pixelBuffer;
+        int pixelBufferIndex;
+        int pixelBufferLength;
+        int screenHeight;
+        int screenWidth;
+        int[] videoBuffer;
+        int videoBufferIndex;
+
+        // palette
+        static private int[] palette =
+        {
+                Colors.ARGB(0x00, 0x00, 0x00),
+                Colors.ARGB(0xFF, 0xFF, 0xFF),
+                Colors.ARGB(0x68, 0x37, 0x2B),
+                Colors.ARGB(0x70, 0xA4, 0xB2),
+                Colors.ARGB(0x6F, 0x3D, 0x86),
+                Colors.ARGB(0x58, 0x8D, 0x43),
+                Colors.ARGB(0x35, 0x28, 0x79),
+                Colors.ARGB(0xB8, 0xC7, 0x6F),
+                Colors.ARGB(0x6F, 0x4F, 0x25),
+                Colors.ARGB(0x43, 0x39, 0x00),
+                Colors.ARGB(0x9A, 0x67, 0x59),
+                Colors.ARGB(0x44, 0x44, 0x44),
+                Colors.ARGB(0x6C, 0x6C, 0x6C),
+                Colors.ARGB(0x9A, 0xD2, 0x84),
+                Colors.ARGB(0x6C, 0x5E, 0xB5),
+                Colors.ARGB(0x95, 0x95, 0x95)
+        };
 
         public int[] GetVideoBuffer()
         {
-            throw new NotImplementedException();
+            return videoBuffer;
         }
 
         public int VirtualWidth
         {
-            get { throw new NotImplementedException(); }
+            get { return screenWidth; }
         }
 
         public int BufferWidth
         {
-            get { throw new NotImplementedException(); }
+            get { return screenWidth; }
         }
 
         public int BufferHeight
         {
-            get { throw new NotImplementedException(); }
+            get { return screenHeight; }
         }
 
         public int BackgroundColor
         {
-            get { throw new NotImplementedException(); }
+            get { return palette[0]; }
         }
     }
 }
