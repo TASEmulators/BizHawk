31b9e37f864cf508b31c19c3c04f1cb9a6a4d0f9
adelikat <adelikat@tasvideos.org>
2013-08-23 03:35:41 +0000

    Start the LuaHawk project, doesn't do anything yet, but will do all the that the lua editor will do, and eventually the lua console will simply open a LuaHawk process

---
+++ LuaHawk/LuaHawkMainForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LuaHawk
+{
+	public partial class LuaHawkMainForm : Form
+	{
+		public LuaHawkMainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
+
+		private void LuaHawkMainForm_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
