e760ff56a4dbd238c04a941e9bc8ee004adc9b86
adelikat <adelikat@tasvideos.org>
2013-09-14 06:02:53 +0000

    Start New Ram Search

---
+++ BizHawk.MultiClient/tools/Watch/NewRamSearch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Globalization;
+
+namespace BizHawk.MultiClient
+{
+	/// <summary>
+	/// A winform designed to search through ram values
+	/// </summary>
+	public partial class NewRamSearch : Form
+	{
+		public NewRamSearch()
+		{
+			SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+			SetStyle(ControlStyles.UserPaint, true);
+			SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+			InitializeComponent();
+		}
+
+		private void RamSearch_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		
+	}
+}
