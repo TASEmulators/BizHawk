c23cdb78df4c5f30a146894e5c6be8d5898ebc9a
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2013-11-03 14:40:55 +0000

    Move MakeRelease.bat to Dist, and fix MakeDiscoHawk.at

--- trunk/Dist/MakeRelease.bat
+++ trunk/Dist/MakeRelease.bat
@@ -1,4 +1,5 @@
 del /s BizHawk.zip
+cd ..\output
 
 rem slimdx has a way of not making it into the output directory, so this is a good time to make sure
 copy ..\..\SlimDx.dll
@@ -9,4 +10,4 @@ rem explicitly list the OK ones here as individual copies. until then....
 
 copy *.dll dll
 
-zip -X -9 -r BizHawk.zip EmuHawk.exe DiscoHawk.exe defctrl.json dll gamedb NES\Palettes Lua Gameboy\Palettes Firmware/BIOS_Info.txt  -x *.pdb -x *.lib -x *.pgd -x *.exp -x dll\libsneshawk-64*.exe
+zip -X -9 -r ..\Dist\BizHawk.zip EmuHawk.exe DiscoHawk.exe defctrl.json dll gamedb NES\Palettes Lua Gameboy\Palettes Firmware/BIOS_Info.txt  -x *.pdb -x *.lib -x *.pgd -x *.exp -x dll\libsneshawk-64*.exe
