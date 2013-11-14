6b90b47bc5e4209a1a5277528e953dd4f628cfb9
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2013-11-14 21:42:29 +0000

    Fix CpuCoreGenerator project - generate new stuff in HuC6280, fix namespaces and file paths, comment out 6502, we don't use that one anymore.

--- trunk/CpuCoreGenerator/Program.cs
+++ trunk/CpuCoreGenerator/Program.cs
@@ -2,14 +2,15 @@
 {
     static void Main()
     {
-        var x = new M6502.CoreGenerator();
-        x.InitOpcodeTable();
-        x.GenerateDisassembler("../../../BizHawk.Emulation/CPUs/MOS 6502/Disassembler.cs");
-        x.GenerateExecutor("../../../BizHawk.Emulation/CPUs/MOS 6502/Execute.cs");
+        //We don't currently use this anymore
+        //var x = new M6502.CoreGenerator();
+        //x.InitOpcodeTable();
+        //x.GenerateDisassembler("../../../BizHawk.Emulation.Common/CPUs/MOS 6502X/Disassembler.cs");
+        //x.GenerateExecutor("../../../BizHawk.Emulation.Common/CPUs/MOS 6502X/Execute.cs");
 
         var y = new HuC6280.CoreGenerator();
         y.InitOpcodeTable();
-        y.GenerateDisassembler("../../../BizHawk.Emulation/CPUs/HuC6280/Disassembler.cs");
-        y.GenerateExecutor("../../../BizHawk.Emulation/CPUs/HuC6280/Execute.cs");
+        y.GenerateDisassembler("../../../BizHawk.Emulation.Common/CPUs/HuC6280/Disassembler.cs");
+        y.GenerateExecutor("../../../BizHawk.Emulation.Common/CPUs/HuC6280/Execute.cs");
     }
 }
