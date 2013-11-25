a7acc10bf64919a1d3f81ffca4f94b70f900a691
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2013-11-25 00:45:05 +0000

    upgrade the version number of the 1.5x branch to 1.5.3 since that is the upcoming release

--- branches/1.5x/VersionInfo.cs
+++ branches/1.5x/VersionInfo.cs
@@ -1,8 +1,8 @@
 static class VersionInfo
 {
-	public const string MAINVERSION = "1.5.2";
+	public const string MAINVERSION = "1.5.3";
 	public const string RELEASEDATE = "August 22, 2013";
-	public static bool INTERIM = true;
+	public static bool INTERIM = false;
 
 	public static string GetEmuVersion()
 	{
