bd1c1196a79601f50758c306df76966a137b8096
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2013-11-27 23:53:27 +0000

    Merge 5456 to 1.5x branch

--- branches/1.5x/VersionInfo.cs
+++ branches/1.5x/VersionInfo.cs
@@ -1,7 +1,7 @@
 static class VersionInfo
 {
 	public const string MAINVERSION = "1.5.3";
-	public const string RELEASEDATE = "August 22, 2013";
+	public const string RELEASEDATE = "November 28, 2013";
 	public static bool INTERIM = false;
 
 	public static string GetEmuVersion()
