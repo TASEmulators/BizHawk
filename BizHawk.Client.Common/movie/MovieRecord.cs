075a1fb0778545b87f93ea8ade68cd243e3ba6b4
adelikat <adelikat@tasvideos.org>
2013-12-02 04:24:45 +0000

    TasMovie.cs - more stubbing out of stuff

--- BizHawk.Client.Common/movie/MovieRecord.cs
+++ BizHawk.Client.Common/movie/MovieRecord.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace BizHawk.Client.Common
 {
-	class MovieRecord : IMovieRecord
+	public class MovieRecord : IMovieRecord
 	{
 		private List<byte> _state = new List<byte>();
 
@@ -19,6 +19,25 @@ namespace BizHawk.Client.Common
 		public MovieRecord()
 		{
 
+		}
+
+		public override string ToString()
+		{
+			//TODO: consider the fileformat of binary and lagged data
+			return Input;
+		}
+	}
+
+	public class MovieRecordList : List<MovieRecord>
+	{
+		public override string ToString()
+		{
+			var sb = new StringBuilder();
+			foreach (var record in this)
+			{
+				sb.AppendLine(record.ToString());
+			}
+			return sb.ToString();
 		}
 	}
 }
