8daccb972d47f968f31c8344c7fac2b39ec4b629
adelikat <adelikat@tasvideos.org>
2014-01-04 16:19:00 +0000

    Ram Search - fix behavior of the Memory Domain label

--- BizHawk.Client.EmuHawk/tools/Watch/RamSearch.cs
+++ BizHawk.Client.EmuHawk/tools/Watch/RamSearch.cs
@@ -272,6 +272,7 @@ namespace BizHawk.Client.EmuHawk
 			ToggleSearchDependentToolBarItems();
 			SetReboot(false);
 			MessageLabel.Text = String.Empty;
+			SetDomainLabel();
 		}
 
 		public void NextCompareTo(bool reverse = false)
@@ -492,7 +493,6 @@ namespace BizHawk.Client.EmuHawk
 		private void SetMemoryDomain(string name)
 		{
 			_settings.Domain = Global.Emulator.MemoryDomains[name];
-			SetDomainLabel();
 			SetReboot(true);
 			SpecificAddressBox.MaxLength = IntHelpers.GetNumDigits(_settings.Domain.Size);
 			DoDomainSizeCheck();
