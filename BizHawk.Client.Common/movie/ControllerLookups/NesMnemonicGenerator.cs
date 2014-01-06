ef797341704ab24b5e94a06c08fb78b081321453
adelikat <adelikat@tasvideos.org>
2014-01-06 03:59:00 +0000

    movies - start another rewrite of mnemonic generation

---
+++ BizHawk.Client.Common/movie/ControllerLookups/NesMnemonicGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BizHawk.Client.Common
+{
+	public class NesMnemonicGenerator : IMnemonicGeneratorCollection
+	{
+		public NesMnemonicGenerator()
+		{
+
+		}
+
+		public IEnumerable<IMnemonicGenerator> Generators
+		{
+			get
+			{
+				return Enumerable.Empty<IMnemonicGenerator>();
+			}
+		}
+	}
+}
