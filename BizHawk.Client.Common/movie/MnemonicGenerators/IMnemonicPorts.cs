c0295316f551f97bf0f483b5352f3675c8f0f590
adelikat <adelikat@tasvideos.org>
2014-01-10 23:08:56 +0000

    Some Movie 2.0 reorg

---
+++ BizHawk.Client.Common/movie/MnemonicGenerators/IMnemonicPorts.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BizHawk.Client.Common
+{
+	/// <summary>
+	/// A console specific collection of Mnemonic generators
+	/// This handles includes all the "business" logic specific to the console
+	/// </summary>
+	public interface IMnemonicPorts
+	{
+		/// <summary>
+		/// Total number of available controller ports (this does not include the console controls
+		/// </summary>
+		int Count { get; }
+
+		/// <summary>
+		/// Source controller to read input state from
+		/// </summary>
+		IController Source { get; set; }
+
+		/// <summary>
+		/// Gets or sets the given port with an IMnemonicGenerator implementation
+		/// Ports are zero based
+		/// Set will throw an InvalidOperationException if a particular implementation is not allowed, this is platform specific logic such as NES doesn't allow a zapper in port 0, etc
+		/// Both will throw an ArgumentOutOfRangeException exception if portNum is not less than Count
+		/// </summary>
+		IMnemonicGenerator this[int portNum] { get; set; }
+
+		/// <summary>
+		/// Gets an IMnemonicGenerator implementation that represents the buttons and controls on the console itself (Reset, Power, etc)
+		/// </summary>
+		IMnemonicGenerator ConsoleControls { get; }
+
+		Dictionary<string, bool> ParseMnemonicString(string mnemonicStr);
+
+		// Analog TODO: this assume the generators are boolean
+		Dictionary<string, bool> GetBoolButtons();
+
+		// TODO: this shouldn't be required, refactor MovieRecord
+		string GenerateMnemonicString(Dictionary<string, bool> buttons);
+
+		string EmptyMnemonic { get; }
+
+		Dictionary<string, char> AvailableMnemonics { get; }
+	}
+}
