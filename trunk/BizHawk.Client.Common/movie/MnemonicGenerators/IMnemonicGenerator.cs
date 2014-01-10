7a376f8116c90a22be0ce666e8f092d0658d60ae
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-01-10 23:08:56 +0000

    Some Movie 2.0 reorg

---
+++ trunk/BizHawk.Client.Common/movie/MnemonicGenerators/IMnemonicGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BizHawk.Client.Common
+{
+	public interface IMnemonicGenerator
+	{
+		IController Source { get; set; }
+
+		string Name { get; }
+
+		/// <summary>
+		/// Will be prepended to all button names
+		/// Example: "P1"
+		/// </summary>
+		string ControllerPrefix { get; set; }
+
+		void Add(string key, char value);
+
+		char this[string key] { get; }
+		bool IsEmpty { get; }
+		string MnemonicString { get; }
+
+		/// <summary>
+		/// Returns a string that represents an empty or default mnemonic 
+		/// </summary>
+		string EmptyMnemonicString { get; }
+
+		// Analog TODO: this assumes the Generator is boolean
+		/// <summary>
+		/// Parses a segment of a full mnemonic string (the content between pipes)
+		/// Note: this assume the pipes are not being passed in!
+		/// </summary>
+		IDictionary<string, bool> ParseMnemonicSegment(string mnemonicSegment);
+
+		// Analog Support TODO: this assume the Generator is boolean
+		//Dictionary<string, bool> GetBoolButtons();
+
+		Dictionary<string, char> AvailableMnemonics { get; }
+	}
+}
---
+++ trunk/BizHawk.Client.Common/movie/MnemonicGenerators/MnemonicGenerators.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using BizHawk.Common;
+using BizHawk.Emulation.Common;
+
+namespace BizHawk.Client.Common
+{
+	public class BooleanControllerMnemonicGenerator : IMnemonicGenerator
+	{
+		private NamedDictionary<string, char> _controllerMnemonics;
+
+		public BooleanControllerMnemonicGenerator(string name, IDictionary<string, char> mnemonics)
+		{
+			_controllerMnemonics = new NamedDictionary<string, char>(name);
+			foreach (var kvp in mnemonics)
+			{
+				_controllerMnemonics.Add(kvp.Key, kvp.Value);
+			}
+		}
+
+		public void Add(string key, char value)
+		{
+			_controllerMnemonics.Add(key, value);
+		}
+
+		public Dictionary<string, char> AvailableMnemonics
+		{
+			get
+			{
+				return _controllerMnemonics.ToDictionary(kvp => ControllerPrefix + " " + kvp.Key, kvp => kvp.Value);
+			}
+		}
+
+		public IController Source { get; set; }
+		public string ControllerPrefix { get; set; }
+		public string Name
+		{
+			get { return _controllerMnemonics.Name; }
+		}
+
+		public char this[string key]
+		{
+			get
+			{
+				return _controllerMnemonics[ControllerPrefix + " " + key];
+			}
+		}
+
+		public bool IsEmpty
+		{
+			get
+			{
+				return _controllerMnemonics.All(kvp => !this.Source.IsPressed(kvp.Key));
+			}
+		}
+
+		public string MnemonicString
+		{
+			get
+			{
+				var sb = new StringBuilder(_controllerMnemonics.Count);
+				foreach (var kvp in _controllerMnemonics)
+				{
+					sb.Append(Source.IsPressed(kvp.Key) ? kvp.Value : '.');
+				}
+
+				return sb.ToString();
+			}
+		}
+
+		public string EmptyMnemonicString
+		{
+			get
+			{
+				var sb = new StringBuilder(_controllerMnemonics.Count);
+				foreach (var kvp in _controllerMnemonics)
+				{
+					sb.Append('.');
+				}
+
+				return sb.ToString();
+			}
+		}
+
+		public IDictionary<string, bool> ParseMnemonicSegment(string mnemonicSegment)
+		{
+			var buttons = new Dictionary<string, bool>();
+			var keys = _controllerMnemonics.Select(kvp => kvp.Key).ToList();
+
+			for (int i = 0; i < mnemonicSegment.Length; i++)
+			{
+				buttons.Add(keys[i], mnemonicSegment[i] != '.');
+			}
+			
+			return buttons;
+		}
+	}
+}
