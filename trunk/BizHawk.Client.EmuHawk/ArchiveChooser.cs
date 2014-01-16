3543de2a1d4f4c94a447c1f9458271d2402b2d9e
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-01-16 02:38:27 +0000

    Archive Chooser - restore double-click selection (weird that I had to do it this way), and resize some things

--- trunk/BizHawk.Client.EmuHawk/ArchiveChooser.cs
+++ trunk/BizHawk.Client.EmuHawk/ArchiveChooser.cs
@@ -15,8 +15,8 @@ namespace BizHawk.Client.EmuHawk
 		IList<ListViewItem> archiveItems = new List<ListViewItem>();
 		ToolTip errorBalloon = new ToolTip();
 
-        static bool useRegEx = false;
-        static bool matchWhileTyping = true;
+		static bool useRegEx = false;
+		static bool matchWhileTyping = true;
 
 		public ArchiveChooser(HawkFile hawkfile)
 		{
@@ -24,17 +24,17 @@ namespace BizHawk.Client.EmuHawk
 
 			errorBalloon.IsBalloon = true;
 			errorBalloon.InitialDelay = 0;
-            if (useRegEx)
-                radRegEx.Checked = true;
-            else
-                radSimple.Checked = true;
-            cbInstantFilter.Checked = matchWhileTyping;
+			if (useRegEx)
+				radRegEx.Checked = true;
+			else
+				radSimple.Checked = true;
+			cbInstantFilter.Checked = matchWhileTyping;
 
 			var items = hawkfile.ArchiveItems;
-			for (int i=0;i<items.Count;i++)
+			for (int i = 0; i < items.Count; i++)
 			{
 				var item = items[i];
-				var lvi = new ListViewItem {Tag = i};
+				var lvi = new ListViewItem { Tag = i };
 				lvi.SubItems.Add(new ListViewItem.ListViewSubItem());
 				lvi.Text = item.name;
 				long size = item.size;
@@ -43,14 +43,14 @@ namespace BizHawk.Client.EmuHawk
 					size -= 16;
 				lvi.SubItems[1].Text = Util.FormatFileSize(size);
 				archiveItems.Add(lvi);
-            }
+			}
 
 			InitializeFileView();
 		}
 
 		private void InitializeFileView()
 		{
-            archiveItems.OrderBy(x => x.Name);
+			archiveItems.OrderBy(x => x.Name);
 
 			lvMembers.BeginUpdate();
 			try
@@ -97,23 +97,23 @@ namespace BizHawk.Client.EmuHawk
 		private void ArchiveChooser_Load(object sender, EventArgs e)
 		{
 			lvMembers.Items[0].Selected = true;
-            tbFilter.Select();
+			tbFilter.Select();
 		}
 
 		private void btnSearch_Click(object sender, EventArgs e)
 		{
 			StartMatching(tbSearch, DoSearch);
-        }
+		}
 
-        private void cbInstantFilter_CheckedChanged(object sender, EventArgs e)
-        {
-            matchWhileTyping = cbInstantFilter.Checked;
-        }
+		private void cbInstantFilter_CheckedChanged(object sender, EventArgs e)
+		{
+			matchWhileTyping = cbInstantFilter.Checked;
+		}
 
-        private void radRegEx_CheckedChanged(object sender, EventArgs e)
-        {
-            useRegEx = radRegEx.Checked;
-        }
+		private void radRegEx_CheckedChanged(object sender, EventArgs e)
+		{
+			useRegEx = radRegEx.Checked;
+		}
 
 		private void tbFilter_TextChanged(object sender, EventArgs e)
 		{
@@ -129,20 +129,21 @@ namespace BizHawk.Client.EmuHawk
 		}
 
 		private void StartMatching(TextBox tb, Action<IMatcher> func)
-	{
+		{
 			try
 			{
 				errorBalloon.Hide(tb);
 				var searchMatcher = CreateMatcher(tb.Text);
-                if (searchMatcher != null)
+				if (searchMatcher != null)
 				{
-                    func(searchMatcher);
+					func(searchMatcher);
 				}
 			}
 			catch (ArgumentException ex)
 			{
 				string errMsg = ex.Message;
 				errMsg = errMsg.Substring(errMsg.IndexOf('-') + 2);
+
 				// Balloon is bugged on first invocation
 				errorBalloon.Show("Error parsing RegEx: " + errMsg, tb);
 				errorBalloon.Show("Error parsing RegEx: " + errMsg, tb);
@@ -151,21 +152,21 @@ namespace BizHawk.Client.EmuHawk
 
 		private void DoSearch(IMatcher searchMatcher)
 		{
-            int count = lvMembers.Items.Count;
+			int count = lvMembers.Items.Count;
 			int searchStartIdx = 0;
 			if (lvMembers.SelectedItems.Count > 0)
 			{
 				searchStartIdx = (lvMembers.SelectedIndices[0] + 1) % count;
-            }
-            int? searchResultIdx = null;
+			}
+			int? searchResultIdx = null;
 
 			for (int i = 0; i < count; ++i)
 			{
-                int curIdx = (searchStartIdx + i) % count;
+				int curIdx = (searchStartIdx + i) % count;
 				if (searchMatcher.Matches(lvMembers.Items[curIdx]))
 				{
-                    searchResultIdx = curIdx;
-                    break;
+					searchResultIdx = curIdx;
+					break;
 				}
 			}
 			if (searchResultIdx != null)
@@ -175,9 +176,9 @@ namespace BizHawk.Client.EmuHawk
 			}
 			else
 			{
-                // Balloon is bugged on first invocation
-                errorBalloon.Show("Could not find search text", tbSearch);
-                errorBalloon.Show("Could not find search text", tbSearch);
+				// Balloon is bugged on first invocation
+				errorBalloon.Show("Could not find search text", tbSearch);
+				errorBalloon.Show("Could not find search text", tbSearch);
 			}
 		}
 
@@ -209,17 +210,18 @@ namespace BizHawk.Client.EmuHawk
 		private class SimpleMatcher : IMatcher
 		{
 			public string[] Keys { get; set; }
-            public bool Matches(ListViewItem value)
+			public bool Matches(ListViewItem value)
 			{
-                string searchedStr = value.Text.ToLower();
-                foreach (string key in Keys)
-                {
-                    if (!searchedStr.Contains(key))
-                    {
-                        return false;
-                    }
-                }
-                return true;
+				string searchedStr = value.Text.ToLower();
+				foreach (string key in Keys)
+				{
+					if (!searchedStr.Contains(key))
+					{
+						return false;
+					}
+				}
+
+				return true;
 			}
 		};
 
@@ -236,9 +238,11 @@ namespace BizHawk.Client.EmuHawk
 		{
 			if (radSimple.Checked)
 			{
-				return new SimpleMatcher {
-                    Keys = searchKey.ToLower().Split(new char[0],
-                        StringSplitOptions.RemoveEmptyEntries)};
+				return new SimpleMatcher
+				{
+					Keys = searchKey.ToLower().Split(new char[0],
+						StringSplitOptions.RemoveEmptyEntries)
+				};
 			}
 			else
 			{
