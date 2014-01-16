7e685551c813d3122ebad34526f0b0bb88da1289
mvl1986 <mvl1986@gmail.com>
2014-01-16 16:41:13 +0000

    Added Sorting to DetailsView in PlayMovie

---
+++ BizHawk.Client.EmuHawk/movie/MovieDetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing;
+
+namespace BizHawk.Client.EmuHawk
+{
+    /// <summary>
+    /// Used for the sorting of the moviedetails in PlayMovie.cs
+    /// </summary>
+   public class MovieDetails
+    {
+       public String keys { get; set; }
+       public String values { get; set; }
+       public Color backgroundColor { get; set; }
+
+        public MovieDetails()
+        {
+            keys = String.Empty;
+            values = String.Empty;
+            backgroundColor = Color.White;
+        }
+    }
+}
