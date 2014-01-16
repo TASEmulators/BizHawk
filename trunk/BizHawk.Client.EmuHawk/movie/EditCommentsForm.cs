b7ef4d14b80cd2a6a11c0f5c2048334bff1288b9
mvl1986@gmail.com <mvl1986@gmail.com@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-01-16 13:47:04 +0000

    Added sorting to the Datagridview

--- trunk/BizHawk.Client.EmuHawk/movie/EditCommentsForm.cs
+++ trunk/BizHawk.Client.EmuHawk/movie/EditCommentsForm.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
-
+using System.ComponentModel;
 using BizHawk.Client.Common;
 
 namespace BizHawk.Client.EmuHawk
@@ -8,11 +8,42 @@ namespace BizHawk.Client.EmuHawk
 	public partial class EditCommentsForm : Form
 	{
 		private IMovie _selectedMovie;
+        private String _lastHeaderClicked;
+        private Boolean _sortReverse;
+        
+        public EditCommentsForm()
+        {
+            InitializeComponent();
+            _lastHeaderClicked = "";
+            _sortReverse = false;
+        }
 
-		public EditCommentsForm()
-		{
-			InitializeComponent();
-		}
+        private void OnColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            SortColumn(CommentGrid.Columns[e.ColumnIndex]);
+        }
+
+        private void SortColumn(DataGridViewColumn e)
+        {
+            ListSortDirection _direction;
+            DataGridViewColumn _column = e;
+            if (_lastHeaderClicked != _column.Name)
+            {
+                _sortReverse = false;
+            }
+            if (!_sortReverse)
+            {
+                _direction = ListSortDirection.Ascending;
+            }
+            else
+            {
+                _direction = ListSortDirection.Descending;
+            }
+            CommentGrid.Sort(_column, _direction);
+            _lastHeaderClicked = _column.Name;
+            _sortReverse = !_sortReverse;
+            CommentGrid.Refresh();
+        }
 
 		private void EditCommentsForm_Load(object sender, EventArgs e)
 		{
