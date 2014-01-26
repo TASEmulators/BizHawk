1225baeeb0dbdc3d23bdb8d11fb53e3db6f86920
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-01-26 13:30:45 +0000

    Lua - new Register method on some more libraries

--- trunk/BizHawk.Client.Common/lua/EmuLuaLibrary.String.cs
+++ trunk/BizHawk.Client.Common/lua/EmuLuaLibrary.String.cs
@@ -20,75 +20,131 @@ namespace BizHawk.Client.Common
 					"startswith",
 					"substring",
 					"contains",
-					"endswith",
+					"endswith"
 				};
 			}
 		}
-		public static string string_hex(object num)
+
+		[LuaMethodAttributes(
+			"hex",
+			"TODO"
+		)]
+		public static string Hex(object num)
 		{
-			string hex = String.Format("{0:X}", LuaLong(num));
-			if (hex.Length == 1) hex = "0" + hex;
+			var hex = String.Format("{0:X}", LuaLong(num));
+			if (hex.Length == 1)
+			{
+				hex = "0" + hex;
+			}
+
 			return hex;
 		}
 
-		public static string string_binary(object num)
+		[LuaMethodAttributes(
+			"binary",
+			"TODO"
+		)]
+		public static string Binary(object num)
 		{
-			string binary = Convert.ToString( LuaLong(num), 2);
+			var binary = Convert.ToString(LuaLong(num), 2);
 			binary = binary.TrimStart('0');
 			return binary;
 		}
 
-		public static string string_octal(object num)
+		[LuaMethodAttributes(
+			"octal",
+			"TODO"
+		)]
+		public static string Octal(object num)
 		{
-			string octal = Convert.ToString(LuaLong(num),8);
-			if (octal.Length == 1) octal = "0" + octal;
+			var octal = Convert.ToString(LuaLong(num), 8);
+			if (octal.Length == 1)
+			{
+				octal = "0" + octal;
+			}
+
 			return octal;
 		}
 
-		public static string string_trim(string str)
+		[LuaMethodAttributes(
+			"trim",
+			"TODO"
+		)]
+		public static string Trim(string str)
 		{
 			return str.Trim();
 		}
 
-		public static string string_replace(string str, string str2, string replace)
+		[LuaMethodAttributes(
+			"replace",
+			"TODO"
+		)]
+		public static string Replace(string str, string str2, string replace)
 		{
-			return str.Replace(str2,replace);
+			return str.Replace(str2, replace);
 		}
 
-		public static string string_toupper(string str)
+		[LuaMethodAttributes(
+			"toupper",
+			"TODO"
+		)]
+		public static string ToUpper(string str)
 		{
 			return str.ToUpper();
 		}
 
-		public static string string_tolower(string str)
+		[LuaMethodAttributes(
+			"tolower",
+			"TODO"
+		)]
+		public static string ToLower(string str)
 		{
 			return str.ToLower();
 		}
 
-		public static string string_substring(string str, object position, object length)
+		[LuaMethodAttributes(
+			"substring",
+			"TODO"
+		)]
+		public static string SubString(string str, object position, object length)
 		{
-			return str.Substring((int) position,(int) length);
+			return str.Substring((int)position, (int)length);
 		}
 
-		public static string string_remove(string str, object position, object count)
+		[LuaMethodAttributes(
+			"remove",
+			"TODO"
+		)]
+		public static string Remove(string str, object position, object count)
 		{
-			return str.Remove((int) position,(int) (count));
+			return str.Remove((int)position, (int)count);
 		}
 
-		public static bool string_contains(string str, string str2)
+		[LuaMethodAttributes(
+			"contains",
+			"TODO"
+		)]
+		public static bool Contains(string str, string str2)
 		{
 			return str.Contains(str2);
 		}
 
-		public static bool string_startswith(string str, string str2)
+		[LuaMethodAttributes(
+			"startswith",
+			"TODO"
+		)]
+		public static bool StartsWith(string str, string str2)
 		{
 			return str.StartsWith(str2);
 		}
 
-		public static bool string_endswith(string str, string str2)
+		[LuaMethodAttributes(
+			"endswith",
+			"TODO"
+		)]
+		public static bool EndsWith(string str, string str2)
 		{
 			return str.EndsWith(str2);
 		}
 	}
 }
-
