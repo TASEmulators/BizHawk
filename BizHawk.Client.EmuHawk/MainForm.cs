036dcd8f3274f1bc9244f347cdff05d4ba0bc434
goyuken <goyuken@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-02-07 02:28:07 +0000

    AV out: add option to pad (instead of stretch) with the video resize output

--- BizHawk.Client.EmuHawk/MainForm.cs
+++ BizHawk.Client.EmuHawk/MainForm.cs
@@ -967,6 +967,7 @@ namespace BizHawk.Client.EmuHawk
 		private long _soundRemainder; // audio timekeeping for video dumping
 		private int _avwriterResizew;
 		private int _avwriterResizeh;
+		private bool _avwriterpad;
 
 		private bool _exit;
 		private bool _runloopFrameProgress;
@@ -2477,7 +2478,7 @@ namespace BizHawk.Client.EmuHawk
 			}
 			else
 			{
-				aw = VideoWriterChooserForm.DoVideoWriterChoserDlg(video_writers, GlobalWin.MainForm, out _avwriterResizew, out _avwriterResizeh);
+				aw = VideoWriterChooserForm.DoVideoWriterChoserDlg(video_writers, GlobalWin.MainForm, out _avwriterResizew, out _avwriterResizeh, out _avwriterpad);
 			}
 
 			foreach (var w in video_writers)
@@ -2668,7 +2669,15 @@ namespace BizHawk.Client.EmuHawk
 						var bmpout = new Bitmap(_avwriterResizew, _avwriterResizeh, PixelFormat.Format32bppArgb);
 						using (var g = Graphics.FromImage(bmpout))
 						{
-							g.DrawImage(bmpin, new Rectangle(0, 0, bmpout.Width, bmpout.Height));
+							if (_avwriterpad)
+							{
+								g.Clear(Color.FromArgb(Global.Emulator.VideoProvider.BackgroundColor));
+								g.DrawImageUnscaled(bmpin, (bmpout.Width - bmpin.Width) / 2, (bmpout.Height - bmpin.Height) / 2);
+							}
+							else
+							{
+								g.DrawImage(bmpin, new Rectangle(0, 0, bmpout.Width, bmpout.Height));
+							}
 						}
 
 						bmpin.Dispose();
