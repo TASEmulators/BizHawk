ced66652b9dbf641f2d50c29ae8cbdbefdeb0c83
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-02-09 22:30:21 +0000

    Merge 6063 and 6069 to 1.6x branch

--- branches/1.6x/VersionInfo.cs
+++ branches/1.6x/VersionInfo.cs
@@ -1,7 +1,7 @@
 static class VersionInfo
 {
 	public const string MAINVERSION = "1.6.0";
-	public const string RELEASEDATE = "Beta built on January 20, 2014";
+	public const string RELEASEDATE = "Beta built on February 08, 2014";
 	public static bool INTERIM = true;
 
 	public static string GetEmuVersion()
