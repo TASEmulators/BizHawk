e5be4fb02121fc89b3ae4fe28bfc752a7a39cbb1
beirich <beirich@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-03-06 04:43:36 +0000

    SMS: Add "external cart ram" mapper for SG1000, fixes The Castle and Othello. Fortunately, both games are somewhat interesting and playable. SG1000 compat now 100% minus games requiring exotic peripherals

---
+++ trunk/BizHawk.Emulation.Cores/Consoles/Sega/SMS/MemoryMap.ExtRam.cs
@@ -0,0 +1,41 @@
+﻿namespace BizHawk.Emulation.Cores.Sega.MasterSystem
+{
+	public partial class SMS
+	{
+		byte[] ExtRam;
+		int ExtRamMask;
+
+		byte ReadMemoryExt(ushort address)
+		{
+			byte ret;
+
+			if (address < 0x8000)
+				ret = RomData[address];
+			else if (address < 0xC000)
+				ret = ExtRam[address & ExtRamMask];
+			else
+				ret = SystemRam[address & RamSizeMask];
+
+			CoreComm.MemoryCallbackSystem.CallRead(address);
+			return ret;
+		}
+
+		void WriteMemoryExt(ushort address, byte value)
+		{
+			if (address < 0xC000 && address >= 0x8000)
+				ExtRam[address & ExtRamMask] = value;
+			else if (address >= 0xC000)
+				SystemRam[address & RamSizeMask] = value;
+
+			CoreComm.MemoryCallbackSystem.CallWrite((uint)address);
+		}
+
+		void InitExt2kMapper(int size)
+		{
+			ExtRam = new byte[size];
+			ExtRamMask = size - 1;
+			Cpu.ReadMemory = ReadMemoryExt;
+			Cpu.WriteMemory = WriteMemoryExt;
+		}
+	}
+}
