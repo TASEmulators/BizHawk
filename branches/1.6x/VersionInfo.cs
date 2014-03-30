d2f28985dcc87b54a523a61f2a5f9ace27e3bdc8
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-03-30 12:01:42 +0000

    1.6x - update version info - this marks the end of 1.6.1 and the beginning of 1.6.2 development

--- branches/1.6x/VersionInfo.cs
+++ branches/1.6x/VersionInfo.cs
@@ -1,7 +1,7 @@
 static class VersionInfo
 {
-	public const string MAINVERSION = "1.6.1";
-	public const string RELEASEDATE = "unknown";
+	public const string MAINVERSION = "1.6.2";
+	public const string RELEASEDATE = "Interim";
 	public static bool INTERIM = true;
 
 	public static string GetEmuVersion()
