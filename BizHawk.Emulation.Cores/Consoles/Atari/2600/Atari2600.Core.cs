696affa5be72b2b26274f3fecf638af7ed4651f7
adelikat <adelikat@tasvideos.org>
2014-04-05 15:04:03 +0000

    Atari 2600 - a bit mroe reorg

--- BizHawk.Emulation.Cores/Consoles/Atari/2600/Atari2600.Core.cs
+++ BizHawk.Emulation.Cores/Consoles/Atari/2600/Atari2600.Core.cs
@@ -193,7 +193,8 @@ namespace BizHawk.Emulation.Cores.Atari.Atari2600
 					_mapper = new mSB();
 					break;
 
-				default: throw new InvalidOperationException("mapper not supported: " + this._game.GetOptionsDict()["m"]);
+				default:
+					throw new InvalidOperationException("mapper not supported: " + _game.GetOptionsDict()["m"]);
 			}
 
 			_mapper.Core = this;
@@ -313,31 +314,5 @@ namespace BizHawk.Emulation.Cores.Atari.Atari2600
 
 			return value;
 		}
-	}
-
-	public class MapperBase
-	{
-		public Atari2600 Core { get; set; }
-		
-		public virtual byte ReadMemory(ushort addr)
-		{
-			return Core.BaseReadMemory(addr);
-		}
-
-		public virtual byte PeekMemory(ushort addr)
-		{
-			return Core.BasePeekMemory(addr);
-		}
-
-		public virtual void WriteMemory(ushort addr, byte value)
-		{
-			Core.BaseWriteMemory(addr, value);
-		}
-
-		public virtual void SyncState(Serializer ser) { }
-
-		public virtual void Dispose() { }
-
-		public virtual void ClockCpu() { }
 	}
 }
---
+++ BizHawk.Emulation.Cores/Consoles/Atari/2600/Mappers/MapperBase.cs
@@ -0,0 +1,30 @@
+﻿using BizHawk.Common;
+
+namespace BizHawk.Emulation.Cores.Atari.Atari2600
+{
+	public class MapperBase
+	{
+		public Atari2600 Core { get; set; }
+
+		public virtual byte ReadMemory(ushort addr)
+		{
+			return Core.BaseReadMemory(addr);
+		}
+
+		public virtual byte PeekMemory(ushort addr)
+		{
+			return Core.BasePeekMemory(addr);
+		}
+
+		public virtual void WriteMemory(ushort addr, byte value)
+		{
+			Core.BaseWriteMemory(addr, value);
+		}
+
+		public virtual void SyncState(Serializer ser) { }
+
+		public virtual void Dispose() { }
+
+		public virtual void ClockCpu() { }
+	}
+}
