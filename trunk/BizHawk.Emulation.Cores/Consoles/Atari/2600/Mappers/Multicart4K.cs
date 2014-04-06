1b6a2a34dae8c721a120f59a3bbb435fadc8e490
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-04-06 23:25:59 +0000

    Atari 2600 - support multi-cart mappers, toggling games is done via Hard Reset

---
+++ trunk/BizHawk.Emulation.Cores/Consoles/Atari/2600/Mappers/Multicart4K.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using BizHawk.Common;
+
+namespace BizHawk.Emulation.Cores.Atari.Atari2600
+{
+	/*
+		Mapper used for multi-cart 4K games
+	*/
+	internal class Multicart4K : MapperBase
+	{
+		private int _gameTotal;
+		private int _currentGame;
+
+		public Multicart4K(int gametotal)
+		{
+			_gameTotal = gametotal;
+			_currentGame = 0;
+		}
+
+		public override void SyncState(Serializer ser)
+		{
+			ser.Sync("gameTotal", ref _gameTotal);
+			ser.Sync("currentGame", ref _currentGame);
+			base.SyncState(ser);
+		}
+
+		public override void HardReset()
+		{
+			IncrementGame();
+		}
+		 
+		private void IncrementGame()
+		{
+			_currentGame++;
+			if (_currentGame >= _gameTotal)
+			{
+				_currentGame = 0;
+			}
+		}
+
+		public override byte ReadMemory(ushort addr)
+		{
+			if (addr < 0x1000)
+			{
+				return base.ReadMemory(addr);
+			}
+
+			return Core.Rom[(addr & 0xFFF) + (_currentGame * 4096)];
+		}
+
+		public override byte PeekMemory(ushort addr)
+		{
+			return ReadMemory(addr);
+		}
+	}
+}
