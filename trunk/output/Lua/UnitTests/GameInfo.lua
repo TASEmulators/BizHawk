53141874ff355326d8db67c1cd127cbbf511a2c6
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-04-13 14:24:38 +0000

    meh, slight tweak to that commit

--- trunk/output/Lua/UnitTests/GameInfo.lua
+++ trunk/output/Lua/UnitTests/GameInfo.lua
@@ -2,9 +2,9 @@ console.log("Game Info library test")
 console.log("Rom Name: " .. gameinfo.getromname());
 console.log("Rom Hash: " .. gameinfo.getromhash());
 console.log("Display Type: " .. emu.getdisplaytype());
-console.log("In Database?: " .. tostring(gameinfo.getindatabase()));
+console.log("In Database?: " .. tostring(gameinfo.indatabase()));
 console.log("Rom Status: " .. gameinfo.getstatus());
-console.log("Is Status Bad?: " .. tostring(gameinfo.getisstatusbad()));
+console.log("Is Status Bad?: " .. tostring(gameinfo.isstatusbad()));
 console.log("Board Type: " .. gameinfo.getboardtype());
 console.log("Game Options: ")
 console.log(gameinfo.getoptions());
