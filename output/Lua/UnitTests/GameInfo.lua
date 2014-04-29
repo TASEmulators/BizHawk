bac7ba9a5dc5a8640552e667d015aa2ec95c08c2
adelikat <adelikat@tasvideos.org>
2014-04-29 21:03:54 +0000

    Remove new line in console.log, Add console.write and console.writeline (console.write is an alias of console.log), update some lua scripts that depended on newline in console.log, add a console library lua unit test

--- output/Lua/UnitTests/GameInfo.lua
+++ output/Lua/UnitTests/GameInfo.lua
@@ -1,10 +1,10 @@
-console.log("Game Info library test")
-console.log("Rom Name: " .. gameinfo.getromname());
-console.log("Rom Hash: " .. gameinfo.getromhash());
-console.log("Display Type: " .. emu.getdisplaytype());
-console.log("In Database?: " .. tostring(gameinfo.indatabase()));
-console.log("Rom Status: " .. gameinfo.getstatus());
-console.log("Is Status Bad?: " .. tostring(gameinfo.isstatusbad()));
-console.log("Board Type: " .. gameinfo.getboardtype());
-console.log("Game Options: ")
-console.log(gameinfo.getoptions());
+console.writeline("Game Info library test")
+console.writeline("Rom Name: " .. gameinfo.getromname());
+console.writeline("Rom Hash: " .. gameinfo.getromhash());
+console.writeline("Display Type: " .. emu.getdisplaytype());
+console.writeline("In Database?: " .. tostring(gameinfo.indatabase()));
+console.writeline("Rom Status: " .. gameinfo.getstatus());
+console.writeline("Is Status Bad?: " .. tostring(gameinfo.isstatusbad()));
+console.writeline("Board Type: " .. gameinfo.getboardtype());
+console.writeline("Game Options: ")
+console.writeline(gameinfo.getoptions());
--- output/Lua/UnitTests/Joypad_WithControllerNumbers.lua
+++ output/Lua/UnitTests/Joypad_WithControllerNumbers.lua
@@ -1,12 +1,12 @@
-console.log("Unit test for joypad.set using the controller parameter")
-console.log("Core Required: NES (or any multi-player core with U,D,L,R,select,start,A,B as buttons)")
-console.log("Correct behavior:")
-console.log("No Directional button shoudl be imparied in any way, should operate as if nothing was ever pressed")
-console.log("A should be off and user can not push buttons to change that")
-console.log("B should be on and the user can not push buttons to change that")
-console.log("Select should be on, but pressing it turns it off")
-console.log("Start should be unaffected")
-console.log("After frame 600, the console will say 'cleared', and now all buttons should be off and unaffected, as if the script was never run");
+console.writeline("Unit test for joypad.set using the controller parameter")
+console.writeline("Core Required: NES (or any multi-player core with U,D,L,R,select,start,A,B as buttons)")
+console.writeline("Correct behavior:")
+console.writeline("No Directional button shoudl be imparied in any way, should operate as if nothing was ever pressed")
+console.writeline("A should be off and user can not push buttons to change that")
+console.writeline("B should be on and the user can not push buttons to change that")
+console.writeline("Select should be on, but pressing it turns it off")
+console.writeline("Start should be unaffected")
+console.writeline("After frame 600, the console will say 'cleared', and now all buttons should be off and unaffected, as if the script was never run");
 
 buttons = { };
 
@@ -37,7 +37,7 @@ while true do
 		turnoff["Start"] = null;
 		
 		joypad.set(turnoff, 1);
-		console.log("cleared")
+		console.writeline("cleared")
 	end
 	
 	emu.frameadvance();
