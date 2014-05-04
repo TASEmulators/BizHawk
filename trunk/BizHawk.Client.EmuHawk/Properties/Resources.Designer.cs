a319d8e25f02cd4b335e9dbbc972c0a9e90b7e6f
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-05-04 13:57:58 +0000

    Rename Core Selection menu to Cores, add N64 Plugin settings menu item, hide N64 menu during NULL emulator since the plug in settings are always accessible now in the core menu

--- trunk/BizHawk.Client.EmuHawk/Properties/Resources.Designer.cs
+++ trunk/BizHawk.Client.EmuHawk/Properties/Resources.Designer.cs
@@ -733,6 +733,16 @@ namespace BizHawk.Client.EmuHawk.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap monitor {
+            get {
+                object obj = ResourceManager.GetObject("monitor", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap MoveDown {
             get {
                 object obj = ResourceManager.GetObject("MoveDown", resourceCulture);
