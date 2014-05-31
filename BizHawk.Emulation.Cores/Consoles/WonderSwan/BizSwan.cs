58efa286df4a82d28bf5640f2564a394a82bba1f
goyuken <goyuken@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-05-31 16:12:59 +0000

    move stuff

--- BizHawk.Emulation.Cores/Consoles/WonderSwan/BizSwan.cs
+++ BizHawk.Emulation.Cores/Consoles/WonderSwan/BizSwan.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Runtime.InteropServices;
-using BizHawk.Common;
+using BizHawk.Emulation.Common;
 
 namespace BizHawk.Emulation.Cores.WonderSwan
 {
