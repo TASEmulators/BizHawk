4fafb148e47843f5e0e1b1d143f168d2d707e7df
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-06-01 20:44:13 +0000

    VirtualPad support for Wonderswan

---
+++ trunk/BizHawk.Client.EmuHawk/tools/VirtualPads/VirtualPadWonderSawn.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace BizHawk.Client.EmuHawk
+{
+	partial class VirtualPadWonderSawn
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.PL = new System.Windows.Forms.CheckBox();
+			this.PD = new System.Windows.Forms.CheckBox();
+			this.PR = new System.Windows.Forms.CheckBox();
+			this.PU = new System.Windows.Forms.CheckBox();
+			this.PL2 = new System.Windows.Forms.CheckBox();
+			this.PD2 = new System.Windows.Forms.CheckBox();
+			this.PR2 = new System.Windows.Forms.CheckBox();
+			this.PU2 = new System.Windows.Forms.CheckBox();
+			this.BStart = new System.Windows.Forms.CheckBox();
+			this.BA = new System.Windows.Forms.CheckBox();
+			this.BB = new System.Windows.Forms.CheckBox();
+			this.BPower = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// PL
+			// 
+			this.PL.Appearance = System.Windows.Forms.Appearance.Button;
+			this.PL.AutoSize = true;
+			this.PL.Image = global::BizHawk.Client.EmuHawk.Properties.Resources.Back;
+			this.PL.Location = new System.Drawing.Point(1, 15);
+			this.PL.Name = "PL";
+			this.PL.Size = new System.Drawing.Size(22, 22);
+			this.PL.TabIndex = 11;
+			this.PL.UseVisualStyleBackColor = true;
+			this.PL.CheckedChanged += new System.EventHandler(this.Buttons_CheckedChanged);
+			// 
+			// PD
+			// 
+			this.PD.Appearance = System.Windows.Forms.Appearance.Button;
+			this.PD.AutoSize = true;
+			this.PD.Image = global::BizHawk.Client.EmuHawk.Properties.Resources.BlueDown;
+			this.PD.Location = new System.Drawing.Point(22, 24);
+			this.PD.Name = "PD";
+			this.PD.Size = new System.Drawing.Size(22, 22);
+			this.PD.TabIndex = 10;
+			this.PD.UseVisualStyleBackColor = true;
+			this.PD.CheckedChanged += new System.EventHandler(this.Buttons_CheckedChanged);
+			// 
+			// PR
+			// 
+			this.PR.Appearance = System.Windows.Forms.Appearance.Button;
+			this.PR.AutoSize = true;
+			this.PR.Image = global::BizHawk.Client.EmuHawk.Properties.Resources.Forward;
+			this.PR.Location = new System.Drawing.Point(43, 15);
+			this.PR.Name = "PR";
+			this.PR.Size = new System.Drawing.Size(22, 22);
+			this.PR.TabIndex = 9;
+			this.PR.UseVisualStyleBackColor = true;
+			this.PR.CheckedChanged += new System.EventHandler(this.Buttons_CheckedChanged);
+			// 
+			// PU
+			// 
+			this.PU.Appearance = System.Windows.Forms.Appearance.Button;
+			this.PU.AutoSize = true;
+			this.PU.Image = global::BizHawk.Client.EmuHawk.Properties.Resources.BlueUp;
+			this.PU.Location = new System.Drawing.Point(22, 3);
+			this.PU.Name = "PU";
+			this.PU.Size = new System.Drawing.Size(22, 22);
+			this.PU.TabIndex = 8;
+			this.PU.UseVisualStyleBackColor = true;
+			this.PU.CheckedChanged += new System.EventHandler(this.Buttons_CheckedChanged);
+			// 
+			// PL2
+			// 
+			this.PL2.Appearance = System.Windows.Forms.Appearance.Button;
+			this.PL2.AutoSize = true;
+			this.PL2.Image = global::BizHawk.Client.EmuHawk.Properties.Resources.Back;
+			this.PL2.Location = new System.Drawing.Point(1, 79);
+			this.PL2.Name = "PL2";
+			this.PL2.Size = new System.Drawing.Size(22, 22);
+			this.PL2.TabIndex = 15;
+			this.PL2.UseVisualStyleBackColor = true;
+			this.PL2.CheckedChanged += new System.EventHandler(this.Buttons_CheckedChanged);
+			// 
+			// PD2
+			// 
+			this.PD2.Appearance = System.Windows.Forms.Appearance.Button;
+			this.PD2.AutoSize = true;
+			this.PD2.Image = global::BizHawk.Client.EmuHawk.Properties.Resources.BlueDown;
+			this.PD2.Location = new System.Drawing.Point(22, 88);
+			this.PD2.Name = "PD2";
+			this.PD2.Size = new System.Drawing.Size(22, 22);
+			this.PD2.TabIndex = 14;
+			this.PD2.UseVisualStyleBackColor = true;
+			this.PD2.CheckedChanged += new System.EventHandler(this.Buttons_CheckedChanged);
+			// 
+			// PR2
+			// 
+			this.PR2.Appearance = System.Windows.Forms.Appearance.Button;
+			this.PR2.AutoSize = true;
+			this.PR2.Image = global::BizHawk.Client.EmuHawk.Properties.Resources.Forward;
+			this.PR2.Location = new System.Drawing.Point(43, 79);
+			this.PR2.Name = "PR2";
+			this.PR2.Size = new System.Drawing.Size(22, 22);
+			this.PR2.TabIndex = 13;
+			this.PR2.UseVisualStyleBackColor = true;
+			this.PR2.CheckedChanged += new System.EventHandler(this.Buttons_CheckedChanged);
+			// 
+			// PU2
+			// 
+			this.PU2.Appearance = System.Windows.Forms.Appearance.Button;
+			this.PU2.AutoSize = true;
+			this.PU2.Image = global::BizHawk.Client.EmuHawk.Properties.Resources.BlueUp;
+			this.PU2.Location = new System.Drawing.Point(22, 67);
+			this.PU2.Name = "PU2";
+			this.PU2.Size = new System.Drawing.Size(22, 22);
+			this.PU2.TabIndex = 12;
+			this.PU2.UseVisualStyleBackColor = true;
+			this.PU2.CheckedChanged += new System.EventHandler(this.Buttons_CheckedChanged);
+			// 
+			// BStart
+			// 
+			this.BStart.Appearance = System.Windows.Forms.Appearance.Button;
+			this.BStart.AutoSize = true;
+			this.BStart.Location = new System.Drawing.Point(79, 78);
+			this.BStart.Name = "BStart";
+			this.BStart.Size = new System.Drawing.Size(24, 23);
+			this.BStart.TabIndex = 21;
+			this.BStart.Text = "S";
+			this.BStart.UseVisualStyleBackColor = true;
+			this.BStart.CheckedChanged += new System.EventHandler(this.Buttons_CheckedChanged);
+			// 
+			// BA
+			// 
+			this.BA.Appearance = System.Windows.Forms.Appearance.Button;
+			this.BA.AutoSize = true;
+			this.BA.Location = new System.Drawing.Point(145, 30);
+			this.BA.Name = "BA";
+			this.BA.Size = new System.Drawing.Size(24, 23);
+			this.BA.TabIndex = 23;
+			this.BA.Text = "A";
+			this.BA.UseVisualStyleBackColor = true;
+			this.BA.CheckedChanged += new System.EventHandler(this.Buttons_CheckedChanged);
+			// 
+			// BB
+			// 
+			this.BB.Appearance = System.Windows.Forms.Appearance.Button;
+			this.BB.AutoSize = true;
+			this.BB.Location = new System.Drawing.Point(122, 35);
+			this.BB.Name = "BB";
+			this.BB.Size = new System.Drawing.Size(24, 23);
+			this.BB.TabIndex = 22;
+			this.BB.Text = "B";
+			this.BB.UseVisualStyleBackColor = true;
+			this.BB.CheckedChanged += new System.EventHandler(this.Buttons_CheckedChanged);
+			// 
+			// BPower
+			// 
+			this.BPower.Appearance = System.Windows.Forms.Appearance.Button;
+			this.BPower.AutoSize = true;
+			this.BPower.ForeColor = System.Drawing.Color.Red;
+			this.BPower.Location = new System.Drawing.Point(108, 78);
+			this.BPower.Name = "BPower";
+			this.BPower.Size = new System.Drawing.Size(47, 23);
+			this.BPower.TabIndex = 24;
+			this.BPower.Text = "Power";
+			this.BPower.UseVisualStyleBackColor = true;
+			this.BPower.CheckedChanged += new System.EventHandler(this.Buttons_CheckedChanged);
+			// 
+			// VirtualPadWonderSawn
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.BPower);
+			this.Controls.Add(this.BA);
+			this.Controls.Add(this.BB);
+			this.Controls.Add(this.BStart);
+			this.Controls.Add(this.PL2);
+			this.Controls.Add(this.PD2);
+			this.Controls.Add(this.PR2);
+			this.Controls.Add(this.PU2);
+			this.Controls.Add(this.PL);
+			this.Controls.Add(this.PD);
+			this.Controls.Add(this.PR);
+			this.Controls.Add(this.PU);
+			this.Name = "VirtualPadWonderSawn";
+			this.Size = new System.Drawing.Size(179, 119);
+			this.Load += new System.EventHandler(this.VirtualPadWonderSawn_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.CheckBox PL;
+		private System.Windows.Forms.CheckBox PD;
+		private System.Windows.Forms.CheckBox PR;
+		private System.Windows.Forms.CheckBox PU;
+		private System.Windows.Forms.CheckBox PL2;
+		private System.Windows.Forms.CheckBox PD2;
+		private System.Windows.Forms.CheckBox PR2;
+		private System.Windows.Forms.CheckBox PU2;
+		private System.Windows.Forms.CheckBox BStart;
+		private System.Windows.Forms.CheckBox BA;
+		private System.Windows.Forms.CheckBox BB;
+		private System.Windows.Forms.CheckBox BPower;
+	}
+}
