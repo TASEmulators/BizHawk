fac7c03a4223bba6598dd83e1f5d9d58c1fae77f
adelikat <adelikat@tasvideos.org>
2014-06-01 02:06:23 +0000

    Add Wonderswan to open ROM dialog extensions

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -4,7 +4,7 @@ static class VersionInfo
 {
 	public const string MAINVERSION = "1.7.0";
 	public static string RELEASEDATE = "Unoffical BETA of unknown origin!";
-	public static bool INTERIM = false;
+	public static bool INTERIM = true;
 
 	public static string GetEmuVersion()
 	{
