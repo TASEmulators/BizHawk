ba4e7d620bdb73c8c102f82b61ca616a6fe05ba5
adelikat <adelikat@tasvideos.org>
2014-06-03 00:34:41 +0000

    Refactor the LuaDocumentation class to remove a bunch of unnecessary stuff

--- BizHawk.Client.Common/lua/LuaDocumentation.cs
+++ BizHawk.Client.Common/lua/LuaDocumentation.cs
@@ -5,132 +5,96 @@ using System.Text;
 
 namespace BizHawk.Client.Common
 {
-	public interface ILuaDocumentation
-	{
-		void Add(string methodLib, string methodName, MethodInfo method, string description);
-	}
-
-	public class LuaDocumentation : ILuaDocumentation
+	public class LuaDocumentation : List<LibraryFunction>
 	{
 		public LuaDocumentation()
+			:base() { }
+	}
+
+	public class LibraryFunction
+	{
+		private readonly string _returnType = string.Empty;
+
+		public LibraryFunction(string methodLib, string methodName, MethodInfo method, string description)
 		{
-			FunctionList = new List<LibraryFunction>();
-		}
+			Library = methodLib;
+			Name = methodName;
+			var info = method.GetParameters();
 
-		public List<LibraryFunction> FunctionList { get; set; }
-
-		public void Add(string methodLib, string methodName, MethodInfo method, string description)
-		{
-			FunctionList.Add(
-				new LibraryFunction(methodLib, methodName, method, description));
-		}
-
-		public void Clear()
-		{
-			FunctionList = new List<LibraryFunction>();
-		}
-
-		public void Sort()
-		{
-			FunctionList = FunctionList.OrderBy(x => x.Library).ThenBy(x => x.Name).ToList();
-		}
-
-		public IEnumerable<string> GetLibraryList()
-		{
-			return FunctionList.Select(x => x.Library);
-		}
-
-		public IEnumerable<LibraryFunction> GetFunctionsByLibrary(string library)
-		{
-			return FunctionList
-				.Where(func => func.Library == library);
-		}
-
-		public class LibraryFunction
-		{
-			private readonly string _returnType = string.Empty;
-
-			public LibraryFunction(string methodLib, string methodName, MethodInfo method, string description)
+			Parameters = new List<string>();
+			foreach (var p in info)
 			{
-				Library = methodLib;
-				Name = methodName;
-				var info = method.GetParameters();
-
-				Parameters = new List<string>();
-				foreach (var p in info)
-				{
-					Parameters.Add(p.ToString());
-				}
-
-				this._returnType = method.ReturnType.ToString();
-
-				Description = description;
+				Parameters.Add(p.ToString());
 			}
 
-			public string Library { get; set; }
-			public string Name { get; set; }
-			public List<string> Parameters { get; set; }
+			_returnType = method.ReturnType.ToString();
 
-			public string Description { get; set; }
+			Description = description;
+		}
 
-			public string ParameterList
+		public string Library { get; set; }
+		public string Name { get; set; }
+		public List<string> Parameters { get; set; }
+
+		public string Description { get; set; }
+
+		public string ParameterList
+		{
+			get
 			{
-				get
+				var list = new StringBuilder();
+				list.Append('(');
+				for (var i = 0; i < Parameters.Count; i++)
 				{
-					var list = new StringBuilder();
-					list.Append('(');
-					for (var i = 0; i < Parameters.Count; i++)
+					var param =
+						Parameters[i].Replace("System", string.Empty)
+									 .Replace(" ", string.Empty)
+									 .Replace(".", string.Empty)
+									 .Replace("LuaInterface", string.Empty)
+									 .Replace("Object[]", "object[] ")
+									 .Replace("Object", "object ")
+									 .Replace("Boolean[]", "bool[] ")
+									 .Replace("Boolean", "bool ")
+									 .Replace("String", "string ")
+									 .Replace("LuaTable", "table ")
+									 .Replace("LuaFunction", "func ")
+									 .Replace("Nullable`1[Int32]", "int? ")
+									 .Replace("Nullable`1[UInt32]", "uint? ")
+									 .Replace("Byte", "byte ")
+									 .Replace("Int16", "short ")
+									 .Replace("Int32", "int ")
+									 .Replace("Int64", "long ")
+									 .Replace("Ushort", "ushort ")
+									 .Replace("Ulong", "ulong ")
+									 .Replace("UInt32", "uint ")
+									 .Replace("UInt64", "ulong ")
+									 .Replace("Double", "double ")
+									 .Replace("Uint", "uint ")
+									 .Replace("Nullable`1[DrawingColor]", "Color? ")
+									 .Replace("DrawingColor", "Color ");
+
+					list.Append(param);
+					if (i < Parameters.Count - 1)
 					{
-						var param =
-							Parameters[i].Replace("System", string.Empty)
-										 .Replace(" ", string.Empty)
-										 .Replace(".", string.Empty)
-										 .Replace("LuaInterface", string.Empty)
-										 .Replace("Object[]", "object[] ")
-										 .Replace("Object", "object ")
-										 .Replace("Boolean[]", "bool[] ")
-										 .Replace("Boolean", "bool ")
-										 .Replace("String", "string ")
-										 .Replace("LuaTable", "table ")
-										 .Replace("LuaFunction", "func ")
-										 .Replace("Nullable`1[Int32]", "int? ")
-										 .Replace("Nullable`1[UInt32]", "uint? ")
-										 .Replace("Byte", "byte ")
-										 .Replace("Int16", "short ")
-										 .Replace("Int32", "int ")
-										 .Replace("Int64", "long ")
-										 .Replace("Ushort", "ushort ")
-										 .Replace("Ulong", "ulong ")
-										 .Replace("UInt32", "uint ")
-										 .Replace("UInt64", "ulong ")
-										 .Replace("Double", "double ")
-										 .Replace("Uint", "uint ")
-										 .Replace("Nullable`1[DrawingColor]", "Color? ")
-										 .Replace("DrawingColor", "Color ");
-
-						list.Append(param);
-						if (i < Parameters.Count - 1)
-						{
-							list.Append(", ");
-						}
+						list.Append(", ");
 					}
-
-					list.Append(')');
-
-					return list.ToString();
 				}
+
+				list.Append(')');
+
+				return list.ToString();
 			}
+		}
 
-			public string ReturnType
+		public string ReturnType
+		{
+			get
 			{
-				get
-				{
-					return _returnType
-						.Replace("System.", string.Empty)
-						.Replace("LuaInterface.", string.Empty)
-						.ToLower()
-						.Trim();
-				}
+				return _returnType
+					.Replace("System.", string.Empty)
+					.Replace("LuaInterface.", string.Empty)
+					.ToLower()
+					.Trim();
 			}
 		}
 	}
