21f570ddf790c87ff42ad44ae35160a55cf879e7
xmikecampx7@gmail.com <xmikecampx7@gmail.com@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-06-04 20:15:33 +0000

    More work on Profile Config dialog.

--- trunk/BizHawk.Client.EmuHawk/config/ProfileConfig.Designer.cs
+++ trunk/BizHawk.Client.EmuHawk/config/ProfileConfig.Designer.cs
@@ -30,12 +30,18 @@
 		{
 			this.OkBtn = new System.Windows.Forms.Button();
 			this.CancelBtn = new System.Windows.Forms.Button();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.checkBox2 = new System.Windows.Forms.CheckBox();
+			this.checkBox3 = new System.Windows.Forms.CheckBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
 			this.SuspendLayout();
 			// 
 			// OkBtn
 			// 
 			this.OkBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.OkBtn.Location = new System.Drawing.Point(146, 227);
+			this.OkBtn.Location = new System.Drawing.Point(247, 337);
 			this.OkBtn.Name = "OkBtn";
 			this.OkBtn.Size = new System.Drawing.Size(60, 23);
 			this.OkBtn.TabIndex = 0;
@@ -47,7 +53,7 @@
 			// 
 			this.CancelBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.CancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.CancelBtn.Location = new System.Drawing.Point(212, 227);
+			this.CancelBtn.Location = new System.Drawing.Point(313, 337);
 			this.CancelBtn.Name = "CancelBtn";
 			this.CancelBtn.Size = new System.Drawing.Size(60, 23);
 			this.CancelBtn.TabIndex = 1;
@@ -55,21 +61,93 @@
 			this.CancelBtn.UseVisualStyleBackColor = true;
 			this.CancelBtn.Click += new System.EventHandler(this.CancelBtn_Click);
 			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "Casual Gaming",
+            "Tool-assisted Speedruns",
+            "Longplays",
+            "Custom.."});
+			this.comboBox1.Location = new System.Drawing.Point(12, 12);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(140, 21);
+			this.comboBox1.TabIndex = 2;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(12, 67);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(166, 17);
+			this.checkBox1.TabIndex = 4;
+			this.checkBox1.Text = "Save Screenshot With States";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			this.checkBox1.MouseHover += new System.EventHandler(this.checkBox1_MouseHover);
+			// 
+			// checkBox2
+			// 
+			this.checkBox2.AutoSize = true;
+			this.checkBox2.Location = new System.Drawing.Point(12, 90);
+			this.checkBox2.Name = "checkBox2";
+			this.checkBox2.Size = new System.Drawing.Size(196, 17);
+			this.checkBox2.TabIndex = 5;
+			this.checkBox2.Text = "Save Large Screenshot With States";
+			this.checkBox2.UseVisualStyleBackColor = true;
+			this.checkBox2.MouseHover += new System.EventHandler(this.checkBox2_MouseHover);
+			// 
+			// checkBox3
+			// 
+			this.checkBox3.AutoSize = true;
+			this.checkBox3.Location = new System.Drawing.Point(12, 113);
+			this.checkBox3.Name = "checkBox3";
+			this.checkBox3.Size = new System.Drawing.Size(111, 17);
+			this.checkBox3.TabIndex = 6;
+			this.checkBox3.Text = "Allow U+D or L+R";
+			this.checkBox3.UseVisualStyleBackColor = true;
+			this.checkBox3.MouseHover += new System.EventHandler(this.checkBox3_MouseHover);
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Location = new System.Drawing.Point(4, 47);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(207, 91);
+			this.groupBox1.TabIndex = 7;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "General Options";
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(217, 47);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.ReadOnly = true;
+			this.richTextBox1.Size = new System.Drawing.Size(165, 91);
+			this.richTextBox1.TabIndex = 8;
+			this.richTextBox1.Text = "";
+			// 
 			// ProfileConfig
 			// 
 			this.AcceptButton = this.OkBtn;
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.CancelButton = this.CancelBtn;
-			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.ClientSize = new System.Drawing.Size(385, 372);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.checkBox3);
+			this.Controls.Add(this.checkBox2);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.comboBox1);
 			this.Controls.Add(this.CancelBtn);
 			this.Controls.Add(this.OkBtn);
+			this.Controls.Add(this.groupBox1);
 			this.Name = "ProfileConfig";
 			this.ShowIcon = false;
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
 			this.Text = "Profile Config";
 			this.Load += new System.EventHandler(this.ProfileConfig_Load);
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -77,5 +155,11 @@
 
 		private System.Windows.Forms.Button OkBtn;
 		private System.Windows.Forms.Button CancelBtn;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.CheckBox checkBox2;
+		private System.Windows.Forms.CheckBox checkBox3;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
 	}
 }
