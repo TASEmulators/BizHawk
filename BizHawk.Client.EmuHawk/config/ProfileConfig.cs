3bb5637120eecba70f679bbddb0b5f73f2c619ec
xmikecampx7 <xmikecampx7@gmail.com>
2014-06-04 20:15:33 +0000

    More work on Profile Config dialog.

--- BizHawk.Client.EmuHawk/config/ProfileConfig.cs
+++ BizHawk.Client.EmuHawk/config/ProfileConfig.cs
@@ -34,5 +34,40 @@ namespace BizHawk.Client.EmuHawk
 			DialogResult = DialogResult.Cancel;
 			Close();
 		}
+
+		private void checkBox1_MouseHover(object sender, EventArgs e)
+		{
+			richTextBox1.Text = "Save Screenshot with Savestates: \r\n * Required for TASing \r\n * Not Recommended for \r\n   Longplays or Casual Gaming";
+		}
+		private void checkBox2_MouseHover(object sender, EventArgs e)
+		{
+			richTextBox1.Text = "Save Large Screenshot With States: \r\n * Required for TASing \r\n * Not Recommended for \r\n   Longplays or Casual Gaming";
+		}
+		private void checkBox3_MouseHover(object sender, EventArgs e)
+		{
+			richTextBox1.Text = "All Up+Down or Left+Right: \r\n * Useful for TASing \r\n * Unchecked for Casual Gaming \r\n * Unknown for longplays";
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (comboBox1.SelectedIndex == 0) //Casual Gaming
+			{
+				checkBox1.Checked = false;
+				checkBox2.Checked = false;
+				checkBox3.Checked = false;
+			}
+			else if (comboBox1.SelectedIndex == 1) //TAS
+			{
+				checkBox1.Checked = true;
+				checkBox2.Checked = true;
+				checkBox3.Checked = true;
+			}
+			else if (comboBox1.SelectedIndex == 2) //Long Plays
+			{
+				checkBox1.Checked = false;
+				checkBox2.Checked = false;
+				checkBox3.Checked = false;
+			}
+		}
 	}
 }
