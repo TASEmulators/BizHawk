5972cfad526c140b75c15ac68d47afbcbb100735
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-06-12 23:57:23 +0000

    Some Movie code reorg

---
+++ trunk/BizHawk.Client.Common/movie/bkm/Movie.InputLog.cs
@@ -0,0 +1,291 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace BizHawk.Client.Common
+{
+	public partial class Movie : IMovie
+	{
+		private readonly MovieLog _log = new MovieLog();
+
+		public string GetInputLog()
+		{
+			var sb = new StringBuilder();
+
+			sb.AppendLine("[Input]");
+
+			foreach (var record in _log)
+			{
+				sb.AppendLine(record);
+			}
+
+			sb.AppendLine("[/Input]");
+
+			return sb.ToString();
+		}
+
+		public bool ExtractInputLog(TextReader reader, out string errorMessage)
+		{
+			errorMessage = string.Empty;
+			int? stateFrame = null;
+
+			// We are in record mode so replace the movie log with the one from the savestate
+			if (!Global.MovieSession.MultiTrack.IsActive)
+			{
+				if (Global.Config.EnableBackupMovies && _makeBackup && _log.Length > 0)
+				{
+					SaveBackup();
+					_makeBackup = false;
+				}
+
+				_log.Clear();
+				while (true)
+				{
+					var line = reader.ReadLine();
+					if (line == null)
+					{
+						break;
+					}
+
+					if (line.Trim() == string.Empty || line == "[Input]")
+					{
+						continue;
+					}
+
+					if (line == "[/Input]")
+					{
+						break;
+					}
+
+					if (line.Contains("Frame 0x")) // NES stores frame count in hex, yay
+					{
+						var strs = line.Split('x');
+						try
+						{
+							stateFrame = int.Parse(strs[1], NumberStyles.HexNumber);
+						}
+						catch
+						{
+							errorMessage = "Savestate Frame number failed to parse";
+							return false;
+						}
+					}
+					else if (line.Contains("Frame "))
+					{
+						var strs = line.Split(' ');
+						try
+						{
+							stateFrame = int.Parse(strs[1]);
+						}
+						catch
+						{
+							errorMessage = "Savestate Frame number failed to parse";
+							return false;
+						}
+					}
+					else if (line[0] == '|')
+					{
+						_log.AppendFrame(line);
+					}
+				}
+			}
+			else
+			{
+				var i = 0;
+				while (true)
+				{
+					var line = reader.ReadLine();
+					if (line == null)
+					{
+						break;
+					}
+
+					if (line.Trim() == string.Empty || line == "[Input]")
+					{
+						continue;
+					}
+
+					if (line == "[/Input]")
+					{
+						break;
+					}
+
+					if (line.Contains("Frame 0x")) // NES stores frame count in hex, yay
+					{
+						var strs = line.Split('x');
+						try
+						{
+							stateFrame = int.Parse(strs[1], NumberStyles.HexNumber);
+						}
+						catch
+						{
+							errorMessage = "Savestate Frame number failed to parse";
+							return false;
+						}
+					}
+					else if (line.Contains("Frame "))
+					{
+						var strs = line.Split(' ');
+						try
+						{
+							stateFrame = int.Parse(strs[1]);
+						}
+						catch
+						{
+							errorMessage = "Savestate Frame number failed to parse";
+							return false;
+						}
+					}
+					else if (line.StartsWith("|"))
+					{
+						_log.SetFrameAt(i, line);
+						i++;
+					}
+				}
+			}
+
+			if (!stateFrame.HasValue)
+			{
+				errorMessage = "Savestate Frame number failed to parse";
+			}
+
+			var stateFramei = stateFrame ?? 0;
+
+			if (stateFramei > 0 && stateFramei < _log.Length)
+			{
+				if (!Global.Config.VBAStyleMovieLoadState)
+				{
+					_log.TruncateStates(stateFramei);
+					_log.TruncateMovie(stateFramei);
+				}
+			}
+			else if (stateFramei > _log.Length) // Post movie savestate
+			{
+				if (!Global.Config.VBAStyleMovieLoadState)
+				{
+					_log.TruncateStates(_log.Length);
+					_log.TruncateMovie(_log.Length);
+				}
+
+				_mode = Moviemode.Finished;
+			}
+
+			if (IsCountingRerecords)
+			{
+				Rerecords++;
+			}
+
+			return true;
+		}
+
+		public bool CheckTimeLines(TextReader reader, out string errorMessage)
+		{
+			// This function will compare the movie data to the savestate movie data to see if they match
+			errorMessage = string.Empty;
+			var log = new MovieLog();
+			var stateFrame = 0;
+			while (true)
+			{
+				var line = reader.ReadLine();
+				if (line == null)
+				{
+					return false;
+				}
+
+				if (line.Trim() == string.Empty)
+				{
+					continue;
+				}
+
+				if (line.Contains("Frame 0x")) // NES stores frame count in hex, yay
+				{
+					var strs = line.Split('x');
+					try
+					{
+						stateFrame = int.Parse(strs[1], NumberStyles.HexNumber);
+					}
+					catch
+					{
+						errorMessage = "Savestate Frame number failed to parse";
+						return false;
+					}
+				}
+				else if (line.Contains("Frame "))
+				{
+					var strs = line.Split(' ');
+					try
+					{
+						stateFrame = int.Parse(strs[1]);
+					}
+					catch
+					{
+						errorMessage = "Savestate Frame number failed to parse";
+						return false;
+					}
+				}
+				else if (line == "[Input]")
+				{
+					continue;
+				}
+				else if (line == "[/Input]")
+				{
+					break;
+				}
+				else if (line[0] == '|')
+				{
+					log.AppendFrame(line);
+				}
+			}
+
+			if (stateFrame == 0)
+			{
+				stateFrame = log.Length;  // In case the frame count failed to parse, revert to using the entire state input log
+			}
+
+			if (_log.Length < stateFrame)
+			{
+				if (IsFinished)
+				{
+					return true;
+				}
+
+				errorMessage = "The savestate is from frame "
+					+ log.Length
+					+ " which is greater than the current movie length of "
+					+ _log.Length;
+
+				return false;
+			}
+
+			for (var i = 0; i < stateFrame; i++)
+			{
+				if (_log[i] != log[i])
+				{
+					errorMessage = "The savestate input does not match the movie input at frame "
+						+ (i + 1)
+						+ ".";
+
+					return false;
+				}
+			}
+
+			if (stateFrame > log.Length) // stateFrame is greater than state input log, so movie finished mode
+			{
+				if (_mode == Moviemode.Play || _mode == Moviemode.Finished)
+				{
+					_mode = Moviemode.Finished;
+					return true;
+				}
+
+				return false;
+			}
+
+			if (_mode == Moviemode.Finished)
+			{
+				_mode = Moviemode.Play;
+			}
+
+			return true;
+		}
+	}
+}
---
+++ trunk/BizHawk.Client.Common/movie/bkm/Movie.ModeApi.cs
@@ -0,0 +1,97 @@
+﻿namespace BizHawk.Client.Common
+{
+	public partial class Movie : IMovie
+	{
+		private enum Moviemode { Inactive, Play, Record, Finished }
+
+		private Moviemode _mode = Moviemode.Inactive;
+
+		public bool IsPlaying
+		{
+			get { return _mode == Moviemode.Play || _mode == Moviemode.Finished; }
+		}
+
+		public bool IsRecording
+		{
+			get { return _mode == Moviemode.Record; }
+		}
+
+		public bool IsActive
+		{
+			get { return _mode != Moviemode.Inactive; }
+		}
+
+		public bool IsFinished
+		{
+			get { return _mode == Moviemode.Finished; }
+		}
+
+		public void StartNewRecording()
+		{
+			// adelikat: ClearSaveRam shouldn't be here at all most likely, especially considering this is an implementation detail
+			// If Starting a new recording requires clearing sram it shoudl be done at a higher layer and not rely on all IMovies doing this
+			// Haven't removed it yet because I coudln't guarantee that power-on movies coudl live without it
+			// And the immediate fire is that Savestate movies are breaking
+			if (!StartsFromSavestate)
+			{
+				Global.Emulator.ClearSaveRam();
+			}
+
+			_mode = Moviemode.Record;
+			if (Global.Config.EnableBackupMovies && _makeBackup && _log.Length > 0)
+			{
+				SaveBackup();
+				_makeBackup = false;
+			}
+
+			_log.Clear();
+		}
+
+		public void StartNewPlayback()
+		{
+			// See StartNewRecording for details as to why this savestate check is here
+			if (!StartsFromSavestate)
+			{
+				Global.Emulator.ClearSaveRam();
+			}
+
+			_mode = Moviemode.Play;
+		}
+
+		public void SwitchToRecord()
+		{
+			_mode = Moviemode.Record;
+		}
+
+		public void SwitchToPlay()
+		{
+			_mode = Moviemode.Play;
+			Save();
+		}
+
+		public void Stop(bool saveChanges = true)
+		{
+			if (saveChanges)
+			{
+				if (_mode == Moviemode.Record || _changes)
+				{
+					Save();
+				}
+			}
+
+			_changes = false;
+			_mode = Moviemode.Inactive;
+		}
+
+		/// <summary>
+		/// If a movie is in playback mode, this will set it to movie finished
+		/// </summary>
+		private void Finish()
+		{
+			if (_mode == Moviemode.Play)
+			{
+				_mode = Moviemode.Finished;
+			}
+		}
+	}
+}
