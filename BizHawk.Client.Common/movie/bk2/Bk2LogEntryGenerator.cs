37d6e0b8de722eea2afb74eb71721967ff4f2a78
adelikat <adelikat@tasvideos.org>
2014-06-16 16:56:09 +0000

    Finish up Bk2 Mnemonic lookup

--- BizHawk.Client.Common/movie/bk2/Bk2LogEntryGenerator.cs
+++ BizHawk.Client.Common/movie/bk2/Bk2LogEntryGenerator.cs
@@ -9,6 +9,7 @@ namespace BizHawk.Client.Common
 {
 	public class Bk2LogEntryGenerator : ILogEntryGenerator
 	{
+		private readonly Bk2MnemonicConstants Mnemonics = new Bk2MnemonicConstants();
 		private IController _source;
 
 		public IMovieController MovieControllerAdapter
@@ -87,60 +88,6 @@ namespace BizHawk.Client.Common
 
 			sb.Append('|');
 			return sb.ToString();
-		}
-
-		private readonly Bk2MnemonicsLookup Mnemonics = new Bk2MnemonicsLookup();
-
-		public class Bk2MnemonicsLookup
-		{
-			public char this[string button]
-			{
-				get
-				{
-					if (SystemOverrides.ContainsKey(Global.Emulator.SystemId) && SystemOverrides[Global.Emulator.SystemId].ContainsKey(button))
-					{
-						return SystemOverrides[Global.Emulator.SystemId][button];
-					}
-					else if (BaseMnemonicLookupTable.ContainsKey(button))
-					{
-						return BaseMnemonicLookupTable[button];
-					}
-
-					return '!';
-				}
-			}
-
-			private readonly Dictionary<string, char> BaseMnemonicLookupTable = new Dictionary<string, char>
-			{
-				{ "P1 Up", 'U' },
-				{ "P1 Down", 'D' },
-				{ "P1 Left", 'L' },
-				{ "P1 Right", 'R' },
-				{ "P1 B", 'B' },
-				{ "P1 A", 'A' },
-				{ "P1 Select", 's' },
-				{ "P1 Start", 'S' },
-
-				{ "P2 Up", 'U' },
-				{ "P2 Down", 'D' },
-				{ "P2 Left", 'L' },
-				{ "P2 Right", 'R' },
-				{ "P2 B", 'B' },
-				{ "P2 A", 'A' },
-				{ "P2 Select", 's' },
-				{ "P2 Start", 'S' },
-			};
-
-			private readonly Dictionary<string, Dictionary<string, char>> SystemOverrides = new Dictionary<string, Dictionary<string, char>>
-			{
-				{
-					"NES",
-					new Dictionary<string, char>
-					{
-						{ "P1 Up", 'Q' }
-					}
-				}
-			};
 		}
 	}
 }
