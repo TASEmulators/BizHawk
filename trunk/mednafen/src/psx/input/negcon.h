91f7268bf260d174fb25c83eae91737fcdd0a779
zeromus@zeromus.org <zeromus@zeromus.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-06-20 04:23:22 +0000

    get rid of the giant old mednafen. add a new small one just for psx, modeled more after how wonderswan was organized.

--- trunk/mednafen/src/psx/input/negcon.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MDFN_PSX_INPUT_NEGCON_H
-#define __MDFN_PSX_INPUT_NEGCON_H
-
-namespace MDFN_IEN_PSX
-{
- InputDevice *Device_neGcon_Create(void);
- extern InputDeviceInputInfoStruct Device_neGcon_IDII[21];
-}
-#endif
