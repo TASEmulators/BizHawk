84657292af5e6c489297341870454cc050a5ed79
zeromus@zeromus.org <zeromus@zeromus.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-06-21 06:43:04 +0000

    if we're going to have an alt_about_image we might as well activate it sometime.

--- trunk/BizHawk.Client.EmuHawk/AboutBox.cs
+++ trunk/BizHawk.Client.EmuHawk/AboutBox.cs
@@ -164,10 +164,11 @@ namespace BizHawk.Client.EmuHawk
 #else
 			Text = "BizHawk Developer Build (RELEASE MODE) SVN r" + SubWCRev.SVN_REV;
 #endif
-			if (DateTime.Now > DateTime.Parse("12/17/2013") && DateTime.Now <= DateTime.Parse("12/25/2013"))
-			{
-				pictureBox1.Image = Properties.Resources.alt_about_image;
-			}
+			if (DateTime.Now.Month == 12)
+				if (DateTime.Now.Day > 17 && DateTime.Now.Day <= 25)
+				{
+					pictureBox1.Image = Properties.Resources.alt_about_image;
+				}
 		}
 
 		private void Close_Click(object sender, EventArgs e)
