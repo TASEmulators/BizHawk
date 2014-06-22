a248e2d99b4066d89ff90e08e30668645cae9e3e
adelikat <adelikat@tasvideos.org>
2014-06-22 17:41:13 +0000

    Virtual Pads - analog stick should be done now (including max X,Y constraints)

--- BizHawk.Client.EmuHawk/tools/VirtualPads/controls/AnalogSticklPanel.cs
+++ BizHawk.Client.EmuHawk/tools/VirtualPads/controls/AnalogSticklPanel.cs
@@ -5,19 +5,48 @@ using BizHawk.Client.Common;
 
 namespace BizHawk.Client.EmuHawk
 {
-	public sealed class AnalogStickPanel : Panel, IVirtualPadControl
+	public sealed class AnalogStickPanel : Panel
 	{
 		public int X = 0;
 		public int Y = 0;
 		public bool HasValue = false;
 
-		private readonly Brush _white_brush = Brushes.White;
-		private readonly Brush _black_brush = Brushes.Black;
-		private readonly Brush _gray_brush = Brushes.LightGray;
-		private readonly Brush _red_brush = Brushes.Red;
-		private readonly Brush _blue_brush = Brushes.DarkBlue;
-		private readonly Pen _black_pen;
-		private readonly Pen _blue_pen;
+		public string XName = string.Empty;
+		public string YName = string.Empty;
+
+		public int MaxX
+		{
+			get { return _maxX; }
+			set
+			{
+				_maxX = value;
+				CheckMax();
+			}
+		}
+
+		public int MaxY
+		{
+			get { return _maxY; }
+			set
+			{
+				_maxY = value;
+				CheckMax();
+			}
+		}
+
+		private int _maxX = 127;
+		private int _maxY = 127;
+
+		private int MinX { get { return 0 - MaxX; } }
+		private int MinY { get { return 0 - MaxY; } }
+
+		private readonly Brush _whiteBrush = Brushes.White;
+		private readonly Brush _blackBrush = Brushes.Black;
+		private readonly Brush _grayBrush = Brushes.LightGray;
+		private readonly Brush _redBrush = Brushes.Red;
+		private readonly Brush _blueBrush = Brushes.DarkBlue;
+		private readonly Pen _blackPen;
+		private readonly Pen _bluePen;
 
 		private readonly Bitmap dot = new Bitmap(7, 7);
 		private readonly Bitmap graydot = new Bitmap(7, 7);
@@ -32,19 +61,19 @@ namespace BizHawk.Client.EmuHawk
 			SetStyle(ControlStyles.Opaque, true);
 			BackColor = Color.Gray;
 			Paint += AnalogControlPanel_Paint;
-			new Pen(_white_brush);
-			_black_pen = new Pen(_black_brush);
-			new Pen(_gray_brush);
-			new Pen(_red_brush);
-			_blue_pen = new Pen(_blue_brush);
+			new Pen(_whiteBrush);
+			_blackPen = new Pen(_blackBrush);
+			new Pen(_grayBrush);
+			new Pen(_redBrush);
+			_bluePen = new Pen(_blueBrush);
 			BorderStyle = BorderStyle.Fixed3D;
 			
 			// Draw the dot into a bitmap
 			Graphics g = Graphics.FromImage(dot);
 			g.Clear(Color.Transparent);
-			g.FillRectangle(_red_brush, 2, 0, 3, 7);
-			g.FillRectangle(_red_brush, 1, 1, 5, 5);
-			g.FillRectangle(_red_brush, 0, 2, 7, 3);
+			g.FillRectangle(_redBrush, 2, 0, 3, 7);
+			g.FillRectangle(_redBrush, 1, 1, 5, 5);
+			g.FillRectangle(_redBrush, 0, 2, 7, 3);
 
 			Graphics gg = Graphics.FromImage(graydot);
 			gg.Clear(Color.Transparent);
@@ -55,20 +84,23 @@ namespace BizHawk.Client.EmuHawk
 
 		private int RealToGFX(int val)
 		{
-			return (val + 128)/2;
+			return (val + 128) / 2;
 		}
 
-		private int GFXToReal(int val)
+		private int GFXToReal(int val, bool isX) // isX is a hack
 		{
+			var max = isX ? MaxX : MaxY;
+			var min = isX ? MinX : MinY;
+
 			int ret = (val * 2);
-			if (ret > Max)
+			if (ret > max)
 			{
-				ret = Max;
+				ret = max;
 			}
 
-			if (ret < Min)
+			if (ret < min)
 			{
-				ret = Min;
+				ret = min;
 			}
 
 			return ret;
@@ -83,8 +115,8 @@ namespace BizHawk.Client.EmuHawk
 		{
 			int? xn = HasValue ? X : (int?)null;
 			int? yn = HasValue ? Y : (int?)null;
-			Global.StickyXORAdapter.SetFloat(Name, xn);
-			Global.StickyXORAdapter.SetFloat(Name.Replace("X", "Y"), yn); // Hack!
+			Global.StickyXORAdapter.SetFloat(XName, xn);
+			Global.StickyXORAdapter.SetFloat(YName, yn);
 
 			Refresh();
 		}
@@ -94,18 +126,19 @@ namespace BizHawk.Client.EmuHawk
 			unchecked
 			{
 				//Background
-				e.Graphics.FillRectangle(_gray_brush, 0, 0, 128, 128);
-				e.Graphics.FillEllipse(_white_brush, 0, 0, 127, 127);
-				e.Graphics.DrawEllipse(_black_pen, 0, 0, 127, 127);
-				e.Graphics.DrawLine(_black_pen, 64, 0, 64, 127);
-				e.Graphics.DrawLine(_black_pen, 0, 63, 127, 63);
+				e.Graphics.FillRectangle(_grayBrush, 0, 0, 128, 128);
+				e.Graphics.FillEllipse(_whiteBrush, 0, 0, 127, 127);
+				e.Graphics.DrawEllipse(_blackPen, 0, 0, 127, 127);
+				e.Graphics.DrawLine(_blackPen, 64, 0, 64, 127);
+				e.Graphics.DrawLine(_blackPen, 0, 63, 127, 63);
 
-				if (Global.MovieSession.Movie.IsPlaying && !Global.MovieSession.Movie.IsFinished)
+				if (Global.MovieSession != null && Global.MovieSession.Movie != null && // For the desinger
+					Global.MovieSession.Movie.IsPlaying && !Global.MovieSession.Movie.IsFinished)
 				{
 					var input = Global.MovieSession.Movie.GetInputState(Global.Emulator.Frame - 1);
 
-					var x = input.GetFloat(Name);
-					var y = input.GetFloat(Name.Replace("X", "Y")); // Hack!
+					var x = input.GetFloat(XName);
+					var y = input.GetFloat(YName);
 
 					var xx = RealToGFX((int)x);
 					var yy = RealToGFX((int)y);
@@ -117,7 +150,7 @@ namespace BizHawk.Client.EmuHawk
 				//Line
 				if (HasValue)
 				{
-					e.Graphics.DrawLine(_blue_pen, 64, 63, RealToGFX(X), 127 - RealToGFX(Y));
+					e.Graphics.DrawLine(_bluePen, 64, 63, RealToGFX(X), 127 - RealToGFX(Y));
 					e.Graphics.DrawImage(dot, RealToGFX(X) - 3, 127 - RealToGFX(Y) - 3);
 				}
 			}
@@ -127,8 +160,8 @@ namespace BizHawk.Client.EmuHawk
 		{
 			if (e.Button == MouseButtons.Left)
 			{
-				X = GFXToReal(e.X - 64);
-				Y = GFXToReal(-(e.Y - 63));
+				X = GFXToReal(e.X - 64, true);
+				Y = GFXToReal(-(e.Y - 63), false);
 				HasValue = true;
 				SetAnalog();
 			}
@@ -144,8 +177,7 @@ namespace BizHawk.Client.EmuHawk
 		protected override void OnMouseUp(MouseEventArgs e)
 		{
 			base.OnMouseUp(e);
-			if (Capture)
-				Capture = false;
+			Capture = false;
 		}
 
 
@@ -164,8 +196,8 @@ namespace BizHawk.Client.EmuHawk
 		{
 			if (e.Button == MouseButtons.Left)
 			{
-				X = GFXToReal(e.X - 64);
-				Y = GFXToReal(-(e.Y - 63));
+				X = GFXToReal(e.X - 64, true);
+				Y = GFXToReal(-(e.Y - 63), false);
 				HasValue = true;
 			}
 			if (e.Button == MouseButtons.Right)
@@ -193,7 +225,27 @@ namespace BizHawk.Client.EmuHawk
 			Refresh();
 		}
 
-		public static int Max = 127;
-		public static int Min = -127;
+		private void CheckMax()
+		{
+			if (X > MaxX)
+			{
+				X = MaxX;
+			}
+			else if (X < MinX)
+			{
+				X = MinX;
+			}
+
+			if (Y > MaxY)
+			{
+				Y = MaxY;
+			}
+			else if (Y < MinY)
+			{
+				Y = MinY;
+			}
+
+			Refresh();
+		}
 	}
 }
