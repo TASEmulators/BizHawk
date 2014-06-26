ab155cc7a2db620ac654c8fcd6764f940ef8d4bb
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-06-26 20:59:00 +0000

    Virtualpads - fix 3 vs 6 button controller logic in genesis

---
+++ trunk/BizHawk.Client.EmuHawk/tools/VirtualPads/schema/GenSchema.cs
@@ -0,0 +1,184 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+
+using BizHawk.Client.Common;
+
+namespace BizHawk.Client.EmuHawk
+{
+	[SchemaAttributes("GEN")]
+	public class GenSchema : IVirtualPadSchema
+	{
+		public IEnumerable<VirtualPad> GetPads()
+		{
+			yield return new VirtualPad(ThreeButtonController(1));
+
+			yield return new VirtualPad(SixButtonController(2));
+		}
+
+		public static PadSchema ThreeButtonController(int controller)
+		{
+			return new PadSchema
+			{
+				IsConsole = false,
+				DefaultSize = new Size(174, 90),
+				Buttons = new[]
+				{
+					new PadSchema.ButtonScema
+					{
+						Name = "P" + controller + " Up",
+						DisplayName = "",
+						Icon = Properties.Resources.BlueUp,
+						Location = new Point(14, 12),
+						Type = PadSchema.PadInputType.Boolean
+					},
+					new PadSchema.ButtonScema
+					{
+						Name = "P" + controller + " Down",
+						DisplayName = "",
+						Icon = Properties.Resources.BlueDown,
+						Location = new Point(14, 56),
+						Type = PadSchema.PadInputType.Boolean
+					},
+					new PadSchema.ButtonScema
+					{
+						Name = "P" + controller + " Left",
+						DisplayName = "",
+						Icon = Properties.Resources.Back,
+						Location = new Point(2, 34),
+						Type = PadSchema.PadInputType.Boolean
+					},
+					new PadSchema.ButtonScema
+					{
+						Name = "P" + controller + " Right",
+						DisplayName = "",
+						Icon = Properties.Resources.Forward,
+						Location = new Point(24, 34),
+						Type = PadSchema.PadInputType.Boolean
+					},
+					new PadSchema.ButtonScema
+					{
+						Name = "P" + controller + " A",
+						DisplayName = "A",
+						Location = new Point(98, 40),
+						Type = PadSchema.PadInputType.Boolean
+					},
+					new PadSchema.ButtonScema
+					{
+						Name = "P" + controller + " B",
+						DisplayName = "B",
+						Location = new Point(122, 40),
+						Type = PadSchema.PadInputType.Boolean
+					},
+					new PadSchema.ButtonScema
+					{
+						Name = "P" + controller + " C",
+						DisplayName = "C",
+						Location = new Point(146, 40),
+						Type = PadSchema.PadInputType.Boolean
+					},
+					new PadSchema.ButtonScema
+					{
+						Name = "P" + controller + " Start",
+						DisplayName = "S",
+						Location = new Point(122, 12),
+						Type = PadSchema.PadInputType.Boolean
+					}
+				}
+			};
+		}
+
+		public static PadSchema SixButtonController(int controller)
+		{
+			return new PadSchema
+			{
+				IsConsole = false,
+				DefaultSize = new Size(174, 90),
+				Buttons = new[]
+				{
+					new PadSchema.ButtonScema
+					{
+						Name = "P" + controller + " Up",
+						DisplayName = "",
+						Icon = Properties.Resources.BlueUp,
+						Location = new Point(14, 12),
+						Type = PadSchema.PadInputType.Boolean
+					},
+					new PadSchema.ButtonScema
+					{
+						Name = "P" + controller + " Down",
+						DisplayName = "",
+						Icon = Properties.Resources.BlueDown,
+						Location = new Point(14, 56),
+						Type = PadSchema.PadInputType.Boolean
+					},
+					new PadSchema.ButtonScema
+					{
+						Name = "P" + controller + " Left",
+						DisplayName = "",
+						Icon = Properties.Resources.Back,
+						Location = new Point(2, 34),
+						Type = PadSchema.PadInputType.Boolean
+					},
+					new PadSchema.ButtonScema
+					{
+						Name = "P" + controller + " Right",
+						DisplayName = "",
+						Icon = Properties.Resources.Forward,
+						Location = new Point(24, 34),
+						Type = PadSchema.PadInputType.Boolean
+					},
+					new PadSchema.ButtonScema
+					{
+						Name = "P" + controller + " A",
+						DisplayName = "A",
+						Location = new Point(98, 40),
+						Type = PadSchema.PadInputType.Boolean
+					},
+					new PadSchema.ButtonScema
+					{
+						Name = "P" + controller + " B",
+						DisplayName = "B",
+						Location = new Point(122, 40),
+						Type = PadSchema.PadInputType.Boolean
+					},
+					new PadSchema.ButtonScema
+					{
+						Name = "P" + controller + " C",
+						DisplayName = "C",
+						Location = new Point(146, 40),
+						Type = PadSchema.PadInputType.Boolean
+					},
+					new PadSchema.ButtonScema
+					{
+						Name = "P" + controller + " X",
+						DisplayName = "X",
+						Location = new Point(98, 65),
+						Type = PadSchema.PadInputType.Boolean
+					},
+					new PadSchema.ButtonScema
+					{
+						Name = "P" + controller + " Y",
+						DisplayName = "Y",
+						Location = new Point(122, 65),
+						Type = PadSchema.PadInputType.Boolean
+					},
+					new PadSchema.ButtonScema
+					{
+						Name = "P" + controller + " Z",
+						DisplayName = "Z",
+						Location = new Point(146, 65),
+						Type = PadSchema.PadInputType.Boolean
+					},
+					new PadSchema.ButtonScema
+					{
+						Name = "P" + controller + " Start",
+						DisplayName = "S",
+						Location = new Point(122, 12),
+						Type = PadSchema.PadInputType.Boolean
+					}
+				}
+			};
+		}
+	}
+}
