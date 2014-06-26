6319876c5c1998aa173ed7b4ab7c50d7f6fba603
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-06-26 19:19:26 +0000

    oops

---
+++ trunk/BizHawk.Client.Common/inputAdapters/InputAdapterExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using BizHawk.Emulation.Common;
+using BizHawk.Client.Common;
+
+namespace BizHawk.Client.Common.InputAdapterExtensions
+{
+	public static class InputAdapterExtensions
+	{
+		/// <summary>
+		/// Creates a new IController that is in a state of a bitwise And of the source and target controllers
+		/// </summary>
+		public static IController And(this IController source, IController target)
+		{
+			return new AndAdapter
+			{
+				Source = source,
+				SourceAnd = target
+			};
+		}
+
+		/// <summary>
+		/// Creates a new IController that is in a state of a bitwise Or of the source and target controllers
+		/// </summary>
+		public static IController Or(this IController source, IController target)
+		{
+			return new ORAdapter
+			{
+				Source = source,
+				SourceOr = target
+			};
+		}
+	}
+}
