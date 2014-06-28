cade02309db4d2561e50a13d4c698533e86d4965
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-06-28 14:19:46 +0000

    Virtualpads - atari 7800

---
+++ trunk/BizHawk.Client.EmuHawk/tools/VirtualPads/schema/A78Schema.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+using BizHawk.Client.Common;
+using BizHawk.Emulation.Cores.Atari.Atari7800;
+
+namespace BizHawk.Client.EmuHawk
+{
+	[SchemaAttributes("A78")]
+	public class A78Schema : IVirtualPadSchema
+	{
+		public IEnumerable<PadSchema> GetPadSchemas()
+		{
+			yield return StandardController(1);
+			yield return StandardController(2);
+			yield return ConsoleButtons();
+		}
+
+		private static PadSchema StandardController(int controller)
+		{
+			return new PadSchema
+			{
+				DisplayName = "Player " + controller,
+				IsConsole = false,
+				DefaultSize = new Size(174, 74),
+				MaxSize = new Size(174, 74),
+				Buttons = new[]
+				{
+					new PadSchema.ButtonScema
+					{
+						Name = "P" + controller + " Up",
+						DisplayName = "",
+						Icon = Properties.Resources.BlueUp,
+						Location = new Point(23, 15),
+						Type = PadSchema.PadInputType.Boolean
+					},
+					new PadSchema.ButtonScema
+					{
+						Name = "P" + controller + " Down",
+						DisplayName = "",
+						Icon = Properties.Resources.BlueDown,
+						Location = new Point(23, 36),
+						Type = PadSchema.PadInputType.Boolean
+					},
+					new PadSchema.ButtonScema
+					{
+						Name = "P" + controller + " Left",
+						DisplayName = "",
+						Icon = Properties.Resources.Back,
+						Location = new Point(2, 24),
+						Type = PadSchema.PadInputType.Boolean
+					},
+					new PadSchema.ButtonScema
+					{
+						Name = "P" + controller + " Right",
+						DisplayName = "",
+						Icon = Properties.Resources.Forward,
+						Location = new Point(44, 24),
+						Type = PadSchema.PadInputType.Boolean
+					},
+					new PadSchema.ButtonScema
+					{
+						Name = "P" + controller + " Trigger",
+						DisplayName = "1",
+						Location = new Point(120, 24),
+						Type = PadSchema.PadInputType.Boolean
+					},
+					new PadSchema.ButtonScema
+					{
+						Name = "P" + controller + " Trigger 2",
+						DisplayName = "2",
+						Location = new Point(145, 24),
+						Type = PadSchema.PadInputType.Boolean
+					}
+				}
+			};
+		}
+
+		private static PadSchema ConsoleButtons()
+		{
+			return new PadSchema
+			{
+				DisplayName = "Console",
+				IsConsole = true,
+				DefaultSize = new Size(215, 50),
+				Buttons = new[]
+				{
+					new PadSchema.ButtonScema
+					{
+						Name = "Select",
+						DisplayName = "Select",
+						Location = new Point(10, 15),
+						Type = PadSchema.PadInputType.Boolean
+					},
+					new PadSchema.ButtonScema
+					{
+						Name = "Reset",
+						DisplayName = "Reset",
+						Location = new Point(60, 15),
+						Type = PadSchema.PadInputType.Boolean
+					},
+					new PadSchema.ButtonScema
+					{
+						Name = "Power",
+						DisplayName = "Power",
+						Location = new Point(108, 15),
+						Type = PadSchema.PadInputType.Boolean
+					},
+					new PadSchema.ButtonScema
+					{
+						Name = "Pause",
+						DisplayName = "Pause",
+						Location = new Point(158, 15),
+						Type = PadSchema.PadInputType.Boolean
+					}
+				}
+			};
+		}
+	}
+}
