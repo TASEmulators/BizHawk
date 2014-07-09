f548c19c09980b35fd8a8c3ac4d29bc235967e24
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-07-09 15:04:16 +0000

    tastudio - some reorg

---
+++ trunk/BizHawk.Client.EmuHawk/tools/TAStudio/TAStudio.IControlMainForm.cs
@@ -0,0 +1,10 @@
+﻿namespace BizHawk.Client.EmuHawk
+{
+	public partial class TAStudio : IControlMainform
+	{
+		public void ToggleReadOnly()
+		{
+			GlobalWin.OSD.AddMessage("TAStudio does not allow manual readonly toggle");
+		}
+	}
+}
---
+++ trunk/BizHawk.Client.EmuHawk/tools/TAStudio/TAStudio.IToolForm.cs
@@ -0,0 +1,60 @@
+﻿using System.Windows.Forms;
+using BizHawk.Client.Common;
+
+namespace BizHawk.Client.EmuHawk
+{
+	public partial class TAStudio : IToolForm
+	{
+		public bool UpdateBefore { get { return false; } }
+
+		public void UpdateValues()
+		{
+			if (!IsHandleCreated || IsDisposed)
+			{
+				return;
+			}
+
+			TasView.ItemCount = _tas.InputLogLength;
+			if (_tas.IsRecording)
+			{
+				TasView.ensureVisible(_tas.InputLogLength - 1);
+			}
+			else
+			{
+				TasView.ensureVisible(Global.Emulator.Frame - 1);
+			}
+		}
+
+		public void Restart()
+		{
+			if (!IsHandleCreated || IsDisposed)
+			{
+				return;
+			}
+		}
+
+		public bool AskSave()
+		{
+			if (_tas.Changes)
+			{
+				GlobalWin.Sound.StopSound();
+				var result = MessageBox.Show("Save Changes?", "Tastudio", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question, MessageBoxDefaultButton.Button3);
+				GlobalWin.Sound.StartSound();
+				if (result == DialogResult.Yes)
+				{
+					SaveTasMenuItem_Click(null, null);
+				}
+				else if (result == DialogResult.No)
+				{
+					return true;
+				}
+				else if (result == DialogResult.Cancel)
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+	}
+}
