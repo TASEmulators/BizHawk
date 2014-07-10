655207d79a038921665ca01772fb4cee729f687b
adelikat <adelikat@tasvideos.org>
2014-07-10 01:58:43 +0000

    TAStudio - some reorg

---
+++ BizHawk.Client.EmuHawk/tools/TAStudio/TAStudioClipboard.cs
@@ -0,0 +1,23 @@
+﻿using BizHawk.Emulation.Common;
+using BizHawk.Client.Common;
+
+namespace BizHawk.Client.EmuHawk
+{
+	public class TasClipboardEntry
+	{
+		public TasClipboardEntry(int frame, IController controllerState)
+		{
+			Frame = frame;
+			ControllerState = controllerState;
+		}
+
+		public int Frame { get; private set; }
+		public IController ControllerState { get; private set; }
+
+		public override string ToString()
+		{
+			var lg = Global.MovieSession.Movie.LogGeneratorInstance();
+			return lg.GenerateLogEntry();
+		}
+	}
+}
