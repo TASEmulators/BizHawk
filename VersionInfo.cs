09f928992609a104961bdaab56925dcaf93f8efd
adelikat <adelikat@tasvideos.org>
2014-07-20 13:10:23 +0000

    update release branch info for the 1.7.2a release

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -1,7 +1,7 @@
 static class VersionInfo
 {
-	public const string MAINVERSION = "1.7.1";
-	public static string RELEASEDATE = "January 22, 2014";
+	public const string MAINVERSION = "1.7.2";
+	public static string RELEASEDATE = "July 20, 2014";
 	public static bool DeveloperBuild = false;
 
 	public static string GetEmuVersion()
