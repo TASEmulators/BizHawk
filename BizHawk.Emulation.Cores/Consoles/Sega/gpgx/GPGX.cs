0e12d5cdc3f701057d5bad39a7fec965b7b91a8e
adelikat <adelikat@tasvideos.org>
2014-07-20 00:26:42 +0000

    pretty up Genesis setting display names

--- BizHawk.Emulation.Cores/Consoles/Sega/gpgx/GPGX.cs
+++ BizHawk.Emulation.Cores/Consoles/Sega/gpgx/GPGX.cs
@@ -760,12 +760,17 @@ namespace BizHawk.Emulation.Cores.Consoles.Sega.gpgx
 
 		public class GPGXSettings
 		{
+			[DisplayName("Background Layer A")]
 			[Description("True to draw BG layer A")]
 			[DefaultValue(true)]
 			public bool DrawBGA { get; set; }
+
+			[DisplayName("Background Layer B")]
 			[Description("True to draw BG layer B")]
 			[DefaultValue(true)]
 			public bool DrawBGB { get; set; }
+
+			[DisplayName("Background Layer W")]
 			[Description("True to draw BG layer W")]
 			[DefaultValue(true)]
 			public bool DrawBGW { get; set; }
@@ -792,12 +797,17 @@ namespace BizHawk.Emulation.Cores.Consoles.Sega.gpgx
 
 		public class GPGXSyncSettings
 		{
+			[DisplayName("Use Six Button Controllers")]
 			[Description("Controls the type of any attached normal controllers; six button controllers are used if true, otherwise three button controllers.  Some games don't work correctly with six button controllers.  Not relevant if other controller types are connected.")]
 			[DefaultValue(true)]
 			public bool UseSixButton { get; set; }
+
+			[DisplayName("Control Type")]
 			[Description("Sets the type of controls that are plugged into the console.  Some games will automatically load with a different control type.")]
 			[DefaultValue(ControlType.Normal)]
 			public ControlType ControlType { get; set; }
+
+			[DisplayName("Autodetect Region")]
 			[Description("Sets the region of the emulated console.  Many games can run on multiple regions and will behave differently on different ones.  Some games may require a particular region.")]
 			[DefaultValue(LibGPGX.Region.Autodetect)]
 			public LibGPGX.Region Region { get; set; }
