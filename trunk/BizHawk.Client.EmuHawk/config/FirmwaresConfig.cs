5604956f1ee866323397183fb561290754157602
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-07-27 01:19:33 +0000

    fix some dangling references to BizHawk.Multiclient mostly in comments, but also in some client facing tooltip and lua documentation

--- trunk/BizHawk.Client.EmuHawk/config/FirmwaresConfig.cs
+++ trunk/BizHawk.Client.EmuHawk/config/FirmwaresConfig.cs
@@ -13,7 +13,7 @@ using BizHawk.Client.Common;
 using BizHawk.Emulation.Common;
 
 //notes: eventually, we intend to have a "firmware acquisition interface" exposed to the emulator cores.
-//it will be implemented by the multiclient, and use firmware keys to fetch the firmware content.
+//it will be implemented by emuhawk, and use firmware keys to fetch the firmware content.
 //however, for now, the cores are using strings from the config class. so we have the `configMember` which is 
 //used by reflection to set the configuration for firmwares which were found
 
