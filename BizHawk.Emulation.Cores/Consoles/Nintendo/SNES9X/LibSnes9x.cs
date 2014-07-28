0c0f9eb93a362b451e1c9891d506db610ead4715
goyuken <goyuken@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-07-28 20:45:21 +0000

    snes9x rough-in

---
+++ BizHawk.Emulation.Cores/Consoles/Nintendo/SNES9X/LibSnes9x.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace BizHawk.Emulation.Cores.Nintendo.SNES9X
+{
+	public class LibSnes9x
+	{
+		const string DllName = "libbizsnes.dll";
+		const CallingConvention CC = CallingConvention.Cdecl;
+
+		[DllImport(DllName, CallingConvention = CC)]
+		public static extern bool debug_init(byte[] data, int length);
+
+		[DllImport(DllName, CallingConvention = CC)]
+		public static extern void debug_advance(int[] data);
+	}
+}
