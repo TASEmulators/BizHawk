71a564b1cfd49f487676e11c77d1763a8b6cb756
adelikat <adelikat@tasvideos.org>
2014-08-17 15:11:04 +0000

    Movie MultitrackRewiringControlllerAdapter to a property in the Multitrack object

--- BizHawk.Client.Common/Global.cs
+++ BizHawk.Client.Common/Global.cs
@@ -15,12 +15,6 @@ namespace BizHawk.Client.Common
 		public static FirmwareManager FirmwareManager;
 		public static Rewinder Rewinder;
 
-		//Movie
-
-		/// <summary>
-		/// the global MovieSession can use this to deal with multitrack player remapping (should this be here? maybe it should be in MovieSession)
-		/// </summary>
-		public static MultitrackRewiringControllerAdapter MultitrackRewiringControllerAdapter = new MultitrackRewiringControllerAdapter();
 		public static MovieSession MovieSession = new MovieSession();
 
 		/// <summary>
--- BizHawk.Client.Common/inputAdapters/InputManager.cs
+++ BizHawk.Client.Common/inputAdapters/InputManager.cs
@@ -16,8 +16,8 @@ namespace BizHawk.Client.Common
 			Global.StickyXORAdapter.Source = Global.UD_LR_ControllerAdapter;
 			Global.AutofireStickyXORAdapter.Source = Global.StickyXORAdapter;
 
-			Global.MultitrackRewiringControllerAdapter.Source = Global.AutofireStickyXORAdapter;
-			Global.MovieInputSourceAdapter.Source = Global.MultitrackRewiringControllerAdapter;
+			Global.MovieSession.MultiTrack.MultitrackRewiringAdapter.Source = Global.AutofireStickyXORAdapter;
+			Global.MovieInputSourceAdapter.Source = Global.MovieSession.MultiTrack.MultitrackRewiringAdapter;
 			Global.ControllerOutput.Source = Global.MovieOutputHardpoint;
 
 			Global.Emulator.Controller = Global.ControllerOutput;
