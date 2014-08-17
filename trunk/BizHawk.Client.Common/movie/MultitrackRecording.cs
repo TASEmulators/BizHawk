dfa737ef5d87ab2d30a1519349c068dbd7d0bc42
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-08-17 15:22:02 +0000

    Some more moving code around related to multitrack

--- trunk/BizHawk.Client.Common/movie/MultitrackRecording.cs
+++ trunk/BizHawk.Client.Common/movie/MultitrackRecording.cs
@@ -1,4 +1,6 @@
-﻿namespace BizHawk.Client.Common
+﻿using BizHawk.Emulation.Common;
+
+namespace BizHawk.Client.Common
 {
 	public class MultitrackRecorder
 	{
@@ -77,6 +79,56 @@
 			{
 				CurrentPlayer = Global.Emulator.ControllerDefinition.PlayerCount;
 			}
+		}
+	}
+
+	/// <summary>
+	/// rewires player1 controls to playerN
+	/// </summary>
+	public class MultitrackRewiringControllerAdapter : IController
+	{
+		public IController Source { get; set; }
+		public int PlayerSource = 1;
+		public int PlayerTargetMask = 0;
+
+		public ControllerDefinition Type { get { return Source.Type; } }
+		public bool this[string button] { get { return IsPressed(button); } }
+
+		// floats can be player number remapped just like boolbuttons
+		public float GetFloat(string name) { return Source.GetFloat(RemapButtonName(name)); }
+
+		private string RemapButtonName(string button)
+		{
+			// Do we even have a source?
+			if (PlayerSource == -1)
+			{
+				return button;
+			}
+
+			// See if we're being asked for a button that we know how to rewire
+			var bnp = ButtonNameParser.Parse(button);
+
+			if (bnp == null)
+			{
+				return button;
+			}
+
+			// Ok, this looks like a normal `P1 Button` type thing. we can handle it
+			// Were we supposed to replace this one?
+			int foundPlayerMask = (1 << bnp.PlayerNum);
+			if ((PlayerTargetMask & foundPlayerMask) == 0)
+			{
+				return button;
+			}
+
+			// Ok, we were. swap out the source player and then grab his button
+			bnp.PlayerNum = PlayerSource;
+			return bnp.ToString();
+		}
+
+		public bool IsPressed(string button)
+		{
+			return Source.IsPressed(RemapButtonName(button));
 		}
 	}
 }
