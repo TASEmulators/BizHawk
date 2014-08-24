24a2b9a94f8a44698ad5bfd4bc315f92decf7aaf
goyuken <goyuken@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-08-24 21:53:48 +0000

    tasstatemanager: fix out of memory on save

--- trunk/BizHawk.Client.Common/movie/tasproj/TasStateManager.cs
+++ trunk/BizHawk.Client.Common/movie/tasproj/TasStateManager.cs
@@ -103,23 +103,31 @@ namespace BizHawk.Client.Common
 			Used = 0;
 		}
 
-		public byte[] ToArray()
+		public void Save(BinaryWriter bw)
 		{
-			MemoryStream ms = new MemoryStream();
-			var bytes = BitConverter.GetBytes(States.Count);
-			ms.Write(bytes, 0, bytes.Length);
+			bw.Write(States.Count);
 			foreach (var kvp in States)
 			{
-				var frame = BitConverter.GetBytes(kvp.Key);
-				ms.Write(frame, 0, frame.Length);
-
-				var stateLen = BitConverter.GetBytes(kvp.Value.Length);
-				ms.Write(stateLen, 0, stateLen.Length);
-				ms.Write(kvp.Value, 0, kvp.Value.Length);
+				bw.Write(kvp.Key);
+				bw.Write(kvp.Value.Length);
+				bw.Write(kvp.Value);
 			}
-
-			return ms.ToArray();
 		}
+
+		public void Load(BinaryReader br)
+		{
+			// ARE WE SUPPOSED TO CLEAR FIRST???
+			int nstates = br.ReadInt32();
+			for (int i = 0; i < nstates; i++)
+			{
+				int frame = br.ReadInt32();
+				int len = br.ReadInt32();
+				byte[] data = br.ReadBytes(len);
+				States.Add(frame, data);
+				Used += len;
+			}
+		}
+
 
 		// Map:
 		// 4 bytes - total savestate count
@@ -127,27 +135,6 @@ namespace BizHawk.Client.Common
 		// 4 bytes - frame
 		// 4 bytes - length of savestate
 		// 0 - n savestate
-		public void FromArray(byte[] bytes)
-		{
-			var position = 0;
-			var stateCount = BitConverter.ToInt32(bytes, 0);
-			position += 4;
-			for (int i = 0; i < stateCount; i++)
-			{
-				var frame = BitConverter.ToInt32(bytes, position);
-				position += 4;
-				var stateLen = BitConverter.ToInt32(bytes, position);
-				position += 4;
-				var state = bytes
-					.Skip(position)
-					.Take(stateLen)
-					.ToArray();
-
-				position += stateLen;
-				States.Add(frame, state);
-				Used += state.Length;
-			}
-		}
 
 		private int Used
 		{
