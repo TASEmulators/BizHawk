17f109905bc2201236f1db0bb6254a6c38c66d79
adelikat <adelikat@tasvideos.org>
2014-08-29 23:48:10 +0000

    TAStudio - some minor designer stuff

--- BizHawk.Client.EmuHawk/tools/TAStudio/TAStudio.Designer.cs
+++ BizHawk.Client.EmuHawk/tools/TAStudio/TAStudio.Designer.cs
@@ -701,6 +701,7 @@ namespace BizHawk.Client.EmuHawk
 			this.TasView.ItemCount = 0;
 			this.TasView.LastVisibleIndex = 22;
 			this.TasView.Location = new System.Drawing.Point(8, 27);
+			this.TasView.MultiSelect = true;
 			this.TasView.Name = "TasView";
 			this.TasView.Size = new System.Drawing.Size(288, 471);
 			this.TasView.TabIndex = 1;
