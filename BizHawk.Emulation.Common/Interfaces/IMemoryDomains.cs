8634d52bed09f9059e77f99952e088607017811f
goyuken <goyuken@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-09-01 21:09:25 +0000

    remove the dummymemorydomainlist as its not needed anymore (if a core has no memory domains, it simply doesn't have the interface)

--- BizHawk.Emulation.Common/Interfaces/IMemoryDomains.cs
+++ BizHawk.Emulation.Common/Interfaces/IMemoryDomains.cs
@@ -5,7 +5,7 @@
 		///The list of all avaialble memory domains
 		/// A memory domain is a byte array that respresents a distinct part of the emulated system.
 		/// By convention the Main Memory is the 1st domain in the list
-		// All cores sould implement a System Bus domain that represents the standard "Open bus" range for that system,
+		// All cores sould implement a System Bus domain that represents the standard cpu bus range for that system,
 		/// and a Main Memory which is typically the WRAM space (for instance, on NES - 0000-07FF),
 		/// Other chips, and ram spaces can be added as well.
 		/// Subdomains of another domain are also welcome.
