a32b5faf95e9a9b299f084eff1803c857e436d86
zeromus <zeromus@zeromus.org>
2014-09-18 00:31:32 +0000

    add AlphaBlend to GDIRenderer

--- BizHawk.Client.EmuHawk/tools/TAStudio/InputRoll.cs
+++ BizHawk.Client.EmuHawk/tools/TAStudio/InputRoll.cs
@@ -632,7 +632,7 @@ namespace BizHawk.Client.EmuHawk
 
 							if (image != null)
 							{
-								Gdi.DrawBitmap(image, new Point(col.Left.Value, point.Y + 1));
+								Gdi.DrawBitmap(image, new Point(col.Left.Value, point.Y + 1),true);
 							}
 							else
 							{
