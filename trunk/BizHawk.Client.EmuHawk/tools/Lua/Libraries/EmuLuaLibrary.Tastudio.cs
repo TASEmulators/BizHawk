767c31527ac8769b741984e1d60e3e04f057273f
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-09-19 21:43:16 +0000

    Start a tastudio lua library

---
+++ trunk/BizHawk.Client.EmuHawk/tools/Lua/Libraries/EmuLuaLibrary.Tastudio.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+
+using BizHawk.Client.Common;
+using LuaInterface;
+
+namespace BizHawk.Client.EmuHawk
+{
+	[Description("A library for manipulating the Tastudio dialog of the EmuHawk client")]
+	public sealed class TastudioLuaLibrary : LuaLibraryBase
+	{
+		public TastudioLuaLibrary(Lua lua)
+			: base(lua) { }
+
+		public TastudioLuaLibrary(Lua lua, Action<string> logOutputCallback)
+			: base(lua, logOutputCallback) { }
+
+		public override string Name { get { return "tastudio"; } }
+
+		[LuaMethodAttributes(
+			"engaged",
+			"returns whether or not tastudio is currently engaged (active)"
+		)]
+		public bool Engaged()
+		{
+			return GlobalWin.Tools.Has<TAStudio>(); // TODO: eventually tastudio should have an engaged flag
+		}
+	}
+}
