8197f96c984ed6975ea625350234107bb19564fb
adelikat <adelikat@tasvideos.org>
2014-11-02 00:22:04 +0000

    TasMovie - make a TasLagLog class to simplify some code

---
+++ BizHawk.Client.Common/movie/tasproj/TasLagLog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BizHawk.Client.Common
+{
+	public class TasLagLog : List<bool>
+	{
+		public void RemoveFrom(int frame)
+		{
+			if (frame + 1 < this.Count)
+			{
+				this.RemoveRange(frame + 1, this.Count - frame - 1);
+			}
+		}
+
+		public bool? Lagged(int index)
+		{
+			if (index < this.Count)
+			{
+				return this[index];
+			}
+
+			return null;
+		}
+	}
+}
