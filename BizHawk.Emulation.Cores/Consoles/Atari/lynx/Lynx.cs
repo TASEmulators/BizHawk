fa8934b1d4ea13be0ce921c81a0480995de39e06
goyuken <goyuken@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-11-02 19:11:19 +0000

    lynx: memory domains.  R/W:  RAM, Save Ram.  R:  Cart A, Cart B.

--- BizHawk.Emulation.Cores/Consoles/Atari/lynx/Lynx.cs
+++ BizHawk.Emulation.Cores/Consoles/Atari/lynx/Lynx.cs
@@ -12,7 +12,7 @@ using Newtonsoft.Json;
 namespace BizHawk.Emulation.Cores.Atari.Lynx
 {
 	[CoreAttributes("Handy", "K. Wilkins", true, false, "mednafen 0-9-34-1", "http://mednafen.sourceforge.net/")]
-	public class Lynx : IEmulator, IVideoProvider, ISyncSoundProvider
+	public class Lynx : IEmulator, IVideoProvider, ISyncSoundProvider, IMemoryDomains
 	{
 		IntPtr Core;
 
@@ -112,6 +112,7 @@ namespace BizHawk.Emulation.Cores.Atari.Lynx
 					BufferWidth = WIDTH;
 					BufferHeight = HEIGHT;
 				}
+				SetupMemoryDomains();
 			}
 			catch
 			{
@@ -160,10 +161,6 @@ namespace BizHawk.Emulation.Cores.Atari.Lynx
 				Core = IntPtr.Zero;
 			}
 		}
-
-		#region debugging
-
-		#endregion
 
 		#region Controller
 
@@ -364,6 +361,34 @@ namespace BizHawk.Emulation.Cores.Atari.Lynx
 		public void DiscardSamples()
 		{
 		}
+
+		#endregion
+
+		#region MemoryDomains
+
+		private void SetupMemoryDomains()
+		{
+			var mms = new List<MemoryDomain>();
+			mms.Add(MemoryDomain.FromIntPtr("RAM", 65536, MemoryDomain.Endian.Little, LibLynx.GetRamPointer(Core), true));
+
+			IntPtr p;
+			int s;
+			if (LibLynx.GetSaveRamPtr(Core, out s, out p))
+				mms.Add(MemoryDomain.FromIntPtr("Save RAM", s, MemoryDomain.Endian.Little, p, true));
+
+			IntPtr p0, p1;
+			int s0, s1;
+			LibLynx.GetReadOnlyCartPtrs(Core, out s0, out p0, out s1, out p1);
+			if (s0 > 0 && p0 != IntPtr.Zero)
+				mms.Add(MemoryDomain.FromIntPtr("Cart A", s0, MemoryDomain.Endian.Little, p0, false));
+			if (s1 > 0 && p1 != IntPtr.Zero)
+				mms.Add(MemoryDomain.FromIntPtr("Cart B", s1, MemoryDomain.Endian.Little, p1, false));
+
+			MemoryDomains = new MemoryDomainList(mms, 0);
+		}
+
+
+		public MemoryDomainList MemoryDomains { get; private set; }
 
 		#endregion
 	}
