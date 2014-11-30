cc94352ba00bef576c4afb270789ece57e4a1239
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-11-30 23:50:07 +0000

    Ti83 - nit picky reorg

---
+++ trunk/BizHawk.Emulation.Cores/Calculator/TI83.IInputPollable.cs
@@ -0,0 +1,22 @@
+﻿using BizHawk.Emulation.Common;
+
+namespace BizHawk.Emulation.Cores.Calculators
+{
+	public partial class TI83 : IInputPollable
+	{
+		private int _lagCount = 0;
+		private bool _lagged = true;
+		private bool _isLag = false;
+
+		public int LagCount
+		{
+			get { return _lagCount; }
+			set { _lagCount = value; }
+		}
+
+		public bool IsLagFrame
+		{
+			get { return _isLag; }
+		}
+	}
+}
