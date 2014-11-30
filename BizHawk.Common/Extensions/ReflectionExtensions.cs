8c4e0f12030235192d46676d7c30afd1721412a5
adelikat <adelikat@tasvideos.org>
2014-11-30 14:04:00 +0000

    Move some extension methods to a better location

--- BizHawk.Common/Extensions/ReflectionExtensions.cs
+++ BizHawk.Common/Extensions/ReflectionExtensions.cs
@@ -206,5 +206,16 @@ namespace BizHawk.Common.ReflectionExtensions
 			return (IntPtr)dyn.Invoke(null, new object[] { new object() });
 		}
 
+		public static bool ThrowsError(this MethodInfo info)
+		{
+			var il = info.GetMethodBody().GetILAsByteArray();
+			return (il[il.Length - 1] == 0x7A);
+		}
+
+		public static bool IsEmpty(this MethodInfo info)
+		{
+			var il = info.GetMethodBody().GetILAsByteArray();
+			return (il.Length == 1 && il[0] == 0x2A);
+		}
 	}
 }
