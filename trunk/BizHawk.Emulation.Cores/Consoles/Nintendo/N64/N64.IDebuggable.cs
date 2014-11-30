24c2bc8cab8da852ef06f603a622e1b20958ac55
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-11-30 15:38:39 +0000

    N64 - break off ICoreService implementations into separate files

---
+++ trunk/BizHawk.Emulation.Cores/Consoles/Nintendo/N64/N64.IDebuggable.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+using BizHawk.Emulation.Common;
+
+namespace BizHawk.Emulation.Cores.Nintendo.N64
+{
+	public partial class N64 : IDebuggable
+	{
+		public IDictionary<string, int> GetCpuFlagsAndRegisters()
+		{
+			// note: the approach this code takes is highly bug-prone
+			var ret = new Dictionary<string, int>();
+			var data = new byte[32 * 8 + 4 + 4 + 8 + 8 + 4 + 4 + 32 * 4 + 32 * 8];
+			api.getRegisters(data);
+
+			for (int i = 0; i < 32; i++)
+			{
+				var reg = BitConverter.ToInt64(data, i * 8);
+				ret.Add("REG" + i + "_lo", (int)(reg));
+				ret.Add("REG" + i + "_hi", (int)(reg >> 32));
+			}
+
+			var PC = BitConverter.ToUInt32(data, 32 * 8);
+			ret.Add("PC", (int)PC);
+
+			ret.Add("LL", BitConverter.ToInt32(data, 32 * 8 + 4));
+
+			var Lo = BitConverter.ToInt64(data, 32 * 8 + 4 + 4);
+			ret.Add("LO_lo", (int)Lo);
+			ret.Add("LO_hi", (int)(Lo >> 32));
+
+			var Hi = BitConverter.ToInt64(data, 32 * 8 + 4 + 4 + 8);
+			ret.Add("HI_lo", (int)Hi);
+			ret.Add("HI_hi", (int)(Hi >> 32));
+
+			ret.Add("FCR0", BitConverter.ToInt32(data, 32 * 8 + 4 + 4 + 8 + 8));
+			ret.Add("FCR31", BitConverter.ToInt32(data, 32 * 8 + 4 + 4 + 8 + 8 + 4));
+
+			for (int i = 0; i < 32; i++)
+			{
+				var reg_cop0 = BitConverter.ToUInt32(data, 32 * 8 + 4 + 4 + 8 + 8 + 4 + 4 + i * 4);
+				ret.Add("CP0 REG" + i, (int)reg_cop0);
+			}
+
+			for (int i = 0; i < 32; i++)
+			{
+				var reg_cop1_fgr_64 = BitConverter.ToInt64(data, 32 * 8 + 4 + 4 + 8 + 8 + 4 + 4 + 32 * 4 + i * 8);
+				ret.Add("CP1 FGR REG" + i + "_lo", (int)reg_cop1_fgr_64);
+				ret.Add("CP1 FGR REG" + i + "_hi", (int)(reg_cop1_fgr_64 >> 32));
+			}
+
+			return ret;
+		}
+
+		[FeatureNotImplemented]
+		public void SetCpuRegister(string register, int value)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
