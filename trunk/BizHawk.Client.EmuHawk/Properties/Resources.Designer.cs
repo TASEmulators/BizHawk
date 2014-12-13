7959acf66894d390959506e75d4214bd12808546
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-12-13 02:16:05 +0000

    Add dualshock controller image to controller config

--- trunk/BizHawk.Client.EmuHawk/Properties/Resources.Designer.cs
+++ trunk/BizHawk.Client.EmuHawk/Properties/Resources.Designer.cs
@@ -992,6 +992,16 @@ namespace BizHawk.Client.EmuHawk.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap psx_dualshock {
+            get {
+                object obj = ResourceManager.GetObject("psx_dualshock", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap QuickNes {
             get {
                 object obj = ResourceManager.GetObject("QuickNes", resourceCulture);
