d01b7b7df0f061ff3f23eb92cc50fc2927b3198f
zeromus@zeromus.org <zeromus@zeromus.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2014-12-21 05:36:44 +0000

    psx - add note that hardcore debug mode might be nice for users of segmented AV dumping

--- trunk/BizHawk.Client.EmuHawk/config/PSX/PSXOptions.Designer.cs
+++ trunk/BizHawk.Client.EmuHawk/config/PSX/PSXOptions.Designer.cs
@@ -108,8 +108,8 @@
 			this.lblTweakedMednafen.Size = new System.Drawing.Size(213, 79);
 			this.lblTweakedMednafen.TabIndex = 28;
 			this.lblTweakedMednafen.Text = "Displays all content at as multiple of 400x300.\r\n • Correct aspect ratio\r\n • Gene" +
-    "rally enjoyable game presentation\r\n • Detail loss at 1x in fewer cases\r\n • Requi" +
-    "res certain display configuration:\r\n";
+					"rally enjoyable game presentation\r\n • Detail loss at 1x in fewer cases\r\n • Requi" +
+					"res certain display configuration:\r\n";
 			// 
 			// rbTweakedMednafenMode
 			// 
@@ -126,10 +126,10 @@
 			// 
 			this.label3.Location = new System.Drawing.Point(246, 39);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(213, 50);
+			this.label3.Size = new System.Drawing.Size(213, 63);
 			this.label3.TabIndex = 26;
 			this.label3.Text = "Displays all content unmodified\r\n • Window size will constantly change\r\n • Aspect" +
-    " ratio is usually wrong";
+					" ratio is usually wrong\r\n • Ideal for segmented AV dumping";
 			// 
 			// rbDebugMode
 			// 
@@ -179,8 +179,8 @@
 			this.lblPixelPro.Size = new System.Drawing.Size(252, 78);
 			this.lblPixelPro.TabIndex = 21;
 			this.lblPixelPro.Text = "Converts content with nearest neighbor to \r\nfit gracefully in a 800x480 window.\r\n" +
-    " • Content is pixel perfect\r\n • Aspect ratio is usually wrong\r\n • Game may seen " +
-    "to have scale varying by mode\r\n\r\n\r\n";
+					" • Content is pixel perfect\r\n • Aspect ratio is usually wrong\r\n • Game may seen " +
+					"to have scale varying by mode\r\n\r\n\r\n";
 			// 
 			// rbPixelPro
 			// 
@@ -203,8 +203,8 @@
 			// 
 			// groupBox2
 			// 
-			this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+			this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+									| System.Windows.Forms.AnchorStyles.Right)));
 			this.groupBox2.Controls.Add(this.lblPAL);
 			this.groupBox2.Controls.Add(this.PAL_LastLineNumeric);
 			this.groupBox2.Controls.Add(this.PAL_FirstLineNumeric);
