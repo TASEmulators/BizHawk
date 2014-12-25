e80de55210335b7ed841433c18d80d9b7b928f28
zeromus <zeromus@zeromus.org>
2014-12-25 19:56:04 +0000

    disc - add a project including mednafen's disc-loading code, for a/b testing bizhawk's disc loading on large disc sets, and add an example a/b test using it.

---
+++ psx/mednadisc/Mednadisc.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "emuware/emuware.h"
+
+class CDAccess;
+
+EW_EXPORT void* mednadisc_LoadCD(const char* fname);
+EW_EXPORT int32 mednadisc_ReadSector(CDAccess* disc, int lba, void* buf2448);
+EW_EXPORT void mednadisc_CloseCD(CDAccess* disc);
