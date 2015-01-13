35c103e868bcef1067b8a68d22213f5f41cf3a81
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2015-01-13 19:55:36 +0000

    Yabause - break up emulator services into separate files

---
+++ trunk/BizHawk.Emulation.Cores/Consoles/Sega/Saturn/Yabause.IDriveLight.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using BizHawk.Emulation.Common;
+
+namespace BizHawk.Emulation.Cores.Sega.Saturn
+{
+	public partial class Yabause : IDriveLight
+	{
+		public bool DriveLightEnabled { get; private set; }
+		public bool DriveLightOn { get; private set; }
+	}
+}
