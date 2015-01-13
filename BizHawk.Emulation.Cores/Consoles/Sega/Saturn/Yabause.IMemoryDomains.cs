d2b0579609ce84bcd5f851ef26a9e464df616485
goyuken <goyuken@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2015-01-13 21:46:28 +0000

    yabause: clean up memory domains slightly

--- BizHawk.Emulation.Cores/Consoles/Sega/Saturn/Yabause.IMemoryDomains.cs
+++ BizHawk.Emulation.Cores/Consoles/Sega/Saturn/Yabause.IMemoryDomains.cs
@@ -18,38 +18,11 @@ namespace BizHawk.Emulation.Cores.Sega.Saturn
 			{
 				int l = nmd.length;
 				IntPtr d = nmd.data;
-				ret.Add(new MemoryDomain(
-					nmd.name,
-					nmd.length,
-					MemoryDomain.Endian.Little,
-					delegate(int addr)
-					{
-						if (addr < 0 || addr >= l)
-							throw new ArgumentOutOfRangeException();
-						unsafe
-						{
-							byte* p = (byte*)d;
-							return p[addr];
-						}
-					},
-					delegate(int addr, byte val)
-					{
-						if (addr < 0 || addr >= l)
-							throw new ArgumentOutOfRangeException();
-						unsafe
-						{
-							byte* p = (byte*)d;
-							p[addr] = val;
-						}
-					}
-				));
+				ret.Add(MemoryDomain.FromIntPtr(nmd.name, nmd.length, MemoryDomain.Endian.Little, nmd.data));
 			}
 
-			// fulfill the prophecy of MainMemory always being MemoryDomains[0]
-			var tmp = ret[2];
-			ret[2] = ret[0];
-			ret[0] = tmp;
-			MemoryDomains = new MemoryDomainList(ret);
+			// main memory is in position 2
+			MemoryDomains = new MemoryDomainList(ret, 2);
 		}
 	}
 }
