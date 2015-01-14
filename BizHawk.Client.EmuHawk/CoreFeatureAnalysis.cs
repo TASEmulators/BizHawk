a46b2130544d3ca47b10490605577c1dcac316b4
adelikat <adelikat@tasvideos.org>
2015-01-14 22:56:08 +0000

    Core Features dialog - make 2 tabs, on the first show information about the currently instantiated core, this will show all registered services and their current state of implementation, 2nd tab will show all cores.  Still todo: gut the functionality of the 2nd tab since it is no longer reliable information

--- BizHawk.Client.EmuHawk/CoreFeatureAnalysis.cs
+++ BizHawk.Client.EmuHawk/CoreFeatureAnalysis.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 
 using BizHawk.Emulation.Common;
 using BizHawk.Emulation.Common.IEmulatorExtensions;
+using BizHawk.Client.Common;
 
 namespace BizHawk.Client.EmuHawk
 {
@@ -23,10 +24,121 @@ namespace BizHawk.Client.EmuHawk
 
 		private void CoreFeatureAnalysis_Load(object sender, EventArgs e)
 		{
+			DoCurrentCoreTree();
+			DoAllCoresTree();
+		}
+
+		private void DoCurrentCoreTree()
+		{
+			CurrentCoreTree.ImageList = new ImageList();
+			CurrentCoreTree.ImageList.Images.Add("Good", Properties.Resources.GreenCheck);
+			CurrentCoreTree.ImageList.Images.Add("Bad", Properties.Resources.ExclamationRed);
+
+			var core = Global.Emulator;
+			var services = Assembly
+				.GetAssembly(typeof(IEmulator))
+				.GetTypes()
+				.Where(t => t.IsInterface)
+				.Where(t => typeof(IEmulatorService).IsAssignableFrom(t))
+				.Where(t => t != typeof(IEmulatorService))
+				.ToList();
+
+			var additionalRegisteredServices = core.ServiceProvider.AvailableServices
+				.Where(s => !services.Contains(s)) 
+				.Where(s => s != core.GetType()); // We don't care about the core itself
+
+			services.AddRange(additionalRegisteredServices);
+
+			CurrentCoreTree.Nodes.Clear();
+			CurrentCoreTree.BeginUpdate();
+
+			var coreNode = new TreeNode
+			{
+				Text = core.Attributes().CoreName + (core.Attributes().Released ? string.Empty : " (UNRELEASED)"),
+				ForeColor = core.Attributes().Released ? Color.Black : Color.DarkGray,
+			};
+
+
+			coreNode.Expand();
+
+			bool missingImplementation = false;
+
+			foreach (var service in services)
+			{
+				bool isImplemented = false;
+				if (core.ServiceProvider.HasService(service))
+				{
+					isImplemented = true;
+				}
+				else if (service.IsAssignableFrom(typeof(ISettable<,>))) // TODO
+				{
+					isImplemented = core.GetType()
+						.GetInterfaces()
+						.Where(t => t.IsGenericType &&
+									t.GetGenericTypeDefinition() == typeof(ISettable<,>))
+						.FirstOrDefault() != null;
+				}
+
+				var serviceNode = new TreeNode
+				{
+					Text = service.Name,
+					ForeColor = isImplemented ? Color.Black : Color.Red
+				};
+
+				bool fullyImplementedInterface = isImplemented;
+
+				if (isImplemented)
+				{
+					foreach (var field in service.GetMethods().OrderBy(f => f.Name))
+					{
+						try
+						{
+							var coreImplementation = core.ServiceProvider.GetService(service).GetType().GetMethod(field.Name);
+
+							if (coreImplementation != null)
+							{
+								var i = coreImplementation.IsImplemented();
+								serviceNode.Nodes.Add(new TreeNode
+								{
+									Text = field.Name,
+									ImageKey = i ? "Good" : "Bad",
+									SelectedImageKey = i ? "Good" : "Bad",
+									StateImageKey = i ? "Good" : "Bad"
+								});
+
+								if (!i)
+								{
+									fullyImplementedInterface = false;
+								}
+							}
+						}
+						catch (Exception)
+						{
+							// TODO: SavestateBinary() and SaveStateBinary(BinaryWriter bw) cause an exception, need to look at signature too
+						}
+					}
+				}
+				else
+				{
+					missingImplementation = true;
+				}
+
+				serviceNode.StateImageKey = serviceNode.SelectedImageKey = serviceNode.ImageKey = fullyImplementedInterface ? "Good" : "Bad";
+
+				coreNode.Nodes.Add(serviceNode);
+			}
+
+			coreNode.StateImageKey = coreNode.SelectedImageKey = coreNode.ImageKey = missingImplementation ? "Bad" : "Good";
+			CurrentCoreTree.Nodes.Add(coreNode);
+
+			CurrentCoreTree.EndUpdate();
+		}
+
+		private void DoAllCoresTree()
+		{
 			CoreTree.ImageList = new ImageList();
 			CoreTree.ImageList.Images.Add("Good", Properties.Resources.GreenCheck);
 			CoreTree.ImageList.Images.Add("Bad", Properties.Resources.ExclamationRed);
-
 
 			var services = Assembly
 				.GetAssembly(typeof(IEmulator))
