c3a4077ffb0ac6b5e2e8c948d6a9c991e9077a90
adelikat <adelikat@tasvideos.org>
2015-01-16 18:37:42 +0000

    Break up Gambatte.cs into separate files

---
+++ BizHawk.Emulation.Cores/Consoles/Nintendo/Gameboy/Gambatte.IDebuggable.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using BizHawk.Emulation.Common;
+
+namespace BizHawk.Emulation.Cores.Nintendo.Gameboy
+{
+	public partial class Gameboy : IDebuggable
+	{
+		public IDictionary<string, RegisterValue> GetCpuFlagsAndRegisters()
+		{
+			int[] data = new int[10];
+			LibGambatte.gambatte_getregs(GambatteState, data);
+
+			return new Dictionary<string, RegisterValue>
+			{
+				{ "PC", (ushort)(data[(int)LibGambatte.RegIndicies.PC] & 0xffff) },
+				{ "SP", (ushort)(data[(int)LibGambatte.RegIndicies.SP] & 0xffff) },
+				{ "A", (byte)(data[(int)LibGambatte.RegIndicies.A] & 0xff) },
+				{ "B", (byte)(data[(int)LibGambatte.RegIndicies.B] & 0xff) },
+				{ "C", (byte)(data[(int)LibGambatte.RegIndicies.C] & 0xff) },
+				{ "D", (byte)(data[(int)LibGambatte.RegIndicies.D] & 0xff) },
+				{ "E", (byte)(data[(int)LibGambatte.RegIndicies.E] & 0xff) },
+				{ "F", (byte)(data[(int)LibGambatte.RegIndicies.F] & 0xff) },
+				{ "H", (byte)(data[(int)LibGambatte.RegIndicies.H] & 0xff) },
+				{ "L", (byte)(data[(int)LibGambatte.RegIndicies.L] & 0xff) }
+			};
+		}
+
+		[FeatureNotImplemented]
+		public void SetCpuRegister(string register, int value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool CanStep(StepType type)
+		{
+			return false;
+		}
+
+		[FeatureNotImplemented]
+		public void Step(StepType type) { throw new NotImplementedException(); }
+
+		public IMemoryCallbackSystem MemoryCallbacks
+		{
+			get { return _memorycallbacks; }
+		}
+
+		private LibGambatte.MemoryCallback readcb;
+		private LibGambatte.MemoryCallback writecb;
+		private LibGambatte.MemoryCallback execcb;
+
+		private MemoryCallbackSystem _memorycallbacks = new MemoryCallbackSystem();
+
+		/// <summary>
+		/// for use in dual core
+		/// </summary>
+		/// <param name="ics"></param>
+		internal void ConnectMemoryCallbackSystem(MemoryCallbackSystem mcs)
+		{
+			_memorycallbacks = mcs;
+		}
+
+		private void InitMemoryCallbacks()
+		{
+			readcb = (addr) => MemoryCallbacks.CallReads(addr);
+			writecb = (addr) => MemoryCallbacks.CallWrites(addr);
+			execcb = (addr) => MemoryCallbacks.CallExecutes(addr);
+			_memorycallbacks.ActiveChanged += RefreshMemoryCallbacks;
+		}
+
+		private void RefreshMemoryCallbacks()
+		{
+			var mcs = MemoryCallbacks;
+
+			LibGambatte.gambatte_setreadcallback(GambatteState, mcs.HasReads ? readcb : null);
+			LibGambatte.gambatte_setwritecallback(GambatteState, mcs.HasWrites ? writecb : null);
+			LibGambatte.gambatte_setexeccallback(GambatteState, mcs.HasExecutes ? execcb : null);
+		}
+	}
+}
---
+++ BizHawk.Emulation.Cores/Consoles/Nintendo/Gameboy/Gambatte.IVideoProvider.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using BizHawk.Emulation.Common;
+
+namespace BizHawk.Emulation.Cores.Nintendo.Gameboy
+{
+	public partial class Gameboy
+	{
+		/// <summary>
+		/// stored image of most recent frame
+		/// </summary>
+		private int[] VideoBuffer = new int[160 * 144];
+
+		public int[] GetVideoBuffer()
+		{
+			return VideoBuffer;
+		}
+
+		public int VirtualWidth
+		{
+			// only sgb changes this, which we don't emulate here
+			get { return 160; }
+		}
+
+		public int VirtualHeight
+		{
+			get { return 144; }
+		}
+
+		public int BufferWidth
+		{
+			get { return 160; }
+		}
+
+		public int BufferHeight
+		{
+			get { return 144; }
+		}
+
+		public int BackgroundColor
+		{
+			get { return 0; }
+		}
+	}
+}
