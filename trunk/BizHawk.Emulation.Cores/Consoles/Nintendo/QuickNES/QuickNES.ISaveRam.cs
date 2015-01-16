1800b70f1e7b3b2fa4c1d7eac3c23124e07fd678
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2015-01-16 01:38:47 +0000

    QuickNES - break up into separate files

---
+++ trunk/BizHawk.Emulation.Cores/Consoles/Nintendo/QuickNES/QuickNES.ISaveRam.cs
@@ -0,0 +1,35 @@
+﻿using BizHawk.Emulation.Common;
+
+namespace BizHawk.Emulation.Cores.Consoles.Nintendo.QuickNES
+{
+	public partial class QuickNES : ISaveRam
+	{
+		public byte[] CloneSaveRam()
+		{
+			LibQuickNES.ThrowStringError(LibQuickNES.qn_battery_ram_save(Context, SaveRamBuff, SaveRamBuff.Length));
+			return (byte[])SaveRamBuff.Clone();
+		}
+
+		public void StoreSaveRam(byte[] data)
+		{
+			LibQuickNES.ThrowStringError(LibQuickNES.qn_battery_ram_load(Context, data, data.Length));
+		}
+
+		public bool SaveRamModified
+		{
+			get
+			{
+				return LibQuickNES.qn_has_battery_ram(Context);
+			}
+		}
+
+		private byte[] SaveRamBuff;
+
+		private void InitSaveRamBuff()
+		{
+			int size = 0;
+			LibQuickNES.ThrowStringError(LibQuickNES.qn_battery_ram_size(Context, ref size));
+			SaveRamBuff = new byte[size];
+		}
+	}
+}
