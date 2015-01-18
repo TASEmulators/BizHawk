ac4e197e22b67b4d1166d7acf32a448224e88597
jdpurcell <jdpurcell@gmail.com>
2015-01-18 19:44:51 +0000

    Fix trackbars in Display config having different background color than their container (workaround applied only at runtime to avoid breaking designer).

---
+++ BizHawk.Client.EmuHawk/CustomControls/TransparentTrackbar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace BizHawk.Client.EmuHawk
+{
+	public class TransparentTrackBar : TrackBar
+	{
+		protected override void OnCreateControl()
+		{
+			if (!DesignMode)
+			{
+				SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+				if (Parent != null)
+					BackColor = Parent.BackColor;
+			}
+
+			base.OnCreateControl();
+		}
+	}
+}
