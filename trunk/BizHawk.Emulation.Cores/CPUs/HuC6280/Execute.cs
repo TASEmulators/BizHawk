06b69838d87eb5dde7ecda861baeeeb44b5ee29d
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2015-01-24 14:52:20 +0000

    Memory callbacks - check the HasReads/Writes/Executes flags during the call methods so that the responsibility does not fall on the core, remove the check in the PCE core

--- trunk/BizHawk.Emulation.Cores/CPUs/HuC6280/Execute.cs
+++ trunk/BizHawk.Emulation.Cores/CPUs/HuC6280/Execute.cs
@@ -59,7 +59,7 @@ namespace BizHawk.Emulation.Cores.Components.H6280
                 LagIFlag = FlagI;
 
                 if (Debug) Logger(State());
-                if (Core.MemoryCallbacks.HasExecutes) { Core.MemoryCallbacks.CallExecutes(PC); }
+                Core.MemoryCallbacks.CallExecutes(PC);
 
                 if (CDLLoggingActive) CDLOpcode();
 
