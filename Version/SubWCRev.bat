c2621e798d9ab5bd12323d47bc886fa4021b3fd8
zeromus <zeromus@zeromus.org>
2015-02-01 03:01:31 +0000

    fix subwcrev stuff. this time, fixes stale versions on f5 or ctrl+f5 builds.
    in the future, we should use svn msbuild community tasks for even cleverer and leaner solutions (which dont depend on subwcrev.exe...)

--- Version/SubWCRev.bat
+++ Version/SubWCRev.bat
@@ -1,39 +1,38 @@
 @echo off
 
 set TEMPFILE="%TEMP%\BIZBUILD-SVN-%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%"
-set SVNREV="%~1svnrev.cs"
+set SVNREV="%~1\svnrev.cs"
 
 rem try generating svnrev from svn now. this will fail if svn is nonexistent, so...
-"%~1SubWCRev.exe" "%~1\..\." "%~1svnrev_template" %TEMPFILE% > nul
+"%~1\SubWCRev.exe" "%~1\..\." "%~1\svnrev_template" %TEMPFILE% > NUL
 
 rem generate a svnrev with sed using no revision number, in case svn isnt available
 if not exist %TEMPFILE% (
-    "%~1sed.exe" s/\$WCREV\$/0/ < "%~1svnrev_template" > %TEMPFILE%
+    "%~1\sed.exe" s/\$WCREV\$/0/ < "%~1\svnrev_template" > %TEMPFILE%
 )
 
 rem ... ignore the error
 SET ERRORLEVEL=0
 
 
-
 rem if we didnt even have a svnrev, then go ahead and copy it
 if not exist %SVNREV% (
-   @copy /y %TEMPFILE% %SVNREV% > NUL
+   copy /y %TEMPFILE% %SVNREV% > NUL
 ) else if exist %TEMPFILE% (
   rem check to see whether its any different, so we dont touch unchanged files
-  fc /b %TEMPFILE% %SVNREV% > nul
+  fc /b %TEMPFILE% %SVNREV% > NUL
   if ERRORLEVEL 2 (
     echo Updated svnrev file
-    @copy /y %TEMPFILE% %SVNREV% > NUL
+    copy /y %TEMPFILE% %SVNREV% > NUL
     goto SKIP
   )
   if ERRORLEVEL 1 (
     echo Updated svnrev file
-    @copy /y %TEMPFILE% %SVNREV% > NUL
+    copy /y %TEMPFILE% %SVNREV% > NUL
     goto SKIP
   )
   if ERRORLEVEL 0 (
-    echo Not touching unchanged svnrev file
+    echo svnrev is indicating no changes
     goto SKIP
   )  
 
