60ed815b687b717ea392a2fc2ef40a3993ed8332
adelikat <adelikat@tasvideos.org>
2015-02-03 00:24:38 +0000

    add some plumbing for a PJM importer

---
+++ BizHawk.Client.Common/movie/import/PJMImport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace BizHawk.Client.Common
+{
+	public static class PJMImport
+	{
+		public static Bk2Movie Import(string path, out string errorMsg, out string warningMsg)
+		{
+			errorMsg = string.Empty;
+			warningMsg = string.Empty;
+			return new Bk2Movie();
+		}
+	}
+}
