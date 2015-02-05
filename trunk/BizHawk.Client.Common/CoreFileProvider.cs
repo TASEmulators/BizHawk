fcb120cd149960e7204cbebd0123e9b178d019ab
goyuken <goyuken@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2015-02-05 23:25:28 +0000

    frontend tracks loaded firmware: proof of concept

--- trunk/BizHawk.Client.Common/CoreFileProvider.cs
+++ trunk/BizHawk.Client.Common/CoreFileProvider.cs
@@ -17,17 +17,6 @@ namespace BizHawk.Client.Common
 			_showWarning = showWarning;
 		}
 
-		public Stream OpenFirmware(string sysId, string key)
-		{
-			var fn = PathFirmware(sysId, key);
-			return new FileStream(fn, FileMode.Open, FileAccess.Read, FileShare.Read);
-		}
-
-		public string PathFirmware(string sysId, string key)
-		{
-			return FirmwareManager.Request(sysId, key);
-		}
-
 		public string PathSubfile(string fname)
 		{
 			return Path.Combine(Path.GetDirectoryName(SubfileDirectory) ?? String.Empty, fname);
