0ff1808101c04570f7c122c2a60d67ad87c50f0d
scepheo <scepheo@gmail.com>
2015-02-08 00:35:32 +0000

    RAM Search: Fixed go-to using index instead of address.

--- BizHawk.Client.EmuHawk/BizBox.Designer.cs
+++ BizHawk.Client.EmuHawk/BizBox.Designer.cs
@@ -51,9 +51,10 @@
 			// 
 			this.OK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.OK.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.OK.Location = new System.Drawing.Point(361, 504);
+			this.OK.Location = new System.Drawing.Point(481, 620);
+			this.OK.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.OK.Name = "OK";
-			this.OK.Size = new System.Drawing.Size(75, 23);
+			this.OK.Size = new System.Drawing.Size(100, 28);
 			this.OK.TabIndex = 0;
 			this.OK.Text = "&Ok";
 			this.OK.UseVisualStyleBackColor = true;
@@ -62,9 +63,10 @@
 			// pictureBox1
 			// 
 			this.pictureBox1.Image = global::BizHawk.Client.EmuHawk.Properties.Resources.corphawk;
-			this.pictureBox1.Location = new System.Drawing.Point(12, 10);
+			this.pictureBox1.Location = new System.Drawing.Point(16, 12);
+			this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(164, 164);
+			this.pictureBox1.Size = new System.Drawing.Size(219, 202);
 			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
 			this.pictureBox1.TabIndex = 1;
 			this.pictureBox1.TabStop = false;
@@ -73,9 +75,10 @@
 			// 
 			this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.linkLabel1.AutoSize = true;
-			this.linkLabel1.Location = new System.Drawing.Point(245, 509);
+			this.linkLabel1.Location = new System.Drawing.Point(327, 626);
+			this.linkLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.linkLabel1.Name = "linkLabel1";
-			this.linkLabel1.Size = new System.Drawing.Size(102, 13);
+			this.linkLabel1.Size = new System.Drawing.Size(132, 17);
 			this.linkLabel1.TabIndex = 2;
 			this.linkLabel1.TabStop = true;
 			this.linkLabel1.Text = "Bizhawk Homepage";
@@ -84,18 +87,20 @@
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(198, 70);
+			this.label1.Location = new System.Drawing.Point(264, 86);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(46, 13);
+			this.label1.Size = new System.Drawing.Size(61, 17);
 			this.label1.TabIndex = 3;
 			this.label1.Text = "Authors:";
 			// 
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(198, 85);
+			this.label2.Location = new System.Drawing.Point(264, 105);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(149, 13);
+			this.label2.Size = new System.Drawing.Size(197, 17);
 			this.label2.TabIndex = 4;
 			this.label2.Text = "vecna, adelikat, zeromus, natt";
 			// 
@@ -103,9 +108,10 @@
 			// 
 			this.label3.AutoSize = true;
 			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label3.Location = new System.Drawing.Point(197, 10);
+			this.label3.Location = new System.Drawing.Point(263, 12);
+			this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(78, 20);
+			this.label3.Size = new System.Drawing.Size(95, 25);
 			this.label3.TabIndex = 5;
 			this.label3.Text = "BizHawk";
 			// 
@@ -113,62 +119,69 @@
 			// 
 			this.label4.AutoSize = true;
 			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label4.Location = new System.Drawing.Point(206, 31);
+			this.label4.Location = new System.Drawing.Point(275, 38);
+			this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(156, 16);
+			this.label4.Size = new System.Drawing.Size(202, 20);
 			this.label4.TabIndex = 6;
 			this.label4.Text = "A multi-Platform Emulator";
 			// 
 			// VersionLabel
 			// 
 			this.VersionLabel.AutoSize = true;
-			this.VersionLabel.Location = new System.Drawing.Point(198, 52);
+			this.VersionLabel.Location = new System.Drawing.Point(264, 64);
+			this.VersionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.VersionLabel.Name = "VersionLabel";
-			this.VersionLabel.Size = new System.Drawing.Size(0, 13);
+			this.VersionLabel.Size = new System.Drawing.Size(0, 17);
 			this.VersionLabel.TabIndex = 7;
 			// 
 			// label5
 			// 
 			this.label5.AutoSize = true;
-			this.label5.Location = new System.Drawing.Point(198, 105);
+			this.label5.Location = new System.Drawing.Point(264, 129);
+			this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(66, 13);
+			this.label5.Size = new System.Drawing.Size(89, 17);
 			this.label5.TabIndex = 8;
 			this.label5.Text = "Contributors:";
 			// 
 			// label6
 			// 
 			this.label6.AutoSize = true;
-			this.label6.Location = new System.Drawing.Point(198, 119);
+			this.label6.Location = new System.Drawing.Point(264, 146);
+			this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label6.Name = "label6";
-			this.label6.Size = new System.Drawing.Size(217, 13);
+			this.label6.Size = new System.Drawing.Size(286, 17);
 			this.label6.TabIndex = 9;
 			this.label6.Text = "DarkKobold, Micro500, TaoTao, Rolanmen1";
 			// 
 			// label7
 			// 
 			this.label7.AutoSize = true;
-			this.label7.Location = new System.Drawing.Point(198, 133);
+			this.label7.Location = new System.Drawing.Point(264, 164);
+			this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label7.Name = "label7";
-			this.label7.Size = new System.Drawing.Size(216, 13);
+			this.label7.Size = new System.Drawing.Size(285, 17);
 			this.label7.TabIndex = 10;
 			this.label7.Text = "Deign, Brandon Evans, Pasky13, link_7777,";
 			// 
 			// label27
 			// 
 			this.label27.AutoSize = true;
-			this.label27.Location = new System.Drawing.Point(198, 147);
+			this.label27.Location = new System.Drawing.Point(264, 181);
+			this.label27.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label27.Name = "label27";
-			this.label27.Size = new System.Drawing.Size(149, 13);
+			this.label27.Size = new System.Drawing.Size(263, 17);
 			this.label27.TabIndex = 12;
-			this.label27.Text = "SaxxonPike, MasterofPuppets";
+			this.label27.Text = "SaxxonPike, MasterofPuppets, Scepheo,";
 			// 
 			// label37
 			// 
 			this.label37.AutoSize = true;
-			this.label37.Location = new System.Drawing.Point(198, 162);
+			this.label37.Location = new System.Drawing.Point(264, 199);
+			this.label37.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label37.Name = "label37";
-			this.label37.Size = new System.Drawing.Size(96, 13);
+			this.label37.Size = new System.Drawing.Size(128, 17);
 			this.label37.TabIndex = 13;
 			this.label37.Text = "null_ptr, MightyMar";
 			// 
@@ -179,20 +192,22 @@
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.CoreInfoPanel.AutoScroll = true;
 			this.CoreInfoPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-			this.CoreInfoPanel.Location = new System.Drawing.Point(12, 180);
+			this.CoreInfoPanel.Location = new System.Drawing.Point(16, 222);
+			this.CoreInfoPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.CoreInfoPanel.Name = "CoreInfoPanel";
-			this.CoreInfoPanel.Size = new System.Drawing.Size(423, 276);
+			this.CoreInfoPanel.Size = new System.Drawing.Size(563, 339);
 			this.CoreInfoPanel.TabIndex = 14;
 			// 
 			// textBox1
 			// 
 			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.textBox1.Location = new System.Drawing.Point(13, 462);
+			this.textBox1.Location = new System.Drawing.Point(17, 569);
+			this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.textBox1.Multiline = true;
 			this.textBox1.Name = "textBox1";
 			this.textBox1.ReadOnly = true;
-			this.textBox1.Size = new System.Drawing.Size(423, 33);
+			this.textBox1.Size = new System.Drawing.Size(563, 40);
 			this.textBox1.TabIndex = 16;
 			this.textBox1.Text = "jabo_direct3d8_patched.dll is distributed with the special permission of the auth" +
     "or.";
@@ -200,10 +215,10 @@
 			// BizBox
 			// 
 			this.AcceptButton = this.OK;
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.CancelButton = this.OK;
-			this.ClientSize = new System.Drawing.Size(448, 536);
+			this.ClientSize = new System.Drawing.Size(597, 660);
 			this.Controls.Add(this.textBox1);
 			this.Controls.Add(this.CoreInfoPanel);
 			this.Controls.Add(this.label37);
@@ -220,7 +235,8 @@
 			this.Controls.Add(this.pictureBox1);
 			this.Controls.Add(this.OK);
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.MinimumSize = new System.Drawing.Size(453, 253);
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.MinimumSize = new System.Drawing.Size(598, 301);
 			this.Name = "BizBox";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
 			this.Text = " BizHawk";
