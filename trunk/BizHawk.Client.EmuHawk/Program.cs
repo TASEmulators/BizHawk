bfdae91ff61ef4202690014dc8a1d14f1cc825ff
jdpurcell@gmail.com <jdpurcell@gmail.com@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2015-02-22 04:43:52 +0000

    Remove unused SlimDX.DirectSound usings.

--- trunk/BizHawk.Client.EmuHawk/Program.cs
+++ trunk/BizHawk.Client.EmuHawk/Program.cs
@@ -5,7 +5,6 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 #if WINDOWS
-using SlimDX.DirectSound;
 using Microsoft.VisualBasic.ApplicationServices;
 #endif
 
