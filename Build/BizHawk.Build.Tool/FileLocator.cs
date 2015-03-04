e7fe6252c624ce4aa355c2f8c860f865eea2efaf
zeromus <zeromus@zeromus.org>
2015-03-04 09:33:04 +0000

    * Added a project which can contain build-related tools (and msbuild tasks if we ever need them) and gave it responsibility for managing the svn revision stuff.
    * With a little more work, it could handle unix and git checkouts too.
    
    * It could pull revisions from submodules as well, if they werent useless gibberish in git
    
    * Unaddressed: theres still a problem where depending on how emuhawk is built, the version project might not trigger

---
+++ Build/BizHawk.Build.Tool/FileLocator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Win32;
+
+static class FileLocator
+{
+	public static string LocateSVNTool(string _toolName)
+	{
+		string t = ToolPathUtil.MakeToolName(_toolName);
+		string dir = null;
+		try
+		{
+			dir = FindToolPath(t);
+		}
+		catch { }
+		if (dir == null)
+			return "";
+		else
+			return System.IO.Path.Combine(dir, t);
+
+	}
+
+	//stolen from MSBuild.Community.Tasks
+	static string FindToolPath(string toolName)
+	{
+		string toolPath =
+				ToolPathUtil.FindInRegistry(toolName) ??
+				ToolPathUtil.FindInPath(toolName) ??
+				ToolPathUtil.FindInProgramFiles(toolName,
+						@"Subversion\bin",
+						@"CollabNet Subversion Server",
+						@"CollabNet Subversion",
+						@"CollabNet Subversion Client",
+						@"VisualSVN\bin",
+						@"VisualSVN Server\bin",
+						@"SlikSvn\bin");
+
+		if (toolPath == null)
+		{
+			throw new Exception("Could not find svn.exe.  Looked in PATH locations and various common folders inside Program Files.");
+		}
+
+		return toolPath;
+	}
+
+
+}
