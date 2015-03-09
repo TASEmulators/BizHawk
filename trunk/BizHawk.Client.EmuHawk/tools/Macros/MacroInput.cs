a7cd43f6ecc3e3a1718d58a742a43223dc24bde9
SuuperW <SuuperW@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2015-03-09 22:30:54 +0000

    Lots of work on the Macro Tool.

--- trunk/BizHawk.Client.EmuHawk/tools/Macros/MacroInput.cs
+++ trunk/BizHawk.Client.EmuHawk/tools/Macros/MacroInput.cs
@@ -1,5 +1,4 @@
-﻿using BizHawk.Emulation.Common;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +6,12 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+
+using BizHawk.Emulation.Common;
+using BizHawk.Client.Common;
+using BizHawk.Client.EmuHawk.WinFormExtensions;
+using BizHawk.Client.EmuHawk.ToolExtensions;
 
 namespace BizHawk.Client.EmuHawk
 {
@@ -14,31 +19,89 @@ namespace BizHawk.Client.EmuHawk
 	{
 		[RequiredService]
 		private IEmulator Emulator { get; set; }
+		// Zones
+		List<MovieZone> zones = new List<MovieZone>();
+		private TasMovie CurrentTasMovie
+		{
+			get { return Global.MovieSession.Movie as TasMovie; }
+		}
 
+		[ConfigPersist]
+		private MacroSettings Settings { get; set; }
+
+		class MacroSettings
+		{
+			public MacroSettings()
+			{
+				RecentMacro = new RecentFiles(8);
+			}
+
+			public RecentFiles RecentMacro { get; set; }
+		}
+
+		private bool _initializing = false;
 		public MacroInputTool()
 		{
+			_initializing = true;
 			InitializeComponent();
 		}
 
-
 		private void MacroInputTool_Load(object sender, EventArgs e)
 		{
+			// Movie recording must be active
+			if (!Global.MovieSession.Movie.IsActive)
+			{
+				MessageBox.Show("In order to use this tool you must be recording a movie.");
+				this.Close();
+				this.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+				return;
+			}
 
+			ReplaceBox.Enabled = CurrentTasMovie is TasMovie;
+			PlaceNum.Enabled = CurrentTasMovie is TasMovie;
+
+			Settings = new MacroSettings();
+
+			MovieZone main = new MovieZone(CurrentTasMovie, 0, CurrentTasMovie.InputLogLength);
+			main.Name = "Entire Movie";
+
+			zones.Add(main);
+			ZonesList.Items.Add(main.Name + " - length: " + main.Length);
+			ZonesList.Items[0] += " [Zones don't change!]";
+
+			SetUpButtonBoxes();
+
+			_initializing = false;
 		}
 
 		public void Restart()
 		{
+			if (_initializing)
+				return;
 
+			zones.Clear();
+			ZonesList.Items.Clear();
+
+			MovieZone main = new MovieZone(CurrentTasMovie, 0, CurrentTasMovie.InputLogLength);
+			main.Name = "Entire Movie";
+
+			zones.Add(main);
+			ZonesList.Items.Add(main.Name + " - length: " + main.Length);
+			ZonesList.Items[0] += " [Zones don't change!]";
 		}
 
+		// These do absolutely nothing.
 		public void UpdateValues()
 		{
-			
-		}
 
+		}
 		public void FastUpdate()
 		{
 
+		}
+		public bool UpdateBefore
+		{
+			get { return true; }
 		}
 
 		public bool AskSaveChanges()
@@ -46,14 +109,151 @@ namespace BizHawk.Client.EmuHawk
 			return true;
 		}
 
-		public bool UpdateBefore
-		{
-			get { return true; } // TODO: think about this
-		}
-
 		private void ExitMenuItem_Click(object sender, EventArgs e)
 		{
 			Close();
 		}
+
+		private void SetZoneButton_Click(object sender, EventArgs e)
+		{
+			if (StartNum.Value >= CurrentTasMovie.InputLogLength || EndNum.Value > CurrentTasMovie.InputLogLength)
+			{
+				MessageBox.Show("Start and end frames must be inside the movie.");
+				return;
+			}
+
+			MovieZone newZone = new MovieZone(CurrentTasMovie, (int)StartNum.Value, (int)(EndNum.Value - StartNum.Value));
+			newZone.Name = "Zone " + zones.Count;
+			zones.Add(newZone);
+			ZonesList.Items.Add(newZone.Name + " - length: " + newZone.Length);
+		}
+
+		private MovieZone selectedZone
+		{
+			get
+			{
+				if (ZonesList.SelectedIndex == -1)
+					return null;
+				return zones[ZonesList.SelectedIndex];
+			}
+		}
+		private bool _selecting = false;
+		private void ZonesList_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (selectedZone == null)
+				return;
+
+			_selecting = true;
+			PlaceNum.Value = selectedZone.Start;
+			ReplaceBox.Checked = selectedZone.Replace;
+			NameTextbox.Text = selectedZone.Name;
+			OverlayBox.Checked = selectedZone.Overlay;
+			_selecting = false;
+		}
+
+		private void NameTextbox_TextChanged(object sender, EventArgs e)
+		{
+			if (selectedZone == null || _selecting)
+				return;
+
+			selectedZone.Name = NameTextbox.Text;
+			ZonesList.Items[ZonesList.SelectedIndex] = selectedZone.Name + " - length: " + selectedZone.Length;
+		}
+		private void PlaceNum_ValueChanged(object sender, EventArgs e)
+		{
+			if (selectedZone == null || _selecting)
+				return;
+
+			selectedZone.Start = (int)PlaceNum.Value;
+		}
+		private void ReplaceBox_CheckedChanged(object sender, EventArgs e)
+		{
+			if (selectedZone == null || _selecting)
+				return;
+
+			selectedZone.Replace = ReplaceBox.Checked;
+		}
+		private void OverlayBox_CheckedChanged(object sender, EventArgs e)
+		{
+			if (selectedZone == null || _selecting)
+				return;
+
+			selectedZone.Overlay = OverlayBox.Checked;
+		}
+		private void CurrentButton_Click(object sender, EventArgs e)
+		{
+			PlaceNum.Value = Global.Emulator.Frame;
+		}
+
+		private void PlaceZoneButton_Click(object sender, EventArgs e)
+		{
+			if (selectedZone == null)
+				return;
+
+			if (!(CurrentTasMovie is TasMovie))
+			{
+				selectedZone.Start = Global.Emulator.Frame;
+			}
+			selectedZone.PlaceZone(CurrentTasMovie);
+		}
+
+		#region "Menu Items"
+		private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			if (selectedZone == null)
+			{
+				MessageBox.Show("Please select a zone first.");
+				return;
+			}
+
+			SaveFileDialog dialog = new SaveFileDialog();
+			dialog.InitialDirectory = SuggestedFolder();
+			dialog.Filter = "Movie Macros (*.bk2m)|*.bk2m|All Files|*.*";
+
+			DialogResult result = dialog.ShowHawkDialog();
+			if (result != DialogResult.OK)
+				return;
+
+			selectedZone.Save(dialog.FileName);
+			Settings.RecentMacro.Add(dialog.FileName);
+		}
+
+		private void loadMacroToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog dialog = new OpenFileDialog();
+			dialog.InitialDirectory = SuggestedFolder();
+			dialog.Filter = "Movie Macros (*.bk2m)|*.bk2m|All Files|*.*";
+
+			DialogResult result = dialog.ShowHawkDialog();
+			if (result != DialogResult.OK)
+				return;
+
+			MovieZone loadZone = new MovieZone(dialog.FileName);
+			zones.Add(loadZone);
+			ZonesList.Items.Add(loadZone.Name + " - length: " + loadZone.Length);
+
+			Settings.RecentMacro.Add(dialog.FileName);
+		}
+
+		private void RecentToolStripMenuItem_DropDownOpened(object sender, EventArgs e)
+		{
+			RecentToolStripMenuItem.DropDownItems.Clear();
+			RecentToolStripMenuItem.DropDownItems.AddRange(
+				Settings.RecentMacro.RecentMenu(DummyLoadProject, true));
+		}
+		private void DummyLoadProject(string path)
+		{
+			MovieZone loadZone = new MovieZone(path);
+			zones.Add(loadZone);
+			ZonesList.Items.Add(loadZone.Name + " - length: " + loadZone.Length);
+		}
+
+		private string SuggestedFolder()
+		{
+			return PathManager.MakeAbsolutePath(Global.Config.PathEntries.MoviesPathFragment, null) +
+				"\\Macros";
+		}
+		#endregion
+
 	}
 }
---
+++ trunk/BizHawk.Client.EmuHawk/tools/Macros/MovieZone.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+using BizHawk.Client.Common;
+using BizHawk.Emulation.Common;
+
+namespace BizHawk.Client.EmuHawk
+{
+	class MovieZone
+	{
+		public string Name;
+
+		public int Start;
+		public int Length;
+		public string InputKey;
+		private string[] _log;
+
+		public bool Replace = true;
+		public bool Overlay = false;
+		public float defaultFloat = 0f;
+
+		private Bk2ControllerAdapter controller;
+		private Bk2ControllerAdapter targetController;
+
+		public MovieZone()
+		{
+		}
+		public MovieZone(TasMovie movie, int start, int length, string key = "")
+		{
+			var lg = Global.MovieSession.LogGeneratorInstance() as Bk2LogEntryGenerator;
+			lg.SetSource(Global.MovieSession.MovieControllerAdapter);
+			targetController = new Bk2ControllerAdapter();
+			targetController.Type = Global.Emulator.ControllerDefinition;
+
+			if (key == "")
+				key = lg.GenerateLogKey();
+			key = key.Replace("LogKey:", "").Replace("#", "");
+			key = key.Substring(0, key.Length - 1);
+
+			InputKey = key;
+			Length = length;
+			_log = new string[length];
+
+			// Get a IController that only contains buttons in key.
+			string[] keys = key.Split('|');
+			ControllerDefinition d = new ControllerDefinition();
+			for (int i = 0; i < keys.Length; i++)
+			{
+				if (Global.Emulator.ControllerDefinition.BoolButtons.Contains(keys[i]))
+					d.BoolButtons.Add(keys[i]);
+				else
+					d.FloatControls.Add(keys[i]);
+			}
+
+			controller = new Bk2ControllerAdapter() { Type = d };
+			var logGenerator = new Bk2LogEntryGenerator("");
+			logGenerator.SetSource(controller);
+			for (int i = 0; i < length; i++)
+			{
+				// Set controller with only the buttons in it's Type.
+				_log[i] = logGenerator.GenerateLogEntry();
+			}
+		}
+
+		public void PlaceZone(TasMovie movie)
+		{
+			// TODO: This should probably do something with undo history batches/naming.
+			if (!Replace)
+				movie.InsertEmptyFrame(Start, Length);
+
+			if (Overlay)
+			{
+				for (int i = 0; i < Length; i++)
+				{
+					// Overlay the frame. Float controls should use the defaultFloat value.
+				}
+			}
+			else
+			{
+				for (int i = 0; i < Length; i++)
+				{ 
+					// Copy over the frame. (Only using the buttons in controller.Type)
+				}
+			}
+
+			if (movie is TasMovie) // Assume TAStudio is open?
+			{
+				if (Global.Emulator.Frame < Start)
+				{
+					// TODO: Go to start of macro? Ask TAStudio to do that?
+
+					// TasMovie.InvalidateAfter(Start) [this is private]
+					// Load last state, Emulate to Start
+
+					// Or do this, if we can accept that TAStudio has to be open.
+					(GlobalWin.Tools.Get<TAStudio>() as TAStudio).GoToFrame(Start);
+
+					GlobalWin.Tools.UpdateBefore();
+					GlobalWin.Tools.UpdateAfter();
+				}
+				else if (GlobalWin.Tools.IsLoaded<TAStudio>())
+				{
+					GlobalWin.Tools.Get<TAStudio>().UpdateValues();
+				}
+			}
+		}
+
+		public void Save(string fileName)
+		{
+			// Save the controller definition/LogKey
+			// Save the controller name and player count. (Only for the user.)
+			// Save whether or not the macro should use overlay input, and/or replace
+
+			File.WriteAllText(fileName, Global.Emulator.ControllerDefinition.Name);
+			File.AppendAllText(fileName, "\n" + Global.Emulator.ControllerDefinition.PlayerCount);
+			File.AppendAllLines(fileName, _log);
+		}
+		public MovieZone(string fileName)
+		{
+			// If the LogKey contains buttons/controls not accepted by the emulator, tell the user and display the macro's controller name and player count.
+
+			if (!File.Exists(fileName))
+				return;
+
+			_log = File.ReadAllLines(fileName);
+			Length = _log.Length;
+			Start = 0;
+
+			Name = Path.GetFileNameWithoutExtension(fileName);
+		}
+	}
+}
