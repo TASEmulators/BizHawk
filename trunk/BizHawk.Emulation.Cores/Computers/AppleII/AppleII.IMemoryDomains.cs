f27f833e2632189f1def56267a35dc1f3f2ad85a
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2015-04-13 21:58:53 +0000

    oops, forgot to add this file

---
+++ trunk/BizHawk.Emulation.Cores/Computers/AppleII/AppleII.IMemoryDomains.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+using BizHawk.Emulation.Common;
+
+namespace BizHawk.Emulation.Cores.Computers.AppleII
+{
+	public partial class AppleII
+	{
+		private void SetupMemoryDomains()
+		{
+			var domains = new List<MemoryDomain>();
+
+			//_machine.Memory.Read
+			var systemBusDomain = new MemoryDomain("System Bus", 0x10000, MemoryDomain.Endian.Little,
+				(addr) =>
+				{
+					if (addr < 0 || addr >= 65536)
+						throw new ArgumentOutOfRangeException();
+					return (byte)_machine.Memory.Read((int)addr);
+				},
+				(addr, value) =>
+				{
+					if (addr < 0 || addr >= 65536)
+						throw new ArgumentOutOfRangeException();
+					_machine.Memory.Write((int)addr, value);
+				});
+
+			domains.Add(systemBusDomain);
+
+			_memoryDomains = new MemoryDomainList(domains);
+			(ServiceProvider as BasicServiceProvider).Register<IMemoryDomains>(_memoryDomains);
+		}
+
+		private IMemoryDomains _memoryDomains;
+	}
+}
