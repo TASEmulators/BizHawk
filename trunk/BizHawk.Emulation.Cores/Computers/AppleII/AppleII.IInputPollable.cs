c1713533ebeb38848ddb966d9898e5ba7caf302c
adelikat@tasvideos.org <adelikat@tasvideos.org@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2015-04-29 20:57:20 +0000

    I like to forget to add files

---
+++ trunk/BizHawk.Emulation.Cores/Computers/AppleII/AppleII.IInputPollable.cs
@@ -0,0 +1,17 @@
+﻿using BizHawk.Emulation.Common;
+
+namespace BizHawk.Emulation.Cores.Computers.AppleII
+{
+	public partial class AppleII : IInputPollable
+	{
+		public int LagCount { get; set; }
+
+		public bool IsLagFrame
+		{
+			get { return _machine.Lagged; }
+			set { _machine.Lagged = value; }
+		}
+
+		public IInputCallbackSystem InputCallbacks { [FeatureNotImplemented]get; private set; }
+	}
+}
