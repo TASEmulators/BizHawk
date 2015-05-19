704ace5eb79ca9af6b8ae018de309f66ab89bdb3
goyuken <goyuken@4bbf4f03-6f44-549e-7e59-35d4b0962838>
2015-05-19 22:01:13 +0000

    AppleII - don't savestate unneeded AV buffers.

--- ExternalCoreProjects/Virtu/Services/AudioService.cs
+++ ExternalCoreProjects/Virtu/Services/AudioService.cs
@@ -22,8 +22,16 @@ namespace Jellyfish.Virtu.Services
 			}
         }
 
+		[Newtonsoft.Json.JsonIgnore] // only relevant if trying to savestate midframe
 		private short[] buff = new short[4096];
+		[Newtonsoft.Json.JsonIgnore] // only relevant if trying to savestate midframe
 		private int pos = 0;
+
+		[System.Runtime.Serialization.OnDeserialized]
+		public void OnDeserialized(System.Runtime.Serialization.StreamingContext context)
+		{
+			pos = 0;
+		}
 
 		public void Clear()
 		{
