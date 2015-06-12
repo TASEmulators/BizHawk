a99c0787a883566b5b4a10e50f59e583215526b8
adelikat <adelikat@tasvideos.org>
2015-06-12 23:51:03 +0000

    Apple II - implement IDisassembler, and IDebuggable.GetCpuFlagsAndRegisters()

---
+++ BizHawk.Emulation.Cores/Computers/AppleII/AppleII.IDisassembler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using BizHawk.Emulation.Common;
+using BizHawk.Emulation.Cores.Components.M6502;
+
+namespace BizHawk.Emulation.Cores.Computers.AppleII
+{
+	public partial class AppleII : IDisassemblable
+	{
+		public string Cpu
+		{
+			get
+			{
+				return "6502";
+			}
+			set
+			{
+			}
+		}
+
+		public string PCRegisterName
+		{
+			get { return "PC"; }
+		}
+
+		public IEnumerable<string> AvailableCpus
+		{
+			get { yield return "6502"; }
+		}
+
+		public string Disassemble(MemoryDomain m, uint addr, out int length)
+		{
+			return MOS6502X.Disassemble((ushort)addr, out length, (a) => m.PeekByte(a));
+		}
+	}
+}
--- ExternalCoreProjects/Virtu/Machine.cs
+++ ExternalCoreProjects/Virtu/Machine.cs
@@ -192,5 +192,25 @@ namespace Jellyfish.Virtu
 
 		public bool Lagged { get; set; }
 		public bool DriveLight { get; set; }
+
+		public IDictionary<string, int> GetCpuFlagsAndRegisters()
+		{
+			return new Dictionary<string, int>
+			{
+				{ "A", Cpu.RA },
+				{ "X", Cpu.RX },
+				{ "Y", Cpu.RY },
+				{ "S", Cpu.RS },
+				{ "PC", Cpu.RPC },
+				{ "Flag C", Cpu.FlagC ? 1 : 0 },
+				{ "Flag Z", Cpu.FlagZ ? 1 : 0 },
+				{ "Flag I", Cpu.FlagI ? 1 : 0 },
+				{ "Flag D", Cpu.FlagD ? 1 : 0 },
+				{ "Flag B", Cpu.FlagB ? 1 : 0 },
+				{ "Flag V", Cpu.FlagV ? 1 : 0 },
+				{ "Flag N", Cpu.FlagN ? 1 : 0 },
+				{ "Flag T", Cpu.FlagT ? 1 : 0 }
+			};
+		}
 	}
 }
