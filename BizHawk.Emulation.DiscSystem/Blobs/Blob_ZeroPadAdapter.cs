33f23e137deb677a6a4ad3c42ccf2ad06064c223
zeromus <zeromus@users.noreply.github.com>
2015-07-07 22:58:51 -0500

    cleanup unused Blob_ZeroTypeStuff and general reorg

--- BizHawk.Emulation.DiscSystem/Blobs/Blob_ZeroPadAdapter.cs
+++ BizHawk.Emulation.DiscSystem/Blobs/Blob_ZeroPadAdapter.cs
@@ -7,22 +7,6 @@ namespace BizHawk.Emulation.DiscSystem
 {
 	public partial class Disc : IDisposable
 	{
-		/// <summary>
-		/// A blob that always reads 0
-		/// </summary>
-		internal sealed class Blob_Zeros : IBlob
-		{
-			public int Read(long byte_pos, byte[] buffer, int offset, int count)
-			{
-				Array.Clear(buffer, offset, count);
-				return count;
-			}
-
-			public void Dispose()
-			{
-			}
-		}
-
 		internal sealed class Blob_ZeroPadAdapter : IBlob
 		{
 			IBlob srcBlob;
@@ -58,44 +42,6 @@ namespace BizHawk.Emulation.DiscSystem
 			{
 			}
 		}
-
-		/// <summary>
-		/// For use with blobs which are prematurely ended: buffers what there is, and zero-pads the rest
-		/// </summary>
-		public sealed class Blob_ZeroPadBuffer : IBlob
-		{
-			public static Blob_ZeroPadBuffer MakeBufferFrom(IBlob baseBlob, long start, int bufferLength)
-			{
-				var ret = new Blob_ZeroPadBuffer();
-
-				//allocate the entire buffer we'll need, and it will already be zero-padded
-				ret.buffer = new byte[bufferLength];
-
-				//read as much as is left
-				baseBlob.Read(start, ret.buffer, 0, bufferLength);
-
-				//if any less got read, well, there were already zeroes there
-				return ret;
-			}
-
-			public int Read(long byte_pos, byte[] buffer, int offset, int count)
-			{
-				int have = buffer.Length;
-				int todo = count;
-				if (have < todo)
-					todo = have;
-				Buffer.BlockCopy(this.buffer, 0, buffer, offset, todo);
-				return todo;
-			}
-
-			byte[] buffer;
-
-			public void Dispose()
-			{
-				buffer = null;
-			}
-		}
-
 		
 	}
 }
