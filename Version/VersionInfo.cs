cedc7d445144fcd5f473ad99ffd526a50cbf8998
hegyak <hegyak@yahoo.com>
2015-07-15 09:27:16 -0700

    We are at Version 1.11.0

--- Version/VersionInfo.cs
+++ Version/VersionInfo.cs
@@ -1,6 +1,6 @@
 static class VersionInfo
 {
-	public const string MAINVERSION = "1.10.0"; // Use numbers only or the new version notification won't work
+	public const string MAINVERSION = "1.11.0"; // Use numbers only or the new version notification won't work
 	public static readonly string RELEASEDATE = "June 15, 2015";
 	public static readonly bool DeveloperBuild = false;
 	public static readonly string HomePage = "http://tasvideos.org/BizHawk.html";
