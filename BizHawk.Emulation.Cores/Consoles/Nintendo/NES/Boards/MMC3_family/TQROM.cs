43f86b5698b610bd0e65128324ccdd6b259a2794
adelikat <adelikat@tasvideos.org>
2015-09-07 22:42:08 -0400

    Cleanup up previous mapper 119 stuff

--- BizHawk.Emulation.Cores/Consoles/Nintendo/NES/Boards/MMC3_family/TQROM.cs
+++ BizHawk.Emulation.Cores/Consoles/Nintendo/NES/Boards/MMC3_family/TQROM.cs
@@ -10,8 +10,9 @@ namespace BizHawk.Emulation.Cores.Nintendo.NES
 			switch (Cart.board_type)
 			{
 				case "MAPPER119":
-				case "NES-TQROM": // High Speed and Pin Bot
 					Cart.vram_size = 8; Cart.wram_size = 0; // Junk ROMs get these wrong
+					break;
+				case "NES-TQROM": // High Speed and Pin Bot
 					AssertPrg(128); AssertChr(64); AssertVram(8); AssertWram(0);
 					break;
 				default:
