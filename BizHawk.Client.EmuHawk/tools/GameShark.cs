6c9006241d56a76424793add901d9cc1766a2fb9
hegyak <hegyak@yahoo.com>
2015-11-14 12:10:23 -0800

    Correct/Cleanup the GameShark Tool.

--- BizHawk.Client.EmuHawk/tools/GameShark.cs
+++ BizHawk.Client.EmuHawk/tools/GameShark.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
 using BizHawk.Emulation.Common;
-//Using the GameBoy Core Directly, is an issue with the limitations for what this tool does.
-using BizHawk.Emulation.Cores.Nintendo.Gameboy;
 using BizHawk.Client.Common;
 using System.Globalization;
 
@@ -10,14 +8,10 @@ namespace BizHawk.Client.EmuHawk
 {
 	public partial class GameShark : Form, IToolForm, IToolFormAutoConfig
 	{
+		[ToolAttributes(released: true, supportedSystems: new[] { "GB" })]
 		//We are using Memory Domains, so we NEED this.
 		[RequiredService]
 		private IMemoryDomains MemoryDomains { get; set; }
-		//Since this is only using GameBoy at the moment, we do this.
-		//TODO:
-		//Expand this so that this tool supports ALL cores that are capable of using GameShark/CodeBreaker/Action Replay/Game Genie
-		[RequiredService]
-		private Gameboy GB { get; set; }
 		public GameShark()
 		{
 			InitializeComponent();
@@ -38,7 +32,7 @@ namespace BizHawk.Client.EmuHawk
 
 		public void FastUpdate()
 		{
-			throw new NotImplementedException();
+			
 		}
 
 		public void Restart()
@@ -77,8 +71,8 @@ namespace BizHawk.Client.EmuHawk
 			RAMAddress = RAMAddress + parseString.Remove(2, 2);
 			//We now have our values.
 			//This part, is annoying...
-			//try
-			//{
+			try
+			{
 				//A Watch needs to be generated so we can make a cheat out of that.  This is due to how the Cheat engine works.
 				//System Bus Domain, The Address to Watch, Byte size (Byte), Hex Display, Description.  Not Big Endian.
 				var watch = Watch.GenerateWatch(MemoryDomains["System Bus"], long.Parse(RAMAddress, NumberStyles.HexNumber), Watch.WatchSize.Byte, Watch.DisplayType.Hex, txtDescription.Text, false);
@@ -87,11 +81,11 @@ namespace BizHawk.Client.EmuHawk
 				//Clear old Inputs
 				txtCheat.Clear();
 				txtDescription.Clear();
-			/*}
-			catch
+			}
+			catch (Exception ex)
 			{
-				MessageBox.Show("An Error occured", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-			} */
+				MessageBox.Show("An Error occured:" + ex.GetType().ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
 		}
 
 		private void btnClear_Click(object sender, EventArgs e)
