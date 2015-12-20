41ba4b6927c30b37003604bc01ff0dd86f3ebd64
hegyak <hegyak@yahoo.com>
2015-12-20 14:41:17 -0800

    This is a DeveloperBuild.

--- Version/VersionInfo.cs
+++ Version/VersionInfo.cs
@@ -2,7 +2,7 @@ static class VersionInfo
 {
 	public const string MAINVERSION = "1.11.4"; // Use numbers only or the new version notification won't work
 	public static readonly string RELEASEDATE = "December 20, 2015";
-	public static readonly bool DeveloperBuild = false;
+	public static readonly bool DeveloperBuild = true;
 	public static readonly string HomePage = "http://tasvideos.org/BizHawk.html";
 
 	public static string GetEmuVersion()
