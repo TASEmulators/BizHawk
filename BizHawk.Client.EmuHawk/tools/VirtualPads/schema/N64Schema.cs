f145f3cc68c0f192a7881dc2caeec39267ec1d7e
Hathor86 <francois.guiot@gmail.com>
2016-01-23 16:16:19 +0100

    Polar Coord
    - Add polar coordinates to VirtualPadAnalogStick
    - Update N64 pad Schema as VirtualPadAnalogStick is a bit larger
    - Add Clamp method to NumberExtensions (allows a IComparable<T> value to be restriced between 2 specified other values)

--- BizHawk.Client.EmuHawk/tools/VirtualPads/schema/N64Schema.cs
+++ BizHawk.Client.EmuHawk/tools/VirtualPads/schema/N64Schema.cs
@@ -26,7 +26,7 @@ namespace BizHawk.Client.EmuHawk
 			return new PadSchema
 			{
 				IsConsole = false,
-				DefaultSize = new Size(220, 316),
+				DefaultSize = new Size(275, 316),
 				Buttons = new[]
 				{
 					new PadSchema.ButtonScema
@@ -34,7 +34,7 @@ namespace BizHawk.Client.EmuHawk
 						Name = "P" + controller + " DPad U",
 						DisplayName = "",
 						Icon = Properties.Resources.BlueUp,
-						Location = new Point(24, 195),
+						Location = new Point(24, 230),
 						Type = PadSchema.PadInputType.Boolean
 					},
 					new PadSchema.ButtonScema
@@ -42,7 +42,7 @@ namespace BizHawk.Client.EmuHawk
 						Name = "P" + controller + " DPad D",
 						DisplayName = "",
 						Icon = Properties.Resources.BlueDown,
-						Location = new Point(24, 216),
+						Location = new Point(24, 251),
 						Type = PadSchema.PadInputType.Boolean
 					},
 					new PadSchema.ButtonScema
@@ -50,7 +50,7 @@ namespace BizHawk.Client.EmuHawk
 						Name = "P" + controller + " DPad L",
 						DisplayName = "",
 						Icon = Properties.Resources.Back,
-						Location = new Point(3, 207),
+						Location = new Point(3, 242),
 						Type = PadSchema.PadInputType.Boolean
 					},
 					new PadSchema.ButtonScema
@@ -58,77 +58,77 @@ namespace BizHawk.Client.EmuHawk
 						Name = "P" + controller + " DPad R",
 						DisplayName = "",
 						Icon = Properties.Resources.Forward,
-						Location = new Point(45, 207),
+						Location = new Point(45, 242),
 						Type = PadSchema.PadInputType.Boolean
 					},
 					new PadSchema.ButtonScema
 					{
 						Name = "P" + controller + " L",
 						DisplayName = "L",
-						Location = new Point(3, 150),
+						Location = new Point(3, 185),
 						Type = PadSchema.PadInputType.Boolean
 					},
 					new PadSchema.ButtonScema
 					{
 						Name = "P" + controller + " R",
 						DisplayName = "R",
-						Location = new Point(191, 150),
+						Location = new Point(191, 185),
 						Type = PadSchema.PadInputType.Boolean
 					},
 					new PadSchema.ButtonScema
 					{
 						Name = "P" + controller + " Z",
 						DisplayName = "Z",
-						Location = new Point(81, 234),
+						Location = new Point(81, 269),
 						Type = PadSchema.PadInputType.Boolean
 					},
 					new PadSchema.ButtonScema
 					{
 						Name = "P" + controller + " Start",
 						DisplayName = "S",
-						Location = new Point(81, 211),
+						Location = new Point(81, 246),
 						Type = PadSchema.PadInputType.Boolean
 					},
 					new PadSchema.ButtonScema
 					{
 						Name = "P" + controller + " B",
 						DisplayName = "B",
-						Location = new Point(127, 211),
+						Location = new Point(127, 246),
 						Type = PadSchema.PadInputType.Boolean
 					},
 					new PadSchema.ButtonScema
 					{
 						Name = "P" + controller + " A",
 						DisplayName = "A",
-						Location = new Point(138, 234),
+						Location = new Point(138, 269),
 						Type = PadSchema.PadInputType.Boolean
 					},
 					new PadSchema.ButtonScema
 					{
 						Name = "P" + controller + " C Up",
 						Icon = Properties.Resources.YellowUp,
-						Location = new Point(173, 175),
+						Location = new Point(173, 210),
 						Type = PadSchema.PadInputType.Boolean
 					},
 					new PadSchema.ButtonScema
 					{
 						Name = "P" + controller + " C Down",
 						Icon = Properties.Resources.YellowDown,
-						Location = new Point(173, 196),
+						Location = new Point(173, 231),
 						Type = PadSchema.PadInputType.Boolean
 					},
 					new PadSchema.ButtonScema
 					{
 						Name = "P" + controller + " C Left",
 						Icon = Properties.Resources.YellowLeft,
-						Location = new Point(152, 189),
+						Location = new Point(152, 221),
 						Type = PadSchema.PadInputType.Boolean
 					},
 					new PadSchema.ButtonScema
 					{
 						Name = "P" + controller + " C Right",
 						Icon = Properties.Resources.YellowRight,
-						Location = new Point(194, 189),
+						Location = new Point(194, 221),
 						Type = PadSchema.PadInputType.Boolean
 					},
 					new PadSchema.ButtonScema
