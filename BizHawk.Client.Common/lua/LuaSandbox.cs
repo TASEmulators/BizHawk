0da6e745e52d52e5acd335234c4e1fab29880d34
zeromus <zeromus@users.noreply.github.com>
2016-01-30 18:37:30 -0600

    move EnvironmentSandbox and LuaSandbox to Client.Common

--- BizHawk.Client.Common/lua/LuaSandbox.cs
+++ BizHawk.Client.Common/lua/LuaSandbox.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using LuaInterface;
-using System.Windows.Forms;
 
 namespace BizHawk.Client.Common
 {
-	class LuaSandbox
+	public class LuaSandbox
 	{
 		protected static Action<string> Logger;
 
