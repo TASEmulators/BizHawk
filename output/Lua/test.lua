cc7b60e4dbb8af7274b46d6281b9381272d0f5e1
adelikat <adelikat@tasvideos.org>
2016-02-06 13:52:33 -0500

    Remove test.lua, not sure when that got checked in

--- output/Lua/test.lua
+++
@@ -1,5 +0,0 @@
-local canvas = gui.createcanvas(200, 200);
-canvas.Clear(0xFF000000);
-canvas.DrawRectangle(50, 50, 100, 100, 0xFFFF0000, 0xFF0000FF);
-canvas.DrawText(0, 0, "Hello, world!");
-canvas.Refresh();
