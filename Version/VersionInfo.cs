3f711e2ce515884b687b0cc47534f0062fba7d4f
hegyak <hegyak@yahoo.com>
2016-02-13 09:25:34 -0800

    Merged d4f2eae8eb2abdb9432e35f6453f4e2c6e44eb2 into Branch/RELEASE
    Updated VersionInfo.cs to 1.11.5

--- Version/VersionInfo.cs
+++ Version/VersionInfo.cs
@@ -1,8 +1,8 @@
 static class VersionInfo
 {
-	public const string MAINVERSION = "1.11.4"; // Use numbers only or the new version notification won't work
-	public static readonly string RELEASEDATE = "December 20, 2015";
-	public static readonly bool DeveloperBuild = true;
+	public const string MAINVERSION = "1.11.5"; // Use numbers only or the new version notification won't work
+	public static readonly string RELEASEDATE = "February 13, 2016";
+	public static readonly bool DeveloperBuild = false;
 	public static readonly string HomePage = "http://tasvideos.org/BizHawk.html";
 
 	public static string GetEmuVersion()
