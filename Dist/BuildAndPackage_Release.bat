75ed1bfca3bb749f70dc3b367a8ad5a76b446369
zeromus <zeromus@users.noreply.github.com>
2016-02-16 17:12:20 -0600

    dont exit from BuildAndPackage_Release.bat

--- Dist/BuildAndPackage_Release.bat
+++ Dist/BuildAndPackage_Release.bat
@@ -1,4 +1,3 @@
 start fart "..\Version\VersionInfo.cs" "DeveloperBuild = true" "DeveloperBuild = false"
 call BuildAndPackage.bat
 start fart "..\Version\VersionInfo.cs" "DeveloperBuild = false" "DeveloperBuild = true"
-exit
