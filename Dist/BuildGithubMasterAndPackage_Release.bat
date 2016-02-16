bcf710871581898e7c64d18b470c0c1b6be7265f
zeromus <zeromus@users.noreply.github.com>
2016-02-16 17:15:03 -0600

    support 64bit only tgit installations and add new batchfile to automatically do a clean clone of github's master branch

---
+++ Dist/BuildGithubMasterAndPackage_Release.bat
@@ -0,0 +1,17 @@
+set PATH=%PATH%;C:\Program Files (x86)\git\bin;C:\Program Files\git\bin
+set WORKDIR=.build-github-master
+rmdir /s /q %WORKDIR%
+mkdir %WORKDIR%
+cd %WORKDIR%
+
+rem http://stackoverflow.com/questions/13750182/git-how-to-archive-from-remote-repository-directly
+git clone --depth=1 --single-branch --branch master http://github.com/TASVideos/BizHawk.git .
+
+cd dist
+call BuildAndPackage_Release.bat
+
+rem make sure we save the output!
+move BizHawk.zip ..\..\BizHawk-Github-Master.zip
+cd ..\..
+
+rmdir /s /q %WORKDIR%
