03b236b0cf1e964374f7f90a50c77693c75c6013
alyosha-tas <alexei.f.k@gmail.com>
2016-04-05 09:24:27 -0400

    Added files via upload

--- BizHawk.Emulation.Cores/Consoles/Atari/2600/Tia/Tia.MissleData.cs
+++ BizHawk.Emulation.Cores/Consoles/Atari/2600/Tia/Tia.MissleData.cs
@@ -2,89 +2,111 @@
 
 namespace BizHawk.Emulation.Cores.Atari.Atari2600
 {
-	public partial class TIA
-	{
-		private struct MissileData
-		{
-			public bool Enabled;
-			public bool ResetToPlayer;
-			public byte HPosCnt;
-			public byte Size;
-			public byte Number;
-			public byte Hm;
-			public byte Collisions;
+    public partial class TIA
+    {
+        private struct MissileData
+        {
+            public bool Enabled;
+            public bool Enabled_use;
+            public int Delay;
+            public int Delay2;
+            public bool ResetToPlayer;
+            public byte HPosCnt;
+            public byte Size;
+            public byte Number;
+            public byte Hm;
+            public byte Collisions;
 
-			public bool Tick()
-			{
-				var result = false;
+            public bool Tick()
+            {
 
-				// At hPosCnt == 0, start drawing the missile, if enabled
-				if (HPosCnt < (1 << Size))
-				{
-					if (Enabled && !ResetToPlayer)
-					{
-						// Draw the missile
-						result = true;
-					}
-				}
 
-				if ((Number & 0x07) == 0x01 || ((Number & 0x07) == 0x03))
-				{
-					if (HPosCnt >= 16 && HPosCnt <= (16 + (1 << Size) - 1))
-					{
-						if (Enabled && !ResetToPlayer)
-						{
-							// Draw the missile
-							result = true;
-						}
-					}
-				}
+                var result = false;
 
-				if ((Number & 0x07) == 0x02 || ((Number & 0x07) == 0x03) || ((Number & 0x07) == 0x06))
-				{
-					if (HPosCnt >= 32 && HPosCnt <= (32 + (1 << Size) - 1))
-					{
-						if (Enabled && !ResetToPlayer)
-						{
-							// Draw the missile
-							result = true;
-						}
-					}
-				}
+                Enabled_use = Enabled;
+                if (Delay2 > 0)
+                {
+                    Enabled_use = true;
+                }
 
-				if ((Number & 0x07) == 0x04 || (Number & 0x07) == 0x06)
-				{
-					if (HPosCnt >= 64 && HPosCnt <= (64 + (1 << Size) - 1))
-					{
-						if (Enabled && !ResetToPlayer)
-						{
-							// Draw the missile
-							result = true;
-						}
-					}
-				}
 
-				// Increment the counter
-				HPosCnt++;
+                // At hPosCnt == 0, start drawing the missile, if enabled
+                if (HPosCnt < (1 << Size))
+                {
+                    if (Enabled_use && !ResetToPlayer && Delay == 0)
+                    {
+                        // Draw the missile
+                        result = true;
+                    }
+                }
 
-				// Counter loops at 160 
-				HPosCnt %= 160;
+                if ((Number & 0x07) == 0x01 || ((Number & 0x07) == 0x03))
+                {
+                    if (HPosCnt >= 16 && HPosCnt <= (16 + (1 << Size) - 1))
+                    {
+                        if (Enabled_use && !ResetToPlayer && Delay == 0)
+                        {
+                            // Draw the missile
+                            result = true;
+                        }
+                    }
+                }
 
-				return result;
-			}
+                if ((Number & 0x07) == 0x02 || ((Number & 0x07) == 0x03) || ((Number & 0x07) == 0x06))
+                {
+                    if (HPosCnt >= 32 && HPosCnt <= (32 + (1 << Size) - 1))
+                    {
+                        if (Enabled_use && !ResetToPlayer && Delay == 0)
+                        {
+                            // Draw the missile
+                            result = true;
+                        }
+                    }
+                }
 
-			public void SyncState(Serializer ser)
-			{
-				ser.BeginSection("Missile");
-				ser.Sync("enabled", ref Enabled);
-				ser.Sync("resetToPlayer", ref ResetToPlayer);
-				ser.Sync("hPosCnt", ref HPosCnt);
-				ser.Sync("size", ref Size);
-				ser.Sync("number", ref Number);
-				ser.Sync("HM", ref Hm);
-				ser.Sync("collisions", ref Collisions);
-				ser.EndSection();
-			}
-		}
-	}
+                if ((Number & 0x07) == 0x04 || (Number & 0x07) == 0x06)
+                {
+                    if (HPosCnt >= 64 && HPosCnt <= (64 + (1 << Size) - 1))
+                    {
+                        if (Enabled_use && !ResetToPlayer && Delay == 0)
+                        {
+                            // Draw the missile
+                            result = true;
+                        }
+                    }
+                }
+
+                // Increment the counter
+                HPosCnt++;
+
+                // Counter loops at 160 
+                HPosCnt %= 160;
+
+                if (Delay > 0)
+                {
+                    Delay--;
+                }
+
+                if (Delay2 > 0)
+                {
+                    Delay2--;
+                }
+
+                return result;
+            }
+
+            public void SyncState(Serializer ser)
+            {
+                ser.BeginSection("Missile");
+                ser.Sync("enabled", ref Enabled);
+                ser.Sync("resetToPlayer", ref ResetToPlayer);
+                ser.Sync("hPosCnt", ref HPosCnt);
+                ser.Sync("size", ref Size);
+                ser.Sync("number", ref Number);
+                ser.Sync("HM", ref Hm);
+                ser.Sync("collisions", ref Collisions);
+                ser.EndSection();
+            }
+        }
+    }
 }
