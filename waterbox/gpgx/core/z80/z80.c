2f405cd01e6171f8d2e8c8cfd5863c3c5650d397
zeromus <zeromus@users.noreply.github.com>
2016-04-15 00:09:40 +0100

    fix gpgx CDL z80 -> MD cart

--- waterbox/gpgx/core/z80/z80.c
+++ waterbox/gpgx/core/z80/z80.c
@@ -594,9 +594,10 @@ void CDLogZ80(uint addr, uint flags)
 	if(addr >= 0x8000)
 	{
     addr = zbank | (addr & 0x7FFF);
-    if (zbank_memory_map[addr >> 16].write)
+    if (zbank_memory_map[addr >> 16].read)
     {
 			//special memory maps are hard to support here.
+			//hopefully, most carts aren't setting this CB for simple accesses to ROMs 
       return;
     }
 
