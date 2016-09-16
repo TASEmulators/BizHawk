8bf4d6e5bcd310d76f24b248946bf28df27d2ef3
adelikat <adelikat@tasvideos.org>
2016-09-16 11:01:13 -0400

    start mapper 195

---
+++ BizHawk.Emulation.Cores/Consoles/Nintendo/NES/Boards/MMC3_family/Mapper195.cs
@@ -0,0 +1,34 @@
+﻿namespace BizHawk.Emulation.Cores.Nintendo.NES
+{
+	public sealed class Mapper195 : MMC3Board_Base
+	{
+		public override bool Configure(NES.EDetectionOrigin origin)
+		{
+			switch (Cart.board_type)
+			{
+				case "MAPPER195":
+					break;
+				default:
+					return false;
+			}
+
+			BaseSetup();
+			return true;
+		}
+
+		public override byte ReadEXP(int addr)
+		{
+			if (addr < 0x1000)
+			{
+				return ROM[(2 << 0x1000) + (addr & 0xFFF)];
+			}
+
+			return base.ReadEXP(addr);
+		}
+
+		public override void WritePRG(int addr, byte value)
+		{
+			base.WritePRG(addr, value);
+		}
+	}
+}
