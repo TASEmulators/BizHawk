2001978a3eb4ad89273cc85d3f39aa1ffda1b1af
adelikat <adelikat@tasvideos.org>
2016-09-16 10:03:28 -0400

    Implement mapper 254

---
+++ BizHawk.Emulation.Cores/Consoles/Nintendo/NES/Boards/MMC3_family/Mapper219.cs
@@ -0,0 +1,19 @@
+﻿namespace BizHawk.Emulation.Cores.Nintendo.NES
+{
+	public sealed class Mapper219 : MMC3Board_Base
+	{
+		public override bool Configure(NES.EDetectionOrigin origin)
+		{
+			switch (Cart.board_type)
+			{
+				case "MAPPER219":
+					break;
+				default:
+					return false;
+			}
+
+			BaseSetup();
+			return true;
+		}
+	}
+}
