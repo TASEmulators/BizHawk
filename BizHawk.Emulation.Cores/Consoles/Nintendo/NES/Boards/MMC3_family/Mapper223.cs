65b3ce30a5443d23c63a7ce7296c054468f502d3
alyosha-tas <alexei.f.k@gmail.com>
2016-09-28 20:42:24 -0400

    fix mapper 223

--- BizHawk.Emulation.Cores/Consoles/Nintendo/NES/Boards/MMC3_family/Mapper223.cs
+++ BizHawk.Emulation.Cores/Consoles/Nintendo/NES/Boards/MMC3_family/Mapper223.cs
@@ -16,7 +16,30 @@ namespace BizHawk.Emulation.Cores.Nintendo.NES
 			}
 
 			BaseSetup();
+
+			mmc3.wram_enable = true;
+			mmc3.wram_write_protect = true;
 			return true;
+		}
+
+		public override void WriteEXP(int addr, byte value)
+		{
+			if (addr>0x1000)
+			{
+				WRAM[addr + 0x4000 - (0x5000 - 0x2000)] = value;
+			}
+			else 
+				base.WriteEXP(addr, value);
+		}
+
+		public override byte ReadEXP(int addr)
+		{
+			if (addr > 0x1000)
+			{
+				return WRAM[addr + 0x4000 - (0x5000 - 0x2000)];
+			}
+			else
+				return base.ReadEXP(addr);
 		}
 	}
 }
