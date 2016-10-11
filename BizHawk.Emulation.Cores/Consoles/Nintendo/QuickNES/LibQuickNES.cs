bd60cf9929d01715e668096347e314dfde96426e
adelikat <adelikat@tasvideos.org>
2016-10-11 17:54:04 -0500

    Quicknes - fallback to neshawk when the ines header fails basic sanity checks, neshawk tends to handle this situation more gracefully, but more importantly this error happens before quicknes can determine if it supports the necessary board

--- BizHawk.Emulation.Cores/Consoles/Nintendo/QuickNES/LibQuickNES.cs
+++ BizHawk.Emulation.Cores/Consoles/Nintendo/QuickNES/LibQuickNES.cs
@@ -249,7 +249,10 @@ namespace BizHawk.Emulation.Cores.Consoles.Nintendo.QuickNES
 			if (p == IntPtr.Zero)
 				return;
 			string s = Marshal.PtrToStringAnsi(p);
-			if (s == "Unsupported mapper" || s == "Not an iNES file") // Not worth making a new exception for the iNES error, they ultimately are the same problem
+			if (s == "Unsupported mapper"
+				|| s == "Not an iNES file" // Not worth making a new exception for the iNES error, they ultimately are the same problem
+				|| s == " truncated file" // This is a garbage rom not worth anyone's time but at least NesHawk handles these better, and these occur before the core has a chance to assess an unsupported mapper
+				) 
 			{
 				throw new Emulation.Common.UnsupportedGameException("Quicknes unsupported mapper");
 			}
