2c1f0013c3f477dbeea5420ce49a9d9fb685d656
adelikat <adelikat@tasvideos.org>
2016-10-13 20:16:26 -0500

    Account for game names having quotes in them, fixes NES Ivan Ironman Stewart's Super Off-Road

--- BizHawk.Client.Common/PathManager.cs
+++ BizHawk.Client.Common/PathManager.cs
@@ -258,7 +258,8 @@ namespace BizHawk.Client.Common
 		{
 			var filesystemSafeName = game.Name
 				.Replace("|", "+")
-				.Replace(":", " -"); // adelikat - Path.GetFileName scraps everything to the left of a colon unfortunately, so we need this hack here
+				.Replace(":", " -") // adelikat - Path.GetFileName scraps everything to the left of a colon unfortunately, so we need this hack here
+				.Replace("\"", ""); // adelikat - Ivan Ironman Stewart's Super Off-Road has quotes in game name
 
 			// zero 06-nov-2015 - regarding the below, i changed my mind. for libretro i want subdirectories here.
 			var filesystemDir = Path.GetDirectoryName(filesystemSafeName);
