f386a97886fd0470207985d11c7cfbf824a77517
adelikat <adelikat@tasvideos.org>
2016-10-23 17:25:30 -0500

    Update version info on release branch

--- Version/VersionInfo.cs
+++ Version/VersionInfo.cs
@@ -1,7 +1,7 @@
 static class VersionInfo
 {
 	public const string MAINVERSION = "1.11.8"; // Use numbers only or the new version notification won't work
-	public static readonly string RELEASEDATE = "October 22, 2016";
+	public static readonly string RELEASEDATE = "October 23, 2016";
 	public static readonly bool DeveloperBuild = false;
 	public static readonly string HomePage = "http://tasvideos.org/BizHawk.html";
 
