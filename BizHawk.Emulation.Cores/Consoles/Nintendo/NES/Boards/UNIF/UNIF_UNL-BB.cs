7893184435d184eb8ea22571902d38cdc49296bf
adelikat <adelikat@tasvideos.org>
2016-10-28 15:11:34 -0500

    NesHawk - support board UNIF_UNL-BB

---
+++ BizHawk.Emulation.Cores/Consoles/Nintendo/NES/Boards/UNIF/UNIF_UNL-BB.cs
@@ -0,0 +1,67 @@
+﻿using BizHawk.Common;
+
+namespace BizHawk.Emulation.Cores.Nintendo.NES
+{
+	public sealed class UNIF_UNL_BB : NES.NESBoardBase
+	{
+		private byte reg, chr;
+		private int prg_mask_32k;
+
+		public override bool Configure(NES.EDetectionOrigin origin)
+		{
+			switch (Cart.board_type)
+			{
+				case "UNIF_UNL-BB":
+					break;
+				default:
+					return false;
+			}
+
+			reg = 0xFF;
+			prg_mask_32k = Cart.prg_size / 32 - 1;
+			SetMirrorType(Cart.pad_h, Cart.pad_v);
+
+			return true;
+		}
+
+		public override void SyncState(Serializer ser)
+		{
+			ser.Sync("reg", ref reg);
+			ser.Sync("chr", ref chr);
+			base.SyncState(ser);
+		}
+
+		public override void WritePRG(int addr, byte value)
+		{
+			addr += 0x8000;
+			if ((addr & 0x9000) == 0x8000)
+			{
+				reg = chr = value;
+			}
+			else
+			{
+				chr = (byte)(value & 1);
+			}
+		}
+
+		public override byte ReadWRAM(int addr)
+		{
+			return ROM[((reg & 3) << 13) + addr];
+		}
+
+		public override byte ReadPRG(int addr)
+		{
+			return ROM[(prg_mask_32k << 15) + addr];
+		}
+
+		public override byte ReadPPU(int addr)
+		{
+			if (addr < 0x2000)
+			{
+				return VROM[((chr & 3) << 13) + addr];
+			}
+
+			return base.ReadPPU(addr);
+		}
+	}
+}
