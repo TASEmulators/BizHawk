bf9956089da8a2dcf8e690bc2ec6c3283e2d29e4
adelikat <adelikat@tasvideos.org>
2016-11-12 09:25:18 -0600

    Lua - default the memory library to system bus (if available)

--- BizHawk.Client.Common/lua/EmuLuaLibrary.Memory.cs
+++ BizHawk.Client.Common/lua/EmuLuaLibrary.Memory.cs
@@ -10,8 +10,6 @@ namespace BizHawk.Client.Common
 	[Description("These functions behavior identically to the mainmemory functions but the user can set the memory domain to read and write from. The default domain is main memory. Use getcurrentmemorydomain(), and usememorydomain() to control which domain is used. Each core has its own set of valid memory domains. Use getmemorydomainlist() to get a list of memory domains for the current core loaded.")]
 	public sealed class MemoryLuaLibrary : LuaMemoryBase
 	{
-		//private int _currentMemoryDomain; // Main memory by default probably (index 0 is currently always main memory but may never be)
-
 		private MemoryDomain _currentMemoryDomain;
 
 		public MemoryLuaLibrary(Lua lua)
@@ -19,7 +17,14 @@ namespace BizHawk.Client.Common
 		{
 			if (MemoryDomainCore != null)
 			{
-				_currentMemoryDomain = MemoryDomainCore.MainMemory;
+				if (MemoryDomainCore.HasSystemBus)
+				{
+					_currentMemoryDomain = MemoryDomainCore.SystemBus;
+				}
+				else
+				{
+					_currentMemoryDomain = MemoryDomainCore.MainMemory;
+				}
 			}
 		}
 
