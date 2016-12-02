ff10ce2cb4b475fdda3721fce4e003084b93e376
adelikat <adelikat@tasvideos.org>
2016-12-02 15:02:25 -0600

    update VersionInfo on release branch

--- Version/VersionInfo.cs
+++ Version/VersionInfo.cs
@@ -3,9 +3,9 @@ using System.IO;
 
 static class VersionInfo
 {
-	public const string MAINVERSION = "1.11.8"; // Use numbers only or the new version notification won't work
+	public const string MAINVERSION = "1.11.9"; // Use numbers only or the new version notification won't work
 
-	public static readonly string RELEASEDATE = "October 25, 2016";
+	public static readonly string RELEASEDATE = "December 2, 2016";
 	public static readonly bool DeveloperBuild = false;
 
 	public static readonly string HomePage = "http://tasvideos.org/BizHawk.html";
