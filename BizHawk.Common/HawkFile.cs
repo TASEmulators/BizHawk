6b5710ea25b920d11d01be5875e5e48574b1c71e
adelikat <adelikat@tasvideos.org>
2016-12-06 12:31:18 -0600

    HawkFile - meh, don't attempt to extract .dll files, these qualify as archives but there is no use case for attempting to load files from within them, then again, there is no reasonable use case for attempting to do so this is basically a useless commit

--- BizHawk.Common/HawkFile.cs
+++ BizHawk.Common/HawkFile.cs
@@ -189,7 +189,7 @@ namespace BizHawk.Common
 		/// <summary>
 		/// these extensions won't even be tried as archives (removes spurious archive detects since some of the signatures are pretty damn weak)
 		/// </summary>
-		public string[] NonArchiveExtensions = { ".smc", ".sfc" };
+		public string[] NonArchiveExtensions = { ".smc", ".sfc", ".dll" };
 
 		/// <summary>
 		/// Parses the given filename to create an un-opened HawkFile with some information available about its path constitution
