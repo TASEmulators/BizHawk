TASEmulators/BizHawk

commit 68a362c8ffd2ba46e414cf36a34a3898dec536d7

Fix subtle bug in renderSound
Bug as follows:

renderSound was true when any of the following:
_currAviWriter!=null&&_currAviWriter.UsesAudio
!IsTurboing || _currAviWriter!=null

So, if _currAviWriter!=null, it didn't matter if it has the UsesAudio
flag on or not.

BizHawk.Client.EmuHawk/MainForm.cs
@@ -2731,7 +2731,6 @@ namespace BizHawk.Client.EmuHawk
 			
 			float atten = 0;
 
-			var coreskipaudio = false;
 			if (runFrame || force)
 			{
 				var isFastForwarding = Global.ClientControls["Fast Forward"] || IsTurboing;
@@ -2824,19 +2823,12 @@ namespace BizHawk.Client.EmuHawk
 				
 				Global.MovieSession.HandleMovieOnFrameLoop();
 
-				coreskipaudio = IsTurboing && _currAviWriter == null;
-
 				//why not skip audio if the user doesnt want sound
-				if (!Global.Config.SoundEnabled)
-					coreskipaudio = true;
+				bool renderSound = Global.Config.SoundEnabled || !IsTurboing;
+				renderSound |= (_currAviWriter != null && _currAviWriter.UsesAudio);
 
-				{
-					bool render = !_throttle.skipnextframe;
-					bool renderSound = !coreskipaudio;
-					if (_currAviWriter != null && _currAviWriter.UsesVideo) render = true;
-					if (_currAviWriter != null && _currAviWriter.UsesAudio) renderSound = true;
-					Global.Emulator.FrameAdvance(render, renderSound);
-				}
+				bool render = !_throttle.skipnextframe || (_currAviWriter != null && _currAviWriter.UsesVideo);
+				Global.Emulator.FrameAdvance(render, renderSound);
 
 				Global.MovieSession.HandleMovieAfterFrameLoop();
 

commit 52d604cb243850600d83ebf495ed6c5aa9c416b9

Fix turbo mode not disabling sound rendering (broken by 68a362c). Apply FF&RW volume when unthrottled.

BizHawk.Client.EmuHawk/MainForm.cs
@@ -2765,7 +2765,7 @@ namespace BizHawk.Client.EmuHawk
 			if (runFrame || force)
 			{
 				var isFastForwarding = Global.ClientControls["Fast Forward"] || IsTurboing;
-				var isFastForwardingOrRewinding = isFastForwarding || isRewinding;
+				var isFastForwardingOrRewinding = isFastForwarding || isRewinding || _unthrottled;
 
 				if (isFastForwardingOrRewinding != _lastFastForwardingOrRewinding)
 				{
@@ -2825,8 +2825,9 @@ namespace BizHawk.Client.EmuHawk
 				Global.MovieSession.HandleMovieOnFrameLoop();
 
 				//why not skip audio if the user doesnt want sound
-				bool renderSound = Global.Config.SoundEnabled || !IsTurboing;
+				bool renderSound = Global.Config.SoundEnabled && !IsTurboing;
 				renderSound |= (_currAviWriter != null && _currAviWriter.UsesAudio);
+				if (!renderSound) atten = 0;
 
 				bool render = !_throttle.skipnextframe || (_currAviWriter != null && _currAviWriter.UsesVideo);
 				Emulator.FrameAdvance(render, renderSound);