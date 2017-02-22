cf68d9a85080c6a23750673a83e410c6e39c691b
Sappharad <sappharad@Mercury.local>
2017-02-21 20:33:29 -0600

    Fresh build of libgpgx, and the updated version number.

--- Version/VersionInfo.cs
+++ Version/VersionInfo.cs
@@ -3,8 +3,8 @@ using System.IO;
 
 static class VersionInfo
 {
-	public const string MAINVERSION = "1.11.8"; // Use numbers only or the new version notification won't work
-	public static readonly string RELEASEDATE = "October 22, 2016";
+	public const string MAINVERSION = "1.12.0"; // Use numbers only or the new version notification won't work
+	public static readonly string RELEASEDATE = "February 21, 2017";
 	public static readonly bool DeveloperBuild = true;
 	public static readonly string HomePage = "http://tasvideos.org/BizHawk.html";
 
