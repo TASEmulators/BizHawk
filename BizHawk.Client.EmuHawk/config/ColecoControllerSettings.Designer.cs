c96a3c9588087d2e78e0e793ea45861b2e28a2b5
alyosha-tas <alexei.f.k@gmail.com>
2017-03-01 10:48:19 +0800

    Coleco Controller

---
+++ BizHawk.Client.EmuHawk/config/ColecoControllerSettings.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace BizHawk.Client.EmuHawk
+{
+	partial class ColecoControllerSettings
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ColecoControllerSettings));
+			this.OkBtn = new System.Windows.Forms.Button();
+			this.CancelBtn = new System.Windows.Forms.Button();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.Port2ComboBox = new System.Windows.Forms.ComboBox();
+			this.Port1ComboBox = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// OkBtn
+			// 
+			this.OkBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.OkBtn.Location = new System.Drawing.Point(170, 249);
+			this.OkBtn.Name = "OkBtn";
+			this.OkBtn.Size = new System.Drawing.Size(60, 23);
+			this.OkBtn.TabIndex = 3;
+			this.OkBtn.Text = "&OK";
+			this.OkBtn.UseVisualStyleBackColor = true;
+			this.OkBtn.Click += new System.EventHandler(this.OkBtn_Click);
+			// 
+			// CancelBtn
+			// 
+			this.CancelBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.CancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.CancelBtn.Location = new System.Drawing.Point(236, 249);
+			this.CancelBtn.Name = "CancelBtn";
+			this.CancelBtn.Size = new System.Drawing.Size(60, 23);
+			this.CancelBtn.TabIndex = 4;
+			this.CancelBtn.Text = "&Cancel";
+			this.CancelBtn.UseVisualStyleBackColor = true;
+			this.CancelBtn.Click += new System.EventHandler(this.CancelBtn_Click);
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(9, 94);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(38, 13);
+			this.label5.TabIndex = 16;
+			this.label5.Text = "Port 2:";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(12, 44);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(38, 13);
+			this.label4.TabIndex = 15;
+			this.label4.Text = "Port 1:";
+			// 
+			// Port2ComboBox
+			// 
+			this.Port2ComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.Port2ComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.Port2ComboBox.FormattingEnabled = true;
+			this.Port2ComboBox.Location = new System.Drawing.Point(12, 110);
+			this.Port2ComboBox.Name = "Port2ComboBox";
+			this.Port2ComboBox.Size = new System.Drawing.Size(284, 21);
+			this.Port2ComboBox.TabIndex = 14;
+			// 
+			// Port1ComboBox
+			// 
+			this.Port1ComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.Port1ComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.Port1ComboBox.FormattingEnabled = true;
+			this.Port1ComboBox.Location = new System.Drawing.Point(12, 60);
+			this.Port1ComboBox.Name = "Port1ComboBox";
+			this.Port1ComboBox.Size = new System.Drawing.Size(284, 21);
+			this.Port1ComboBox.TabIndex = 13;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 14);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(146, 13);
+			this.label1.TabIndex = 17;
+			this.label1.Text = "ColecoVision Controller Settings";
+			// 
+			// ColecoControllerSettings
+			// 
+			this.AcceptButton = this.OkBtn;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.CancelButton = this.CancelBtn;
+			this.ClientSize = new System.Drawing.Size(308, 284);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.Port2ComboBox);
+			this.Controls.Add(this.Port1ComboBox);
+			this.Controls.Add(this.CancelBtn);
+			this.Controls.Add(this.OkBtn);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "ColecoControllerSettings";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Controller Settings";
+			this.Load += new System.EventHandler(this.ColecoControllerSettings_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button OkBtn;
+		private System.Windows.Forms.Button CancelBtn;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.ComboBox Port2ComboBox;
+		private System.Windows.Forms.ComboBox Port1ComboBox;
+		private System.Windows.Forms.Label label1;
+	}
+}
