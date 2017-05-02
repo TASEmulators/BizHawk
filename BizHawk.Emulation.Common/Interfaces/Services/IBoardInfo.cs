8795b869d7196253af144528d5cf997caf900fb5
adelikat <adelikat@tasvideos.org>
2017-05-02 08:05:59 -0500

    Englishing

--- BizHawk.Emulation.Common/Interfaces/Services/IBoardInfo.cs
+++ BizHawk.Emulation.Common/Interfaces/Services/IBoardInfo.cs
@@ -2,7 +2,7 @@
 {
 	/// <summary>
 	/// An <seealso cref="IEmulatorService"/> that returns cart/mapper/board information about the Game hardware itself, if available
-	/// Currently the board name is the only property but this could be expanded to support more detail informations
+	/// Currently the board name is the only property but this could be expanded to support more detailed information
 	/// </summary>
 	/// <seealso cref="IEmulator"/>
 	/// <seealso cref="IEmulatorService"/>
