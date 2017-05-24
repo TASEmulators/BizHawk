b2eecd7bec16a48a377a26490f6bc284493235f7
alyosha-tas <alexei.f.k@gmail.com>
2017-05-24 19:36:34 -0400

    Add files via upload

---
+++ BizHawk.Emulation.Cores/Consoles/Atari/A7800Hawk/A7800Hawk.IInputPollable.cs
@@ -0,0 +1,24 @@
+﻿using BizHawk.Emulation.Common;
+
+namespace BizHawk.Emulation.Cores.Atari.A7800Hawk
+{
+	public partial class A7800Hawk : IInputPollable
+	{
+		public int LagCount
+		{
+			get { return _lagcount; }
+			set { _lagcount = value; }
+		}
+
+		public bool IsLagFrame
+		{
+			get { return _islag; }
+			set { _islag = value; }
+		}
+
+		public IInputCallbackSystem InputCallbacks { get; } = new InputCallbackSystem();
+
+		private bool _islag = true;
+		private int _lagcount;
+	}
+}
---
+++ BizHawk.Emulation.Cores/Consoles/Atari/A7800Hawk/Maria.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using BizHawk.Emulation.Common;
+
+namespace BizHawk.Emulation.Cores.Atari.A7800Hawk
+{
+	// Emulates the Atari 7800 Maria graphics chip
+	public class Maria : IVideoProvider
+	{
+		public int _frameHz;
+
+		public int[] _vidbuffer;
+		public int[] _palette;
+
+		public int[] GetVideoBuffer()
+		{
+			return _vidbuffer;
+		}
+
+		public int VirtualWidth => 275;
+		public int VirtualHeight => BufferHeight;
+		public int BufferWidth { get; private set; }
+		public int BufferHeight { get; private set; }
+		public int BackgroundColor => unchecked((int)0xff000000);
+		public int VsyncNumerator => _frameHz;
+		public int VsyncDenominator => 1;
+
+		public void FrameAdvance()
+		{
+
+		}
+
+		public void Reset()
+		{
+
+		}
+
+	}
+}
