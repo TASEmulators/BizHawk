7f45b4880fc737b4b9a9c4cbb5f90352ccd7e536
adelikat <adelikat@tasvideos.org>
2017-05-27 14:44:45 -0500

    c64 - another savestate fix

--- BizHawk.Emulation.Cores/Computers/Commodore64/Serial/Drive1541.cs
+++ BizHawk.Emulation.Cores/Computers/Commodore64/Serial/Drive1541.cs
@@ -89,7 +89,7 @@ namespace BizHawk.Emulation.Cores.Computers.Commodore64.Serial
 			ser.Sync("SystemCpuClockNumerator", ref _cpuClockNum);
 			ser.Sync("SystemDriveCpuRatioDifference", ref _ratioDifference);
 			ser.Sync("DriveLightOffTime", ref _driveLightOffTime);
-			ser.Sync("TrackImageData", ref _trackImageData[1]);
+			ser.Sync("TrackImageData", ref _trackImageData, useNull: false);
 
 			ser.Sync("DiskDensityCounter", ref _diskDensityCounter);
 			ser.Sync("DiskSupplementaryCounter", ref _diskSupplementaryCounter);
