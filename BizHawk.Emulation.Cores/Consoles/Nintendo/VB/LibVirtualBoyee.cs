a52e9d7dc1b21b3da1eb5cf4abb15967dbc52eb8
nattthebear <goyuken@gmail.com>
2017-05-28 17:17:18 -0400

    https://www.youtube.com/watch?v=0sKRRY5tQz8

---
+++ BizHawk.Emulation.Cores/Consoles/Nintendo/VB/LibVirtualBoyee.cs
@@ -0,0 +1,92 @@
+﻿using BizHawk.Common.BizInvoke;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BizHawk.Emulation.Cores.Consoles.Nintendo.VB
+{
+	public abstract class LibVirtualBoyee
+	{
+		private const CallingConvention CC = CallingConvention.Cdecl;
+
+		[StructLayout(LayoutKind.Sequential)]
+		public struct Rect
+		{
+			public int X;
+			public int Y;
+			public int W;
+			public int H;
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		public class EmulateSpec
+		{
+			// Pitch(32-bit) must be equal to width and >= the "fb_width" specified in the MDFNGI struct for the emulated system.
+			// Height must be >= to the "fb_height" specified in the MDFNGI struct for the emulated system.
+			// The framebuffer pointed to by surface->pixels is written to by the system emulation code.
+			public IntPtr Pixels;
+
+			// Pointer to sound buffer, set by the driver code, that the emulation code should render sound to.
+			// Guaranteed to be at least 500ms in length, but emulation code really shouldn't exceed 40ms or so.  Additionally, if emulation code
+			// generates >= 100ms, 
+			// DEPRECATED: Emulation code may set this pointer to a sound buffer internal to the emulation module.
+			public IntPtr SoundBuf;
+
+			// Number of cycles that this frame consumed, using MDFNGI::MasterClock as a time base.
+			// Set by emulation code.
+			public long MasterCycles;
+
+			// Set by the system emulation code every frame, to denote the horizontal and vertical offsets of the image, and the size
+			// of the image.  If the emulated system sets the elements of LineWidths, then the width(w) of this structure
+			// is ignored while drawing the image.
+			public Rect DisplayRect;
+
+			// Maximum size of the sound buffer, in frames.  Set by the driver code.
+			public int SoundBufMaxSize;
+
+			// Number of frames currently in internal sound buffer.  Set by the system emulation code, to be read by the driver code.
+			public int SoundBufSize;
+
+			// 0 UDLR SelectStartBA UDLR(right dpad) LtrigRtrig 13
+			public Buttons Buttons;
+		}
+
+		public enum MemoryArea : int
+		{
+			Wram, Sram, Rom
+		}
+
+		public enum Buttons : int
+		{
+			Up = 0x1,
+			Down = 0x2,
+			Left = 0x4,
+			Right = 0x8,
+			Select = 0x10,
+			Start = 0x20,
+			B = 0x40,
+			A = 0x80,
+			Up_R = 0x100,
+			Down_R = 0x200,
+			Left_R = 0x400,
+			Right_R = 0x800,
+			L = 0x1000,
+			R = 0x2000
+		}
+
+		[BizImport(CC)]
+		public abstract bool Load(byte[] rom, int length);
+
+		[BizImport(CC)]
+		public abstract void GetMemoryArea(MemoryArea which, ref IntPtr ptr, ref int size);
+
+		[BizImport(CC)]
+		public abstract void Emulate(EmulateSpec espec);
+
+		[BizImport(CC)]
+		public abstract void HardReset();
+	}
+}
---
+++ BizHawk.Emulation.Cores/Consoles/Nintendo/VB/VirtualBoyee.cs
@@ -0,0 +1,135 @@
+﻿using BizHawk.Common.BizInvoke;
+using BizHawk.Emulation.Common;
+using BizHawk.Emulation.Cores.Waterbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BizHawk.Emulation.Cores.Consoles.Nintendo.VB
+{
+	public class VirtualBoyee : IEmulator, IVideoProvider
+	{
+		private PeRunner _exe;
+		private LibVirtualBoyee _boyee;
+
+		[CoreConstructor("VB")]
+		public VirtualBoyee(CoreComm comm, byte[] rom)
+		{
+			ServiceProvider = new BasicServiceProvider(this);
+			CoreComm = comm;
+
+			_exe = new PeRunner(new PeRunnerOptions
+			{
+				Path = comm.CoreFileProvider.DllPath(),
+				Filename = "vb.wbx",
+				NormalHeapSizeKB = 1024,
+				SealedHeapSizeKB = 12 * 1024,
+				InvisibleHeapSizeKB = 6 * 1024,
+				SpecialHeapSizeKB = 64
+			});
+
+			_boyee = BizInvoker.GetInvoker<LibVirtualBoyee>(_exe, _exe);
+
+			if (!_boyee.Load(rom, rom.Length))
+			{
+				throw new InvalidOperationException("Core rejected the rom");
+			}
+		}
+
+		private bool _disposed = false;
+
+		public void Dispose()
+		{
+			if (!_disposed)
+			{
+				_exe.Dispose();
+				_exe = null;
+				_disposed = true;
+			}
+		}
+
+		public IEmulatorServiceProvider ServiceProvider { get; private set; }
+
+		public unsafe void FrameAdvance(IController controller, bool render, bool rendersound = true)
+		{
+			var scratch = new short[16384];
+
+			fixed(int*vp = _videoBuffer)
+				fixed(short*sp = scratch)
+			{
+				var spec = new LibVirtualBoyee.EmulateSpec
+				{
+					Pixels = (IntPtr)vp,
+					SoundBuf = (IntPtr)sp,
+					SoundBufMaxSize = 8192
+				};
+
+				_boyee.Emulate(spec);
+				VirtualWidth = BufferWidth = spec.DisplayRect.W;
+				VirtualWidth = BufferHeight = spec.DisplayRect.H;
+				Console.WriteLine(spec.SoundBufSize);
+			}
+
+			Frame++;
+
+			/*_core.biz_set_input_callback(InputCallbacks.Count > 0 ? _inputCallback : null);
+
+			if (controller.IsPressed("Power"))
+				_core.biz_hard_reset();
+			else if (controller.IsPressed("Reset"))
+				_core.biz_soft_reset();
+
+			UpdateControls(controller);
+			Frame++;
+			LibSnes9x.frame_info frame = new LibSnes9x.frame_info();
+
+			_core.biz_run(frame, _inputState);
+			IsLagFrame = frame.padread == 0;
+			if (IsLagFrame)
+				LagCount++;
+			using (_exe.EnterExit())
+			{
+				Blit(frame);
+				Sblit(frame);
+			}*/
+		}
+
+		public int Frame { get; private set; }
+
+		public void ResetCounters()
+		{
+			Frame = 0;
+		}
+
+		public string SystemId { get { return "VB"; } }
+		public bool DeterministicEmulation { get { return true; } }
+		public CoreComm CoreComm { get; private set; }
+
+		public ControllerDefinition ControllerDefinition => NullController.Instance.Definition;
+
+		#region IVideoProvider
+
+		private int[] _videoBuffer = new int[0];
+
+		public int[] GetVideoBuffer()
+		{
+			throw new NotImplementedException();
+		}
+
+		public int VirtualWidth { get; private set; }
+		public int VirtualHeight { get; private set; }
+
+		public int BufferWidth { get; private set; }
+		public int BufferHeight { get; private set; }
+
+		public int VsyncNumerator { get; private set; }
+
+		public int VsyncDenominator { get; private set; }
+
+		public int BackgroundColor => unchecked((int)0xff000000);
+
+		#endregion
+	}
+}
