66b13a2d78cba8d0d6530f34037124120bf6fc94
alyosha-tas <alexei.f.k@gmail.com>
2017-05-29 15:05:06 -0400

    Update CartridgeDevice.cs

--- BizHawk.Emulation.Cores/Computers/Commodore64/Cartridge/CartridgeDevice.cs
+++ BizHawk.Emulation.Cores/Computers/Commodore64/Cartridge/CartridgeDevice.cs
@@ -81,6 +81,9 @@ namespace BizHawk.Emulation.Cores.Computers.Commodore64.Cartridge
 					case 0x0005:    // Ocean
 						result = new Mapper0005(chipAddress, chipBank, chipData);
 						break;
+					case 0x0007:    // Fun Play
+						result = new Mapper0007(chipData, game, exrom);
+						break;
 					case 0x0008:    // SuperGame
 						result = new Mapper0008(chipData);
 						break;
