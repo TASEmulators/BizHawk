a7ef10322eeb2b8658517c414f3dd02f79522ba2
nattthebear <goyuken@gmail.com>
2017-06-10 07:57:43 -0400

    snes:  move files to waterbox area

---
+++ waterbox/libsnes/bsnes/snes/controller/gamepad/gamepad.hpp
@@ -0,0 +1,9 @@
+struct Gamepad : Controller {
+  uint2 data();
+  void latch(bool data);
+  Gamepad(bool port);
+  void serialize(serializer& s);
+private:
+  bool latched;
+  unsigned counter;
+};
---
+++ waterbox/libsnes/bsnes/snes/controller/mouse/mouse.hpp
@@ -0,0 +1,11 @@
+struct Mouse : Controller {
+  uint2 data();
+  void latch(bool data);
+  Mouse(bool port);
+  void serialize(serializer& s);
+private:
+  bool latched;
+  unsigned counter;
+  int position_x;
+  int position_y;
+};
