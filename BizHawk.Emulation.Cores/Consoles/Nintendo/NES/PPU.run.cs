7814a9ed304d0808d1212636409ad58c29f1eb40
alyosha-tas <alexei.f.k@gmail.com>
2017-06-16 21:41:13 -0400

    NESHawk: Clean up and minor refactor
    Simplify the code base a bit and gets a performance boost as well.

--- BizHawk.Emulation.Cores/Consoles/Nintendo/NES/PPU.run.cs
+++ BizHawk.Emulation.Cores/Consoles/Nintendo/NES/PPU.run.cs
@@ -1,6 +1,4 @@
-﻿//http://nesdev.parodius.com/bbs/viewtopic.php?p=4571&sid=db4c7e35316cc5d734606dd02f11dccb
-
-//TODO - correctly emulate PPU OFF state
+﻿//TODO - correctly emulate PPU OFF state
 
 using BizHawk.Common;
 using System;
@@ -10,6 +8,7 @@ namespace BizHawk.Emulation.Cores.Nintendo.NES
 	sealed partial class PPU
 	{
 		const int kFetchTime = 2;
+		const int kLineTime = 341;
 
 		struct BGDataRecord
 		{
@@ -78,7 +77,9 @@ namespace BizHawk.Emulation.Cores.Nintendo.NES
 				if (reg_2001.color_disable)
 					pixelcolor_latch_2 &= 0x30;
 
-				xbuf[(target - 2)] = PaletteAdjustPixel(pixelcolor_latch_2);
+				//TODO - check flashing sirens in werewolf
+				//tack on the deemph bits. THESE MAY BE ORDERED WRONG. PLEASE CHECK IN THE PALETTE CODE
+				xbuf[(target - 2)] = (short)(pixelcolor_latch_2 | reg_2001.intensity_lsl_6);
 			}
 
 			if (row_check >= 1)
@@ -158,14 +159,6 @@ namespace BizHawk.Emulation.Cores.Nintendo.NES
 			} //switch(cycle)
 		}
 
-		//TODO - check flashing sirens in werewolf
-		short PaletteAdjustPixel(int pixel)
-		{
-			//tack on the deemph bits. THESE MAY BE ORDERED WRONG. PLEASE CHECK IN THE PALETTE CODE
-			return (short)(pixel | reg_2001.intensity_lsl_6);
-		}
-
-		const int kLineTime = 341;
 		public unsafe void FrameAdvance()
 		{
 			BGDataRecord* bgdata = stackalloc BGDataRecord[34]; //one at the end is junk, it can never be rendered
@@ -191,12 +184,13 @@ namespace BizHawk.Emulation.Cores.Nintendo.NES
 			runppu(3);
 			bool nmi_destiny = reg_2000.vblank_nmi_gen && Reg2002_vblank_active;
 			runppu(3);
-			if (nmi_destiny) TriggerNMI();
+			if (nmi_destiny) nes.cpu.NMI = true;
 			nes.Board.AtVsyncNMI();
 			runppu(postNMIlines * kLineTime - delay);
 
-			//this seems to run just before the dummy scanline begins
-			clear_2002();
+			//this seems to happen just before the dummy scanline begins
+			Reg2002_objhit = Reg2002_objoverflow = 0;
+			Reg2002_vblank_clear_pending = true;
 
 			idleSynch ^= true;
 
@@ -224,8 +218,8 @@ namespace BizHawk.Emulation.Cores.Nintendo.NES
 				yp = sl - 1;
 				ppuphase = PPUPHASE.BG;
 
-				// "If PPUADDR is not less then 8 when rendering starts, the first 8 fights in OAM and written to from 
-				// the current location off PPUADDR"
+				// "If PPUADDR is not less then 8 when rendering starts, the first 8 bytes in OAM and written to from 
+				// the current location off PPUADDR"			
 				if (sl == 0 && PPUON && reg_2003 >= 8 && region==Region.NTSC)
 				{
 					for (int i = 0; i < 8; i++)
@@ -233,7 +227,7 @@ namespace BizHawk.Emulation.Cores.Nintendo.NES
 						OAM[i] = OAM[reg_2003 & 0xF8 + i];
 					}
 				}
-
+				
 				if (NTViewCallback != null && yp == NTViewCallback.Scanline) NTViewCallback.Callback();
 				if (PPUViewCallback != null && yp == PPUViewCallback.Scanline) PPUViewCallback.Callback();
 
@@ -255,7 +249,8 @@ namespace BizHawk.Emulation.Cores.Nintendo.NES
 
 						//check all the conditions that can cause things to render in these 8px
 						bool renderspritenow = show_obj_new && (xt > 0 || reg_2001.show_obj_leftmost);
-						bool renderbgnow; 
+						bool renderbgnow;
+						bool hit_pending = false;
 
 						for (int xp = 0; xp < 8; xp++, rasterpos++)
 						{
@@ -350,7 +345,8 @@ namespace BizHawk.Emulation.Cores.Nintendo.NES
 									{
 										if (yp >= read_value && yp < read_value + spriteHeight && PPUON)
 										{
-											Reg2002_objoverflow = true;
+											hit_pending = true;
+											//Reg2002_objoverflow = true;
 										}
 
 										if (yp >= read_value && yp < read_value + spriteHeight && spr_true_count == 0)
@@ -399,6 +395,12 @@ namespace BizHawk.Emulation.Cores.Nintendo.NES
 								Read_bgdata(xp, ref bgdata[xt + 2]);
 							else
 								runppu(1);
+
+							if (hit_pending)
+							{
+								hit_pending = false;
+								Reg2002_objoverflow = true;
+							}
 
 							renderbgnow =  show_bg_new && (xt > 0 || reg_2001.show_bg_leftmost);
 							//bg pos is different from raster pos due to its offsetability.
