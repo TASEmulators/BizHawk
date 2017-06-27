TASEmulators/BizHawk

commit 43cb50d5c3bc21fb5cc39d51aaedb059f8583d98

Misc cleanups in Client.Common

BizHawk.Client.Common/QuickBmpFile.cs
@@ -5,6 +5,10 @@ using System.Runtime.InteropServices;
 using BizHawk.Common;
 using BizHawk.Emulation.Common;
 
+// ReSharper disable InconsistentNaming
+// ReSharper disable StyleCop.SA1304
+// ReSharper disable StyleCop.SA1307
+// ReSharper disable StyleCop.SA1401
 namespace BizHawk.Client.Common
 {
 	public class QuickBmpFile
@@ -12,23 +16,24 @@ namespace BizHawk.Client.Common
 		#region Structs
 
 		[StructLayout(LayoutKind.Sequential, Pack = 1)]
-		class BITMAPFILEHEADER
+		private class Bitmapfileheader
 		{
+			public readonly uint bfSize;
 			public ushort bfType;
-			public uint bfSize;
+			
 			public ushort bfReserved1;
 			public ushort bfReserved2;
 			public uint bfOffBits;
 
-			public BITMAPFILEHEADER()
+			public Bitmapfileheader()
 			{
 				bfSize = (uint)Marshal.SizeOf(this);
 			}
 
-			public static BITMAPFILEHEADER FromStream(Stream s)
+			public static Bitmapfileheader FromStream(Stream s)
 			{
-				var ret = GetObject<BITMAPFILEHEADER>(s);
-				if (ret.bfSize != Marshal.SizeOf(typeof(BITMAPFILEHEADER)))
+				var ret = GetObject<Bitmapfileheader>(s);
+				if (ret.bfSize != Marshal.SizeOf(typeof(Bitmapfileheader)))
 				{
 					throw new InvalidOperationException();
 				}
@@ -38,9 +43,9 @@ namespace BizHawk.Client.Common
 		}
 
 		[StructLayout(LayoutKind.Sequential)]
-		class BITMAPINFOHEADER
+		private class Bitmapinfoheader
 		{
-			public uint biSize;
+			public readonly uint biSize;
 			public int biWidth;
 			public int biHeight;
 			public ushort biPlanes;
@@ -52,15 +57,15 @@ namespace BizHawk.Client.Common
 			public uint biClrUsed;
 			public uint biClrImportant;
 
-			public BITMAPINFOHEADER()
+			public Bitmapinfoheader()
 			{
 				biSize = (uint)Marshal.SizeOf(this);
 			}
 
-			public static BITMAPINFOHEADER FromStream(Stream s)
+			public static Bitmapinfoheader FromStream(Stream s)
 			{
-				var ret = GetObject<BITMAPINFOHEADER>(s);
-				if (ret.biSize != Marshal.SizeOf(typeof(BITMAPINFOHEADER)))
+				var ret = GetObject<Bitmapinfoheader>(s);
+				if (ret.biSize != Marshal.SizeOf(typeof(Bitmapinfoheader)))
 				{
 					throw new InvalidOperationException();
 				}
@@ -141,18 +146,18 @@ namespace BizHawk.Client.Common
 		{
 			int w = dst.Width;
 			int h = dst.Height;
-			int in_w = src.Width;
-			int in_h = src.Height;
+			int inW = src.Width;
+			int inH = src.Height;
 			int* sp = src.Data;
 			int* dp = dst.Data;
 
 			// vflip along the way
 			for (int j = h - 1; j >= 0; j--)
 			{
-				sp = src.Data + in_w * (j * in_h / h);
+				sp = src.Data + (inW * (j * inH / h));
 				for (int i = 0; i < w; i++)
 				{
-					dp[i] = sp[i * in_w / w];
+					dp[i] = sp[i * inW / w];
 				}
 
 				dp += w;
@@ -163,17 +168,17 @@ namespace BizHawk.Client.Common
 		{
 			int w = dst.Width;
 			int h = dst.Height;
-			int in_w = src.Width;
-			int in_h = src.Height;
+			int inW = src.Width;
+			int inH = src.Height;
 			int* sp = src.Data;
 			int* dp = dst.Data;
 
 			for (int j = 0; j < h; j++)
 			{
-				sp = src.Data + in_w * (j * in_h / h);
+				sp = src.Data + (inW * (j * inH / h));
 				for (int i = 0; i < w; i++)
 				{
-					dp[i] = sp[i * in_w / w];
+					dp[i] = sp[i * inW / w];
 				}
 
 				dp += w;
@@ -239,8 +244,8 @@ namespace BizHawk.Client.Common
 
 		public static unsafe bool Load(IVideoProvider v, Stream s)
 		{
-			var bf = BITMAPFILEHEADER.FromStream(s);
-			var bi = BITMAPINFOHEADER.FromStream(s);
+			var bf = Bitmapfileheader.FromStream(s);
+			var bi = Bitmapinfoheader.FromStream(s);
 			if (bf.bfType != 0x4d42
 				|| bf.bfOffBits != bf.bfSize + bi.biSize
 				|| bi.biPlanes != 1
@@ -250,17 +255,17 @@ namespace BizHawk.Client.Common
 				return false;
 			}
 
-			int in_w = bi.biWidth;
-			int in_h = bi.biHeight;
+			int inW = bi.biWidth;
+			int inH = bi.biHeight;
 
-			byte[] src = new byte[in_w * in_h * 4];
+			byte[] src = new byte[inW * inH * 4];
 			s.Read(src, 0, src.Length);
 			if (v is LoadedBMP)
 			{
 				var l = v as LoadedBMP;
-				l.BufferWidth = in_w;
-				l.BufferHeight = in_h;
-				l.VideoBuffer = new int[in_w * in_h];
+				l.BufferWidth = inW;
+				l.BufferHeight = inH;
+				l.VideoBuffer = new int[inW * inH];
 			}
 
 			int[] dst = v.GetVideoBuffer();
@@ -273,8 +278,8 @@ namespace BizHawk.Client.Common
 					Blit(new BMP
 					{
 						Data = (int*)srcp,
-						Width = in_w,
-						Height = in_h
+						Width = inW,
+						Height = inH
 					},
 					new BMP
 					{
@@ -290,8 +295,8 @@ namespace BizHawk.Client.Common
 
 		public static unsafe void Save(IVideoProvider v, Stream s, int w, int h)
 		{
-			var bf = new BITMAPFILEHEADER();
-			var bi = new BITMAPINFOHEADER();
+			var bf = new Bitmapfileheader();
+			var bi = new Bitmapinfoheader();
 			bf.bfType = 0x4d42;
 			bf.bfOffBits = bf.bfSize + bi.biSize;
 

BizHawk.Client.Common/BinarySaveStates.cs
@@ -86,8 +86,8 @@ namespace BizHawk.Client.Common
 			}
 		}
 
-		public virtual string ReadName => Name;
-		public virtual string WriteName => Ext != null ? Name + '.' + Ext : Name;
+		public string ReadName => Name;
+		public string WriteName => Ext != null ? Name + '.' + Ext : Name;
 
 		public string Name { get; protected set; }
 		public string Ext { get; protected set; }
@@ -203,7 +203,7 @@ namespace BizHawk.Client.Common
 			}
 		}
 
-		private static byte[] zipheader = { 0x50, 0x4b, 0x03, 0x04 };
+		private static readonly byte[] Zipheader = { 0x50, 0x4b, 0x03, 0x04 };
 		public static BinaryStateLoader LoadAndDetect(string filename, bool isMovieLoad = false)
 		{
 			var ret = new BinaryStateLoader();
@@ -212,7 +212,7 @@ namespace BizHawk.Client.Common
 			{
 				byte[] data = new byte[4];
 				fs.Read(data, 0, 4);
-				if (!data.SequenceEqual(zipheader))
+				if (!data.SequenceEqual(Zipheader))
 				{
 					return null;
 				}

BizHawk.Client.Common/ControllerBinding.cs
@@ -100,7 +100,7 @@ namespace BizHawk.Client.Common
 					}
 
 					// zero 09-mar-2015 - not sure if adding + 1 here is correct.. but... maybe?
-					var output = (input * multiplier + 10000.0f) * (range.Max - range.Min + 1) / 20000.0f + range.Min;
+					var output = (((input * multiplier) + 10000.0f) * (range.Max - range.Min + 1) / 20000.0f) + range.Min;
 
 					// zero 09-mar-2015 - at this point, we should only have integers, since thats all 100% of consoles ever see
 					// if this becomes a problem we can add flags to the range and update GUIs to be able to display floats

BizHawk.Client.Common/FirmwareManager.cs
@@ -96,8 +96,9 @@ namespace BizHawk.Client.Common
 			return resolved.FilePath;
 		}
 
-		public class RealFirmwareReader : IDisposable
+		private class RealFirmwareReader : IDisposable
 		{
+			private readonly List<RealFirmwareFile> _files = new List<RealFirmwareFile>();
 			private SHA1 _sha1 = SHA1.Create();
 
 			public void Dispose()
@@ -117,13 +118,12 @@ namespace BizHawk.Client.Common
 				}
 
 				rff.Hash = _sha1.Hash.BytesToHexString();
-				dict[rff.Hash] = rff;
+				Dict[rff.Hash] = rff;
 				_files.Add(rff);
 				return rff;
 			}
 
-			public readonly Dictionary<string, RealFirmwareFile> dict = new Dictionary<string, RealFirmwareFile>();
-			private readonly List<RealFirmwareFile> _files = new List<RealFirmwareFile>();
+			public Dictionary<string, RealFirmwareFile> Dict { get; } = new Dictionary<string, RealFirmwareFile>();
 		}
 
 		public void DoScanAndResolve()
@@ -184,12 +184,12 @@ namespace BizHawk.Client.Common
 						var hash = fo.Hash;
 
 						// did we find this firmware?
-						if (reader.dict.ContainsKey(hash))
+						if (reader.Dict.ContainsKey(hash))
 						{
 							// rad! then we can use it
 							var ri = new ResolutionInfo
 								{
-									FilePath = reader.dict[hash].FileInfo.FullName,
+									FilePath = reader.Dict[hash].FileInfo.FullName,
 									KnownFirmwareFile = FirmwareDatabase.FirmwareFilesByHash[hash],
 									Hash = hash,
 									Size = fo.Size

BizHawk.Client.Common/Global.cs
@@ -31,9 +31,9 @@ namespace BizHawk.Client.Common
 		public static AutofireController AutofireNullControls;
 
 		// the movie will be spliced inbetween these if it is present
-		public static CopyControllerAdapter MovieInputSourceAdapter = new CopyControllerAdapter();
-		public static CopyControllerAdapter MovieOutputHardpoint = new CopyControllerAdapter();
-		public static MultitrackRewiringControllerAdapter MultitrackRewiringAdapter = new MultitrackRewiringControllerAdapter();
+		public static readonly CopyControllerAdapter MovieInputSourceAdapter = new CopyControllerAdapter();
+		public static readonly CopyControllerAdapter MovieOutputHardpoint = new CopyControllerAdapter();
+		public static readonly MultitrackRewiringControllerAdapter MultitrackRewiringAdapter = new MultitrackRewiringControllerAdapter();
 
 		// dont take my word for it, since the final word is actually in RewireInputChain, but here is a guide...
 		// user -> Input -> ActiveController -> UDLR -> StickyXORPlayerInputAdapter -> TurboAdapter(TBD) -> Lua(?TBD?) -> ..
@@ -47,26 +47,26 @@ namespace BizHawk.Client.Common
 		public static AutofireController AutoFireController;
 
 		// the "output" port for the controller chain.
-		public static CopyControllerAdapter ControllerOutput = new CopyControllerAdapter();
+		public static readonly CopyControllerAdapter ControllerOutput = new CopyControllerAdapter();
 
-		public static UD_LR_ControllerAdapter UD_LR_ControllerAdapter = new UD_LR_ControllerAdapter();
+		public static readonly UD_LR_ControllerAdapter UD_LR_ControllerAdapter = new UD_LR_ControllerAdapter();
 
-		public static AutoFireStickyXorAdapter AutofireStickyXORAdapter = new AutoFireStickyXorAdapter();
+		public static readonly AutoFireStickyXorAdapter AutofireStickyXORAdapter = new AutoFireStickyXorAdapter();
 
 		/// <summary>
 		/// provides an opportunity to mutate the player's input in an autohold style
 		/// </summary>
-		public static StickyXorAdapter StickyXORAdapter = new StickyXorAdapter();
+		public static readonly StickyXorAdapter StickyXORAdapter = new StickyXorAdapter();
 
 		/// <summary>
 		/// Used to AND to another controller, used for Joypad.Set()
 		/// </summary>
-		public static OverrideAdaptor LuaAndAdaptor = new OverrideAdaptor();
+		public static readonly OverrideAdaptor LuaAndAdaptor = new OverrideAdaptor();
 
 		/// <summary>
 		/// fire off one-frame logical button clicks here. useful for things like ti-83 virtual pad and reset buttons
 		/// </summary>
-		public static ClickyVirtualPadController ClickyVirtualPadController = new ClickyVirtualPadController();
+		public static readonly ClickyVirtualPadController ClickyVirtualPadController = new ClickyVirtualPadController();
 
 		public static SimpleController MovieOutputController = new SimpleController();
 

BizHawk.Client.Common/SavestateManager.cs
@@ -62,7 +62,7 @@ namespace BizHawk.Client.Common
 
 						using (new SimpleTime("Save Framebuffer"))
 						{
-							bs.PutLump(BinaryStateLump.Framebuffer, (s) => QuickBmpFile.Save(Global.Emulator.AsVideoProvider(), s, out_w, out_h));
+							bs.PutLump(BinaryStateLump.Framebuffer, s => QuickBmpFile.Save(Global.Emulator.AsVideoProvider(), s, out_w, out_h));
 						}
 					}
 				}

BizHawk.Client.Common/SevenZipWriter.cs
@@ -10,19 +10,20 @@ namespace BizHawk.Client.Common
 	{
 		private class RangBuffer
 		{
-			const int LEN = 4096;
-			const int MASK = 4095;
-			byte[] buff = new byte[LEN];
+			private const int Len = 4096;
+			private const int Mask = 4095;
 
-			int wpos = 0;
-			int rpos = 0;
+			private readonly byte[] _buff = new byte[Len];
 
-			bool writeclosed;
-			bool readclosed;
+			private readonly object _sharedlock = new object();
+			private readonly ManualResetEvent _full = new ManualResetEvent(true);
+			private readonly ManualResetEvent _empty = new ManualResetEvent(false);
 
-			object sharedlock = new object();
-			ManualResetEvent full = new ManualResetEvent(true);
-			ManualResetEvent empty = new ManualResetEvent(false);
+			private int _wpos;
+			private int _rpos;
+
+			private bool _writeclosed;
+			private bool _readclosed;
 
 			public Stream W { get; }
 			public Stream R { get; }
@@ -38,23 +39,23 @@ namespace BizHawk.Client.Common
 				// slow, but faster than using the other overload with byte[1]
 				while (true)
 				{
-					empty.WaitOne();
-					lock (sharedlock)
+					_empty.WaitOne();
+					lock (_sharedlock)
 					{
-						if (rpos != wpos)
+						if (_rpos != _wpos)
 						{
-							byte ret = buff[rpos++];
-							rpos &= MASK;
-							full.Set();
+							byte ret = _buff[_rpos++];
+							_rpos &= Mask;
+							_full.Set();
 							return ret;
 						}
-						else if (writeclosed)
+						else if (_writeclosed)
 						{
 							return -1;
 						}
 						else
 						{
-							empty.Reset();
+							_empty.Reset();
 						}
 					}
 				}
@@ -65,14 +66,14 @@ namespace BizHawk.Client.Common
 				int ret = 0;
 				while (count > 0)
 				{
-					empty.WaitOne();
-					lock (sharedlock)
+					_empty.WaitOne();
+					lock (_sharedlock)
 					{
-						int start = rpos;
-						int end = wpos;
+						int start = _rpos;
+						int end = _wpos;
 						if (end < start) // wrap
 						{
-							end = LEN;
+							end = Len;
 						}
 
 						if (end - start > count)
@@ -83,20 +84,20 @@ namespace BizHawk.Client.Common
 						int c = end - start;
 						if (c > 0)
 						{
-							Buffer.BlockCopy(buff, start, buffer, offset, c);
+							Buffer.BlockCopy(_buff, start, buffer, offset, c);
 							count -= c;
 							ret += c;
 							offset += c;
-							rpos = end & MASK;
-							full.Set();
+							_rpos = end & Mask;
+							_full.Set();
 						}
-						else if (writeclosed)
+						else if (_writeclosed)
 						{
 							break;
 						}
 						else
 						{
-							empty.Reset();
+							_empty.Reset();
 						}
 					}
 				}
@@ -106,10 +107,10 @@ namespace BizHawk.Client.Common
 
 			public void CloseRead()
 			{
-				lock (sharedlock)
+				lock (_sharedlock)
 				{
-					readclosed = true;
-					full.Set();
+					_readclosed = true;
+					_full.Set();
 				}
 			}
 
@@ -117,25 +118,24 @@ namespace BizHawk.Client.Common
 			{
 				while (true)
 				{
-					full.WaitOne();
-					lock (sharedlock)
+					_full.WaitOne();
+					lock (_sharedlock)
 					{
-						int next = (wpos + 1) & MASK;
-						if (next != rpos)
+						int next = (_wpos + 1) & Mask;
+						if (next != _rpos)
 						{
-							buff[wpos] = value;
-							wpos = next;
-							empty.Set();
+							_buff[_wpos] = value;
+							_wpos = next;
+							_empty.Set();
 							return true;
 						}
-						else if (readclosed)
+
+						if (_readclosed)
 						{
 							return false;
 						}
-						else
-						{
-							full.Reset();
-						}
+
+						_full.Reset();
 					}
 				}
 			}
@@ -145,14 +145,14 @@ namespace BizHawk.Client.Common
 				int ret = 0;
 				while (count > 0)
 				{
-					full.WaitOne();
-					lock (sharedlock)
+					_full.WaitOne();
+					lock (_sharedlock)
 					{
-						int start = wpos;
-						int end = (rpos - 1) & MASK;
+						int start = _wpos;
+						int end = (_rpos - 1) & Mask;
 						if (end < start) // wrap
 						{
-							end = LEN;
+							end = Len;
 						}
 
 						if (end - start > count)
@@ -163,20 +163,20 @@ namespace BizHawk.Client.Common
 						int c = end - start;
 						if (c > 0)
 						{
-							Buffer.BlockCopy(buffer, offset, buff, start, c);
+							Buffer.BlockCopy(buffer, offset, _buff, start, c);
 							count -= c;
 							ret += c;
 							offset += c;
-							wpos = end & MASK;
-							empty.Set();
+							_wpos = end & Mask;
+							_empty.Set();
 						}
-						else if (readclosed)
+						else if (_readclosed)
 						{
 							break;
 						}
 						else
 						{
-							full.Reset();
+							_full.Reset();
 						}
 					}
 				}
@@ -186,10 +186,10 @@ namespace BizHawk.Client.Common
 
 			public void CloseWrite()
 			{
-				lock (sharedlock)
+				lock (_sharedlock)
 				{
-					writeclosed = true;
-					empty.Set();
+					_writeclosed = true;
+					_empty.Set();
 				}
 			}
 
@@ -262,11 +262,12 @@ namespace BizHawk.Client.Common
 
 			private class RStream : Stream
 			{
-				public override bool CanRead { get { return true; } }
-				public override bool CanSeek { get { return false; } }
-				public override bool CanWrite { get { return false; } }
+				public override bool CanRead => true;
+				public override bool CanSeek => false;
+				public override bool CanWrite => false;
+
 				public override void Flush() { }
-				public override long Length { get { return 1; } } // { get { throw new NotSupportedException(); } }
+				public override long Length => 1; // { get { throw new NotSupportedException(); } }
 				public override long Seek(long offset, SeekOrigin origin) { return 0; } // { throw new NotSupportedException(); }
 				public override void SetLength(long value) { throw new NotSupportedException(); }
 				public override long Position
@@ -328,43 +329,43 @@ namespace BizHawk.Client.Common
 			}
 		}
 
-		private SevenZip.SevenZipCompressor svc;
+		private readonly SevenZip.SevenZipCompressor _svc;
+		private readonly string _path;
 
-		private bool first = true;
-		private string path;
-		private int compressionlevel;
+		private bool _first = true;
+		private int _compressionlevel;
 
 		public SevenZipWriter(string path, int compressionlevel)
 		{
-			this.path = path;
-			this.compressionlevel = compressionlevel;
+			_path = path;
+			_compressionlevel = compressionlevel;
 
-			svc = new SevenZip.SevenZipCompressor { ArchiveFormat = SevenZip.OutArchiveFormat.Zip };
+			_svc = new SevenZip.SevenZipCompressor { ArchiveFormat = SevenZip.OutArchiveFormat.Zip };
 
 			switch (compressionlevel)
 			{
 				default:
 				case 0:
-					svc.CompressionLevel = SevenZip.CompressionLevel.None;
+					_svc.CompressionLevel = SevenZip.CompressionLevel.None;
 					break;
 				case 1:
 				case 2:
-					svc.CompressionLevel = SevenZip.CompressionLevel.Fast;
+					_svc.CompressionLevel = SevenZip.CompressionLevel.Fast;
 					break;
 				case 3:
 				case 4:
-					svc.CompressionLevel = SevenZip.CompressionLevel.Low;
+					_svc.CompressionLevel = SevenZip.CompressionLevel.Low;
 					break;
 				case 5:
 				case 6:
-					svc.CompressionLevel = SevenZip.CompressionLevel.Normal;
+					_svc.CompressionLevel = SevenZip.CompressionLevel.Normal;
 					break;
 				case 7:
 				case 8:
-					svc.CompressionLevel = SevenZip.CompressionLevel.High;
+					_svc.CompressionLevel = SevenZip.CompressionLevel.High;
 					break;
 				case 9:
-					svc.CompressionLevel = SevenZip.CompressionLevel.Ultra;
+					_svc.CompressionLevel = SevenZip.CompressionLevel.Ultra;
 					break;
 			}
 		}
@@ -374,19 +375,19 @@ namespace BizHawk.Client.Common
 			var dict = new Dictionary<string, Stream>();
 			var r = new RangBuffer();
 			dict[name] = r.R;
-			if (first)
+			if (_first)
 			{
-				first = false;
-				svc.CompressionMode = SevenZip.CompressionMode.Create;
+				_first = false;
+				_svc.CompressionMode = SevenZip.CompressionMode.Create;
 			}
 			else
 			{
-				svc.CompressionMode = SevenZip.CompressionMode.Append;
+				_svc.CompressionMode = SevenZip.CompressionMode.Append;
 			}
 
 			var task = Task.Factory.StartNew(() =>
 				{
-					svc.CompressStreamDictionary(dict, path);
+					_svc.CompressStreamDictionary(dict, _path);
 				});
 			try
 			{

BizHawk.Client.Common/SystemInfo.cs
@@ -206,7 +206,7 @@ namespace BizHawk.Client.Common
 		}
 
 		/// <summary>
-		/// Gets the haschode for current insance
+		/// Gets the haschode for current instance
 		/// </summary>
 		/// <returns>This instance hashcode</returns>
 		public override int GetHashCode()
@@ -241,7 +241,7 @@ namespace BizHawk.Client.Common
 		/// </summary>
 		/// <param name="system1">First <see cref="SystemInfo"/></param>
 		/// <param name="system2">Second <see cref="SystemInfo"/></param>
-		/// <returns>True if both system are diferent; otherwise, false</returns>
+		/// <returns>True if both system are different; otherwise, false</returns>
 		public static bool operator !=(SystemInfo system1, SystemInfo system2)
 		{
 			return !(system1 == system2);

commit 00968423a051324c5c069a783ed48d1a4a1b639a

fix crash loading tasproj with bitmap thumbnails caused by 43cb50d5c3bc21fb5cc39d51aaedb059f8583d98

BizHawk.Client.Common/QuickBmpFile.cs
@@ -16,24 +16,23 @@ namespace BizHawk.Client.Common
 		#region Structs
 
 		[StructLayout(LayoutKind.Sequential, Pack = 1)]
-		private class Bitmapfileheader
+		private class BITMAPFILEHEADER
 		{
-			public readonly uint bfSize;
 			public ushort bfType;
-			
+			public uint bfSize;
 			public ushort bfReserved1;
 			public ushort bfReserved2;
 			public uint bfOffBits;
 
-			public Bitmapfileheader()
+			public BITMAPFILEHEADER()
 			{
 				bfSize = (uint)Marshal.SizeOf(this);
 			}
 
-			public static Bitmapfileheader FromStream(Stream s)
+			public static BITMAPFILEHEADER FromStream(Stream s)
 			{
-				var ret = GetObject<Bitmapfileheader>(s);
-				if (ret.bfSize != Marshal.SizeOf(typeof(Bitmapfileheader)))
+				var ret = GetObject<BITMAPFILEHEADER>(s);
+				if (ret.bfSize != Marshal.SizeOf(typeof(BITMAPFILEHEADER)))
 				{
 					throw new InvalidOperationException();
 				}
@@ -42,10 +41,11 @@ namespace BizHawk.Client.Common
 			}
 		}
 
-		[StructLayout(LayoutKind.Sequential)]
-		private class Bitmapinfoheader
+		[StructLayout(LayoutKind.Sequential, Pack = 1)]
+		private class BITMAPINFOHEADER
 		{
-			public readonly uint biSize;
+			public uint biSize;
+
 			public int biWidth;
 			public int biHeight;
 			public ushort biPlanes;
@@ -57,15 +57,15 @@ namespace BizHawk.Client.Common
 			public uint biClrUsed;
 			public uint biClrImportant;
 
-			public Bitmapinfoheader()
+			public BITMAPINFOHEADER()
 			{
 				biSize = (uint)Marshal.SizeOf(this);
 			}
 
-			public static Bitmapinfoheader FromStream(Stream s)
+			public static BITMAPINFOHEADER FromStream(Stream s)
 			{
-				var ret = GetObject<Bitmapinfoheader>(s);
-				if (ret.biSize != Marshal.SizeOf(typeof(Bitmapinfoheader)))
+				var ret = GetObject<BITMAPINFOHEADER>(s);
+				if (ret.biSize != Marshal.SizeOf(typeof(BITMAPINFOHEADER)))
 				{
 					throw new InvalidOperationException();
 				}
@@ -244,8 +244,8 @@ namespace BizHawk.Client.Common
 
 		public static unsafe bool Load(IVideoProvider v, Stream s)
 		{
-			var bf = Bitmapfileheader.FromStream(s);
-			var bi = Bitmapinfoheader.FromStream(s);
+			var bf = BITMAPFILEHEADER.FromStream(s);
+			var bi = BITMAPINFOHEADER.FromStream(s);
 			if (bf.bfType != 0x4d42
 				|| bf.bfOffBits != bf.bfSize + bi.biSize
 				|| bi.biPlanes != 1
@@ -295,8 +295,8 @@ namespace BizHawk.Client.Common
 
 		public static unsafe void Save(IVideoProvider v, Stream s, int w, int h)
 		{
-			var bf = new Bitmapfileheader();
-			var bi = new Bitmapinfoheader();
+			var bf = new BITMAPFILEHEADER();
+			var bi = new BITMAPINFOHEADER();
 			bf.bfType = 0x4d42;
 			bf.bfOffBits = bf.bfSize + bi.biSize;
 