cc33617f746b087d61b18dcc53a9c0295bbff856
zeromus <zeromus@users.noreply.github.com>
2017-07-06 20:48:46 -0500

    fix crash in snes virtualpad when mouse is used

--- BizHawk.Client.EmuHawk/tools/VirtualPads/VirtualPad.cs
+++ BizHawk.Client.EmuHawk/tools/VirtualPads/VirtualPad.cs
@@ -84,7 +84,7 @@ namespace BizHawk.Client.EmuHawk
 						PadBox.Controls.Add(new VirtualPadAnalogStick
 						{
 							Name = button.Name,
-							SecondaryName = button.SecondaryNames.Any() ? button.SecondaryNames[0] : "",
+							SecondaryName = (button.SecondaryNames != null && button.SecondaryNames.Any()) ? button.SecondaryNames[0] : "",
 							Location = UIHelper.Scale(button.Location),
 							Size = UIHelper.Scale(new Size(180 + 79, 200 + 9)),
 							RangeX = new float[] { button.MinValue, button.MidValue, button.MaxValue },
