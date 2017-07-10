0f686a0de17f221d003e71c497f6f2a08ab22778
zeromus <zeromus@users.noreply.github.com>
2017-07-10 12:58:52 -0500

    * revert strict string checking behaviour from nlua Revision 10f4d0ce78a2a3cb9b727ac55c8705385f0ded7e (we dont want it, probably)
    * do a better job porting old commits from bizhawk (should fix memory leaks, optional arguments)

--- BizHawk.Client.EmuHawk/tools/Lua/Libraries/EmuLuaLibrary.cs
+++ BizHawk.Client.EmuHawk/tools/Lua/Libraries/EmuLuaLibrary.cs
@@ -199,10 +199,10 @@ namespace BizHawk.Client.EmuHawk
 
 				var execResult = script.Resume(0);
 
-				//_lua.RunScheduledDisposes();
+				_lua.RunScheduledDisposes();
 
 				// not sure how this is going to work out, so do this too
-				//script.RunScheduledDisposes();
+				script.RunScheduledDisposes();
 
 				_currThread = null;
 				var result = new ResumeResult();
