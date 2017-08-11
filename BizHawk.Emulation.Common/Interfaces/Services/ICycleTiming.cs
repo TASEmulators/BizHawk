5b97e5c7f63fadda8ee075a760643c1767485a27
nattthebear <goyuken@gmail.com>
2017-08-11 19:42:53 -0400

    Add ICycleTiming interface plus one implementation so I can feel like I did something

---
+++ BizHawk.Emulation.Common/Interfaces/Services/ICycleTiming.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BizHawk.Emulation.Common
+{
+	public interface ICycleTiming
+	{
+		/// <summary>
+		/// Total elapsed emulation time relative to <see cref="ClockRate"/>
+		/// </summary>
+		long CycleCount { get; }
+		/// <summary>
+		/// Clock Rate in hz for <see cref="CycleCount"/>
+		/// </summary>
+		double ClockRate { get; }
+	}
+}
