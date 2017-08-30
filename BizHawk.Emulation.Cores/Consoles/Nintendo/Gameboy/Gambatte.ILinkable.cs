56cdf329298a034a1604abe1f1c5c041d9d8b69f
Tastyfish <crazychris32@gmail.com>
2017-08-30 15:07:17 -0400

    Make Gambatte show link status for printer connection

---
+++ BizHawk.Emulation.Cores/Consoles/Nintendo/Gameboy/Gambatte.ILinkable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BizHawk.Emulation.Common;
+
+namespace BizHawk.Emulation.Cores.Nintendo.Gameboy
+{
+	public partial class Gameboy : ILinkable
+	{
+		public bool LinkConnected { get; private set; }
+	}
+}
