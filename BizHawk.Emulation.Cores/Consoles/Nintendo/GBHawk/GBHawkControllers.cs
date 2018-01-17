956518bfc1275e6fef36d29a0dd565922dec064c
alyosha-tas <alexei.f.k@gmail.com>
2018-01-17 09:41:59 -0500

    GBHawk: update controller

--- BizHawk.Emulation.Cores/Consoles/Nintendo/GBHawk/GBHawkControllers.cs
+++ BizHawk.Emulation.Cores/Consoles/Nintendo/GBHawk/GBHawkControllers.cs
@@ -57,7 +57,7 @@ namespace BizHawk.Emulation.Cores.Nintendo.GBHawk
 
 		private static readonly string[] BaseDefinition =
 		{
-			"Right", "Left", "Up", "Down", "A", "B", "Select", "Start", "Power"
+			"Up", "Down", "Left", "Right", "Start", "Select", "B", "A", "Power"
 		};
 
 		public void SyncState(Serializer ser)
