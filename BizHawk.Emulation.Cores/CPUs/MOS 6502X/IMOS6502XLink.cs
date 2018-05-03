45d444ff58ed7da42f5a307a4b0f8db9454eb25a
Scepheo <scepheo@gmail.com>
2018-05-03 22:31:41 +0200

    Use generic interface type on MOS 6052X for talking to the emulator core

---
+++ BizHawk.Emulation.Cores/CPUs/MOS 6502X/IMOS6502XLink.cs
@@ -0,0 +1,13 @@
+﻿namespace BizHawk.Emulation.Cores.Components.M6502
+{
+	public interface IMOS6502XLink
+	{
+		byte ReadMemory(ushort address);
+		byte DummyReadMemory(ushort address);
+		byte PeekMemory(ushort address);
+		void WriteMemory(ushort address, byte value);
+
+		// This only calls when the first byte of an instruction is fetched.
+		void OnExecFetch(ushort address);
+	}
+}
