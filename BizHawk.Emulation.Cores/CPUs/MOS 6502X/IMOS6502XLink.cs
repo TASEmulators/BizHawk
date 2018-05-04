492401195ad5890a442de5c2f791df67a3722f88
Scepheo <scepheo@gmail.com>
2018-05-05 00:57:23 +0200

    Add comment explaining why IMOS6502XLink exists.

--- BizHawk.Emulation.Cores/CPUs/MOS 6502X/IMOS6502XLink.cs
+++ BizHawk.Emulation.Cores/CPUs/MOS 6502X/IMOS6502XLink.cs
@@ -1,5 +1,10 @@
 ﻿namespace BizHawk.Emulation.Cores.Components.M6502
 {
+	// Interface that has all the methods required by the MOS 6502X to talk to
+	// the emulator core.
+	// Should only be used as a generic type argument for the MOS 6502X, and
+	// implementations should be structs where possible. This combination allows
+	// the JITer to generate much faster code than calling a Func<> or Action<>.
 	public interface IMOS6502XLink
 	{
 		byte ReadMemory(ushort address);
