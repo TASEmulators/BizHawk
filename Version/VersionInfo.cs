2174f1d305404d09612096e0d007b8054a5cc85b
feos <feos-theos@yandex.ru>
2019-01-06 18:26:31 +0300

    nice

--- Version/VersionInfo.cs
+++ Version/VersionInfo.cs
@@ -3,7 +3,7 @@ using System.IO;
 internal static class VersionInfo
 {
 	// keep this updated at every major release
-	public const string Mainversion = "2.4.0"; // Use numbers only or the new version notification won't work
+	public const string Mainversion = "2.3.1"; // Use numbers only or the new version notification won't work
 	public static readonly string RELEASEDATE = "January 6, 2019";
 	public static readonly bool DeveloperBuild = false;
 	public static readonly string HomePage = "http://tasvideos.org/BizHawk.html";
