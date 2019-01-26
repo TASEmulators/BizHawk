b6173db95c492bcfd19b3a2013eb83573adbbde7
Asnivor <coding@asnitech.co.uk>
2019-01-26 20:07:00 +0000

    Linux VBANext - is built and should work once the mono default attribute issue is sorted

--- vbanext/instance.cpp
+++ vbanext/instance.cpp
@@ -13537,7 +13537,12 @@ void operator delete(void *p)
 	std::free(p);
 }
 
+
+#ifdef _WIN32
 #define EXPORT extern "C" __declspec(dllexport)
+#elif __linux__
+#define EXPORT extern "C"
+#endif
 
 // public interface follows
 EXPORT Gigazoid *Create()
