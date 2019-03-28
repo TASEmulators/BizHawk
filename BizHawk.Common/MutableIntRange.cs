e985e028ddeb1fb1ecb8374bde38cd46579ceeeb
YoshiRulz <OSSYoshiRulz@gmail.com>
2019-03-28 13:01:24 +1000

    Extract backing fields in MutableIntRange, prevents assigning Max < Min

--- BizHawk.Common/MutableIntRange.cs
+++ BizHawk.Common/MutableIntRange.cs
@@ -4,27 +4,52 @@ namespace BizHawk.Common
 {
 	public class MutableIntRange
 	{
-		public int Min;
-		public int Max;
+		private int _min;
+		private int _max;
+
+		public int Min
+		{
+			get
+			{
+				return _min;
+			}
+			set
+			{
+				if (_max < value) throw new ArgumentException();
+				_min = value;
+			}
+		}
+
+		public int Max
+		{
+			get
+			{
+				return _max;
+			}
+			set
+			{
+				if (value < _min) throw new ArgumentException();
+				_max = value;
+			}
+		}
 
 		public MutableIntRange(int min, int max)
 		{
-			if (max < min) throw new ArgumentException();
-			Min = min;
-			Max = max;
+			_min = min;
+			Max = max; // setter may throw ArgumentException
 		}
 
-		public int Constrain(int i) => i < Min ? Min : i > Max ? Max : i;
+		public int Constrain(int i) => i < _min ? _min : i > _max ? _max : i;
 
-		/// <returns>true if i is in the inclusive range Min..Max, false otherwise</returns>
-		public bool Covers(int i) => Min <= i && i <= Max;
+		/// <returns>true if i is in the inclusive range <see cref="Min"/>..<see cref="Max"/>, false otherwise</returns>
+		public bool Covers(int i) => _min <= i && i <= _max;
 
-		public uint GetCount() => (uint) ((long) Max - Min + 1);
+		public uint GetCount() => (uint) ((long) _max - _min + 1);
 
-		/// <returns>true if i is in the exclusive range Min..Max, false otherwise</returns>
+		/// <returns>true if i is in the exclusive range <see cref="Min"/>..<see cref="Max"/>, false otherwise</returns>
 		/// <remarks>
 		/// You probably want <see cref="Covers"/>
 		/// </remarks>
-		public bool StrictContains(int i) => Min < i && i < Max;
+		public bool StrictContains(int i) => _min < i && i < _max;
 	}
 }
