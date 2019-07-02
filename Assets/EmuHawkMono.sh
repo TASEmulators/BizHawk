4842442ef70d8d84ff8899431b3c945f32c9728d
YoshiRulz <OSSYoshiRulz@gmail.com>
2019-07-02 16:34:37 +1000

    Set MONO_WINFORMS_XIM_STYLE=disabled on all Debian-based distros

--- Assets/EmuHawkMono.sh
+++ Assets/EmuHawkMono.sh
@@ -8,8 +8,7 @@ libpath=""
 if [ "$(command -v lsb_release)" ]; then
 	case "$(lsb_release -i | cut -c17- | tr -d "\n")" in
 		"Arch"|"ManjaroLinux") libpath="/usr/lib/wine";;
-		"Debian"|"LinuxMint") libpath="/usr/lib/x86_64-linux-gnu/wine";;
-		"Ubuntu") libpath="/usr/lib/x86_64-linux-gnu/wine"; export MONO_WINFORMS_XIM_STYLE=disabled;; # see https://bugzilla.xamarin.com/show_bug.cgi?id=28047#c9
+		"Debian"|"LinuxMint"|"Ubuntu") libpath="/usr/lib/x86_64-linux-gnu/wine"; export MONO_WINFORMS_XIM_STYLE=disabled;; # see https://bugzilla.xamarin.com/show_bug.cgi?id=28047#c9
 	esac
 else
 	printf "Distro does not provide LSB release info API! (You've met with a terrible fate, haven't you?)\n"
