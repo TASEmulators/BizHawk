TASEmulators/BizHawk

commit 42764f0019990c93d3eb61719c97f03ecfb68a71

Add preliminary Unix compatibility (using Mono; resolves #1384) (#1380)
* Move PlatformSpecificLinkedLibs and implementations to common and rename
* Specify file ext. at LoadPlatformSpecific call site
* Move Client.Common.Global.RunningOnUnix to PlatformLinkedLibSingleton
* Inline var Resolver
* Use PlatformLinkedLibManager internally
* Move plugin load check to LinkedLibManager, use LinkedLibManager
* Interpolate
* Return exit code from dlclose/FreeLibrary
* Skip all calls to externs in BlipBufDll when using mono
* Use PlatformLinkedLibManager in SevenZipLibraryManager
* Add expected return value to workaround (from testing on Win32)
* Remove ".dll" from DllImport attr, remove temporary workaround, see desc.
The library can be built by changing the output file name in
`.../blip_buf/Makefile` to `libblip_buf.so`, and running `make`. It will be
loaded if placed in the `.../output` folder.
* Remove unused code, add TODO (this class is req. for Waterbox.PeWrapper)
The TODO is to [rewrite with
C#](https://docs.microsoft.com/en-us/dotnet/standard/io/memory-mapped-files)
instead of importing from `kernel32.dll`.
* Update OpenTK again but better (for #1384)
* Add Mono run script
* Add libblip_buf.so (temporary)
Temporary because it should be a separate package which BizHawk depends on.
* Add distro detection, add "already running" and "unknown distro" messages
* Gray-out Lua Console on Unix
* Extract superclass from EmuLuaLibrary, add shell implementation for Unix
* Specify libdl version, Fedora doesn't have the versionless symlink
* Remove empty `ToolStripMenuItem`, null `Text` caused crash on Unix
* Transform OpenTK keyboard input into a `List<KeyEvent>` and read that
Also fixes crash on rebind
* Remove debug `using ...;`

BizHawk.Client.EmuHawk/Program.cs
@@ -17,26 +17,25 @@ namespace BizHawk.Client.EmuHawk
 {
 	static class Program
 	{
-		static bool RunningOnUnix = Environment.OSVersion.Platform == PlatformID.Unix || Environment.OSVersion.Platform == PlatformID.MacOSX;
-
 		static Program()
 		{
 			//this needs to be done before the warnings/errors show up
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 
-			PlatformSpecificLinkedLibs libLoader = RunningOnUnix ? (PlatformSpecificLinkedLibs) new UnixMono() : (PlatformSpecificLinkedLibs) new Win32();
+			var libLoader = PlatformLinkedLibSingleton.LinkedLibManager;
 
 			//http://www.codeproject.com/Articles/310675/AppDomain-AssemblyResolve-Event-Tips
 
 			//try loading libraries we know we'll need
 			//something in the winforms, etc. code below will cause .net to popup a missing msvcr100.dll in case that one's missing
 			//but oddly it lets us proceed and we'll then catch it here
-			var d3dx9 = libLoader.LoadPlatformSpecific("d3dx9_43.dll");
-			var vc2015 = libLoader.LoadPlatformSpecific("vcruntime140.dll");
-			var vc2012 = libLoader.LoadPlatformSpecific("msvcr120.dll"); //TODO - check version?
-			var vc2010 = libLoader.LoadPlatformSpecific("msvcr100.dll"); //TODO - check version?
-			var vc2010p = libLoader.LoadPlatformSpecific("msvcp100.dll");
+			var libExt = PlatformLinkedLibSingleton.RunningOnUnix ? ".dll.so" : ".dll";
+			var d3dx9 = libLoader.LoadPlatformSpecific($"d3dx9_43{libExt}");
+			var vc2015 = libLoader.LoadPlatformSpecific($"vcruntime140{libExt}");
+			var vc2012 = libLoader.LoadPlatformSpecific($"msvcr120{libExt}"); //TODO - check version?
+			var vc2010 = libLoader.LoadPlatformSpecific($"msvcr100{libExt}"); //TODO - check version?
+			var vc2010p = libLoader.LoadPlatformSpecific($"msvcp100{libExt}");
 			bool fail = false, warn = false;
 			warn |= d3dx9 == IntPtr.Zero;
 			fail |= vc2015 == IntPtr.Zero;
@@ -66,7 +65,7 @@ namespace BizHawk.Client.EmuHawk
 			libLoader.FreePlatformSpecific(vc2010);
 			libLoader.FreePlatformSpecific(vc2010p);
 
-			if (!RunningOnUnix)
+			if (!PlatformLinkedLibSingleton.RunningOnUnix)
 			{
 				// this will look in subdirectory "dll" to load pinvoked stuff
 				string dllDir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "dll");
@@ -88,63 +87,6 @@ namespace BizHawk.Client.EmuHawk
 		static int Main(string[] args)
 		{
 			return SubMain(args);
-		}
-
-		private interface PlatformSpecificLinkedLibs
-		{
-			IntPtr LoadPlatformSpecific(string dllToLoad);
-			IntPtr GetProcAddr(IntPtr hModule, string procName);
-			void FreePlatformSpecific(IntPtr hModule);
-		}
-		private class Win32 : PlatformSpecificLinkedLibs
-		{
-			[DllImport("kernel32.dll")]
-			private static extern IntPtr LoadLibrary(string dllToLoad);
-			[DllImport("kernel32.dll")]
-			private static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
-			[DllImport("kernel32.dll")]
-			private static extern void FreeLibrary(IntPtr hModule);
-			public IntPtr LoadPlatformSpecific(string dllToLoad)
-			{
-				return LoadLibrary(dllToLoad);
-			}
-			public IntPtr GetProcAddr(IntPtr hModule, string procName)
-			{
-				return GetProcAddress(hModule, procName);
-			}
-			public void FreePlatformSpecific(IntPtr hModule)
-			{
-				FreeLibrary(hModule);
-			}
-		}
-		private class UnixMono : PlatformSpecificLinkedLibs
-		{
-			// This class is copied from a tutorial, so don't git blame and then email me expecting insight.
-			const int RTLD_NOW = 2;
-			[DllImport("libdl.so")]
-			private static extern IntPtr dlopen(String fileName, int flags);
-			[DllImport("libdl.so")]
-			private static extern IntPtr dlerror();
-			[DllImport("libdl.so")]
-			private static extern IntPtr dlsym(IntPtr handle, String symbol);
-			[DllImport("libdl.so")]
-			private static extern int dlclose(IntPtr handle);
-			public IntPtr LoadPlatformSpecific(string dllToLoad)
-			{
-				return dlopen(dllToLoad + ".so", RTLD_NOW);
-			}
-			public IntPtr GetProcAddr(IntPtr hModule, string procName)
-			{
-				dlerror();
-				var res = dlsym(hModule, procName);
-				var errPtr = dlerror();
-				if (errPtr != IntPtr.Zero) throw new Exception("dlsym: " + Marshal.PtrToStringAnsi(errPtr));
-				return res;
-			}
-			public void FreePlatformSpecific(IntPtr hModule)
-			{
-				dlclose(hModule);
-			}
 		}
 
 		private interface PlatformSpecificMainLoopCrashHandler
@@ -258,7 +200,7 @@ namespace BizHawk.Client.EmuHawk
 				}
 			}
 		}
-		private static PlatformSpecificMainLoopCrashHandler mainLoopCrashHandler = RunningOnUnix
+		private static PlatformSpecificMainLoopCrashHandler mainLoopCrashHandler = PlatformLinkedLibSingleton.RunningOnUnix
 			? (PlatformSpecificMainLoopCrashHandler) new UnixMonoMainLoopCrashHandler()
 			: (PlatformSpecificMainLoopCrashHandler) new Win32MainLoopCrashHandler();
 
@@ -323,7 +265,7 @@ namespace BizHawk.Client.EmuHawk
 			GlobalWin.GLManager = GLManager.Instance;
 
 			//now create the "GL" context for the display method. we can reuse the IGL_TK context if opengl display method is chosen
-			if (RunningOnUnix) Global.Config.DispMethod = Config.EDispMethod.GdiPlus;
+			if (PlatformLinkedLibSingleton.RunningOnUnix) Global.Config.DispMethod = Config.EDispMethod.GdiPlus;
 		REDO_DISPMETHOD:
 			if (Global.Config.DispMethod == Config.EDispMethod.GdiPlus)
 				GlobalWin.GL = new Bizware.BizwareGL.Drivers.GdiPlus.IGL_GdiPlus();
@@ -371,7 +313,7 @@ namespace BizHawk.Client.EmuHawk
 				goto REDO_DISPMETHOD;
 			}
 
-			if (!RunningOnUnix)
+			if (!PlatformLinkedLibSingleton.RunningOnUnix)
 			{
 				//WHY do we have to do this? some intel graphics drivers (ig7icd64.dll 10.18.10.3304 on an unknown chip on win8.1) are calling SetDllDirectory() for the process, which ruins stuff.
 				//The relevant initialization happened just before in "create IGL context".

BizHawk.Emulation.Cores/Consoles/Nintendo/N64/NativeApi/mupen64plusCoreApi.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Threading;
 
+using BizHawk.Common;
 using BizHawk.Emulation.Common;
 using System.Text;
 
@@ -26,17 +27,7 @@ namespace BizHawk.Emulation.Cores.Nintendo.N64.NativeApi
 		bool event_frameend = false;
 		bool event_breakpoint = false;
 
-		[DllImport("kernel32.dll")]
-		public static extern UInt32 GetLastError();
-
-		[DllImport("kernel32.dll")]
-		public static extern IntPtr LoadLibrary(string dllToLoad);
-
-		[DllImport("kernel32.dll")]
-		public static extern IntPtr GetProcAddress(IntPtr hModule, string procedureName);
-
-		[DllImport("kernel32.dll")]
-		public static extern bool FreeLibrary(IntPtr hModule);
+		private static readonly PlatformLinkedLibSingleton.PlatformLinkedLibManager libLoader = PlatformLinkedLibSingleton.LinkedLibManager;
 
 		public enum m64p_error
 		{
@@ -506,7 +497,7 @@ namespace BizHawk.Emulation.Cores.Nintendo.N64.NativeApi
 			}
 			this.bizhawkCore = bizhawkCore;
 
-			CoreDll = LoadLibrary("mupen64plus.dll");
+			CoreDll = libLoader.LoadPlatformSpecific("mupen64plus");
 			if (CoreDll == IntPtr.Zero)
 				throw new InvalidOperationException(string.Format("Failed to load mupen64plus.dll"));
 
@@ -605,41 +596,41 @@ namespace BizHawk.Emulation.Cores.Nintendo.N64.NativeApi
 		/// </summary>
 		void connectFunctionPointers()
 		{
-			m64pCoreStartup = (CoreStartup)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "CoreStartup"), typeof(CoreStartup));
-			m64pCoreShutdown = (CoreShutdown)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "CoreShutdown"), typeof(CoreShutdown));
-			m64pCoreDoCommandByteArray = (CoreDoCommandByteArray)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "CoreDoCommand"), typeof(CoreDoCommandByteArray));
-			m64pCoreDoCommandPtr = (CoreDoCommandPtr)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "CoreDoCommand"), typeof(CoreDoCommandPtr));
-			m64pCoreDoCommandRefInt = (CoreDoCommandRefInt)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "CoreDoCommand"), typeof(CoreDoCommandRefInt));
-			m64pCoreDoCommandFrameCallback = (CoreDoCommandFrameCallback)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "CoreDoCommand"), typeof(CoreDoCommandFrameCallback));
-			m64pCoreDoCommandVICallback = (CoreDoCommandVICallback)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "CoreDoCommand"), typeof(CoreDoCommandVICallback));
-			m64pCoreDoCommandRenderCallback = (CoreDoCommandRenderCallback)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "CoreDoCommand"), typeof(CoreDoCommandRenderCallback));
-			m64pCoreAttachPlugin = (CoreAttachPlugin)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "CoreAttachPlugin"), typeof(CoreAttachPlugin));
-			m64pCoreDetachPlugin = (CoreDetachPlugin)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "CoreDetachPlugin"), typeof(CoreDetachPlugin));
-			m64pConfigOpenSection = (ConfigOpenSection)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "ConfigOpenSection"), typeof(ConfigOpenSection));
-			m64pConfigSetParameter = (ConfigSetParameter)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "ConfigSetParameter"), typeof(ConfigSetParameter));
-			m64pConfigSetParameterStr = (ConfigSetParameterStr)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "ConfigSetParameter"), typeof(ConfigSetParameterStr));
-			m64pCoreSaveState = (savestates_save_bkm)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "savestates_save_bkm"), typeof(savestates_save_bkm));
-			m64pCoreLoadState = (savestates_load_bkm)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "savestates_load_bkm"), typeof(savestates_load_bkm));
-			m64pDebugMemGetPointer = (DebugMemGetPointer)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "DebugMemGetPointer"), typeof(DebugMemGetPointer));
-			m64pDebugSetCallbacks = (DebugSetCallbacks)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "DebugSetCallbacks"), typeof(DebugSetCallbacks));
-			m64pDebugBreakpointLookup = (DebugBreakpointLookup)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "DebugBreakpointLookup"), typeof(DebugBreakpointLookup));
-			m64pDebugBreakpointCommand = ( DebugBreakpointCommand )Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "DebugBreakpointCommand"), typeof(DebugBreakpointCommand));
-			m64pDebugGetState = (DebugGetState)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "DebugGetState"), typeof(DebugGetState));
-			m64pDebugSetRunState = (DebugSetRunState)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "DebugSetRunState"), typeof(DebugSetRunState));
-			m64pDebugStep = (DebugStep)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "DebugStep"), typeof(DebugStep));
-			m64pMemGetSize = (MemGetSize)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "MemGetSize"), typeof(MemGetSize));
-			m64pinit_saveram = (init_saveram)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "init_saveram"), typeof(init_saveram));
-			m64psave_saveram = (save_saveram)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "save_saveram"), typeof(save_saveram));
-			m64pload_saveram = (load_saveram)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "load_saveram"), typeof(load_saveram));
+			m64pCoreStartup = (CoreStartup)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "CoreStartup"), typeof(CoreStartup));
+			m64pCoreShutdown = (CoreShutdown)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "CoreShutdown"), typeof(CoreShutdown));
+			m64pCoreDoCommandByteArray = (CoreDoCommandByteArray)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "CoreDoCommand"), typeof(CoreDoCommandByteArray));
+			m64pCoreDoCommandPtr = (CoreDoCommandPtr)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "CoreDoCommand"), typeof(CoreDoCommandPtr));
+			m64pCoreDoCommandRefInt = (CoreDoCommandRefInt)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "CoreDoCommand"), typeof(CoreDoCommandRefInt));
+			m64pCoreDoCommandFrameCallback = (CoreDoCommandFrameCallback)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "CoreDoCommand"), typeof(CoreDoCommandFrameCallback));
+			m64pCoreDoCommandVICallback = (CoreDoCommandVICallback)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "CoreDoCommand"), typeof(CoreDoCommandVICallback));
+			m64pCoreDoCommandRenderCallback = (CoreDoCommandRenderCallback)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "CoreDoCommand"), typeof(CoreDoCommandRenderCallback));
+			m64pCoreAttachPlugin = (CoreAttachPlugin)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "CoreAttachPlugin"), typeof(CoreAttachPlugin));
+			m64pCoreDetachPlugin = (CoreDetachPlugin)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "CoreDetachPlugin"), typeof(CoreDetachPlugin));
+			m64pConfigOpenSection = (ConfigOpenSection)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "ConfigOpenSection"), typeof(ConfigOpenSection));
+			m64pConfigSetParameter = (ConfigSetParameter)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "ConfigSetParameter"), typeof(ConfigSetParameter));
+			m64pConfigSetParameterStr = (ConfigSetParameterStr)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "ConfigSetParameter"), typeof(ConfigSetParameterStr));
+			m64pCoreSaveState = (savestates_save_bkm)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "savestates_save_bkm"), typeof(savestates_save_bkm));
+			m64pCoreLoadState = (savestates_load_bkm)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "savestates_load_bkm"), typeof(savestates_load_bkm));
+			m64pDebugMemGetPointer = (DebugMemGetPointer)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "DebugMemGetPointer"), typeof(DebugMemGetPointer));
+			m64pDebugSetCallbacks = (DebugSetCallbacks)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "DebugSetCallbacks"), typeof(DebugSetCallbacks));
+			m64pDebugBreakpointLookup = (DebugBreakpointLookup)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "DebugBreakpointLookup"), typeof(DebugBreakpointLookup));
+			m64pDebugBreakpointCommand = ( DebugBreakpointCommand )Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "DebugBreakpointCommand"), typeof(DebugBreakpointCommand));
+			m64pDebugGetState = (DebugGetState)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "DebugGetState"), typeof(DebugGetState));
+			m64pDebugSetRunState = (DebugSetRunState)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "DebugSetRunState"), typeof(DebugSetRunState));
+			m64pDebugStep = (DebugStep)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "DebugStep"), typeof(DebugStep));
+			m64pMemGetSize = (MemGetSize)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "MemGetSize"), typeof(MemGetSize));
+			m64pinit_saveram = (init_saveram)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "init_saveram"), typeof(init_saveram));
+			m64psave_saveram = (save_saveram)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "save_saveram"), typeof(save_saveram));
+			m64pload_saveram = (load_saveram)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "load_saveram"), typeof(load_saveram));
 
-			m64pSetTraceCallback = (SetTraceCallback)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "SetTraceCallback"), typeof(SetTraceCallback));
+			m64pSetTraceCallback = (SetTraceCallback)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "SetTraceCallback"), typeof(SetTraceCallback));
 
-			m64pGetRegisters = (GetRegisters)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "GetRegisters"), typeof(GetRegisters));
+			m64pGetRegisters = (GetRegisters)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "GetRegisters"), typeof(GetRegisters));
 
-			m64p_read_memory_8 = (biz_read_memory)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "biz_read_memory"), typeof(biz_read_memory));
-			m64p_write_memory_8 = (biz_write_memory)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "biz_write_memory"), typeof(biz_write_memory));
+			m64p_read_memory_8 = (biz_read_memory)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "biz_read_memory"), typeof(biz_read_memory));
+			m64p_write_memory_8 = (biz_write_memory)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "biz_write_memory"), typeof(biz_write_memory));
 
-			m64p_decode_op = (biz_r4300_decode_op)Marshal.GetDelegateForFunctionPointer(GetProcAddress(CoreDll, "biz_r4300_decode_op"), typeof(biz_r4300_decode_op));
+			m64p_decode_op = (biz_r4300_decode_op)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(CoreDll, "biz_r4300_decode_op"), typeof(biz_r4300_decode_op));
 		}
 
 		/// <summary>
@@ -933,7 +924,7 @@ namespace BizHawk.Emulation.Cores.Nintendo.N64.NativeApi
 
 				m64pCoreDoCommandPtr(m64p_command.M64CMD_ROM_CLOSE, 0, IntPtr.Zero);
 				m64pCoreShutdown();
-				FreeLibrary(CoreDll);
+				libLoader.FreePlatformSpecific(CoreDll);
 
 				disposed = true;
 			}
@@ -953,18 +944,16 @@ namespace BizHawk.Emulation.Cores.Nintendo.N64.NativeApi
 				DetachPlugin(type);
 
 			AttachedPlugin plugin;
-			plugin.dllHandle = LoadLibrary(PluginName);
-			if (plugin.dllHandle == IntPtr.Zero)
-				throw new InvalidOperationException(string.Format("Failed to load plugin {0}, error code: 0x{1:X}", PluginName, GetLastError()));
+			plugin.dllHandle = libLoader.LoadPlatformSpecific(PluginName);
 
-			plugin.dllStartup = (PluginStartup)Marshal.GetDelegateForFunctionPointer(GetProcAddress(plugin.dllHandle, "PluginStartup"), typeof(PluginStartup));
-			plugin.dllShutdown = (PluginShutdown)Marshal.GetDelegateForFunctionPointer(GetProcAddress(plugin.dllHandle, "PluginShutdown"), typeof(PluginShutdown));
+			plugin.dllStartup = (PluginStartup)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(plugin.dllHandle, "PluginStartup"), typeof(PluginStartup));
+			plugin.dllShutdown = (PluginShutdown)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(plugin.dllHandle, "PluginShutdown"), typeof(PluginShutdown));
 			plugin.dllStartup(CoreDll, null, null);
 
 			m64p_error result = m64pCoreAttachPlugin(type, plugin.dllHandle);
 			if (result != m64p_error.M64ERR_SUCCESS)
 			{
-				FreeLibrary(plugin.dllHandle);
+				libLoader.FreePlatformSpecific(plugin.dllHandle);
 				throw new InvalidOperationException(string.Format("Error during attaching plugin {0}", PluginName));
 			}
 
@@ -980,7 +969,7 @@ namespace BizHawk.Emulation.Cores.Nintendo.N64.NativeApi
 				plugins.Remove(type);
 				m64pCoreDetachPlugin(type);
 				plugin.dllShutdown();
-				FreeLibrary(plugin.dllHandle);
+				libLoader.FreePlatformSpecific(plugin.dllHandle);
 			}
 		}
 

Assets/EmuHawkMono.sh
@@ -1,0 +1,18 @@
+#!/bin/sh
+cd "$(dirname "$0")"
+if [ "$(ps -C "mono" -o "cmd" --no-headers | grep "EmuHawk.exe")" ]; then
+	echo "EmuHawk is already running, exiting..."
+	exit 0
+fi
+libpath=""
+if [ "$(command -v lsb_release)" ]; then
+	case "$(lsb_release -i | cut -c17- | tr -d "\n")" in
+		"Arch"|"ManjaroLinux") libpath="/usr/lib/wine";;
+		"Ubuntu"|"LinuxMint") libpath="/usr/lib/x86_64-linux-gnu/wine";;
+	esac
+fi
+if [ -z "$libpath" ]; then
+	printf "%s\n" "Unknown distro, assuming WINE library location is /usr/lib/wine..."
+	libpath="/usr/lib/wine"
+fi
+LD_LIBRARY_PATH="$libpath" mono ./EmuHawk.exe

BizHawk.Client.Common/7z/LibraryManager.cs
@@ -14,6 +14,8 @@
     along with SevenZipSharp.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+using BizHawk.Common;
+
 using System;
 using System.Collections.Generic;
 #if !WINCE && !MONO
@@ -87,6 +89,8 @@ namespace SevenZip
         // private static string _LibraryVersion;
         private static bool? _modifyCapabale;
 
+        private static readonly PlatformLinkedLibSingleton.PlatformLinkedLibManager libLoader = PlatformLinkedLibSingleton.LinkedLibManager;
+
         private static void InitUserInFormat(object user, InArchiveFormat format)
         {
             if (!_inArchives.ContainsKey(user))
@@ -148,16 +152,16 @@ namespace SevenZip
 								//{
 								//    throw new SevenZipLibraryException("DLL file does not exist.");
 								//}
-                if ((_modulePtr = NativeMethods.LoadLibrary(_libraryFileName)) == IntPtr.Zero)
+                if ((_modulePtr = libLoader.LoadPlatformSpecific(_libraryFileName)) == IntPtr.Zero)
                 {
 									//try a different directory
 									string alternateFilename = Path.Combine(Path.Combine(Path.GetDirectoryName(_libraryFileName),"dll"),"7z.dll");
-									if ((_modulePtr = NativeMethods.LoadLibrary(alternateFilename)) == IntPtr.Zero)
+									if ((_modulePtr = libLoader.LoadPlatformSpecific(alternateFilename)) == IntPtr.Zero)
                     throw new SevenZipLibraryException("failed to load library.");
                 }
-                if (NativeMethods.GetProcAddress(_modulePtr, "GetHandlerProperty") == IntPtr.Zero)
+                if (libLoader.GetProcAddr(_modulePtr, "GetHandlerProperty") == IntPtr.Zero)
                 {
-                    NativeMethods.FreeLibrary(_modulePtr);
+                    libLoader.FreePlatformSpecific(_modulePtr);
                     throw new SevenZipLibraryException("library is invalid.");
                 }
             }
@@ -431,7 +435,7 @@ namespace SevenZip
                     if (_totalUsers == 0)
                     {
 #if !WINCE && !MONO
-                        NativeMethods.FreeLibrary(_modulePtr);
+                        libLoader.FreePlatformSpecific(_modulePtr);
 
 #endif
                         _modulePtr = IntPtr.Zero;
@@ -466,7 +470,7 @@ namespace SevenZip
                     }
                     var createObject = (NativeMethods.CreateObjectDelegate)
                         Marshal.GetDelegateForFunctionPointer(
-                            NativeMethods.GetProcAddress(_modulePtr, "CreateObject"),
+                            libLoader.GetProcAddr(_modulePtr, "CreateObject"),
                             typeof(NativeMethods.CreateObjectDelegate));
                     if (createObject == null)
                     {
@@ -525,7 +529,7 @@ namespace SevenZip
                     }
                     var createObject = (NativeMethods.CreateObjectDelegate)
                         Marshal.GetDelegateForFunctionPointer(
-                            NativeMethods.GetProcAddress(_modulePtr, "CreateObject"),
+                            libLoader.GetProcAddr(_modulePtr, "CreateObject"),
                             typeof(NativeMethods.CreateObjectDelegate));
                     if (createObject == null)
                     {

BizHawk.Client.Common/7z/NativeMethods.cs
@@ -35,18 +35,8 @@ namespace SevenZip
             [MarshalAs(UnmanagedType.Interface)] out object outObject);
 
         #endregion
-
-        [DllImport("kernel32.dll", BestFitMapping = false, ThrowOnUnmappableChar = true)]
-        public static extern IntPtr LoadLibrary([MarshalAs(UnmanagedType.LPStr)] string fileName);
-
-        [DllImport("kernel32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool FreeLibrary(IntPtr hModule);
-
-        [DllImport("kernel32.dll", BestFitMapping = false, ThrowOnUnmappableChar = true)]
-        public static extern IntPtr GetProcAddress(IntPtr hModule, [MarshalAs(UnmanagedType.LPStr)] string procName);
 		#endif
-		
+
 		#if WINCE
         [DllImport("7z.dll", EntryPoint="CreateObject")]
         public static extern int CreateCOMObject(

BizHawk.Client.Common/Global.cs
@@ -159,7 +159,5 @@ namespace BizHawk.Client.Common
 		}
 
 		public static Dictionary<string, object> UserBag = new Dictionary<string, object>();
-
-		public static bool RunningOnUnix = Environment.OSVersion.Platform == PlatformID.Unix || Environment.OSVersion.Platform == PlatformID.MacOSX;
 	}
 }

BizHawk.Client.EmuHawk/BizHawk.Client.EmuHawk.csproj
@@ -943,6 +943,8 @@
     <Compile Include="tools\Lua\Libraries\EmuLuaLibrary.Input.cs" />
     <Compile Include="tools\Lua\Libraries\EmuLuaLibrary.Savestate.cs" />
     <Compile Include="tools\Lua\Libraries\EmuLuaLibrary.Tastudio.cs" />
+    <Compile Include="tools\Lua\Libraries\NotReallyLuaLibrary.cs" />
+    <Compile Include="tools\Lua\Libraries\PlatformEmuLuaLibrary.cs" />
     <Compile Include="tools\Lua\LuaAutocompleteInstaller.cs" />
     <Compile Include="tools\Lua\LuaCanvas.cs">
       <SubType>Form</SubType>

commit 77bbc41286257391c49236f8aa9040f554fbd519

Fix prerequisites dialog (broken by 42764f00 and 436b6452).

BizHawk.Client.EmuHawk/Program.cs
@@ -49,7 +49,7 @@ namespace BizHawk.Client.EmuHawk
 					};
 					var box = new BizHawk.Client.EmuHawk.CustomControls.PrereqsAlert(!fail)
 					{
-						textBox1 = { Text = string.Concat("\n", alertLines) }
+						textBox1 = { Text = string.Join(Environment.NewLine, alertLines) }
 					};
 					box.ShowDialog();
 					if (fail) System.Diagnostics.Process.GetCurrentProcess().Kill();

BizHawk.Common/OSTailoredCode.cs
@@ -75,12 +75,7 @@ namespace BizHawk.Common
 			[DllImport("kernel32.dll")]
 			private static extern bool FreeLibrary(IntPtr hModule); // return type was annotated MarshalAs(UnmanagedType.Bool)
 
-			public IntPtr LoadPlatformSpecific(string dllToLoad)
-			{
-				var p = LoadLibrary(dllToLoad);
-				if (p == IntPtr.Zero) throw new InvalidOperationException($"got null pointer, error code {GetLastError()}");
-				return p;
-			}
+			public IntPtr LoadPlatformSpecific(string dllToLoad) => LoadLibrary(dllToLoad);
 			public IntPtr GetProcAddr(IntPtr hModule, string procName) => GetProcAddress(hModule, procName);
 			public int FreePlatformSpecific(IntPtr hModule) => FreeLibrary(hModule) ? 1 : 0;
 		}

BizHawk.Emulation.Cores/Consoles/Nintendo/N64/NativeApi/mupen64plusCoreApi.cs
@@ -935,6 +935,8 @@ namespace BizHawk.Emulation.Cores.Nintendo.N64.NativeApi
 
 			AttachedPlugin plugin;
 			plugin.dllHandle = libLoader.LoadPlatformSpecific(PluginName);
+			if (plugin.dllHandle == IntPtr.Zero)
+				throw new InvalidOperationException($"Failed to load plugin {PluginName}");
 
 			plugin.dllStartup = (PluginStartup)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(plugin.dllHandle, "PluginStartup"), typeof(PluginStartup));
 			plugin.dllShutdown = (PluginShutdown)Marshal.GetDelegateForFunctionPointer(libLoader.GetProcAddr(plugin.dllHandle, "PluginShutdown"), typeof(PluginShutdown));