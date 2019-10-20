7fc4e82ef9fef740b1e5bc4ad21c7f6f63e0180e
adelikat <adelikat@tasvideos.org>
2019-10-19 19:32:34 -0500

    Debugger Breakpoints - use ListView in virtual mode instead of VirtualListView

--- BizHawk.Client.EmuHawk/tools/Debugger/BreakpointControl.Designer.cs
+++ BizHawk.Client.EmuHawk/tools/Debugger/BreakpointControl.Designer.cs
@@ -36,7 +36,7 @@
 			this.RemoveBreakpointButton = new System.Windows.Forms.Button();
 			this.DuplicateBreakpointButton = new System.Windows.Forms.Button();
 			this.EditBreakpointButton = new System.Windows.Forms.Button();
-			this.BreakpointView = new BizHawk.Client.EmuHawk.VirtualListView();
+			this.BreakpointView = new System.Windows.Forms.ListView();
 			this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
 			this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
 			this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
@@ -118,7 +118,6 @@
 			this.BreakpointView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
 			| System.Windows.Forms.AnchorStyles.Left) 
 			| System.Windows.Forms.AnchorStyles.Right)));
-			this.BreakpointView.BlazingFast = false;
 			this.BreakpointView.CheckBoxes = true;
 			this.BreakpointView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
 			this.columnHeader1,
@@ -129,16 +128,13 @@
 			this.BreakpointView.FullRowSelect = true;
 			this.BreakpointView.GridLines = true;
 			this.BreakpointView.HideSelection = false;
-			this.BreakpointView.ItemCount = 0;
+			this.BreakpointView.VirtualListSize = 0;
 			this.BreakpointView.Location = new System.Drawing.Point(0, 19);
 			this.BreakpointView.Name = "BreakpointView";
-			this.BreakpointView.SelectAllInProgress = false;
-			this.BreakpointView.selectedItem = -1;
 			this.BreakpointView.Size = new System.Drawing.Size(366, 365);
 			this.BreakpointView.TabIndex = 5;
 			this.BreakpointView.TabStop = false;
 			this.BreakpointView.UseCompatibleStateImageBehavior = false;
-			this.BreakpointView.UseCustomBackground = true;
 			this.BreakpointView.View = System.Windows.Forms.View.Details;
 			this.BreakpointView.ItemActivate += new System.EventHandler(this.BreakpointView_ItemActivate);
 			this.BreakpointView.SelectedIndexChanged += new System.EventHandler(this.BreakpointView_SelectedIndexChanged);
@@ -184,7 +180,7 @@
 
 		#endregion
 
-		private VirtualListView BreakpointView;
+		private System.Windows.Forms.ListView BreakpointView;
 		public System.Windows.Forms.ColumnHeader columnHeader1;
 		private System.Windows.Forms.ColumnHeader columnHeader2;
 		private System.Windows.Forms.Button AddBreakpointButton;
