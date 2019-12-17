867c7fb2327b3667db0cf39348e6a6752154623f
SuuperW <SuuperW@gmail.com>
2019-12-16 19:07:48 -0600

    Create firmware folder if it doesn't exist on Windows; otherwise, an exception is thrown.

--- BizHawk.Client.EmuHawk/config/FirmwaresConfig.cs
+++ BizHawk.Client.EmuHawk/config/FirmwaresConfig.cs
@@ -359,7 +359,7 @@ namespace BizHawk.Client.EmuHawk
         private void tbbOpenFolder_Click(object sender, EventArgs e)
         {
             var frmWares = PathManager.MakeAbsolutePath(Global.Config.PathEntries.FirmwaresPathFragment, null);
-			if (OSTailoredCode.CurrentOS != OSTailoredCode.DistinctOS.Windows && !Directory.Exists(frmWares))
+			if (!Directory.Exists(frmWares))
 				Directory.CreateDirectory(frmWares);
             System.Diagnostics.Process.Start(frmWares);
         }
