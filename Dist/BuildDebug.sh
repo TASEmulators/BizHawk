c31050feb61ebb4afa9e9143b16fdb978315a3ac
YoshiRulz <OSSYoshiRulz@gmail.com>
2019-12-26 15:57:33 +1000

    Migrate from packages.config to PackageReference

--- Dist/BuildDebug.sh
+++ Dist/BuildDebug.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cd "$(dirname "$0")/.." && nuget restore BizHawk.sln && CscToolExe="$(which csc)" dotnet build BizHawk.sln "$@"
+cd "$(dirname "$0")/.." && CscToolExe="$(which csc)" dotnet build BizHawk.sln -c Debug -m -p:MachineNuGetPackageDir=$HOME/.nuget/packages "$@"
