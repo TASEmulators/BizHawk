86061d9d4e34133d73a3dd3c3bd6c2cd46b88838
SuuperW <SuuperW@gmail.com>
2019-12-28 07:55:37 -0600

    MelonDS: Add singleInstance attribute to core.

--- BizHawk.Emulation.Cores/Consoles/Nintendo/NDS/MelonDS.cs
+++ BizHawk.Emulation.Cores/Consoles/Nintendo/NDS/MelonDS.cs
@@ -10,7 +10,7 @@ using BizHawk.Emulation.Common;
 
 namespace BizHawk.Emulation.Cores.Consoles.Nintendo.NDS
 {
-	[Core("MelonDS", "Arisotura")]
+	[Core("MelonDS", "Arisotura", singleInstance: true)]
 	unsafe public partial class MelonDS : IEmulator
 	{
 		private BasicServiceProvider _serviceProvider;
