69cc3ab3fd030329a0a9da12f910cf51aaa6ad02
adelikat <adelikat@tasvideos.org>
2020-01-12 15:05:49 -0600

    meh, might as well update version info to match release branch

--- Version/VersionInfo.cs
+++ Version/VersionInfo.cs
@@ -3,8 +3,8 @@ using System.IO;
 internal static class VersionInfo
 {
 	// keep this updated at every major release
-	public const string Mainversion = "2.3.2"; // Use numbers only or the new version notification won't work
-	public static readonly string RELEASEDATE = "June 18, 2019";
+	public const string Mainversion = "2.3.3"; // Use numbers only or the new version notification won't work
+	public static readonly string RELEASEDATE = "January 12, 2019";
 	public static readonly bool DeveloperBuild = true;
 	public static readonly string HomePage = "http://tasvideos.org/BizHawk.html";
 
