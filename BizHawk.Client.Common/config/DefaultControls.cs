23a5780d0d8575cde8010fcbcedf62dc5aab885e
adelikat <adelikat@tasvideos.org>
2020-01-21 10:01:21 -0600

    DefaultControls - make properties instead of fields

--- BizHawk.Client.Common/config/DefaultControls.cs
+++ BizHawk.Client.Common/config/DefaultControls.cs
@@ -5,8 +5,13 @@ namespace BizHawk.Client.Common
 	// Represents the defaults used in defctrl.json
 	public class DefaultControls
 	{
-		public Dictionary<string, Dictionary<string, string>> AllTrollers = new Dictionary<string, Dictionary<string, string>>();
-		public Dictionary<string, Dictionary<string, string>> AllTrollersAutoFire = new Dictionary<string, Dictionary<string, string>>();
-		public Dictionary<string, Dictionary<string, AnalogBind>> AllTrollersAnalog = new Dictionary<string, Dictionary<string, AnalogBind>>();
+		public Dictionary<string, Dictionary<string, string>> AllTrollers { get; set; }
+			= new Dictionary<string, Dictionary<string, string>>();
+
+		public Dictionary<string, Dictionary<string, string>> AllTrollersAutoFire { get; set; }
+			= new Dictionary<string, Dictionary<string, string>>();
+
+		public Dictionary<string, Dictionary<string, AnalogBind>> AllTrollersAnalog { get; set; }
+			= new Dictionary<string, Dictionary<string, AnalogBind>>();
 	}
 }
