02dc42d4533c7844da631447c7df7ed936166a72
YoshiRulz <OSSYoshiRulz@gmail.com>
2020-02-06 23:51:46 +1000

    Use MSBuild to copy output instead of bash in external tool template

--- ExternalToolProjects/.build_net48_from_cwd_debug.sh
+++ ExternalToolProjects/.build_net48_from_cwd_debug.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-name="$(basename "$PWD").dll"
-CscToolExe="$(which csc)" dotnet build -c Debug -m && cp -f "bin/Debug/net48/$name" "../../output/ExternalTools/$name"
+CscToolExe="$(which csc)" dotnet build -c Debug -m
--- ExternalToolProjects/.build_net48_from_cwd_release.sh
+++ ExternalToolProjects/.build_net48_from_cwd_release.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-name="$(basename "$PWD").dll"
-CscToolExe="$(which csc)" dotnet build -c Release -m && cp -f "bin/Release/net48/$name" "../../output/ExternalTools/$name"
+CscToolExe="$(which csc)" dotnet build -c Release -m
