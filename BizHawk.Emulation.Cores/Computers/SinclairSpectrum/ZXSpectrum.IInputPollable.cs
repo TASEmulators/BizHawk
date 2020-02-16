b03b6eb9def6ad7d30961016f734b0df36d53a95
adelikat <adelikat@tasvideos.org>
2020-02-16 16:10:26 -0600

    convert spaces to tabs in some ZXSpectrum files

--- BizHawk.Emulation.Cores/Computers/SinclairSpectrum/ZXSpectrum.IInputPollable.cs
+++ BizHawk.Emulation.Cores/Computers/SinclairSpectrum/ZXSpectrum.IInputPollable.cs
@@ -1,29 +1,28 @@
-﻿using System;
-using BizHawk.Emulation.Common;
+﻿using BizHawk.Emulation.Common;
 
 namespace BizHawk.Emulation.Cores.Computers.SinclairSpectrum
 {
-    /// <summary>
-    /// ZXHawk: Core Class
-    /// * IInputPollable *
-    /// </summary>
-    public partial class ZXSpectrum : IInputPollable
-    {
-        public int LagCount
-        {
-            get => _lagCount;
-            set => _lagCount = value;
-        }
+	/// <summary>
+	/// ZXHawk: Core Class
+	/// * IInputPollable *
+	/// </summary>
+	public partial class ZXSpectrum : IInputPollable
+	{
+		public int LagCount
+		{
+			get => _lagCount;
+			set => _lagCount = value;
+		}
 
-        public bool IsLagFrame
-        {
-            get => _isLag;
-            set => _isLag = value;
-        }
+		public bool IsLagFrame
+		{
+			get => _isLag;
+			set => _isLag = value;
+		}
 
-        public IInputCallbackSystem InputCallbacks { get; }
+		public IInputCallbackSystem InputCallbacks { get; }
 
-        private int _lagCount = 0;
-        private bool _isLag = false;
-    }
+		private int _lagCount = 0;
+		private bool _isLag = false;
+	}
 }
--- BizHawk.Emulation.Cores/Computers/SinclairSpectrum/ZXSpectrum.Util.cs
+++ BizHawk.Emulation.Cores/Computers/SinclairSpectrum/ZXSpectrum.Util.cs
@@ -4,82 +4,82 @@ using System.Linq.Expressions;
 
 namespace BizHawk.Emulation.Cores.Computers.SinclairSpectrum
 {
-    /// <summary>
-    /// ZXHawk: Core Class
-    /// * Misc Utilities *
-    /// </summary>
-    public partial class ZXSpectrum
-    {
-        /// <summary>
-        /// Helper method that returns a single INT32 from a BitArray
-        /// </summary>
-        public static int GetIntFromBitArray(BitArray bitArray)
-        {
-            if (bitArray.Length > 32)
-                throw new ArgumentException("Argument length shall be at most 32 bits.");
+	/// <summary>
+	/// ZXHawk: Core Class
+	/// * Misc Utilities *
+	/// </summary>
+	public partial class ZXSpectrum
+	{
+		/// <summary>
+		/// Helper method that returns a single INT32 from a BitArray
+		/// </summary>
+		public static int GetIntFromBitArray(BitArray bitArray)
+		{
+			if (bitArray.Length > 32)
+				throw new ArgumentException("Argument length shall be at most 32 bits.");
 
-            int[] array = new int[1];
-            bitArray.CopyTo(array, 0);
-            return array[0];
-        }
+			int[] array = new int[1];
+			bitArray.CopyTo(array, 0);
+			return array[0];
+		}
 
-        /// <summary>
-        /// POKEs a memory bus address
-        /// </summary>
-        public void PokeMemory(ushort addr, byte value)
-        {
-            _machine.WriteBus(addr, value);
-        }
+		/// <summary>
+		/// POKEs a memory bus address
+		/// </summary>
+		public void PokeMemory(ushort addr, byte value)
+		{
+			_machine.WriteBus(addr, value);
+		}
 
 		/// <summary>
 		/// Called by MainForm so that the core label can display a more detailed tooltip about the emulated spectrum model
 		/// </summary>
-        public string GetMachineType()
-        {
-            string m = "";
-            switch (SyncSettings.MachineType)
-            {
-                case MachineType.ZXSpectrum16:
-                    m = "(Sinclair) ZX Spectrum 16K";
-                    break;
-                case MachineType.ZXSpectrum48:
-                    m = "(Sinclair) ZX Spectrum 48K";
-                    break;
-                case MachineType.ZXSpectrum128:
-                    m = "(Sinclair) ZX Spectrum 128K";
-                    break;
-                case MachineType.ZXSpectrum128Plus2:
-                    m = "(Amstrad) ZX Spectrum 128K +2";
-                    break;
-                case MachineType.ZXSpectrum128Plus2a:
-                    m = "(Amstrad) ZX Spectrum 128K +2a";
-                    break;
-                case MachineType.ZXSpectrum128Plus3:
-                    m = "(Amstrad) ZX Spectrum 128K +3";
-                    break;
+		public string GetMachineType()
+		{
+			string m = "";
+			switch (SyncSettings.MachineType)
+			{
+				case MachineType.ZXSpectrum16:
+					m = "(Sinclair) ZX Spectrum 16K";
+					break;
+				case MachineType.ZXSpectrum48:
+					m = "(Sinclair) ZX Spectrum 48K";
+					break;
+				case MachineType.ZXSpectrum128:
+					m = "(Sinclair) ZX Spectrum 128K";
+					break;
+				case MachineType.ZXSpectrum128Plus2:
+					m = "(Amstrad) ZX Spectrum 128K +2";
+					break;
+				case MachineType.ZXSpectrum128Plus2a:
+					m = "(Amstrad) ZX Spectrum 128K +2a";
+					break;
+				case MachineType.ZXSpectrum128Plus3:
+					m = "(Amstrad) ZX Spectrum 128K +3";
+					break;
 				case MachineType.Pentagon128:
 					m = "(Clone) Pentagon 128K";
 					break;
-            }
+			}
 
-            return m;
-        }
+			return m;
+		}
 
 		/// <summary>
 		/// Called by MainForm - dumps a close approximation of the Spectaculator SZX snapshot format
 		/// DEV use only - this is nowhere near accurate
 		/// </summary>
-        public byte[] GetSZXSnapshot()
-        {
-            return SZX.ExportSZX(_machine);
-        }
+		public byte[] GetSZXSnapshot()
+		{
+			return SZX.ExportSZX(_machine);
+		}
 
 		/// <summary>
 		/// Utility method to get MemberName from an object
 		/// </summary>
-        public static string GetMemberName<T, TValue>(Expression<Func<T, TValue>> memberAccess)
-        {
-            return ((MemberExpression)memberAccess.Body).Member.Name;
-        }
-    }
+		public static string GetMemberName<T, TValue>(Expression<Func<T, TValue>> memberAccess)
+		{
+			return ((MemberExpression)memberAccess.Body).Member.Name;
+		}
+	}
 }
