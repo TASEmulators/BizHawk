d3c048c13cdb6eda39ccc017b85a6d1671bb713b
adelikat <adelikat@tasvideos.org>
2020-02-18 07:22:56 -0600

    simplify atari 7800 mapper code

--- BizHawk.Emulation.Cores/Consoles/Atari/A7800Hawk/Mappers/MapperDefault.cs
+++ BizHawk.Emulation.Cores/Consoles/Atari/A7800Hawk/Mappers/MapperDefault.cs
@@ -15,46 +15,42 @@ namespace BizHawk.Emulation.Cores.Atari.A7800Hawk
 				{
 					return Core._hsram[addr - 0x1000];
 				}
+
 				return 0xFF;
 			}
-			else if (addr < 0x4000)
+
+			if (addr < 0x4000)
 			{
 				// could be either RAM mirror or ROM
 				if (addr >= 0x3000 && Core._hsbios != null)
 				{
 					return Core._hsbios[addr - 0x3000];
 				}
-				else
-				{
-					return Core.RAM[0x800 + addr & 0x7FF];
-				}
+
+				return Core.RAM[0x800 + addr & 0x7FF];
 			}
-			else if (addr < 0x8000 && Core.is_pokey)
+
+			if (addr < 0x8000 && Core.is_pokey)
 			{
 				return Core.pokey.ReadReg(addr & 0xF);
 			}
-			else
+
+			// cartridge and other OPSYS
+			if (Core._rom.Length >= 0x10000 - addr
+				&& Core.A7800_control_register.Bit(2))
 			{
-				// cartridge and other OPSYS
-				if ((Core._rom.Length >= 0x10000 - addr) && Core.A7800_control_register.Bit(2))
-				{
-					return Core._rom[Core._rom.Length - (0x10000 - addr)];
-				}
-				else if (addr >= (0x10000-Core._bios.Length) && !Core.A7800_control_register.Bit(2))
-				{
-					return Core._bios[addr - (0x10000 - Core._bios.Length)];		
-				}
-				else
-				{
-					return 0x00;
-				}
+				return Core._rom[Core._rom.Length - (0x10000 - addr)];
 			}
+
+			if (addr >= (0x10000-Core._bios.Length) && !Core.A7800_control_register.Bit(2))
+			{
+				return Core._bios[addr - (0x10000 - Core._bios.Length)];
+			}
+
+			return 0x00;
 		}
 
-		public override byte PeekMemory(ushort addr)
-		{
-			return ReadMemory(addr);
-		}
+		public override byte PeekMemory(ushort addr) => ReadMemory(addr);
 
 		public override void WriteMemory(ushort addr, byte value)
 		{
--- BizHawk.Emulation.Cores/Consoles/Atari/A7800Hawk/Mappers/MapperSGE.cs
+++ BizHawk.Emulation.Cores/Consoles/Atari/A7800Hawk/Mappers/MapperSGE.cs
@@ -7,7 +7,7 @@ namespace BizHawk.Emulation.Cores.Atari.A7800Hawk
 	// Have to add 1 to bank number to get correct bank value
 	public class MapperSGE : MapperBase
 	{
-		public byte bank = 0;
+		private byte _bank;
 
 		public override byte ReadMemory(ushort addr)
 		{
@@ -18,65 +18,59 @@ namespace BizHawk.Emulation.Cores.Atari.A7800Hawk
 				{
 					return Core._hsram[addr - 0x1000];
 				}
+
 				return 0xFF;
 			}
-			else if (addr < 0x4000)
+
+			if (addr < 0x4000)
 			{
 				// could be either RAM mirror or ROM
 				if (addr >= 0x3000 && Core._hsbios != null)
 				{
 					return Core._hsbios[addr - 0x3000];
 				}
-				else if (Core.is_pokey)
+
+				if (Core.is_pokey)
 				{
 					return Core.pokey.ReadReg(addr & 0xF);
 				}
-				else
-				{
-					return Core.RAM[0x800 + addr & 0x7FF];
-				}
+
+				return Core.RAM[0x800 + addr & 0x7FF];
 			}
-			else
+
+			// cartridge and other OPSYS
+			if (addr >= 0x10000 - Core._bios.Length && !Core.A7800_control_register.Bit(2))
 			{
-				// cartridge and other OPSYS
-				if (addr >= (0x10000 - Core._bios.Length) && !Core.A7800_control_register.Bit(2))
-				{
-					return Core._bios[addr - (0x10000 - Core._bios.Length)];
-				}
-				else
-				{
-					if (addr >=0xC000)
-					{
-						// bank 7 is fixed
-						return Core._rom[Core._rom.Length - (0x10000 - addr)];
-					}
-					else if (addr >= 0x8000)
-					{
-						// return whatever bank is there
-						// but remember we need to add 1 to adjust for the extra bank at the beginning
-						int temp_addr = addr - 0x8000;
-						return Core._rom[temp_addr + (bank + 1) * 0x4000];
-					}
-					/*
-					else if (Core.is_pokey)
-					{
-						return Core.pokey.ReadReg(addr & 0xF);
-					}
-					*/
-					else
-					{
-					// return the 16k extra ROM (located at beginning of file)
-					int temp_addr = addr - 0x4000;
-						return Core._rom[temp_addr];
-					}				
-				}
+				return Core._bios[addr - (0x10000 - Core._bios.Length)];
 			}
+
+			if (addr >=0xC000)
+			{
+				// bank 7 is fixed
+				return Core._rom[Core._rom.Length - (0x10000 - addr)];
+			}
+
+			int tempAddr;
+			if (addr >= 0x8000)
+			{
+				// return whatever bank is there
+				// but remember we need to add 1 to adjust for the extra bank at the beginning
+				tempAddr = addr - 0x8000;
+				return Core._rom[tempAddr + (_bank + 1) * 0x4000];
+			}
+			/*
+			if (Core.is_pokey)
+			{
+				return Core.pokey.ReadReg(addr & 0xF);
+			}
+			*/
+
+			// return the 16k extra ROM (located at beginning of file)
+			tempAddr = addr - 0x4000;
+			return Core._rom[tempAddr];
 		}
 
-		public override byte PeekMemory(ushort addr)
-		{
-			return ReadMemory(addr);
-		}
+		public override byte PeekMemory(ushort addr) => ReadMemory(addr);
 
 		public override void WriteMemory(ushort addr, byte value)
 		{
@@ -108,7 +102,7 @@ namespace BizHawk.Emulation.Cores.Atari.A7800Hawk
 				// cartridge and other OPSYS
 				if (addr>=0x8000)
 				{
-					bank = (byte)(value & 0x7);
+					_bank = (byte)(value & 0x7);
 				}
 				else if (Core.is_pokey)
 				{
@@ -124,7 +118,7 @@ namespace BizHawk.Emulation.Cores.Atari.A7800Hawk
 
 		public override void SyncState(Serializer ser)
 		{
-			ser.Sync("Bank", ref bank);
+			ser.Sync("Bank", ref _bank);
 		}
 	}
 }
