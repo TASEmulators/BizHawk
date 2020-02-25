35697aaefa425e6525acdd3d9a0971258d3503cc
adelikat <adelikat@tasvideos.org>
2020-02-25 13:15:52 -0600

    cleanup Page.cs

--- Bizware/BizHawk.Bizware.BizwareGL/Borrowed/BitmapFontParser/Page.cs
+++ Bizware/BizHawk.Bizware.BizwareGL/Borrowed/BitmapFontParser/Page.cs
@@ -1,37 +1,24 @@
-﻿//public domain assumed from cyotek.com
+﻿using System.IO;
 
-using System.IO;
-
+// public domain assumed from cyotek.com
 namespace Cyotek.Drawing.BitmapFont
 {
-  public struct Page
-  {
-    #region  Public Constructors
+	public struct Page
+	{
+		public Page(int id, string fileName)
+		  : this()
+		{
+			FileName = fileName;
+			Id = id;
+		}
 
-    public Page(int id, string fileName)
-      : this()
-    {
-      this.FileName = fileName;
-      this.Id = id;
-    }
+		public override string ToString()
+		{
+			return $"{Id} ({Path.GetFileName(FileName)})";
+		}
 
-    #endregion  Public Constructors
+		public string FileName { get; set; }
 
-    #region  Public Methods
-
-    public override string ToString()
-    {
-      return string.Format("{0} ({1})", this.Id, Path.GetFileName(this.FileName));
-    }
-
-    #endregion  Public Methods
-
-    #region  Public Properties
-
-    public string FileName { get; set; }
-
-    public int Id { get; set; }
-
-    #endregion  Public Properties
-  }
+		public int Id { get; set; }
+	}
 }
