69ad9050d2a6bf135fedd7506557f68719cf267f
adelikat <adelikat@tasvideos.org>
2020-02-28 17:52:04 -0600

    proof of concept - add these attributes to custom control values that hard code an base property that would otherwise be settable by the designer

--- BizHawk.Client.EmuHawk/CustomControls/AutosizedLabel.cs
+++ BizHawk.Client.EmuHawk/CustomControls/AutosizedLabel.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel;
 using System.Windows.Forms;
 
 namespace BizHawk.Client.EmuHawk.CustomControls
 {
 	public class AutosizedLabel : Label
 	{
+		
+		[Browsable(false)]
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
 		public override bool AutoSize => true;
 
 		public AutosizedLabel() : base() {
