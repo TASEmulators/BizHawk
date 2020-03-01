7415dfc4a991cf49784452504acf1fddcd1753c8
adelikat <adelikat@tasvideos.org>
2020-03-01 13:22:35 -0600

    remove unused NitsujaSynchronizer, even though it was the default, the only usage of ISynchronizingAudioBuffer is the vecna one

--- BizHawk.Emulation.Common/Sound/Utilities/Metaspu.cs
+++ BizHawk.Emulation.Common/Sound/Utilities/Metaspu.cs
@@ -1,23 +1,15 @@
-﻿using System;
-
-namespace BizHawk.Emulation.Common
+﻿namespace BizHawk.Emulation.Common
 {
 	public static class Metaspu
 	{
 		public static ISynchronizingAudioBuffer MetaspuConstruct(ESynchMethod method)
 		{
-			return method switch
-			{
-				ESynchMethod.Vecna => new VecnaSynchronizer(),
-				ESynchMethod.Nitsuja => new NitsujaSynchronizer(),
-				_ => new NitsujaSynchronizer()
-			};
+			return new VecnaSynchronizer();
 		}
 	}
 
 	public enum ESynchMethod
 	{
-		Nitsuja, // nitsuja's
 		Vecna // vecna
 	}
 }
