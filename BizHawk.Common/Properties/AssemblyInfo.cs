489084242fb3090b1e3792ea44039daa9c7ece85
YoshiRulz <OSSYoshiRulz@gmail.com>
2020-03-20 09:37:31 +1000

    Move [AssemblyVersion]/[AssemblyFileVersion] to VersionInfo.cs
    ApiHawk was using hardcoded "1.1.0.0". BizInvoke, Bizware.BizwareGL, and
    WinForms.Controls were not annotated, and had the default "0.0.0.0". All four of
    these projects did not include the Version source files.

--- BizHawk.Common/Properties/AssemblyInfo.cs
+++ BizHawk.Common/Properties/AssemblyInfo.cs
@@ -19,16 +19,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f8e9ad29-89d8-478b-a699-ed3369703b6a")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion(VersionInfo.MainVersion + "." + SubWCRev.SVN_REV)]
-[assembly: AssemblyFileVersion(VersionInfo.MainVersion + "." + SubWCRev.SVN_REV)]
