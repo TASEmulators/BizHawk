1f6322806114614e35ac8e79dfa674feedaf2e1a
adelikat <adelikat@tasvideos.org>
2020-03-22 11:41:48 -0500

    C64 virtualpad schema - simplify

--- BizHawk.Client.EmuHawk/tools/VirtualPads/schema/C64Schema.cs
+++ BizHawk.Client.EmuHawk/tools/VirtualPads/schema/C64Schema.cs
@@ -48,338 +48,81 @@ namespace BizHawk.Client.EmuHawk
 				DefaultSize = new Size(500, 150),
 				Buttons = new[]
 				{
-					new ButtonSchema(16, 18)
-					{
-						Name = "Key Left Arrow",
-						DisplayName = "←"
-					},
-					new ButtonSchema(46, 18)
-					{
-						Name = "Key 1",
-						DisplayName = "1"
-					},
-					new ButtonSchema(70, 18)
-					{
-						Name = "Key 2",
-						DisplayName = "2"
-					},
-					new ButtonSchema(94, 18)
-					{
-						Name = "Key 3",
-						DisplayName = "3"
-					},
-					new ButtonSchema(118, 18)
-					{
-						Name = "Key 4",
-						DisplayName = "4"
-					},
-					new ButtonSchema(142, 18)
-					{
-						Name = "Key 5",
-						DisplayName = "5"
-					},
-					new ButtonSchema(166, 18)
-					{
-						Name = "Key 6",
-						DisplayName = "6"
-					},
-					new ButtonSchema(190, 18)
-					{
-						Name = "Key 7",
-						DisplayName = "7"
-					},
-					new ButtonSchema(214, 18)
-					{
-						Name = "Key 8",
-						DisplayName = "8"
-					},
-					new ButtonSchema(238, 18)
-					{
-						Name = "Key 9",
-						DisplayName = "9"
-					},
-					new ButtonSchema(262, 18)
-					{
-						Name = "Key 0",
-						DisplayName = "0"
-					},
-					new ButtonSchema(286, 18)
-					{
-						Name = "Key Plus",
-						DisplayName = "+"
-					},
-					new ButtonSchema(310, 18)
-					{
-						Name = "Key Minus",
-						DisplayName = "-"
-					},
-					new ButtonSchema(330, 18)
-					{
-						Name = "Key Pound",
-						DisplayName = "£"
-					},
-					new ButtonSchema(354, 18)
-					{
-						Name = "Key Clear/Home",
-						DisplayName = "C/H"
-					},
-					new ButtonSchema(392, 18)
-					{
-						Name = "Key Insert/Delete",
-						DisplayName = "I/D"
-					},
-					new ButtonSchema(450, 18)
-					{
-						Name = "Key F1",
-						DisplayName = "F 1"
-					},
-					new ButtonSchema(450, 42)
-					{
-						Name = "Key F3",
-						DisplayName = "F 3"
-					},
-					new ButtonSchema(450, 66)
-					{
-						Name = "Key F5",
-						DisplayName = "F 5"
-					},
-					new ButtonSchema(450, 90)
-					{
-						Name = "Key F7",
-						DisplayName = "F 7"
-					},
-					new ButtonSchema(16, 42)
-					{
-						Name = "Key Control",
-						DisplayName = "CTRL"
-					},
-					new ButtonSchema(62, 42)
-					{
-						Name = "Key Q",
-						DisplayName = "Q"
-					},
-					new ButtonSchema(88, 42)
-					{
-						Name = "Key W",
-						DisplayName = "W"
-					},
-					new ButtonSchema(116, 42)
-					{
-						Name = "Key E",
-						DisplayName = "E"
-					},
-					new ButtonSchema(140, 42)
-					{
-						Name = "Key R",
-						DisplayName = "R"
-					},
-					new ButtonSchema(166, 42)
-					{
-						Name = "Key T",
-						DisplayName = "T"
-					},
-					new ButtonSchema(190, 42)
-					{
-						Name = "Key Y",
-						DisplayName = "Y"
-					},
-					new ButtonSchema(214, 42)
-					{
-						Name = "Key U",
-						DisplayName = "U"
-					},
-					new ButtonSchema(240, 42)
-					{
-						Name = "Key I",
-						DisplayName = "I"
-					},
-					new ButtonSchema(260, 42)
-					{
-						Name = "Key O",
-						DisplayName = "O"
-					},
-					new ButtonSchema(286, 42)
-					{
-						Name = "Key P",
-						DisplayName = "P"
-					},
-					new ButtonSchema(310, 42)
-					{
-						Name = "Key At",
-						DisplayName = "@"
-					},
-					new ButtonSchema(338, 42)
-					{
-						Name = "Key Asterisk",
-						DisplayName = "*"
-					},
-					new ButtonSchema(360, 42)
-					{
-						Name = "Key Up Arrow",
-						DisplayName = "↑"
-					},
-					new ButtonSchema(390, 42)
-					{
-						Name = "Key Restore",
-						DisplayName = "RST"
-					},
-					new ButtonSchema(12, 66)
-					{
-						Name = "Key Run/Stop",
-						DisplayName = "R/S"
-					},
-					new ButtonSchema(50, 66)
-					{
-						Name = "Key Lck",
-						DisplayName = "Lck"
-					},
-					new ButtonSchema(86, 66)
-					{
-						Name = "Key A",
-						DisplayName = "A"
-					},
-					new ButtonSchema(110, 66)
-					{
-						Name = "Key S",
-						DisplayName = "S"
-					},
-					new ButtonSchema(134, 66)
-					{
-						Name = "Key D",
-						DisplayName = "D"
-					},
-					new ButtonSchema(160, 66)
-					{
-						Name = "Key F",
-						DisplayName = "F"
-					},
-					new ButtonSchema(184, 66)
-					{
-						Name = "Key G",
-						DisplayName = "G"
-					},
-					new ButtonSchema(210, 66)
-					{
-						Name = "Key H",
-						DisplayName = "H"
-					},
-					new ButtonSchema(236, 66)
-					{
-						Name = "Key J",
-						DisplayName = "J"
-					},
-					new ButtonSchema(258, 66)
-					{
-						Name = "Key K",
-						DisplayName = "K"
-					},
-					new ButtonSchema(282, 66)
-					{
-						Name = "Key L",
-						DisplayName = "L"
-					},
-					new ButtonSchema(306, 66)
-					{
-						Name = "Key Colon",
-						DisplayName = ":"
-					},
-					new ButtonSchema(326, 66)
-					{
-						Name = "Key Semicolon",
-						DisplayName = ";"
-					},
-					new ButtonSchema(346, 66)
-					{
-						Name = "Key Equal",
-						DisplayName = "="
-					},
-					new ButtonSchema(370, 66)
-					{
-						Name = "Key Return",
-						DisplayName = "Return"
-					},
-					new ButtonSchema(8, 90)
-					{
-						Name = "Key Commodore",
-						DisplayName = "C64"
-					},
-					new ButtonSchema(44, 90)
-					{
-						Name = "Key Left Shift",
-						DisplayName = "Shift"
-					},
-					new ButtonSchema(82, 90)
-					{
-						Name = "Key Z",
-						DisplayName = "Z"
-					},
-					new ButtonSchema(106, 90)
-					{
-						Name = "Key X",
-						DisplayName = "X"
-					},
-					new ButtonSchema(130, 90)
-					{
-						Name = "Key C",
-						DisplayName = "C"
-					},
-					new ButtonSchema(154, 90)
-					{
-						Name = "Key V",
-						DisplayName = "V"
-					},
-					new ButtonSchema(178, 90)
-					{
-						Name = "Key B",
-						DisplayName = "B"
-					},
-					new ButtonSchema(202, 90)
-					{
-						Name = "Key N",
-						DisplayName = "N"
-					},
-					new ButtonSchema(226, 90)
-					{
-						Name = "Key M",
-						DisplayName = "M"
-					},
-					new ButtonSchema(252, 90)
-					{
-						Name = "Key Comma",
-						DisplayName = ","
-					},
-					new ButtonSchema(272, 90)
-					{
-						Name = "Key Period",
-						DisplayName = "."
-					},
-					new ButtonSchema(292, 90)
-					{
-						Name = "Key Slash",
-						DisplayName = "/"
-					},
-					new ButtonSchema(314, 90)
-					{
-						Name = "Key Right Shift",
-						DisplayName = "Shift"
-					},
-					new ButtonSchema(352, 90)
-					{
-						Name = "Key Cursor Up/Down",
-						DisplayName = "Csr U"
-					},
-					new ButtonSchema(396, 90)
-					{
-						Name = "Key Cursor Left/Right",
-						DisplayName = "Csr L"
-					},
-					new ButtonSchema(120, 114)
-					{
-						Name = "Key Space",
-						DisplayName = "                          Space                          "
-					}
+					Key(16, 18, "Left Arrow", "←"),
+					Key(46, 18, "1"),
+					Key(70, 18, "2"),
+					Key(94, 18, "3"),
+					Key(118, 18, "4"),
+					Key(142, 18, "5"),
+					Key(166, 18, "6"),
+					Key(190, 18, "7"),
+					Key(214, 18, "8"),
+					Key(238, 18, "9"),
+					Key(262, 18, "0"),
+					Key(286, 18, "Plus", "+"),
+					Key(310, 18, "Minus", "-"),
+					Key(330, 18, "Pound", "£"),
+					Key(354, 18, "Key Clear/Home", "C/H"),
+					Key(392, 18, "Key Insert/Delete", "I/D"),
+					Key(450, 18, "F1", "F 1"),
+					Key(450, 42, "F3", "F 3"),
+					Key(450, 66, "F5", "F 5"),
+					Key(450, 90, "F7", "F 7"),
+					Key(16, 42, "Control", "CTRL"),
+					Key(62, 42, "Q"),
+					Key(88, 42, "W"),
+					Key(116, 42, "E"),
+					Key(140, 42, "R"),
+					Key(166, 42, "T"),
+					Key(190, 42, "Y"),
+					Key(214, 42, "U"),
+					Key(240, 42, "I"),
+					Key(260, 42, "O"),
+					Key(286, 42, "P"),
+					Key(310, 42, "At", "@"),
+					Key(338, 42, "Asterisk", "*"),
+					Key(360, 42, "Up Arrow", "↑"),
+					Key(390, 42, "Restore", "RST"),
+					Key(12, 66, "Run/Stop", "R/S"),
+					Key(50, 66, "Lck"),
+					Key(86, 66, "A"),
+					Key(110, 66, "S"),
+					Key(134, 66, "D"),
+					Key(160, 66, "F"),
+					Key(184, 66, "G"),
+					Key(210, 66, "H"),
+					Key(236, 66, "J"),
+					Key(258, 66, "K"),
+					Key(282, 66, "L"),
+					Key(306, 66, "Colon", ":"),
+					Key(326, 66, "Semicolon", ";"),
+					Key(346, 66, "Equal", "="),
+					Key(370, 66, "Return"),
+					Key(8, 90, "Commodore", "C64"),
+					Key(44, 90, "Left Shift", "Shift"),
+					Key(82, 90, "Z"),
+					Key(106, 90, "X"),
+					Key(130, 90, "C"),
+					Key(154, 90, "V"),
+					Key(178, 90, "B"),
+					Key(202, 90, "N"),
+					Key(226, 90, "M"),
+					Key(252, 90, "Comma", ","),
+					Key(272, 90, "Period", "."),
+					Key(292, 90, "Slash", "/"),
+					Key(314, 90, "Right Shift", "Shift"),
+					Key(352, 90, "Cursor Up/Down", "Csr U"),
+					Key(396, 90, "Cursor Left/Right", "Csr L"),
+					Key(120, 114, "Space", "                          Space                          ")
 				}
 			};
 		}
+
+		private static ButtonSchema Key(int x, int y, string name, string displayName = null)
+			=> new ButtonSchema(x, y)
+			{
+				Name = "Key " + name,
+				DisplayName = displayName ?? name
+			};
 	}
 }
