TASEmulators/BizHawk

commit 95bc69b4487fe6787606cbe459151ba339e8bd36

some prepwork for opengl texture ID importing (skip rendertarget resolve on OGL display method) but there are still problems and it can't be enabled yet

BizHawk.Client.EmuHawk/DisplayManager/DisplayManager.cs
@@ -348,7 +348,7 @@ namespace BizHawk.Client.EmuHawk
 		/// Then it will stuff it into the bound PresentationPanel.
 		/// ---
 		/// If the int[] is size=1, then it contains an openGL texture ID (and the size should be as specified from videoProvider)
-		/// Don't worry about the case where the frontend isnt using opengl; it isnt supported yet, and it will be my responsibility to deal with anyway
+		/// Don't worry about the case where the frontend isnt using opengl; DisplayManager deals with it
 		/// </summary>
 		public void UpdateSource(IVideoProvider videoProvider)
 		{
@@ -362,6 +362,20 @@ namespace BizHawk.Client.EmuHawk
 				
 			};
 			UpdateSourceInternal(job);
+		}
+
+		public BitmapBuffer RenderVideoProvider(IVideoProvider videoProvider)
+		{
+			var job = new JobInfo
+			{
+				videoProvider = videoProvider,
+				simulate = false,
+				chain_outsize = new Size(videoProvider.BufferWidth, videoProvider.BufferHeight),
+				offscreen = true,
+				includeOSD = false
+			};
+			UpdateSourceInternal(job);
+			return job.offscreenBB;
 		}
 
 		public BitmapBuffer RenderOffscreen(IVideoProvider videoProvider, bool includeOSD)
@@ -653,6 +667,7 @@ namespace BizHawk.Client.EmuHawk
 			fPresent.TextureSize = new Size(bufferWidth, bufferHeight);
 			fPresent.BackgroundColor = videoProvider.BackgroundColor;
 			fPresent.GuiRenderer = Renderer;
+			fPresent.Flip = isGlTextureId;
 			fPresent.Config_FixAspectRatio = Global.Config.DispFixAspectRatio;
 			fPresent.Config_FixScaleInteger = Global.Config.DispFixScaleInteger;
 			fPresent.Padding = ClientExtraPadding;
@@ -681,7 +696,7 @@ namespace BizHawk.Client.EmuHawk
 			//begin rendering on this context
 			//should this have been done earlier?
 			//do i need to check this on an intel video card to see if running excessively is a problem? (it used to be in the FinalTarget command below, shouldnt be a problem)
-			//GraphicsControl.Begin();
+			//GraphicsControl.Begin(); //CRITICAL POINT for yabause+GL
 
 			GlobalWin.GL.BeginScene();
 
@@ -736,6 +751,7 @@ namespace BizHawk.Client.EmuHawk
 			if (job.offscreen)
 			{
 				job.offscreenBB = rtCurr.Texture2d.Resolve();
+				job.offscreenBB.DiscardAlpha();
 			}
 			else
 			{

BizHawk.Client.Common/SavestateManager.cs
@@ -38,18 +38,28 @@ namespace BizHawk.Client.Common
 				{
 					var vp = Global.Emulator.VideoProvider();
 					var buff = vp.GetVideoBuffer();
-
-					int out_w = vp.BufferWidth;
-					int out_h = vp.BufferHeight;
-
-					// if buffer is too big, scale down screenshot
-					if (!Global.Config.NoLowResLargeScreenshotWithStates && buff.Length >= Global.Config.BigScreenshotSize)
+					if (buff.Length == 1)
 					{
-						out_w /= 2;
-						out_h /= 2;
+						//is a hacky opengl texture ID. can't handle this now!
+						//need to discuss options
+						//1. cores must be able to provide a pixels videoprovider in addition to a texture ID, on command (not very hard overall but interface changing and work per core)
+						//2. SavestateManager must be setup with a mechanism for resolving texture IDs (even less work, but sloppy)
+						//There are additional problems with AVWriting. They depend on VideoProvider providing pixels.
 					}
-					using (new SimpleTime("Save Framebuffer"))
-						bs.PutLump(BinaryStateLump.Framebuffer, (s) => QuickBmpFile.Save(Global.Emulator.VideoProvider(), s, out_w, out_h));
+					else
+					{
+						int out_w = vp.BufferWidth;
+						int out_h = vp.BufferHeight;
+
+						// if buffer is too big, scale down screenshot
+						if (!Global.Config.NoLowResLargeScreenshotWithStates && buff.Length >= Global.Config.BigScreenshotSize)
+						{
+							out_w /= 2;
+							out_h /= 2;
+						}
+						using (new SimpleTime("Save Framebuffer"))
+							bs.PutLump(BinaryStateLump.Framebuffer, (s) => QuickBmpFile.Save(Global.Emulator.VideoProvider(), s, out_w, out_h));
+					}
 				}
 
 				if (Global.MovieSession.Movie.IsActive)

BizHawk.Client.EmuHawk/DisplayManager/Filters/Gui.cs
@@ -178,6 +178,7 @@ namespace BizHawk.Client.EmuHawk.Filters
 		public Size TextureSize, VirtualTextureSize;
 		public int BackgroundColor;
 		public IGuiRenderer GuiRenderer;
+		public bool Flip;
 		public IGL GL;
 		bool nop;
 		LetterboxingLogic LL;
@@ -237,6 +238,8 @@ namespace BizHawk.Client.EmuHawk.Filters
 			if (state.SurfaceFormat.Size != OutputSize)
 				need = true;
 			if (FilterOption != eFilterOption.None)
+				need = true;
+			if (Flip)
 				need = true;
 
 			if (!need)
@@ -318,7 +321,13 @@ namespace BizHawk.Client.EmuHawk.Filters
 			}
 
 
-			GuiRenderer.Draw(InputTexture,LL.vx,LL.vy,LL.vw,LL.vh);
+			GuiRenderer.Modelview.Translate(LL.vx, LL.vy);
+			if (Flip)
+			{
+				GuiRenderer.Modelview.Scale(1, -1);
+				GuiRenderer.Modelview.Translate(0, -LL.vh);
+			}
+			GuiRenderer.Draw(InputTexture,0,0,LL.vw,LL.vh);
 
 			GuiRenderer.End();
 		}

BizHawk.Client.EmuHawk/MainForm.cs
@@ -916,7 +916,6 @@ namespace BizHawk.Client.EmuHawk
 		{
 			using (var bb = GlobalWin.DisplayManager.RenderOffscreen(Global.Emulator.VideoProvider(), Global.Config.Screenshot_CaptureOSD))
 			{
-				bb.DiscardAlpha();
 				using (var img = bb.ToSysdrawingBitmap())
 					Clipboard.SetImage(img);
 			}
@@ -1895,11 +1894,9 @@ namespace BizHawk.Client.EmuHawk
 			}
 		}
 
-		private static unsafe BitmapBuffer MakeScreenshotImage()
+		private static BitmapBuffer MakeScreenshotImage()
 		{
-			var bb = new BitmapBuffer(Global.Emulator.VideoProvider().BufferWidth, Global.Emulator.VideoProvider().BufferHeight, Global.Emulator.VideoProvider().GetVideoBuffer());
-			bb.DiscardAlpha();
-			return bb;
+			return GlobalWin.DisplayManager.RenderVideoProvider(Global.Emulator.VideoProvider());
 		}
 
 		private void SaveSlotSelectedMessage()

BizHawk.Client.EmuHawk/Program.cs
@@ -127,7 +127,7 @@ namespace BizHawk.Client.EmuHawk
 			GlobalWin.IGL_GL = new Bizware.BizwareGL.Drivers.OpenTK.IGL_TK(2,0,false);
 
 			//setup the GL context manager, needed for coping with multiple opengl cores vs opengl display method
-			GLManager.CreateInstance();
+			GLManager.CreateInstance(GlobalWin.IGL_GL);
 			GlobalWin.GLManager = GLManager.Instance;
 			GlobalWin.CR_GL = GlobalWin.GLManager.GetContextForIGL(GlobalWin.GL);
 

BizHawk.Emulation.Cores/Consoles/Sega/Saturn/Yabause.cs
@@ -86,7 +86,7 @@ namespace BizHawk.Emulation.Cores.Sega.Saturn
 
 		public IEmulatorServiceProvider ServiceProvider { get; private set; }
 
-		static object glContext;
+		object glContext;
 
 		void ActivateGL()
 		{
@@ -329,7 +329,14 @@ namespace BizHawk.Emulation.Cores.Sega.Saturn
 		#region IVideoProvider
 
 		int[] VideoBuffer = new int[704 * 512];
-		public int[] GetVideoBuffer() { return VideoBuffer; }
+		int[] TextureIdBuffer = new int[1]; //todo
+		public int[] GetVideoBuffer() {
+			//doesn't work yet
+			//if (SyncSettings.UseGL)
+			//  return new[] { VideoBuffer[0] };
+			//else
+				return VideoBuffer;
+		}
 		public int VirtualWidth { get { return BufferWidth; } }
 		public int VirtualHeight { get { return BufferHeight; } }
 		public int BufferWidth { get; private set; }

Bizware/BizHawk.Bizware.BizwareGL.OpenTK/IGL_TK.cs
@@ -23,6 +23,7 @@ using BizHawk.Bizware.BizwareGL;
 using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
+using otkg = OpenTK.Graphics;
 
 namespace BizHawk.Bizware.BizwareGL.Drivers.OpenTK
 {
@@ -496,7 +497,7 @@ namespace BizHawk.Bizware.BizwareGL.Drivers.OpenTK
 
 		public Texture2d WrapGLTexture2d(IntPtr glTexId, int width, int height)
 		{
-			return new Texture2d(this as IGL, glTexId, width, height);
+			return new Texture2d(this as IGL, glTexId.ToInt32(), width, height);
 		}
 
 		public void LoadTextureData(Texture2d tex, BitmapBuffer bmp)
@@ -590,6 +591,7 @@ namespace BizHawk.Bizware.BizwareGL.Drivers.OpenTK
 			var bb = new BitmapBuffer(tex.IntWidth, tex.IntHeight);
 			var bmpdata = bb.LockBits();
 			GL.GetTexImage(TextureTarget.Texture2D, 0, PixelFormat.Bgra, PixelType.UnsignedByte, bmpdata.Scan0);
+			var err = GL.GetError();
 			bb.UnlockBits(bmpdata);
 			return bb;
 		}

Bizware/BizHawk.Bizware.BizwareGL/IGuiRenderer.cs
@@ -35,8 +35,11 @@ namespace BizHawk.Bizware.BizwareGL
 		/// draws the specified Art resource with the specified offset, with the specified size. This could be tricky if youve applied other rotate or scale transforms first.
 		/// </summary>
 		void Draw(Art art, float x, float y, float width, float height);
-		void Draw(Texture2d art, float x, float y, float width, float height);
 
+		/// <summary>
+		/// draws the specified Texture with the specified offset, with the specified size. This could be tricky if youve applied other rotate or scale transforms first.
+		/// </summary>
+		void Draw(Texture2d art, float x, float y, float width, float height);
 
 		/// <summary>
 		/// draws the specified texture2d resource.

commit 90246dd4c124663ae391cc71a7f2140b58525584

fix raw screenshots, which previously were a mixed up mess of user filters and sysdrawing downscaling. fixes #1902.
This was broken by 95bc69b4487fe6787606cbe459151ba339e8bd36 (or else it was benign at that time and broken later, but the fundamental change was here).

This caused the raw screenshots to go through the displaymanager instead of very simply converting the core's videoprovider at that point. The reason was because only the displaymanager had the power to render an opengl core's texture ID.

In this commit, I fixed it so that displaymanager had the power to render a videoprovider WITHOUT the user's scaling filters

BizHawk.Client.EmuHawk/DisplayManager/DisplayManager.cs
@@ -222,7 +222,7 @@ namespace BizHawk.Client.EmuHawk
 			return padding;
 		}
 
-		FilterProgram BuildDefaultChain(Size chainInSize, Size chainOutSize, bool includeOSD)
+		FilterProgram BuildDefaultChain(Size chainInSize, Size chainOutSize, bool includeOSD, bool includeUserFilters)
 		{
 			// select user special FX shader chain
 			var selectedChainProperties = new Dictionary<string, object>();
@@ -231,6 +231,8 @@ namespace BizHawk.Client.EmuHawk
 			{
 				selectedChain = ShaderChain_hq2x;
 			}
+
+			//ZOOM
 
 			if (Global.Config.TargetDisplayFilter == 2 && ShaderChain_scanlines != null && ShaderChain_scanlines.Available)
 			{
@@ -242,6 +244,9 @@ namespace BizHawk.Client.EmuHawk
 			{
 				selectedChain = ShaderChain_user;
 			}
+
+			if (!includeUserFilters)
+				selectedChain = null;
 
 			var fPresent = new Filters.FinalPresentation(chainOutSize);
 			var fInput = new Filters.SourceImage(chainInSize);
@@ -437,7 +442,8 @@ namespace BizHawk.Client.EmuHawk
 				VideoProvider = videoProvider,
 				Simulate = displayNothing,
 				ChainOutsize = GraphicsControl.Size,
-				IncludeOSD = true
+				IncludeOSD = true,
+				IncludeUserFilters = true
 			};
 			UpdateSourceInternal(job);
 		}
@@ -456,7 +462,8 @@ namespace BizHawk.Client.EmuHawk
 				Simulate = false,
 				ChainOutsize = targetSize,
 				Offscreen = true,
-				IncludeOSD = false
+				IncludeOSD = false,
+				IncludeUserFilters = false
 			};
 			UpdateSourceInternal(job);
 			return job.OffscreenBb;
@@ -473,7 +480,8 @@ namespace BizHawk.Client.EmuHawk
 				Simulate = false,
 				ChainOutsize = GraphicsControl.Size,
 				Offscreen = true,
-				IncludeOSD = includeOSD
+				IncludeOSD = includeOSD,
+				IncludeUserFilters = true,
 			};
 			UpdateSourceInternal(job);
 			return job.OffscreenBb;
@@ -657,7 +665,8 @@ namespace BizHawk.Client.EmuHawk
 			{
 				VideoProvider = fvp,
 				Simulate = true,
-				ChainOutsize = chainOutsize
+				ChainOutsize = chainOutsize,
+				IncludeUserFilters = true
 			};
 			var filterProgram = UpdateSourceInternal(job);
 
@@ -681,6 +690,7 @@ namespace BizHawk.Client.EmuHawk
 			public bool Offscreen;
 			public BitmapBuffer OffscreenBb;
 			public bool IncludeOSD;
+			public bool IncludeUserFilters;
 		}
 
 		private FilterProgram UpdateSourceInternal(JobInfo job)
@@ -770,7 +780,7 @@ namespace BizHawk.Client.EmuHawk
 			//build the default filter chain and set it up with services filters will need
 			Size chainInsize = new Size(bufferWidth, bufferHeight);
 
-			var filterProgram = BuildDefaultChain(chainInsize, chainOutsize, job.IncludeOSD);
+			var filterProgram = BuildDefaultChain(chainInsize, chainOutsize, job.IncludeOSD, job.IncludeUserFilters);
 			filterProgram.GuiRenderer = Renderer;
 			filterProgram.GL = GL;
 