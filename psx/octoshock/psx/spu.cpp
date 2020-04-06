25a9c1e5d636b8b0d0e2792edd7100e50eb3b206
zeromus <zeromus@users.noreply.github.com>
2020-04-05 23:21:55 -0400

    update to Mednafen 0.9.39-unstable : spu, timer

--- psx/octoshock/psx/spu.cpp
+++ psx/octoshock/psx/spu.cpp
@@ -1,19 +1,25 @@
-/* Mednafen - Multi-system Emulator
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
+/******************************************************************************/
+/* Mednafen Sony PS1 Emulation Module                                         */
+/******************************************************************************/
+/* spu.cpp:
+**  Copyright (C) 2011-2016 Mednafen Team
+**
+** This program is free software; you can redistribute it and/or
+** modify it under the terms of the GNU General Public License
+** as published by the Free Software Foundation; either version 2
+** of the License, or (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software Foundation, Inc.,
+** 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#pragma GCC optimize ("unroll-loops")
 
 /* TODO:
 	Note to self: Emulating the SPU at more timing accuracy than sample, and emulating the whole SPU RAM write port FIFO thing and hypothetical periodic FIFO commit to
@@ -465,7 +471,7 @@ void PS_SPU::CacheEnvelope(SPU_Voice *voice)
  ADSR->SustainLevel = (Sl + 1) << 11;
 }
 
-void PS_SPU::ResetEnvelope(SPU_Voice *voice)
+INLINE void PS_SPU::ResetEnvelope(SPU_Voice *voice)
 {
  SPU_ADSR *ADSR = &voice->ADSR;
 
@@ -474,7 +480,7 @@ void PS_SPU::ResetEnvelope(SPU_Voice *voice)
  ADSR->Phase = ADSR_ATTACK;
 }
 
-void PS_SPU::ReleaseEnvelope(SPU_Voice *voice)
+INLINE void PS_SPU::ReleaseEnvelope(SPU_Voice *voice)
 {
  SPU_ADSR *ADSR = &voice->ADSR;
 
@@ -483,7 +489,7 @@ void PS_SPU::ReleaseEnvelope(SPU_Voice *voice)
 }
 
 
-void PS_SPU::RunEnvelope(SPU_Voice *voice)
+INLINE void PS_SPU::RunEnvelope(SPU_Voice *voice)
 {
  SPU_ADSR *ADSR = &voice->ADSR;
  int increment;
