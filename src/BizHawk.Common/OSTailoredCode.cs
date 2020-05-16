3e51bbe5c24f00a7e72a84276ce9da80c95792fe
YoshiRulz <OSSYoshiRulz@gmail.com>
2020-05-16 12:35:04 +1000

    Revert "Fix up Nullable<IntPtr> brain damage"
    This reverts commit aa8fe56ef9f1147c57c6731bf990471a1f4fff5c.

--- src/BizHawk.Common/OSTailoredCode.cs
+++ src/BizHawk.Common/OSTailoredCode.cs
@@ -34,7 +34,7 @@ namespace BizHawk.Common
 		{
 			int FreeByPtr(IntPtr hModule);
 
-			IntPtr GetProcAddrOrNull(IntPtr hModule, string procName);
+			IntPtr? GetProcAddrOrNull(IntPtr hModule, string procName);
 
 			/// <exception cref="InvalidOperationException">could not find symbol</exception>
 			IntPtr GetProcAddrOrThrow(IntPtr hModule, string procName);
@@ -61,18 +61,17 @@ namespace BizHawk.Common
 
 			public int FreeByPtr(IntPtr hModule) => dlclose(hModule);
 
-			public IntPtr GetProcAddrOrNull(IntPtr hModule, string procName)
+			public IntPtr? GetProcAddrOrNull(IntPtr hModule, string procName)
 			{
 				var p = dlsym(hModule, procName);
-				return p;
+				return p == IntPtr.Zero ? (IntPtr?) null : p;
 			}
 
 			public IntPtr GetProcAddrOrThrow(IntPtr hModule, string procName)
 			{
 				_ = dlerror(); // the Internet said to do this
 				var p = GetProcAddrOrNull(hModule, procName);
-				if (p != IntPtr.Zero)
-					return p;
+				if (p != null) return p.Value;
 				var errCharPtr = dlerror();
 				throw new InvalidOperationException($"error in {nameof(dlsym)}{(errCharPtr == IntPtr.Zero ? string.Empty : $": {Marshal.PtrToStringAnsi(errCharPtr)}")}");
 			}
@@ -105,19 +104,13 @@ namespace BizHawk.Common
 
 			public int FreeByPtr(IntPtr hModule) => FreeLibrary(hModule) ? 0 : 1;
 
-			public IntPtr GetProcAddrOrNull(IntPtr hModule, string procName)
+			public IntPtr? GetProcAddrOrNull(IntPtr hModule, string procName)
 			{
 				var p = GetProcAddress(hModule, procName);
-				return p;
+				return p == IntPtr.Zero ? (IntPtr?) null : p;
 			}
 
-			public IntPtr GetProcAddrOrThrow(IntPtr hModule, string procName)
-			{
-				var ret = GetProcAddrOrNull(hModule, procName);
-				if (ret == IntPtr.Zero)
-					throw new InvalidOperationException($"got null pointer from {nameof(GetProcAddress)}, error code: {GetLastError()}");
-				return ret;
-			}
+			public IntPtr GetProcAddrOrThrow(IntPtr hModule, string procName) => GetProcAddrOrNull(hModule, procName) ?? throw new InvalidOperationException($"got null pointer from {nameof(GetProcAddress)}, error code: {GetLastError()}");
 
 			public IntPtr? LoadOrNull(string dllToLoad)
 			{
