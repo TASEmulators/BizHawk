ff219321f70821db2e16346caca3adf4bbe25013
adelikat <adelikat@tasvideos.org>
2020-05-16 15:07:00 -0500

    update version info

--- Version/VersionInfo.cs
+++ Version/VersionInfo.cs
@@ -9,8 +9,8 @@ using System.Reflection;
 internal static class VersionInfo
 {
 	// keep this updated at every major release
-	public const string MainVersion = "2.4.1"; // Use numbers only or the new version notification won't work
-	public const string ReleaseDate = "May 2, 2020";
+	public const string MainVersion = "2.4.2"; // Use numbers only or the new version notification won't work
+	public const string ReleaseDate = "May 17, 2020";
 	public const string HomePage = "http://tasvideos.org/BizHawk.html";
 	public static readonly bool DeveloperBuild = false;
 
