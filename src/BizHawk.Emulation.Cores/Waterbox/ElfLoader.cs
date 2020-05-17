48157f1955d9e373c703a2a528d8da28115a2dc8
nattthebear <goyuken@gmail.com>
2020-05-17 11:29:16 -0400

    wip of ElfLoader, and move some things in PeRunner out into a new file

---
+++ src/BizHawk.Emulation.Cores/Waterbox/ElfLoader.cs
@@ -0,0 +1,343 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using BizHawk.BizInvoke;
+using BizHawk.Common;
+using ELFSharp.ELF;
+using ELFSharp.ELF.Sections;
+using ELFSharp.ELF.Segments;
+
+namespace BizHawk.Emulation.Cores.Waterbox
+{
+	public class ElfLoader : IImportResolver, IDisposable
+	{
+		private readonly ELF<ulong> _elf;
+		private readonly byte[] _elfHash;
+
+		private readonly List<SymbolEntry<ulong>> _allSymbols;
+		private readonly Dictionary<string, SymbolEntry<ulong>> _visibleSymbols;
+		private readonly Dictionary<string, Section<ulong>> _sectionsByName;
+		private readonly List<SymbolEntry<ulong>> _importSymbols;
+
+		private readonly Section<ulong> _imports;
+		private readonly Section<ulong> _sealed;
+		private readonly Section<ulong> _invisible;
+
+		private readonly List<Section<ulong>> _savedSections;
+
+		private readonly bool _skipCoreConsistencyCheck;
+		private readonly bool _skipMemoryConsistencyCheck;
+
+		private bool _everythingSealed;
+
+		public MemoryBlockBase Memory { get; private set; }
+
+		public string ModuleName { get; }
+
+		public ElfLoader(string moduleName, byte[] fileData, ulong assumedStart, bool skipCoreConsistencyCheck, bool skipMemoryConsistencyCheck)
+		{
+			ModuleName = moduleName;
+			_skipCoreConsistencyCheck = skipCoreConsistencyCheck;
+			_skipMemoryConsistencyCheck = skipMemoryConsistencyCheck;
+
+			_elfHash = WaterboxUtils.Hash(fileData);
+			_elf = ELFReader.Load<ulong>(new MemoryStream(fileData, false), true);
+
+			var loadsegs = _elf.Segments.Where(s => s.Type == SegmentType.Load);
+			var start = loadsegs.Min(s => s.Address);
+			start = WaterboxUtils.AlignDown(start);
+			var end = loadsegs.Max(s => s.Address + s.Size);
+			end = WaterboxUtils.AlignUp(start);
+			var size = end - start;
+
+			if (start != assumedStart)
+				throw new InvalidOperationException($"{nameof(assumedStart)} did not match actual origin in elf file");
+			
+			if (_elf.Sections.Any(s => s.Name.StartsWith(".rel")))
+				throw new InvalidOperationException("Elf has relocations!");
+
+			_allSymbols = ((ISymbolTable)_elf.GetSection(".symtab"))
+				.Entries
+				.Cast<SymbolEntry<ulong>>()
+				.ToList();
+
+			_sectionsByName = _elf.Sections
+				.ToDictionary(s => s.Name);
+			
+			_sectionsByName.TryGetValue(".wbxsyscall", out _imports);
+			_sectionsByName.TryGetValue(".sealed", out _sealed);
+			_sectionsByName.TryGetValue(".invis", out _invisible);
+
+			_savedSections = _elf.Sections
+				.Where(s => (s.Flags & SectionFlags.Allocatable) != 0 && (s.Flags & SectionFlags.Writable) != 0)
+				.Where(s => !IsSpecialReadonlySection(s) && s != _invisible)
+				.OrderBy(s => s.LoadAddress)
+				.ToList();
+
+			_visibleSymbols = _allSymbols
+				.Where(s => s.Binding == SymbolBinding.Global)
+				// TODO: Filter visibility https://github.com/konrad-kruczynski/elfsharp/issues/71
+				.ToDictionary(s => s.Name);
+			
+			_importSymbols = _visibleSymbols.Values
+				.Where(s => s.PointedSection == _imports)
+				.ToList();
+
+
+			Memory = MemoryBlock.CallPlatformCtor(start, size);
+			Memory.Activate();
+			Memory.Protect(Memory.Start, Memory.Size, MemoryBlockBase.Protection.RW);
+
+			foreach (var seg in loadsegs)
+			{
+				var data = seg.GetFileContents();
+				Marshal.Copy(data, 0, Z.US(seg.Address), Math.Min((int)seg.Size, (int)seg.FileSize));
+			}
+
+			PrintSections();
+			PrintGdbData();
+			PrintTopSavableSymbols();
+		}
+
+		private void PrintGdbData()
+		{
+			Console.WriteLine("GDB Symbol Load:");
+			var symload = $"add-sym {ModuleName} {_sectionsByName[".text"].LoadAddress}";
+			if (_sectionsByName.ContainsKey(".data"))
+				symload += $" -s .data {_sectionsByName[".data"].LoadAddress}";
+			if (_sectionsByName.ContainsKey(".bss"))
+				symload += $" -s .bss {_sectionsByName[".bss"].LoadAddress}";
+		}
+
+		private void PrintSections()
+		{
+			Console.WriteLine($"Mounted `{ModuleName}` @{Memory.Start:x16}");
+			foreach (var s in _elf.Sections.OrderBy(s => s.LoadAddress))
+			{
+				Console.WriteLine("  @{0:x16} {1}{2}{3} `{4}` {5} bytes",
+					s.LoadAddress,
+					(s.Flags & SectionFlags.Allocatable) != 0 ? "R" : " ",
+					(s.Flags & SectionFlags.Writable) != 0 ? "W" : " ",
+					(s.Flags & SectionFlags.Executable) != 0 ? "X" : " ",
+					s.Name,
+					s.Size);
+			}
+		}
+
+		private void PrintTopSavableSymbols()
+		{
+			Console.WriteLine("Top savestate symbols:");
+			var tops = _allSymbols
+				.Where(s => s.PointedSection != null && (s.PointedSection.Flags & SectionFlags.Writable) != 0)
+				.OrderByDescending(s => s.Size)
+				.Take(30)
+				.Select(s => $"  {s.Name} size {s.Size}");
+
+			foreach (var text in tops)
+			{
+				Console.WriteLine(text);
+			}
+		}
+
+		/// <summary>
+		/// Returns true if section is readonly after init
+		/// </summary>
+		private bool IsSpecialReadonlySection(Section<ulong> sec)
+		{
+			return sec.Name.Contains(".rel.ro")
+				|| sec.Name.StartsWith(".got")
+				|| sec == _imports
+				|| sec == _sealed;
+		}
+
+		/// <summary>
+		/// Set normal (post-seal) memory protections
+		/// </summary>
+		private void Protect()
+		{
+			Memory.Protect(Memory.Start, Memory.Size, MemoryBlockBase.Protection.R);
+			foreach (var sec in _elf.Sections.Where(s => (s.Flags & SectionFlags.Allocatable) != 0 && !IsSpecialReadonlySection(s)))
+			{
+				if ((sec.Flags & SectionFlags.Executable) != 0)
+					Memory.Protect(sec.LoadAddress, sec.Size, MemoryBlockBase.Protection.RX);
+				else if ((sec.Flags & SectionFlags.Writable) != 0)
+					Memory.Protect(sec.LoadAddress, sec.Size, MemoryBlockBase.Protection.RW);
+			}
+		}
+
+		// connect all of the .wbxsyscall stuff
+		public void ConnectSyscalls(IImportResolver syscalls)
+		{
+			if (_everythingSealed)
+				Memory.Protect(Memory.Start, Memory.Size, MemoryBlockBase.Protection.R);
+
+			var tmp = new IntPtr[1];
+
+			foreach (var s in _importSymbols)
+			{
+				if (s.Size == 8)
+				{
+					var p = syscalls.GetProcAddrOrThrow(s.Name);
+					tmp[0] = p;
+					Marshal.Copy(tmp, 0, Z.US(s.Value), 1);
+				}
+				else
+				{
+					if ((s.Size & 7) != 0)
+						throw new InvalidOperationException($"Symbol {s.Name} has unexpected size");
+					var count = (int)(s.Size >> 3);
+					for (var i = 0; i < count; i++)
+					{
+						var p = syscalls.GetProcAddrOrThrow($"{s.Name}[{i}]");
+						tmp[0] = p;
+						Marshal.Copy(tmp, 0, Z.US(s.Value + (ulong)(i * 8)), 1);
+					}
+				}
+			}
+
+			if (_everythingSealed)
+				Protect();
+		}
+
+		public void SealImportsAndTakeXorSnapshot()
+		{
+			if (_everythingSealed)
+				throw new InvalidOperationException($"{nameof(ElfLoader)} already sealed!");
+
+			// save import values, then zero them all (for hash purposes), then take our snapshot, then load them again,
+			// then set the .idata area to read only
+			byte[] impData = null;
+			if (_imports != null)
+			{
+				impData = new byte[_imports.Size];
+				Marshal.Copy(Z.US(_imports.LoadAddress), impData, 0, (int)_imports.Size);
+				WaterboxUtils.ZeroMemory(Z.US(_imports.LoadAddress), (long)_imports.Size);
+			}
+			else
+			{
+				throw new Exception("Call natt (_imports??)");
+			}
+			byte[] invData = null;
+			if (_invisible != null)
+			{
+				invData = new byte[_invisible.Size];
+				Marshal.Copy(Z.US(_invisible.LoadAddress), invData, 0, (int)_invisible.Size);
+				WaterboxUtils.ZeroMemory(Z.US(_invisible.LoadAddress), (long)_invisible.Size);
+			}
+			Memory.SaveXorSnapshot();
+			if (_imports != null)
+			{
+				Marshal.Copy(impData, 0, Z.US(_imports.LoadAddress), (int)_imports.Size);
+			}
+			if (_invisible != null)
+			{
+				Marshal.Copy(invData, 0, Z.US(_invisible.LoadAddress), (int)_invisible.Size);
+			}
+
+			Protect();
+			_everythingSealed = true;
+		}
+
+		private bool _disposed = false;
+
+		public void Dispose()
+		{
+			if (!_disposed)
+			{
+				Memory.Dispose();
+				Memory = null;
+				_disposed = true;
+			}
+		}
+
+		public IntPtr GetProcAddrOrZero(string entryPoint)
+		{
+			if (_visibleSymbols.TryGetValue(entryPoint, out var sym))
+			{
+				return Z.US(sym.Value);
+			}
+			else
+			{
+				return IntPtr.Zero;
+			}
+		}
+
+		public IntPtr GetProcAddrOrThrow(string entryPoint)
+		{
+			if (_visibleSymbols.TryGetValue(entryPoint, out var sym))
+			{
+				return Z.US(sym.Value);
+			}
+			else
+			{
+				throw new InvalidOperationException($"Couldn't find {nameof(entryPoint)} {entryPoint} in {ModuleName}");
+			}
+		}
+
+		const ulong MAGIC = 0x6018ab7df99310ca;
+
+		public void SaveStateBinary(BinaryWriter bw)
+		{
+			if (!_everythingSealed)
+				throw new InvalidOperationException(".idata sections must be closed before saving state");
+
+			bw.Write(MAGIC);
+			bw.Write(_elfHash);
+			bw.Write(Memory.XorHash);
+
+			foreach (var s in _savedSections)
+			{
+				var ms = Memory.GetXorStream(s.LoadAddress, s.Size, false);
+				bw.Write(s.Size);
+				ms.CopyTo(bw.BaseStream);
+			}
+		}
+
+		public void LoadStateBinary(BinaryReader br)
+		{
+			if (!_everythingSealed)
+				// operations happening in the wrong order.  probable cause: internal logic error.  make sure frontend calls Seal
+				throw new InvalidOperationException(".idata sections must be closed before loading state");
+
+			if (br.ReadUInt64() != MAGIC)
+				// file id is missing.  probable cause:  garbage savestate
+				throw new InvalidOperationException("Savestate corrupted!");
+
+			var elfHash = br.ReadBytes(_elfHash.Length);
+			if (_skipCoreConsistencyCheck)
+			{
+				throw new InvalidOperationException("We decided that the core consistency check should always run");
+			}
+			else
+			{
+				if (!elfHash.SequenceEqual(_elfHash))
+					// the .dll file that is loaded now has a different hash than the .dll that created the savestate
+					throw new InvalidOperationException("Core consistency check failed.  Is this a savestate from a different version?");
+			}
+
+			var xorHash = br.ReadBytes(Memory.XorHash.Length);
+			if (!_skipMemoryConsistencyCheck)
+			{
+				if (!xorHash.SequenceEqual(Memory.XorHash))
+					// the post-Seal memory state is different. probable cause:  different rom or different version of rom,
+					// different syncsettings
+					throw new InvalidOperationException("Memory consistency check failed.  Is this savestate from different SyncSettings?");
+			}
+
+			Memory.Protect(Memory.Start, Memory.Size, MemoryBlockBase.Protection.RW);
+
+			foreach (var s in _savedSections)
+			{
+				if (br.ReadUInt64() != s.Size)
+					throw new InvalidOperationException("Unexpected section size for " + s.Name);
+
+				var ms = Memory.GetXorStream(s.LoadAddress, s.Size, true);
+				WaterboxUtils.CopySome(br.BaseStream, ms, (long)s.Size);
+			}
+
+			Protect();
+		}
+	}
+}
---
+++ src/BizHawk.Emulation.Cores/Waterbox/Syscalls.cs
@@ -0,0 +1,526 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using BizHawk.BizInvoke;
+using BizHawk.Emulation.Common;
+
+namespace BizHawk.Emulation.Cores.Waterbox
+{
+	/// <summary>
+	/// syscall emulation layer
+	/// </summary>
+	internal class Syscalls : IBinaryStateable
+	{
+		public interface IFileObject : IBinaryStateable
+		{
+			bool Open(FileAccess access);
+			bool Close();
+			Stream Stream { get; }
+			string Name { get; }
+		}
+
+		private class SpecialFile : IFileObject
+		{
+			// stdin, stdout, stderr
+			public string Name { get; }
+			public Stream Stream { get; }
+			public bool Close() => false;
+			public bool Open(FileAccess access) => false;
+
+			public void SaveStateBinary(BinaryWriter writer) { }
+			public void LoadStateBinary(BinaryReader reader) { }
+
+			public SpecialFile(Stream stream, string name)
+			{
+				Stream = stream;
+				Name = name;
+			}
+		}
+
+		private class ReadonlyFirmware : IFileObject
+		{
+			private readonly byte[] _data;
+			private readonly byte[] _hash;
+
+			public string Name { get; }
+			public Stream Stream { get; private set; }
+			public bool Close()
+			{
+				if (Stream == null)
+					return false;
+				Stream = null;
+				return true;
+			}
+
+			public bool Open(FileAccess access)
+			{
+				if (Stream != null || access != FileAccess.Read)
+					return false;
+				Stream = new MemoryStream(_data, false);
+				return true;
+			}
+
+			public void LoadStateBinary(BinaryReader br)
+			{
+				if (!br.ReadBytes(_hash.Length).SequenceEqual(_hash))
+					throw new InvalidOperationException("Savestate internal firmware mismatch");
+				var pos = br.ReadInt64();
+				if (pos == -1)
+				{
+					Stream = null;
+				}
+				else
+				{
+					if (Stream == null)
+						Open(FileAccess.Read);
+					Stream.Position = pos;
+				}
+			}
+
+			public void SaveStateBinary(BinaryWriter bw)
+			{
+				bw.Write(_hash);
+				bw.Write(Stream != null ? Stream.Position : -1);
+			}
+
+			public ReadonlyFirmware(byte[] data, string name)
+			{
+				_data = data;
+				_hash = WaterboxUtils.Hash(data);
+				Name = name;
+			}
+		}
+
+		private class TransientFile : IFileObject
+		{
+			private bool _inUse = false;
+			public string Name { get; }
+			public Stream Stream { get; }
+			public bool Close()
+			{
+				if (_inUse)
+				{
+					_inUse = false;
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+
+			public bool Open(FileAccess access)
+			{
+				if (_inUse)
+				{
+					return false;
+				}
+				else
+				{
+					// TODO: if access != RW, the resultant handle lets you do those all anyway
+					_inUse = true;
+					Stream.Position = 0;
+					return true;
+				}
+			}
+
+			public void LoadStateBinary(BinaryReader br)
+			{
+				throw new InvalidOperationException("Internal savestate error!");
+			}
+
+			public void SaveStateBinary(BinaryWriter bw)
+			{
+				throw new InvalidOperationException("Transient files cannot be savestated!");
+			}
+
+			public TransientFile(byte[] data, string name)
+			{
+				Stream = new MemoryStream();
+				Name = name;
+				if (data != null)
+				{
+					Stream.Write(data, 0, data.Length);
+					Stream.Position = 0;
+				}
+			}
+
+			public byte[] GetContents()
+			{
+				if (_inUse)
+					throw new InvalidOperationException();
+				return ((MemoryStream)Stream).ToArray();
+			}
+		}
+
+		private readonly List<IFileObject> _openFiles = new List<IFileObject>();
+		private readonly Dictionary<string, IFileObject> _availableFiles = new Dictionary<string, IFileObject>();
+
+		private readonly PeRunner _parent;
+		public Syscalls(PeRunner parent)
+		{
+			_parent = parent;
+			var stdin = new SpecialFile(Stream.Null, "___stdin");
+			var stdout = new SpecialFile(Console.OpenStandardOutput(), "___stdout");
+			var stderr = new SpecialFile(Console.OpenStandardError(), "___stderr");
+
+			_openFiles = new List<IFileObject>
+			{
+				stdin,
+				stdout,
+				stderr
+			};
+			_availableFiles = new Dictionary<string, IFileObject>
+			{
+				[stdin.Name] = stdin,
+				[stdout.Name] = stdout,
+				[stderr.Name] = stderr
+			};
+		}
+
+		private Stream StreamForFd(int fd)
+		{
+			if (fd >= 0 && fd < _openFiles.Count)
+				return _openFiles[fd].Stream;
+			else
+				return null;
+		}
+
+		[BizExport(CallingConvention.Cdecl, EntryPoint = "__wsyscalltab[12]")]
+		public UIntPtr Brk(UIntPtr _p)
+		{
+			var heap = _parent._heap;
+
+			var start = heap.Memory.Start;
+			var end = start + heap.Used;
+			var max = heap.Memory.End;
+
+			var p = (ulong)_p;
+
+			if (p < start || p > max)
+			{
+				// failure: return current break
+				return Z.UU(end);
+			}
+			else if (p > end)
+			{
+				// increase size of heap
+				heap.Allocate(p - end, 1);
+				return Z.UU(p);
+			}
+			else if (p < end)
+			{
+				throw new InvalidOperationException("We don't support shrinking heaps");
+			}
+			else
+			{
+				// no change
+				return Z.UU(end);
+			}
+		}
+
+		[BizExport(CallingConvention.Cdecl, EntryPoint = "__wsyscalltab[16]")]
+		public int IoCtl(int fd, ulong req)
+		{
+			return 0; // sure it worked, honest
+		}
+
+		public struct Iovec
+		{
+			public IntPtr Base;
+			public ulong Length;
+		}
+
+		[BizExport(CallingConvention.Cdecl, EntryPoint = "__wsyscalltab[0]")]
+		public long Read(int fd, IntPtr buff, ulong count)
+		{
+			var s = StreamForFd(fd);
+			if (s == null || !s.CanRead)
+				return -1;
+			var tmp = new byte[count];
+			var ret = s.Read(tmp, 0, (int)count);
+			Marshal.Copy(tmp, 0, buff, ret);
+			return ret;
+		}
+
+		[BizExport(CallingConvention.Cdecl, EntryPoint = "__wsyscalltab[1]")]
+		public long Write(int fd, IntPtr buff, ulong count)
+		{
+			var s = StreamForFd(fd);
+			if (s == null || !s.CanWrite)
+				return -1;
+			var tmp = new byte[count];
+			Marshal.Copy(buff, tmp, 0, (int)count);
+			s.Write(tmp, 0, tmp.Length);
+			return (long)count;
+		}
+
+		[BizExport(CallingConvention.Cdecl, EntryPoint = "__wsyscalltab[19]")]
+		public unsafe long Readv(int fd, Iovec* iov, int iovcnt)
+		{
+			long ret = 0;
+			for (int i = 0; i < iovcnt; i++)
+			{
+				var len = Read(fd, iov[i].Base, iov[i].Length);
+				if (len < 0)
+					return len;
+				ret += len;
+				if (len != (long)iov[i].Length)
+					break;
+			}
+			return ret;
+		}
+		[BizExport(CallingConvention.Cdecl, EntryPoint = "__wsyscalltab[20]")]
+		public unsafe long Writev(int fd, Iovec* iov, int iovcnt)
+		{
+			long ret = 0;
+			for (int i = 0; i < iovcnt; i++)
+			{
+				if (iov[i].Base != IntPtr.Zero)
+					ret += Write(fd, iov[i].Base, iov[i].Length);
+			}
+			return ret;
+		}
+
+		[BizExport(CallingConvention.Cdecl, EntryPoint = "__wsyscalltab[2]")]
+		public int Open(string path, int flags, int mode)
+		{
+			if (!_availableFiles.TryGetValue(path, out var o))
+				return -1;
+			if (_openFiles.Contains(o))
+				return -1;
+			FileAccess access;
+			switch (flags & 3)
+			{
+				case 0:
+					access = FileAccess.Read;
+					break;
+				case 1:
+					access = FileAccess.Write;
+					break;
+				case 2:
+					access = FileAccess.ReadWrite;
+					break;
+				default:
+					return -1;
+			}
+			if (!o.Open(access))
+				return -1;
+			int fd;
+			for (fd = 0; fd < _openFiles.Count; fd++)
+				if (_openFiles[fd] == null)
+					break;
+			if (fd == _openFiles.Count)
+				_openFiles.Add(o);
+			else
+				_openFiles[fd] = o;
+			return fd;
+		}
+		[BizExport(CallingConvention.Cdecl, EntryPoint = "__wsyscalltab[3]")]
+		public int Close(int fd)
+		{
+			if (fd < 0 || fd >= _openFiles.Count)
+				return -1;
+			var o = _openFiles[fd];
+			if (o == null || !o.Close())
+				return -1;
+			_openFiles[fd] = null;
+			return 0;
+		}
+		[BizExport(CallingConvention.Cdecl, EntryPoint = "__wsyscalltab[8]")]
+		public long Seek(int fd, long offset, int type)
+		{
+			var s = StreamForFd(fd);
+			if (s == null || !s.CanSeek)
+				return -1;
+			SeekOrigin o;
+			switch (type)
+			{
+				case 0:
+					o = SeekOrigin.Begin;
+					break;
+				case 1:
+					o = SeekOrigin.Current;
+					break;
+				case 2:
+					o = SeekOrigin.End;
+					break;
+				default:
+					return -1;
+			}
+			return s.Seek(offset, o);
+		}
+
+		[BizExport(CallingConvention.Cdecl, EntryPoint = "__wsyscalltab[4]")]
+		public int Stat(string path, IntPtr statbuf)
+		{
+			return -1;
+		}
+
+		[BizExport(CallingConvention.Cdecl, EntryPoint = "__wsyscalltab[5]")]
+		public int Fstat(int fd, IntPtr statbuf)
+		{
+			return -1;
+		}
+
+		[BizExport(CallingConvention.Cdecl, EntryPoint = "__wsyscalltab[218]")]
+		public long SetTidAddress(IntPtr address)
+		{
+			return 8675309;
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		public class TimeSpec
+		{
+			public long Seconds;
+			public long NanoSeconds;
+		}
+
+		[BizExport(CallingConvention.Cdecl, EntryPoint = "__wsyscalltab[228]")]
+		public int SysClockGetTime(int which, [In, Out] TimeSpec time)
+		{
+			time.Seconds = 1495889068;
+			time.NanoSeconds = 0;
+			return 0;
+		}
+
+		[BizExport(CallingConvention.Cdecl, EntryPoint = "__wsyscalltab[9]")]
+		public IntPtr MMap(IntPtr address, UIntPtr size, int prot, int flags, int fd, IntPtr offs)
+		{
+			if (address != IntPtr.Zero)
+				return Z.SS(-1);
+			MemoryBlockBase.Protection mprot;
+			switch (prot)
+			{
+				case 0: mprot = MemoryBlockBase.Protection.None; break;
+				default:
+				case 6: // W^X
+				case 7: // W^X
+				case 4: // exec only????
+				case 2: return Z.SS(-1); // write only????
+				case 3: mprot = MemoryBlockBase.Protection.RW; break;
+				case 1: mprot = MemoryBlockBase.Protection.R; break;
+				case 5: mprot = MemoryBlockBase.Protection.RX; break;
+			}
+			if ((flags & 0x20) == 0)
+			{
+				// MAP_ANONYMOUS is required
+				return Z.SS(-1);
+			}
+			if ((flags & 0xf00) != 0)
+			{
+				// various unsupported flags
+				return Z.SS(-1);
+			}
+
+			var ret = _parent._mmapheap.Map((ulong)size, mprot);
+			return ret == 0 ? Z.SS(-1) : Z.US(ret);
+		}
+		[BizExport(CallingConvention.Cdecl, EntryPoint = "__wsyscalltab[25]")]
+		public IntPtr MRemap(UIntPtr oldAddress, UIntPtr oldSize,
+			UIntPtr newSize, int flags)
+		{
+			if ((flags & 2) != 0)
+			{
+				// don't support MREMAP_FIXED
+				return Z.SS(-1);
+			}
+			var ret = _parent._mmapheap.Remap((ulong)oldAddress, (ulong)oldSize, (ulong)newSize,
+				(flags & 1) != 0);
+			return ret == 0 ? Z.SS(-1) : Z.US(ret);
+		}
+		[BizExport(CallingConvention.Cdecl, EntryPoint = "__wsyscalltab[11]")]
+		public int MUnmap(UIntPtr address, UIntPtr size)
+		{
+			return _parent._mmapheap.Unmap((ulong)address, (ulong)size) ? 0 : -1;
+		}
+
+		[BizExport(CallingConvention.Cdecl, EntryPoint = "__wsyscalltab[10]")]
+		public int MProtect(UIntPtr address, UIntPtr size, int prot)
+		{
+			MemoryBlockBase.Protection mprot;
+			switch (prot)
+			{
+				case 0: mprot = MemoryBlockBase.Protection.None; break;
+				default:
+				case 6: // W^X
+				case 7: // W^X
+				case 4: // exec only????
+				case 2: return -1; // write only????
+				case 3: mprot = MemoryBlockBase.Protection.RW; break;
+				case 1: mprot = MemoryBlockBase.Protection.R; break;
+				case 5: mprot = MemoryBlockBase.Protection.RX; break;
+			}
+			return _parent._mmapheap.Protect((ulong)address, (ulong)size, mprot) ? 0 : -1;
+		}
+
+		public void SaveStateBinary(BinaryWriter bw)
+		{
+			bw.Write(_availableFiles.Count);
+			foreach (var f in _availableFiles.Values.OrderBy(f => f.Name))
+			{
+				bw.Write(f.Name);
+				f.SaveStateBinary(bw);
+			}
+			bw.Write(_openFiles.Count);
+			foreach (var f in _openFiles)
+			{
+				bw.Write(f != null);
+				if (f != null)
+					bw.Write(f.Name);
+			}
+		}
+
+		public void LoadStateBinary(BinaryReader br)
+		{
+			if (_availableFiles.Count != br.ReadInt32())
+				throw new InvalidOperationException("Internal savestate error:  Filelist change");
+			foreach (var f in _availableFiles.Values.OrderBy(f => f.Name))
+			{
+				if (br.ReadString() != f.Name)
+					throw new InvalidOperationException("Internal savestate error:  Filelist change");
+				f.LoadStateBinary(br);
+			}
+			var c = br.ReadInt32();
+			_openFiles.Clear();
+			for (int i = 0; i < c; i++)
+			{
+				_openFiles.Add(br.ReadBoolean() ? _availableFiles[br.ReadString()] : null);
+			}
+		}
+
+		private T RemoveFileInternal<T>(string name)
+			where T : IFileObject
+		{
+			if (!_availableFiles.TryGetValue(name, out var o))
+				throw new InvalidOperationException("File was never registered!");
+			if (o.GetType() != typeof(T))
+				throw new InvalidOperationException("Object was not a the right kind of file");
+			if (_openFiles.Contains(o))
+				throw new InvalidOperationException("Core never closed the file!");
+			_availableFiles.Remove(name);
+			return (T)o;
+		}
+
+		public void AddReadonlyFile(byte[] data, string name)
+		{
+			_availableFiles.Add(name, new ReadonlyFirmware(data, name));
+		}
+
+		public void RemoveReadonlyFile(string name)
+		{
+			RemoveFileInternal<ReadonlyFirmware>(name);
+		}
+
+		public void AddTransientFile(byte[] data, string name)
+		{
+			_availableFiles.Add(name, new TransientFile(data, name));
+		}
+		public byte[] RemoveTransientFile(string name)
+		{
+			return RemoveFileInternal<TransientFile>(name).GetContents();
+		}
+	}
+}
