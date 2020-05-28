d13a1b54a2b04e69e090377a68bf3ca640ab1fc6
nattthebear <goyuken@gmail.com>
2020-05-28 07:40:20 -0400

    code cleanup
    Remove three Stream.CopyTo(Stream) variants that all did the exact same thing as the framework method Stream.CopyTo(Stream).  None of them were used anywhere.

--- src/BizHawk.Common/Extensions/IOExtensions.cs
+++ src/BizHawk.Common/Extensions/IOExtensions.cs
@@ -58,43 +58,6 @@ namespace BizHawk.Common.IOExtensions
 			}
 		}
 
-		public static void CopyTo(this Stream src, Stream dest)
-		{
-			int size = src.CanSeek ? Math.Min((int)(src.Length - src.Position), 0x2000) : 0x2000;
-			byte[] buffer = new byte[size];
-			int n;
-			do
-			{
-				n = src.Read(buffer, 0, buffer.Length);
-				dest.Write(buffer, 0, n);
-			}
-			while (n != 0);
-		}
-
-		public static void CopyTo(this MemoryStream src, Stream dest)
-		{
-			dest.Write(src.GetBuffer(), (int)src.Position, (int)(src.Length - src.Position));
-		}
-
-		public static void CopyTo(this Stream src, MemoryStream dest)
-		{
-			if (src.CanSeek)
-			{
-				int pos = (int)dest.Position;
-				int length = (int)(src.Length - src.Position) + pos;
-				dest.SetLength(length);
-
-				while (pos < length)
-				{
-					pos += src.Read(dest.GetBuffer(), pos, length - pos);
-				}
-			}
-			else
-			{
-				src.CopyTo(dest);
-			}
-		}
-
 		public static void Write(this BinaryWriter bw, int[] buffer)
 		{
 			foreach (int b in buffer)
