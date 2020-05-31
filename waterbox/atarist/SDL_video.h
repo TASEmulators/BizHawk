ceeb70d6c96cff48fe0c0ee2c7762637a24464a5
nattthebear <goyuken@gmail.com>
2020-05-31 17:25:30 -0400

    For a rainy day: hatari
    Just barely started looking at this one.  There's SDL everywhere, but the core doesn't seem overly complex.

---
+++ waterbox/atarist/SDL_video.h
@@ -0,0 +1,8 @@
+#pragma once
+
+typedef struct {
+
+} SDL_Surface;
+typedef struct {
+
+} SDL_Rect;
---
+++ waterbox/atarist/config.h
@@ -0,0 +1,116 @@
+/* CMake config.h for Hatari */
+
+/* Define if you have a PNG compatible library */
+// #define HAVE_LIBPNG 1
+
+/* Define if you have a readline compatible library */
+// #define HAVE_LIBREADLINE 1
+
+/* Define if you have the PortAudio library */
+// #define HAVE_PORTAUDIO 1
+
+/* Define if you have the PortMidi library */
+// #define HAVE_PORTMIDI 1
+
+/* Define if you have the capsimage library */
+// #define HAVE_CAPSIMAGE 1
+// #define CAPSIMAGE_VERSION @CAPSIMAGE_VERSION@
+
+/* Define if you have a X11 environment */
+// #define HAVE_X11 1
+
+/* Define to 1 if you have the `z' library (-lz). */
+#define HAVE_LIBZ 1
+
+/* Define to 1 if you have the <zlib.h> header file. */
+#define HAVE_ZLIB_H 1
+
+/* Define to 1 if you have the <termios.h> header file. */
+#define HAVE_TERMIOS_H 1
+
+/* Define to 1 if you have the <sys/ioctl.h> header file. */
+#define HAVE_SYS_IOCTL_H 1
+
+/* Define to 1 if you have the <glob.h> header file. */
+#define HAVE_GLOB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <SDL_config.h> header file. */
+#define HAVE_SDL_CONFIG_H 1
+
+/* Define to 1 if you have the <sys/time.h> header file. */
+#define HAVE_SYS_TIME_H 1
+
+/* Define to 1 if you have the <sys/times.h> header file. */
+#define HAVE_SYS_TIMES_H 1
+
+/* Define to 1 if you have the <utime.h> header file. */
+// #define HAVE_UTIME_H 1
+
+/* Define to 1 if you have the <sys/utime.h> header file. */
+// #define HAVE_SYS_UTIME_H 1
+
+/* Define to 1 if you have the `cfmakeraw' function. */
+#define HAVE_CFMAKERAW 1
+
+/* Define to 1 if you have the `tcsetattr' function. */
+// #define HAVE_TCSETATTR 1
+
+/* Define to 1 if you have the 'setenv' function. */
+// #define HAVE_SETENV 1
+
+/* Define to 1 if you have the `select' function. */
+#define HAVE_SELECT 1
+
+/* Define to 1 if you have unix domain sockets */
+// #define HAVE_UNIX_DOMAIN_SOCKETS 1
+
+/* Define to 1 if you have the 'gettimeofday' function. */
+#define HAVE_GETTIMEOFDAY 1
+
+/* Define to 1 if you have the 'nanosleep' function. */
+#define HAVE_NANOSLEEP 1
+
+/* Define to 1 if you have the 'alphasort' function. */
+#define HAVE_ALPHASORT 1
+
+/* Define to 1 if you have the 'scandir' function. */
+#define HAVE_SCANDIR 1
+
+/* Define to 1 if you have the 'statvfs' function. */
+#define HAVE_STATVFS 1
+
+/* Define to 1 if you have the 'fseeko' function. */
+#define HAVE_FSEEKO 1
+
+/* Define to 1 if you have the 'ftello' function. */
+#define HAVE_FTELLO 1
+
+/* Define to 1 if you have the 'flock' function. */
+// #define HAVE_FLOCK 1
+
+/* Define to 1 if you have the 'strlcpy' function. */
+#define HAVE_LIBC_STRLCPY 1
+
+/* Define to 1 if you have the 'd_type' member in the 'dirent' struct */
+#define HAVE_DIRENT_D_TYPE 1
+
+/* Relative path from bindir to datadir */
+#define BIN2DATADIR "DATA:"
+
+/* Define to 1 to enable DSP 56k emulation for Falcon mode */
+#define ENABLE_DSP_EMU 1
+
+/* Define to 1 to enable WINUAE cpu  */
+#define ENABLE_WINUAE_CPU 1
+
+/* Define to 1 to use less memory - at the expense of emulation speed */
+// #define ENABLE_SMALL_MEM 1
+
+/* Define to 1 to enable trace logs - undefine to slightly increase speed */
+// #define ENABLE_TRACING 1
+
+/* Define to 1 if udev support is available */
+// #define HAVE_UDEV 1
