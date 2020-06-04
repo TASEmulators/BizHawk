178e7c407d6d9fcc8a080b43562864922c8e0cca
TiKevin83 <travismcgeehan@gmail.com>
2020-06-04 02:11:26 -0400

    Test class and basic unit test

---
+++ src/BizHawk.Tests/Client.Common/Movie/MovieConversionExtensionTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BizHawk.Client.Common.MovieConversionExtensions;
+
+namespace BizHawk.Common.Tests.Client.Common.Movie
+{
+	[TestClass]
+	public class MovieConversionExtensionTests
+	{
+		private string oldBk2FileName = "C:\\Temp\\TestMovie.bk2";
+
+		[TestMethod]
+		public void GetNewFileNameFromBk2()
+		{
+			var actual = MovieConversionExtensions.GetNewFileName(oldBk2FileName);
+			Assert.AreEqual("C:\\Temp\\TestMovie.tasproj", actual);
+		}
+	}
+}
