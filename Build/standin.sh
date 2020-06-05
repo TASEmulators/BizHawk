3e0d07f5237c29b110d2880e2fb40d8cf18029b4
YoshiRulz <OSSYoshiRulz@gmail.com>
2020-06-06 02:06:39 +1000

    Update Version project name in source control versioning script
    fixes b88f20fae

--- Build/standin.sh
+++ Build/standin.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cd "$(dirname "$0")/../src/Version" && printf "static class SubWCRev\n{\n\tpublic const string SVN_REV = \"%s\";\n\tpublic const string GIT_BRANCH = \"%s\";\n\tpublic const string GIT_SHORTHASH = \"%s\";\n}" "$(git rev-list HEAD --count)" "$(git rev-parse --abbrev-ref HEAD)" "$(git log -1 --format="%h")" >svnrev.cs
+cd "$(dirname "$0")/../src/BizHawk.Version" && printf "static class SubWCRev\n{\n\tpublic const string SVN_REV = \"%s\";\n\tpublic const string GIT_BRANCH = \"%s\";\n\tpublic const string GIT_SHORTHASH = \"%s\";\n}" "$(git rev-list HEAD --count)" "$(git rev-parse --abbrev-ref HEAD)" "$(git log -1 --format="%h")" >svnrev.cs
