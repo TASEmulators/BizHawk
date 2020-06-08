ddfd6fd49a1a0a04f73355f06e38e419a1a4f328
adelikat <adelikat@tasvideos.org>
2020-06-08 11:35:19 -0500

    put IRewinder in its own file

---
+++ src/BizHawk.Client.Common/rewind/IRewinder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BizHawk.Client.Common
+{
+	public interface IRewinder : IDisposable
+	{
+		int Count { get; }
+		float FullnessRatio { get; }
+		long Size { get; }
+		int RewindFrequency { get; }
+
+		bool Active { get; }
+
+		void Capture(int frame);
+		bool Rewind(int frames);
+
+		void Suspend();
+		void Resume();
+	}
+}
--- src/BizHawk.Client.Common/rewind/Rewinder.cs
+++ src/BizHawk.Client.Common/rewind/Rewinder.cs
@@ -4,22 +4,6 @@ using BizHawk.Emulation.Common;
 
 namespace BizHawk.Client.Common
 {
-	public interface IRewinder : IDisposable
-	{
-		int Count { get; }
-		float FullnessRatio { get; }
-		long Size { get; }
-		int RewindFrequency { get; }
-
-		bool Active { get; }
-
-		void Capture(int frame);
-		bool Rewind(int frames);
-
-		void Suspend();
-		void Resume();
-	}
-
 	public class Rewinder : IRewinder
 	{
 		private readonly IStatable _statableCore;
