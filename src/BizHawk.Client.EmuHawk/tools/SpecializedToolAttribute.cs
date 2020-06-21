3d45799fc592f44dca9aaf63e5ed8b05e845c5eb
YoshiRulz <OSSYoshiRulz@gmail.com>
2020-06-21 10:49:44 +1000

    Specify [SpecializedTool] belongs on classes

--- src/BizHawk.Client.EmuHawk/tools/SpecializedToolAttribute.cs
+++ src/BizHawk.Client.EmuHawk/tools/SpecializedToolAttribute.cs
@@ -6,6 +6,7 @@ namespace BizHawk.Client.EmuHawk
 	/// <summary>
 	/// Defines a <see cref="IToolForm"/> as a specialized tool that is for a specific system or core
 	/// </summary>
+	[AttributeUsage(AttributeTargets.Class)]
 	public class SpecializedToolAttribute : Attribute
 	{
 		public SpecializedToolAttribute(string displayName)
