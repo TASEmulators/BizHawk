f754735aaf4b8ecb8db9f248f868bf9e79bd1d94
adelikat <adelikat@tasvideos.org>
2020-06-29 14:26:54 -0500

    more setup

--- src/BizHawk.Client.EmuHawk/config/Messages/MessageEdit.Designer.cs
+++ src/BizHawk.Client.EmuHawk/config/Messages/MessageEdit.Designer.cs
@@ -28,18 +28,170 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+            this.PositionGroupBox = new System.Windows.Forms.GroupBox();
+            this.BR = new System.Windows.Forms.RadioButton();
+            this.BL = new System.Windows.Forms.RadioButton();
+            this.TR = new System.Windows.Forms.RadioButton();
+            this.TL = new System.Windows.Forms.RadioButton();
+            this.label2 = new BizHawk.WinForms.Controls.LocLabelEx();
+            this.label1 = new BizHawk.WinForms.Controls.LocLabelEx();
+            this.YNumeric = new System.Windows.Forms.NumericUpDown();
+            this.XNumeric = new System.Windows.Forms.NumericUpDown();
+            this.PositionPanel = new System.Windows.Forms.Panel();
+            this.PositionGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.YNumeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.XNumeric)).BeginInit();
             this.SuspendLayout();
+            // 
+            // PositionGroupBox
+            // 
+            this.PositionGroupBox.Controls.Add(this.BR);
+            this.PositionGroupBox.Controls.Add(this.BL);
+            this.PositionGroupBox.Controls.Add(this.TR);
+            this.PositionGroupBox.Controls.Add(this.TL);
+            this.PositionGroupBox.Controls.Add(this.label2);
+            this.PositionGroupBox.Controls.Add(this.label1);
+            this.PositionGroupBox.Controls.Add(this.YNumeric);
+            this.PositionGroupBox.Controls.Add(this.XNumeric);
+            this.PositionGroupBox.Controls.Add(this.PositionPanel);
+            this.PositionGroupBox.Location = new System.Drawing.Point(3, 3);
+            this.PositionGroupBox.Name = "PositionGroupBox";
+            this.PositionGroupBox.Size = new System.Drawing.Size(307, 299);
+            this.PositionGroupBox.TabIndex = 4;
+            this.PositionGroupBox.TabStop = false;
+            this.PositionGroupBox.Text = "Position";
+            // 
+            // BR
+            // 
+            this.BR.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.BR.AutoSize = true;
+            this.BR.Location = new System.Drawing.Point(289, 253);
+            this.BR.Name = "BR";
+            this.BR.Size = new System.Drawing.Size(14, 13);
+            this.BR.TabIndex = 8;
+            this.BR.TabStop = true;
+            this.BR.UseVisualStyleBackColor = true;
+            this.BR.CheckedChanged += new System.EventHandler(this.BR_CheckedChanged);
+            // 
+            // BL
+            // 
+            this.BL.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.BL.AutoSize = true;
+            this.BL.Location = new System.Drawing.Point(6, 253);
+            this.BL.Name = "BL";
+            this.BL.Size = new System.Drawing.Size(14, 13);
+            this.BL.TabIndex = 7;
+            this.BL.TabStop = true;
+            this.BL.UseVisualStyleBackColor = true;
+            this.BL.CheckedChanged += new System.EventHandler(this.BL_CheckedChanged);
+            // 
+            // TR
+            // 
+            this.TR.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.TR.AutoSize = true;
+            this.TR.Location = new System.Drawing.Point(288, 18);
+            this.TR.Name = "TR";
+            this.TR.Size = new System.Drawing.Size(14, 13);
+            this.TR.TabIndex = 6;
+            this.TR.TabStop = true;
+            this.TR.UseVisualStyleBackColor = true;
+            this.TR.CheckedChanged += new System.EventHandler(this.TR_CheckedChanged);
+            // 
+            // TL
+            // 
+            this.TL.AutoSize = true;
+            this.TL.Location = new System.Drawing.Point(6, 18);
+            this.TL.Name = "TL";
+            this.TL.Size = new System.Drawing.Size(14, 13);
+            this.TL.TabIndex = 5;
+            this.TL.TabStop = true;
+            this.TL.UseVisualStyleBackColor = true;
+            this.TL.CheckedChanged += new System.EventHandler(this.TL_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.Location = new System.Drawing.Point(92, 273);
+            this.label2.Name = "label2";
+            this.label2.Text = "y";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.Location = new System.Drawing.Point(27, 274);
+            this.label1.Name = "label1";
+            this.label1.Text = "x";
+            // 
+            // YNumeric
+            // 
+            this.YNumeric.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.YNumeric.Location = new System.Drawing.Point(106, 271);
+            this.YNumeric.Maximum = new decimal(new int[] {
+            256,
+            0,
+            0,
+            0});
+            this.YNumeric.Name = "YNumeric";
+            this.YNumeric.Size = new System.Drawing.Size(44, 20);
+            this.YNumeric.TabIndex = 2;
+            this.YNumeric.Value = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            // 
+            // XNumeric
+            // 
+            this.XNumeric.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.XNumeric.Location = new System.Drawing.Point(43, 271);
+            this.XNumeric.Maximum = new decimal(new int[] {
+            256,
+            0,
+            0,
+            0});
+            this.XNumeric.Name = "XNumeric";
+            this.XNumeric.Size = new System.Drawing.Size(44, 20);
+            this.XNumeric.TabIndex = 1;
+            this.XNumeric.Value = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            // 
+            // PositionPanel
+            // 
+            this.PositionPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.PositionPanel.Location = new System.Drawing.Point(22, 18);
+            this.PositionPanel.Name = "PositionPanel";
+            this.PositionPanel.Size = new System.Drawing.Size(264, 248);
+            this.PositionPanel.TabIndex = 0;
             // 
             // MessageEdit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.PositionGroupBox);
             this.Name = "MessageEdit";
-            this.Size = new System.Drawing.Size(203, 292);
+            this.Size = new System.Drawing.Size(318, 310);
+            this.PositionGroupBox.ResumeLayout(false);
+            this.PositionGroupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.YNumeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.XNumeric)).EndInit();
             this.ResumeLayout(false);
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.GroupBox PositionGroupBox;
+		private System.Windows.Forms.RadioButton BR;
+		private System.Windows.Forms.RadioButton BL;
+		private System.Windows.Forms.RadioButton TR;
+		private System.Windows.Forms.RadioButton TL;
+		private WinForms.Controls.LocLabelEx label2;
+		private WinForms.Controls.LocLabelEx label1;
+		private System.Windows.Forms.NumericUpDown YNumeric;
+		private System.Windows.Forms.NumericUpDown XNumeric;
+		private System.Windows.Forms.Panel PositionPanel;
 	}
 }
--- src/BizHawk.Client.EmuHawk/config/Messages/MessageEdit.cs
+++ src/BizHawk.Client.EmuHawk/config/Messages/MessageEdit.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BizHawk.Client.Common;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -9,148 +10,36 @@ namespace BizHawk.Client.EmuHawk
 	// this is a little messy right now because of remnants of the old config system
 	public partial class MessageEdit : UserControl
 	{
-		// the dictionary that results are saved to
-		private Dictionary<string, string> _realConfigObject;
-
-		// if nonnull, the list of keys to use.  used to have the config panel operate on a smaller list than the whole dictionary;
-		// for instance, to show only a single player
-		private List<string> _realConfigButtons;
-
-		private readonly List<string> _buttons = new List<string>();
-
-		private readonly int _inputMarginLeft = UIHelper.ScaleX(0);
-		private readonly int _labelPadding = UIHelper.ScaleX(5);
-		private readonly int _marginTop = UIHelper.ScaleY(0);
-		private readonly int _ySpacing = UIHelper.ScaleY(24);
-		private readonly int _inputSize = UIHelper.ScaleX(170);
-		private readonly int _charWidth = UIHelper.ScaleX(9);
-
-		public ToolTip Tooltip { get; set; }
-
-		private readonly List<InputCompositeWidget> _inputs = new List<InputCompositeWidget>();
-
-		private Size _panelSize = new Size(0, 0);
-
-		private bool _autoTab;
+		private MessagePosition _messagePosition;
 
 		public MessageEdit()
 		{
 			InitializeComponent();
 		}
 
-		private void ClearAll()
+		public void Bind(MessagePosition messagePosition)
 		{
-			_inputs.ForEach(x => x.Clear());
+			_messagePosition = messagePosition;
 		}
 
-		/// <summary>
-		/// save to config
-		/// </summary>
-		/// <param name="saveConfigObject">if non-null, save to possibly different config object than originally initialized from</param>
-		public void Save(Dictionary<string, string> saveConfigObject = null)
+		private void TL_CheckedChanged(object sender, EventArgs e)
 		{
-			var saveTo = saveConfigObject ?? _realConfigObject;
-			for (int button = 0; button < _buttons.Count; button++)
-			{
-				var bindings = _inputs[button].Bindings;
-				if (!string.IsNullOrWhiteSpace(bindings))
-				{
-					saveTo[_buttons[button]] = bindings;
-				}
-			}
+
 		}
 
-		public void LoadSettings(Dictionary<string, string> config, bool autoTab, List<string> buttons = null, int? width = null, int? height = null)
+		private void BL_CheckedChanged(object sender, EventArgs e)
 		{
-			_autoTab = autoTab;
-			if (width.HasValue && height.HasValue)
-			{
-				_panelSize = new Size(width.Value, height.Value);
-			}
-			else
-			{
-				_panelSize = Size;
-			}
-			
-			_realConfigObject = config;
-			_realConfigButtons = buttons;
-			SetButtonList();
-			Startup();
-			SetWidgetStrings();
+
 		}
 
-		private void SetButtonList()
+		private void TR_CheckedChanged(object sender, EventArgs e)
 		{
-			_buttons.Clear();
-			IEnumerable<string> bl = _realConfigButtons ?? (IEnumerable<string>)_realConfigObject.Keys;
-			foreach (string s in bl)
-			{
-				_buttons.Add(s);
-			}
+
 		}
 
-		private void SetWidgetStrings()
+		private void BR_CheckedChanged(object sender, EventArgs e)
 		{
-			for (int button = 0; button < _buttons.Count; button++)
-			{
-				if (!_realConfigObject.TryGetValue(_buttons[button], out var s))
-				{
-					s = "";
-				}
 
-				_inputs[button].Bindings = s;
-			}
-		}
-
-		private void Startup()
-		{
-			var labelWidth = _buttons.Max(b => b.Length) * _charWidth;
-			var columnWidth = _inputSize + (_labelPadding * 2) + labelWidth;
-			int x = _inputMarginLeft;
-			int y = _marginTop - _ySpacing;
-			for (int i = 0; i < _buttons.Count; i++)
-			{
-				y += _ySpacing;
-				if (y > (_panelSize.Height - UIHelper.ScaleY(62)))
-				{
-					y = _marginTop;
-					x += columnWidth;
-				}
-
-				var iw = new InputCompositeWidget
-				{
-					Location = new Point(x, y),
-					Size = new Size(_inputSize, UIHelper.ScaleY(23)),
-					TabIndex = i,
-					AutoTab = _autoTab
-				};
-
-				iw.SetupTooltip(Tooltip, null);
-
-				iw.BringToFront();
-				Controls.Add(iw);
-				_inputs.Add(iw);
-				var label = new Label
-				{
-					Location = new Point(x + _inputSize + _labelPadding, y + UIHelper.ScaleY(3)),
-					Size = new Size(UIHelper.ScaleX(labelWidth), UIHelper.ScaleY(15)),
-					Text = _buttons[i].Replace('_', ' ').Trim()
-				};
-
-				Tooltip.SetToolTip(label, label.Text);
-
-				Controls.Add(label);
-			}
-		}
-
-		public void SetAutoTab(bool value)
-		{
-			_inputs.ForEach(x => x.AutoTab = value);
-		}
-
-		private void ClearToolStripMenuItem_Click(object sender, EventArgs e)
-		{
-			ClearAll();
 		}
 	}
 }
