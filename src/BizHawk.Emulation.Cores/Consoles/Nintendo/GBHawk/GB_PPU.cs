7aa1a497f7ca5282da71976f6245f8fd915b059f
alyosha-tas <alexei.f.k@gmail.com>
2020-06-30 17:15:00 -0400

    GBHawk: only latch new window y value if greater then current line (fixes pokemon crystal artifact)

--- src/BizHawk.Emulation.Cores/Consoles/Nintendo/GBHawk/GB_PPU.cs
+++ src/BizHawk.Emulation.Cores/Consoles/Nintendo/GBHawk/GB_PPU.cs
@@ -120,8 +120,8 @@ namespace BizHawk.Emulation.Cores.Nintendo.GBHawk
 					break;
 				case 0xFF4A: // WY
 					window_y = value;
-					if (!window_started) 
-					{ 
+					if (!window_started && (!LCDC.Bit(7) || (value > LY))) 
+					{
 						window_y_latch = window_y;
 						window_y_tile = 0;
 						window_y_tile_inc = 0;
