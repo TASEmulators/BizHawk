948b42b84b5ef388f2ba7ecfd5f2f23b9b16bd87
adelikat <adelikat@tasvideos.org>
2020-06-30 10:30:58 -0500

    MessageRow - progress

--- src/BizHawk.Client.EmuHawk/config/Messages/MessageRow.Designer.cs
+++ src/BizHawk.Client.EmuHawk/config/Messages/MessageRow.Designer.cs
@@ -28,18 +28,45 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+            this.RowRadio = new System.Windows.Forms.RadioButton();
+            this.LocationLabel = new BizHawk.WinForms.Controls.LocLabelEx();
             this.SuspendLayout();
+            // 
+            // RowRadio
+            // 
+            this.RowRadio.AutoSize = true;
+            this.RowRadio.Location = new System.Drawing.Point(3, 3);
+            this.RowRadio.Name = "RowRadio";
+            this.RowRadio.Size = new System.Drawing.Size(94, 17);
+            this.RowRadio.TabIndex = 0;
+            this.RowRadio.TabStop = true;
+            this.RowRadio.Text = "Frame Counter";
+            this.RowRadio.UseVisualStyleBackColor = true;
+            // 
+            // LocationLabel
+            // 
+            this.LocationLabel.AllowDrop = true;
+            this.LocationLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.LocationLabel.Location = new System.Drawing.Point(156, 5);
+            this.LocationLabel.Name = "LocationLabel";
+            this.LocationLabel.Text = "255, 255";
             // 
             // MessageRow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.LocationLabel);
+            this.Controls.Add(this.RowRadio);
             this.Name = "MessageRow";
-            this.Size = new System.Drawing.Size(203, 292);
+            this.Size = new System.Drawing.Size(224, 31);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.RadioButton RowRadio;
+		private WinForms.Controls.LocLabelEx LocationLabel;
 	}
 }
--- src/BizHawk.Client.EmuHawk/config/Messages/MessageRow.cs
+++ src/BizHawk.Client.EmuHawk/config/Messages/MessageRow.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BizHawk.Client.Common;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -9,9 +10,18 @@ namespace BizHawk.Client.EmuHawk
 	// this is a little messy right now because of remnants of the old config system
 	public partial class MessageRow : UserControl
 	{
+		private MessagePosition _messagePosition = new MessagePosition();
+
 		public MessageRow()
 		{
 			InitializeComponent();
+		}
+
+		public void Bind(string displayName, MessagePosition position)
+		{
+			_messagePosition = position;
+			RowRadio.Text = displayName;
+			LocationLabel.Text = position.ToCoordinateStr();
 		}
 	}
 }
