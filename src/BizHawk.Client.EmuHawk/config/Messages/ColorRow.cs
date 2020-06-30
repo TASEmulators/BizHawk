8d49f0499a08e535ec0c06213c451614c80c1e65
adelikat <adelikat@tasvideos.org>
2020-06-30 13:14:42 -0500

    progress

--- src/BizHawk.Client.EmuHawk/config/Messages/ColorRow.cs
+++ src/BizHawk.Client.EmuHawk/config/Messages/ColorRow.cs
@@ -1,5 +1,6 @@
 ﻿using BizHawk.Client.Common;
 using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace BizHawk.Client.EmuHawk
@@ -7,14 +8,34 @@ namespace BizHawk.Client.EmuHawk
 	// this is a little messy right now because of remnants of the old config system
 	public partial class ColorRow : UserControl
 	{
+		private int _color;
+
 		public ColorRow()
 		{
 			InitializeComponent();
 		}
 
-		public void Bind(string displayName, int color)
+		public void Bind(string displayName, ref int color)
 		{
-			
+			DisplayNameLabel.Text = displayName;
+			_color = color;
+			SetColor();
+		}
+
+		private void SetColor()
+		{
+			ColorPanel.BackColor = Color.FromArgb(_color);
+			ColorText.Text = $"{_color:X8}";
+		}
+
+		private void ColorPanel_Click(object sender, EventArgs e)
+		{
+			using var colorPicker = new ColorDialog { FullOpen = true };
+			if (colorPicker.ShowDialog().IsOk())
+			{
+				_color = colorPicker.Color.ToArgb();
+				SetColor();
+			}
 		}
 	}
 }
