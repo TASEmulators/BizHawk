8e9b0c491a16004da750c71b8084441ef3cc5d0d
adelikat <adelikat@tasvideos.org>
2020-06-30 10:08:31 -0500

    start mesage row component

---
+++ src/BizHawk.Client.EmuHawk/config/Messages/MessageRow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace BizHawk.Client.EmuHawk
+{
+	// this is a little messy right now because of remnants of the old config system
+	public partial class MessageRow : UserControl
+	{
+		public MessageRow()
+		{
+			InitializeComponent();
+		}
+	}
+}
