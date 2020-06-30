f41e289d23e249eccc7a6333063b026b0802a54f
adelikat <adelikat@tasvideos.org>
2020-06-30 12:51:47 -0500

    start ColorRow user control

---
+++ src/BizHawk.Client.EmuHawk/config/Messages/ColorRow.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace BizHawk.Client.EmuHawk
+{
+	partial class ColorRow
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.SuspendLayout();
+            // 
+            // ColorRow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "ColorRow";
+            this.Size = new System.Drawing.Size(224, 83);
+            this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
---
+++ src/BizHawk.Client.EmuHawk/config/Messages/ColorRow.cs
@@ -0,0 +1,20 @@
+﻿using BizHawk.Client.Common;
+using System;
+using System.Windows.Forms;
+
+namespace BizHawk.Client.EmuHawk
+{
+	// this is a little messy right now because of remnants of the old config system
+	public partial class ColorRow : UserControl
+	{
+		public ColorRow()
+		{
+			InitializeComponent();
+		}
+
+		public void Bind(string displayName, int color)
+		{
+			
+		}
+	}
+}
