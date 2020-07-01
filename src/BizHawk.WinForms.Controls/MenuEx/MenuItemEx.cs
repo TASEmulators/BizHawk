9114d4b92f7a031fd8487f19c72f500c993b8985
adelikat <adelikat@tasvideos.org>
2020-07-01 14:49:05 -0500

    add MenuItemEx and StatusLabelEx and use in MainForm

---
+++ src/BizHawk.WinForms.Controls/MenuEx/MenuItemEx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BizHawk.WinForms.Controls
+{
+	public class ToolStripMenuItemEx : ToolStripMenuItem
+	{
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+		public new Size Size => base.Size;
+
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+		public new string Name => Guid.NewGuid().ToString();
+	}
+
+	public class ToolStripSeparatorEx : ToolStripSeparator
+	{
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+		public new Size Size => base.Size;
+
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+		public new string Name => Guid.NewGuid().ToString();
+	}
+}
---
+++ src/BizHawk.WinForms.Controls/MenuEx/StatusLabelEx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BizHawk.WinForms.Controls
+{
+	public class StatusLabelEx : ToolStripStatusLabel
+	{
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+		public new Size Size => base.Size;
+
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+		public new string Name => Guid.NewGuid().ToString();
+	}
+}
