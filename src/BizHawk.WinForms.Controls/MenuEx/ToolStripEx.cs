2be1d0418251ab67fec33c1db76928fdfc7fd1a7
adelikat <adelikat@tasvideos.org>
2020-07-03 14:29:48 -0500

    cleanup ToolStripEx

--- src/BizHawk.WinForms.Controls/MenuEx/ToolStripEx.cs
+++ src/BizHawk.WinForms.Controls/MenuEx/ToolStripEx.cs
@@ -3,7 +3,6 @@ using System.ComponentModel;
 using System.Drawing;
 using System.Windows.Forms;
 
-//credit: http://blogs.msdn.com/b/rickbrew/archive/2006/01/09/511003.aspx
 namespace BizHawk.WinForms.Controls
 {
 	/// <summary>
@@ -30,11 +29,8 @@ namespace BizHawk.WinForms.Controls
 
 	internal sealed class NativeConstants
 	{
-		private NativeConstants() { }
 		internal const uint WM_MOUSEACTIVATE = 0x21;
 		internal const uint MA_ACTIVATE = 1;
 		internal const uint MA_ACTIVATEANDEAT = 2;
-		internal const uint MA_NOACTIVATE = 3;
-		internal const uint MA_NOACTIVATEANDEAT = 4;
 	}
 }
