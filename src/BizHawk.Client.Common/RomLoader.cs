TASEmulators/BizHawk

commit 316a9d15685dc31b62ef437dc721f77e648db019

gamedb - replace pico with picodrive, remove core name look up in romloader in favor of this pico change combined with a case insensitive compare

src/BizHawk.Client.Common/RomLoader.cs
@@ -328,32 +328,24 @@ namespace BizHawk.Client.Common
 			throw new NotImplementedException("M3U not supported!");
 		}
 
-		private static string ForcedCoreToCoreName(string forcedCore)
-		{
-			// TODO: Is this yet another list of core names really needed?  Let's just make the gamedb less dumb
-			return forcedCore switch
-			{
-				"snes9x" => CoreNames.Snes9X,
-				"bsnes" => CoreNames.Bsnes,
-				"quicknes" => CoreNames.QuickNes,
-				"pico" => CoreNames.PicoDrive,
-				_ => null
-			};
-		}
-
 		private IEmulator MakeCoreFromCoreInventory(CoreInventoryParameters cip)
 		{
 			_config.PreferredCores.TryGetValue(cip.Game.System, out var preferredCore);
-			var forcedCore = ForcedCoreToCoreName(cip.Game.ForcedCore);
+			var forcedCore = cip.Game.ForcedCore;
 			var cores = CoreInventory.Instance.GetCores(cip.Game.System)
 				.OrderBy(c =>
 				{
 					if (c.Name == preferredCore)
+					{
 						return (int)CorePriority.UserPreference;
-					else if (c.Name == forcedCore)
+					}
+
+					if (string.Equals(c.Name, forcedCore, StringComparison.InvariantCulture))
+					{
 						return (int)CorePriority.GameDbPreference;
-					else
-						return (int)c.Priority;
+					}
+
+					return (int)c.Priority;
 				})
 				.ToList();
 			if (cores.Count == 0)
@@ -435,8 +427,6 @@ namespace BizHawk.Client.Common
 					);
 					rom.GameInfo.Name = gameName;
 					return;
-				default:
-					break;
 			}
 			var cip = new CoreInventoryParameters(this)
 			{

commit 59be09b5152ed53ec25dc3aa01871bdf1a6ebb40

Make picodrive work again
All picodrive games were broken by 316a9d15685dc31b62ef437dc7

src/BizHawk.Client.Common/RomLoader.cs
@@ -340,7 +340,7 @@ namespace BizHawk.Client.Common
 						return (int)CorePriority.UserPreference;
 					}
 
-					if (string.Equals(c.Name, forcedCore, StringComparison.InvariantCulture))
+					if (string.Equals(c.Name, forcedCore, StringComparison.InvariantCultureIgnoreCase))
 					{
 						return (int)CorePriority.GameDbPreference;
 					}