TASEmulators/BizHawk

commit 175556529ea40b78a246edfbe020e699f1711b03

waterboxhost refactor
Set up a second mirror of guest memory; easily accomplished because we were already using memfd_create / CreateFileMappingW.
This lets us simplify a lot of host code that has to access guest memory that may not be active right now, or might have been mprotect()ed to something weird.  Activate is only needed now to run guest code, or when the C# side wants to peer into guest memory for memory domains and such (waterboxhost does not share the mirror address with the C# side).

waterbox/waterboxhost/src/host.rs
@@ -1,5 +1,5 @@
 use crate::*;
-use crate::{memory_block::ActivatedMemoryBlock, syscall_defs::*};
+use crate::{syscall_defs::*};
 use memory_block::{MemoryBlock, Protection};
 use std::{os::raw::c_char, ffi::CStr};
 use fs::{FileDescriptor, FileSystem/*, MissingFileCallback*/};
@@ -27,10 +27,9 @@ impl WaterboxHost {
 		let elf_addr = ElfLoader::elf_addr(&wbx);
 		let layout = layout_template.make_layout(elf_addr)?;
 		let mut memory_block = MemoryBlock::new(layout.all());
-		let mut b = memory_block.enter();
-		let elf = ElfLoader::new(&wbx, &image_file[..], module_name, &layout, &mut b)?;
+		let elf = ElfLoader::new(&wbx, &image_file[..], module_name, &layout, &mut memory_block)?;
 		let fs = FileSystem::new();
-		drop(b);
+
 		unsafe { gdb::register(&image_file[..]) }
 		let mut res = Box::new(WaterboxHost {
 			fs,
@@ -44,11 +43,10 @@ impl WaterboxHost {
 			context: Context::new(layout.main_thread.end(), syscall),
 			thunks,
 		});
-
-		let mut active = res.activate();
+		res.activate();
 		println!("Calling _start()");
-		active.run_guest_simple(active.h.elf.entry_point());
-		drop(active);
+		res.run_guest_simple(res.elf.entry_point());
+		res.deactivate();
 
 		Ok(res)
 	}
@@ -57,74 +55,72 @@ impl WaterboxHost {
 		self.active
 	}
 
-	pub fn activate(&mut self) -> Box<ActivatedWaterboxHost> {
-		context::prepare_thread();
-		let h = unsafe { &mut *(self as *mut WaterboxHost) };
-		let b = self.memory_block.enter();
-		let mut res = Box::new(ActivatedWaterboxHost {
-			h,
-			b,
-		});
-		res.h.active = true;
-		res.h.context.host_ptr = res.as_mut() as *mut ActivatedWaterboxHost as usize;
-		res
+	pub fn activate(&mut self) {
+		if !self.active {
+			context::prepare_thread();
+			self.context.host_ptr = self as *mut WaterboxHost as usize;
+			self.memory_block.activate();
+			self.active = true;
+		}
+	}
+
+	pub fn deactivate(&mut self) {
+		if self.active {
+			self.context.host_ptr = 0;
+			self.memory_block.deactivate();
+			self.active = false;
+		}
 	}
 }
+
 impl Drop for WaterboxHost {
 	fn drop(&mut self) {
+		self.deactivate();
 		unsafe { gdb::deregister(&self.image_file[..]) }
 	}
 }
 
-pub struct ActivatedWaterboxHost<'a> {
-	h: &'a mut WaterboxHost,
-	b: ActivatedMemoryBlock<'a>,
-}
-impl<'a> Drop for ActivatedWaterboxHost<'a> {
-	fn drop(&mut self) {
-		self.h.active = false;
-		self.h.context.host_ptr = 0;
-	}
-}
-
-impl<'a> ActivatedWaterboxHost<'a> {
+impl WaterboxHost {
 	pub fn get_external_callback_ptr(&mut self, callback: ExternalCallback, slot: usize) -> anyhow::Result<usize> {
 		if slot >= CALLBACK_SLOTS {
 			Err(anyhow!("slot must be less than {}", CALLBACK_SLOTS))
 		} else {
-			self.h.context.extcall_slots[slot] = Some(callback);
+			self.context.extcall_slots[slot] = Some(callback);
 			Ok(context::get_callback_ptr(slot))
 		}
 	}
 	pub fn get_external_callin_ptr(&mut self, ptr: usize) -> anyhow::Result<usize> {
-		self.h.thunks.get_thunk_for_proc(ptr, &mut self.h.context as *mut Context)
+		self.thunks.get_thunk_for_proc(ptr, &mut self.context as *mut Context)
 	}
 	pub fn get_proc_addr(&mut self, name: &str) -> anyhow::Result<usize> {
-		let ptr = self.h.elf.get_proc_addr(name);
+		let ptr = self.elf.get_proc_addr(name);
 		if ptr == 0 {
 			Ok(0)
 		} else {
-			self.h.thunks.get_thunk_for_proc(ptr, &mut self.h.context as *mut Context)
+			self.thunks.get_thunk_for_proc(ptr, &mut self.context as *mut Context)
 		}
 	}
 	pub fn get_proc_addr_raw(&mut self, name: &str) -> anyhow::Result<usize> {
-		let ptr = self.h.elf.get_proc_addr(name);
+		let ptr = self.elf.get_proc_addr(name);
 		Ok(ptr)
 	}
 	fn check_sealed(&self) -> anyhow::Result<()> {
-		if !self.h.sealed {
+		if !self.sealed {
 			Err(anyhow!("Not sealed!"))
 		} else {
 			Ok(())
 		}
 	}
 	pub fn seal(&mut self) -> anyhow::Result<()> {
-		if self.h.sealed {
+		if self.sealed {
 			return Err(anyhow!("Already sealed!"))
 		}
 
-		fn run_proc(h: &mut ActivatedWaterboxHost, name: &str) {
-			match h.h.elf.get_proc_addr(name) {
+		let was_active = self.active;
+		self.activate();
+
+		fn run_proc(h: &mut WaterboxHost, name: &str) {
+			match h.elf.get_proc_addr(name) {
 				0 => (),
 				ptr => {
 					println!("Calling {}()", name);
@@ -135,47 +131,51 @@ impl<'a> ActivatedWaterboxHost<'a> {
 		run_proc(self, "co_clean");
 		run_proc(self, "ecl_seal");
 
-		self.h.elf.seal(&mut self.b);
-		self.b.seal();
-		self.h.sealed = true;
+		self.elf.seal(&mut self.memory_block);
+		self.memory_block.seal()?;
+
+		if was_active {
+			self.deactivate();
+		}
+		self.sealed = true;
 		Ok(())
 	}
 	pub fn mount_file(&mut self, name: String, data: Vec<u8>, writable: bool) -> anyhow::Result<()> {
-		self.h.fs.mount(name, data, writable)
+		self.fs.mount(name, data, writable)
 	}
 	pub fn unmount_file(&mut self, name: &str) -> anyhow::Result<Vec<u8>> {
-		self.h.fs.unmount(name)
+		self.fs.unmount(name)
 	}
 	// pub fn set_missing_file_callback(&mut self, cb: Option<MissingFileCallback>) {
-	// 	self.h.fs.set_missing_file_callback(cb);
+	// 	self.fs.set_missing_file_callback(cb);
 	// }
 
 	/// Run a guest entry point that takes no arguments
 	pub fn run_guest_simple(&mut self, entry_point: usize) {
-		context::call_guest_simple(entry_point, &mut self.h.context);
+		context::call_guest_simple(entry_point, &mut self.context);
 	}
 }
 
 const SAVE_START_MAGIC: &str = "ActivatedWaterboxHost_v1";
 const SAVE_END_MAGIC: &str = "ʇsoHxoqɹǝʇɐMpǝʇɐʌᴉʇɔ∀";
-impl<'a> IStateable for ActivatedWaterboxHost<'a> {
+impl IStateable for WaterboxHost {
 	fn save_state(&mut self, stream: &mut dyn Write) -> anyhow::Result<()> {
 		self.check_sealed()?;
 		bin::write_magic(stream, SAVE_START_MAGIC)?;
-		self.h.fs.save_state(stream)?;
-		bin::write(stream, &self.h.program_break)?;
-		self.h.elf.save_state(stream)?;
-		self.b.save_state(stream)?;
+		self.fs.save_state(stream)?;
+		bin::write(stream, &self.program_break)?;
+		self.elf.save_state(stream)?;
+		self.memory_block.save_state(stream)?;
 		bin::write_magic(stream, SAVE_END_MAGIC)?;
 		Ok(())
 	}
 	fn load_state(&mut self, stream: &mut dyn Read) -> anyhow::Result<()> {
 		self.check_sealed()?;
 		bin::verify_magic(stream, SAVE_START_MAGIC)?;
-		self.h.fs.load_state(stream)?;
-		bin::read(stream, &mut self.h.program_break)?;
-		self.h.elf.load_state(stream)?;
-		self.b.load_state(stream)?;
+		self.fs.load_state(stream)?;
+		bin::read(stream, &mut self.program_break)?;
+		self.elf.load_state(stream)?;
+		self.memory_block.load_state(stream)?;
 		bin::verify_magic(stream, SAVE_END_MAGIC)?;
 		Ok(())
 	}
@@ -228,7 +228,7 @@ fn arg_to_statbuff<'a>(arg: usize) -> &'a mut KStat {
 
 extern "sysv64" fn syscall(
 	a1: usize, a2: usize, a3: usize, a4: usize, _a5: usize, _a6: usize,
-	nr: SyscallNumber, h: &mut ActivatedWaterboxHost
+	nr: SyscallNumber, h: &mut WaterboxHost
 ) -> SyscallReturn {
 	match nr {
 		NR_MMAP => {
@@ -252,43 +252,43 @@ extern "sysv64" fn syscall(
 				}
 			}
 			let no_replace = flags & MAP_FIXED_NOREPLACE != 0;
-			let arena_addr = h.h.layout.mmap;
-			let res = h.b.mmap(AddressRange { start: a1, size: a2 }, prot, arena_addr, no_replace)?;
+			let arena_addr = h.layout.mmap;
+			let res = h.memory_block.mmap(AddressRange { start: a1, size: a2 }, prot, arena_addr, no_replace)?;
 			syscall_ok(res)
 		},
 		NR_MREMAP => {
-			let arena_addr = h.h.layout.mmap;
-			let res = h.b.mremap(AddressRange { start: a1, size: a2 }, a3, arena_addr)?;
+			let arena_addr = h.layout.mmap;
+			let res = h.memory_block.mremap(AddressRange { start: a1, size: a2 }, a3, arena_addr)?;
 			syscall_ok(res)
 		},
 		NR_MPROTECT => {
 			let prot = arg_to_prot(a3)?;
-			let res = h.b.mprotect(AddressRange { start: a1, size: a2 }, prot);
+			let res = h.memory_block.mprotect(AddressRange { start: a1, size: a2 }, prot);
 			syscall_ret(res)
 		},
-		NR_MUNMAP => syscall_ret(h.b.munmap(AddressRange { start: a1, size: a2 })),
+		NR_MUNMAP => syscall_ret(h.memory_block.munmap(AddressRange { start: a1, size: a2 })),
 		NR_MADVISE => {
 			match a3 {
-				MADV_DONTNEED => syscall_ret(h.b.madvise_dontneed(AddressRange { start: a1, size: a2 })),
+				MADV_DONTNEED => syscall_ret(h.memory_block.madvise_dontneed(AddressRange { start: a1, size: a2 })),
 				_ => syscall_ok(0),
 			}
 		},
 		NR_STAT => {
 			let name = arg_to_str(a1)?;
-			syscall_ret(h.h.fs.stat(&name, arg_to_statbuff(a2)))
+			syscall_ret(h.fs.stat(&name, arg_to_statbuff(a2)))
 		},
 		NR_FSTAT => {
-			syscall_ret(h.h.fs.fstat(arg_to_fd(a1)?, arg_to_statbuff(a2)))
+			syscall_ret(h.fs.fstat(arg_to_fd(a1)?, arg_to_statbuff(a2)))
 		},
 		NR_IOCTL => syscall_ok(0),
 		NR_READ => {
 			unsafe {
-				syscall_ret_i64(h.h.fs.read(arg_to_fd(a1)?, std::slice::from_raw_parts_mut(a2 as *mut u8, a3)))
+				syscall_ret_i64(h.fs.read(arg_to_fd(a1)?, std::slice::from_raw_parts_mut(a2 as *mut u8, a3)))
 			}
 		},
 		NR_WRITE => {
 			unsafe {
-				syscall_ret_i64(h.h.fs.write(arg_to_fd(a1)?, std::slice::from_raw_parts(a2 as *const u8, a3)))
+				syscall_ret_i64(h.fs.write(arg_to_fd(a1)?, std::slice::from_raw_parts(a2 as *const u8, a3)))
 			}
 		},
 		NR_READV => {
@@ -298,7 +298,7 @@ extern "sysv64" fn syscall(
 				let iov = std::slice::from_raw_parts_mut(a2 as *mut Iovec, a3);
 				for io in iov {
 					if io.iov_base != 0 {
-						ret += h.h.fs.read(fd, io.slice_mut())?;
+						ret += h.fs.read(fd, io.slice_mut())?;
 					}
 				}
 				syscall_ok(ret as usize)
@@ -311,19 +311,19 @@ extern "sysv64" fn syscall(
 				let iov = std::slice::from_raw_parts(a2 as *const Iovec, a3);
 				for io in iov {
 					if io.iov_base != 0 {
-						ret += h.h.fs.write(fd, io.slice())?;
+						ret += h.fs.write(fd, io.slice())?;
 					}
 				}
 				syscall_ok(ret as usize)
 			}
 		},
 		NR_OPEN => {
-			syscall_ret_val(h.h.fs.open(&arg_to_str(a1)?, a2 as i32, a3 as i32).map(|x| x.0 as usize))
+			syscall_ret_val(h.fs.open(&arg_to_str(a1)?, a2 as i32, a3 as i32).map(|x| x.0 as usize))
 		},
-		NR_CLOSE => syscall_ret(h.h.fs.close(arg_to_fd(a1)?)),
-		NR_LSEEK => syscall_ret_i64(h.h.fs.seek(arg_to_fd(a1)?, a2 as i64, a3 as i32)),
-		NR_TRUNCATE => syscall_ret(h.h.fs.truncate(&arg_to_str(a1)?, a2 as i64)),
-		NR_FTRUNCATE => syscall_ret(h.h.fs.ftruncate(arg_to_fd(a1)?, a2 as i64)),
+		NR_CLOSE => syscall_ret(h.fs.close(arg_to_fd(a1)?)),
+		NR_LSEEK => syscall_ret_i64(h.fs.seek(arg_to_fd(a1)?, a2 as i64, a3 as i32)),
+		NR_TRUNCATE => syscall_ret(h.fs.truncate(&arg_to_str(a1)?, a2 as i64)),
+		NR_FTRUNCATE => syscall_ret(h.fs.ftruncate(arg_to_fd(a1)?, a2 as i64)),
 		// TODO: 99% sure nothing calls this
 		NR_SET_THREAD_AREA => syscall_err(ENOSYS),
 		// TODO: What calls this?
@@ -338,8 +338,8 @@ extern "sysv64" fn syscall(
 		},
 		NR_BRK => {
 			// TODO: This could be done on the C side
-			let addr = h.h.layout.sbrk;
-			let old = h.h.program_break;
+			let addr = h.layout.sbrk;
+			let old = h.program_break;
 			let res = if a1 != align_down(a1) {
 				old
 			} else if a1 < addr.start {
@@ -351,13 +351,13 @@ extern "sysv64" fn syscall(
 				eprintln!("Failed to satisfy allocation of {} bytes on sbrk heap", a1 - old);
 				old	
 			} else if a1 > old {
-				h.b.mmap_fixed(AddressRange { start: old, size: a1 - old }, Protection::RW, true).unwrap();
+				h.memory_block.mmap_fixed(AddressRange { start: old, size: a1 - old }, Protection::RW, true).unwrap();
 				println!("Allocated {} bytes on sbrk heap, usage {}/{}", a1 - old, a1 - addr.start, addr.size);
 				a1
 			} else {
 				old
 			};
-			h.h.program_break = res;
+			h.program_break = res;
 			syscall_ok(res)
 		},
 		_ => syscall_ret(unimp(nr)),

src/BizHawk.Emulation.Cores/Waterbox/WaterboxHost.cs
@@ -71,7 +71,6 @@ namespace BizHawk.Emulation.Cores.Waterbox
 	public unsafe class WaterboxHost : IMonitor, IImportResolver, IBinaryStateable, IDisposable, ICallbackAdjuster
 	{
 		private IntPtr _nativeHost;
-		private IntPtr _activatedNativeHost;
 		private int _enterCount;
 		private object _keepAliveDelegate;
 
@@ -132,12 +131,9 @@ namespace BizHawk.Emulation.Cores.Waterbox
 
 		public IntPtr GetProcAddrOrZero(string entryPoint)
 		{
-			using (this.EnterExit())
-			{
-				var retobj = new ReturnData();
-				NativeImpl.wbx_get_proc_addr_raw(_activatedNativeHost, entryPoint, retobj);
-				return retobj.GetDataOrThrow();
-			}
+			var retobj = new ReturnData();
+			NativeImpl.wbx_get_proc_addr_raw(_nativeHost, entryPoint, retobj);
+			return retobj.GetDataOrThrow();
 		}
 
 		public IntPtr GetProcAddrOrThrow(string entryPoint)
@@ -155,32 +151,23 @@ namespace BizHawk.Emulation.Cores.Waterbox
 
 		public IntPtr GetCallbackProcAddr(IntPtr exitPoint, int slot)
 		{
-			using (this.EnterExit())
-			{
-				var retobj = new ReturnData();
-				NativeImpl.wbx_get_callback_addr(_activatedNativeHost, exitPoint, slot, retobj);
-				return retobj.GetDataOrThrow();
-			}
+			var retobj = new ReturnData();
+			NativeImpl.wbx_get_callback_addr(_nativeHost, exitPoint, slot, retobj);
+			return retobj.GetDataOrThrow();
 		}
 
 		public IntPtr GetCallinProcAddr(IntPtr entryPoint)
 		{
-			using (this.EnterExit())
-			{
-				var retobj = new ReturnData();
-				NativeImpl.wbx_get_callin_addr(_activatedNativeHost, entryPoint, retobj);
-				return retobj.GetDataOrThrow();
-			}
+			var retobj = new ReturnData();
+			NativeImpl.wbx_get_callin_addr(_nativeHost, entryPoint, retobj);
+			return retobj.GetDataOrThrow();
 		}
 
 		public void Seal()
 		{
-			using (this.EnterExit())
-			{
-				var retobj = new ReturnData();
-				NativeImpl.wbx_seal(_activatedNativeHost, retobj);
-				retobj.GetDataOrThrow();
-			}
+			var retobj = new ReturnData();
+			NativeImpl.wbx_seal(_nativeHost, retobj);
+			retobj.GetDataOrThrow();
 			Console.WriteLine("WaterboxHost Sealed!");
 		}
 
@@ -191,12 +178,9 @@ namespace BizHawk.Emulation.Cores.Waterbox
 		/// <param name="name">the filename that the unmanaged core will access the file by</param>
 		public void AddReadonlyFile(byte[] data, string name)
 		{
-			using (this.EnterExit())
-			{
-				var retobj = new ReturnData();
-				NativeImpl.wbx_mount_file(_activatedNativeHost, name, Reader(new MemoryStream(data, false)), IntPtr.Zero, false, retobj);
-				retobj.GetDataOrThrow();
-			}
+			var retobj = new ReturnData();
+			NativeImpl.wbx_mount_file(_nativeHost, name, Reader(new MemoryStream(data, false)), IntPtr.Zero, false, retobj);
+			retobj.GetDataOrThrow();
 		}
 
 		/// <summary>
@@ -205,12 +189,9 @@ namespace BizHawk.Emulation.Cores.Waterbox
 		/// </summary>
 		public void RemoveReadonlyFile(string name)
 		{
-			using (this.EnterExit())
-			{
-				var retobj = new ReturnData();
-				NativeImpl.wbx_unmount_file(_activatedNativeHost, name, null, IntPtr.Zero, retobj);
-				retobj.GetDataOrThrow();
-			}
+			var retobj = new ReturnData();
+			NativeImpl.wbx_unmount_file(_nativeHost, name, null, IntPtr.Zero, retobj);
+			retobj.GetDataOrThrow();
 		}
 
 		/// <summary>
@@ -219,12 +200,9 @@ namespace BizHawk.Emulation.Cores.Waterbox
 		/// </summary>
 		public void AddTransientFile(byte[] data, string name)
 		{
-			using (this.EnterExit())
-			{
-				var retobj = new ReturnData();
-				NativeImpl.wbx_mount_file(_activatedNativeHost, name, Reader(new MemoryStream(data, false)), IntPtr.Zero, true, retobj);
-				retobj.GetDataOrThrow();
-			}
+			var retobj = new ReturnData();
+			NativeImpl.wbx_mount_file(_nativeHost, name, Reader(new MemoryStream(data, false)), IntPtr.Zero, true, retobj);
+			retobj.GetDataOrThrow();
 		}
 
 		/// <summary>
@@ -233,14 +211,11 @@ namespace BizHawk.Emulation.Cores.Waterbox
 		/// <returns>The state of the file when it was removed</returns>
 		public byte[] RemoveTransientFile(string name)
 		{
-			using (this.EnterExit())
-			{
 				var retobj = new ReturnData();
 				var ms = new MemoryStream();
-				NativeImpl.wbx_unmount_file(_activatedNativeHost, name, Writer(ms), IntPtr.Zero, retobj);
+				NativeImpl.wbx_unmount_file(_nativeHost, name, Writer(ms), IntPtr.Zero, retobj);
 				retobj.GetDataOrThrow();
 				return ms.ToArray();
-			}
 		}
 
 		// public class MissingFileResult
@@ -284,22 +259,15 @@ namespace BizHawk.Emulation.Cores.Waterbox
 
 		public void SaveStateBinary(BinaryWriter bw)
 		{
-			using (this.EnterExit())
-			{
-				var retobj = new ReturnData();
-				NativeImpl.wbx_save_state(_activatedNativeHost, Writer(bw.BaseStream), IntPtr.Zero, retobj);
-				retobj.GetDataOrThrow();
-			}
+			var retobj = new ReturnData();
+			NativeImpl.wbx_save_state(_nativeHost, Writer(bw.BaseStream), IntPtr.Zero, retobj);
+			retobj.GetDataOrThrow();
 		}
 
 		public void LoadStateBinary(BinaryReader br)
-		{
-			using (this.EnterExit())
-			{
-				var retobj = new ReturnData();
-				NativeImpl.wbx_load_state(_activatedNativeHost, Reader(br.BaseStream), IntPtr.Zero, retobj);
-				retobj.GetDataOrThrow();
-			}
+		{				var retobj = new ReturnData();
+			NativeImpl.wbx_load_state(_nativeHost, Reader(br.BaseStream), IntPtr.Zero, retobj);
+			retobj.GetDataOrThrow();
 		}
 
 		public void Enter()
@@ -308,7 +276,7 @@ namespace BizHawk.Emulation.Cores.Waterbox
 			{
 				var retobj = new ReturnData();
 				NativeImpl.wbx_activate_host(_nativeHost, retobj);
-				_activatedNativeHost = retobj.GetDataOrThrow();
+				retobj.GetDataOrThrow();
 			}
 			_enterCount++;
 		}
@@ -322,9 +290,8 @@ namespace BizHawk.Emulation.Cores.Waterbox
 			else if (_enterCount == 1)
 			{
 				var retobj = new ReturnData();
-				NativeImpl.wbx_deactivate_host(_activatedNativeHost, retobj);
+				NativeImpl.wbx_deactivate_host(_nativeHost, retobj);
 				retobj.GetDataOrThrow();
-				_activatedNativeHost = IntPtr.Zero;
 			}
 			_enterCount--;
 		}
@@ -334,11 +301,10 @@ namespace BizHawk.Emulation.Cores.Waterbox
 			if (_nativeHost != IntPtr.Zero)
 			{
 				var retobj = new ReturnData();
-				if (_activatedNativeHost != IntPtr.Zero)
+				if (_enterCount != 0)
 				{
-					NativeImpl.wbx_deactivate_host(_activatedNativeHost, retobj);
+					NativeImpl.wbx_deactivate_host(_nativeHost, retobj);
 					Console.Error.WriteLine("Warn: Disposed of WaterboxHost which was active");
-					_activatedNativeHost = IntPtr.Zero;
 				}
 				NativeImpl.wbx_destroy_host(_nativeHost, retobj);
 				_enterCount = 0;

src/BizHawk.Emulation.Cores/Waterbox/WaterboxHostNative.cs
@@ -127,7 +127,7 @@ namespace BizHawk.Emulation.Cores.Waterbox
 		public abstract void wbx_create_host(MemoryLayoutTemplate layout, string moduleName, ReadCallback wbx, IntPtr userdata, ReturnData /*WaterboxHost*/ ret);
 
 		/// <summary>
-		/// Tear down a host environment.  May not be called while the environment is active.
+		/// Tear down a host environment.  If called while the environment is active, will deactivate it first.
 		/// </summary>
 		[BizImport(CallingConvention.Cdecl)]
 		public abstract void wbx_destroy_host(IntPtr /*WaterboxHost*/ obj, ReturnData /*void*/ ret);
@@ -136,23 +136,24 @@ namespace BizHawk.Emulation.Cores.Waterbox
 		/// Activate a host environment.  This swaps it into memory and makes it available for use.
 		/// Pointers to inside the environment are only valid while active.  Uses a mutex internally
 		/// so as to not stomp over other host environments in the same 4GiB slice.
-		/// Returns a pointer to the activated object, used to do most other functions.
+		/// Ignored if host is already active.
 		/// </summary>
 		[BizImport(CallingConvention.Cdecl)]
-		public abstract void wbx_activate_host(IntPtr /*WaterboxHost*/ obj, ReturnData /*ActivatedWaterboxHost*/ ret);
+		public abstract void wbx_activate_host(IntPtr /*WaterboxHost*/ obj, ReturnData /*void*/ ret);
 
 		/// <summary>
 		/// Deactivates a host environment, and releases the mutex.
+		/// Ignored if host is not active
 		/// </summary>
 		[BizImport(CallingConvention.Cdecl)]
-		public abstract void wbx_deactivate_host(IntPtr /*ActivatedWaterboxHost*/ obj, ReturnData /*void*/ ret);
+		public abstract void wbx_deactivate_host(IntPtr /*WaterboxHost*/ obj, ReturnData /*void*/ ret);
 		/// <summary>
 		/// Returns a thunk suitable for calling an exported function from the guest executable.  This pointer is only valid
 		/// while the host is active.  A missing proc is not an error and simply returns 0.  The guest function must be,
 		/// and the returned callback will be, sysv abi, and will only pass up to 6 int/ptr args and no other arg types.
 		/// </summary>
 		[BizImport(CallingConvention.Cdecl)]
-		public abstract void wbx_get_proc_addr(IntPtr /*ActivatedWaterboxHost*/ obj, string name, ReturnData /*UIntPtr*/ ret);
+		public abstract void wbx_get_proc_addr(IntPtr /*WaterboxHost*/ obj, string name, ReturnData /*UIntPtr*/ ret);
 		/// <summary>
 		/// Returns a thunk suitable for calling an arbitrary entry point into the guest executable.  This pointer is only valid
 		/// while the host is active.  wbx_get_proc_addr already calls this internally on pointers it returns, so this call is
@@ -160,14 +161,14 @@ namespace BizHawk.Emulation.Cores.Waterbox
 		/// a pointer to another function).
 		/// </summary>
 		[BizImport(CallingConvention.Cdecl)]
-		public abstract void wbx_get_callin_addr(IntPtr /*ActivatedWaterboxHost*/ obj, IntPtr ptr, ReturnData /*UIntPtr*/ ret);
+		public abstract void wbx_get_callin_addr(IntPtr /*WaterboxHost*/ obj, IntPtr ptr, ReturnData /*UIntPtr*/ ret);
 		/// <summary>
 		/// Returns the raw address of a function exported from the guest.  `wbx_get_proc_addr()` is equivalent to
 		/// `wbx_get_callin_addr(wbx_get_proc_addr_raw()).  Most things should not use this directly, as the returned
 		/// pointer will not have proper stack hygiene and will crash on syscalls from the guest.
 		/// </summary>
 		[BizImport(CallingConvention.Cdecl)]
-		public abstract void wbx_get_proc_addr_raw(IntPtr /*ActivatedWaterboxHost*/ obj, string name, ReturnData /*UIntPtr*/ ret);
+		public abstract void wbx_get_proc_addr_raw(IntPtr /*WaterboxHost*/ obj, string name, ReturnData /*UIntPtr*/ ret);
 		/// <summary>
 		/// Returns a function pointer suitable for passing to the guest to allow it to call back while active.
 		/// Slot number is an integer that is used to keep pointers consistent across runs:  If the host is loaded
@@ -176,12 +177,12 @@ namespace BizHawk.Emulation.Cores.Waterbox
 		/// The returned thunk will be, and the callback must be, sysv abi and will only pass up to 6 int/ptr args and no other arg types.
 		/// </summary>
 		[BizImport(CallingConvention.Cdecl)]
-		public abstract void wbx_get_callback_addr(IntPtr /*ActivatedWaterboxHost*/ obj, IntPtr callback, int slot, ReturnData /*UIntPtr*/ ret);
+		public abstract void wbx_get_callback_addr(IntPtr /*WaterboxHost*/ obj, IntPtr callback, int slot, ReturnData /*UIntPtr*/ ret);
 		/// <summary>
 		/// Calls the seal operation, which is a one time action that prepares the host to save states.
 		/// </summary>
 		[BizImport(CallingConvention.Cdecl)]
-		public abstract void wbx_seal(IntPtr /*ActivatedWaterboxHost*/ obj, ReturnData /*void*/ ret);
+		public abstract void wbx_seal(IntPtr /*WaterboxHost*/ obj, ReturnData /*void*/ ret);
 
 		/// <summary>
 		/// Mounts a file in the environment.  All data will be immediately consumed from the reader, which will not be used after this call.
@@ -190,14 +191,14 @@ namespace BizHawk.Emulation.Cores.Waterbox
 		/// but it must exist in every savestate and the exact sequence of add_file calls must be consistent from savestate to savestate.
 		/// </summary>
 		[BizImport(CallingConvention.Cdecl)]
-		public abstract void wbx_mount_file(IntPtr /*ActivatedWaterboxHost*/ obj, string name, ReadCallback reader, IntPtr userdata, bool writable, ReturnData /*void*/ ret);
+		public abstract void wbx_mount_file(IntPtr /*WaterboxHost*/ obj, string name, ReadCallback reader, IntPtr userdata, bool writable, ReturnData /*void*/ ret);
 
 		/// <summary>
 		/// Remove a file previously added.  Writer is optional; if provided, the contents of the file at time of removal will be dumped to it.
 		/// It is an error to remove a file which is currently open in the guest.
 		/// </summary>
 		[BizImport(CallingConvention.Cdecl)]
-		public abstract void wbx_unmount_file(IntPtr /*ActivatedWaterboxHost*/ obj, string name, WriteCallback writer, IntPtr userdata, ReturnData /*void*/ ret);
+		public abstract void wbx_unmount_file(IntPtr /*WaterboxHost*/ obj, string name, WriteCallback writer, IntPtr userdata, ReturnData /*void*/ ret);
 
 #if false
 		/// <summary>
@@ -208,7 +209,7 @@ namespace BizHawk.Emulation.Cores.Waterbox
 		/// as wbx_mount_file().  You may free resources associated with the MissingFileResult whenever control next returns to your code.
 		/// </summary>
 		[BizImport(CallingConvention.Cdecl)]
-		public abstract void wbx_set_missing_file_callback(IntPtr /*ActivatedWaterboxHost*/ obj, MissingFileCallback mfc_o);
+		public abstract void wbx_set_missing_file_callback(IntPtr /*WaterboxHost*/ obj, MissingFileCallback mfc_o);
 #endif
 
 		/// <summary>
@@ -216,7 +217,7 @@ namespace BizHawk.Emulation.Cores.Waterbox
 		/// Must always be called with the same sequence and contents of readonly files.
 		/// </summary>
 		[BizImport(CallingConvention.Cdecl)]
-		public abstract void wbx_save_state(IntPtr /*ActivatedWaterboxHost*/ obj, WriteCallback writer, IntPtr userdata, ReturnData /*void*/ ret);
+		public abstract void wbx_save_state(IntPtr /*WaterboxHost*/ obj, WriteCallback writer, IntPtr userdata, ReturnData /*void*/ ret);
 
 		/// <summary>
 		/// Load state.  Must not be called before seal.  Must not be called with any writable files mounted.
@@ -225,7 +226,7 @@ namespace BizHawk.Emulation.Cores.Waterbox
 		/// Errors generally poison the environment; sorry!
 		/// </summary>
 		[BizImport(CallingConvention.Cdecl)]
-		public abstract void wbx_load_state(IntPtr /*ActivatedWaterboxHost*/ obj, ReadCallback reader, IntPtr userdata, ReturnData /*void*/ ret);
+		public abstract void wbx_load_state(IntPtr /*WaterboxHost*/ obj, ReadCallback reader, IntPtr userdata, ReturnData /*void*/ ret);
 
 		/// <summary>
 		/// Control whether the host automatically evicts blocks from memory when they are not active.  For the best performance,

waterbox/waterboxhost/Cargo.toml
@@ -16,7 +16,7 @@ lto = true
 bitflags = "1.2.1"
 page_size = "0.4.2"
 lazy_static = "1.4.0"
-getset = "0.1.1"
+#getset = "0.1.1"
 parking_lot = "0.10.2"
 itertools = "0.9.0"
 goblin = { version = "0.2.3", features = ["elf64", "std"] }

commit c7e8744664968923f1e448e04c397f36d933e049

Add an extra exclamation point to indicate that code is VERY IMPORTANT
This broke any waterbox core that called in to native code in the same EnterExit() right after sealing.  All nyma cores were broken, 32x was not, didn't check the rest.  Regressed in 175556529ea40b78a246edfbe020e.

It worked fine in release mode, theoretically

waterbox/waterboxhost/src/host.rs
@@ -134,7 +134,7 @@ impl WaterboxHost {
 		self.elf.seal(&mut self.memory_block);
 		self.memory_block.seal()?;
 
-		if was_active {
+		if !was_active {
 			self.deactivate();
 		}
 		self.sealed = true;