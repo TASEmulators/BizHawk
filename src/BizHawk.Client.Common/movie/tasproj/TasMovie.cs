55886c8e0ca6fbcb48f552520b70c740d1faa237
adelikat <adelikat@tasvideos.org>
2020-08-23 13:21:57 -0500

    reserve the frame before markers, not hte marker itself, users expect instant navigation to markers, and since we always navigate 1 frame before the target frame to emulate and get a frame buffer, this is the frame that must be reserved

--- src/BizHawk.Client.Common/movie/tasproj/TasMovie.cs
+++ src/BizHawk.Client.Common/movie/tasproj/TasMovie.cs
@@ -341,9 +341,12 @@ namespace BizHawk.Client.Common
 
 		private bool IsReserved(int frame)
 		{
-			// Branches should already be in the reserved list, but it doesn't hurt to check
-			return Markers.Any(m => m.Frame == frame)
-				|| Branches.Any(b => b.Frame == frame);
+			
+			// Why the frame before?
+			// because we always navigate to the frame before and emulate 1 frame so that we ensure a proper frame buffer on the screen
+			// users want instant navigation to markers, so to do this, we need to reserve the frame before the marker, not the marker itself
+			return Markers.Any(m => m.Frame - 1 == frame)
+				|| Branches.Any(b => b.Frame == frame); // Branches should already be in the reserved list, but it doesn't hurt to check
 		}
 	}
 }
