dc7f5ef7a2bf98fd8e8ae8cd15ff4254dfe68c87
YoshiRulz <OSSYoshiRulz@gmail.com>
2020-08-30 05:52:50 +1000

    Fix typo
    fixes a11a7017a

--- src/BizHawk.Client.Common/Controller.cs
+++ src/BizHawk.Client.Common/Controller.cs
@@ -69,8 +69,8 @@ namespace BizHawk.Client.Common
 				value *= kvp.Value.Mult;
 
 				// -1..1 -> range
-				value += range.Mid;
 				value *= Math.Max(range.Mid - range.Min, range.Max - range.Mid);
+				value += range.Mid;
 
 				// finally, constrain to range again in case the original value was unexpectedly large, or the deadzone and scale made it so, or the axis is lopsided
 				_axes[kvp.Key] = ((int) value).ConstrainWithin(range.Range);
