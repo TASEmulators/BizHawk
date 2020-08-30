76886af08d447b3a078471a35d6d098549023971
Paige Seivertson <pzeig66@yahoo.com>
2020-08-30 16:40:04 -0400

    Added Enable RPC Checkbox config handling

--- src/BizHawk.Client.EmuHawk/config/EmuHawkOptions.cs
+++ src/BizHawk.Client.EmuHawk/config/EmuHawkOptions.cs
@@ -69,6 +69,7 @@ namespace BizHawk.Client.EmuHawk
 			RunInBackgroundCheckbox.Checked = _config.RunInBackground;
 			AcceptBackgroundInputCheckbox.Checked = _config.AcceptBackgroundInput;
 			AcceptBackgroundInputControllerOnlyCheckBox.Checked = _config.AcceptBackgroundInputControllerOnly;
+			EnableRPC.Checked = _config.EnableRPC;
 			HandleAlternateKeyboardLayoutsCheckBox.Checked = _config.HandleAlternateKeyboardLayouts;
 			NeverAskSaveCheckbox.Checked = _config.SuppressAskSave;
 			SingleInstanceModeCheckbox.Checked = _config.SingleInstanceMode;
@@ -118,6 +119,7 @@ namespace BizHawk.Client.EmuHawk
 			_config.RunInBackground = RunInBackgroundCheckbox.Checked;
 			_config.AcceptBackgroundInput = AcceptBackgroundInputCheckbox.Checked;
 			_config.AcceptBackgroundInputControllerOnly = AcceptBackgroundInputControllerOnlyCheckBox.Checked;
+			_config.EnableRPC = EnableRPC.Checked;
 			_config.HandleAlternateKeyboardLayouts = HandleAlternateKeyboardLayoutsCheckBox.Checked;
 			_config.SuppressAskSave = NeverAskSaveCheckbox.Checked;
 			_config.SingleInstanceMode = SingleInstanceModeCheckbox.Checked;
