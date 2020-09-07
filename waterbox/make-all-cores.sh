6047a12ad8c5b1a915199ce388200c25da164740
nattthebear <goyuken@gmail.com>
2020-09-07 11:07:48 -0400

    Rework and fix up invisible allocations and sound/video format init in nyma cores.
    See the submodule commits for the tech details.  Fixes #2365.  As a side effect, Faust state sizes should be moderately smaller now.

--- waterbox/make-all-cores.sh
+++ waterbox/make-all-cores.sh
@@ -10,12 +10,4 @@ cd sameboy && make -f Makefile $1 -j && cd -
 cd snes9x && make -f Makefile $1 -j && cd -
 cd uzem && make -f Makefile $1 -j && cd -
 cd vb && make -f Makefile $1 -j && cd -
-cd nyma && make -f faust.mak $1 -j && cd -
-cd nyma && make -f hyper.mak $1 -j && cd -
-#cd nyma && make -f lynx.mak $1 -j && cd -
-cd nyma && make -f ngp.mak $1 -j && cd -
-cd nyma && make -f pcfx.mak $1 -j && cd -
-cd nyma && make -f ss.mak $1 -j && cd -
-cd nyma && make -f turbo.mak $1 -j && cd -
-#cd nyma && make -f vb.mak $1 -j && cd -
-#cd nyma && make -f wswan.mak $1 -j && cd -
+cd nyma && ./make-all-released-cores.sh $1 && cd -
--- waterbox/nyma/rebuild-all-released-cores.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-make -f turbo.mak install -j
-make -f hyper.mak install -j
-make -f ngp.mak install -j
-make -f faust.mak install -j
-make -f pcfx.mak install -j
-make -f ss.mak install -j
