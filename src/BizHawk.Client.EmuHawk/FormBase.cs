TASEmulators/BizHawk

commit aa1de1c9d23709ba53b6f3b3fffb1499d0f26dab

Unify setting of window titles, add static title option (fixes #1996)
The only forms that did anything special with their window title were Basic Bot,
CDL, Hex Editor, Log Window, MainForm, and TAStudio. That behaviour is in
WindowTitle, while the rest use WindowTitleStatic.
The implementations of WindowTitleStatic in those six forms are new. There's a
checkbox in `Config` > `Display...` > `Misc` to disable their special behaviour
and use the new static titles.
The Text property is hidden from Designer de/serialisation as well.

src/BizHawk.Client.EmuHawk/FormBase.cs
@@ -1,0 +1,40 @@
+#nullable enable
+
+using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+using BizHawk.Client.Common;
+
+namespace BizHawk.Client.EmuHawk
+{
+	public class FormBase : Form
+	{
+		private string? _windowTitleStatic;
+
+		public Config? Config { get; protected set; }
+
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+		public override string Text
+		{
+			get => base.Text;
+			set => throw new InvalidOperationException("window title can only be changed by calling " + nameof(UpdateWindowTitle) + " (which calls " + nameof(WindowTitle) + " getter)");
+		}
+
+		protected virtual string WindowTitle => WindowTitleStatic;
+
+		/// <remarks>To enforce the "static title" semantics for implementations, this getter will be called once and cached.</remarks>
+		protected virtual string WindowTitleStatic => throw new NotImplementedException("you have to implement this; the Designer prevents this from being an abstract method");
+
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+			UpdateWindowTitle();
+		}
+
+		public void UpdateWindowTitle()
+			=> base.Text = Config?.UseStaticWindowTitles == true
+				? (_windowTitleStatic ??= WindowTitleStatic)
+				: WindowTitle;
+	}
+}

commit afbce436cd5805395ddaa6eecbb91f7b60842347

Restore FormBase.WindowTitleStatic semantics without breaking Designer
fixes daf6f516c which fixed regression in aa1de1c9d

src/BizHawk.Client.EmuHawk/FormBase.cs
@@ -24,7 +24,9 @@ namespace BizHawk.Client.EmuHawk
 		protected virtual string WindowTitle => WindowTitleStatic;
 
 		/// <remarks>To enforce the "static title" semantics for implementations, this getter will be called once and cached.</remarks>
-		protected virtual string WindowTitleStatic => GetType().Name;
+		protected virtual string WindowTitleStatic => DesignMode
+			? $"(will take value from WindowTitle/WindowTitleStatic)"
+			: throw new NotImplementedException("you have to implement this; the Designer prevents this from being an abstract method");
 
 		protected override void OnLoad(EventArgs e)
 		{