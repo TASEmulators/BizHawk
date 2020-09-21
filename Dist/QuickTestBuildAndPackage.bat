5ae13a230903137b5b8f49ab4ccb7490455f83fe
YoshiRulz <OSSYoshiRulz@gmail.com>
2020-09-22 09:20:03 +1000

    Change build path of executables to output/dll
    * post-build step moves .exes up a level, to output
    * also fixed OpenTK.dll.config location
    * also removed redundant step from packaging scripts
    * future executables need to be capable of resolving assemblies in dll

--- Dist/QuickTestBuildAndPackage.bat
+++ Dist/QuickTestBuildAndPackage.bat
@@ -25,13 +25,6 @@ cd ..\output
 rem slimdx has a way of not making it into the output directory, so this is a good time to make sure
 copy ..\..\SlimDx.dll
 
-rem at the present we support moving all these dlls into the dll subdirectory
-rem that might be troublesome some day..... if it does get troublesome, then we'll have to 
-rem explicitly list the OK ones here as individual copies. until then....
-
-copy *.dll dll
-copy *.dll.config dll
-
 rem Now, we're about to zip and then unzip. Why, you ask? Because that's just the way this evolved.
 ..\dist\zip.exe -X -r ..\Dist\%NAME% EmuHawk.exe EmuHawk.exe.config DiscoHawk.exe DiscoHawk.exe.config defctrl.json EmuHawkMono.sh dll shaders gamedb Tools NES\Palettes Lua Gameboy\Palettes -x *.pdb -x *.lib -x *.pgd -x *.ipdb -x *.iobj -x *.exp -x dll\libsneshawk-64*.exe -x *.ilk -x dll\gpgx.elf -x dll\miniclient.* 
 
