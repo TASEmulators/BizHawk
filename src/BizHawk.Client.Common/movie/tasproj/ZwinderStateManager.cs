ceb6836962d0df659f4a867764eb8a4791a00d0c
adelikat <adelikat@tasvideos.org>
2020-09-29 18:01:14 -0500

    remove a space

--- src/BizHawk.Client.Common/movie/tasproj/ZwinderStateManager.cs
+++ src/BizHawk.Client.Common/movie/tasproj/ZwinderStateManager.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using  BizHawk.Common;
+using BizHawk.Common;
 using BizHawk.Emulation.Common;
 
 namespace BizHawk.Client.Common
