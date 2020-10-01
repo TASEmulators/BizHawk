8da552581979f59003999ce75c1dc00139d28134
zeromus <zeromus@users.noreply.github.com>
2020-10-01 03:40:19 -0400

    resume m3u support by turning the m3u to an xml game and then loading that. fixes #2378

--- src/BizHawk.Emulation.DiscSystem/Disc.cs
+++ src/BizHawk.Emulation.DiscSystem/Disc.cs
@@ -18,6 +18,43 @@ namespace BizHawk.Emulation.DiscSystem
 {
 	public sealed partial class Disc : IDisposable
 	{
+		public static Disc CreateAnyType(string path, Action<string> errorCallback)
+		{
+			return Create(null, path, errorCallback);
+		}
+
+		public static Disc Create(DiscType? type, string path, Action<string> errorCallback)
+		{
+			//--- load the disc in a context which will let us abort if it's going to take too long
+			var discMountJob = new DiscMountJob { IN_FromPath = path, IN_SlowLoadAbortThreshold = 8 };
+			discMountJob.Run();
+			var disc = discMountJob.OUT_Disc ?? throw new InvalidOperationException($"Can't find the file specified: {path}");
+
+			if (discMountJob.OUT_SlowLoadAborted)
+			{
+				errorCallback("This disc would take too long to load. Run it through DiscoHawk first, or find a new rip because this one is probably junk");
+				return null;
+			}
+
+			if (discMountJob.OUT_ErrorLevel)
+			{
+				throw new InvalidOperationException($"\r\n{discMountJob.OUT_Log}");
+			}
+
+			if (type != null)
+			{
+				var discType = new DiscIdentifier(disc).DetectDiscType();
+
+				if (type.HasValue && discType != type)
+				{
+					errorCallback($"Not a {type} disc");
+					return null;
+				}
+			}
+
+			return disc;
+		}
+
 		/// <summary>
 		/// Automagically loads a disc, without any fine-tuned control at all
 		/// </summary>
