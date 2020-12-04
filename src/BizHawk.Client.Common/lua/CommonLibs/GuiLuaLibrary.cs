TASEmulators/BizHawk

commit c8e10120db344420c41183dd4b186ccf4d2179b7

Encapsulate conversion to/from LuaTable, stop passing NLua to Lua libs

src/BizHawk.Client.Common/lua/CommonLibs/GuiLuaLibrary.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Linq;
 
 using NLua;
 
@@ -9,8 +10,8 @@ namespace BizHawk.Client.Common
 	{
 		public Func<int, int, int?, int?, LuaTable> CreateLuaCanvasCallback { get; set; }
 
-		public GuiLuaLibrary(LuaLibraries luaLibsImpl, ApiContainer apiContainer, Lua lua, Action<string> logOutputCallback)
-			: base(luaLibsImpl, apiContainer, lua, logOutputCallback) {}
+		public GuiLuaLibrary(LuaLibraries luaLibsImpl, ApiContainer apiContainer, Action<string> logOutputCallback)
+			: base(luaLibsImpl, apiContainer, logOutputCallback) {}
 
 		public override string Name => "gui";
 
@@ -62,7 +63,7 @@ namespace BizHawk.Client.Common
 			{
 				var pointsArr = new Point[4];
 				var i = 0;
-				foreach (LuaTable point in points.Values)
+				foreach (var point in _th.EnumerateValues<LuaTable>(points))
 				{
 					pointsArr[i] = new Point(LuaInt(point[1]), LuaInt(point[2]));
 					i++;
@@ -123,11 +124,12 @@ namespace BizHawk.Client.Common
 		[LuaMethod("drawPolygon", "Draws a polygon using the table of coordinates specified in points. This should be a table of tables(each of size 2). If x or y is passed, the polygon will be translated by the passed coordinate pair. Line is the color of the polygon. Background is the optional fill color")]
 		public void DrawPolygon(LuaTable points, int? offsetX = null, int? offsetY = null, Color? line = null, Color? background = null)
 		{
+			var pointsList = _th.EnumerateValues<LuaTable>(points).ToList();
 			try
 			{
-				var pointsArr = new Point[points.Values.Count];
+				var pointsArr = new Point[pointsList.Count];
 				var i = 0;
-				foreach (LuaTable point in points.Values)
+				foreach (var point in pointsList)
 				{
 					pointsArr[i] = new Point(LuaInt(point[1]) + (offsetX ?? 0), LuaInt(point[2]) + (offsetY ?? 0));
 					i++;

src/BizHawk.Client.EmuHawk/tools/Lua/Libraries/FormsLuaLibrary.cs
@@ -14,8 +14,8 @@ namespace BizHawk.Client.EmuHawk
 	[Description("A library for creating and managing custom dialogs")]
 	public sealed class FormsLuaLibrary : LuaLibraryBase
 	{
-		public FormsLuaLibrary(LuaLibraries luaLibsImpl, ApiContainer apiContainer, Lua lua, Action<string> logOutputCallback)
-			: base(luaLibsImpl, apiContainer, lua, logOutputCallback) {}
+		public FormsLuaLibrary(LuaLibraries luaLibsImpl, ApiContainer apiContainer, Action<string> logOutputCallback)
+			: base(luaLibsImpl, apiContainer, logOutputCallback) {}
 
 		public override string Name => "forms";
 
@@ -192,7 +192,7 @@ namespace BizHawk.Client.EmuHawk
 				return 0;
 			}
 
-			var dropdownItems = items.Values.Cast<string>().ToList();
+			var dropdownItems = _th.EnumerateValues<string>(items).ToList();
 			dropdownItems.Sort();
 
 			var dropdown = new LuaDropDown(dropdownItems);
@@ -1246,7 +1246,7 @@ namespace BizHawk.Client.EmuHawk
 						{
 							if (control is LuaDropDown)
 							{
-								var dropdownItems = items.Values.Cast<string>().ToList();
+								var dropdownItems = _th.EnumerateValues<string>(items).ToList();
 								dropdownItems.Sort();
 								(control as LuaDropDown).SetItems(dropdownItems);
 							}

src/BizHawk.Client.EmuHawk/tools/Lua/Win32LuaLibraries.cs
@@ -41,6 +41,7 @@ namespace BizHawk.Client.EmuHawk
 			}
 
 			if (true /*NLua.Lua.WhichLua == "NLua"*/) _lua["keepalives"] = _lua.NewTable();
+			_th = new NLuaTableHelper(_lua);
 			_mainForm = mainForm;
 			LuaWait = new AutoResetEvent(false);
 			Docs.Clear();
@@ -59,7 +60,7 @@ namespace BizHawk.Client.EmuHawk
 
 				if (addLibrary)
 				{
-					var instance = (LuaLibraryBase) Activator.CreateInstance(lib, this, apiContainer, _lua, (Action<string>) LogToLuaConsole);
+					var instance = (LuaLibraryBase) Activator.CreateInstance(lib, this, apiContainer, (Action<string>) LogToLuaConsole);
 					ServiceInjector.UpdateServices(serviceProvider, instance);
 
 					// TODO: make EmuHawk libraries have a base class with common properties such as this
@@ -79,7 +80,7 @@ namespace BizHawk.Client.EmuHawk
 						{
 							var canvas = new LuaCanvas(width, height, x, y, LogToLuaConsole);
 							canvas.Show();
-							return _lua.TableFromObject(canvas);
+							return _th.ObjectToTable(canvas);
 						};
 					}
 					else if (instance is TAStudioLuaLibrary tastudioLib)
@@ -105,6 +106,8 @@ namespace BizHawk.Client.EmuHawk
 		private Lua _lua = new Lua();
 		private Lua _currThread;
 
+		private readonly NLuaTableHelper _th;
+
 		private static Action<object[]> _logToLuaConsoleCallback = a => Console.WriteLine("a Lua lib is logging during init and the console lib hasn't been initialised yet");
 
 		private FormsLuaLibrary FormsLibrary => (FormsLuaLibrary)Libraries[typeof(FormsLuaLibrary)];
@@ -116,6 +119,8 @@ namespace BizHawk.Client.EmuHawk
 		public override GuiLuaLibrary GuiLibrary => (GuiLuaLibrary) Libraries[typeof(GuiLuaLibrary)];
 
 		private static void LogToLuaConsole(object outputs) => _logToLuaConsoleCallback(new[] { outputs });
+
+		public override NLuaTableHelper GetTableHelper() => _th;
 
 		public override void Restart(IEmulatorServiceProvider newServiceProvider)
 		{

src/BizHawk.Client.Common/lua/CommonLibs/ClientLuaLibrary.cs
@@ -26,8 +26,8 @@ namespace BizHawk.Client.Common
 
 		public IMainFormForApi MainForm { get; set; }
 
-		public ClientLuaLibrary(LuaLibraries luaLibsImpl, ApiContainer apiContainer, Lua lua, Action<string> logOutputCallback)
-			: base(luaLibsImpl, apiContainer, lua, logOutputCallback) {}
+		public ClientLuaLibrary(LuaLibraries luaLibsImpl, ApiContainer apiContainer, Action<string> logOutputCallback)
+			: base(luaLibsImpl, apiContainer, logOutputCallback) {}
 
 		public override string Name => "client";
 
@@ -231,7 +231,7 @@ namespace BizHawk.Client.Common
 		[LuaMethod("transformPoint", "Transforms a point (x, y) in emulator space to a point in client space")]
 		public LuaTable TransformPoint(int x, int y) {
 			var transformed = APIs.EmuClient.TransformPoint(new Point(x, y));
-			var table = Lua.NewTable();
+			var table = _th.CreateTable();
 			table["x"] = transformed.X;
 			table["y"] = transformed.Y;
 			return table;
@@ -262,14 +262,14 @@ namespace BizHawk.Client.Common
 
 		[LuaMethodExample("local nlcliget = client.getavailabletools( );")]
 		[LuaMethod("getavailabletools", "Returns a list of the tools currently open")]
-		public LuaTable GetAvailableTools() => APIs.Tool.AvailableTools.Select(tool => tool.Name.ToLower()).EnumerateToLuaTable(Lua);
+		public LuaTable GetAvailableTools() => _th.EnumerateToLuaTable(APIs.Tool.AvailableTools.Select(tool => tool.Name.ToLower()));
 
 		[LuaMethodExample("local nlcliget = client.gettool( \"Tool name\" );")]
 		[LuaMethod("gettool", "Returns an object that represents a tool of the given name (not case sensitive). If the tool is not open, it will be loaded if available. Use gettools to get a list of names")]
 		public LuaTable GetTool(string name)
 		{
 			var selectedTool = APIs.Tool.GetTool(name);
-			return selectedTool == null ? null : Lua.TableFromObject(selectedTool);
+			return selectedTool == null ? null : _th.ObjectToTable(selectedTool);
 		}
 
 		[LuaMethodExample("local nlclicre = client.createinstance( \"objectname\" );")]
@@ -277,7 +277,7 @@ namespace BizHawk.Client.Common
 		public LuaTable CreateInstance(string name)
 		{
 			var instance = APIs.Tool.GetTool(name);
-			return instance == null ? null : Lua.TableFromObject(instance);
+			return instance == null ? null : _th.ObjectToTable(instance);
 		}
 
 		[LuaMethodExample("client.displaymessages( true );")]

src/BizHawk.Client.Common/lua/CommonLibs/CommLuaLibrary.cs
@@ -4,8 +4,6 @@ using System.ComponentModel;
 using System.Net.WebSockets;
 using System.Text;
 
-using NLua;
-
 namespace BizHawk.Client.Common
 {
 	[Description("A library for communicating with other programs")]
@@ -13,8 +11,8 @@ namespace BizHawk.Client.Common
 	{
 		private readonly IDictionary<Guid, ClientWebSocketWrapper> _websockets = new Dictionary<Guid, ClientWebSocketWrapper>();
 
-		public CommLuaLibrary(LuaLibraries luaLibsImpl, ApiContainer apiContainer, Lua lua, Action<string> logOutputCallback)
-			: base(luaLibsImpl, apiContainer, lua, logOutputCallback) {}
+		public CommLuaLibrary(LuaLibraries luaLibsImpl, ApiContainer apiContainer, Action<string> logOutputCallback)
+			: base(luaLibsImpl, apiContainer, logOutputCallback) {}
 
 		public override string Name => "comm";
 

src/BizHawk.Client.Common/lua/CommonLibs/EmulationLuaLibrary.cs
@@ -13,8 +13,8 @@ namespace BizHawk.Client.Common
 		public Action FrameAdvanceCallback { get; set; }
 		public Action YieldCallback { get; set; }
 
-		public EmulationLuaLibrary(LuaLibraries luaLibsImpl, ApiContainer apiContainer, Lua lua, Action<string> logOutputCallback)
-			: base(luaLibsImpl, apiContainer, lua, logOutputCallback) {}
+		public EmulationLuaLibrary(LuaLibraries luaLibsImpl, ApiContainer apiContainer, Action<string> logOutputCallback)
+			: base(luaLibsImpl, apiContainer, logOutputCallback) {}
 
 		public override string Name => "emu";
 
@@ -44,12 +44,7 @@ namespace BizHawk.Client.Common
 
 		[LuaMethodExample("local nlemuget = emu.getregisters( );")]
 		[LuaMethod("getregisters", "returns the complete set of available flags and registers for a given core")]
-		public LuaTable GetRegisters()
-		{
-			return APIs.Emulation
-				.GetRegisters()
-				.ToLuaTable(Lua);
-		}
+		public LuaTable GetRegisters() => _th.DictToTable(APIs.Emulation.GetRegisters());
 
 		[LuaMethodExample("emu.setregister( emu.getregisters( )[ 0 ], -1000 );")]
 		[LuaMethod("setregister", "sets the given register name to the given value")]

src/BizHawk.Client.Common/lua/CommonLibs/GameInfoLuaLibrary.cs
@@ -7,8 +7,8 @@ namespace BizHawk.Client.Common
 {
 	public sealed class GameInfoLuaLibrary : LuaLibraryBase
 	{
-		public GameInfoLuaLibrary(LuaLibraries luaLibsImpl, ApiContainer apiContainer, Lua lua, Action<string> logOutputCallback)
-			: base(luaLibsImpl, apiContainer, lua, logOutputCallback) {}
+		public GameInfoLuaLibrary(LuaLibraries luaLibsImpl, ApiContainer apiContainer, Action<string> logOutputCallback)
+			: base(luaLibsImpl, apiContainer, logOutputCallback) {}
 
 		public override string Name => "gameinfo";
 
@@ -38,11 +38,6 @@ namespace BizHawk.Client.Common
 
 		[LuaMethodExample("local nlgamget = gameinfo.getoptions( );")]
 		[LuaMethod("getoptions", "returns the game options for the currently loaded rom. Options vary per platform")]
-		public LuaTable GetOptions()
-		{
-			return APIs.GameInfo
-				.GetOptions()
-				.ToLuaTable(Lua);
-		}
+		public LuaTable GetOptions() => _th.DictToTable(APIs.GameInfo.GetOptions());
 	}
 }

src/BizHawk.Client.Common/lua/CommonLibs/InputLuaLibrary.cs
@@ -6,17 +6,17 @@ namespace BizHawk.Client.Common
 {
 	public sealed class InputLuaLibrary : LuaLibraryBase
 	{
-		public InputLuaLibrary(LuaLibraries luaLibsImpl, ApiContainer apiContainer, Lua lua, Action<string> logOutputCallback)
-			: base(luaLibsImpl, apiContainer, lua, logOutputCallback) {}
+		public InputLuaLibrary(LuaLibraries luaLibsImpl, ApiContainer apiContainer, Action<string> logOutputCallback)
+			: base(luaLibsImpl, apiContainer, logOutputCallback) {}
 
 		public override string Name => "input";
 
 		[LuaMethodExample("local nlinpget = input.get( );")]
 		[LuaMethod("get", "Returns a lua table of all the buttons the user is currently pressing on their keyboard and gamepads\nAll buttons that are pressed have their key values set to true; all others remain nil.")]
-		public LuaTable Get() => APIs.Input.Get().ToLuaTable(Lua);
+		public LuaTable Get() => _th.DictToTable(APIs.Input.Get());
 
 		[LuaMethodExample("local nlinpget = input.getmouse( );")]
 		[LuaMethod("getmouse", "Returns a lua table of the mouse X/Y coordinates and button states. Table keys are X, Y, Left, Middle, Right, XButton1, XButton2, Wheel.")]
-		public LuaTable GetMouse() => APIs.Input.GetMouse().ToLuaTable(Lua);
+		public LuaTable GetMouse() => _th.DictToTable(APIs.Input.GetMouse());
 	}
 }

commit b541fe5b4091bfef20fecfbccc083eebbdd0b0ce

Use NLuaTableHelper for more conversion to/from LuaTable
also fix indexing that I broke in c8e10120d

src/BizHawk.Client.Common/lua/CommonLibs/GuiLuaLibrary.cs
@@ -63,9 +63,10 @@ namespace BizHawk.Client.Common
 			{
 				var pointsArr = new Point[4];
 				var i = 0;
-				foreach (var point in _th.EnumerateValues<LuaTable>(points))
+				foreach (var point in _th.EnumerateValues<LuaTable>(points)
+					.Select(table => _th.EnumerateValues<double>(table).ToList()))
 				{
-					pointsArr[i] = new Point(LuaInt(point[1]), LuaInt(point[2]));
+					pointsArr[i] = new Point(LuaInt(point[0]), LuaInt(point[1]));
 					i++;
 					if (i >= 4)
 					{
@@ -124,14 +125,15 @@ namespace BizHawk.Client.Common
 		[LuaMethod("drawPolygon", "Draws a polygon using the table of coordinates specified in points. This should be a table of tables(each of size 2). If x or y is passed, the polygon will be translated by the passed coordinate pair. Line is the color of the polygon. Background is the optional fill color")]
 		public void DrawPolygon(LuaTable points, int? offsetX = null, int? offsetY = null, Color? line = null, Color? background = null)
 		{
-			var pointsList = _th.EnumerateValues<LuaTable>(points).ToList();
+			var pointsList = _th.EnumerateValues<LuaTable>(points)
+				.Select(table => _th.EnumerateValues<double>(table).ToList()).ToList();
 			try
 			{
 				var pointsArr = new Point[pointsList.Count];
 				var i = 0;
 				foreach (var point in pointsList)
 				{
-					pointsArr[i] = new Point(LuaInt(point[1]) + (offsetX ?? 0), LuaInt(point[2]) + (offsetY ?? 0));
+					pointsArr[i] = new Point(LuaInt(point[0]) + (offsetX ?? 0), LuaInt(point[1]) + (offsetY ?? 0));
 					i++;
 				}
 				APIs.Gui.DrawPolygon(pointsArr, line, background);

src/BizHawk.Client.EmuHawk/tools/Lua/Libraries/FormsLuaLibrary.cs
@@ -422,7 +422,7 @@ namespace BizHawk.Client.EmuHawk
 				return 0;
 			}
 
-			var pictureBox = new LuaPictureBox();
+			var pictureBox = new LuaPictureBox { TableHelper = _th };
 			form.Controls.Add(pictureBox);
 
 			if (x.HasValue && y.HasValue)

src/BizHawk.Client.EmuHawk/tools/Lua/LuaCanvas.cs
@@ -15,10 +15,11 @@ namespace BizHawk.Client.EmuHawk
 	{
 		private readonly Action<string> LogOutputCallback;
 
-		public LuaCanvas(int width, int height, int? x, int? y, Action<string> logOutputCallback)
+		public LuaCanvas(int width, int height, int? x, int? y, NLuaTableHelper tableHelper, Action<string> logOutputCallback)
 		{
 			LogOutputCallback = logOutputCallback;
 			InitializeComponent();
+			luaPictureBox.TableHelper = tableHelper;
 			luaPictureBox.Image = Properties.Resources.LuaPictureBox;
 			luaPictureBox.Width = width;
 			luaPictureBox.Height = height;

src/BizHawk.Client.EmuHawk/tools/Lua/LuaPictureBox.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
+
+using BizHawk.Client.Common;
 
 using NLua;
 
@@ -13,6 +16,8 @@ namespace BizHawk.Client.EmuHawk
 
 		private readonly Dictionary<Color, SolidBrush> _solidBrushes = new Dictionary<Color, SolidBrush>();
 		private readonly Dictionary<Color, Pen> _pens = new Dictionary<Color, Pen>();
+
+		internal NLuaTableHelper TableHelper { private get; set; }
 
 		private SolidBrush GetBrush(Color color)
 		{
@@ -78,9 +83,10 @@ namespace BizHawk.Client.EmuHawk
 			var pointsArr = new Point[4];
 
 			var i = 0;
-			foreach (LuaTable point in points.Values)
+			foreach (var point in TableHelper.EnumerateValues<LuaTable>(points)
+				.Select(table => TableHelper.EnumerateValues<double>(table).ToList()))
 			{
-				pointsArr[i] = new Point((int)(double)(point[1]), (int)(double)(point[2]));
+				pointsArr[i] = new Point((int) point[0], (int) point[1]);
 				i++;
 				if (i >= 4)
 				{
@@ -252,11 +258,13 @@ namespace BizHawk.Client.EmuHawk
 
 		public void DrawPolygon(LuaTable points, int? x = null, int? y = null, Color? line = null, Color? background = null)
 		{
-			var pointsArr = new Point[points.Values.Count];
+			var pointsList = TableHelper.EnumerateValues<LuaTable>(points)
+				.Select(table => TableHelper.EnumerateValues<double>(table).ToList()).ToList();
+			var pointsArr = new Point[pointsList.Count];
 			var i = 0;
-			foreach (LuaTable point in points.Values)
+			foreach (var point in pointsList)
 			{
-				pointsArr[i] = new Point((int)(double)(point[1]) + x ?? 0, (int)(double)(point[2]) + y ?? 0);
+				pointsArr[i] = new Point((int) point[0] + x ?? 0, (int) point[1] + y ?? 0);
 				i++;
 			}
 

src/BizHawk.Client.EmuHawk/tools/Lua/Win32LuaLibraries.cs
@@ -78,7 +78,7 @@ namespace BizHawk.Client.EmuHawk
 					{
 						guiLib.CreateLuaCanvasCallback = (width, height, x, y) =>
 						{
-							var canvas = new LuaCanvas(width, height, x, y, LogToLuaConsole);
+							var canvas = new LuaCanvas(width, height, x, y, _th, LogToLuaConsole);
 							canvas.Show();
 							return _th.ObjectToTable(canvas);
 						};