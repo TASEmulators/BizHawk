05676234ed06ab613c9e3199d5b0739448c0d6b1
YoshiRulz <OSSYoshiRulz@gmail.com>
2021-02-12 14:06:06 +1000

    Move remaining assets from /output to /Assets

--- Dist/CleanupBuildOutputDirs.sh
+++ Dist/CleanupBuildOutputDirs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cd "$(dirname "$0")/.." && (rm -r src/*/bin src/*/obj test_output output; git restore output)
+cd "$(dirname "$0")/.." && (rm -r src/*/bin src/*/obj test_output output; mkdir output)
