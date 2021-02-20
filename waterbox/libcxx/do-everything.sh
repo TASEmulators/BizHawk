c75ea8efd1510d1e616747c2593f0ed00e617f79
YoshiRulz <OSSYoshiRulz@gmail.com>
2021-02-21 00:17:53 +1000

    Cleanup libcxx scripts

--- waterbox/libcxx/do-everything.sh
+++ waterbox/libcxx/do-everything.sh
@@ -1,25 +1,10 @@
 #!/bin/sh
-
-./configure-for-waterbox-phase--
-cd build-
-make -j
-make install
-cd ..
-
-./configure-for-waterbox-phase-0
-cd build0
-make -j
-make install
-cd ..
-
-./configure-for-waterbox-phase-1
-cd build1
-make -j
-make install
-cd ..
-
-./configure-for-waterbox-phase-2
-cd build2
-make -j
-make install
-cd ..
+set -e
+./configure-for-waterbox-phase-- && cd build- && make -j && make install && cd ..
+printf "completed phase -1\n"
+./configure-for-waterbox-phase-0 && cd build0 && make -j && make install && cd ..
+printf "completed phase 0\n"
+./configure-for-waterbox-phase-1 && cd build1 && make -j && make install && cd ..
+printf "completed phase 1\n"
+./configure-for-waterbox-phase-2 && cd build2 && make -j && make install && cd ..
+printf "completed phase 2\n"
