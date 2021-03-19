f0b9e2e828d6dab1c46fa0847b4db4858c6cc8a6
YoshiRulz <OSSYoshiRulz@gmail.com>
2021-03-19 10:16:53 +1000

    Make PathEntry sealed, add constructor and use in PathEntryCollection

--- src/BizHawk.Client.Common/config/PathEntry.cs
+++ src/BizHawk.Client.Common/config/PathEntry.cs
@@ -3,7 +3,7 @@ using Newtonsoft.Json;
 
 namespace BizHawk.Client.Common
 {
-	public class PathEntry
+	public sealed class PathEntry
 	{
 		public string Type { get; set; }
 		[JsonIgnore]
@@ -15,6 +15,14 @@ namespace BizHawk.Client.Common
 		}
 		public string System { get; set; }
 		public int Ordinal { get; set; }
+
+		public PathEntry(string system, int ordinal, string type, string path)
+		{
+			Ordinal = ordinal;
+			Path = path;
+			System = system;
+			Type = type;
+		}
 
 		internal bool IsSystem(string systemID)
 		{
