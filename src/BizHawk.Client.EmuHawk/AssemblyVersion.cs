ee30fc49bdbb912d36fafb9d9d0493b28b273643
Morilli <35152647+Morilli@users.noreply.github.com>
2021-04-03 16:34:04 +0200

    Revert my previous commit and add version info in dotnet invocation instead, fix batch files

--- src/BizHawk.Client.EmuHawk/AssemblyVersion.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-
-using BizHawk.Common;
-
-[assembly: AssemblyVersion(VersionInfo.MainVersion)]
-[assembly: AssemblyFileVersion(VersionInfo.MainVersion + "." + VersionInfo.SVN_REV)]
-[assembly: AssemblyInformationalVersion(VersionInfo.MainVersion + "." + VersionInfo.SVN_REV + "#" + VersionInfo.GIT_SHORTHASH)]
