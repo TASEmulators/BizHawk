d32f7344ae52248defb3c94a5524c183047a35ee
YoshiRulz <OSSYoshiRulz@gmail.com>
2021-04-08 09:24:12 +1000

    Fix RecordMovie having a broken layout on Linux

--- src/BizHawk.Client.EmuHawk/movie/RecordMovie.cs
+++ src/BizHawk.Client.EmuHawk/movie/RecordMovie.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
 using System.Linq;
 
 using BizHawk.Emulation.Common;
 using BizHawk.Client.Common;
+using BizHawk.Common;
 
 namespace BizHawk.Client.EmuHawk
 {
@@ -37,6 +39,14 @@ namespace BizHawk.Client.EmuHawk
 			InitializeComponent();
 			Icon = Properties.Resources.TAStudioIcon;
 			BrowseBtn.Image = Properties.Resources.OpenFile;
+			if (OSTailoredCode.IsUnixHost) Load += (_, _) =>
+			{
+				//HACK to make this usable on Linux. No clue why this Form in particular is so much worse, maybe the GroupBox? --yoshi
+				DefaultAuthorCheckBox.Location += new Size(0, 20);
+				var s = new Size(0, 36);
+				OK.Location += s;
+				Cancel.Location += s;
+			};
 
 			if (!_emulator.HasSavestates())
 			{
