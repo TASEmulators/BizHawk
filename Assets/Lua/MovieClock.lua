698ecb9735c42c404c10e3134cfdd7fe2c2a2457
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2021-04-08 01:05:05 -0700

    use cycle based time for Gambatte/SubGBHawk

--- Assets/Lua/MovieClock.lua
+++ Assets/Lua/MovieClock.lua
@@ -1,12 +1,20 @@
 while true do
 	if (movie.isloaded()) then
-		fps = movie.getfps();
-		frames = emu.framecount();
-		tseconds = (frames / fps)
+		if movie.getheader()["Core"] == "Gambatte" then
+			fps = 2 ^ 21;
+			frames = emu.totalexecutedcycles();
+		elseif movie.getheader()["Core"] == "SubGBHawk" then
+			fps = 2 ^ 22;
+			frames = emu.totalexecutedcycles();
+		else
+			fps = movie.getfps();
+			frames = emu.framecount();
+		end
+		tseconds = (frames / fps);
 		secondsraw = tseconds % 60;
 		shift = 10 ^ 2;
 		seconds = math.floor((secondsraw * shift) + 0.5) / shift;
-		secondsstr = string.format("%.2f", seconds)
+		secondsstr = string.format("%.2f", seconds);
 		if (seconds < 10) then
 			secondsstr = "0" .. secondsstr;
 		end
