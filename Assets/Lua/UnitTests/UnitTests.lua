d8d42b9f81fa3f84467276a4c6122efd6dc72a95
nattthebear <goyuken@gmail.com>
2021-04-27 19:37:53 -0400

    The next time I look at this I want to see waterbox builds actually fucking working again.  This means copying to dll as they are now, and not silently being overridden by stale files stored in another folder.  This is not my problem to fix because I did not fucking break it.

--- Assets/Lua/UnitTests/UnitTests.lua
+++
@@ -1,5 +0,0 @@
-0 .\Lua\UnitTests\Joypad_RoundTrip.lua
-0 .\Lua\UnitTests\Joypad_Set.lua
-0 .\Lua\UnitTests\Joypad_WithControllerNumbers.lua
-0 .\Lua\UnitTests\GameInfo.lua
-0 .\Lua\UnitTests\Console.lua
