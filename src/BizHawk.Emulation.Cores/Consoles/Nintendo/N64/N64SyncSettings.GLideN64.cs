8b79720b9df9be545886d6f3efc1aa49f992747d
Zach <yamkoo@gmail.com>
2021-05-02 07:39:13 -0700

    N64 plugin categories (#2729)
    * GLideN64 categories
    
    * Remove maximize button from n64 plugins window.
    
    Maximize breaks the anchors for the property grids, and the window is fixed size anyway.
    
    * GlideMk2 categories
    
    * Glide64 categories
    
    * Rice categories
    
    Also fixed typo "WinFrame" -> "Wireframe"

--- src/BizHawk.Emulation.Cores/Consoles/Nintendo/N64/N64SyncSettings.GLideN64.cs
+++ src/BizHawk.Emulation.Cores/Consoles/Nintendo/N64/N64SyncSettings.GLideN64.cs
@@ -70,11 +70,16 @@ namespace BizHawk.Emulation.Cores.Nintendo.N64
 				OverscanPalRight = 0;
 			}
 
+			[DefaultValue(true)]
+			[DisplayName("Use Default Hacks")]
+			[Description("Use automatically detected framebuffer settings for games from .ini file.")]
+			[Category("Framebuffer")]
 			public bool UseDefaultHacks { get; set; }
 
 			[DefaultValue(0)]
 			[DisplayName("Multi-sampling")]
 			[Description("Enable/Disable MultiSampling (0=off, 2,4,8,16=quality)")]
+			[Category("Video")]
 			public int MultiSampling { get; set; }
 
 			public enum AspectRatioMode
@@ -95,6 +100,7 @@ namespace BizHawk.Emulation.Cores.Nintendo.N64
 			[DefaultValue(AspectRatioMode.FourThree)]
 			[DisplayName("Aspect Ratio")]
 			[Description("Screen aspect ratio (0=stretch, 1=force 4:3, 2=force 16:9, 3=adjust)")]
+			[Category("Video")]
 			public AspectRatioMode AspectRatio { get; set; }
 
 			public enum SwapMode
@@ -112,11 +118,13 @@ namespace BizHawk.Emulation.Cores.Nintendo.N64
 			[DefaultValue(SwapMode.OnVIUpdateCall)]
 			[DisplayName("Buffer swap mode")]
 			[Description("Swap frame buffers (0=On VI update call, 1=On VI origin change, 2=On buffer update)")]
+			[Category("Framebuffer")]
 			public SwapMode BufferSwapMode { get; set; }
 
 			[DefaultValue(0)]
 			[DisplayName("Use native resolution factor")]
 			[Description("Frame buffer size is the factor of N64 native resolution.")]
+			[Category("Emulation")]
 			public int UseNativeResolutionFactor { get; set; }
 
 			public enum bilinearFilteringMode
@@ -131,56 +139,67 @@ namespace BizHawk.Emulation.Cores.Nintendo.N64
 			[DefaultValue(bilinearFilteringMode.Standard)]
 			[DisplayName("Bilinear filtering mode")]
 			[Description("Bilinear filtering mode (0=N64 3point, 1=standard)")]
+			[Category("Video")]
 			public bilinearFilteringMode bilinearMode { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Enable Halos Removal")]
 			[Description("Remove halos around filtered textures")]
+			[Category("Video")]
 			public bool enableHalosRemoval { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Max level of Anisotropic Filtering")]
 			[Description("Max level of Anisotropic Filtering, 0 for off")]
+			[Category("Video")]
 			public bool MaxAnisotropy { get; set; }
 
 			[DefaultValue(500)]
 			[DisplayName("Cache Size")]
 			[Description("Size of texture cache in megabytes. Good value is VRAM*3/4")]
+			[Category("Texture Enhancement")]
 			public int CacheSize { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Show Internal Resolution")]
 			[Description("Show internal resolution.")]
+			[Category("OSD")]
 			public bool ShowInternalResolution { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Show Rendering Resolution")]
 			[Description("Show rendering resolution.")]
+			[Category("OSD")]
 			public bool ShowRenderingResolution { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("FXAA")]
 			[Description("Enable Fast Approximate Anti-Aliasing.")]
+			[Category("Video")]
 			public bool FXAA { get; set; }
 
 			[DefaultValue(true)]
 			[DisplayName("Color noise emulation")]
 			[Description("Enable color noise emulation.")]
+			[Category("Emulation")]
 			public bool EnableNoise { get; set; }
 
 			[DefaultValue(true)]
 			[DisplayName("LOD emulation")]
 			[Description("Enable LOD emulation.")]
+			[Category("Emulation")]
 			public bool EnableLOD { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("HW Lighting")]
 			[Description("Enable hardware per-pixel lighting.")]
+			[Category("Emulation")]
 			public bool EnableHWLighting { get; set; }
 
 			[DefaultValue(true)]
 			[DisplayName("Shaders storage")]
 			[Description("Use persistent storage for compiled shaders.")]
+			[Category("Emulation")]
 			public bool EnableShadersStorage { get; set; }
 
 			public enum TexrectCoordsMode
@@ -198,96 +217,115 @@ namespace BizHawk.Emulation.Cores.Nintendo.N64
 			[DefaultValue(TexrectCoordsMode.Off)]
 			[DisplayName("Correct Texrect Coords")]
 			[Description("Make texrect coordinates continuous to avoid black lines between them. (0=Off, 1=Auto, 2=Force)")]
+			[Category("Emulation")]
 			public TexrectCoordsMode CorrectTexrectCoords { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Native Res Texrects")]
 			[Description("Render 2D texrects in native resolution to fix misalignment between parts of 2D image.")]
+			[Category("Emulation")]
 			public bool EnableNativeResTexrects { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Legacy Blending")]
 			[Description("Do not use shaders to emulate N64 blending modes. Works faster on slow GPU. Can cause glitches.")]
+			[Category("Emulation")]
 			public bool EnableLegacyBlending { get; set; }
 
 			[DefaultValue(true)]
 			[DisplayName("Fragment Depth Write")]
 			[Description("Enable writing of fragment depth. Some mobile GPUs do not support it, thus it made optional. Leave enabled.")]
+			[Category("Framebuffer")]
 			public bool EnableFragmentDepthWrite { get; set; }
 
 			[DefaultValue(true)]
 			[DisplayName("FB Emulation")]
 			[Description("Enable frame and|or depth buffer emulation.")]
+			[Category("Framebuffer")]
 			public bool EnableFBEmulation { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Copy auxiliary to RDRAM")]
 			[Description("Copy auxiliary buffers to RDRAM")]
+			[Category("Framebuffer")]
 			public bool EnableCopyAuxiliaryToRDRAM { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("N64 Depth Compare")]
 			[Description("Enable N64 depth compare instead of OpenGL standard one. Experimental.")]
+			[Category("Framebuffer")]
 			public bool EnableN64DepthCompare { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Enable Overscan")]
 			[Description("Enable resulted image crop by Overscan.")]
+			[Category("Video")]
 			public bool EnableOverscan { get; set; }
 
 			[DefaultValue(0)]
 			[DisplayName("Overscan NTSC Top")]
 			[Description("NTSC mode. Top bound of Overscan.")]
+			[Category("Video")]
 			public int OverscanNtscTop { get; set; }
 
 			[DefaultValue(0)]
 			[DisplayName("Overscan NTSC Bottom")]
 			[Description("NTSC mode. Bottom bound of Overscan.")]
+			[Category("Video")]
 			public int OverscanNtscBottom { get; set; }
 
 			[DefaultValue(0)]
 			[DisplayName("Overscan NTSC Left")]
 			[Description("NTSC mode. Left bound of Overscan.")]
+			[Category("Video")]
 			public int OverscanNtscLeft { get; set; }
 
 			[DefaultValue(0)]
 			[DisplayName("Overscan NTSC Right")]
 			[Description("NTSC mode. Right bound of Overscan.")]
+			[Category("Video")]
 			public int OverscanNtscRight { get; set; }
 
 			[DefaultValue(0)]
 			[DisplayName("Overscan PAL Top")]
 			[Description("PAL mode. Top bound of Overscan.")]
+			[Category("Video")]
 			public int OverscanPalTop { get; set; }
 
 			[DefaultValue(0)]
 			[DisplayName("Overscan PAL Bottom")]
 			[Description("PAL mode. Bottom bound of Overscan.")]
+			[Category("Video")]
 			public int OverscanPalBottom { get; set; }
 
 			[DefaultValue(0)]
 			[DisplayName("Overscan PAL Left")]
 			[Description("PAL mode. Left bound of Overscan.")]
+			[Category("Video")]
 			public int OverscanPalLeft { get; set; }
 
 			[DefaultValue(0)]
 			[DisplayName("Overscan PAL Right")]
 			[Description("PAL mode. Right bound of Overscan.")]
+			[Category("Video")]
 			public int OverscanPalRight { get; set; }
 
 			[DefaultValue(true)]
 			[DisplayName("FB Info")]
 			[Description("Disable buffers read/write with FBInfo. Use for games, which do not work with FBInfo.")]
+			[Category("Framebuffer")]
 			public bool DisableFBInfo { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("FB Info Read Color Chunk")]
 			[Description("Read color buffer by 4kb chunks (strict follow to FBRead specification)")]
+			[Category("Framebuffer")]
 			public bool FBInfoReadColorChunk { get; set; }
 
 			[DefaultValue(true)]
 			[DisplayName("FB Info Read Depth Chunk")]
 			[Description("Read depth buffer by 4kb chunks (strict follow to FBRead specification)")]
+			[Category("Framebuffer")]
 			public bool FBInfoReadDepthChunk { get; set; }
 
 			public enum CopyColorToRDRAMMode
@@ -305,6 +343,7 @@ namespace BizHawk.Emulation.Cores.Nintendo.N64
 			[DefaultValue(CopyColorToRDRAMMode.AsyncMode)]
 			[DisplayName("Copy Color To RDRAM")]
 			[Description("Enable color buffer copy to RDRAM (0=do not copy, 1=copy in sync mode, 2=copy in async mode)")]
+			[Category("Framebuffer")]
 			public CopyColorToRDRAMMode EnableCopyColorToRDRAM { get; set; }
 
 			public enum CopyDepthToRDRAMMode
@@ -322,11 +361,13 @@ namespace BizHawk.Emulation.Cores.Nintendo.N64
 			[DefaultValue(CopyDepthToRDRAMMode.DoNotCopy)]
 			[DisplayName("Copy Depth To RDRAM")]
 			[Description("Enable depth buffer copy to RDRAM  (0=do not copy, 1=copy from video memory, 2=use software render)")]
+			[Category("Framebuffer")]
 			public CopyDepthToRDRAMMode EnableCopyDepthToRDRAM { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Copy Color From RDRAM")]
 			[Description("Enable color buffer copy from RDRAM.")]
+			[Category("Framebuffer")]
 			public bool EnableCopyColorFromRDRAM { get; set; }
 
 			public enum TextureFilterMode
@@ -356,6 +397,7 @@ namespace BizHawk.Emulation.Cores.Nintendo.N64
 			[DefaultValue(TextureFilterMode.None)]
 			[DisplayName("Texture filter")]
 			[Description("Texture filter (0=none, 1=Smooth filtering 1, 2=Smooth filtering 2, 3=Smooth filtering 3, 4=Smooth filtering 4, 5=Sharp filtering 1, 6=Sharp filtering 2)")]
+			[Category("Texture Enhancement")]
 			public TextureFilterMode txFilterMode { get; set; }
 
 			public enum TextureEnhancementMode
@@ -406,71 +448,85 @@ namespace BizHawk.Emulation.Cores.Nintendo.N64
 			[DefaultValue(TextureEnhancementMode.None)]
 			[DisplayName("Texture Enhancement Mode")]
 			[Description("Texture Enhancement (0=none, 1=store as is, 2=X2, 3=X2SAI, 4=HQ2X, 5=HQ2XS, 6=LQ2X, 7=LQ2XS, 8=HQ4X, 9=2xBRZ, 10=3xBRZ, 11=4xBRZ, 12=5xBRZ), 13=6xBRZ)")]
+			[Category("Texture Enhancement")]
 			public TextureEnhancementMode txEnhancementMode { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Texture Deposterize")]
 			[Description("Deposterize texture before enhancement.")]
+			[Category("Texture Enhancement")]
 			public bool txDeposterize { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Texture Filter Ignore BG")]
 			[Description("Don't filter background textures.")]
+			[Category("Texture Enhancement")]
 			public bool txFilterIgnoreBG { get; set; }
 
 			[DefaultValue(100)]
 			[DisplayName("Texture Cache Size")]
 			[Description("Size of filtered textures cache in megabytes.")]
+			[Category("Texture Enhancement")]
 			public int txCacheSize { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Texture Hires Enable")]
 			[Description("Use high-resolution texture packs if available.")]
+			[Category("Texture Enhancement")]
 			public bool txHiresEnable { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Texture Hires Full Alpha Channel")]
 			[Description("Allow to use alpha channel of high-res texture fully.")]
+			[Category("Texture Enhancement")]
 			public bool txHiresFullAlphaChannel { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Texture Hres Alt CRC")]
 			[Description("Use alternative method of paletted textures CRC calculation.")]
+			[Category("Texture Enhancement")]
 			public bool txHresAltCRC { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Texture Dump")]
 			[Description("Enable dump of loaded N64 textures.")]
+			[Category("Texture Enhancement")]
 			public bool txDump { get; set; }
 
 			[DefaultValue(true)]
 			[DisplayName("Texture Cache Compression")]
 			[Description("Zip textures cache.")]
+			[Category("Texture Enhancement")]
 			public bool txCacheCompression { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Texture Force 16bpp")]
 			[Description("Force use 16bit texture formats for HD textures.")]
+			[Category("Texture Enhancement")]
 			public bool txForce16bpp { get; set; }
 
 			[DefaultValue(true)]
 			[DisplayName("Texture Save Cache")]
 			[Description("Save texture cache to hard disk.")]
+			[Category("Texture Enhancement")]
 			public bool txSaveCache { get; set; }
 
 			[DefaultValue("")]
 			[DisplayName("Texture Path")]
 			[Description("Path to folder with hi-res texture packs.")]
+			[Category("Texture Enhancement")]
 			public string txPath { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Enable Bloom")]
 			[Description("Enable bloom filter")]
+			[Category("Emulation")]
 			public bool EnableBloom { get; set; }
 
 			[DefaultValue(4)]
 			[DisplayName("Bloom Threshold Level")]
 			[Description("Brightness threshold level for bloom. Values [2, 6]")]
+			[Category("Emulation")]
 			public int bloomThresholdLevel { get; set; }
 
 			public enum BlendMode
@@ -488,26 +544,31 @@ namespace BizHawk.Emulation.Cores.Nintendo.N64
 			[DefaultValue(BlendMode.Strong)]
 			[DisplayName("Bloom Blend Mode")]
 			[Description("Bloom blend mode (0=Strong, 1=Mild, 2=Light)")]
+			[Category("Emulation")]
 			public BlendMode bloomBlendMode { get; set; }
 
 			[DefaultValue(10)]
 			[DisplayName("Blur Amount")]
 			[Description("Blur radius. Values [2, 10]")]
+			[Category("Emulation")]
 			public int blurAmount { get; set; }
 
 			[DefaultValue(20)]
 			[DisplayName("Blur Strength")]
 			[Description("Blur strength. Values [10, 100]")]
+			[Category("Emulation")]
 			public int blurStrength { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Force Gamma Correction")]
 			[Description("Force gamma correction.")]
+			[Category("Emulation")]
 			public bool ForceGammaCorrection { get; set; }
 
 			[DefaultValue(2.0)]
 			[DisplayName("Gamma Correction Level")]
 			[Description("Gamma correction level.")]
+			[Category("Emulation")]
 			public float GammaCorrectionLevel { get; set; }
 
 			public N64GLideN64PluginSettings Clone()
--- src/BizHawk.Emulation.Cores/Consoles/Nintendo/N64/N64SyncSettings.GlideMk2.cs
+++ src/BizHawk.Emulation.Cores/Consoles/Nintendo/N64/N64SyncSettings.GlideMk2.cs
@@ -57,174 +57,218 @@ namespace BizHawk.Emulation.Cores.Nintendo.N64
 
 			[DefaultValue(true)]
 			[DisplayName("Wrapper FBO")]
+			[Category("General")]
 			public bool wrpFBO { get; set; }
 
 			[DefaultValue(0)]
 			[DisplayName("Card ID")]
+			[Category("General")]
 			public int card_id { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Use Sts1 Only")]
+			[Category("Per Game Settings")]
 			public bool use_sts1_only { get; set; }
 
 			[DefaultValue(true)]
 			[DisplayName("Optimize Texture Rectangle")]
+			[Category("Per Game Settings")]
 			public bool optimize_texrect { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Increase Texture Rectangle Edge")]
+			[Category("Per Game Settings")]
 			public bool increase_texrect_edge { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Ignore Aux Copy")]
+			[Category("Per Game Settings")]
 			public bool ignore_aux_copy { get; set; }
 
 			[DefaultValue(true)]
 			[DisplayName("High Res Buffer Clear")]
+			[Category("Per Game Settings")]
 			public bool hires_buf_clear { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Force Micro Check")]
+			[Category("Per Game Settings")]
 			public bool force_microcheck { get; set; }
 
 			[DefaultValue(true)]
 			[DisplayName("Fog Enabled")]
+			[Category("Per Game Settings")]
 			public bool fog { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Smart Framebuffer")]
+			[Category("Per Game Settings")]
 			public bool fb_smart { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Framebuffer Read Alpha")]
+			[Category("Per Game Settings")]
 			public bool fb_read_alpha { get; set; }
 
 			[DefaultValue(true)]
 			[DisplayName("High Res Framebuffer")]
+			[Category("Per Game Settings")]
 			public bool fb_hires { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Detect CPU Writes")]
+			[Category("Per Game Settings")]
 			public bool detect_cpu_write { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Decrease Fill Rectangle Edge")]
+			[Category("Per Game Settings")]
 			public bool decrease_fillrect_edge { get; set; }
 
 			[DefaultValue(true)]
 			[DisplayName("Buffer Clear")]
+			[Category("Per Game Settings")]
 			public bool buff_clear { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Alt Texture Size")]
+			[Category("Per Game Settings")]
 			public bool alt_tex_size { get; set; }
 
 			[DefaultValue(1)]
 			[DisplayName("Swap Mode")]
+			[Category("More Per Game Settings")]
 			public int swapmode { get; set; }
 
 			[DefaultValue(1041204192)]
 			[DisplayName("Stipple Pattern")]
+			[Category("More Per Game Settings")]
 			public int stipple_pattern { get; set; }
 
 			[DefaultValue(2)]
 			[DisplayName("Stipple Mode")]
+			[Category("More Per Game Settings")]
 			public int stipple_mode { get; set; }
 
 			[DefaultValue(0)]
 			[DisplayName("LOD Calcuation")]
+			[Category("More Per Game Settings")]
 			public int lodmode { get; set; }
 
 			[DefaultValue(0)]
 			[DisplayName("Filtering")]
+			[Category("More Per Game Settings")]
 			public int filtering { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Wrapper Anisotropic Filtering")]
+			[Category("General")]
 			public bool wrpAnisotropic { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Correct Viewport")]
+			[Category("Per Game Settings")]
 			public bool correct_viewport { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Force Calc Sphere")]
+			[Category("Per Game Settings")]
 			public bool force_calc_sphere { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Pal230")]
+			[Category("Per Game Settings")]
 			public bool pal230 { get; set; }
 
 			[DefaultValue(true)]
 			[DisplayName("Texture Correction")]
+			[Category("Per Game Settings")]
 			public bool texture_correction { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("N64 Z Scale")]
+			[Category("Per Game Settings")]
 			public bool n64_z_scale { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Old Style Adither")]
+			[Category("Per Game Settings")]
 			public bool old_style_adither { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Z Mode Compare Less")]
+			[Category("Per Game Settings")]
 			public bool zmode_compare_less { get; set; }
 
 			[DefaultValue(true)]
 			[DisplayName("Adjust Aspect")]
+			[Category("Per Game Settings")]
 			public bool adjust_aspect { get; set; }
 
 			[DefaultValue(true)]
 			[DisplayName("Clip Z Max")]
+			[Category("Per Game Settings")]
 			public bool clip_zmax { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Clip Z Min")]
+			[Category("Per Game Settings")]
 			public bool clip_zmin { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Force Quad 3D")]
+			[Category("Per Game Settings")]
 			public bool force_quad3d { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Useless is Useless")]
+			[Category("Per Game Settings")]
 			public bool useless_is_useless { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Framebuffer read every frame")]
+			[Category("Per Game Settings")]
 			public bool fb_read_always { get; set; }
 
 			[DefaultValue(false)]
 			[DisplayName("Get Framebuffer Info")]
+			[Category("General")]
 			public bool fb_get_info { get; set; }
 
 			[DefaultValue(true)]
 			[DisplayName("Framebuffer Render")]
+			[Category("General")]
 			public bool fb_render { get; set; }
 
 			[DefaultValue(0)]
 			[DisplayName("Aspect Mode")]
+			[Category("More Per Game Settings")]
 			public int aspectmode { get; set; }
 
 			[DefaultValue(1)]
 			[DisplayName("Framebuffer CRC Mode")]
+			[Category("More Per Game Settings")]
 			public int fb_crc_mode { get; set; }
 
 			[DefaultValue(true)]
 			[DisplayName("Fast CRC")]
+			[Category("Per Game Settings")]
 			public bool fast_crc { get; set; }
 
 			[DefaultValue(0)]
 			[DisplayName("Use Default Hacks")]
+			[Description("Use defaults for current game. This overrides all per game settings.")]
+			[Category("Hacks")]
 			public bool UseDefaultHacks { get; set; }
 
 			[DefaultValue(0)]
 			[DisplayName("Enable Hacks for Game")]
+			[Category("More Per Game Settings")]
 			public int enable_hacks_for_game { get; set; }
 
 			[DefaultValue(0)]
 			[DisplayName("Read Back to Screen")]
+			[Category("More Per Game Settings")]
 			public int read_back_to_screen { get; set; }
 
 			public N64Glide64mk2PluginSettings Clone()
