73d64078e6856a84d088b07d075329e47ff49e29
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2021-07-20 01:20:21 -0700

    attempt to preserve 2c8295b behavior

--- src/BizHawk.Emulation.Cores/Consoles/Nintendo/N64/N64.cs
+++ src/BizHawk.Emulation.Cores/Consoles/Nintendo/N64/N64.cs
@@ -15,10 +15,11 @@ namespace BizHawk.Emulation.Cores.Nintendo.N64
 		/// Create mupen64plus Emulator
 		/// </summary>
 		/// <param name="game">Game information of game to load</param>
-		/// <param name="rom">Rom that should be loaded</param>
+		/// <param name="file">Rom that should be loaded</param>
+		/// <param name="rom">byte-swapped rom data</param>
 		/// <param name="syncSettings">N64SyncSettings object</param>
 		[CoreConstructor("N64")]
-		public N64(GameInfo game, byte[] rom, N64Settings settings, N64SyncSettings syncSettings)
+		public N64(GameInfo game, byte[] file, byte[] rom, N64Settings settings, N64SyncSettings syncSettings)
 		{
 			ServiceProvider = new BasicServiceProvider(this);
 			InputCallbacks = new InputCallbackSystem();
@@ -78,7 +79,7 @@ namespace BizHawk.Emulation.Cores.Nintendo.N64
 			//not sure why this works... if we put the plugin initializations in here, we get deadlocks in some SDL initialization. doesnt make sense to me...
 			RunThreadAction(() =>
 			{
-				api = new mupen64plusApi(this, rom, videosettings, SaveType, (int)coreType, _disableExpansionSlot);
+				api = new mupen64plusApi(this, file, videosettings, SaveType, (int)coreType, _disableExpansionSlot);
 			});
 
 			// Order is important because the register with the mupen core
