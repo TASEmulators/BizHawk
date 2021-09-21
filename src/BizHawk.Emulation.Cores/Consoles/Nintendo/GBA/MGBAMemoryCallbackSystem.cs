d0f627feac9f84712f8cb9f786b3c6c3b4eb2c4b
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2021-09-20 23:01:17 -0700

    fix https://github.com/TASVideos/BizHawk/issues/2805

--- src/BizHawk.Emulation.Cores/Consoles/Nintendo/GBA/MGBAMemoryCallbackSystem.cs
+++ src/BizHawk.Emulation.Cores/Consoles/Nintendo/GBA/MGBAMemoryCallbackSystem.cs
@@ -60,7 +60,7 @@ namespace BizHawk.Emulation.Cores.Nintendo.GBA
 			}
 			else
 			{
-				MGBAHawk.ZZHacky.BizSetMemCallback(_mgba.Core, container.CallDelegate);
+				MGBAHawk.ZZHacky.BizSetMemCallback(_mgba.Core, container._cd);
 				container.ID = MGBAHawk.ZZHacky.BizSetWatchpoint(_mgba.Core, callback.Address.Value, container.WatchPointType);
 			}
 
@@ -169,7 +169,8 @@ namespace BizHawk.Emulation.Cores.Nintendo.GBA
 			}
 		}
 
-		public LibmGBA.MemCallback CallDelegate;
+		public LibmGBA.MemCallback _cd => CallDelegate;
+		public static LibmGBA.MemCallback CallDelegate;
 
 		private void Call(uint addr, LibmGBA.mWatchpointType type, uint oldValue, uint newValue)
 		{
