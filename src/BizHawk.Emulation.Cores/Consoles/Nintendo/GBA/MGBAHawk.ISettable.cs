8dd1827875d22a2909eee7794a1499a6f020eb01
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2021-09-21 20:47:18 -0700

    override sync settings

--- src/BizHawk.Emulation.Cores/Consoles/Nintendo/GBA/MGBAHawk.ISettable.cs
+++ src/BizHawk.Emulation.Cores/Consoles/Nintendo/GBA/MGBAHawk.ISettable.cs
@@ -165,6 +165,47 @@ namespace BizHawk.Emulation.Cores.Nintendo.GBA
 			[DefaultValue(typeof(DateTime), "2010-01-01")]
 			public DateTime RTCInitialTime { get; set; }
 
+			[DisplayName("Save Type")]
+			[Description("Save type used in games.")]
+			[DefaultValue(LibmGBA.SaveType.Autodetect)]
+			public LibmGBA.SaveType OverrideSaveType { get; set; }
+			public enum HardwareSelection : int
+			{
+				Autodetect = 0,
+				True = 1,
+				False = 2,
+			}
+
+			[DisplayName("RTC")]
+			[Description("")]
+			[DefaultValue(HardwareSelection.Autodetect)]
+			public HardwareSelection OverrideRtc { get; set; }
+
+			[DisplayName("Rumble")]
+			[Description("")]
+			[DefaultValue(HardwareSelection.Autodetect)]
+			public HardwareSelection OverrideRumble { get; set; }
+
+			[DisplayName("Light Sensor")]
+			[Description("")]
+			[DefaultValue(HardwareSelection.Autodetect)]
+			public HardwareSelection OverrideLightSensor { get; set; }
+
+			[DisplayName("Gyro")]
+			[Description("")]
+			[DefaultValue(HardwareSelection.Autodetect)]
+			public HardwareSelection OverrideGyro { get; set; }
+
+			[DisplayName("Tilt")]
+			[Description("")]
+			[DefaultValue(HardwareSelection.Autodetect)]
+			public HardwareSelection OverrideTilt { get; set; }
+
+			[DisplayName("GB Player Detection")]
+			[Description("")]
+			[DefaultValue(false)]
+			public bool OverrideGbPlayerDetect { get; set; }
+
 			public SyncSettings()
 			{
 				SettingsUtil.SetDefaultValues(this);
