52fb2517a801cebdc458762115d208f00406bdd4
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2021-10-10 16:53:20 -0700

    improve ds input script. TODO: non-inverted horizontal display doesn't work at all with this script, and rot270 is partially broken

--- Assets/Lua/NDS/StylusInputDisplay.lua
+++ Assets/Lua/NDS/StylusInputDisplay.lua
@@ -4,6 +4,12 @@ local upColor = 'lime'
 local downColor = 'red'
 local dotColor = 'blue'
 
+client.setwindowsize(client.getwindowsize()) -- assert a sane resolution
+
+invert = false
+-- FIXME: No idea how to get lua to detect inverted screens, set this to true if inverted screens are active
+-- (typing "invert = true" without the quotes in the console works)
+
 function Draw(x, y, maxX, maxY, isDown)
 	color = upColor
 	if isDown then
@@ -43,16 +49,33 @@ while true do
 		btns = movie.getinput(emu.framecount() - 1)
 	end
 
-	local x = btns['Touch X']
-	local y = btns['Touch Y']
+	local wsz = client.getwindowsize()
+	local w = client.screenwidth() / wsz
+	local xo = 0
+	local yo = 0
+
+	if w == 512 then -- horizontal
+		if invert then
+			xo = xo - 256
+		else
+			error("Non-inverted horizontal screens are unsupported")
+		end
+	else -- vertical
+		if invert then
+			yo = yo - 192
+		else
+			-- don't need to do anything here
+		end
+	end
+
+	local x = btns['Touch X'] + xo
+	local y = btns['Touch Y'] + yo
 	local isDown = btns['Touch']
 
 	pts = client.transformPoint(x, y)
 	local tx = pts["x"];
 	local ty = pts["y"];
-	local wx = client.screenwidth() / 256
-	local wy = client.screenheight() / 384
-	Draw(tx / wx, ty / wy, 10000, 10000, isDown)
+	Draw(tx / wsz, ty / wsz, 10000, 10000, isDown)
 
 	emu.yield()
 end
