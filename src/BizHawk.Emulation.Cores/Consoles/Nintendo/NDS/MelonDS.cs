d61cddabff8f6b9c2b16fcaa2d2c93df872ad50c
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2021-10-20 20:00:27 -0700

    and hook up set reg, seems to work fine

--- src/BizHawk.Emulation.Cores/Consoles/Nintendo/NDS/MelonDS.cs
+++ src/BizHawk.Emulation.Cores/Consoles/Nintendo/NDS/MelonDS.cs
@@ -575,8 +575,24 @@ namespace BizHawk.Emulation.Cores.Consoles.Nintendo.NDS
 			return ret;
 		}
 
-		[FeatureNotImplemented]
-		public void SetCpuRegister(string register, int value) => throw new NotImplementedException();
+		public void SetCpuRegister(string register, int value)
+		{
+			if (register.Length != 7 && register.Length != 8)
+			{
+				throw new InvalidOperationException("Wrong String Length???");
+			}
+			int ncpu = int.Parse(register.Substring(3, 1));
+			if (ncpu != 9 && ncpu != 7)
+			{
+				throw new InvalidOperationException("Invalid CPU???");
+			}
+			int index = int.Parse(register.Substring(6, register.Length - 6));
+			if (index < 0 || index > 15)
+			{
+				throw new InvalidOperationException("Invalid Reg Index???");
+			}
+			_core.SetReg(ncpu == 9 ? 0 : 1, index, value);
+		}
 
 		public bool CanStep(StepType type) => false;
 
