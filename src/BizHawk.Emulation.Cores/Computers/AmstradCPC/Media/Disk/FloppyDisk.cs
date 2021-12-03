19685cdf074a301dd77d4247481119fd8e8d4656
YoshiRulz <OSSYoshiRulz@gmail.com>
2021-12-04 07:19:06 +1000

    Multi-target .NET Framework and .NET 8

--- src/BizHawk.Emulation.Cores/Computers/AmstradCPC/Media/Disk/FloppyDisk.cs
+++ src/BizHawk.Emulation.Cores/Computers/AmstradCPC/Media/Disk/FloppyDisk.cs
@@ -4,8 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-using BizHawk.Common.CollectionExtensions;
 using BizHawk.Common.StringExtensions;
+
+using CollectionExtensions = BizHawk.Common.CollectionExtensions.CollectionExtensions;
 
 namespace BizHawk.Emulation.Cores.Computers.AmstradCPC
 {
