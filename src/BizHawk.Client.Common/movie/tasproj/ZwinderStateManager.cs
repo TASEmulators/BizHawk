TASEmulators/BizHawk

commit 6c5447f5da728fc3aef06220aa96049f4e8442f2

ZwinderStateManager: Use the gap buffer for forced capture to avoid crowding the "current" buffer and thus reducing it's actual span of covered frames. (#2569)
also remove a redundant conditional

src/BizHawk.Client.Common/movie/tasproj/ZwinderStateManager.cs
@@ -291,13 +291,10 @@ namespace BizHawk.Client.Common
 
 			// We do not want to consider reserved states for a notion of Last
 			// reserved states can include future states in the case of branch states
-			if (frame <= LastRing)
+			if ((frame <= LastRing && NeedsGap(frame)) || force)
 			{
-				if (NeedsGap(frame))
-				{
-					CaptureGap(frame, source);
-				}
-
+				// We use the gap buffer for forced capture to avoid crowding the "current" buffer and thus reducing it's actual span of covered frames.
+				CaptureGap(frame, source);
 				return;
 			}
 

src/BizHawk.Client.Common/movie/tasproj/TasMovie.cs
@@ -192,10 +192,8 @@ namespace BizHawk.Client.Common
 
 			LagLog[Emulator.Frame] = _inputPollable.IsLagFrame;
 
-			if (!TasStateManager.HasState(Emulator.Frame))
-			{
-				TasStateManager.Capture(Emulator.Frame, Emulator.AsStatable(), Emulator.Frame == LastEditedFrame - 1);
-			}
+			// We will forbibly capture a state for the last edited frame (requested by #916 for case of "platforms with analog stick")
+			TasStateManager.Capture(Emulator.Frame, Emulator.AsStatable(), Emulator.Frame == LastEditedFrame - 1);
 		}
 
 		

commit 014cd51ae4dd6dc781376c8790037933f0f1ba6b

Fix a regression from commit 6c5447f5da728fc3aef06220aa96049f4e8442f2 that was causing states to be added out of order.

src/BizHawk.Client.Common/movie/tasproj/ZwinderStateManager.cs
@@ -319,12 +319,21 @@ namespace BizHawk.Client.Common
 				return;
 			}
 
+			// We use the gap buffer for forced capture to avoid crowding the "current" buffer and thus reducing it's actual span of covered frames.
+			if (force)
+			{
+				CaptureGap(frame, source);
+				return;
+			}
+
 			// We do not want to consider reserved states for a notion of Last
 			// reserved states can include future states in the case of branch states
-			if ((frame <= LastRing && NeedsGap(frame)) || force)
+			if (frame <= LastRing)
 			{
-				// We use the gap buffer for forced capture to avoid crowding the "current" buffer and thus reducing it's actual span of covered frames.
-				CaptureGap(frame, source);
+				if (NeedsGap(frame))
+				{
+					CaptureGap(frame, source);
+				}
 				return;
 			}
 