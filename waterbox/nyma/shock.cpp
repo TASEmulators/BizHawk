d908e4be2c872be2f805da1a950df456af547b89
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2022-01-23 19:01:14 -0800

    saveram stuff

--- waterbox/nyma/shock.cpp
+++ waterbox/nyma/shock.cpp
@@ -2,6 +2,7 @@
 #include <src/mednafen.h>
 #include <src/psx/psx.h>
 #include <src/psx/spu.h>
+#include <src/psx/frontio.h>
 #include "nyma.h"
 #include <emulibc.h>
 #include <waterboxcore.h>
@@ -22,26 +23,24 @@ namespace MDFN_IEN_PSX
 	extern PS_GPU GPU;
 	extern PS_SPU *SPU;
 	extern PS_CPU *CPU;
+	extern FrontIO *FIO;
+	extern MultiAccessSizeMem<512 * 1024, false> *BIOSROM;
+	extern MultiAccessSizeMem<65536, false> *PIOMem;
 }
 
-#define MemoryDomainFunctions(N,R,W,O)\
-static void Access##N(uint8_t* buffer, int64_t address, int64_t count, bool write)\
-{\
-	if (write)\
-	{\
-		while (count--)\
-			W(O + address++, *buffer++);\
-	}\
-	else\
-	{\
-		while (count--)\
-			*buffer++ = R(O + address++);\
-	}\
+static void AccessSystemBus(uint8_t* buffer, int64_t address, int64_t count, bool write)
+{
+	if (write)
+	{
+		while (count--)
+			PSX_MemPoke8(address++, *buffer++);
+	}
+	else
+	{
+		while (count--)
+			*buffer++ = PSX_MemPeek8(address++);
+	}
 }
-
-MemoryDomainFunctions(BIOSROM, PSX_MemPeek8, PSX_MemPoke8, 0x1FC00000);
-MemoryDomainFunctions(PIOMem, PSX_MemPeek8, PSX_MemPoke8, 0x1F000000);
-MemoryDomainFunctions(SystemBus, PSX_MemPeek8, PSX_MemPoke8, 0);
 
 ECL_EXPORT void GetMemoryAreas(MemoryArea* m)
 {
@@ -58,8 +57,48 @@ ECL_EXPORT void GetMemoryAreas(MemoryArea* m)
 	AddMemoryDomain("MainRAM", MainRAM.data8, 2048*1024, MEMORYAREA_FLAGS_WRITABLE | MEMORYAREA_FLAGS_WORDSIZE4 | MEMORYAREA_FLAGS_PRIMARY);
 	AddMemoryDomain("GPURAM", GPU.GPURAM, 2*512*1024, MEMORYAREA_FLAGS_WRITABLE | MEMORYAREA_FLAGS_WORDSIZE4);
 	AddMemoryDomain("SPURAM", SPU->SPURAM, 512*1024, MEMORYAREA_FLAGS_WRITABLE | MEMORYAREA_FLAGS_WORDSIZE4);
-	AddMemoryDomain("BiosROM", (void*)AccessBIOSROM, 512*1024, MEMORYAREA_FLAGS_WRITABLE | MEMORYAREA_FLAGS_WORDSIZE4 | MEMORYAREA_FLAGS_FUNCTIONHOOK);
-	AddMemoryDomain("PIOMem", (void*)AccessPIOMem, 64*1024, MEMORYAREA_FLAGS_WRITABLE | MEMORYAREA_FLAGS_WORDSIZE4 | MEMORYAREA_FLAGS_FUNCTIONHOOK);
+	AddMemoryDomain("BiosROM", BIOSROM->data8, 512*1024, MEMORYAREA_FLAGS_WRITABLE | MEMORYAREA_FLAGS_WORDSIZE4);
+	AddMemoryDomain("PIOMem", PIOMem ? PIOMem->data8 : NULL, 64*1024, MEMORYAREA_FLAGS_WRITABLE | MEMORYAREA_FLAGS_WORDSIZE4);
 	AddMemoryDomain("DCache", CPU->ScratchRAM.data8, 1024, MEMORYAREA_FLAGS_WRITABLE | MEMORYAREA_FLAGS_WORDSIZE4);
 	AddMemoryDomain("System Bus", (void*)AccessSystemBus, 1ull << 32, MEMORYAREA_FLAGS_WRITABLE | MEMORYAREA_FLAGS_WORDSIZE4 | MEMORYAREA_FLAGS_FUNCTIONHOOK);
+}
+
+ECL_EXPORT int GetMemcardSize(int which)
+{
+	try
+	{
+		return FIO->PossibleDevices[which].Memcard->GetNVSize();
+	}
+	catch(...)
+	{
+		return 0;
+	}
+}
+
+ECL_EXPORT void GetMemcardData(int which)
+{
+	try
+	{
+		char ext[64];
+		trio_snprintf(ext, sizeof(ext), "%d.mcr", which);
+		FIO->SaveMemcard(which, MDFN_MakeFName(MDFNMKF_SAV, 0, ext));
+	}
+	catch(...)
+	{
+	}
+}
+
+ECL_EXPORT bool PutMemcardData(int which)
+{
+	try
+	{
+		char ext[64];
+		trio_snprintf(ext, sizeof(ext), "%d.mcr", which);
+		FIO->LoadMemcard(which, MDFN_MakeFName(MDFNMKF_SAV, 0, ext));
+		return true;
+	}
+	catch(...)
+	{
+		return false;
+	}
 }
