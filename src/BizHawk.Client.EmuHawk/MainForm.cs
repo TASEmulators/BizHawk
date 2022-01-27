TASEmulators/BizHawk

commit 937b9663937ca274237ea96e82c92f6498a0edbb

Revert "Comment says we want to update after a turbo seek. So, don't update if we weren't turbo seeking. (This removes double-updating tools after reaching the desired frame, unless turbo seek is turned on.)"
This reverts commit dfc1436c9f3e33503d0f7ef95d369a8f1af6299c.

This caused the progress bar to not disappear after finishing a normal seek.

src/BizHawk.Client.EmuHawk/MainForm.cs
@@ -844,11 +844,10 @@ namespace BizHawk.Client.EmuHawk
 
 			set
 			{
-				bool wasTurboSeeking = IsTurboSeeking;
 				_pauseOnFrame = value;
 				SetPauseStatusBarIcon();
 
-				if (wasTurboSeeking && value == null) // TODO: make an Event handler instead, but the logic here is that after turbo seeking, tools will want to do a real update when the emulator finally pauses
+				if (value == null) // TODO: make an Event handler instead, but the logic here is that after turbo seeking, tools will want to do a real update when the emulator finally pauses
 				{
 					Tools.UpdateToolsBefore();
 					Tools.UpdateToolsAfter();

commit e3e1b222e275fcd123a3d1e1a59b983940416da7

Comment says we want to update after a turbo seek. So, don't update if we weren't turbo seeking. (This removes double-updating tools after reaching the desired frame, unless turbo seek is turned on.)
This commit addresses the regression mentioned in commit 937b966.

src/BizHawk.Client.EmuHawk/MainForm.cs
@@ -844,10 +844,11 @@ namespace BizHawk.Client.EmuHawk
 
 			set
 			{
+				bool wasTurboSeeking = IsTurboSeeking;
 				_pauseOnFrame = value;
 				SetPauseStatusBarIcon();
 
-				if (value == null) // TODO: make an Event handler instead, but the logic here is that after turbo seeking, tools will want to do a real update when the emulator finally pauses
+				if (wasTurboSeeking && value == null) // TODO: make an Event handler instead, but the logic here is that after turbo seeking, tools will want to do a real update when the emulator finally pauses
 				{
 					Tools.UpdateToolsBefore();
 					Tools.UpdateToolsAfter();

src/BizHawk.Client.EmuHawk/tools/TAStudio/TAStudio.ListView.cs
@@ -123,6 +123,7 @@ namespace BizHawk.Client.EmuHawk
 			if (CurrentTasMovie != null)
 			{
 				RefreshDialog();
+				UpdateProgressBar();
 			}
 		}
 