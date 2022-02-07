21a4821b5ccc39349e462101027f6c2d24b328da
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2022-02-06 22:12:35 -0800

    meh

--- waterbox/ares64/BizInterface.cpp
+++ waterbox/ares64/BizInterface.cpp
@@ -8,27 +8,30 @@
 struct BizPlatform : ares::Platform {
 	auto attach(ares::Node::Object) -> void override;
 	auto pak(ares::Node::Object) -> shared_pointer<vfs::directory> override;
-	auto video(ares::Node::Video::Screen, const u32* data, u32, u32, u32) -> void override;
+	auto video(ares::Node::Video::Screen, const u32* data, u32 pitch, u32 width, u32 height) -> void override;
 	auto input(ares::Node::Input::Input) -> void override;
 
 	shared_pointer<vfs::directory> bizpak = new vfs::directory;
-	u32* videoBuf = nullptr;
-	ares::Node::Video::Screen screen;
 	ares::Node::Audio::Stream stream;
+	u32* videobuf = nullptr;
+	u32 pitch = 0;
+	u32 width = 0;
+	u32 height = 0;
 };
 
 auto BizPlatform::attach(ares::Node::Object node) -> void {
-	if (auto screen = node->cast<ares::Node::Video::Screen>()) {
-		screen->setProgressive(true);
-		this->screen = screen;
-	}
 	if (auto stream = node->cast<ares::Node::Audio::Stream>()) {
 		stream->setResamplerFrequency(44100);
 		this->stream = stream;
 	}
 }
 auto BizPlatform::pak(ares::Node::Object) -> shared_pointer<vfs::directory> { return bizpak; }
-auto BizPlatform::video(ares::Node::Video::Screen, const u32* data, u32, u32, u32) -> void { videoBuf = (u32*)data; }
+auto BizPlatform::video(ares::Node::Video::Screen, const u32* data, u32 pitch, u32 width, u32 height) -> void {
+	videobuf = (u32*)data;
+	this->pitch = pitch;
+	this->width = width;
+	this->height = height;
+}
 auto BizPlatform::input(ares::Node::Input::Input) -> void {};
 
 static ares::Node::System root;
@@ -108,10 +111,10 @@ EXPORT void FrameAdvance(MyFrameInfo* f)
 {
 	// handle input
 	root->run();
-	f->Width = platform.screen->width();
-	f->Height = platform.screen->height();
-	u32 pitch = platform.screen->canvasWidth();
-	u32* src = platform.videoBuf;
+	f->Width = platform.width;
+	f->Height = platform.height;
+	u32 pitch = platform.pitch;
+	u32* src = platform.videobuf;
 	u32* dst = f->VideoBuffer;
 	for (int i = 0; i < f->Height; i++)
 	{
