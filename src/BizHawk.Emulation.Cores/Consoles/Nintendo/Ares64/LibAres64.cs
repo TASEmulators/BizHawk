0e461a24982ac851eca67c370fdc51569e079cad
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2022-02-10 20:47:37 -0800

    apparently explicit layout just dont work, thanks c#!

--- src/BizHawk.Emulation.Cores/Consoles/Nintendo/Ares64/LibAres64.cs
+++ src/BizHawk.Emulation.Cores/Consoles/Nintendo/Ares64/LibAres64.cs
@@ -35,40 +35,26 @@ namespace BizHawk.Emulation.Cores.Consoles.Nintendo.Ares64
 			Rumblepak,
 		}
 
-		[StructLayout(LayoutKind.Explicit)]
+		[StructLayout(LayoutKind.Sequential)]
 		public new class FrameInfo : LibWaterboxCore.FrameInfo
 		{
-			[FieldOffset(44)]
 			public Buttons P1Buttons;
-			[FieldOffset(48)]
 			public short P1XAxis;
-			[FieldOffset(50)]
 			public short P1YAxis;
 
-			[FieldOffset(52)]
 			public Buttons P2Buttons;
-			[FieldOffset(56)]
 			public short P2XAxis;
-			[FieldOffset(58)]
 			public short P2YAxis;
 
-			[FieldOffset(60)]
 			public Buttons P3Buttons;
-			[FieldOffset(64)]
 			public short P3XAxis;
-			[FieldOffset(68)]
 			public short P3YAxis;
 
-			[FieldOffset(70)]
 			public Buttons P4Buttons;
-			[FieldOffset(74)]
 			public short P4XAxis;
-			[FieldOffset(76)]
 			public short P4YAxis;
 
-			[FieldOffset(78)]
 			public bool Reset;
-			[FieldOffset(79)]
 			public bool Power;
 		}
 
