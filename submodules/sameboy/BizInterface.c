178264ce67ae0f254c5e8acc33401f72bf014e32
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2022-04-02 10:22:18 -0700

    update sameboy, fix bug with SRAM loading

--- submodules/sameboy/BizInterface.c
+++ submodules/sameboy/BizInterface.c
@@ -8,20 +8,15 @@
 	#define EXPORT __attribute__((visibility("default")))
 #endif
 
+typedef int8_t s8;
 typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
 
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
-
-typedef enum
-{
-	IS_DMG = 0,
-	IS_CGB = 1,
-	IS_AGB = 2,
-	RTC_ACCURATE = 4,
-} LoadFlags;
 
 typedef void (*input_callback_t)(void);
 typedef void (*trace_callback_t)(u16);
