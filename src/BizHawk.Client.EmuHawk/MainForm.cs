TASEmulators/BizHawk

commit a858282a8d4119e48b9aae60e7d85b0b4f2e852d

Fix #1513 hopefully properly

src/BizHawk.Client.EmuHawk/MainForm.cs
@@ -2287,7 +2287,7 @@ namespace BizHawk.Client.EmuHawk
 
 		public BitmapBuffer MakeScreenshotImage()
 		{
-			return DisplayManager.RenderVideoProvider(_currentVideoProvider);
+			return new BitmapBuffer(_currentVideoProvider.BufferWidth, _currentVideoProvider.BufferHeight, _currentVideoProvider.GetVideoBuffer().ToArray());
 		}
 
 		private void SaveSlotSelectedMessage()

src/BizHawk.Client.Common/DisplayManager/DisplayManagerBase.cs
@@ -498,27 +498,6 @@ namespace BizHawk.Client.Common
 			return null;
 		}
 
-		public BitmapBuffer RenderVideoProvider(IVideoProvider videoProvider)
-		{
-			// TODO - we might need to gather more Config.DispXXX properties here, so they can be overridden
-			var targetSize = new Size(videoProvider.BufferWidth, videoProvider.BufferHeight);
-			var padding = CalculateCompleteContentPaddingSum(true,true);
-			targetSize.Width += padding.Horizontal;
-			targetSize.Height += padding.Vertical;
-
-			var job = new JobInfo
-			{
-				VideoProvider = videoProvider,
-				Simulate = false,
-				ChainOutsize = targetSize,
-				Offscreen = true,
-				IncludeOSD = false,
-				IncludeUserFilters = false
-			};
-			UpdateSourceInternal(job);
-			return job.OffscreenBb;
-		}
-
 		/// <summary>
 		/// Does the entire display process to an offscreen buffer, suitable for a 'client' screenshot.
 		/// </summary>
@@ -910,8 +889,7 @@ namespace BizHawk.Client.Common
 			}
 
 			//POOPY. why are we delivering the GL context this way? such bad
-			ScreenControlNDS fNDS = filterProgram["CoreScreenControl"] as ScreenControlNDS;
-			if (fNDS != null)
+			if (filterProgram["CoreScreenControl"] is ScreenControlNDS fNDS)
 			{
 				fNDS.GuiRenderer = _renderer;
 				fNDS.GL = _gl;

commit 7d268e244babc1d7472fc8400febbc0dd7e2cc87

fix #3166
was a regression from a858282a8d4119e48b9aae60e7d85b0b4f2e852d

src/BizHawk.Client.EmuHawk/MainForm.cs
@@ -2287,7 +2287,9 @@ namespace BizHawk.Client.EmuHawk
 
 		public BitmapBuffer MakeScreenshotImage()
 		{
-			return new BitmapBuffer(_currentVideoProvider.BufferWidth, _currentVideoProvider.BufferHeight, _currentVideoProvider.GetVideoBuffer().ToArray());
+			var ret = new BitmapBuffer(_currentVideoProvider.BufferWidth, _currentVideoProvider.BufferHeight, _currentVideoProvider.GetVideoBuffer().ToArray());
+			ret.DiscardAlpha();
+			return ret;
 		}
 
 		private void SaveSlotSelectedMessage()