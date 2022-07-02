42d35f89f3cb4ba09efab4e691ad48a8da120e3e
tom_mai78101 <tom.mai78101@gmail.com>
2022-07-02 00:22:12 -0400

    Fixed the calls being reversed, per feedback.

--- src/BizHawk.Client.EmuHawk/tools/Debugger/GenericDebugger.cs
+++ src/BizHawk.Client.EmuHawk/tools/Debugger/GenericDebugger.cs
@@ -293,8 +293,8 @@ namespace BizHawk.Client.EmuHawk
 
 		private void ToPCBtn_Click(object sender, EventArgs e)
 		{
-			UpdateDisassembler();
 			UpdatePC();
+			UpdateDisassembler();
 			DisassemblerView.Refresh();
 		}
 
