afb0b8a435127dc8bb908391224fc8e5a9e300d9
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2022-08-10 02:55:01 -0700

    maybe fix tests?

--- Dist/.BuildTestInConfigX.sh
+++ Dist/.BuildTestInConfigX.sh
@@ -5,5 +5,4 @@ if [ ! -e "BizHawk.sln" ]; then
 fi
 config="$1"
 shift
-export LD_LIBRARY_PATH="$PWD/output/dll:$LD_LIBRARY_PATH"
 Dist/.InvokeCLIOnMainSln.sh "test" "$config" -a . -l "junit;LogFilePath=$PWD/test_output/{assembly}.coverage.xml;MethodFormat=Class;FailureBodyFormat=Verbose" "$@"
