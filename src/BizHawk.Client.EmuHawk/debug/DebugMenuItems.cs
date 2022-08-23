5b859960e9a72a663e4fe53434c9ade6dc539613
YoshiRulz <OSSYoshiRulz@gmail.com>
2022-08-24 04:19:22 +1000

    Move types for debug menu to own files

---
+++ src/BizHawk.Client.EmuHawk/debug/DebugMenuItems.cs
@@ -0,0 +1,45 @@
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+using BizHawk.WinForms.Controls;
+
+namespace BizHawk.Client.EmuHawk.ForDebugging
+{
+	internal sealed class DebugVSystemChildItem : ToolStripMenuItemEx
+	{
+		public string? RequiresCore = null;
+
+		public bool RequiresLoadedRom = true;
+
+		public DebugVSystemChildItem(string labelText, Action onClick)
+		{
+			Text = labelText;
+			Click += (_, _) => onClick();
+		}
+	}
+
+	internal sealed class DebugVSystemMenuItem : ToolStripMenuItemEx
+	{
+		public readonly IReadOnlyCollection<string> SysIDs;
+
+		public DebugVSystemMenuItem(string sysID, params string[] extraSysIDs)
+		{
+			SysIDs = new[] { sysID }.Concat(extraSysIDs).ToHashSet();
+			Text = sysID;
+		}
+	}
+
+	internal static class GenControl
+	{
+		public static Button Button(string text, int width, Action onClick)
+		{
+			SzButtonEx btn = new() { Size = new(width, 23), Text = text };
+			btn.Click += (_, _) => onClick();
+			return btn;
+		}
+	}
+}
---
+++ src/BizHawk.Client.EmuHawk/debug/FirmwareAutopatchDebugToolForm.cs
@@ -0,0 +1,88 @@
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+using BizHawk.Client.Common;
+using BizHawk.Common;
+using BizHawk.Emulation.Common;
+using BizHawk.WinForms.Controls;
+
+namespace BizHawk.Client.EmuHawk.ForDebugging
+{
+	internal sealed class FirmwareAutopatchDebugToolForm : ToolFormBase
+	{
+		public const string TOOL_NAME = "Manual Firmware Autopatching Tool";
+
+		protected override string WindowTitleStatic
+			=> TOOL_NAME;
+
+		public FirmwareAutopatchDebugToolForm()
+		{
+			static string LabelFragment(string hash)
+				=> $"{hash.Substring(0, 8)}... {FirmwareDatabase.FirmwareFilesByHash[hash].RecommendedName}";
+			List<(string Label, FirmwarePatchOption PatchOption)> patches = FirmwareDatabase.AllPatches
+				.Select(static fpo => ($"{LabelFragment(fpo.BaseHash)} --> {LabelFragment(fpo.TargetHash)}", fpo)).ToList();
+			patches.Sort(static (a, b) => a.Label.CompareTo(b.Label));
+			ComboBox comboPatchsets = new() { Size = new(300, 23) };
+			foreach (var tuple in patches) comboPatchsets.Items.Add(tuple.Label);
+			SzTextBoxEx txtBaseFile = new() { Size = new(224, 23) };
+			void ShowBaseFilePicker()
+			{
+				using OpenFileDialog ofd = new() { InitialDirectory = Config!.PathEntries.FirmwareAbsolutePath() };
+				this.ShowDialogAsChild(ofd);
+				txtBaseFile.Text = ofd.FileName;
+			}
+			CheckBoxEx cbDryRun = new() { Checked = true, Text = "dry run (skip writing to disk)" };
+			void DoPatch()
+			{
+				var fpo = patches[comboPatchsets.SelectedIndex].PatchOption;
+				try
+				{
+					if (!cbDryRun.Checked)
+					{
+						var (filePath, _, _) = FirmwareManager.PerformPatchOnDisk(txtBaseFile.Text, in fpo, Config!.PathEntries);
+						// if the base file (or patchset) is wrong, too bad
+						this.ModalMessageBox($"wrote {filePath}");
+						return;
+					}
+					var @base = File.ReadAllBytes(txtBaseFile.Text);
+					var (_, actualHash) = FirmwareManager.PerformPatchInMemory(@base, in fpo);
+					if (actualHash == fpo.TargetHash)
+					{
+						this.ModalMessageBox("success");
+						return;
+					}
+					// else something happened, figure out what it was
+					var baseHash = SHA1Checksum.ComputeDigestHex(@base);
+					this.ModalMessageBox(baseHash == fpo.BaseHash
+						? $"patchset declared with target\nSHA1:{fpo.TargetHash}\nbut produced\nSHA1:{actualHash}\n(is the patch wrong, or the hash?)"
+						: $"patchset declared for base\nSHA1:{fpo.BaseHash}\nbut\nSHA1:{baseHash}\nwas provided");
+				}
+				catch (Exception e)
+				{
+					this.ModalMessageBox($"caught {e.GetType().Name}:\n{e}");
+				}
+			}
+			ClientSize = new(320, 200);
+			SuspendLayout();
+			Controls.Add(new SingleColumnFLP
+			{
+				Controls =
+				{
+					new LabelEx { Text = "apply" },
+					comboPatchsets,
+					new LabelEx { Text = "to file" },
+					new SingleRowFLP { Controls = { txtBaseFile, GenControl.Button("(browse)", width: 75, ShowBaseFilePicker) } },
+					cbDryRun,
+					new LabelEx { Text = "patched files are saved in dir set as \"Temp Files\"" },
+					GenControl.Button("--> patch", width: 75, DoPatch),
+				},
+			});
+			ResumeLayout();
+		}
+	}
+}
