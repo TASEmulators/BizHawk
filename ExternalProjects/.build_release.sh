466d052c2d59d2d25e0d0203673a341feb9a0b38
YoshiRulz <OSSYoshiRulz@gmail.com>
2022-09-28 07:07:01 +1000

    Clean up project files of external .NET projects

---
+++ ExternalProjects/.build_release.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+dotnet build -c Release -m
--- ExternalProjects/FlatBuffers.GenOutput/build_release_and_copy_asm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-outDir="../../References"
-cd "$(dirname "$0")" && rm -f "$outDir/FlatBuffers.GenOutput.dll" && dotnet build -c Release "$@" && cp bin/Release/*/FlatBuffers.GenOutput.dll $outDir
