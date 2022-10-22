e073173a8c9f7c22e9a3f6ae7488dbaa40a344cf
Morilli <35152647+Morilli@users.noreply.github.com>
2022-10-22 03:18:59 +0200

    Update make-all-cores.sh

--- waterbox/make-all-cores.sh
+++ waterbox/make-all-cores.sh
@@ -3,11 +3,14 @@ set -e
 
 cd emulibc && make -f Makefile $1 -j && cd -
 cd libco && make -f Makefile $1 -j && cd -
+cd ares64 && make -f Makefile $1 -j && cd -
+cd bsnescore && make -f Makefile $1 -j && cd -
 cd gpgx && make -f Makefile $1 -j && cd -
 cd libsnes && make -f Makefile $1 -j && cd -
 cd melon && make -f Makefile $1 -j && cd -
 cd picodrive && make -f Makefile $1 -j && cd -
 cd snes9x && make -f Makefile $1 -j && cd -
 cd tic80 && make -f Makefile $1 -j && cd -
+cd virtualjaguar && make -f Makefile $1 -j && cd -
 cd uzem && make -f Makefile $1 -j && cd -
 cd nyma && ./make-all-released-cores.sh $1 && cd -
