77c4e37b2cee05b3871261e807f61cdfb36d218f
YoshiRulz <OSSYoshiRulz@gmail.com>
2022-11-07 04:32:17 +1000

    (broken) Use NS cursor for TAStudio axis double-click-and-drag

--- src/BizHawk.Client.EmuHawk/tools/TAStudio/TAStudio.ListView.cs
+++ src/BizHawk.Client.EmuHawk/tools/TAStudio/TAStudio.ListView.cs
@@ -714,6 +714,7 @@ namespace BizHawk.Client.EmuHawk
 								CurrentTasMovie.ChangeLog.BeginNewBatch($"Axis Edit: {frame}");
 								_axisEditColumn = buttonName;
 								AxisEditRow = frame;
+								Cursor = Cursors.SizeNS;
 								_axisTypedValue = "";
 								_axisEditYPos = e.Y;
 								_axisBackupState = CurrentTasMovie.GetAxisState(_axisEditRow, _axisEditColumn);
@@ -815,6 +816,8 @@ namespace BizHawk.Client.EmuHawk
 			}
 
 			MainForm.BlockFrameAdvance = false;
+
+			Cursor = Cursors.Default;
 		}
 
 		private void TasView_MouseUp(object sender, MouseEventArgs e)
