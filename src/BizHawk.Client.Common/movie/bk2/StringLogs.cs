3f352792a6dd70c879de22908a95d04fc54e99c9
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2022-11-22 09:18:51 -0800

    remove unused using from experiments in previous commit

--- src/BizHawk.Client.Common/movie/bk2/StringLogs.cs
+++ src/BizHawk.Client.Common/movie/bk2/StringLogs.cs
@@ -2,10 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Runtime.InteropServices;
 using System.Text;
 
-using BizHawk.BizInvoke;
 using BizHawk.Common;
 
 namespace BizHawk.Client.Common
