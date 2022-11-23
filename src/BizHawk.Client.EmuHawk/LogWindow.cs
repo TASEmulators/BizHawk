e269bfd49f1cbc32991a8b57cc7b2ba4f31322fc
adelikat <adelikat@tasvideos.org>
2022-11-23 10:54:59 -0600

    Log window - when copying pasting "MD5:2345" and Sha1, strip the md5 and sha1 out.  I just want the number if I'm copying pasting the single line.  If someone finds this objectionable, feel free to revert, but this savesme a lot of time

--- src/BizHawk.Client.EmuHawk/LogWindow.cs
+++ src/BizHawk.Client.EmuHawk/LogWindow.cs
@@ -152,7 +152,8 @@ namespace BizHawk.Client.EmuHawk
 			var sb = new StringBuilder();
 			lock(_lines)
 				foreach (int i in virtualListView1.SelectedIndices)
-					sb.AppendLine(_lines[i]);
+					sb.AppendLine(_lines[i].Replace("MD5:", "").Replace("SHA1:", ""));
+
 			if (sb.Length > 0)
 				Clipboard.SetText(sb.ToString(), TextDataFormat.Text);
 		}
