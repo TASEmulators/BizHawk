400b1b589b004f1b3bf2d59be63405ac027dc84c
YoshiRulz <OSSYoshiRulz@gmail.com>
2022-12-07 06:20:28 +1000

    Inline `RecordMovie.InitializeComponent` (Designer file)

--- src/BizHawk.Client.EmuHawk/movie/RecordMovie.cs
+++ src/BizHawk.Client.EmuHawk/movie/RecordMovie.cs
@@ -7,18 +7,33 @@ using System.Linq;
 using BizHawk.Emulation.Common;
 using BizHawk.Client.Common;
 using BizHawk.Common;
+using BizHawk.WinForms.Controls;
 
 namespace BizHawk.Client.EmuHawk
 {
 	// TODO - Allow relative paths in record TextBox
-	public partial class RecordMovie : Form, IDialogParent
+	public sealed class RecordMovie : Form, IDialogParent
 	{
+		private const string START_FROM_POWERON = "Power-On";
+
+		private const string START_FROM_SAVERAM = "SaveRam";
+
+		private const string START_FROM_SAVESTATE = "Now";
+
 		private readonly IMainFormForTools _mainForm;
 		private readonly Config _config;
 		private readonly GameInfo _game;
 		private readonly IEmulator _emulator;
 		private readonly IMovieSession _movieSession;
 		private readonly FirmwareManager _firmwareManager;
+
+		private readonly TextBox AuthorBox;
+
+		private readonly CheckBox DefaultAuthorCheckBox;
+
+		private readonly TextBox RecordBox;
+
+		private readonly ComboBox StartFromCombo;
 
 		public IDialogController DialogController => _mainForm;
 
@@ -38,9 +53,129 @@ namespace BizHawk.Client.EmuHawk
 			_emulator = core;
 			_movieSession = movieSession;
 			_firmwareManager = firmwareManager;
-			InitializeComponent();
-			Icon = Properties.Resources.TAStudioIcon;
+
+			Button Cancel = new();
+			Button OK = new();
+			Button BrowseBtn = new();
+			RecordBox = new();
+			StartFromCombo = new();
+			GroupBox groupBox1 = new();
+			DefaultAuthorCheckBox = new();
+			AuthorBox = new();
+			LocLabelEx label3 = new();
+			LocLabelEx label2 = new();
+			LocLabelEx label1 = new();
+			groupBox1.SuspendLayout();
+			SuspendLayout();
+
+			Cancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+			Cancel.DialogResult = DialogResult.Cancel;
+			Cancel.Location = new(391, 135);
+			Cancel.Name = "Cancel";
+			Cancel.Size = new(75, 23);
+			Cancel.TabIndex = 1;
+			Cancel.Text = "&Cancel";
+			Cancel.UseVisualStyleBackColor = true;
+			Cancel.Click += Cancel_Click;
+
+			OK.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+			OK.Location = new(310, 135);
+			OK.Name = "OK";
+			OK.Size = new(75, 23);
+			OK.TabIndex = 0;
+			OK.Text = "&OK";
+			OK.UseVisualStyleBackColor = true;
+			OK.Click += Ok_Click;
+
+			BrowseBtn.Anchor = AnchorStyles.Top | AnchorStyles.Right;
 			BrowseBtn.Image = Properties.Resources.OpenFile;
+			BrowseBtn.Location = new(423, 13);
+			BrowseBtn.Name = "BrowseBtn";
+			BrowseBtn.Size = new(25, 23);
+			BrowseBtn.TabIndex = 1;
+			BrowseBtn.UseVisualStyleBackColor = true;
+			BrowseBtn.Click += BrowseBtn_Click;
+
+			RecordBox.AllowDrop = true;
+			RecordBox.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+			RecordBox.Location = new(83, 13);
+			RecordBox.Name = "RecordBox";
+			RecordBox.Size = new(334, 20);
+			RecordBox.TabIndex = 0;
+			RecordBox.DragDrop += RecordBox_DragDrop;
+			RecordBox.DragEnter += RecordBox_DragEnter;
+
+			StartFromCombo.DropDownStyle = ComboBoxStyle.DropDownList;
+			StartFromCombo.FormattingEnabled = true;
+			StartFromCombo.Location = new(83, 65);
+			StartFromCombo.MaxDropDownItems = 32;
+			StartFromCombo.Name = "StartFromCombo";
+			StartFromCombo.Size = new(152, 21);
+			StartFromCombo.TabIndex = 3;
+			StartFromCombo.Items.Add(START_FROM_POWERON);
+			StartFromCombo.Items.Add(START_FROM_SAVESTATE);
+			StartFromCombo.Items.Add(START_FROM_SAVERAM);
+
+			groupBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+			groupBox1.Location = new(12, 12);
+			groupBox1.Name = "groupBox1";
+			groupBox1.Size = new(454, 112);
+			groupBox1.TabIndex = 0;
+			groupBox1.TabStop = false;
+			groupBox1.Controls.Add(DefaultAuthorCheckBox);
+			groupBox1.Controls.Add(AuthorBox);
+			groupBox1.Controls.Add(StartFromCombo);
+			groupBox1.Controls.Add(BrowseBtn);
+			groupBox1.Controls.Add(label3);
+			groupBox1.Controls.Add(label2);
+			groupBox1.Controls.Add(label1);
+			groupBox1.Controls.Add(RecordBox);
+
+			DefaultAuthorCheckBox.Anchor = AnchorStyles.Right;
+			DefaultAuthorCheckBox.AutoSize = true;
+			DefaultAuthorCheckBox.Location = new(327, 64);
+			DefaultAuthorCheckBox.Name = "DefaultAuthorCheckBox";
+			DefaultAuthorCheckBox.Size = new(121, 17);
+			DefaultAuthorCheckBox.TabIndex = 6;
+			DefaultAuthorCheckBox.Text = "Make default author";
+			DefaultAuthorCheckBox.UseVisualStyleBackColor = true;
+
+			AuthorBox.AllowDrop = true;
+			AuthorBox.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+			AuthorBox.Location = new(83, 39);
+			AuthorBox.Name = "AuthorBox";
+			AuthorBox.Size = new(365, 20);
+			AuthorBox.TabIndex = 2;
+
+			label3.Location = new(36, 41);
+			label3.Name = "label3";
+			label3.Text = "Author:";
+
+			label2.Location = new(6, 68);
+			label2.Name = "label2";
+			label2.Text = "Record From:";
+
+			label1.Location = new(51, 16);
+			label1.Name = "label1";
+			label1.Text = "File:";
+
+			AcceptButton = OK;
+			AutoScaleDimensions = new(6.0f, 13.0f);
+			AutoScaleMode = AutoScaleMode.Font;
+			CancelButton = Cancel;
+			ClientSize = new(478, 163);
+			Icon = Properties.Resources.TAStudioIcon;
+			MaximizeBox = false;
+			MaximumSize = new(1440, 201);
+			MinimizeBox = false;
+			MinimumSize = new(425, 201);
+			Name = "RecordMovie";
+			StartPosition = FormStartPosition.CenterParent;
+			Text = "Record Movie";
+			Controls.Add(groupBox1);
+			Controls.Add(OK);
+			Controls.Add(Cancel);
+			Load += RecordMovie_Load;
 			if (OSTailoredCode.IsUnixHost) Load += (_, _) =>
 			{
 				//HACK to make this usable on Linux. No clue why this Form in particular is so much worse, maybe the GroupBox? --yoshi
@@ -50,6 +185,10 @@ namespace BizHawk.Client.EmuHawk
 				OK.Location += s;
 				Cancel.Location += s;
 			};
+
+			groupBox1.ResumeLayout(performLayout: false);
+			groupBox1.PerformLayout();
+			ResumeLayout(performLayout: false);
 
 			if (!_emulator.HasSavestates())
 			{
@@ -119,7 +258,7 @@ namespace BizHawk.Client.EmuHawk
 					Directory.CreateDirectory(fileInfo.DirectoryName);
 				}
 
-				if (StartFromCombo.SelectedItem.ToString() == "Now" && _emulator.HasSavestates())
+				if (StartFromCombo.SelectedItem.ToString() is START_FROM_SAVESTATE && _emulator.HasSavestates())
 				{
 					var core = _emulator.AsStatable();
 
@@ -143,7 +282,7 @@ namespace BizHawk.Client.EmuHawk
 						movieToRecord.SavestateFramebuffer = (int[])_emulator.AsVideoProvider().GetVideoBuffer().Clone();
 					}
 				}
-				else if (StartFromCombo.SelectedItem.ToString() == "SaveRam"  && _emulator.HasSaveRam())
+				else if (StartFromCombo.SelectedItem.ToString() is START_FROM_SAVERAM && _emulator.HasSaveRam())
 				{
 					var core = _emulator.AsSaveRam();
 					movieToRecord.StartsFromSaveRam = true;
