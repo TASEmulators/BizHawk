7c3e35f3e739ba93c89c23567704759d12c0a5c8
YoshiRulz <OSSYoshiRulz@gmail.com>
2023-03-17 06:32:23 +1000

    Fix window title after loading rom via FW config (fixes #3581)

--- src/BizHawk.Client.EmuHawk/MainForm.cs
+++ src/BizHawk.Client.EmuHawk/MainForm.cs
@@ -3835,10 +3835,9 @@ namespace BizHawk.Client.EmuHawk
 					//path = ioa_openrom.Path;
 				}
 
-				var oldGame = Game;
 				var result = loader.LoadRom(path, nextComm, ioaRetro?.CorePath, forcedCoreName: MovieSession.QueuedCoreName);
 
-				Game = result ? loader.Game : oldGame;
+				if (result) Game = loader.Game;
 
 				// we need to replace the path in the OpenAdvanced with the canonical one the user chose.
 				// It can't be done until loader.LoadRom happens (for CanonicalFullPath)
