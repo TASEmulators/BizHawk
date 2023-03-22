1f6b06a3ed2d8bb10586aadf8a0eb379ea395be6
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2023-03-21 22:28:04 -0700

    kinda revert last commit apparently, git submodule does support --filter, except it for some reason requires a very new git, added in 2.36.0, which is newer than what Ubuntu 22.04 provides ):

--- waterbox/libcxx/setup-llvm.sh
+++ waterbox/libcxx/setup-llvm.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 set -e
 LLVM_TAG=llvmorg-16.0.0
+LLVM_DIRS="clang/lib/Headers cmake compiler-rt libunwind libcxx libcxxabi"
 LLVM_PATH=../llvm-project
 
 if [ -d "$LLVM_PATH" ]; then
 	if ! git -C "$LLVM_PATH" rev-parse $LLVM_TAG > /dev/null 2>&1; then
-		git submodule deinit -f "$LLVM_PATH"
 		rm -rf "$LLVM_PATH"
+		git submodule deinit -f "$LLVM_PATH"
 	fi
 fi
 
 if [ ! -d "$LLVM_PATH" ]; then
-	git submodule update --init --depth=1 --single-branch --progress "$LLVM_PATH"
+	git submodule init "$LLVM_PATH"
+	git clone --filter=tree:0 --sparse https://github.com/llvm/llvm-project.git "$LLVM_PATH"
+fi
+
+cd "$LLVM_PATH"
+if [ `git describe` != $LLVM_TAG ] || ! ls $LLVM_DIRS > /dev/null 2>&1; then
+	git checkout $LLVM_TAG && git sparse-checkout set $LLVM_DIRS
 fi
