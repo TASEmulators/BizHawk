3f37a414d88eba40bbb1a11fae37f9ec3a6a0c9c
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2023-03-25 19:54:29 -0700

    edit the llvm setup script a bit more so it more matches actual submodule behavior, also fix it when git directory is completely missing

--- waterbox/libcxx/setup-llvm.sh
+++ waterbox/libcxx/setup-llvm.sh
@@ -3,17 +3,19 @@ set -e
 LLVM_TAG=llvmorg-16.0.0
 LLVM_DIRS="clang/lib/Headers cmake compiler-rt libunwind libcxx libcxxabi"
 LLVM_PATH=../llvm-project
+LLVM_GIT_DIR=$(git rev-parse --absolute-git-dir)/modules/waterbox/llvm-project
 
 if [ -d "$LLVM_PATH" ]; then
-	if ! git -C "$LLVM_PATH" rev-parse $LLVM_TAG > /dev/null 2>&1; then
+	if [ ! -e "$LLVM_PATH/.git" ] || ! git -C "$LLVM_PATH" rev-parse $LLVM_TAG > /dev/null 2>&1; then
 		rm -rf "$LLVM_PATH"
 		git submodule deinit -f "$LLVM_PATH"
+		rm -rf "$LLVM_GIT_DIR"
 	fi
 fi
 
 if [ ! -d "$LLVM_PATH" ]; then
 	git submodule init "$LLVM_PATH"
-	git clone --filter=tree:0 --sparse https://github.com/llvm/llvm-project.git "$LLVM_PATH"
+	git clone --separate-git-dir="$LLVM_GIT_DIR" --filter=tree:0 --sparse https://github.com/llvm/llvm-project.git "$LLVM_PATH"
 fi
 
 cd "$LLVM_PATH"
