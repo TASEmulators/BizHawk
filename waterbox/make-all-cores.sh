9c872b9fca417bbd78f7f6dc8c2ae1674e4891df
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2023-03-26 16:56:46 -0700

    fix ordering in make-all-cores script too

--- waterbox/make-all-cores.sh
+++ waterbox/make-all-cores.sh
@@ -11,8 +11,8 @@ cd melon && make -f Makefile $1 -j && cd -
 cd picodrive && make -f Makefile $1 -j && cd -
 cd snes9x && make -f Makefile $1 -j && cd -
 cd tic80 && make -f Makefile $1 -j && cd -
-cd virtualjaguar && make -f Makefile $1 -j && cd -
 cd uzem && make -f Makefile $1 -j && cd -
+cd virtualjaguar && make -f Makefile $1 -j && cd -
 cd nyma && ./make-all-released-cores.sh $1 && cd -
 
 # this won't include MAME by default, due to the large amount of time it takes to build it
