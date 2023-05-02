efd1f5d2c38b98829ba48cfd66cfb7cc2a09f623
vadosnaprimer <feykomylce@gmail.com>
2023-05-02 19:14:22 +0300

    are we that quick? let's find out

--- src/BizHawk.Common/VersionInfo.cs
+++ src/BizHawk.Common/VersionInfo.cs
@@ -13,7 +13,7 @@ namespace BizHawk.Common
 		/// </remarks>
 		public static readonly string MainVersion = "2.9.1";
 
-		public static readonly string ReleaseDate = "April 7, 2023";
+		public static readonly string ReleaseDate = "May 2, 2023";
 
 		public static readonly string HomePage = "https://tasvideos.org/BizHawk";
 
