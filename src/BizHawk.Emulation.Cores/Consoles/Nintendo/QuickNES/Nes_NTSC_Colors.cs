3d5b068c2f89d27cd99082f58b9fb1fb0bc7e741
nattthebear <goyuken@gmail.com>
2023-05-26 07:01:56 -0400

    QuickNES: Slightly tweak palette rounding computation
    Fixes #3668

--- src/BizHawk.Emulation.Cores/Consoles/Nintendo/QuickNES/Nes_NTSC_Colors.cs
+++ src/BizHawk.Emulation.Cores/Consoles/Nintendo/QuickNES/Nes_NTSC_Colors.cs
@@ -88,9 +88,9 @@ namespace BizHawk.Emulation.Cores.Consoles.Nintendo.QuickNES
 			g = Math.Min(1.0f, Math.Max(0.0f, g));
 			b = Math.Min(1.0f, Math.Max(0.0f, b));
 
-			outp[0] = (byte)(r * 255);
-			outp[1] = (byte)(g * 255);
-			outp[2] = (byte)(b * 255);
+			outp[0] = (byte)Math.Round(r * 255);
+			outp[1] = (byte)Math.Round(g * 255);
+			outp[2] = (byte)Math.Round(b * 255);
 		}
 	}
 }
