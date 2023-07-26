f55d2c2b8cac57adc455e23701187ec602869bdd
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2023-07-25 19:14:24 -0700

    Swap System.Data.SQLite with Microsoft.Data.Sqlite
    Apparently this is supposed to be a kind of successor? https://learn.microsoft.com/en-us/dotnet/standard/data/sqlite/compare

--- src/BizHawk.Client.Common/Api/Classes/SQLiteApi.cs
+++ src/BizHawk.Client.Common/Api/Classes/SQLiteApi.cs
@@ -1,23 +1,26 @@
-﻿using System.Collections.Generic;
-using System.Data;
-using System.Data.SQLite;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Microsoft.Data.Sqlite;
 
 namespace BizHawk.Client.Common
 {
 	public sealed class SQLiteApi : ISQLiteApi
 	{
-		private SQLiteConnection _dbConnection;
+		private SqliteConnection _dbConnection;
 
 		public string CreateDatabase(string name)
 		{
 			try
 			{
-				SQLiteConnection.CreateFile(name);
+				File.Create(name).Dispose();
 			}
-			catch (SQLiteException sqlEx)
+			catch (Exception ex)
 			{
-				return sqlEx.Message;
+				return ex.Message;
 			}
+
 			return "Database Created Successfully";
 		}
 
@@ -25,18 +28,18 @@ namespace BizHawk.Client.Common
 		{
 			try
 			{
-				_dbConnection = new SQLiteConnection(
-					new SQLiteConnectionStringBuilder {
-						DataSource = name,
-						Version = 3,
-						JournalMode = SQLiteJournalModeEnum.Wal, // Allows for reads and writes to happen at the same time
-						DefaultIsolationLevel = IsolationLevel.ReadCommitted, // This only helps make the database lock left. May be pointless now
-						SyncMode = SynchronizationModes.Off // This shortens the delay for do synchronous calls.
-					}.ToString()
-				);
+				_dbConnection?.Dispose();
+				_dbConnection = new(new SqliteConnectionStringBuilder { DataSource = name }.ToString());
 				_dbConnection.Open();
+				using var initCmds = new SqliteCommand(null, _dbConnection);
+				// Allows for reads and writes to happen at the same time
+				initCmds.CommandText = "PRAGMA journal_mode = 'wal'";
+				initCmds.ExecuteNonQuery();
+				// This shortens the delay for do synchronous calls
+				initCmds.CommandText = "PRAGMA synchronous = 'off'";
+				initCmds.ExecuteNonQuery();
 			}
-			catch (SQLiteException sqlEx)
+			catch (SqliteException sqlEx)
 			{
 				return sqlEx.Message;
 			}
@@ -52,10 +55,11 @@ namespace BizHawk.Client.Common
 			try
 			{
 				_dbConnection.Open();
-				new SQLiteCommand(query, _dbConnection).ExecuteNonQuery();
+				using var cmd = new SqliteCommand(query, _dbConnection);
+				cmd.ExecuteNonQuery();
 				result = "Command ran successfully";
 			}
-			catch (SQLiteException sqlEx)
+			catch (SqliteException sqlEx)
 			{
 				result = sqlEx.Message;
 			}
@@ -71,7 +75,7 @@ namespace BizHawk.Client.Common
 			try
 			{
 				_dbConnection.Open();
-				using var command = new SQLiteCommand($"PRAGMA read_uncommitted =1;{query}", _dbConnection);
+				using var command = new SqliteCommand($"PRAGMA read_uncommitted =1;{query}", _dbConnection);
 				using var reader = command.ExecuteReader();
 				if (reader.HasRows)
 				{
@@ -92,7 +96,7 @@ namespace BizHawk.Client.Common
 					result = "No rows found";
 				}
 			}
-			catch (SQLiteException sqlEx)
+			catch (SqliteException sqlEx)
 			{
 				result = sqlEx.Message;
 			}
