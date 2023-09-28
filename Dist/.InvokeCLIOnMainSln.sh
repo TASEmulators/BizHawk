15d663f68da12cf6bce188cd4cb5440ebc42fc81
James Groom <OSSYoshiRulz@gmail.com>
2023-09-29 04:49:18 +1000

    Silence "not found" error when building w/o Git
    fixes 096066150

--- Dist/.InvokeCLIOnMainSln.sh
+++ Dist/.InvokeCLIOnMainSln.sh
@@ -11,5 +11,5 @@ shift
 if [ -z "$NUGET_PACKAGES" ]; then export NUGET_PACKAGES="$HOME/.nuget/packages"; fi
 printf "running 'dotnet %s' in %s configuration, extra args: %s\n" "$cmd" "$config" "$*"
 version=$(grep -Po "MainVersion = \"\K(.*)(?=\")" src/BizHawk.Common/VersionInfo.cs)
-git_hash="$(git rev-parse --verify HEAD || printf "0000000000000000000000000000000000000000")"
+git_hash="$(git rev-parse --verify HEAD 2>/dev/null || printf "0000000000000000000000000000000000000000")"
 dotnet "$cmd" BizHawk.sln -c "$config" -m -clp:NoSummary -p:Version="$version" -p:SourceRevisionId="$git_hash" "$@"
