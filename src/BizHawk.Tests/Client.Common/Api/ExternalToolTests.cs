TASEmulators/BizHawk

commit 0c8ce9356f54bfde316520f01888457c66060304

Update GuiApi so that TestDrawingWithTwoScripts passes. This fixes the bug where if two lua scripts draw stuff on the same frame only one script's drawings are visible. Replaces a bunch of obscure ThisIsTheLuaHack and locking/unlocking code with a simple pair of methods.

src/BizHawk.Client.Common/Api/Classes/GuiApi.cs
@@ -53,14 +53,19 @@ namespace BizHawk.Client.Common
 
 		private DisplaySurfaceID? _usingSurfaceID = null;
 
-		public bool EnableLuaAutolockHack = false;
-
 		public bool HasGUISurface => _GUISurface != null;
+
+		private bool _frameStarted = false;
 
 		public GuiApi(Action<string> logCallback, DisplayManagerBase displayManager)
 		{
 			LogCallback = logCallback;
 			_displayManager = displayManager;
+
+			// These will set the drawing surfaces and display them.
+			// This allows drawing and seeing results without any actual frames occurring.
+			BeginFrame();
+			EndFrame();
 		}
 
 		private SolidBrush GetBrush(Color color)
@@ -85,68 +90,19 @@ namespace BizHawk.Client.Common
 		private IDisplaySurface GetRelevantSurface(DisplaySurfaceID? surfaceID)
 		{
 			var nnID = surfaceID ?? _usingSurfaceID ?? throw new Exception();
-			void ThisIsTheLuaAutolockHack()
-			{
-				try
-				{
-					UnlockSurface(nnID);
-					LockSurface(nnID);
-				}
-				catch (InvalidOperationException ex)
-				{
-					LogCallback(ex.ToString());
-				}
-			}
 			switch (nnID)
 			{
 				case DisplaySurfaceID.EmuCore:
-					if (_GUISurface == null && EnableLuaAutolockHack) ThisIsTheLuaAutolockHack();
 					return _GUISurface;
 				case DisplaySurfaceID.Client:
-					if (_clientSurface == null && EnableLuaAutolockHack) ThisIsTheLuaAutolockHack();
 					return _clientSurface;
 				default:
 					throw new Exception();
 			}
 		}
 
-		private void LockSurface(DisplaySurfaceID surfaceID)
-		{
-			switch (surfaceID)
-			{
-				case DisplaySurfaceID.EmuCore:
-					if (_GUISurface != null) throw new InvalidOperationException("attempt to lock surface without unlocking previous");
-					_GUISurface = _displayManager.LockApiHawkSurface(surfaceID, clear: true);
-					break;
-				case DisplaySurfaceID.Client:
-					if (_clientSurface != null) throw new InvalidOperationException("attempt to lock surface without unlocking previous");
-					_clientSurface = _displayManager.LockApiHawkSurface(surfaceID, clear: true);
-					break;
-				default:
-					throw new ArgumentException(message: "not a valid enum member", paramName: nameof(surfaceID));
-			}
-		}
-
-		private void UnlockSurface(DisplaySurfaceID surfaceID)
-		{
-			switch (surfaceID)
-			{
-				case DisplaySurfaceID.EmuCore:
-					if (_GUISurface != null) _displayManager.UnlockApiHawkSurface(_GUISurface);
-					_GUISurface = null;
-					break;
-				case DisplaySurfaceID.Client:
-					if (_clientSurface != null) _displayManager.UnlockApiHawkSurface(_clientSurface);
-					_clientSurface = null;
-					break;
-				default:
-					throw new ArgumentException(message: "not a valid enum member", paramName: nameof(surfaceID));
-			}
-		}
-
 		public void WithSurface(DisplaySurfaceID surfaceID, Action drawingCallsFunc)
 		{
-			LockSurface(surfaceID);
 			_usingSurfaceID = surfaceID;
 			try
 			{
@@ -155,26 +111,35 @@ namespace BizHawk.Client.Common
 			finally
 			{
 				_usingSurfaceID = null;
-				UnlockSurface(surfaceID);
 			}
 		}
 
-		public readonly ref struct LuaAutoUnlockHack
+		/// <summary>
+		/// Starts drawing on new surfaces and clears them, but does not display the new surfaces.
+		/// Use this with EndFrame for double-buffered display (avoid flickering during drawing operations)
+		/// </summary>
+		public void BeginFrame()
 		{
-			private readonly GuiApi _guiApi;
-
-			internal LuaAutoUnlockHack(GuiApi guiApi)
-				=> _guiApi = guiApi;
-
-			public void Dispose()
+			// I think the "Lock" stuff is mis-named. All it actually does it track what's been "locked" and use that as the current surface. (and disallow re-locking or re-unlocking)
+			// Anyway, calling LockSurface will return a cleared surface we can draw on without displaying it. --SuuperW
+			_GUISurface = _displayManager.LockApiHawkSurface(DisplaySurfaceID.EmuCore, true);
+			_clientSurface = _displayManager.LockApiHawkSurface(DisplaySurfaceID.Client, true);
+			_frameStarted = true;
+		}
+		/// <summary>
+		/// Displays the current drawing surfaces.
+		/// More drawing can still happen on these surfaces until BeginFrame is called.
+		/// </summary>
+		public void EndFrame()
+		{
+			if (_frameStarted)
 			{
-				_guiApi.UnlockSurface(DisplaySurfaceID.EmuCore);
-				_guiApi.UnlockSurface(DisplaySurfaceID.Client);
+				_displayManager.UnlockApiHawkSurface(_GUISurface);
+				_displayManager.UnlockApiHawkSurface(_clientSurface);
+				_frameStarted = false;
 			}
 		}
 
-		public LuaAutoUnlockHack ThisIsTheLuaAutoUnlockHack()
-			=> new(this);
 
 		public void DrawNew(string name, bool clear)
 		{
@@ -638,8 +603,7 @@ namespace BizHawk.Client.Common
 
 		public void Dispose()
 		{
-			UnlockSurface(DisplaySurfaceID.EmuCore);
-			UnlockSurface(DisplaySurfaceID.Client);
+			EndFrame();
 			foreach (var brush in _solidBrushes.Values) brush.Dispose();
 			foreach (var brush in _pens.Values) brush.Dispose();
 		}

src/BizHawk.Tests/Client.Common/lua/TestLuaDrawing.cs
@@ -71,7 +71,7 @@ namespace BizHawk.Tests.Client.Common.Lua
 			var buffer = displayManager.RenderOffscreenLua(vp);
 
 			Assert.AreEqual(buffer.GetPixel(2, 2), Color.Red.ToArgb());
-			Assert.AreEqual(buffer.GetPixel(2, 4), 0xff00ff00);
+			Assert.AreEqual((uint)buffer.GetPixel(2, 4), 0xff00ff00);
 		}
 
 	}

src/BizHawk.Client.Common/Api/ApiManager.cs
@@ -100,7 +100,6 @@ namespace BizHawk.Client.Common
 		{
 			_luaContainer?.Dispose();
 			_luaContainer = Register(serviceProvider, logCallback, mainForm, displayManager, inputManager, movieSession, toolManager, config, emulator, game);
-			((GuiApi) _luaContainer.Gui).EnableLuaAutolockHack = true;
 			return _luaContainer;
 		}
 	}

src/BizHawk.Client.Common/lua/LuaLibrariesBase.cs
@@ -169,8 +169,6 @@ namespace BizHawk.Client.Common
 
 		public void CallSaveStateEvent(string name)
 		{
-			using var luaAutoUnlockHack = GuiAPI.ThisIsTheLuaAutoUnlockHack();
-
 			try
 			{
 				foreach (var lf in RegisteredFunctions.Where(static l => l.Event == NamedLuaFunction.EVENT_TYPE_SAVESTATE).ToList())
@@ -186,7 +184,7 @@ namespace BizHawk.Client.Common
 
 		public void CallLoadStateEvent(string name)
 		{
-			using var luaAutoUnlockHack = GuiAPI.ThisIsTheLuaAutoUnlockHack();
+			GuiAPI.BeginFrame();
 
 			try
 			{
@@ -199,13 +197,15 @@ namespace BizHawk.Client.Common
 			{
 				LogToLuaConsole($"error running function attached by lua function event.onloadstate\nError message: {e.Message}");
 			}
+
+			GuiAPI.EndFrame();
 		}
 
 		public void CallFrameBeforeEvent()
 		{
 			if (IsUpdateSupressed) return;
 
-			using var luaAutoUnlockHack = GuiAPI.ThisIsTheLuaAutoUnlockHack();
+			GuiAPI.BeginFrame();
 
 			try
 			{
@@ -224,8 +224,6 @@ namespace BizHawk.Client.Common
 		{
 			if (IsUpdateSupressed) return;
 
-			using var luaAutoUnlockHack = GuiAPI.ThisIsTheLuaAutoUnlockHack();
-
 			try
 			{
 				foreach (var lf in RegisteredFunctions.Where(static l => l.Event == NamedLuaFunction.EVENT_TYPE_POSTFRAME).ToList())
@@ -237,12 +235,12 @@ namespace BizHawk.Client.Common
 			{
 				LogToLuaConsole($"error running function attached by lua function event.onframeend\nError message: {e.Message}");
 			}
+
+			GuiAPI.EndFrame();
 		}
 
 		public void CallExitEvent(LuaFile lf)
 		{
-			using var luaAutoUnlockHack = GuiAPI.ThisIsTheLuaAutoUnlockHack();
-
 			foreach (var exitCallback in RegisteredFunctions
 				.Where(l => l.Event == NamedLuaFunction.EVENT_TYPE_ENGINESTOP
 					&& (l.LuaFile.Path == lf.Path || ReferenceEquals(l.LuaFile.Thread, lf.Thread)))
@@ -312,7 +310,6 @@ namespace BizHawk.Client.Common
 		public (bool WaitForFrame, bool Terminated) ResumeScript(LuaFile lf)
 		{
 			_currThread = lf.Thread;
-			using var luaAutoUnlockHack = GuiAPI.ThisIsTheLuaAutoUnlockHack();
 
 			try
 			{

commit 8bd025d12718e366ebb15cd06245e4fc8c26bb71

This addresses the regression from commit 0c8ce935 that made external tools unable to draw. This removes the old obsolete methods IGuiApi.DrawNew and DrawFinish (they didn't do anything; any scripts using them already didn't work).

src/BizHawk.Client.Common/Api/Classes/GuiApi.cs
@@ -55,8 +55,6 @@ namespace BizHawk.Client.Common
 
 		public bool HasGUISurface => _GUISurface != null;
 
-		private bool _frameStarted = false;
-
 		public GuiApi(Action<string> logCallback, DisplayManagerBase displayManager)
 		{
 			LogCallback = logCallback;
@@ -103,7 +101,9 @@ namespace BizHawk.Client.Common
 
 		public void WithSurface(DisplaySurfaceID surfaceID, Action drawingCallsFunc)
 		{
+			BeginFrame();
 			_usingSurfaceID = surfaceID;
+
 			try
 			{
 				drawingCallsFunc();
@@ -111,52 +111,60 @@ namespace BizHawk.Client.Common
 			finally
 			{
 				_usingSurfaceID = null;
+				EndFrame();
 			}
+		}
+
+		private IDisplaySurface LockSurface(DisplaySurfaceID surface)
+		{
+			// I think the "Lock" stuff is mis-named. All it actually does it track what's been "locked" and use that as the current surface. (and disallow re-locking or re-unlocking)
+			// Anyway, calling LockSurface will return a cleared surface we can draw on without displaying it. --SuuperW
+
+			// There might be multiple API users at once and therefore multiple GuiApi instances.
+			// In that case, we don't want to get a new surface. We want the current one.
+			var locked = _displayManager.PeekApiHawkLockedSurface(surface);
+			if (locked != null)
+			{
+				// Someone else has locked surfaces. Use those.
+				return locked;
+			}
+
+			IDisplaySurface current = surface == DisplaySurfaceID.EmuCore ? _GUISurface : _clientSurface;
+			if (current != _displayManager.GetCurrentSurface(surface))
+				return _displayManager.GetCurrentSurface(surface);
+			else
+				return _displayManager.LockApiHawkSurface(surface, true);
+		}
+		private void UnlockSurface(DisplaySurfaceID surface)
+		{
+			IDisplaySurface current = surface == DisplaySurfaceID.EmuCore ? _GUISurface : _clientSurface;
+			if (current != null && _displayManager.PeekApiHawkLockedSurface(surface) == current)
+				_displayManager.UnlockApiHawkSurface(current);
 		}
 
 		/// <summary>
 		/// Starts drawing on new surfaces and clears them, but does not display the new surfaces.
 		/// Use this with EndFrame for double-buffered display (avoid flickering during drawing operations)
+		/// This method is safe to call multiple times.
+		/// 
+		/// This is how the LuaConsole can manage drawing in a way that supports having multiple scripts. (WithSurface does not support that)
+		/// This method and WithSurface will probably be replaced with an event-based method at some point.
 		/// </summary>
 		public void BeginFrame()
 		{
-			// I think the "Lock" stuff is mis-named. All it actually does it track what's been "locked" and use that as the current surface. (and disallow re-locking or re-unlocking)
-			// Anyway, calling LockSurface will return a cleared surface we can draw on without displaying it. --SuuperW
-			_GUISurface = _displayManager.LockApiHawkSurface(DisplaySurfaceID.EmuCore, true);
-			_clientSurface = _displayManager.LockApiHawkSurface(DisplaySurfaceID.Client, true);
-			_frameStarted = true;
+			_GUISurface = LockSurface(DisplaySurfaceID.EmuCore);
+			_clientSurface = LockSurface(DisplaySurfaceID.Client);
 		}
 		/// <summary>
 		/// Displays the current drawing surfaces.
 		/// More drawing can still happen on these surfaces until BeginFrame is called.
+		/// This method is safe to call multiple times.
 		/// </summary>
 		public void EndFrame()
 		{
-			if (_frameStarted)
-			{
-				_displayManager.UnlockApiHawkSurface(_GUISurface);
-				_displayManager.UnlockApiHawkSurface(_clientSurface);
-				_frameStarted = false;
-			}
+			UnlockSurface(DisplaySurfaceID.EmuCore);
+			UnlockSurface(DisplaySurfaceID.Client);
 		}
-
-
-		public void DrawNew(string name, bool clear)
-		{
-			switch (name)
-			{
-				case null:
-				case "emu":
-					LogCallback("the `DrawNew(\"emu\")` function has been deprecated");
-					return;
-				case "native":
-					throw new InvalidOperationException("the ability to draw in the margins with `DrawNew(\"native\")` has been removed");
-				default:
-					throw new InvalidOperationException("invalid surface name");
-			}
-		}
-
-		public void DrawFinish() => LogCallback("the `DrawFinish()` function has been deprecated");
 
 		public void SetPadding(int all) => _padding = (all, all, all, all);
 

src/BizHawk.Client.Common/Api/Interfaces/IGuiApi.cs
@@ -10,13 +10,12 @@ namespace BizHawk.Client.Common
 		ImageAttributes GetAttributes();
 		void SetAttributes(ImageAttributes a);
 
+		/// <summary>
+		/// This is the intended way for external tools to draw. All drawing calls (per surfaceID) should be done in a single call to WithSurface.
+		/// Each call will clear the given surface before calling the drawingCallsFunc.
+		/// This method will probably be replaced with an event-based method at some point.
+		/// </summary>
 		void WithSurface(DisplaySurfaceID surfaceID, Action drawingCallsFunc);
-
-		[Obsolete]
-		void DrawNew(string name, bool clear = true);
-
-		[Obsolete]
-		void DrawFinish();
 
 		[Obsolete]
 		bool HasGUISurface { get; }

src/BizHawk.Client.Common/DisplayManager/DisplayManagerBase.cs
@@ -979,6 +979,9 @@ namespace BizHawk.Client.Common
 		public IDisplaySurface PeekApiHawkLockedSurface(DisplaySurfaceID surfaceID)
 			=> _apiHawkIDToSurface.TryGetValue(surfaceID, out var surface) ? surface : null;
 
+		public IDisplaySurface GetCurrentSurface(DisplaySurfaceID surfaceID)
+			=> _apiHawkSurfaceSets[surfaceID].GetCurrent();
+
 		public IDisplaySurface LockApiHawkSurface(DisplaySurfaceID surfaceID, bool clear)
 		{
 			if (_apiHawkIDToSurface.ContainsKey(surfaceID))

src/BizHawk.Client.Common/lua/CommonLibs/GuiLuaLibrary.cs
@@ -20,18 +20,6 @@ namespace BizHawk.Client.Common
 		private DisplaySurfaceID UseOrFallback(string surfaceName)
 			=> DisplaySurfaceIDParser.Parse(surfaceName) ?? _rememberedSurfaceID;
 
-#pragma warning disable CS0612
-		[LuaDeprecatedMethod]
-		[LuaMethod("DrawNew", "Changes drawing target to the specified lua surface name. This may clobber any previous drawing to this surface (pass false if you don't want it to)")]
-		public void DrawNew(string name, bool? clear = true)
-			=> APIs.Gui.DrawNew(name, clear ?? true);
-
-		[LuaDeprecatedMethod]
-		[LuaMethod("DrawFinish", "Finishes drawing to the current lua surface and causes it to get displayed.")]
-		public void DrawFinish()
-			=> APIs.Gui.DrawFinish();
-#pragma warning restore CS0612
-
 		[LuaMethodExample("gui.addmessage( \"Some message\" );")]
 		[LuaMethod("addmessage", "Adds a message to the OSD's message area")]
 		public void AddMessage(string message)

src/BizHawk.Tests/Client.Common/lua/TestLuaDrawing.cs
@@ -14,9 +14,25 @@ namespace BizHawk.Tests.Client.Common.Lua
 	{
 #pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
 		// null values are initialized in the setup method
-		private ILuaLibraries luaLibraries = null;
+		internal ILuaLibraries luaLibraries = null;
 		private DisplayManagerBase displayManager = null;
 #pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+
+		public TestLuaDrawing() { }
+
+		internal TestLuaDrawing(IMainFormForApi mainForm, Config config, DisplayManagerBase displayManager)
+		{
+			this.displayManager = displayManager;
+
+			IGameInfo gameInfo = new GameInfo();
+			luaLibraries = new TestLuaLibraries(
+				mainForm,
+				displayManager,
+				config,
+				gameInfo
+			);
+			luaLibraries.Restart(config, gameInfo);
+		}
 
 		private const string pathToTestLuaScripts = "Client.Common/lua/LuaScripts";
 

src/BizHawk.Tests/Client.Common/Api/ExternalToolTests.cs
@@ -1,10 +1,15 @@
-﻿using System.IO;
+﻿using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Reflection;
+
+using BizHawk.Bizware.BizwareGL;
 using BizHawk.Client.Common;
 using BizHawk.Emulation.Common;
+using BizHawk.Tests.Client.Common.Lua;
 using BizHawk.Tests.Implementations;
 using BizHawk.Tests.Mocks;
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BizHawk.Tests.Client.Common.Api
@@ -70,6 +75,45 @@ namespace BizHawk.Tests.Client.Common.Api
 
 			TestExternalAPI externalApi = LoadExternalTool(toolManager);
 			Assert.AreEqual(mainFormApi.Emulator.AsVideoProviderOrDefault().BufferHeight, externalApi.APIs.EmuClient.BufferHeight());
+		}
+
+		[TestMethod]
+		public void TestExternalToolCanDraw()
+		{
+			IMainFormForApi mainFormApi = new MockMainFormForApi(new NullEmulator());
+			DisplayManagerBase displayManager = new TestDisplayManager(mainFormApi.Emulator);
+			TestToolManager toolManager = new TestToolManager(mainFormApi, config, displayManager);
+
+			TestExternalAPI externalApi = LoadExternalTool(toolManager);
+			IGuiApi guiApi = externalApi.APIs.Gui;
+
+			guiApi.DrawPixel(2, 2, Color.Blue, DisplaySurfaceID.Client);
+			BitmapBufferVideoProvider vp = new BitmapBufferVideoProvider(new BitmapBuffer(8, 8));
+			var buffer = displayManager.RenderOffscreenLua(vp);
+			Assert.AreEqual(buffer.GetPixel(2, 2), Color.Blue.ToArgb());
+		}
+
+		[TestMethod]
+		public void TestExternalToolCanDrawWhileLuaIsRunning()
+		{
+			IMainFormForApi mainFormApi = new MockMainFormForApi(new NullEmulator());
+			DisplayManagerBase displayManager = new TestDisplayManager(mainFormApi.Emulator);
+			TestToolManager toolManager = new TestToolManager(mainFormApi, config, displayManager);
+
+			TestExternalAPI externalApi = LoadExternalTool(toolManager);
+			IGuiApi guiApi = externalApi.APIs.Gui;
+			TestLuaDrawing luaTester = new TestLuaDrawing(mainFormApi, config, displayManager); // not the default constructor; we need to share a DisplayManager at least
+
+			// trigger some frames with both to ensure we can cycle through buffers
+			guiApi.WithSurface(DisplaySurfaceID.Client, () => { });
+			luaTester.luaLibraries.CallFrameBeforeEvent();
+			luaTester.luaLibraries.CallFrameAfterEvent();
+
+			// draw
+			guiApi.DrawPixel(2, 2, Color.Blue, DisplaySurfaceID.Client);
+			BitmapBufferVideoProvider vp = new BitmapBufferVideoProvider(new BitmapBuffer(8, 8));
+			var buffer = displayManager.RenderOffscreenLua(vp);
+			Assert.AreEqual(Color.Blue.ToArgb(), buffer.GetPixel(2, 2));
 		}
 	}
 }

src/BizHawk.Tests/Client.Common/Api/GuiApiTests.cs
@@ -3,7 +3,6 @@
 using BizHawk.Bizware.BizwareGL;
 using BizHawk.Client.Common;
 using BizHawk.Emulation.Common;
-
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BizHawk.Tests.Client.Common.Api
@@ -79,5 +78,20 @@ namespace BizHawk.Tests.Client.Common.Api
 			buffer = displayManager.RenderOffscreenLua(vp);
 			Assert.AreEqual(buffer.GetPixel(2, 2), Color.Red.ToArgb());
 		}
+
+		[TestMethod]
+		public void TestWithSurfaceClearsSurface()
+		{
+			BitmapBufferVideoProvider vp = new BitmapBufferVideoProvider(new BitmapBuffer(8, 8));
+
+			guiApi.WithSurface(DisplaySurfaceID.Client, () => guiApi.DrawPixel(2, 2, Color.Blue));
+			var buffer = displayManager.RenderOffscreenLua(vp);
+			Assert.AreEqual(Color.Blue.ToArgb(), buffer.GetPixel(2, 2));
+
+			guiApi.WithSurface(DisplaySurfaceID.Client, () => { });
+			buffer = displayManager.RenderOffscreenLua(vp);
+			Assert.AreEqual(Color.Black.ToArgb(), buffer.GetPixel(2, 2));
+		}
+
 	}
 }