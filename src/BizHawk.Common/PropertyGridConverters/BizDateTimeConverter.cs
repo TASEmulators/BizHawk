282417c9afe662c936e0e609e3de74bfa168fcbb
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2023-11-08 11:58:38 -0800

    pass DateTimeFormatInfo.CurrentInfo here instead of suppressing the warning fixes 5ae15c8

--- src/BizHawk.Common/PropertyGridConverters/BizDateTimeConverter.cs
+++ src/BizHawk.Common/PropertyGridConverters/BizDateTimeConverter.cs
@@ -9,10 +9,8 @@ namespace BizHawk.Common
 		public override object ConvertTo(ITypeDescriptorContext? context, CultureInfo? culture, object? value, Type destinationType)
 		{
 			if (value is not DateTime d || destinationType != typeof(string)) throw new NotSupportedException("can only do DateTime --> string");
-#pragma warning disable CA1305
 			// this function is explicitly supposed to be locale dependent
-			return d.ToString();
-#pragma warning restore CA1305
+			return d.ToString(DateTimeFormatInfo.CurrentInfo);
 		}
 	}
 }
