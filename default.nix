cbd0bc8d0b42a754c809d8daa88809ef4c3ad7e3
James Groom <OSSYoshiRulz+GitHub@gmail.com>
2023-12-02 18:40:47 +0000

    Nix expr: Bump Nixpkgs to 23.11

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/23.05.tar.gz") {}
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/23.11.tar.gz") {}
 , lib ? pkgs.lib
 , stdenv ? pkgs.stdenvNoCC
 # infrastructure
