10ac5ddb3cb3f99a2672b72a0d8dffccb474211a
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2023-12-04 17:06:27 -0800

    rebuild citra on debian 11, remove dumb libc++ rundep
    minor changes were needed on the c++ side (luckily it was only the bizhawk specific code which needed changes)
    limits.h force include is a hack due to one of citra's externals relying on some implicit std header including which doesn't happen on gcc10's libstdc++

--- citra/build_release.sh
+++ citra/build_release.sh
@@ -3,7 +3,10 @@ set -e
 if [ -z "$CC" ]; then export CC="clang"; fi
 if [ -z "$CXX" ]; then export CXX="clang++"; fi
 
-# libc++ must be used currently as Citra fails to build with stdlibc++ due to a stdlibc++ bug
+# stdlibc++ has a bug in some versions (at least in Ubuntu 22.04, probably Debian 12 too) that will cause compilation to fail for Citra
+# Debian 11's stdlibc++ is unaffected (and probably Ubuntu 20.04's too)
+# Note that Debian 10's stdlibc++ is too old (nearly no c++20 support), so at least Debian 11 must be used to compile this core
+# At least cmake 3.20 must be present too, so get cmake from bullseye-backports
 
 rm -rf build
 mkdir build
@@ -14,5 +17,5 @@ cmake ../citra -DENABLE_SDL2=OFF -DUSE_SYSTEM_SDL2=OFF -DENABLE_QT=OFF -DENABLE_
  -DENABLE_LIBUSB=OFF -DUSE_DISCORD_PRESENCE=OFF -DUSE_SYSTEM_BOOST=OFF -DUSE_SYSTEM_OPENSSL=OFF -DUSE_SYSTEM_LIBUSB=OFF \
  -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=OFF -DENABLE_HEADLESS=ON -DCMAKE_PROGRAM_PATH=$GLSLANG_VALIDATOR_DIR \
  -DCMAKE_BUILD_TYPE=Release -DENABLE_LTO=ON -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX \
- -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_CXX_FLAGS="-stdlib=libc++ -Wno-deprecated" -G Ninja
+ -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_CXX_FLAGS="-Wno-deprecated -include limits.h" -G Ninja
 ninja
