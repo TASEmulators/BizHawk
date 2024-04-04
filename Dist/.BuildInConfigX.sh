TASEmulators/BizHawk

commit 7e8623fddab6139d1180b1cf40f086fc15037a3d

Retain `BizHawk.Client.` prefix in executables' assembly names

src/MainSlnExecutable.props
@@ -2,7 +2,7 @@
 	<Import Project="MainSlnCommon.props" />
 	<PropertyGroup>
 		<OutputPath>$(MSBuildProjectDirectory)/../../output</OutputPath>
-		<AssemblyName>$(MSBuildProjectName.Substring($([MSBuild]::Add($(MSBuildProjectName.LastIndexOf('.')), 1))))</AssemblyName>
+		<ProjectNameTail>$(MSBuildProjectName.Substring($([MSBuild]::Add($(MSBuildProjectName.LastIndexOf('.')), 1))))</ProjectNameTail>
 	</PropertyGroup>
 	<PropertyGroup Condition=" '$(Configuration)' == 'Debug' ">
 		<OutputType>Exe</OutputType>
@@ -12,11 +12,13 @@
 	</PropertyGroup>
 	<Target Name="PostBuild" AfterTargets="PostBuildEvent">
 		<ItemGroup>
+			<ExecutableAndConfigFromExecProj Include="$(OutputPath)$(MSBuildProjectName).exe*" />
 			<NotExecFilesFromExecProj Include="$(OutputPath)*.deps.json" />
 			<NotExecFilesFromExecProj Include="$(OutputPath)*.dll" />
 			<NotExecFilesFromExecProj Include="$(OutputPath)*.pdb" Exclude="$(OutputPath)EmuHawk.pdb;$(OutputPath)DiscoHawk.pdb" />
 			<NotExecFilesFromExecProj Include="$(OutputPath)*.xml" />
 		</ItemGroup>
 		<Move Condition=" $(IsTargetingNetFramework) " SourceFiles="@(NotExecFilesFromExecProj)" DestinationFolder="$(OutputPath)dll/" />
+		<Move SourceFiles="@(ExecutableAndConfigFromExecProj)" DestinationFiles="@(ExecutableAndConfigFromExecProj->Replace($(MSBuildProjectName), $(ProjectNameTail)))" /> <!-- keep assembly name as e.g. `BizHawk.Client.EmuHawk`, but rename file to simply `EmuHawk.exe` as we've been doing -->
 	</Target>
 </Project>

commit ba1a1beca391c6bb51c9e6e10b890d0ee475f067

Rework build scripts for main solution, now using `dotnet publish`
no idea how this will go on Windows, but apparently it's been broken
since 7e8623fdd anyway

Dist/.BuildInConfigX.sh
@@ -7,3 +7,4 @@ fi
 config="$1"
 shift
 Dist/.InvokeCLIOnMainSln.sh "build" "$config" "$@"
+Dist/.InvokeCLIOnMainSln.sh publish "$config" --no-build "$@"

Dist/Package.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")/.."
 rm -fr "$targetDir" && mkdir -p "$targetDir"
 find "output" -type f \( -wholename "output/EmuHawk.exe" -o -wholename "output/DiscoHawk.exe" -o -wholename "output/*.config" -o -wholename "output/defctrl.json" -o -wholename "output/EmuHawkMono.sh" -o -wholename "output/dll/*" -o -wholename "output/Shaders/*" -o -wholename "output/gamedb/*" -o -wholename "output/Tools/*" -o -wholename "output/NES/Palettes/*" -o -wholename "output/Lua/*" -o -wholename "output/Gameboy/Palettes/*" -o -wholename "output/overlay/*" \) \
 	-not -name "*.pdb" -not -name "*.lib" -not -name "*.pgd" -not -name "*.ipdb" -not -name "*.iobj" -not -name "*.exp" -not -name "*.ilk" \
-	-not -wholename "output/dll/*.xml" -not -wholename "output/dll/libsneshawk-64*.exe" -not -wholename "output/dll/gpgx.elf" -not -wholename "output/dll/miniclient.*" \
+	-not -wholename "output/dll/libsneshawk-64*.exe" -not -wholename "output/dll/gpgx.elf" -not -wholename "output/dll/miniclient.*" \
 	-exec install -D -m644 "{}" "packaged_{}" \;
 cd "$targetDir"
 if [ "$1" = "windows-x64" ]; then

Dist/QuickTestBuildAndPackage.bat
@@ -10,6 +10,7 @@ git --version > NUL
 @if errorlevel 1 goto MISSINGGIT
 
 dotnet build ..\BizHawk.sln -c Release --no-incremental
+dotnet publish ..\BizHawk.sln -c Release --no-build
 @if not errorlevel 0 goto DOTNETBUILDFAILED
 rem -p:Platform="Any CPU"
 rem -p:RunAnalyzersDuringBuild=true

after.BizHawk.sln.targets
@@ -1,0 +1,24 @@
+<Project>
+	<PropertyGroup>
+		<FinalPublishDir>$(SolutionDir)output/</FinalPublishDir>
+	</PropertyGroup>
+	<Target Name="CombineOutputDirs" AfterTargets="Publish">
+		<ItemGroup>
+			<Assets Include="$(SolutionDir)Assets/**/*" />
+			<ExecProjFilesForPublication Include="$(SolutionDir)src/BizHawk.Client.DiscoHawk/bin/$(Configuration)/net48/publish/**/*" />
+			<ExecProjFilesForPublication Include="$(SolutionDir)src/BizHawk.Client.EmuHawk/bin/$(Configuration)/net48/publish/**/*" />
+		</ItemGroup>
+		<!-- what happens if there's a conflict? hopefully the first <Copy/> is executed first -->
+		<Copy SourceFiles="@(ExecProjFilesForPublication)" DestinationFolder="$(FinalPublishDir)dll" />
+		<Copy SourceFiles="@(Assets)" DestinationFolder="$(FinalPublishDir)%(RecursiveDir)" />
+	</Target>
+	<Target Name="FinalisePublication" AfterTargets="CombineOutputDirs">
+		<ItemGroup>
+			<ExecutablesAndSupplements Include="$(FinalPublishDir)dll/BizHawk.Client.DiscoHawk.*" />
+			<ExecutablesAndSupplements Include="$(FinalPublishDir)dll/BizHawk.Client.EmuHawk.*" />
+		</ItemGroup>
+		<Move SourceFiles="@(ExecutablesAndSupplements)" DestinationFiles="@(ExecutablesAndSupplements->Replace('dll/BizHawk.Client.', ''))" />
+		<Delete Files="$(FinalPublishDir)dll/doc_comments.xml" />
+		<MakeDir Directories="$(FinalPublishDir)ExternalTools;$(FinalPublishDir)Firmware;$(FinalPublishDir)Tools" />
+	</Target>
+</Project>

src/BizHawk.Client.EmuHawk/BizHawk.Client.EmuHawk.csproj
@@ -40,7 +40,6 @@
 		<EmbeddedResource Remove="images/sms-icon.png" />
 		<EmbeddedResource Remove="images/tastudio/ts_h_piano_*.png" />
 		<EmbeddedResource Remove="images/tastudio/ts_v_piano_*.png" />
-		<None Include="$(ProjectDir)../../Assets/**/*.*" CopyToOutputDirectory="PreserveNewest" />
 		<None Remove="packages.config" />
 		<None Remove="Properties/Resources.resources" />
 	</ItemGroup>

src/MainSlnCommon.props
@@ -1,11 +1,10 @@
 <Project>
 	<Import Project="../Common.props" />
 	<PropertyGroup>
-		<AppendTargetFrameworkToOutputPath>false</AppendTargetFrameworkToOutputPath>
 		<DefineConstants>$(DefineConstants);AVI_SUPPORT</DefineConstants>
-		<DocumentationFile>$(MSBuildProjectDirectory)/bin/doc_comments.xml</DocumentationFile>
+		<GenerateDocumentationFile>true</GenerateDocumentationFile> <!-- it would be nice to rename this to *.api_reference.xml or something, but it seems https://github.com/dotnet/standard/issues/614 was never fixed -->
 		<NoWarn>$(NoWarn);CS1573;CS1591;NU1702</NoWarn>
-		<OutputPath>$(MSBuildProjectDirectory)/../../output/dll</OutputPath>
+		<PublishDocumentationFiles>false</PublishDocumentationFiles>
 		<ResolveAssemblyWarnOrErrorOnTargetArchitectureMismatch>None</ResolveAssemblyWarnOrErrorOnTargetArchitectureMismatch>
 	</PropertyGroup>
 	<PropertyGroup Condition=" '$(Configuration)' == 'Release' And '$(SolutionDir)' != '' ">

src/MainSlnExecutable.props
@@ -1,25 +1,9 @@
 <Project>
 	<Import Project="MainSlnCommon.props" />
-	<PropertyGroup>
-		<OutputPath>$(MSBuildProjectDirectory)/../../output</OutputPath>
-		<ProjectNameTail>$(MSBuildProjectName.Substring($([MSBuild]::Add($(MSBuildProjectName.LastIndexOf('.')), 1))))</ProjectNameTail>
-	</PropertyGroup>
 	<PropertyGroup Condition=" '$(Configuration)' == 'Debug' ">
 		<OutputType>Exe</OutputType>
 	</PropertyGroup>
 	<PropertyGroup Condition=" '$(Configuration)' == 'Release' ">
 		<OutputType>WinExe</OutputType>
 	</PropertyGroup>
-	<Target Name="PostBuild" AfterTargets="PostBuildEvent">
-		<ItemGroup>
-			<ExecutableAndConfigFromExecProj Include="$(OutputPath)$(MSBuildProjectName).exe*" />
-			<NotExecFilesFromExecProj Include="$(OutputPath)*.deps.json" />
-			<NotExecFilesFromExecProj Include="$(OutputPath)*.dll" />
-			<NotExecFilesFromExecProj Include="$(OutputPath)*.pdb" Exclude="$(OutputPath)EmuHawk.pdb;$(OutputPath)DiscoHawk.pdb" />
-			<NotExecFilesFromExecProj Include="$(OutputPath)*.xml" />
-		</ItemGroup>
-		<Move Condition=" $(IsTargetingNetFramework) " SourceFiles="@(NotExecFilesFromExecProj)" DestinationFolder="$(OutputPath)dll/" />
-		<Move SourceFiles="@(ExecutableAndConfigFromExecProj)" DestinationFiles="@(ExecutableAndConfigFromExecProj->Replace($(MSBuildProjectName), $(ProjectNameTail)))" /> <!-- keep assembly name as e.g. `BizHawk.Client.EmuHawk`, but rename file to simply `EmuHawk.exe` as we've been doing -->
-		<MakeDir Directories="$(OutputPath)ExternalTools;$(OutputPath)Firmware;$(OutputPath)Tools" />
-	</Target>
 </Project>

src/TestProjects.props
@@ -2,7 +2,6 @@
 	<Import Project="MainSlnCommon.props" />
 	<PropertyGroup>
 		<EnableMSTestRunner>true</EnableMSTestRunner>
-		<OutputPath>$(ProjectDir)../../test_output</OutputPath>
 		<OutputType>Exe</OutputType>
 	</PropertyGroup>
 	<ItemGroup>