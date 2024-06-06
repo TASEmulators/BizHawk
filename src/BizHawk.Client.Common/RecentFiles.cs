dd92ea3cbf79fa9714321850758e39a23bb9284e
Morilli <35152647+Morilli@users.noreply.github.com>
2024-06-06 15:30:11 +0200

    Fix tests and analyzers

--- src/BizHawk.Client.Common/RecentFiles.cs
+++ src/BizHawk.Client.Common/RecentFiles.cs
@@ -7,6 +7,8 @@ namespace BizHawk.Client.Common
 {
 	public class RecentFiles
 	{
+		[JsonInclude]
+		[JsonPropertyOrder(-1)]
 		// ReSharper disable once FieldCanBeMadeReadOnly.Local
 		private List<string> recentlist;
 
