ffa5bb9b74e1859567169e692cb3a84105344abd
YoshiRulz <OSSYoshiRulz+git@gmail.com>
2024-06-08 13:39:50 +1000

    Use a polyglot BASH/Batch script as a shim for the Git commit hook

--- Dist/install_git_hooks.ps1
+++ Dist/install_git_hooks.ps1
@@ -1,24 +1,19 @@
 $targetDir = "$PSScriptRoot/../.git/hooks"
 if (Test-Path $targetDir -PathType Container) { # is Git repo
-	$magicHeader = '# placed here by BizHawk build scripts and may be updated automatically'
 	$PSCommandFilename = Split-Path $PSCommandPath -Leaf
-	foreach ($f in Get-ChildItem "$PSScriptRoot/git_hooks") {
+	foreach ($f in Get-ChildItem "$PSScriptRoot/git_hook_shims") {
 		$target = Join-Path $targetDir $f.Name
-		if (Test-Path $target -PathType Leaf) { # target file exists
-			if ($(Get-FileHash $target).Hash -ne $(Get-FileHash $f.FullName).Hash) { # files differ
-				$head = Get-Content $target -TotalCount 3
-				if ($magicHeader -in $head) {
-					echo "[$PSCommandFilename] updating existing Git hook $($f.Name)"
-					Copy-Item $f $target
-				} else {
-					echo "[$PSCommandFilename] found existing Git hook $($f.Name), please resolve conflict manually"
-					# should probably make the scripts extensible then...
-					exit 1
-				}
-			}
-		} else {
+		if (!(Test-Path $target -PathType Leaf)) { # target file doesn't exist
 			echo "[$PSCommandFilename] creating Git hook $($f.Name)"
 			Copy-Item $f $target
+			#TODO generate shim? the only difference between different shims would be the filename in the Batch part (and if there was an equivalent to `basename $0` then that would be the same too
+			#TODO use symlinks on Linux
+		} elseif ($(Get-FileHash $target).Hash -ne $(Get-FileHash $f.FullName).Hash) { # files differ
+			$head = Get-Content $target -TotalCount 3
+			echo "[$PSCommandFilename] found existing Git hook $($f.Name), please resolve conflict manually"
+			#TODO should REALLY make the scripts extensible then...
+			exit 1
 		}
+		# else no-op
 	}
 }
