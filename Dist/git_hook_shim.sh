e19c8fe5f5ca0ff4b29a8e6a0933ab842f0c38b7
YoshiRulz <OSSYoshiRulz+git@gmail.com>
2024-06-09 21:42:16 +1000

    Fix Git hook detecting global PowerShell but trying to use dotnet tool
    fixes c5f6dcd4e

--- Dist/git_hook_shim.sh
+++ Dist/git_hook_shim.sh
@@ -4,5 +4,5 @@ pwsh="$(command -v pwsh)"
 if [ -z "$pwsh" ]; then pwsh="$(command -v dotnet) pwsh"; fi
 if ! ("$pwsh" -v >/dev/null 2>/dev/null); then exit 0; fi
 kind="$(basename "$0")"
-dotnet pwsh "./Dist/git_hooks/$kind.ps1" "$@"
-if [ -e "./Dist/git_hooks/$kind.local.ps1" ]; then dotnet pwsh "./Dist/git_hooks/$kind.local.ps1" "$@"; fi
+"$pwsh" "./Dist/git_hooks/$kind.ps1" "$@"
+if [ -e "./Dist/git_hooks/$kind.local.ps1" ]; then "$pwsh" "./Dist/git_hooks/$kind.local.ps1" "$@"; fi
