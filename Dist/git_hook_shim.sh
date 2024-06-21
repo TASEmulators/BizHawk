62c1c11698f2aadcdfa66e0a4d62ce23804cb66f
YoshiRulz <OSSYoshiRulz+git@gmail.com>
2024-06-21 22:56:12 +1000

    Have Git hook shim skip execution when checking out old commits

--- Dist/git_hook_shim.sh
+++ Dist/git_hook_shim.sh
@@ -6,5 +6,7 @@ if ! ("$pwsh" -v >/dev/null 2>/dev/null); then
 	printf "pwsh not found in PATH; skipping %s hook\n" "$kind"
 	exit 0
 fi
-"$pwsh" "./Dist/git_hooks/$kind.ps1" "$@" || exit $?
-if [ -e "./Dist/git_hooks/$kind.local.ps1" ]; then "$pwsh" "./Dist/git_hooks/$kind.local.ps1" "$@" || exit $?; fi
+if [ -e "./Dist/git_hooks/$kind.ps1" ]; then
+	"$pwsh" "./Dist/git_hooks/$kind.ps1" "$@" || exit $?
+	if [ -e "./Dist/git_hooks/$kind.local.ps1" ]; then "$pwsh" "./Dist/git_hooks/$kind.local.ps1" "$@" || exit $?; fi
+fi
