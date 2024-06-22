cfc76ee47a72a38bb9b09f1c49566a4e9de5df76
Morilli <35152647+Morilli@users.noreply.github.com>
2024-06-22 23:03:54 +0200

    Fix subframe core check in tastudio bk2 exporting

--- src/BizHawk.Client.EmuHawk/tools/TAStudio/TAStudio.MenuItems.cs
+++ src/BizHawk.Client.EmuHawk/tools/TAStudio/TAStudio.MenuItems.cs
@@ -246,9 +246,7 @@ namespace BizHawk.Client.EmuHawk
 		{
 			_autosaveTimer.Stop();
 			
-			if (Emulator is Emulation.Cores.Nintendo.SubNESHawk.SubNESHawk
-				|| Emulator is Emulation.Cores.Nintendo.Gameboy.Gameboy
-				|| Emulator is Emulation.Cores.Nintendo.SubGBHawk.SubGBHawk)
+			if (Emulator.HasCycleTiming())
 			{
 				DialogController.ShowMessageBox("This core requires emulation to be on the last frame when writing the movie, otherwise movie length will appear incorrect.\nTAStudio can't handle this, so Export BK2, play it to the end, and then Save Movie.", "Warning", EMsgBoxIcon.Warning);
 			}
