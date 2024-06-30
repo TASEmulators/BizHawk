97dc7108c30ef827158b9153369be244ae39c041
feos <feykomylce@gmail.com>
2024-06-30 15:47:24 +0300

    uae: "support" rds and reduce magic

--- src/BizHawk.Emulation.Cores/Computers/Amiga/PUAE.cs
+++ src/BizHawk.Emulation.Cores/Computers/Amiga/PUAE.cs
@@ -71,8 +71,35 @@ namespace BizHawk.Emulation.Cores.Computers.Amiga
 			{
 				if (lp.Roms[index].Extension.ToLowerInvariant() == ".hdf")
 				{
-					_exe.AddReadonlyFile(lp.Roms[index].FileData, FileNames.HD + index);
-					AppendSetting($"hardfile2=ro,DH0:\"{FileNames.HD + index}\",32,1,2,512,0,,uae0");
+					var access = "ro";
+					var device_name = "DH0";
+					var volume_name = FileNames.HD + index;
+					var blocks_per_track = 32;
+					var surfaces = 1;
+					var reserved = 2;
+					var block_size = 512;
+					var boot_priority = 0;
+					var filesys_path = "";
+					var controller_unit = "uae0";
+
+					if (Encoding.ASCII.GetString(lp.Roms[index].RomData, 0, 3) == "RDS")
+					{
+						blocks_per_track = 0;
+						surfaces = 0;
+						reserved = 0;
+					}
+					_exe.AddReadonlyFile(lp.Roms[index].FileData, volume_name);
+						AppendSetting($"hardfile2=" +
+							$"{access}," +
+							$"{device_name}:" +
+							$"\"{volume_name}\"," +
+							$"{blocks_per_track}," +
+							$"{surfaces}," +
+							$"{reserved}," +
+							$"{block_size}," +
+							$"{boot_priority}," +
+							$"{filesys_path}," +
+							$"{controller_unit}");
 				}
 				else
 				{
