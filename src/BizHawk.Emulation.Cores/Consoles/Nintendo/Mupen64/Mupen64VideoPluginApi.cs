4a74a68703ef55fa706c09e2ae2f834d63298139
Morilli <35152647+Morilli@users.noreply.github.com>
2024-07-02 10:12:50 +0200

    Working video output

--- src/BizHawk.Emulation.Cores/Consoles/Nintendo/Mupen64/Mupen64VideoPluginApi.cs
+++ src/BizHawk.Emulation.Cores/Consoles/Nintendo/Mupen64/Mupen64VideoPluginApi.cs
@@ -8,9 +8,9 @@ public abstract class Mupen64VideoPluginApi : Mupen64PluginApi
 	[BizImport(CallingConvention.Cdecl)]
 	public abstract void ReadScreen2(IntPtr dest, ref int width, ref int height, int front);
 
-	public unsafe void ReadScreen2(int[] dest, ref int width, ref int height, int front)
+	public unsafe void ReadScreen2(byte[] dest, ref int width, ref int height, int front)
 	{
-		fixed (int* destPointer = dest)
+		fixed (byte* destPointer = dest)
 			ReadScreen2((IntPtr)destPointer, ref width, ref height, front);
 	}
 }
