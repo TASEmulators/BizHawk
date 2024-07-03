c188feb50c517f45ee81041312eeffe9ca5c6cb5
YoshiRulz <OSSYoshiRulz+git@gmail.com>
2024-07-03 22:49:51 +1000

    Move `SimpleGDIPDisplayManager` to `BizHawk.Client.Common`

--- src/BizHawk.Tests.Testroms.GB/DummyFrontend.cs
+++ src/BizHawk.Tests.Testroms.GB/DummyFrontend.cs
@@ -78,41 +78,6 @@ namespace BizHawk.Tests.Testroms.GB
 				=> throw new NotImplementedException(); // only used by Encore
 		}
 
-		public sealed class SimpleGDIPDisplayManager : DisplayManagerBase
-		{
-			private SimpleGDIPDisplayManager(Config config, IEmulator emuCore, IGL_GDIPlus glImpl)
-				: base(
-					config,
-					emuCore,
-					inputManager: null,
-					movieSession: null,
-					EDispMethod.GdiPlus,
-					glImpl,
-					new GDIPlusGuiRenderer(glImpl))
-				{}
-
-			public SimpleGDIPDisplayManager(Config config, IEmulator emuCore, Func<(int Width, int Height)> getVirtualSize)
-				: this(config, emuCore, new IGL_GDIPlus()) {}
-
-			protected override void ActivateGraphicsControlContext() {}
-
-			public override void ActivateOpenGLContext() {}
-
-			protected override int GetGraphicsControlDpi()
-				=> DisplayManagerBase.DEFAULT_DPI;
-
-			protected override Size GetGraphicsControlSize()
-				=> throw new NotImplementedException();
-
-			public override Size GetPanelNativeSize()
-				=> throw new NotImplementedException();
-
-			protected override Point GraphicsControlPointToClient(Point p)
-				=> throw new NotImplementedException();
-
-			protected override void SwapBuffersOfGraphicsControl() {}
-		}
-
 		private static int _totalFrames = 0;
 
 		private static readonly object _totalFramesMutex = new();
