fc1459d6beacaa1ae6f6bf631e66bd6b0cfce3bb
Morilli <35152647+Morilli@users.noreply.github.com>
2024-07-07 05:08:06 +0200

    Implement savestates

---
+++ src/BizHawk.Emulation.Cores/Consoles/Nintendo/Mupen64/Mupen64.IStatable.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using BizHawk.Common;
+using BizHawk.Emulation.Common;
+
+namespace BizHawk.Emulation.Cores.Consoles.Nintendo.Mupen64;
+
+public partial class Mupen64 : IStatable
+{
+	private enum SavestatesType
+	{
+		Unknown,
+		M64P,
+		Pj64Zip,
+		Pj64Unc
+	}
+
+	public bool AvoidRewind => false;
+
+	public void SaveStateBinary(BinaryWriter writer)
+	{
+		writer.Write(Frame);
+
+		var tempFileName = TempFileManager.GetTempFilename("mupen64-savestate");
+		using var tempFile = new FileStream(tempFileName, FileMode.Create, FileAccess.ReadWrite, FileShare.Write);
+
+		Mupen64Api.CoreDoCommand(Mupen64Api.m64p_command.M64CMD_STATE_SAVE, (int)SavestatesType.M64P, tempFileName);
+		tempFile.CopyTo(writer.BaseStream);
+	}
+
+	public void LoadStateBinary(BinaryReader reader)
+	{
+		Frame = reader.ReadInt32();
+
+		var tempFileName = TempFileManager.GetTempFilename("mupen64-savestate");
+		using var tempFile = new FileStream(tempFileName, FileMode.Create, FileAccess.Write, FileShare.Read);
+		reader.BaseStream.CopyTo(tempFile);
+
+		var error = Mupen64Api.CoreDoCommand(Mupen64Api.m64p_command.M64CMD_STATE_LOAD, (int)SavestatesType.M64P, tempFileName);
+	}
+}
