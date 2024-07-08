ece608abc39abdac1e5570297a65bb72153fa034
Morilli <35152647+Morilli@users.noreply.github.com>
2024-07-08 17:50:50 +0200

    Implement ISaveRam

---
+++ src/BizHawk.Emulation.Cores/Consoles/Nintendo/Mupen64/Mupen64.ISaveRam.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using BizHawk.Emulation.Common;
+
+namespace BizHawk.Emulation.Cores.Consoles.Nintendo.Mupen64;
+
+public partial class Mupen64 : ISaveRam
+{
+	public byte[] CloneSaveRam()
+	{
+		int saveRamSize = 0;
+		Mupen64Api.GetSaveRamSize(ref saveRamSize);
+		byte[] saveRamBuffer = new byte[saveRamSize];
+
+		Mupen64Api.GetSaveRam(saveRamBuffer);
+
+		return saveRamBuffer;
+	}
+
+	public void StoreSaveRam(byte[] data)
+	{
+		int saveRamSize = 0;
+		Mupen64Api.GetSaveRamSize(ref saveRamSize);
+		if (saveRamSize != data.Length)
+		{
+			throw new InvalidOperationException($"Core expects a savestate of size {saveRamSize}, but got {data.Length} bytes!");
+		}
+
+		Mupen64Api.PutSaveRam(data);
+	}
+
+	public bool SaveRamModified => true;
+}
