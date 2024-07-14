466f74a41a0fde36c529d13633275b5291d5b130
Morilli <35152647+Morilli@users.noreply.github.com>
2024-07-14 21:36:17 +0200

    Initial diassemble and trace support, lots missing
    TODO:
    - allow enabling/disabling debugger dynamically, it really doesn't need to be on permanently, requires a mupen-side change tho
    - provide registers
    - figure out why size of mupen went +800kb, is it just -lopcodes, -lintl, -liberty?
    - debugger (trace and debugger itself) doesn't work in dynarec, notify or unregister services accordingly
    - figure out why cached interpreter has <60 fps

---
+++ src/BizHawk.Emulation.Cores/Consoles/Nintendo/Mupen64/Mupen64.ITraceable.cs
@@ -0,0 +1,32 @@
+﻿using BizHawk.Emulation.Common;
+using static BizHawk.Emulation.Cores.Consoles.Nintendo.Mupen64.Mupen64Api.m64p_dbg_runstate;
+
+namespace BizHawk.Emulation.Cores.Consoles.Nintendo.Mupen64;
+
+public partial class Mupen64 : ITraceable
+{
+	public string Header => "R3400: PC, mnemonic, operands";
+
+	private readonly Mupen64Api.dbg_frontend_init _debuggerInitCallback;
+	private readonly Mupen64Api.dbg_frontend_update _traceCallback;
+
+	private void DebuggerInitCallback() => Mupen64Api.DebugSetRunState(RUNNING);
+
+	private void TraceCallback(uint pc)
+	{
+		string disassembly = this.Disassemble(_memoryDomains.SystemBus, pc, out _);
+		string registerInfo = ""; // TODO
+		_sink.Put(new TraceInfo(disassembly, registerInfo));
+	}
+
+	private ITraceSink _sink;
+	public ITraceSink Sink
+	{
+		get => _sink;
+		set
+		{
+			Mupen64Api.DebugSetRunState(value is null ? RUNNING : STEPPING);
+			_sink = value;
+		}
+	}
+}
--- src/BizHawk.Emulation.Cores/Consoles/Nintendo/Mupen64/Mupen64Api.cs
+++ src/BizHawk.Emulation.Cores/Consoles/Nintendo/Mupen64/Mupen64Api.cs
@@ -13,78 +13,78 @@ public abstract class Mupen64Api
 
 	public enum m64p_error
 	{
-		M64ERR_SUCCESS = 0,
-		M64ERR_NOT_INIT,        /* Function is disallowed before InitMupen64Plus() is called */
-		M64ERR_ALREADY_INIT,    /* InitMupen64Plus() was called twice */
-		M64ERR_INCOMPATIBLE,    /* API versions between components are incompatible */
-		M64ERR_INPUT_ASSERT,    /* Invalid parameters for function call, such as ParamValue=NULL for GetCoreParameter() */
-		M64ERR_INPUT_INVALID,   /* Invalid input data, such as ParamValue="maybe" for SetCoreParameter() to set a BOOL-type value */
-		M64ERR_INPUT_NOT_FOUND, /* The input parameter(s) specified a particular item which was not found */
-		M64ERR_NO_MEMORY,       /* Memory allocation failed */
-		M64ERR_FILES,           /* Error opening, creating, reading, or writing to a file */
-		M64ERR_INTERNAL,        /* Internal error (bug) */
-		M64ERR_INVALID_STATE,   /* Current program state does not allow operation */
-		M64ERR_PLUGIN_FAIL,     /* A plugin function returned a fatal error */
-		M64ERR_SYSTEM_FAIL,     /* A system function call, such as an SDL or file operation, failed */
-		M64ERR_UNSUPPORTED,     /* Function call is not supported (ie, core not built with debugger) */
-		M64ERR_WRONG_TYPE       /* A given input type parameter cannot be used for desired operation */
+		SUCCESS = 0,
+		NOT_INIT,        /* Function is disallowed before InitMupen64Plus() is called */
+		ALREADY_INIT,    /* InitMupen64Plus() was called twice */
+		INCOMPATIBLE,    /* API versions between components are incompatible */
+		INPUT_ASSERT,    /* Invalid parameters for function call, such as ParamValue=NULL for GetCoreParameter() */
+		INPUT_INVALID,   /* Invalid input data, such as ParamValue="maybe" for SetCoreParameter() to set a BOOL-type value */
+		INPUT_NOT_FOUND, /* The input parameter(s) specified a particular item which was not found */
+		NO_MEMORY,       /* Memory allocation failed */
+		FILES,           /* Error opening, creating, reading, or writing to a file */
+		INTERNAL,        /* Internal error (bug) */
+		INVALID_STATE,   /* Current program state does not allow operation */
+		PLUGIN_FAIL,     /* A plugin function returned a fatal error */
+		SYSTEM_FAIL,     /* A system function call, such as an SDL or file operation, failed */
+		UNSUPPORTED,     /* Function call is not supported (ie, core not built with debugger) */
+		WRONG_TYPE       /* A given input type parameter cannot be used for desired operation */
 	}
 
 	public enum m64p_command
 	{
-		M64CMD_NOP = 0,
-		M64CMD_ROM_OPEN,
-		M64CMD_ROM_CLOSE,
-		M64CMD_ROM_GET_HEADER,
-		M64CMD_ROM_GET_SETTINGS,
-		M64CMD_EXECUTE,
-		M64CMD_STOP,
-		M64CMD_PAUSE,
-		M64CMD_RESUME,
-		M64CMD_CORE_STATE_QUERY,
-		M64CMD_STATE_LOAD,
-		M64CMD_STATE_SAVE,
-		M64CMD_STATE_SET_SLOT,
-		M64CMD_SEND_SDL_KEYDOWN,
-		M64CMD_SEND_SDL_KEYUP,
-		M64CMD_SET_FRAME_CALLBACK,
-		M64CMD_TAKE_NEXT_SCREENSHOT,
-		M64CMD_CORE_STATE_SET,
-		M64CMD_READ_SCREEN,
-		M64CMD_RESET,
-		M64CMD_ADVANCE_FRAME
+		NOP = 0,
+		ROM_OPEN,
+		ROM_CLOSE,
+		ROM_GET_HEADER,
+		ROM_GET_SETTINGS,
+		EXECUTE,
+		STOP,
+		PAUSE,
+		RESUME,
+		CORE_STATE_QUERY,
+		STATE_LOAD,
+		STATE_SAVE,
+		STATE_SET_SLOT,
+		SEND_SDL_KEYDOWN,
+		SEND_SDL_KEYUP,
+		SET_FRAME_CALLBACK,
+		TAKE_NEXT_SCREENSHOT,
+		CORE_STATE_SET,
+		READ_SCREEN,
+		RESET,
+		ADVANCE_FRAME
 	}
 
 	public enum m64p_plugin_type
 	{
-		M64PLUGIN_NULL = 0,
-		M64PLUGIN_RSP = 1,
-		M64PLUGIN_GFX,
-		M64PLUGIN_AUDIO,
-		M64PLUGIN_INPUT,
-		M64PLUGIN_CORE
+		NULL = 0,
+		RSP = 1,
+		GFX,
+		AUDIO,
+		INPUT,
+		CORE
 	}
 
 	public enum m64p_core_param
 	{
-		M64CORE_EMU_STATE = 1,
-		M64CORE_VIDEO_MODE,
-		M64CORE_SAVESTATE_SLOT,
-		M64CORE_SPEED_FACTOR,
-		M64CORE_SPEED_LIMITER,
-		M64CORE_VIDEO_SIZE,
-		M64CORE_AUDIO_VOLUME,
-		M64CORE_AUDIO_MUTE,
-		M64CORE_INPUT_GAMESHARK,
-		M64CORE_STATE_LOADCOMPLETE,
-		M64CORE_STATE_SAVECOMPLETE
+		EMU_STATE = 1,
+		VIDEO_MODE,
+		SAVESTATE_SLOT,
+		SPEED_FACTOR,
+		SPEED_LIMITER,
+		VIDEO_SIZE,
+		AUDIO_VOLUME,
+		AUDIO_MUTE,
+		INPUT_GAMESHARK,
+		STATE_LOADCOMPLETE,
+		STATE_SAVECOMPLETE
 	}
 
 	public enum m64p_emu_state
 	{
-		M64EMU_STOPPED = 1,
-		M64EMU_RUNNING,
-		M64EMU_PAUSED
+		STOPPED = 1,
+		RUNNING,
+		PAUSED
 	}
 
 	[StructLayout(LayoutKind.Sequential)]
@@ -96,37 +96,37 @@ public abstract class Mupen64Api
 
 	public enum m64p_video_mode
 	{
-		M64VIDEO_NONE = 1,
-		M64VIDEO_WINDOWED,
-		M64VIDEO_FULLSCREEN
+		NONE = 1,
+		WINDOWED,
+		FULLSCREEN
 	}
 
 	public enum m64p_video_flags
 	{
-		M64VIDEOFLAG_SUPPORT_RESIZING = 1
+		SUPPORT_RESIZING = 1
 	}
 
 	public enum m64p_GLattr
 	{
-		M64P_GL_DOUBLEBUFFER = 1,
-		M64P_GL_BUFFER_SIZE,
-		M64P_GL_DEPTH_SIZE,
-		M64P_GL_RED_SIZE,
-		M64P_GL_GREEN_SIZE,
-		M64P_GL_BLUE_SIZE,
-		M64P_GL_ALPHA_SIZE,
-		M64P_GL_SWAP_CONTROL,
-		M64P_GL_MULTISAMPLEBUFFERS,
-		M64P_GL_MULTISAMPLESAMPLES,
-		M64P_GL_CONTEXT_MAJOR_VERSION,
-		M64P_GL_CONTEXT_MINOR_VERSION,
-		M64P_GL_CONTEXT_PROFILE_MASK
+		DOUBLEBUFFER = 1,
+		BUFFER_SIZE,
+		DEPTH_SIZE,
+		RED_SIZE,
+		GREEN_SIZE,
+		BLUE_SIZE,
+		ALPHA_SIZE,
+		SWAP_CONTROL,
+		MULTISAMPLEBUFFERS,
+		MULTISAMPLESAMPLES,
+		CONTEXT_MAJOR_VERSION,
+		CONTEXT_MINOR_VERSION,
+		CONTEXT_PROFILE_MASK
 	}
 
 	public enum m64p_render_mode
 	{
-		M64P_RENDER_OPENGL = 0,
-		M64P_RENDER_VULKAN
+		OPENGL = 0,
+		VULKAN
 	}
 
 	public enum m64p_dbg_memptr_type
@@ -140,10 +140,17 @@ public abstract class Mupen64Api
 	}
 
 	public enum m64p_type {
-		M64TYPE_INT = 1,
-		M64TYPE_FLOAT,
-		M64TYPE_BOOL,
-		M64TYPE_STRING
+		INT = 1,
+		FLOAT,
+		BOOL,
+		STRING
+	}
+
+	public enum m64p_dbg_runstate
+	{
+		PAUSED = 0,
+		STEPPING,
+		RUNNING
 	}
 
 	private const int VidExtFunctions = 17;
@@ -221,6 +228,10 @@ public abstract class Mupen64Api
 	public delegate void StateCallback(IntPtr context2, m64p_core_param paramChanged, int newValue);
 	public delegate void DebugCallback(IntPtr context, int level, string message);
 
+	public delegate void dbg_frontend_init();
+	public delegate void dbg_frontend_update(uint pc);
+	public delegate void dbg_frontend_vi();
+
 	[BizImport(CallingConvention.Cdecl)]
 	public abstract m64p_error CoreStartup(int apiVersion, string configPath, string dataPath, IntPtr context, DebugCallback debugCallback, IntPtr context2, StateCallback stateCallback);
 
@@ -250,7 +261,7 @@ public abstract class Mupen64Api
 	public unsafe m64p_error CoreStateSet(m64p_core_param parameter, int value)
 	{
 		int* valuePointer = &value;
-		return CoreDoCommand(m64p_command.M64CMD_CORE_STATE_SET, (int)parameter, (IntPtr)valuePointer);
+		return CoreDoCommand(m64p_command.CORE_STATE_SET, (int)parameter, (IntPtr)valuePointer);
 	}
 
 	[BizImport(CallingConvention.Cdecl, Compatibility = true)]
@@ -275,6 +286,9 @@ public abstract class Mupen64Api
 	public abstract byte DebugMemRead8(uint address);
 
 	[BizImport(CallingConvention.Cdecl)]
+	public abstract uint DebugMemRead32(uint address);
+
+	[BizImport(CallingConvention.Cdecl)]
 	public abstract void DebugMemWrite8(uint address, byte value);
 
 	[BizImport(CallingConvention.Cdecl)]
@@ -282,4 +296,16 @@ public abstract class Mupen64Api
 
 	[BizImport(CallingConvention.Cdecl)]
 	public abstract m64p_error ConfigSetParameter(IntPtr configSectionHandle, string paramName, m64p_type paramType, IntPtr paramValue);
+
+	[BizImport(CallingConvention.Cdecl)]
+	public abstract m64p_error DebugSetCallbacks(dbg_frontend_init dbgFrontendInit, dbg_frontend_update dbgFrontendUpdate, dbg_frontend_vi dbgFrontendVi);
+
+	[BizImport(CallingConvention.Cdecl)]
+	public abstract m64p_error DebugSetRunState(m64p_dbg_runstate runstate);
+
+	[BizImport(CallingConvention.Cdecl)]
+	public abstract m64p_error DebugStep();
+
+	[BizImport(CallingConvention.Cdecl)]
+	public abstract void DebugDecodeOp(uint instruction, byte[] op, byte[] args, int pc);
 }
