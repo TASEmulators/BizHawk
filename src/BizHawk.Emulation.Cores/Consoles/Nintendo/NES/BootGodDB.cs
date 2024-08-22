TASEmulators/BizHawk

commit e38726db50186caa52aaac3b4e5b2cf7bf0be826

Unify checksum helper methods

src/BizHawk.Emulation.Cores/Consoles/Nintendo/NES/BootGodDB.cs
@@ -135,7 +135,7 @@ namespace BizHawk.Emulation.Cores.Nintendo.NES
 						{
 							currCart = new CartInfo();
 							currCart.System = xmlReader.GetAttribute("system");
-							currCart.Sha1 = "sha1:" + xmlReader.GetAttribute("sha1");
+							currCart.Sha1 = $"SHA1:{xmlReader.GetAttribute("sha1")}";
 							currCart.Name = currName;
 							state = 2;
 						}

src/BizHawk.Emulation.Cores/Consoles/Nintendo/QuickNES/QuickNES.cs
@@ -9,7 +9,6 @@ using BizHawk.Common;
 using BizHawk.Emulation.Common;
 using BizHawk.Emulation.Cores.Nintendo.NES;
 using BizHawk.BizInvoke;
-using BizHawk.Common.BufferExtensions;
 
 namespace BizHawk.Emulation.Cores.Consoles.Nintendo.QuickNES
 {
@@ -245,7 +244,7 @@ namespace BizHawk.Emulation.Cores.Consoles.Nintendo.QuickNES
 				for (int i = 0; i < chrrom.Size; i++)
 					ms.WriteByte(chrrom.PeekByte(i));
 
-			string sha1 = ms.ToArray().HashSHA1();
+			var sha1 = SHA1Checksum.ComputeDigestHex(ms.ToArray());
 			Console.WriteLine("Hash for BootGod: {0}", sha1);
 
 			// Bail out on ROM's known to not be playable by this core
@@ -300,7 +299,7 @@ namespace BizHawk.Emulation.Cores.Consoles.Nintendo.QuickNES
 		// we need to do this from the raw file since QuickNES hasn't had time to process it yet.
 		private byte[] FixInesHeader(byte[] file)
 		{
-			string sha1 = BufferExtensions.HashSHA1(file);
+			var sha1 = SHA1Checksum.ComputeDigestHex(file);
 			bool didSomething = false;
 
 			Console.WriteLine(sha1);

src/BizHawk.BizInvoke/WaterboxUtils.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Security.Cryptography;
 
 namespace BizHawk.BizInvoke
 {
@@ -20,18 +19,6 @@ namespace BizHawk.BizInvoke
 				dst.Write(buff, 0, r);
 				len -= r;
 			}
-		}
-
-		public static byte[] Hash(byte[] data)
-		{
-			using var h = SHA1.Create();
-			return h.ComputeHash(data);
-		}
-
-		public static byte[] Hash(Stream s)
-		{
-			using var h = SHA1.Create();
-			return h.ComputeHash(s);
 		}
 
 		public static unsafe void ZeroMemory(IntPtr mem, long length)

src/BizHawk.Client.Common/Api/Classes/MemoryApi.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
 
 using BizHawk.Common;
-using BizHawk.Common.BufferExtensions;
 using BizHawk.Emulation.Common;
 
 namespace BizHawk.Client.Common
@@ -235,8 +233,7 @@ namespace BizHawk.Client.Common
 			}
 			var data = new byte[count];
 			for (var i = 0; i < count; i++) data[i] = d.PeekByte(addr + i);
-			using var hasher = SHA256.Create();
-			return hasher.ComputeHash(data).BytesToHexString();
+			return SHA256Checksum.ComputeDigestHex(data);
 		}
 
 		public uint ReadByte(long addr, string domain = null) => ReadUnsigned(addr, 1, domain);

src/BizHawk.Client.Common/RomLoader.cs
@@ -233,7 +233,7 @@ namespace BizHawk.Client.Common
 			var discType = new DiscIdentifier(disc).DetectDiscType();
 			var discHasher = new DiscHasher(disc);
 			var discHash = discType == DiscType.SonyPSX
-				? discHasher.Calculate_PSX_BizIDHash().ToString("X8")
+				? discHasher.Calculate_PSX_BizIDHash()
 				: discHasher.OldHash();
 
 			var game = Database.CheckDatabase(discHash);

src/BizHawk.Client.Common/XmlGame.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Xml;
 
 using BizHawk.Common;
-using BizHawk.Common.BufferExtensions;
 using BizHawk.Common.IOExtensions;
 using BizHawk.Emulation.Common;
 
@@ -99,12 +98,11 @@ namespace BizHawk.Client.Common
 
 						ret.Assets.Add(new KeyValuePair<string, byte[]>(filename, data));
 						ret.AssetFullPaths.Add(fullPath);
-						using var sha1 = System.Security.Cryptography.SHA1.Create();
-						sha1.TransformFinalBlock(data, 0, data.Length);
-						hashStream.Write(sha1.Hash, 0, sha1.Hash.Length);
+						var sha1 = SHA1Checksum.Compute(data);
+						hashStream.Write(sha1, 0, sha1.Length);
 					}
 
-					ret.GI.Hash = hashStream.GetBuffer().HashSHA1(0, (int)hashStream.Length);
+					ret.GI.Hash = SHA1Checksum.ComputeDigestHex(hashStream.GetBufferAsSpan());
 					hashStream.Close();
 					if (originalIndex != null)
 					{

src/BizHawk.Client.Common/fwmanager/FirmwareManager.cs
@@ -1,14 +1,13 @@
 ﻿#nullable enable
 
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Security.Cryptography;
 using System.IO;
 using System.Linq;
 
-using BizHawk.Common.BufferExtensions;
+using BizHawk.Common;
 using BizHawk.Common.CollectionExtensions;
+using BizHawk.Common.IOExtensions;
 using BizHawk.Emulation.Common;
 
 namespace BizHawk.Client.Common
@@ -20,9 +19,7 @@ namespace BizHawk.Client.Common
 		public static (byte[] Patched, string ActualHash) PerformPatchInMemory(byte[] @base, in FirmwarePatchOption patchOption)
 		{
 			var patched = patchOption.Patches.Aggregate(seed: @base, (a, fpd) => fpd.ApplyToMutating(a));
-			using var sha1 = SHA1.Create();
-			sha1.ComputeHash(patched);
-			return (patched, sha1.Hash.BytesToHexString());
+			return (patched, SHA1Checksum.ComputeDigestHex(patched));
 		}
 
 		public static (string FilePath, int FileSize, FirmwareFile FF) PerformPatchOnDisk(string baseFilename, in FirmwarePatchOption patchOption, PathEntryCollection pathEntries)
@@ -95,26 +92,16 @@ namespace BizHawk.Client.Common
 			return resolved.FilePath;
 		}
 
-		private sealed class RealFirmwareReader : IDisposable
+		private sealed class RealFirmwareReader
 		{
 			private readonly Dictionary<string, RealFirmwareFile> _dict = new();
 
-			private SHA1? _sha1 = SHA1.Create();
-
 			public IReadOnlyDictionary<string, RealFirmwareFile> Dict => _dict;
-
-			public void Dispose()
-			{
-				_sha1?.Dispose();
-				_sha1 = null;
-			}
 
 			public RealFirmwareFile Read(FileInfo fi)
 			{
-				if (_sha1 == null) throw new ObjectDisposedException(nameof(RealFirmwareReader));
 				using var fs = fi.OpenRead();
-				_sha1!.ComputeHash(fs);
-				var hash = _sha1.Hash.BytesToHexString();
+				var hash = SHA1Checksum.ComputeDigestHex(fs.ReadAllBytes());
 				return _dict![hash] = new RealFirmwareFile(fi, hash);
 			}
 		}
@@ -133,7 +120,7 @@ namespace BizHawk.Client.Common
 				if (!_firmwareSizes.Contains(fi.Length)) return false;
 
 				// check the hash
-				using var reader = new RealFirmwareReader();
+				var reader = new RealFirmwareReader();
 				reader.Read(fi);
 				var hash = reader.Dict.Values.First().Hash;
 				return FirmwareDatabase.FirmwareFiles.Any(a => a.Hash == hash);
@@ -146,7 +133,7 @@ namespace BizHawk.Client.Common
 
 		public void DoScanAndResolve(PathEntryCollection pathEntries, IDictionary<string, string> userSpecifications)
 		{
-			using var reader = new RealFirmwareReader();
+			var reader = new RealFirmwareReader();
 
 			// build a list of files under the global firmwares path, and build a hash for each of them (as ResolutionInfo) while we're at it
 			var todo = new Queue<DirectoryInfo>(new[] { new DirectoryInfo(pathEntries.AbsolutePathFor(pathEntries.FirmwaresPathFragment, null)) });

src/BizHawk.Client.EmuHawk/MainForm.Debug.cs
@@ -5,12 +5,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Security.Cryptography;
 using System.Windows.Forms;
 
 using BizHawk.Client.Common;
 using BizHawk.Client.EmuHawk.Properties;
-using BizHawk.Common.BufferExtensions;
+using BizHawk.Common;
 using BizHawk.Emulation.Common;
 using BizHawk.Emulation.Cores;
 using BizHawk.Emulation.Cores.Nintendo.GBA;
@@ -88,9 +87,7 @@ namespace BizHawk.Client.EmuHawk
 							return;
 						}
 						// else something happened, figure out what it was
-						using var sha1 = SHA1.Create();
-						sha1.ComputeHash(@base);
-						var baseHash = sha1.Hash.BytesToHexString();
+						var baseHash = SHA1Checksum.ComputeDigestHex(@base);
 						this.ModalMessageBox(baseHash == fpo.BaseHash
 							? $"patchset declared with target\nSHA1:{fpo.TargetHash}\nbut produced\nSHA1:{actualHash}\n(is the patch wrong, or the hash?)"
 							: $"patchset declared for base\nSHA1:{fpo.BaseHash}\nbut\nSHA1:{baseHash}\nwas provided");

commit 8c9587e00bb34d0c08b0104eee209bb23405cec4

Fix BootGodDB lookup being completely broken in quicknes
- closes #3703

Presumably broken since e38726db50186caa52aaac3b4e5b2cf7bf0be826.

src/BizHawk.Emulation.Cores/Consoles/Nintendo/NES/BootGodDB.cs
@@ -136,7 +136,7 @@ namespace BizHawk.Emulation.Cores.Nintendo.NES
 						{
 							currCart = new CartInfo();
 							currCart.System = xmlReader.GetAttribute("system");
-							currCart.Sha1 = $"SHA1:{xmlReader.GetAttribute("sha1")}";
+							currCart.Sha1 = $"{SHA1Checksum.PREFIX}:{xmlReader.GetAttribute("sha1")}";
 							currCart.Name = currName;
 							state = 2;
 						}

src/BizHawk.Emulation.Cores/Consoles/Nintendo/QuickNES/QuickNES.cs
@@ -285,7 +285,7 @@ namespace BizHawk.Emulation.Cores.Consoles.Nintendo.QuickNES
 				throw new UnsupportedGameException("Game known to not be playable in this core");
 			}
 
-			sha1 = "sha1:" + sha1; // huh?
+			sha1 = $"{SHA1Checksum.PREFIX}:{sha1}";
 			var carts = BootGodDb.Identify(sha1);
 
 			if (carts.Count > 0)