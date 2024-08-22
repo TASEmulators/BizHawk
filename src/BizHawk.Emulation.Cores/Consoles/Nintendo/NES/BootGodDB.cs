f010d6f8024c698c5c7c25e105a67cc305e933a3
Morilli <35152647+Morilli@users.noreply.github.com>
2024-08-22 22:54:21 +0200

    Fix bootgod exception on missing game hash
    fixes 633216be143fe997624ab16505a5be49265dcaeb

--- src/BizHawk.Emulation.Cores/Consoles/Nintendo/NES/BootGodDB.cs
+++ src/BizHawk.Emulation.Cores/Consoles/Nintendo/NES/BootGodDB.cs
@@ -158,7 +158,7 @@ namespace BizHawk.Emulation.Cores.Nintendo.NES
 #else
 			if (acquire == null) throw new InvalidOperationException("Bootgod DB not initialized. It's a client responsibility because only a client knows where the database is located.");
 			acquire.WaitOne();
-			return instance._sha1Table[sha1];
+			return instance._sha1Table.GetValueOrPutNew(sha1);
 #endif
 		}
 	}
