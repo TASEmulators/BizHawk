f6bf38141aa116e78246c905efaa9ca709d034de
Morilli <35152647+Morilli@users.noreply.github.com>
2024-09-21 12:37:36 +0200

    Improve watch editor layout
    see #4046

--- src/BizHawk.Client.EmuHawk/tools/Watch/WatchEditor.Designer.cs
+++ src/BizHawk.Client.EmuHawk/tools/Watch/WatchEditor.Designer.cs
@@ -28,170 +28,177 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new BizHawk.WinForms.Controls.LocLabelEx();
-            this.label2 = new BizHawk.WinForms.Controls.LocLabelEx();
-            this.AddressBox = new BizHawk.Client.EmuHawk.HexTextBox();
-            this.NotesBox = new System.Windows.Forms.TextBox();
-            this.OK = new System.Windows.Forms.Button();
-            this.Cancel = new System.Windows.Forms.Button();
-            this.label6 = new BizHawk.WinForms.Controls.LocLabelEx();
-            this.DomainDropDown = new System.Windows.Forms.ComboBox();
-            this.label3 = new BizHawk.WinForms.Controls.LocLabelEx();
-            this.SizeDropDown = new System.Windows.Forms.ComboBox();
-            this.DisplayTypeLael = new BizHawk.WinForms.Controls.LocLabelEx();
-            this.DisplayTypeDropDown = new System.Windows.Forms.ComboBox();
-            this.BigEndianCheckBox = new System.Windows.Forms.CheckBox();
-            this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.Location = new System.Drawing.Point(9, 9);
-            this.label1.Name = "label1";
-            this.label1.Text = "Address: 0x";
-            // 
-            // label2
-            // 
-            this.label2.Location = new System.Drawing.Point(9, 35);
-            this.label2.Name = "label2";
-            this.label2.Text = "Notes:";
-            // 
-            // AddressBox
-            // 
-            this.AddressBox.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
-            this.AddressBox.Location = new System.Drawing.Point(69, 6);
-            this.AddressBox.MaxLength = 8;
-            this.AddressBox.Name = "AddressBox";
-            this.AddressBox.Nullable = false;
-            this.AddressBox.Size = new System.Drawing.Size(100, 20);
-            this.AddressBox.TabIndex = 1;
-            this.AddressBox.Text = "00000000";
-            // 
-            // NotesBox
-            // 
-            this.NotesBox.Location = new System.Drawing.Point(69, 32);
-            this.NotesBox.MaxLength = 256;
-            this.NotesBox.Name = "NotesBox";
-            this.NotesBox.Size = new System.Drawing.Size(100, 20);
-            this.NotesBox.TabIndex = 5;
-            // 
-            // OK
-            // 
-            this.OK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.OK.Location = new System.Drawing.Point(12, 260);
-            this.OK.Name = "OK";
-            this.OK.Size = new System.Drawing.Size(75, 23);
-            this.OK.TabIndex = 30;
-						this.OK.Text = "OK";
-            this.OK.UseVisualStyleBackColor = true;
-            this.OK.Click += new System.EventHandler(this.Ok_Click);
-            // 
-            // Cancel
-            // 
-            this.Cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.Cancel.Location = new System.Drawing.Point(123, 260);
-            this.Cancel.Name = "Cancel";
-            this.Cancel.Size = new System.Drawing.Size(75, 23);
-            this.Cancel.TabIndex = 35;
-            this.Cancel.Text = "Cancel";
-            this.Cancel.UseVisualStyleBackColor = true;
-            this.Cancel.Click += new System.EventHandler(this.Cancel_Click);
-            // 
-            // label6
-            // 
-            this.label6.Location = new System.Drawing.Point(11, 214);
-            this.label6.Name = "label6";
-            this.label6.Text = "Memory Domain";
-            // 
-            // DomainDropDown
-            // 
-            this.DomainDropDown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.DomainDropDown.FormattingEnabled = true;
-            this.DomainDropDown.Location = new System.Drawing.Point(12, 230);
-            this.DomainDropDown.Name = "DomainDropDown";
-            this.DomainDropDown.Size = new System.Drawing.Size(141, 21);
-            this.DomainDropDown.TabIndex = 25;
-            this.DomainDropDown.SelectedIndexChanged += new System.EventHandler(this.DomainComboBox_SelectedIndexChanged);
-            // 
-            // label3
-            // 
-            this.label3.Location = new System.Drawing.Point(9, 64);
-            this.label3.Name = "label3";
-            this.label3.Text = "Size";
-            // 
-            // SizeDropDown
-            // 
-            this.SizeDropDown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.SizeDropDown.FormattingEnabled = true;
-            this.SizeDropDown.Items.AddRange(new object[] {
+			this.label1 = new BizHawk.WinForms.Controls.LocLabelEx();
+			this.label2 = new BizHawk.WinForms.Controls.LocLabelEx();
+			this.NotesBox = new System.Windows.Forms.TextBox();
+			this.OK = new System.Windows.Forms.Button();
+			this.Cancel = new System.Windows.Forms.Button();
+			this.label6 = new BizHawk.WinForms.Controls.LocLabelEx();
+			this.DomainDropDown = new System.Windows.Forms.ComboBox();
+			this.label3 = new BizHawk.WinForms.Controls.LocLabelEx();
+			this.SizeDropDown = new System.Windows.Forms.ComboBox();
+			this.DisplayTypeLabel = new BizHawk.WinForms.Controls.LocLabelEx();
+			this.DisplayTypeDropDown = new System.Windows.Forms.ComboBox();
+			this.BigEndianCheckBox = new System.Windows.Forms.CheckBox();
+			this.BigEndianLabel = new BizHawk.WinForms.Controls.LocLabelEx();
+			this.AddressBox = new BizHawk.Client.EmuHawk.HexTextBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(6, 39);
+			this.label1.Name = "label1";
+			this.label1.Text = "Address:        0x";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(6, 66);
+			this.label2.Name = "label2";
+			this.label2.Text = "Notes:";
+			// 
+			// NotesBox
+			// 
+			this.NotesBox.Location = new System.Drawing.Point(90, 63);
+			this.NotesBox.MaxLength = 256;
+			this.NotesBox.Name = "NotesBox";
+			this.NotesBox.Size = new System.Drawing.Size(123, 20);
+			this.NotesBox.TabIndex = 2;
+			// 
+			// OK
+			// 
+			this.OK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.OK.Location = new System.Drawing.Point(12, 170);
+			this.OK.Name = "OK";
+			this.OK.Size = new System.Drawing.Size(75, 23);
+			this.OK.TabIndex = 6;
+			this.OK.Text = "OK";
+			this.OK.UseVisualStyleBackColor = true;
+			this.OK.Click += new System.EventHandler(this.Ok_Click);
+			// 
+			// Cancel
+			// 
+			this.Cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.Cancel.Location = new System.Drawing.Point(135, 170);
+			this.Cancel.Name = "Cancel";
+			this.Cancel.Size = new System.Drawing.Size(75, 23);
+			this.Cancel.TabIndex = 7;
+			this.Cancel.Text = "Cancel";
+			this.Cancel.UseVisualStyleBackColor = true;
+			this.Cancel.Click += new System.EventHandler(this.Cancel_Click);
+			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(6, 12);
+			this.label6.Name = "label6";
+			this.label6.Text = "Memory Domain";
+			// 
+			// DomainDropDown
+			// 
+			this.DomainDropDown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.DomainDropDown.FormattingEnabled = true;
+			this.DomainDropDown.Location = new System.Drawing.Point(90, 9);
+			this.DomainDropDown.Name = "DomainDropDown";
+			this.DomainDropDown.Size = new System.Drawing.Size(123, 21);
+			this.DomainDropDown.TabIndex = 0;
+			this.DomainDropDown.SelectedIndexChanged += new System.EventHandler(this.DomainComboBox_SelectedIndexChanged);
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(6, 93);
+			this.label3.Name = "label3";
+			this.label3.Text = "Size";
+			// 
+			// SizeDropDown
+			// 
+			this.SizeDropDown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.SizeDropDown.FormattingEnabled = true;
+			this.SizeDropDown.Items.AddRange(new object[] {
             "1 Byte",
             "2 Byte",
             "4 Byte"});
-            this.SizeDropDown.Location = new System.Drawing.Point(10, 80);
-            this.SizeDropDown.Name = "SizeDropDown";
-            this.SizeDropDown.Size = new System.Drawing.Size(141, 21);
-            this.SizeDropDown.TabIndex = 10;
-            this.SizeDropDown.SelectedIndexChanged += new System.EventHandler(this.SizeDropDown_SelectedIndexChanged);
-            // 
-            // DisplayTypeLael
-            // 
-            this.DisplayTypeLael.Location = new System.Drawing.Point(11, 106);
-            this.DisplayTypeLael.Name = "DisplayTypeLael";
-            this.DisplayTypeLael.Text = "Display Type";
-            // 
-            // DisplayTypeDropDown
-            // 
-            this.DisplayTypeDropDown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.DisplayTypeDropDown.FormattingEnabled = true;
-            this.DisplayTypeDropDown.Items.AddRange(new object[] {
+			this.SizeDropDown.Location = new System.Drawing.Point(90, 90);
+			this.SizeDropDown.Name = "SizeDropDown";
+			this.SizeDropDown.Size = new System.Drawing.Size(123, 21);
+			this.SizeDropDown.TabIndex = 3;
+			this.SizeDropDown.SelectedIndexChanged += new System.EventHandler(this.SizeDropDown_SelectedIndexChanged);
+			// 
+			// DisplayTypeLabel
+			// 
+			this.DisplayTypeLabel.Location = new System.Drawing.Point(6, 120);
+			this.DisplayTypeLabel.Name = "DisplayTypeLabel";
+			this.DisplayTypeLabel.Text = "Display Type";
+			// 
+			// DisplayTypeDropDown
+			// 
+			this.DisplayTypeDropDown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.DisplayTypeDropDown.FormattingEnabled = true;
+			this.DisplayTypeDropDown.Items.AddRange(new object[] {
             "1 Byte",
             "2 Byte",
             "4 Byte"});
-            this.DisplayTypeDropDown.Location = new System.Drawing.Point(12, 122);
-            this.DisplayTypeDropDown.Name = "DisplayTypeDropDown";
-            this.DisplayTypeDropDown.Size = new System.Drawing.Size(141, 21);
-            this.DisplayTypeDropDown.TabIndex = 15;
-            this.DisplayTypeDropDown.SelectedIndexChanged += new System.EventHandler(this.DisplayTypeDropDown_SelectedIndexChanged);
-            // 
-            // BigEndianCheckBox
-            // 
-            this.BigEndianCheckBox.AutoSize = true;
-            this.BigEndianCheckBox.Location = new System.Drawing.Point(14, 159);
-            this.BigEndianCheckBox.Name = "BigEndianCheckBox";
-            this.BigEndianCheckBox.Size = new System.Drawing.Size(77, 17);
-            this.BigEndianCheckBox.TabIndex = 20;
-            this.BigEndianCheckBox.Text = "Big Endian";
-            this.BigEndianCheckBox.UseVisualStyleBackColor = true;
-            // 
-            // WatchEditor
-            // 
-            this.AcceptButton = this.OK;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.CancelButton = this.Cancel;
-            this.ClientSize = new System.Drawing.Size(213, 296);
-            this.Controls.Add(this.BigEndianCheckBox);
-            this.Controls.Add(this.DisplayTypeLael);
-            this.Controls.Add(this.DisplayTypeDropDown);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.SizeDropDown);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.DomainDropDown);
-            this.Controls.Add(this.Cancel);
-            this.Controls.Add(this.OK);
-            this.Controls.Add(this.NotesBox);
-            this.Controls.Add(this.AddressBox);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "WatchEditor";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "New Watch";
-            this.Load += new System.EventHandler(this.RamWatchNewWatch_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.DisplayTypeDropDown.Location = new System.Drawing.Point(90, 117);
+			this.DisplayTypeDropDown.Name = "DisplayTypeDropDown";
+			this.DisplayTypeDropDown.Size = new System.Drawing.Size(123, 21);
+			this.DisplayTypeDropDown.TabIndex = 4;
+			this.DisplayTypeDropDown.SelectedIndexChanged += new System.EventHandler(this.DisplayTypeDropDown_SelectedIndexChanged);
+			// 
+			// BigEndianCheckBox
+			// 
+			this.BigEndianCheckBox.AutoSize = true;
+			this.BigEndianCheckBox.Location = new System.Drawing.Point(91, 148);
+			this.BigEndianCheckBox.Name = "BigEndianCheckBox";
+			this.BigEndianCheckBox.Size = new System.Drawing.Size(15, 14);
+			this.BigEndianCheckBox.TabIndex = 5;
+			this.BigEndianCheckBox.UseVisualStyleBackColor = true;
+			// 
+			// BigEndianLabel
+			// 
+			this.BigEndianLabel.Location = new System.Drawing.Point(6, 147);
+			this.BigEndianLabel.Name = "BigEndianLabel";
+			this.BigEndianLabel.Text = "Big Endian";
+			// 
+			// AddressBox
+			// 
+			this.AddressBox.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+			this.AddressBox.Location = new System.Drawing.Point(90, 36);
+			this.AddressBox.MaxLength = 8;
+			this.AddressBox.Name = "AddressBox";
+			this.AddressBox.Nullable = false;
+			this.AddressBox.Size = new System.Drawing.Size(123, 20);
+			this.AddressBox.TabIndex = 1;
+			this.AddressBox.Text = "00000000";
+			// 
+			// WatchEditor
+			// 
+			this.AcceptButton = this.OK;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.CancelButton = this.Cancel;
+			this.ClientSize = new System.Drawing.Size(225, 206);
+			this.Controls.Add(this.BigEndianLabel);
+			this.Controls.Add(this.BigEndianCheckBox);
+			this.Controls.Add(this.DisplayTypeLabel);
+			this.Controls.Add(this.DisplayTypeDropDown);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.SizeDropDown);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.DomainDropDown);
+			this.Controls.Add(this.Cancel);
+			this.Controls.Add(this.OK);
+			this.Controls.Add(this.NotesBox);
+			this.Controls.Add(this.AddressBox);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "WatchEditor";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "New Watch";
+			this.Load += new System.EventHandler(this.RamWatchNewWatch_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -207,8 +214,9 @@
 		private System.Windows.Forms.ComboBox DomainDropDown;
 		private BizHawk.WinForms.Controls.LocLabelEx label3;
 		private System.Windows.Forms.ComboBox SizeDropDown;
-		private BizHawk.WinForms.Controls.LocLabelEx DisplayTypeLael;
+		private BizHawk.WinForms.Controls.LocLabelEx DisplayTypeLabel;
 		private System.Windows.Forms.ComboBox DisplayTypeDropDown;
 		private System.Windows.Forms.CheckBox BigEndianCheckBox;
-    }
+		private WinForms.Controls.LocLabelEx BigEndianLabel;
+	}
 }
