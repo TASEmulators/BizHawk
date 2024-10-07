bcf7f981f4399a0198a53c40cf579f25c1d9ed09
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2024-10-07 01:12:35 -0700

    Rebuild Floooh for Linux

--- ExternalProjects/FlooohZ80/build_linux.sh
+++ ExternalProjects/FlooohZ80/build_linux.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 if [ -z "$BIZHAWKBUILD_HOME" ]; then export BIZHAWKBUILD_HOME="$(realpath "$(dirname "$0")/../..")"; fi
-if [ -z "$CXX" ]; then export CXX="clang++"; fi
+if [ -z "$CC" ]; then export CXX="clang"; fi
 
 mkdir -p build
-$CXX -std=c++11 -O3 -flto -fvisibility=hidden -fPIC -shared -s FlooohZ80.cpp -o build/libFlooohZ80.so
+$CC -std=c11 -O3 -fvisibility=hidden -fPIC -shared -s FlooohZ80.c -o build/libFlooohZ80.so
 
 cp build/libFlooohZ80.so "$BIZHAWKBUILD_HOME/Assets/dll"
 if [ -e "$BIZHAWKBUILD_HOME/output" ]; then
