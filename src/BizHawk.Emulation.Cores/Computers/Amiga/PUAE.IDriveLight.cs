b1c43a6379c3de95cee6afb9f77dae889ad14f06
feos <feykomylce@gmail.com>
2024-12-09 20:39:54 +0300

    file

---
+++ src/BizHawk.Emulation.Cores/Computers/Amiga/PUAE.IDriveLight.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using BizHawk.Common;
+using BizHawk.Emulation.Common;
+
+namespace BizHawk.Emulation.Cores.Computers.Amiga
+{
+	public partial class PUAE : IDriveLight
+	{
+		public bool DriveLightEnabled { get; }
+		public bool DriveLightOn { get; private set; }
+		public string DriveLightIconDescription => "Floppy Drive Activity";
+	}
+}
