ebd9cde0631c158713f3ca0e3fbb37f113e2ce7c
YoshiRulz <OSSYoshiRulz+git@gmail.com>
2025-01-08 03:10:58 +1000

    Bump `VersionInfo.MainVersion` for dev builds

--- src/BizHawk.Common/VersionInfo.cs
+++ src/BizHawk.Common/VersionInfo.cs
@@ -10,7 +10,7 @@ namespace BizHawk.Common
 		/// Bump this immediately after release.
 		/// Only use '0'..'9' and '.' or it will fail to parse and the new version notification won't work.
 		/// </remarks>
-		public static readonly string MainVersion = "2.10";
+		public static readonly string MainVersion = "2.10.1";
 
 		public static readonly string ReleaseDate = "January 7th, 2025";
 
