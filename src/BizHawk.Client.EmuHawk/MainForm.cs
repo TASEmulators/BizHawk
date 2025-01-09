fe871ba5c7c8688e60dac6dedb136ae1531e932d
YoshiRulz <OSSYoshiRulz+git@gmail.com>
2025-01-09 22:35:08 +1000

    Prompt to switch cores before recording with A2600Hawk or TI83Hawk

--- src/BizHawk.Client.EmuHawk/MainForm.cs
+++ src/BizHawk.Client.EmuHawk/MainForm.cs
@@ -4275,7 +4275,9 @@ namespace BizHawk.Client.EmuHawk
 			var recommendedCore = currentCoreName switch
 			{
 				CoreNames.Snes9X => CoreNames.Bsnes115,
+				CoreNames.TI83Hawk => CoreNames.Emu83,
 				CoreNames.QuickNes => CoreNames.NesHawk,
+				CoreNames.Atari2600Hawk => CoreNames.Stella,
 				CoreNames.HyperNyma => CoreNames.TurboNyma,
 				_ => null
 			};
