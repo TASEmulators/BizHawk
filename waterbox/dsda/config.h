db2da6bbd2fd662750a5b0a1f528f4c940836eaf
SergioMartin86 <eienburuu@gmail.com>
2025-01-26 19:20:57 +0100

    Compiling/linking dsda

---
+++ waterbox/dsda/config.h
@@ -0,0 +1,35 @@
+#define PACKAGE_NAME "dsda-doom"
+#define PACKAGE_TARNAME "dsda-doom"
+#define WAD_DATA "dsda-doom.wad"
+#define PACKAGE_VERSION "0.28.2"
+#define PACKAGE_STRING "dsda-doom 0.28.2"
+
+#define DOOMWADDIR "/usr/local/share/games/doom"
+#define DSDA_ABSOLUTE_PWAD_PATH "/usr/local/share/games/doom"
+
+/* #undef WORDS_BIGENDIAN */
+
+#define HAVE_GETOPT
+#define HAVE_MMAP
+/* #undef HAVE_CREATE_FILE_MAPPING */
+#define HAVE_STRSIGNAL
+#define HAVE_MKSTEMP
+#define HAVE_GETPWUID
+
+#define HAVE_SYS_WAIT_H
+#define HAVE_UNISTD_H
+//#define HAVE_ASM_BYTEORDER_H
+#define HAVE_DIRENT_H
+
+/* #undef HAVE_LIBSDL2_IMAGE */
+/* #undef HAVE_LIBSDL2_MIXER */
+
+/* #undef HAVE_LIBMAD */
+/* #undef HAVE_LIBFLUIDSYNTH */
+/* #undef HAVE_LIBDUMB */
+/* #undef HAVE_LIBVORBISFILE */
+/* #undef HAVE_LIBPORTMIDI */
+
+/* #undef SIMPLECHECKS */
+
+/* #undef RANGECHECK */
