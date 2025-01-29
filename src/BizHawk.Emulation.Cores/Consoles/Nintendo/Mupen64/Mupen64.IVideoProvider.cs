ff8a511ced7c8f887274e626fe9b8c0e6d344571
Morilli <35152647+Morilli@users.noreply.github.com>
2025-01-29 17:15:12 +0100

    implement video frame skip

--- src/BizHawk.Emulation.Cores/Consoles/Nintendo/Mupen64/Mupen64.IVideoProvider.cs
+++ src/BizHawk.Emulation.Cores/Consoles/Nintendo/Mupen64/Mupen64.IVideoProvider.cs
@@ -23,29 +23,33 @@ public partial class Mupen64 : IVideoProvider
 
 	private int[] _videoBuffer = [ ];
 	private byte[] _retVideoBuffer = [ ];
+	private bool _render;
 
 	private readonly EventWaitHandle _frameFinished = new AutoResetEvent(false);
 
 	private void FrameCallback(uint frameIndex)
 	{
-		int width = 0;
-		int height = 0;
-		VideoPluginApi.ReadScreen2(IntPtr.Zero, ref width, ref height, 1);
-		Debug.Assert(width <= BufferWidth);
-		Debug.Assert(height <= BufferHeight);
-
-		Array.Clear(_videoBuffer, width * height, _videoBuffer.Length - width * height);
-
-		VideoPluginApi.ReadScreen2(_retVideoBuffer, ref width, ref height, 1);
-		// the returned video buffer is in format RGB888 and also flipped vertically
-		for (int y = 0; y < height; y++)
-		for (int x = 0; x < width; x++)
+		if (_render)
 		{
-			byte r = _retVideoBuffer[3*(height-y-1) * width + 3*x];
-			byte g = _retVideoBuffer[3*(height-y-1) * width + 3*x + 1];
-			byte b = _retVideoBuffer[3*(height-y-1) * width + 3*x + 2];
-			int argb = (r << 16) | (g << 8) | (b << 0);
-			_videoBuffer[y * width + x] = argb;
+			int width = 0;
+			int height = 0;
+			VideoPluginApi.ReadScreen2(IntPtr.Zero, ref width, ref height, 1);
+			Debug.Assert(width <= BufferWidth);
+			Debug.Assert(height <= BufferHeight);
+
+			Array.Clear(_videoBuffer, width * height, _videoBuffer.Length - width * height);
+
+			VideoPluginApi.ReadScreen2(_retVideoBuffer, ref width, ref height, 1);
+			// the returned video buffer is in format RGB888 and also flipped vertically
+			for (int y = 0; y < height; y++)
+			for (int x = 0; x < width; x++)
+			{
+				byte r = _retVideoBuffer[3 * (height - y - 1) * width + 3 * x];
+				byte g = _retVideoBuffer[3 * (height - y - 1) * width + 3 * x + 1];
+				byte b = _retVideoBuffer[3 * (height - y - 1) * width + 3 * x + 2];
+				int argb = (r << 16) | (g << 8) | (b << 0);
+				_videoBuffer[y * width + x] = argb;
+			}
 		}
 
 		_frameFinished.Set();
