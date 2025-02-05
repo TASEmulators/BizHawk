15a34e789addd6dd73d05948a6e74b8775dfdc3b
Sergio Martin <martiser@ethz.ch>
2025-02-05 06:15:12 +0100

    Adding importer for Hexen movies

--- src/BizHawk.Client.Common/movie/import/HexenLmpImport.cs
+++ src/BizHawk.Client.Common/movie/import/HexenLmpImport.cs
@@ -8,15 +8,9 @@ namespace BizHawk.Client.Common
 {
 	// LMP file format: https://doomwiki.org/wiki/Demo#Technical_information
 	// In better detail, from archive.org: http://web.archive.org/web/20070630072856/http://demospecs.planetquake.gamespy.com/lmp/lmp.html
-	[ImporterFor("Doom", ".lmp")]
-	internal class LmpImport : MovieImporter
-	{
-		private enum DemoFormat
-		{
-			DoomUpTo12,
-			DoomPost12
-		}
-
+	[ImporterFor("Hexen", ".hexenlmp")]
+	internal class HexenLmpImport : MovieImporter
+	{ 
 		protected override void RunImport()
 		{
 			Result.Movie.HeaderEntries[HeaderKeys.Core] = CoreNames.DSDA;
@@ -30,50 +24,47 @@ namespace BizHawk.Client.Common
 			var input = SourceFile.OpenRead().ReadAllBytes();
 			Stream sr = new MemoryStream(input);
 
-			// Reading signature
-			var signature = sr.ReadByte();
-
-			// Try to decide game version based on signature
-			DemoFormat presumedFormat = DemoFormat.DoomUpTo12;
-			DSDA.CompatibilityLevelEnum presumedCompatibilityLevel = DSDA.CompatibilityLevelEnum.C0;
-			if (signature > 102) // >1.2
-			{
-			   presumedFormat = DemoFormat.DoomPost12;
-			   if (signature < 109) presumedCompatibilityLevel = DSDA.CompatibilityLevelEnum.C1; // 1.666
-			   if (signature >= 109) presumedCompatibilityLevel = DSDA.CompatibilityLevelEnum.C2; // 1.9
-			   Console.WriteLine("Reading DOOM LMP demo version: {0}", signature);
-			}
-			else Console.WriteLine("Reading DOOM LMP demo version: <=1.12");
-
 			// Parsing header
-			byte skillLevel = (byte) signature; // For <=1.2, the first byte is already the skill level
-			if (presumedFormat == DemoFormat.DoomPost12) skillLevel = (byte)sr.ReadByte();
+			byte skillLevel = (byte)sr.ReadByte();
 			byte episode = (byte) sr.ReadByte();
 			byte map = (byte) sr.ReadByte();
-			byte multiplayerMode = (byte) sr.ReadByte();
-			byte monstersRespawn = (byte) sr.ReadByte();
-			byte fastMonsters = (byte) sr.ReadByte();
-			byte noMonsters = (byte) sr.ReadByte();
-			byte displayPlayer = (byte) sr.ReadByte();
 			byte player1Present = (byte) sr.ReadByte();
+			byte player1Class = (byte) sr.ReadByte();
 			byte player2Present = (byte) sr.ReadByte();
+			byte player2Class = (byte) sr.ReadByte();
 			byte player3Present = (byte) sr.ReadByte();
+			byte player3Class = (byte) sr.ReadByte();
 			byte player4Present = (byte) sr.ReadByte();
+			byte player4Class = (byte) sr.ReadByte();
+
+			// Players [5-8] ignored
+			byte player5Present = (byte) sr.ReadByte();
+			byte player5Class = (byte) sr.ReadByte();
+			byte player6Present = (byte) sr.ReadByte();
+			byte player6Class = (byte) sr.ReadByte();
+			byte player7Present = (byte) sr.ReadByte();
+			byte player7Class = (byte) sr.ReadByte();
+			byte playerPresent = (byte) sr.ReadByte();
+			byte player8Class = (byte) sr.ReadByte();
 
 			// Setting values
 			syncSettings.SkillLevel = (DSDA.SkillLevelEnum) (skillLevel+1);
 			syncSettings.InitialEpisode = episode;
 			syncSettings.InitialMap = map;
-			syncSettings.MultiplayerMode = (DSDA.MultiplayerModeEnum) multiplayerMode;
-			syncSettings.MonstersRespawn = monstersRespawn > 0;
-			syncSettings.FastMonsters = fastMonsters > 0;
-			syncSettings.NoMonsters = noMonsters > 0;
-			settings.DisplayPlayer = displayPlayer;
+			syncSettings.MultiplayerMode = DSDA.MultiplayerModeEnum.M0;
+			syncSettings.MonstersRespawn = false;
+			syncSettings.FastMonsters = false;
+			syncSettings.NoMonsters = false;
+			settings.DisplayPlayer = 0;
 			syncSettings.Player1Present = player1Present > 0;
+			syncSettings.Player1Class = (DSDA.HexenClassEnum) player1Class;
 			syncSettings.Player2Present = player2Present > 0;
+			syncSettings.Player2Class = (DSDA.HexenClassEnum) player2Class;
 			syncSettings.Player3Present = player3Present > 0;
+			syncSettings.Player3Class = (DSDA.HexenClassEnum) player3Class;
 			syncSettings.Player4Present = player4Present > 0;
-			syncSettings.CompatibilityMode = presumedCompatibilityLevel;
+			syncSettings.Player4Class = (DSDA.HexenClassEnum) player4Class;
+			syncSettings.CompatibilityMode = DSDA.CompatibilityLevelEnum.C0;
 
 			var doomController1 = new DoomController(0);
 			var controller = new SimpleController(doomController1.Definition);
@@ -123,6 +114,18 @@ namespace BizHawk.Client.Common
 
 			bool altWeapon = (specialValue & 0b00100000) > 5;
 			controller[$"P{playerId} Alt Weapon"] = altWeapon;
+
+			sbyte flylook = (sbyte) sr.ReadByte();
+			controller.AcceptNewAxis($"P{playerId} Fly / Look", flylook);
+
+			sbyte useArtifact = (sbyte) sr.ReadByte();
+			controller.AcceptNewAxis($"P{playerId} Use Artifact", useArtifact & 0b00111111);
+
+			bool isJump = (useArtifact & 0b10000000) > 1;
+			controller[$"P{playerId} Jump"] = isJump;
+
+			bool isEndPlayer = (useArtifact & 0b01000000) > 1;
+			controller[$"P{playerId} End Player"] = isEndPlayer;
 		}
 	}
 }
