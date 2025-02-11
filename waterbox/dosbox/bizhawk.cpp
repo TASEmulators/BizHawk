08f48c799239aefb005d3f934e0e5e55ad66fb44
SergioMartin86 <eienburuu@gmail.com>
2025-02-11 19:24:13 +0100

    Adding placeholder

---
+++ waterbox/dosbox/bizhawk.cpp
@@ -0,0 +1,33 @@
+#include "bizhawk.hpp"
+
+ECL_EXPORT bool Init(int argc, char **argv)
+{
+	return true;
+}
+
+ECL_EXPORT void FrameAdvance(MyFrameInfo* f)
+{
+}
+
+uint8_t mainRAM[256];
+
+ECL_EXPORT void GetMemoryAreas(MemoryArea *m)
+{
+	m[0].Data  = mainRAM;
+	m[0].Name  = "Main RAM";
+	m[0].Size  = 256;
+	m[0].Flags = MEMORYAREA_FLAGS_WORDSIZE1 | MEMORYAREA_FLAGS_WRITABLE | MEMORYAREA_FLAGS_PRIMARY;
+
+}
+
+void (*LEDCallback)();
+ECL_EXPORT void SetLEDCallback(void (*callback)())
+{
+	LEDCallback = callback;
+}
+
+void (*InputCallback)();
+ECL_EXPORT void SetInputCallback(void (*callback)())
+{
+	InputCallback = callback;
+}
---
+++ waterbox/dosbox/bizhawk.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+// system
+#include <limits.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+// waterbox
+#include "emulibc.h"
+#include "waterboxcore.h"
+
+typedef struct
+{
+	int value;
+} MyFrameInfo;
