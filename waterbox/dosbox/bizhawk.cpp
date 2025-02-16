c160b745fdabbb4d131075ca12c57cf8ad11ec8d
SergioMartin86 <eienburuu@gmail.com>
2025-02-16 08:17:27 +0100

    Now running timer correctly

--- waterbox/dosbox/bizhawk.cpp
+++ waterbox/dosbox/bizhawk.cpp
@@ -11,6 +11,11 @@ extern void VGA_SetupDrawing(Bitu /*val*/);
 cothread_t _emuCoroutine;
 cothread_t _driverCoroutine;
 
+#define __FPS__ 60
+double ticksElapsed;
+uint32_t ticksElapsedInt;
+uint32_t _GetTicks() { return ticksElapsedInt; }
+
 ECL_EXPORT bool Init(int argc, char **argv)
 {
 	// FILE* f = fopen("FloppyDisk0", "rb");
@@ -28,6 +33,10 @@ ECL_EXPORT bool Init(int argc, char **argv)
 		setenv("SDL_VIDEODRIVER", "dummy", 1);
 		setenv("SDL_AUDIODRIVER", "dummy", 1);
 
+		// Setting timer
+		ticksElapsed = 0.0;
+		ticksElapsedInt = 0;
+
 	_driverCoroutine = co_active();
 	constexpr size_t stackSize = 4 * 1024 * 1024;
 	_emuCoroutine = co_create(stackSize, runMain);
@@ -38,26 +47,21 @@ ECL_EXPORT bool Init(int argc, char **argv)
 
 ECL_EXPORT void FrameAdvance(MyFrameInfo* f)
 {
-	co_switch(_emuCoroutine);
+	// Advancing timer
+	constexpr double ticksPerFrame = 1000.0 / __FPS__;
+	ticksElapsed += ticksPerFrame; // Miliseconds per frame
+	ticksElapsedInt = (uint32_t)std::floor(ticksElapsed);
+	printf("Time Elapsed: %f / %u (delta: %f)\n", ticksElapsed,ticksElapsedInt,ticksPerFrame);
 
-	VGA_SetupDrawing(0);
+	co_switch(_emuCoroutine);
 
 	printf("w: %u, h: %u, bytes: %p\n", sdl.surface->w, sdl.surface->h, sdl.surface->pixels);
 	f->base.Width = sdl.surface->w;
 	f->base.Height = sdl.surface->h;
 
-	if (render.scale.cacheRead != nullptr)
-	{
-			printf("cacheRead: %p\n", sdl.surface);
-			printf("w: %u, h: %u, bytes: %p\n", sdl.surface->w, sdl.surface->h, render.scale.cacheRead);
-			size_t checksum = 0;
-			for (size_t i = 0; i < sdl.surface->w * sdl.surface->h * 4; i++) checksum += ((uint8_t*)render.scale.cacheRead)[i];
-			printf("Checksum: %lu\n", checksum);
-	}
-
-	// 	size_t checksum = 0;
-	// for (size_t i = 0; i < sdl.surface->w * sdl.surface->h * 4; i++) checksum += ((uint8_t*)sdl.surface->pixels)[i];
-	// printf("Checksum: %lu\n", checksum);
+	size_t checksum = 0;
+	for (size_t i = 0; i < sdl.surface->w * sdl.surface->h * 4; i++) checksum += ((uint8_t*)sdl.surface->pixels)[i];
+	printf("Video checksum: %lu\n", checksum);
 	memcpy(f->base.VideoBuffer, sdl.surface->pixels, f->base.Width * f->base.Height * 4);
 }
 
