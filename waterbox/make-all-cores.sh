44dc0f4461f104b9bcebe93cb03dc4d739c6b0c4
Morilli <35152647+Morilli@users.noreply.github.com>
2025-03-14 14:48:38 +0100

    pass args and fix ares build call

--- waterbox/make-all-cores.sh
+++ waterbox/make-all-cores.sh
@@ -2,25 +2,25 @@
 set -e
 cd "$(dirname "$0")"
 
-make -C emulibc
-make -C libco
-make -C ares64
-make -C bsnescore
-make -C dsda
-make -C gpgx
-make -C libsnes
-make -C melon
-make -C picodrive
-make -C stella
-make -C snes9x
-make -C tic80
-make -C uae
-make -C uzem
-make -C virtualjaguar
+make -C emulibc $1 -j
+make -C libco $1 -j
+./ares64/make-both.sh $1
+make -C bsnescore $1 -j
+make -C dsda $1 -j
+make -C gpgx $1 -j
+make -C libsnes $1 -j
+make -C melon $1 -j
+make -C picodrive $1 -j
+make -C stella $1 -j
+make -C snes9x $1 -j
+make -C tic80 $1 -j
+make -C uae $1 -j
+make -C uzem $1 -j
+make -C virtualjaguar $1 -j
 ./nyma/make-all-released-cores.sh $1
 
 # this won't include MAME by default, due to the large amount of time it takes to build it
 # to include MAME just do INCLUDE_MAME=1 ./make-all-cores.sh
 if test "$INCLUDE_MAME" ; then
-	make -C mame-arcade
+	make -C mame-arcade $1 -j
 fi
