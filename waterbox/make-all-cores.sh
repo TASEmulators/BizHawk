3a4815d3da40522b106edacd663a905a80f2e447
Morilli <35152647+Morilli@users.noreply.github.com>
2025-03-14 14:41:10 +0100

    make make-all-cores.sh error properly

--- waterbox/make-all-cores.sh
+++ waterbox/make-all-cores.sh
@@ -1,25 +1,26 @@
 #!/bin/sh
 set -e
+cd "$(dirname "$0")"
 
-cd emulibc && make -f Makefile $1 -j && cd -
-cd libco && make -f Makefile $1 -j && cd -
-cd ares64 && ./make-both.sh $1 && cd -
-cd bsnescore && make -f Makefile $1 -j && cd -
-cd dsda && make -f Makefile $1 -j && cd -
-cd gpgx && make -f Makefile $1 -j && cd -
-cd libsnes && make -f Makefile $1 -j && cd -
-cd melon && make -f Makefile $1 -j && cd -
-cd picodrive && make -f Makefile $1 -j && cd -
-cd stella && make -f Makefile $1 -j && cd -
-cd snes9x && make -f Makefile $1 -j && cd -
-cd tic80 && make -f Makefile $1 -j && cd -
-cd uae && make -f Makefile $1 -j && cd -
-cd uzem && make -f Makefile $1 -j && cd -
-cd virtualjaguar && make -f Makefile $1 -j && cd -
-cd nyma && ./make-all-released-cores.sh $1 && cd -
+make -C emulibc
+make -C libco
+make -C ares64
+make -C bsnescore
+make -C dsda
+make -C gpgx
+make -C libsnes
+make -C melon
+make -C picodrive
+make -C stella
+make -C snes9x
+make -C tic80
+make -C uae
+make -C uzem
+make -C virtualjaguar
+./nyma/make-all-released-cores.sh $1
 
 # this won't include MAME by default, due to the large amount of time it takes to build it
 # to include MAME just do INCLUDE_MAME=1 ./make-all-cores.sh
 if test "$INCLUDE_MAME" ; then
-	cd mame-arcade && make -f Makefile $1 -j && cd -
+	make -C mame-arcade
 fi
