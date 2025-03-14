4a3ad3a1d1437a5bb5b3829dc849da7ef28be424
feos <feykomylce@gmail.com>
2025-03-14 18:10:36 +0300

    dsda: fix build, spacing

--- waterbox/dsda/BizhawkInterface.h
+++ waterbox/dsda/BizhawkInterface.h
@@ -3,20 +3,25 @@
 
 #include "emulibc.h"
 #include "d_player.h"
-#include "w_wad.h"
-#include "p_mobj.h"
 #include "doomstat.h"
 #include "g_game.h"
-
+#include "p_mobj.h"
+#include "w_wad.h"
 #include "dsda/args.h"
 
 extern int headlessMain(int argc, char **argv);
 extern void headlessRunSingleTick();
-extern void headlessUpdateSounds(void);
 extern void headlessClearTickCommand();
 extern void headlessSetTickCommand(int playerId, int forwardSpeed, int strafingSpeed, int turningSpeed, int fire, int action, int weapon, int automap, int lookfly, int artifact, int jump, int endPlayer);
+extern void headlessGetMapName(char *outString);
+extern void headlessSetSaveStatePointer(void *savePtr, int saveStateSize);
+extern size_t headlessGetEffectiveSaveSize();
+extern void dsda_ArchiveAll(void);
+extern void dsda_UnArchiveAll(void);
+extern void D_AddFile (const char *file, wad_source_t source, void *const buffer, const size_t size);
+extern void AddIWAD(const char *iwad, void *const buffer, const size_t size);
 
-  // Video-related functions
+// Video
 extern void headlessUpdateVideo(void);
 extern void* headlessGetVideoBuffer();
 extern int headlessGetVideoPitch();
@@ -24,21 +29,15 @@ extern int headlessGetVideoWidth();
 extern int headlessGetVideoHeight();
 extern void headlessEnableVideoRendering();
 extern void headlessDisableVideoRendering();
+extern uint32_t* headlessGetPallette();
+
+// Audio
+extern void headlessUpdateSounds(void);
 extern void headlessEnableAudioRendering();
 extern void headlessDisableAudioRendering();
-  uint32_t* headlessGetPallette();
-
-extern void headlessSetSaveStatePointer(void* savePtr, int saveStateSize);
-  size_t headlessGetEffectiveSaveSize();
-extern void dsda_ArchiveAll(void);
-extern void dsda_UnArchiveAll(void);
-extern void headlessGetMapName(char* outString);
-  
-extern void D_AddFile (const char *file, wad_source_t source, void* const buffer, const size_t size);
-extern void AddIWAD(const char *iwad, void* const buffer, const size_t size); 
-extern unsigned char * I_CaptureAudio (int* nsamples);
 extern void I_InitSound(void);
 extern void I_SetSoundCap (void);
+extern uint8_t *I_CaptureAudio (int *nsamples);
 
 // Players information
 extern int enableOutput;
@@ -55,45 +54,45 @@ uint32_t _convertedPaletteBuffer[PALETTE_SIZE];
 
 enum MemoryArrayType
 {
-	ARRAY_THINGS = 0,
-	ARRAY_LINES = 1,
-	ARRAY_SECTORS = 2
+  ARRAY_THINGS = 0,
+  ARRAY_LINES = 1,
+  ARRAY_SECTORS = 2
 };
 
 struct InitSettings
 {
-	int _Player1Present;
-	int _Player2Present;
-	int _Player3Present;
-	int _Player4Present;
-	int _Player1Class;
-	int _Player2Class;
-	int _Player3Class;
-	int _Player4Class;
-	int _PreventLevelExit;
-	int _PreventGameEnd;
+  int Player1Present;
+  int Player2Present;
+  int Player3Present;
+  int Player4Present;
+  int Player1Class;
+  int Player2Class;
+  int Player3Class;
+  int Player4Class;
+  int PreventLevelExit;
+  int PreventGameEnd;
 } __attribute__((packed));
 
 struct PackedPlayerInput
 {
-	int _RunSpeed;
-	int _StrafingSpeed;
-	int _TurningSpeed;
-	int _WeaponSelect;
-	int _Fire;
-	int _Action;
-	int _Automap;
-	int _FlyLook;
-	int _ArtifactUse;
-	int _Jump;
-	int _EndPlayer;
+  int RunSpeed;
+  int StrafingSpeed;
+  int TurningSpeed;
+  int WeaponSelect;
+  int Fire;
+  int Action;
+  int Automap;
+  int FlyLook;
+  int ArtifactUse;
+  int Jump;
+  int EndPlayer;
 } __attribute__((packed));
 
 struct PackedRenderInfo
 {
-	int _RenderVideo;
-	int _RenderAudio;
-	int _PlayerPointOfView;
+  int RenderVideo;
+  int RenderAudio;
+  int PlayerPointOfView;
 } __attribute__((packed));
 
 #endif
