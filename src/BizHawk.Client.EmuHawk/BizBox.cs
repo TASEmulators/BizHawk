b88c557b461f1e841ef8bfd16129bdefba7afd02
Morilli <35152647+Morilli@users.noreply.github.com>
2025-03-26 19:23:25 +0100

    speedup BizBox (About dialog) load

--- src/BizHawk.Client.EmuHawk/BizBox.cs
+++ src/BizHawk.Client.EmuHawk/BizBox.cs
@@ -36,6 +36,8 @@ namespace BizHawk.Client.EmuHawk
 			VersionLabel.Text = VersionInfo.GetFullVersionDetails();
 			DateLabel.Text = VersionInfo.ReleaseDate;
 			(linkLabel2.Text, linkLabel2.Tag) = VersionInfo.GetGitCommitLink();
+
+			CoreInfoPanel.SuspendLayout();
 			foreach (var core in CoreInventory.Instance.SystemsFlat
 				.OrderBy(static core => core.CoreAttr.Released)
 				.ThenByDescending(static core => core.Name, StringComparer.OrdinalIgnoreCase))
@@ -45,6 +47,7 @@ namespace BizHawk.Client.EmuHawk
 					Dock = DockStyle.Top
 				});
 			}
+			CoreInfoPanel.ResumeLayout();
 		}
 
 		private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
