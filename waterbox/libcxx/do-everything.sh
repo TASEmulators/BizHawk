cf12159f55af489b78973df282160b1c0d6ed234
Morilli <35152647+Morilli@users.noreply.github.com>
2025-03-31 02:56:48 +0200

    make do-everything.sh fail properly

--- waterbox/libcxx/do-everything.sh
+++ waterbox/libcxx/do-everything.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 set -e
 ./setup-llvm.sh
-./configure-for-waterbox-phase-- && cd build- && make -j && make install && cd ..
-printf "completed phase -1\n"
-./configure-for-waterbox-phase-0 && cd build0 && make -j && make install && cd ..
-printf "completed phase 0\n"
-./configure-for-waterbox-phase-1 && cd build1 && make -j && make install && cd ..
-printf "completed phase 1\n"
-./configure-for-waterbox-phase-2 && cd build2 && make -j && make install && cd ..
-printf "completed phase 2\n"
+
+configure_and_install () {
+	build_dir=build$1
+	./configure-for-waterbox-phase-$1
+	cmake --build $build_dir
+	cmake --install $build_dir
+	printf "completed phase $1\n"
+}
+
+configure_and_install -
+configure_and_install 0
+configure_and_install 1
+configure_and_install 2
