e672cc4cd864083b4bc6809ad9792f9ba4b41380
Morilli <35152647+Morilli@users.noreply.github.com>
2025-04-11 14:32:47 +0200

    remove unused using

--- src/BizHawk.Client.Common/movie/tasproj/TasMovie.cs
+++ src/BizHawk.Client.Common/movie/tasproj/TasMovie.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
 using System.IO;
-using System.Linq;
 
 using BizHawk.Common.StringExtensions;
 using BizHawk.Emulation.Common;
