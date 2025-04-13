75fc58041e16dcfca68cf6eca2509396eea409e4
feos <feykomylce@gmail.com>
2025-04-13 10:44:59 +0300

    bind mouse capture to middle mouse button
    pretty standard across emus that do this upon hotkey

--- src/BizHawk.Client.Common/config/Binding.cs
+++ src/BizHawk.Client.Common/config/Binding.cs
@@ -66,7 +66,7 @@ namespace BizHawk.Client.Common
 			Bind("General", "Toggle Messages");
 			Bind("General", "Toggle Display Nothing");
 			Bind("General", "Accept Background Input");
-			Bind("General", "Capture Mouse");
+			Bind("General", "Capture Mouse", "WMouse M");
 
 			Bind("Save States", "Save State 1", "Shift+F1");
 			Bind("Save States", "Save State 2", "Shift+F2");
