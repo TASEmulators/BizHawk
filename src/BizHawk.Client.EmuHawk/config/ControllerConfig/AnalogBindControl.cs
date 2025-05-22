498fabc576380e99bfd733251aa1016603de113b
YoshiRulz <OSSYoshiRulz+git@gmail.com>
2025-05-23 03:50:36 +1000

    Make binding an axis to the same mouse axis twice switch to relative
    resolves #4310

--- src/BizHawk.Client.EmuHawk/config/ControllerConfig/AnalogBindControl.cs
+++ src/BizHawk.Client.EmuHawk/config/ControllerConfig/AnalogBindControl.cs
@@ -37,10 +37,25 @@ namespace BizHawk.Client.EmuHawk
 			{
 				timer1.Stop();
 				_listening = false;
-				_bind.Value = bindValue;
-				textBox1.Text = Bind.Value;
 				buttonBind.Text = "Bind!";
 				Input.Instance.StopListeningForAxisEvents();
+				// rebinding the same mouse axis toggles between relative and absolute
+				const string AXIS_MX = "WMouse X";
+				const string AXIS_MRX = "RMouse X";
+				const string AXIS_MY = "WMouse Y";
+				const string AXIS_MRY = "RMouse Y";
+				var oldBind = _bind.Value;
+				if (bindValue is AXIS_MX)
+				{
+					if (oldBind is AXIS_MX) bindValue = AXIS_MRX;
+					else if (oldBind is AXIS_MRX) bindValue = AXIS_MX;
+				}
+				else if (bindValue is AXIS_MY)
+				{
+					if (oldBind is AXIS_MY) bindValue = AXIS_MRY;
+					else if (oldBind is AXIS_MRY) bindValue = AXIS_MY;
+				}
+				textBox1.Text = _bind.Value = bindValue;
 			}
 		}
 
