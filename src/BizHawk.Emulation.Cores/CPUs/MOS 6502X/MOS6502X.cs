d465ea006216573afefbba696f14db58182bfbae
100thCoin <onehundredthcoin@gmail.com>
2025-05-24 23:03:46 -0400

    Added the 6502 address bus to the SyncState
    Added the 6502 address bus to the SyncState

--- src/BizHawk.Emulation.Cores/CPUs/MOS 6502X/MOS6502X.cs
+++ src/BizHawk.Emulation.Cores/CPUs/MOS 6502X/MOS6502X.cs
@@ -185,6 +185,7 @@ namespace BizHawk.Emulation.Cores.Components.M6502
 			ser.Sync(nameof(rdy_freeze), ref rdy_freeze);
 			ser.Sync(nameof(ext_ppu_cycle), ref ext_ppu_cycle);
 			ser.Sync(nameof(H), ref H);
+			ser.Sync(nameof(address_bus), ref address_bus);
 			ser.EndSection();
 		}
 
