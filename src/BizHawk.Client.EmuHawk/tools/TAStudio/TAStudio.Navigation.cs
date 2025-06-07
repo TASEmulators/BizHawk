3ef7dce1ed7df04243bf83d673308dcdcb44dcb9
SuuperW <SuuperW@gmail.com>
2025-06-07 01:35:21 -0500

    GoToFrame's "future frame" cases were just buggy versions of the normal case. So just use the code of StartAtNearestFrameAndEmulate instead. Fixes multiple bugs when seeking to frames past the end of the movie.

--- src/BizHawk.Client.EmuHawk/tools/TAStudio/TAStudio.Navigation.cs
+++ src/BizHawk.Client.EmuHawk/tools/TAStudio/TAStudio.Navigation.cs
@@ -4,6 +4,81 @@ namespace BizHawk.Client.EmuHawk
 {
 	public partial class TAStudio
 	{
+		/// <summary> 
+		/// Seek to the given frame, past or future, and load a state first if doing so gets us there faster.
+		/// Does nothing if we are already on the given frame.
+		/// </summary>
+		public void GoToFrame(int frame, bool fromLua = false, bool fromRewinding = false, bool OnLeftMouseDown = false)
+		{
+			if (frame == Emulator.Frame)
+			{
+				return;
+			}
+
+			// Unpausing after a seek may seem like we aren't really seeking at all:
+			// what is the significance of a seek to frame if we don't pause?
+			// Answer: We use this in order to temporarily disable recording mode when the user navigates to a frame. (to avoid recording between whatever is the most recent state and the user-specified frame)
+			// as well as ... to enable turbo-seek when navigating while unpaused? not sure (because this doesn't work)
+			//   ... actually, fromRewinding is never set to true in any call AND NEVER WAS. TODO: Fix that.
+			WasRecording = CurrentTasMovie.IsRecording() || WasRecording;
+			_unpauseAfterSeeking = (fromRewinding || WasRecording) && !MainForm.EmulatorPaused;
+			TastudioPlayMode();
+
+			var closestState = GetPriorStateForFramebuffer(frame);
+			if (frame < Emulator.Frame || closestState.Key > Emulator.Frame)
+			{
+				LoadState(closestState, true);
+			}
+			closestState.Value.Dispose();
+
+			if (fromLua)
+			{
+				bool wasPaused = MainForm.EmulatorPaused;
+
+				// why not use this? because I'm not letting the form freely run. it all has to be under this loop.
+				// i could use this and then poll StepRunLoop_Core() repeatedly, but.. that's basically what I'm doing
+				// PauseOnFrame = frame;
+
+				while (Emulator.Frame != frame)
+				{
+					MainForm.SeekFrameAdvance();
+				}
+
+				if (!wasPaused)
+				{
+					MainForm.UnpauseEmulator();
+				}
+
+				// lua botting users will want to re-activate record mode automatically -- it should be like nothing ever happened
+				if (WasRecording)
+				{
+					TastudioRecordMode();
+				}
+
+				// now the next section won't happen since we're at the right spot
+			}
+
+			// Seek needs to happen if any of:
+			// 1) We should pause once reaching the target frame (that's what a seek is): currently paused OR currently seeking
+			// 2) We are using _unpauseAfterSeeking (e.g. to manage recording state)
+			// Otherwise, just don't seek and emulation will happily continue.
+			if (MainForm.EmulatorPaused || _seekingTo != -1 || _unpauseAfterSeeking)
+			{
+				StartSeeking(frame);
+			}
+			else
+			{
+				// GUI users may want to be protected from clobbering their video when skipping around...
+				// well, users who are rewinding aren't. (that gets done through the seeking system in the call above)
+				// users who are clicking around.. I don't know.
+			}
+
+			if (!OnLeftMouseDown)
+			{
+				MaybeFollowCursor();
+			}
+		}
+
 		/// <summary>
 		/// Only goes to go to the frame if it is an event before current emulation, otherwise it is just a future event that can freely be edited
 		/// </summary>
@@ -25,44 +100,6 @@ namespace BizHawk.Client.EmuHawk
 				else
 				{
 					_triggerAutoRestore = false;
-				}
-			}
-		}
-
-		public void GoToFrame(int frame, bool fromLua = false, bool fromRewinding = false, bool OnLeftMouseDown = false)
-		{
-			// If seeking to a frame before or at the end of the movie, use StartAtNearestFrameAndEmulate
-			// Otherwise, load the latest state (if not already there) and seek while recording.
-			WasRecording = CurrentTasMovie.IsRecording() || WasRecording;
-
-			if (frame <= CurrentTasMovie.InputLogLength)
-			{
-				// Get as close as we can then emulate there
-				StartAtNearestFrameAndEmulate(frame, fromLua, fromRewinding);
-				if (!OnLeftMouseDown) { MaybeFollowCursor(); }
-			}
-			else // Emulate to a future frame
-			{
-				if (frame == Emulator.Frame + 1) // We are at the end of the movie and advancing one frame, therefore we are recording, simply emulate a frame
-				{
-					bool wasPaused = MainForm.EmulatorPaused;
-					MainForm.FrameAdvance();
-					if (!wasPaused)
-					{
-						MainForm.UnpauseEmulator();
-					}
-				}
-				else
-				{
-					TastudioPlayMode();
-
-					var lastState = GetPriorStateForFramebuffer(frame);
-					if (lastState.Key > Emulator.Frame)
-					{
-						LoadState(lastState, true);
-					}
-
-					StartSeeking(frame);
 				}
 			}
 		}
