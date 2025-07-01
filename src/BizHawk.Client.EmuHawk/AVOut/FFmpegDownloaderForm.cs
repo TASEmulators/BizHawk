9cd1b09348fccf34982518b9e99004431f8a83da
YoshiRulz <OSSYoshiRulz+git@gmail.com>
2025-07-02 08:01:13 +1000

    Code style fixes to `{FFmpeg,RAIntegration}DownloaderForm`

--- src/BizHawk.Client.EmuHawk/AVOut/FFmpegDownloaderForm.cs
+++ src/BizHawk.Client.EmuHawk/AVOut/FFmpegDownloaderForm.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading;
 using System.Windows.Forms;
 
@@ -44,24 +45,16 @@ namespace BizHawk.Client.EmuHawk
 				using var fs = File.Create(FFmpegService.FFmpegPath); // check writable before bothering with the download
 				using (var evt = new ManualResetEvent(false))
 				{
-					using (var client = new System.Net.WebClient())
+					using (var client = new WebClient())
 					{
-						System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
+						ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 						client.DownloadFileAsync(new Uri(FFmpegService.Url), fn);
-						client.DownloadProgressChanged += (object sender, System.Net.DownloadProgressChangedEventArgs e) =>
-						{
-							pct = e.ProgressPercentage;
-						};
-						client.DownloadFileCompleted += (object sender, System.ComponentModel.AsyncCompletedEventArgs e) =>
-						{
-							//we don't really need a status. we'll just try to unzip it when it's done
-							evt.Set();
-						};
+						client.DownloadProgressChanged += (_, progressArgs) => pct = progressArgs.ProgressPercentage;
+						client.DownloadFileCompleted += (_, _) => evt.Set(); // we don't really need a status, we'll just try to unzip it when it's done
 
-						for (; ; )
+						while (true)
 						{
-							if (evt.WaitOne(10))
-								break;
+							if (evt.WaitOne(10)) break;
 
 							//if the gui thread ordered an exit, cancel the download and wait for it to acknowledge
 							if (exiting)
@@ -74,11 +67,10 @@ namespace BizHawk.Client.EmuHawk
 					}
 				}
 
-				//throw new Exception("test of download failure");
+//				throw new Exception("test of download failure");
 
 				//if we were ordered to exit, bail without wasting any more time
-				if (exiting)
-					return;
+				if (exiting) return;
 
 				//try acquiring file
 				using (var hf = new HawkFile(fn))
@@ -86,8 +78,7 @@ namespace BizHawk.Client.EmuHawk
 					using (var exe = OSTailoredCode.IsUnixHost ? hf.BindArchiveMember("ffmpeg") : hf.BindFirstOf(".exe"))
 					{
 						//last chance. exiting, don't dump the new ffmpeg file
-						if (exiting)
-							return;
+						if (exiting) return;
 						exe!.GetStream().CopyTo(fs);
 						fs.Dispose();
 						if (OSTailoredCode.IsUnixHost)
@@ -110,8 +101,14 @@ namespace BizHawk.Client.EmuHawk
 			}
 			finally
 			{
-				try { File.Delete(fn); }
-				catch { }
+				try
+				{
+					File.Delete(fn);
+				}
+				catch
+				{
+					// ignore
+				}
 			}
 		}
 
@@ -141,8 +138,7 @@ namespace BizHawk.Client.EmuHawk
 		private void timer1_Tick(object sender, EventArgs e)
 		{
 			//if it's done, close the window. the user will be smart enough to reopen it
-			if (succeeded)
-				Close();
+			if (succeeded) Close();
 			if (failed)
 			{
 				failed = false;
@@ -159,4 +155,3 @@ namespace BizHawk.Client.EmuHawk
 		}
 	}
 }
-
