4c48266c7fe9b87d8024b289f477cc1474e541b4
SuuperW <SuuperW@gmail.com>
2025-07-05 18:38:58 -0500

    Use the marker list collection changed event to drive TAStudio refreshing, instead of doing a manual refresh on undo and everywhere else. Is this actually any better? Another option for undo purposes could be to have Undo/Redo return true/false indicating if the only change was to markers.

--- src/BizHawk.Client.EmuHawk/tools/TAStudio/TAStudio.MenuItems.cs
+++ src/BizHawk.Client.EmuHawk/tools/TAStudio/TAStudio.MenuItems.cs
@@ -297,13 +297,11 @@ namespace BizHawk.Client.EmuHawk
 		private void UndoMenuItem_Click(object sender, EventArgs e)
 		{
 			CurrentTasMovie.ChangeLog.Undo();
-			RefreshDialog(); // ?? redundant, except if undoing markers
 		}
 
 		private void RedoMenuItem_Click(object sender, EventArgs e)
 		{
 			CurrentTasMovie.ChangeLog.Redo();
-			RefreshDialog(); // ?? redundant, except if undoing markers
 		}
 
 		private void ShowUndoHistoryMenuItem_Click(object sender, EventArgs e)
@@ -620,7 +618,6 @@ namespace BizHawk.Client.EmuHawk
 		{
 			CurrentTasMovie.Markers.RemoveAll(m => TasView.IsRowSelected(m.Frame));
 			MarkerControl.UpdateMarkerCount();
-			RefreshDialog();
 		}
 
 		private void ClearGreenzoneMenuItem_Click(object sender, EventArgs e)
