TASEmulators/BizHawk

commit fa15e35603f391ae4bdc7670dee8e72d19600ae9

Remove `GEN` `[CoreConstructor]` from PicoDrive32x
fixes a42b25976

src/BizHawk.Emulation.Cores/Consoles/Sega/PicoDrive/PicoDrive.cs
@@ -18,7 +18,6 @@ namespace BizHawk.Emulation.Cores.Consoles.Sega.PicoDrive
 		private readonly DiscSectorReader _cdReader;
 		private readonly bool _isPal;
 
-		[CoreConstructor(VSystemID.Raw.GEN, Priority = CorePriority.Low)]
 		[CoreConstructor(VSystemID.Raw.Sega32X)]
 		public PicoDrive(CoreComm comm, GameInfo game, byte[] rom, bool deterministic, SyncSettings syncSettings)
 			: this(comm, game, rom, null, deterministic, syncSettings)

commit c8ef4b7923d52a8067de9c9724ca430d5ca3cb44

Fixup 32X loading
Broken since fa15e35603f391ae4bdc7670dee8e72d19600ae9

Resolves #4419

src/BizHawk.Client.EmuHawk/RetroAchievements/RCheevos.Debug.cs
@@ -412,7 +412,6 @@ namespace BizHawk.Client.EmuHawk
 					VSystemID.Raw.GBA => ConsoleID.GBA,
 					VSystemID.Raw.GBC => ConsoleID.GBC,
 					VSystemID.Raw.GBL => ConsoleID.GB,
-					VSystemID.Raw.GEN when rom.GameInfo.GetBool("32X", false) => ConsoleID.Sega32X,
 					VSystemID.Raw.GEN => ConsoleID.MegaDrive,
 					VSystemID.Raw.GG => ConsoleID.GameGear,
 					VSystemID.Raw.GGL => ConsoleID.GameGear,

src/BizHawk.Emulation.Common/Database/Database.cs
@@ -492,7 +492,6 @@ namespace BizHawk.Emulation.Common
 
 				case ".32X":
 					game.System = VSystemID.Raw.Sega32X;
-					game.AddOption("32X", "true");
 					break;
 
 				case ".VEC":

src/BizHawk.Emulation.Cores/Consoles/Sega/PicoDrive/PicoDrive.cs
@@ -117,7 +117,7 @@ namespace BizHawk.Emulation.Cores.Consoles.Sega.PicoDrive
 			VsyncNumerator = _isPal ? 53203424 : 53693175;
 			VsyncDenominator = _isPal ? 3420 * 313 : 3420 * 262;
 
-			Is32XActive = game["32X"];
+			Is32XActive = game.System == VSystemID.Raw.Sega32X;
 		}
 
 		public bool Is32XActive { get; }