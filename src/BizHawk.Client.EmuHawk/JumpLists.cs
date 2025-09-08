ad5fa5f1133471fb3bdccc8fb282d2e0fe90ef0c
Morilli <35152647+Morilli@users.noreply.github.com>
2025-09-08 03:19:36 +0200

    Fix JumpList code and revert dynamic code
    mostly reverts c2fa9070a3fd8e787ca82a1089a40d44fe4807fb because I don't see this being helpful and fixes regression introduced in 859ea8e2491ac2594eabcdf86f6fbb1db9f493cd.

--- src/BizHawk.Client.EmuHawk/JumpLists.cs
+++ src/BizHawk.Client.EmuHawk/JumpLists.cs
@@ -1,34 +1,21 @@
-using System.IO;
 using System.Reflection;
+using System.Windows;
+using System.Windows.Shell;
 
 namespace BizHawk.Client.EmuHawk
 {
 	public static class JumpLists
 	{
-		private static readonly Type JumpList;
-		private static readonly Type JumpTask;
+		private static readonly Application App;
 
 		static JumpLists()
 		{
-			try
+			App = new Application();
+			var jmp = new JumpList
 			{
-				var presentationFramework =
-					Assembly.Load(
-						"PresentationFramework, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
-				var application = presentationFramework.GetType("System.Windows.Application");
-				JumpList = presentationFramework.GetType("System.Windows.Shell.JumpList");
-				JumpTask = presentationFramework.GetType("System.Windows.Shell.JumpTask");
-				var app = Activator.CreateInstance(application);
-				dynamic jmp = Activator.CreateInstance(JumpList);
-				jmp.ShowRecentCategory = true;
-				JumpList
-					.GetMethod("SetJumpList")
-					?.Invoke(null, new[] {app, jmp});
-			}
-			catch
-			{
-				// Do nothing
-			}
+				ShowRecentCategory = true,
+			};
+			JumpList.SetJumpList(App, jmp);
 		}
 
 		/// <summary>
@@ -38,25 +25,15 @@ namespace BizHawk.Client.EmuHawk
 		/// <param name="title">The text displayed in the jumplist entry</param>
 		public static void AddRecentItem(string fullPath, string title)
 		{
-			try
-			{
-				var execPath = AppContext.BaseDirectory;
-				dynamic ji = Activator.CreateInstance(JumpTask);
+			string exepath = Assembly.GetEntryAssembly()!.Location;
 
-				ji.ApplicationPath = execPath;
-				ji.Arguments = $"\"{fullPath}\"";
-				ji.Title = title;
-				// for some reason, this doesn't work
-				ji.WorkingDirectory = Path.GetDirectoryName(execPath);
-
-				JumpList
-					.GetMethod("AddToRecentCategory", new[] {JumpTask})
-					?.Invoke(null, new[] {ji});
-			}
-			catch
+			var ji = new JumpTask
 			{
-				// Do nothing
-			}
+				ApplicationPath = exepath,
+				Arguments = $"\"{fullPath}\"",
+				Title = title,
+			};
+			JumpList.AddToRecentCategory(ji);
 		}
 	}
 }
