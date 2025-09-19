7e909b30646e0e9e008536ab624b8039533439bf
kalimag <kalimag@users.noreply.github.com>
2025-09-19 21:05:27 +0200

    Remove try-catch

--- src/BizHawk.Common/HawkFile/HawkFile.cs
+++ src/BizHawk.Common/HawkFile/HawkFile.cs
@@ -81,16 +81,9 @@ namespace BizHawk.Common
 			string? autobind = null;
 			var split1 = SplitArchiveMemberPath(path);
 			if (split1 != null) (path, autobind) = split1.Value;
-			try
-			{
-				if (!string.IsNullOrEmpty(path)) path = Path.GetFullPath(path);
-			}
-			catch
-			{
-				// let invalid paths be handled by File.Exists check instead of exception
-			}
-			FullPathWithoutMember = path;
 			Exists = _rootExists = File.Exists(path);
+			if (_rootExists) path = Path.GetFullPath(path);
+			FullPathWithoutMember = path;
 			if (!_rootExists) return;
 
 			if (DearchivalMethod != null && allowArchives)
