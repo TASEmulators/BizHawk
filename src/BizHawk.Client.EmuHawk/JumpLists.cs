1c86b2819d0150eec63e38c25cd0aa11945fadd8
kalimag <kalimag@users.noreply.github.com>
2025-09-19 06:11:38 +0200

    Fix Windows jump list entries for libretro
    Escape double quotes in command line argument to let serialized `OpenAdvanced_Libretro` roundtrip

--- src/BizHawk.Client.EmuHawk/JumpLists.cs
+++ src/BizHawk.Client.EmuHawk/JumpLists.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Shell;
 
@@ -28,10 +29,32 @@ namespace BizHawk.Client.EmuHawk
 			var ji = new JumpTask
 			{
 				ApplicationPath = exepath,
-				Arguments = $"\"{fullPath}\"",
+				Arguments = QuoteAndEscapeArgument(fullPath),
 				Title = title,
 			};
 			JumpList.AddToRecentCategory(ji);
+		}
+
+		/// <summary>
+		/// Wrap a single command line argument in double quotes and escape characters
+		/// so that it correctly roundtrips back to <c>args[]</c> as a single argument.
+		/// </summary>
+		private static string QuoteAndEscapeArgument(string argument)
+		{
+			if (!string.IsNullOrEmpty(argument) && argument.IndexOfAny([ ' ', '"', '\t', '\n' ]) == -1)
+				return argument;
+
+			// Windows/.NET command line mangling
+			// see https://learn.microsoft.com/en-us/dotnet/api/system.environment.getcommandlineargs#remarks
+
+			// any double quote needs to be escaped with a backslash
+			// any series of backslashes *directly preceding* a double quote also need to be escaped
+			// so basically: insert \ before ", double any existing \ before a "
+			// special case: backslashes at the end of the string also need to be escaped (so the wrapping double quote isn't)
+			return '"' + Regex.Replace(argument ?? "",
+				@"(\\*)(""|\\\z)", // group $1: 0 or more backslashes; group $2: double quote, or backslash at end of string
+				"$1$1\\$2" // repeat group $1 twice, then backslash and group $2
+			) + '"';
 		}
 	}
 }
