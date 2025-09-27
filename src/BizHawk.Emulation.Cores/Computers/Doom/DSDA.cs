c92b640fc3c941319d72bcec54d7edc34756d707
feos <feykomylce@gmail.com>
2025-09-27 19:06:41 +0300

    dsda: disable damage palette when full vision is on

--- src/BizHawk.Emulation.Cores/Computers/Doom/DSDA.cs
+++ src/BizHawk.Emulation.Cores/Computers/Doom/DSDA.cs
@@ -124,8 +124,8 @@ namespace BizHawk.Emulation.Cores.Computers.Doom
 				+ $"render_wipescreen {(_syncSettings.RenderWipescreen ? 1 : 0)}\n"
 				+ $"map_trail_mode {(_settings.MapTrail ? 2 : 0)}\n"
 				+ $"map_trail_size {_settings.MapTrailSize}\n"
-				+ "render_stretch_hud 1\n"       // patch_stretch_doom_format
-				+ "boom_translucent_sprites 0\n" // may become a setting at some point
+				+ $"palette_ondamage {(_settings.FullVision ? 0 : 1)}\n"
+				+ "render_stretch_hud 1\n" // patch_stretch_doom_format
 				+ "uncapped_framerate 0\n"
 				+ "dsda_show_level_splits 0\n"
 			);
