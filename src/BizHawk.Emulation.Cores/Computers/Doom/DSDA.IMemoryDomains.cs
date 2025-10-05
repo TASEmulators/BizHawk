24f323e4f0bb8ab11b07781f925958eb7b4151f8
feos <feykomylce@gmail.com>
2025-10-06 01:11:40 +0300

    dsda: more custom domain insanity

--- src/BizHawk.Emulation.Cores/Computers/Doom/DSDA.IMemoryDomains.cs
+++ src/BizHawk.Emulation.Cores/Computers/Doom/DSDA.IMemoryDomains.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System.Buffers.Binary;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
+using BizHawk.Common;
 using BizHawk.Emulation.Common;
+using BizHawk.Emulation.Cores.Waterbox;
 
 namespace BizHawk.Emulation.Cores.Computers.Doom
 {
@@ -10,9 +14,6 @@ namespace BizHawk.Emulation.Cores.Computers.Doom
 
 		private void SetupMemoryDomains()
 		{
-			var pagesDomain = _elf.GetPagesDomain();
-			var internalMemorySize = pagesDomain.Size * 4096;
-
 			var domains = new List<MemoryDomain>
 			{
 				new MemoryDomainDelegate(
@@ -79,43 +80,211 @@ namespace BizHawk.Emulation.Cores.Computers.Doom
 					},
 					null,
 					1),
-				new MemoryDomainDelegate(
-					"Internal Memory",
-					internalMemorySize,
-					MemoryDomain.Endian.Little,
-					addr =>
+				new InternalWbxMemory(_elf),
+				_elf.GetPagesDomain(),
+			};
+
+			MemoryDomains = new MemoryDomainList(domains);
+			((BasicServiceProvider)ServiceProvider).Register(MemoryDomains);
+		}
+
+		private class InternalWbxMemory : MemoryDomain
+		{
+			private const long WBX_BASE_ADDR = 0x36F_0000_0000;
+			private const uint WBX_PAGE_SIZE = 0x1000;
+			private const uint WBX_PAGE_MASK = WBX_PAGE_SIZE - 1;
+			private const byte WBX_PAGE_READABLE_BITFLAG = 1 << 0;
+
+			private readonly WaterboxHost _exe;
+			private readonly MemoryDomain _pagesDomain;
+
+			public InternalWbxMemory(WaterboxHost exe)
+			{
+				_exe = exe;
+				_pagesDomain = exe.GetPagesDomain();
+
+				Name = "System Bus";
+				Size = _pagesDomain.Size * WBX_PAGE_SIZE;
+				WordSize = 4;
+				EndianType = Endian.Little;
+				Writable = false;
+			}
+
+			private ReadOnlySpan<byte> GetPage(uint addr)
+			{
+				var pageNum = addr / WBX_PAGE_SIZE;
+				if ((_pagesDomain.PeekByte(pageNum) & WBX_PAGE_READABLE_BITFLAG)
+					!= WBX_PAGE_READABLE_BITFLAG)
+				{
+					return [ ];
+				}
+
+				return Util.UnsafeSpanFromPointer(
+					ptr: (IntPtr) (WBX_BASE_ADDR + addr),
+					length: (int) (WBX_PAGE_SIZE - (addr & WBX_PAGE_MASK)));
+			}
+
+			public override byte PeekByte(long addr)
+			{
+				var page = GetPage((uint) addr);
+				if (page.IsEmpty)
+				{
+					return 0;
+				}
+
+				using (_exe.EnterExit())
+				{
+					return page[0];
+				}
+			}
+
+			public override ushort PeekUshort(long addr, bool bigEndian)
+			{
+				// if we cross a page boundary, we need to read multiple pages
+				if ((addr & WBX_PAGE_MASK) > WBX_PAGE_MASK - 1)
+				{
+					return base.PeekUshort(addr, bigEndian);
+				}
+
+				var page = GetPage((uint) addr);
+				if (page.IsEmpty)
+				{
+					return 0;
+				}
+
+				using (_exe.EnterExit())
+				{
+					return bigEndian
+						? BinaryPrimitives.ReadUInt16BigEndian(page)
+						: BinaryPrimitives.ReadUInt16LittleEndian(page);
+				}
+			}
+
+			public override uint PeekUint(long addr, bool bigEndian)
+			{
+				// if we cross a page boundary, we need to read multiple pages
+				if ((addr & WBX_PAGE_MASK) > WBX_PAGE_MASK - 3)
+				{
+					return base.PeekUint(addr, bigEndian);
+				}
+
+				var page = GetPage((uint) addr);
+				if (page.IsEmpty)
+				{
+					return 0;
+				}
+
+				using (_exe.EnterExit())
+				{
+					return bigEndian
+						? BinaryPrimitives.ReadUInt32BigEndian(page)
+						: BinaryPrimitives.ReadUInt32LittleEndian(page);
+				}
+			}
+
+			private void BulkPeekByte(uint startAddr, Span<byte> values)
+			{
+				using (_exe.EnterExit())
+				{
+					while (!values.IsEmpty)
 					{
-						if (addr >= internalMemorySize)
+						var page = GetPage(startAddr);
+						var numBytes = Math.Min(
+							values.Length,
+							(int) (WBX_PAGE_SIZE - (startAddr & WBX_PAGE_MASK)));
+						if (page.IsEmpty)
 						{
-							throw new ArgumentOutOfRangeException(
-								paramName: nameof(addr),
-								addr, message: "address out of range");
-						}
-
-						var baseAddress = (IntPtr) 0x36f_0000_0000; // hardcoded for wbx
-						var page = addr / 4096;
-						const int readablePageBit = 1;
-
-						if ((pagesDomain.PeekByte(page) & readablePageBit) is not 0)
-						{
-							byte ret = 0;
-							unsafe
-							{
-								ret = ((byte*)baseAddress)[addr];
-							}
-							return ret;
+							values[..numBytes].Clear();
 						}
 						else
 						{
-							return 0;
+							page[..numBytes].CopyTo(values);
 						}
-					},
-					null,
-					1),
-				pagesDomain,
-			};
-			MemoryDomains = new MemoryDomainList(domains);
-			((BasicServiceProvider)ServiceProvider).Register(MemoryDomains);
+
+						values = values[numBytes..];
+						startAddr += (uint) numBytes;
+					}
+				}
+			}
+
+			public override void BulkPeekByte(Range<long> addresses, byte[] values)
+			{
+				if (addresses is null)
+					throw new ArgumentNullException(paramName: nameof(addresses));
+				if (values is null)
+					throw new ArgumentNullException(paramName: nameof(values));
+
+				if ((long) addresses.Count() != values.Length)
+				{
+					throw new InvalidOperationException("Invalid length of values array");
+				}
+
+				BulkPeekByte((uint) addresses.Start, values);
+			}
+
+			public override void BulkPeekUshort(Range<long> addresses, bool bigEndian, ushort[] values)
+			{
+				if (addresses is null)
+					throw new ArgumentNullException(paramName: nameof(addresses));
+				if (values is null)
+					throw new ArgumentNullException(paramName: nameof(values));
+
+				var start = addresses.Start;
+				var end = addresses.EndInclusive + 1;
+
+				if ((start & 1) != 0 || (end & 1) != 0)
+					throw new InvalidOperationException("The API contract doesn't define what to do for unaligned reads and writes!");
+
+				if (values.LongLength * 2 != end - start)
+				{
+					// a longer array could be valid, but nothing needs that so don't support it for now
+					throw new InvalidOperationException("Invalid length of values array");
+				}
+
+				BulkPeekByte((uint) addresses.Start, MemoryMarshal.AsBytes(values.AsSpan()));
+
+				if (bigEndian)
+				{
+					for (var i = 0; i < values.Length; i++)
+					{
+						values[i] = BinaryPrimitives.ReverseEndianness(values[i]);
+					}
+				}
+			}
+
+			public override void BulkPeekUint(Range<long> addresses, bool bigEndian, uint[] values)
+			{
+				if (addresses is null)
+					throw new ArgumentNullException(paramName: nameof(addresses));
+				if (values is null)
+					throw new ArgumentNullException(paramName: nameof(values));
+
+				var start = addresses.Start;
+				var end = addresses.EndInclusive + 1;
+
+				if ((start & 3) != 0 || (end & 3) != 0)
+					throw new InvalidOperationException("The API contract doesn't define what to do for unaligned reads and writes!");
+
+				if (values.LongLength * 4 != end - start)
+				{
+					// a longer array could be valid, but nothing needs that so don't support it for now
+					throw new InvalidOperationException("Invalid length of values array");
+				}
+
+				BulkPeekByte((uint) addresses.Start, MemoryMarshal.AsBytes(values.AsSpan()));
+
+				if (bigEndian)
+				{
+					for (var i = 0; i < values.Length; i++)
+					{
+						values[i] = BinaryPrimitives.ReverseEndianness(values[i]);
+					}
+				}
+			}
+
+			public override void PokeByte(long addr, byte val)
+			{
+			}
 		}
 	}
 }
