474fe58d9f4ca38624bfb424973a57a4ed922892
YoshiRulz <OSSYoshiRulz+git@gmail.com>
2025-11-09 14:43:20 +1000

    Nix expr: Forked Nixpkgs at 24.05 and backported .NET 10

--- Dist/nixpkgs.nix
+++ Dist/nixpkgs.nix
@@ -1,7 +1,7 @@
 {
 	nixpkgs-24_05 = system: import (builtins.fetchTarball {
-		url = "https://github.com/NixOS/nixpkgs/archive/24.05.tar.gz";
-		sha256 = "1lr1h35prqkd1mkmzriwlpvxcb34kmhc9dnr48gkm8hh089hifmx";
+		url = "https://github.com/YoshiRulz/nixpkgs/archive/a9302067440792f6137e4e96f3bfcbb1ed086eb2.tar.gz"; # .NET 10 backported to 24.05, since that was the last release before ebd3b37e6, see https://github.com/NixOS/nixpkgs/pull/327651#issuecomment-3499977198
+		sha256 = "1app1qh40blfr2r1dkkpp7r22kmbq50x4zicrak8rrcgamxd0lxb";
 	}) { inherit system; };
 	nixpkgs-23_05 = system: fetchzip: import (fetchzip {
 		url = "https://github.com/NixOS/nixpkgs/archive/23.05.tar.gz";
