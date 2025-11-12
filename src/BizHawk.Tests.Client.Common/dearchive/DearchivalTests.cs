c60d8d1cca9fc853290bea5d507a65e3262e8b40
YoshiRulz <OSSYoshiRulz+git@gmail.com>
2025-11-12 16:13:56 +1000

    Refactor `DearchivalTests`

--- src/BizHawk.Tests.Client.Common/dearchive/DearchivalTests.cs
+++ src/BizHawk.Tests.Client.Common/dearchive/DearchivalTests.cs
@@ -1,5 +1,5 @@
-﻿using System.IO;
-using System.Linq;
+﻿using System.Collections.Generic;
+using System.IO;
 
 using BizHawk.Client.Common;
 using BizHawk.Common;
@@ -12,13 +12,14 @@ namespace BizHawk.Tests.Client.Common.Dearchive
 	{
 		private const string EMBED_GROUP = "data.dearchive.";
 
-		private static readonly (string Filename, bool HasSharpCompressSupport)[] TestCases = {
-			("m3_scy_change.7z", true),
-			("m3_scy_change.gb.gz", true),
-			("m3_scy_change.rar", true),
-			("m3_scy_change.bsdtar.tar", true),
-			("m3_scy_change.gnutar.tar", true),
-			("m3_scy_change.zip", true),
+		private static IEnumerable<object?[]> TestCases { get; } = new[]
+		{
+			new object?[] { "m3_scy_change.7z", true },
+			new object?[] { "m3_scy_change.gb.gz", true },
+			new object?[] { "m3_scy_change.rar", true },
+			new object?[] { "m3_scy_change.bsdtar.tar", true },
+			new object?[] { "m3_scy_change.gnutar.tar", true },
+			new object?[] { "m3_scy_change.zip", true },
 		};
 
 		private readonly Lazy<byte[]> _rom = new(static () => ReflectionCache.EmbeddedResourceStream(EMBED_GROUP + "m3_scy_change.gb").ReadAllBytes());
@@ -30,16 +31,16 @@ namespace BizHawk.Tests.Client.Common.Dearchive
 		public void SanityCheck() => Assert.AreEqual("SHA1:70DCA8E791878BDD32426391E4233EA52B47CDD1", SHA1Checksum.ComputePrefixedHex(Rom));
 #pragma warning restore BHI1600
 
+		[DynamicData(nameof(TestCases))]
 		[TestMethod]
-		public void TestSharpCompress()
+		public void TestSharpCompress(string filename, bool hasSharpCompressSupport)
 		{
+			if (!hasSharpCompressSupport) return;
 			var sc = SharpCompressDearchivalMethod.Instance;
-			foreach (var filename in TestCases.Where(testCase => testCase.HasSharpCompressSupport)
-				.Select(testCase => testCase.Filename))
-			{
+			/*scope*/{
 				var archive = ReflectionCache.EmbeddedResourceStream(EMBED_GROUP + filename);
 				Assert.IsTrue(sc.CheckSignature(archive, filename), $"{filename} is an archive, but wasn't detected as such"); // puts the seek pos of the Stream param back where it was (in this case at the start)
-				var af = sc.Construct(archive);
+				using var af = sc.Construct(archive);
 				var items = af.Scan();
 				Assert.IsNotNull(items, $"{filename} contains 1 file, but it couldn't be enumerated correctly");
 				Assert.AreEqual(1, items!.Count, $"{filename} contains 1 file, but was detected as containing {items.Count} files");
