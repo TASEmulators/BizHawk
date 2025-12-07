c1df4e6bd539e89364ad8308e77a80420416c8ad
SuuperW <SuuperW@gmail.com>
2025-12-07 14:26:23 -0600

    Make current file tracking non-static by moving it to LuaLibraries instance.

--- src/BizHawk.Client.Common/lua/ILuaLibraries.cs
+++ src/BizHawk.Client.Common/lua/ILuaLibraries.cs
@@ -2,6 +2,8 @@ namespace BizHawk.Client.Common
 {
 	public interface ILuaLibraries
 	{
+		LuaFile CurrentFile { get; }
+
 		/// <remarks>pretty hacky... we don't want a lua script to be able to restart itself by rebooting the core</remarks>
 		bool IsRebootingCore { get; set; }
 
