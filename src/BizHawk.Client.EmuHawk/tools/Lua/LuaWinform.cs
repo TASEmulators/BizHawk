4e19989567b2ad77e23094a685e08e2932bcf6a2
SuuperW <SuuperW@gmail.com>
2025-12-10 00:55:44 -0600

    Keep Lua scripts running if a LuaWinform is open, like if an event is registered. Sandbox the form's close event. Close the script's forms when the script is stopped.

--- src/BizHawk.Client.EmuHawk/tools/Lua/LuaWinform.cs
+++ src/BizHawk.Client.EmuHawk/tools/Lua/LuaWinform.cs
@@ -5,23 +5,44 @@ using BizHawk.Client.Common;
 
 namespace BizHawk.Client.EmuHawk
 {
-	public partial class LuaWinform : Form
+	public partial class LuaWinform : Form, IKeepFileRunning
 	{
 		public List<LuaEvent> ControlEvents { get; } = new List<LuaEvent>();
 
 		private readonly LuaFile _ownerFile;
 		private readonly ILuaLibraries _luaImp;
+		private readonly LuaFunction/*?*/ _closeCallback;
+		private bool _closed = false;
 
 		public bool BlocksInputWhenFocused { get; set; } = true;
 
-		public LuaWinform(LuaFile ownerFile, ILuaLibraries luaLibraries, Action<IntPtr> formsWindowClosedCallback)
+		public LuaWinform(LuaFile ownerFile, ILuaLibraries luaLibraries, Action<IntPtr> formsWindowClosedCallback, LuaFunction luaCloseCallback = null)
 		{
 			_ownerFile = ownerFile;
 			_luaImp = luaLibraries;
+			_closeCallback = luaCloseCallback;
+
 			InitializeComponent();
+
 			Icon = Properties.Resources.TextDocIcon;
 			StartPosition = FormStartPosition.CenterParent;
-			Closing += (o, e) => formsWindowClosedCallback(Handle);
+
+			_ownerFile.AddDisposable(this);
+			Closing += (o, e) =>
+			{
+				if (_closed) return;
+				_closed = true;
+
+				formsWindowClosedCallback(Handle); // This one first. _closeCallback may end up disposing this instance.
+				if (_closeCallback != null)
+				{
+					_luaImp.Sandbox(
+						luaFile: _ownerFile,
+						callback: () => _ = _closeCallback.Call(),
+						prohibitedApis: ApiGroup.YIELDING);
+				}
+				_ownerFile.RemoveDisposable(this);
+			};
 		}
 
 		public void DoLuaEvent(IntPtr handle)
