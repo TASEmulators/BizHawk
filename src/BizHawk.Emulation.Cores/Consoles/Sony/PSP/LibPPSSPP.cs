2dc30e1fb36884275777a8c4f04dcf8f3132c44c
Sergio Martin <eienburuu@gmail.com>
2026-02-24 19:07:20 +0100

    Experimental native PPSSPP port (squashed PR #4284)
    * Adding base
    
    * progress
    
    * Building
    
    * Fixing memareas
    
    * Adding base
    
    * Progress
    
    * updates to bizhawk
    
    * Taking ppsspp out of waterbox, trying to run it ported
    
    * renaming old implementation to _wbx before adding the ported one
    
    * creating adapted psp
    
    * Revert "creating adapted psp"
    
    This reverts commit 9a2b83f804f1e2ce0c1550c61f76959975e751fa.
    
    * Revert "renaming old implementation to _wbx before adding the ported one"
    
    This reverts commit c6690c8af946bf6bdb418d4ad1759288c79b9cb0.
    
    * Reverting some changes, removing waterbox base class
    
    * Stable base for new PPSSPP core
    
    * Added msvc
    
    * [WIP] working on windows compilation
    
    * Compiling but still not linking
    
    * Progress
    
    * Producing dll
    
    * Progress
    
    * ported running
    
    * Now running on windows with ffmpeg
    
    * Updating ppsspp
    
    * Progress with ported
    
    * updates
    
    * Properly working with myst
    
    * Updates
    
    * Load/save working
    
    * Proper load/save and controllers fixed
    
    * Preventing crash on save dialog
    
    * compiling on linux
    
    * Preserving symbols
    
    * Adding gitignore
    
    * Progress
    
    * Adding sdl config
    
    * Windows now compiling. Font error fixed
    
    * built linux
    
    * Adding UI fonts
    
    * Resync with upstream
    
    * Detecting inputs
    
    * Updating linux compilation
    
    * adding sound
    
    * Added sound
    
    * Remove unused imports
    
    * Fix line-end whitespace
    
    * Use `#if false` for commented-out code blocks
    
    * Uploading release lib instead of debug for testing purposes
    
    * Addressing comments and fixing warnings
    
    * Addressing styler complaints
    
    * Addressing styler complaints
    
    * Removing settings
    
    * Addressing comments
    
    * attempting to make ppsspp in linux load the proper dll, still failing
    
    * fixing proper dynamic linking
    
    * Adding missing libs for ppsspp, proper linking, and disabling problematic logging functions
    
    * Adding missing symbolic links
    
    * Addressing comments
    
    * Adding missing SDL symlink
    
    * use static ffmpeg libraries
    
    changes the linux linking process to use static ffmpeg libraries and system-provided SDL2 (could also link our Assets/dll/libSDL2.so I guess)
    
    * forgot make install
    
    * Revert changes to VS Code dotfiles
    
    if this is relevant outside Jaffar, do it in a separate PR
    (bars)
    
    * Update PPSSPP.IVideoProvider.cs
    
    Fixing proper video height
    
    * Remove Right Analog
    
    * Remove Right Analog
    
    * Remove Right Analog
    
    * fix submodule names
    
    * alphabetise in `RomLoader.MakeGameFromDisc`
    
    * Remove debugging print
    
    * Unimplement `ISettable<,>` service on `PPSSPP`
    
    * Using temp folder for files
    
    * Fixing rewind, changing compiler to clang, fixing video having 2 more pixels height
    
    * Pushing linux lib
    
    * Fixing analyzer error
    
    * remove SDL submodule
    
    * Fixing video resolution height 270 -> 272
    
    * fix retro_log_printf_callback
    
    * fix UB causing divide by zero exceptions
    
    should probably be fixed properly upstream
    
    oh and someone else rebuild linux kthx
    
    * Updating PPSSPP with CPP's DVD loading fix and rebasing with upstream PPSSPP
    
    * Revert "Updating PPSSPP with CPP's DVD loading fix and rebasing with upstream PPSSPP"
    
    This reverts commit a8d724e2b1f04a7e3050eef058077c2fec822f38.
    
    * recomp of dll
    
    * Changes according to Yoshi's request
    
    ---------
    
    Co-authored-by: Sergio Martin <martiser@ethz.ch>
    Co-authored-by: YoshiRulz <OSSYoshiRulz+git@gmail.com>
    Co-authored-by: Morilli <35152647+Morilli@users.noreply.github.com>
    Co-authored-by: Spikestuff <Spikestuff@users.noreply.github.com>

---
+++ src/BizHawk.Emulation.Cores/Consoles/Sony/PSP/LibPPSSPP.cs
@@ -0,0 +1,78 @@
+﻿using System.Runtime.InteropServices;
+using BizHawk.BizInvoke;
+
+namespace BizHawk.Emulation.Cores.Consoles.Sony.PSP
+{
+	public abstract class LibPPSSPP
+	{
+		private const CallingConvention CC = CallingConvention.Cdecl;
+
+		[UnmanagedFunctionPointer(CC)]
+		public delegate void CDReadCallback(int lba, IntPtr dst);
+
+		[UnmanagedFunctionPointer(CC)]
+		public delegate int CDSectorCountCallback();
+
+		[UnmanagedFunctionPointer(CC)]
+		public delegate void InputCallback();
+
+		[BizImport(CC)]
+		public abstract void SetCdCallbacks(CDReadCallback cdrc, CDSectorCountCallback cdscc);
+
+		[BizImport(CC)]
+		public abstract void SetInputCallback(InputCallback ipc);
+
+		[BizImport(CC, Compatibility = true)]
+		public abstract bool Init(string gameFile, string userDataPath);
+
+		[BizImport(CC, Compatibility = true)]
+		public abstract void FrameAdvance(FrameInfo f);
+
+		[BizImport(CC, Compatibility = true)]
+		public abstract int GetStateSize();
+
+		[BizImport(CC, Compatibility = true)]
+		public abstract void SaveState(byte[] buffer);
+
+		[BizImport(CC, Compatibility = true)]
+		public abstract void LoadState(byte[] buffer, int stateLen);
+
+		[BizImport(CC, Compatibility = true)]
+		public abstract bool loadResource(string resourceName, byte[] buffer, int resourceLen);
+
+		[BizImport(CC, Compatibility = true)]
+
+		public abstract void Deinit();
+
+		[BizImport(CC)]
+		public abstract void GetVideo(int[] buffer);
+
+		[BizImport(CC)]
+		public abstract int GetAudio(short[] buffer);
+
+		[StructLayout(LayoutKind.Sequential)]
+		public struct GamepadInputs
+		{
+			public int Up;
+			public int Down;
+			public int Left;
+			public int Right;
+			public int Start;
+			public int Select;
+			public int ButtonSquare;
+			public int ButtonTriangle;
+			public int ButtonCircle;
+			public int ButtonCross;
+			public int ButtonLTrigger;
+			public int ButtonRTrigger;
+			public int LeftAnalogX;
+			public int LeftAnalogY;
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		public class FrameInfo
+		{
+			public GamepadInputs input;
+		}
+	}
+}
---
+++ src/BizHawk.Emulation.Cores/Consoles/Sony/PSP/PPSSPP.IStatable.cs
@@ -0,0 +1,53 @@
+using System.IO;
+
+using BizHawk.Emulation.Common;
+
+namespace BizHawk.Emulation.Cores.Consoles.Sony.PSP
+{
+	public partial class PPSSPP : IStatable
+	{
+		private byte[] _stateBuf = [ ];
+
+		public bool AvoidRewind => false;
+
+		public void SaveStateBinary(BinaryWriter writer)
+		{
+			var stateLen = _libPPSSPP.GetStateSize();
+			writer.Write(stateLen);
+
+			if (stateLen != _stateBuf.Length)
+			{
+				_stateBuf = new byte[stateLen];
+			}
+
+			_libPPSSPP.SaveState(_stateBuf);
+			writer.Write(_stateBuf, 0, stateLen);
+
+			// other variables
+			writer.Write(IsLagFrame);
+			writer.Write(LagCount);
+			writer.Write(Frame);
+		}
+
+		public void LoadStateBinary(BinaryReader reader)
+		{
+			var stateLen = reader.ReadInt32();
+
+			if (stateLen != _stateBuf.Length)
+			{
+				_stateBuf = new byte[stateLen];
+			}
+
+			reader.Read(_stateBuf, 0, stateLen);
+			_libPPSSPP.LoadState(_stateBuf, stateLen);
+
+			// other variables
+			IsLagFrame = reader.ReadBoolean();
+			LagCount = reader.ReadInt32();
+			Frame = reader.ReadInt32();
+
+			// memory domain pointers are no longer valid, reset them
+			// WireMemoryDomains();
+		}
+	}
+}
