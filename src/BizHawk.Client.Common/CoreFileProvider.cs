TASEmulators/BizHawk

commit 0681dd2590ffe8e68e3383d273566118e557c6bb

set proper libretro game data
- closes #4202

src/BizHawk.Client.Common/RomLoader.cs
@@ -793,7 +793,15 @@ namespace BizHawk.Client.Common
 				{
 					// must be done before LoadNoGame (which triggers retro_init and the paths to be consumed by the core)
 					// game name == name of core
-					Game = game = new GameInfo { Name = Path.GetFileNameWithoutExtension(launchLibretroCore), System = VSystemID.Raw.Libretro };
+					game = Disc.IsValidExtension(file.Extension)
+						? MakeGameFromDisc(
+							InstantiateDiscFor(path),
+							ext: file.Extension,
+							name: Path.GetFileNameWithoutExtension(file.Name),
+							fastFailUnsupportedSystems: false)
+						: new RomGame(file).GameInfo;
+					game.Name = $"{game.Name} [{Path.GetFileNameWithoutExtension(launchLibretroCore)}]";
+					game.System = VSystemID.Raw.Libretro;
 					var retro = new LibretroHost(nextComm, game, launchLibretroCore);
 					nextEmulator = retro;
 
@@ -841,16 +849,6 @@ namespace BizHawk.Client.Common
 							retro.Dispose();
 							return false;
 						}
-						// else success; update game name
-						var ext = file.Extension;
-						var gi = Disc.IsValidExtension(ext)
-							? MakeGameFromDisc(
-								InstantiateDiscFor(path),
-								ext: ext,
-								name: Path.GetFileNameWithoutExtension(file.Name),
-								fastFailUnsupportedSystems: false)
-							: new RomGame(file).GameInfo;
-						Game.Name = $"{gi.Name} [{Game.Name/* core name */}]";
 					}
 				}
 				else

commit e3bea5233c56b5a7fa7cdca413d6e21575d9f6af

cleanup libretro pathentry handling
- should close #4669

Presumably broke in 0681dd2590ffe8e68e3383d273566118e557c6bb since that sets different game data before instantiating the libretro host. Relying on the game "name" when we actually want the core name was wrong to begin with imo, so I've simplified the logic to never rely on `GameInfo`.

src/BizHawk.Client.Common/CoreFileProvider.cs
@@ -27,12 +27,12 @@ namespace BizHawk.Client.Common
 		}
 
 		// Poop
-		public string GetRetroSaveRAMDirectory(IGameInfo game)
-			=> _pathEntries.RetroSaveRamAbsolutePath(game);
+		public string GetRetroSaveRAMDirectory(string corePath)
+			=> _pathEntries.RetroSaveRamAbsolutePath(Path.GetFileNameWithoutExtension(corePath));
 
 		// Poop
-		public string GetRetroSystemPath(IGameInfo game)
-			=> _pathEntries.RetroSystemAbsolutePath(game);
+		public string GetRetroSystemPath(string corePath)
+			=> _pathEntries.RetroSystemAbsolutePath(Path.GetFileNameWithoutExtension(corePath));
 
 		public string GetUserPath(string sysID, bool temp)
 		{

src/BizHawk.Client.Common/RomLoader.cs
@@ -849,7 +849,7 @@ namespace BizHawk.Client.Common
 						game = new GameInfo { Name = Path.GetFileNameWithoutExtension(launchLibretroCore) };
 					}
 					game.System = VSystemID.Raw.Libretro;
-					var retro = new LibretroHost(nextComm, game, launchLibretroCore);
+					var retro = new LibretroHost(nextComm, launchLibretroCore);
 					nextEmulator = retro;
 
 					if (retro.Description.SupportsNoGame && string.IsNullOrEmpty(path))

src/BizHawk.Client.Common/config/PathEntryCollectionExtensions.cs
@@ -245,32 +245,19 @@ namespace BizHawk.Client.Common
 		}
 
 		// Shenanigans
-		public static string RetroSaveRamAbsolutePath(this PathEntryCollection collection, IGameInfo game)
+		public static string RetroSaveRamAbsolutePath(this PathEntryCollection collection, string coreName)
 		{
-			var name = game.FilesystemSafeName();
-			name = Path.GetDirectoryName(name);
-			if (name is null) name = string.Empty;
-			else if (name.Length is 0) name = game.FilesystemSafeName();
-			var pathEntry = collection[game.System, "Save RAM"]
-				?? collection[game.System, "Base"];
+			var pathEntry = collection[VSystemID.Raw.Libretro, "Save RAM"];
 
-			return Path.Combine(collection.AbsolutePathFor(pathEntry.Path, game.System), name);
+			return Path.Combine(collection.AbsolutePathFor(pathEntry.Path, VSystemID.Raw.Libretro), coreName);
 		}
 
 		// Shenanigans
-		public static string RetroSystemAbsolutePath(this PathEntryCollection collection, IGameInfo game)
+		public static string RetroSystemAbsolutePath(this PathEntryCollection collection, string coreName)
 		{
-			var name = game.FilesystemSafeName();
-			name = Path.GetDirectoryName(name);
-			if (string.IsNullOrEmpty(name))
-			{
-				name = game.FilesystemSafeName();
-			}
+			var pathEntry = collection[VSystemID.Raw.Libretro, "System"];
 
-			var pathEntry = collection[game.System, "System"]
-				?? collection[game.System, "Base"];
-
-			return Path.Combine(collection.AbsolutePathFor(pathEntry.Path, game.System), name);
+			return Path.Combine(collection.AbsolutePathFor(pathEntry.Path, VSystemID.Raw.Libretro), coreName);
 		}
 
 		public static string AutoSaveRamAbsolutePath(this PathEntryCollection collection, IGameInfo game, IMovie movie)

src/BizHawk.Client.EmuHawk/MainForm.Events.cs
@@ -220,7 +220,7 @@ namespace BizHawk.Client.EmuHawk
 
 		private void OpenAdvancedMenuItem_Click(object sender, EventArgs e)
 		{
-			using var oac = new OpenAdvancedChooser(this, Config, CreateCoreComm, Game, RunLibretroCoreChooser);
+			using var oac = new OpenAdvancedChooser(this, Config, CreateCoreComm, RunLibretroCoreChooser);
 			if (this.ShowDialogWithTempMute(oac) == DialogResult.Cancel) return;
 
 			if (oac.Result == AdvancedRomLoaderType.LibretroLaunchNoGame)

src/BizHawk.Client.EmuHawk/OpenAdvancedChooser.cs
@@ -29,8 +29,6 @@ namespace BizHawk.Client.EmuHawk
 
 		private RetroDescription _currentDescription;
 
-		private readonly IGameInfo _game;
-
 		private readonly Func<bool> _libretroCoreChooserCallback;
 
 		public IDialogController DialogController { get; }
@@ -39,11 +37,10 @@ namespace BizHawk.Client.EmuHawk
 
 		public FilesystemFilterSet SuggestedExtensionFilter = null;
 
-		public OpenAdvancedChooser(IDialogController dialogController, Config config, Func<CoreComm> createCoreComm, IGameInfo game, Func<bool> libretroCoreChooserCallback)
+		public OpenAdvancedChooser(IDialogController dialogController, Config config, Func<CoreComm> createCoreComm, Func<bool> libretroCoreChooserCallback)
 		{
 			_config = config;
 			_createCoreComm = createCoreComm;
-			_game = game;
 			_libretroCoreChooserCallback = libretroCoreChooserCallback;
 			DialogController = dialogController;
 
@@ -82,7 +79,7 @@ namespace BizHawk.Client.EmuHawk
 			try
 			{
 				var coreComm = _createCoreComm();
-				using var retro = new LibretroHost(coreComm, _game, core, true);
+				using var retro = new LibretroHost(coreComm, core, true);
 				if (retro.Description.SupportsNoGame)
 					btnLibretroLaunchNoGame.Enabled = true;
 				if (!string.IsNullOrEmpty(retro.Description.ValidExtensions))

src/BizHawk.Emulation.Common/Interfaces/ICoreFileProvider.cs
@@ -8,12 +8,12 @@
 		/// <summary>
 		/// produces a path that contains saveram... because libretro cores need it
 		/// </summary>
-		string GetRetroSaveRAMDirectory(IGameInfo game);
+		string GetRetroSaveRAMDirectory(string corePath);
 
 		/// <summary>
 		/// produces a path for use as a libretro system path (different for each core)
 		/// </summary>
-		string GetRetroSystemPath(IGameInfo game);
+		string GetRetroSystemPath(string corePath);
 
 		/// <summary>
 		/// produces a 'user' path for a given system id

src/BizHawk.Emulation.Cores/Libretro/Libretro.cs
@@ -73,12 +73,11 @@ namespace BizHawk.Emulation.Cores.Libretro
 			}
 		}
 
-		/// <remarks>does not keep a reference to <paramref name="game"/></remarks>
-		public LibretroHost(CoreComm comm, IGameInfo game, string corePath, bool analysis = false)
+		public LibretroHost(CoreComm comm, string corePath, bool analysis = false)
 			: this(
 				comm,
-				libretroSystemDir: comm.CoreFileProvider.GetRetroSystemPath(game),
-				libretroSaveRAMDir: comm.CoreFileProvider.GetRetroSaveRAMDirectory(game),
+				libretroSystemDir: comm.CoreFileProvider.GetRetroSystemPath(corePath),
+				libretroSaveRAMDir: comm.CoreFileProvider.GetRetroSaveRAMDirectory(corePath),
 				corePath: corePath,
 				analysis: analysis) {}
 