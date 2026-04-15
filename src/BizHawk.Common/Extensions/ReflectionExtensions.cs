36b7d1c385f8b85965590aae8a7d50121bfa4006
YoshiRulz <OSSYoshiRulz+git@gmail.com>
2026-04-16 08:03:07 +1000

    Add `ReflectionExtensions.{IsNRT,IsNullableT}(ParameterInfo)`

--- src/BizHawk.Common/Extensions/ReflectionExtensions.cs
+++ src/BizHawk.Common/Extensions/ReflectionExtensions.cs
@@ -88,6 +88,62 @@ namespace BizHawk.Common.ReflectionExtensions
 			return (T)o.GetType().GetCustomAttributes(typeof(T), false)[0];
 		}
 
+		/// <returns>
+		/// <see langword="true"/> if <paramref name="parameter"/>'s type was referenced within <c>#nullable enable</c> with a <c>?</c>,
+		/// <see langword="false"/> if it was referenced within <c>#nullable enable</c> without a <c>?</c>,
+		/// or <see langword="null"/> if it was referenced within <c>#nullable disable</c>
+		/// </returns>
+		/// <exception cref="ArgumentException"><paramref name="parameter"/> is { <see cref="ParameterInfo.ParameterType" />.<see cref="Type.IsValueType" />: <see langword="true"/> }</exception>
+		public static bool? IsNRT(this ParameterInfo parameter)
+		{
+			if (parameter.ParameterType.IsValueType) throw new ArgumentException(paramName: nameof(parameter), message: "this parameter is a value type; it does not participate in nullable reference types");
+
+			// Check [Nullable] on the parameter first
+			const byte AnnotatedNotNull = 1; // https://github.com/dotnet/roslyn/blob/main/docs/features/nullable-metadata.md
+			var nrtAttr = parameter.GetCustomAttributes().FirstOrDefault(attr => attr.GetType().FullName is "System.Runtime.CompilerServices.NullableAttribute"); // reflection required because the type is Source Generated for each assembly
+			if (nrtAttr?.GetType().GetField("NullableFlags").GetValue(nrtAttr) is byte[] flags) // have to use reflection here for the same reason
+			{
+				return flags[0] is not AnnotatedNotNull;
+			}
+
+			// Check [NullableContext] on the method and containing type(s)
+			MemberInfo parent = parameter.Member;
+			while (parent is not null)
+			{
+				var contextAttr = parent.GetCustomAttributes().FirstOrDefault(attr => attr.GetType().FullName is "System.Runtime.CompilerServices.NullableContextAttribute"); // ditto
+				if (contextAttr?.GetType().GetField("Flag").GetValue(contextAttr) is byte flag)
+				{
+					return flag is not AnnotatedNotNull;
+				}
+				parent = parent.DeclaringType;
+			}
+
+			return null;
+		}
+
+		/// <returns>
+		/// (when <paramref name="pi"/> is { <see cref="ParameterInfo.ParameterType" />.<see cref="Type.IsValueType" />: <see langword="true"/> })
+		/// <see langword="true"/> iff <paramref name="pi"/>'s type is any parameterisation of <see cref="Nullable{T}"/>
+		/// <br/>(when <paramref name="pi"/> is { <see cref="ParameterInfo.ParameterType" />.<see cref="Type.IsValueType" />: <see langword="false"/> })
+		/// <see langword="true"/> if <paramref name="pi"/>'s type was referenced within <c>#nullable enable</c> with a <c>?</c>,
+		/// <see langword="false"/> if it was referenced within <c>#nullable enable</c> without a <c>?</c>,
+		/// or <see langword="null"/> if it was referenced within <c>#nullable disable</c>
+		/// </returns>
+		/// <remarks>simple wrapper over <see cref="IsNRT"/> and <see cref="IsNullableT(ParameterInfo)"/></remarks>
+		public static bool? IsNRTOrNullableT(this ParameterInfo pi)
+		{
+			var type = pi.ParameterType;
+			return type.IsValueType ? IsNullableT(type) : IsNRT(pi);
+		}
+
+		/// <returns><see langword="true"/> iff <paramref name="pi"/>'s type is any parameterisation of <see cref="Nullable{T}"/></returns>
+		/// <exception cref="ArgumentException"><paramref name="pi"/> is { <see cref="ParameterInfo.ParameterType" />.<see cref="Type.IsValueType" />: <see langword="false"/> }</exception>
+		public static bool IsNullableT(this ParameterInfo pi)
+		{
+			var type = pi.ParameterType;
+			return type.IsValueType ? type.IsNullableT() : throw new ArgumentException(paramName: nameof(pi), message: "this parameter is a reference type so it's obviously not Nullable<T>");
+		}
+
 		/// <returns><see langword="true"/> iff <paramref name="type"/> is any parameterisation of <see cref="Nullable{T}"/></returns>
 		public static bool IsNullableT(this Type type)
 			=> type.IsConstructedGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>);
--- src/BizHawk.Tests.Common/ReflectionExtensions/ReflectionExtensionTests.cs
+++ src/BizHawk.Tests.Common/ReflectionExtensions/ReflectionExtensionTests.cs
@@ -1,3 +1,7 @@
+#pragma warning disable IDE0240 // just making sure NRTs are set up right for this file
+#pragma warning disable IDE0241 // ditto
+#nullable enable
+
 using BizHawk.Common.ReflectionExtensions;
 
 namespace BizHawk.Tests.Common.ReflectionExtensions
@@ -5,6 +9,56 @@ namespace BizHawk.Tests.Common.ReflectionExtensions
 	[TestClass]
 	public sealed class ReflectionExtensionTests
 	{
+		private static class Dummy
+		{
+#if false
+#nullable disable
+			public static readonly string Field_RefT_Ambig = default!;
+#nullable restore
+
+			public static readonly string Field_RefT_NonNull = default!;
+
+			public static readonly string? Field_RefT_Nullable = default;
+
+			public static readonly int Field_ValT_NonNull = default;
+
+			public static readonly int? Field_ValT_Nullable = default;
+#endif
+
+#nullable disable
+			public static string Method_RefT_Ambig(string arg)
+				=> arg;
+#nullable restore
+
+			public static string Method_RefT_NonNull(string arg)
+				=> arg;
+
+			public static string? Method_RefT_Nullable(string? arg)
+				=> arg;
+
+			public static int Method_ValT_NonNull(int arg)
+				=> arg;
+
+			public static int? Method_ValT_Nullable(int? arg)
+				=> arg;
+		}
+
+		private static readonly Type DummyClass = typeof(Dummy);
+
+#if false
+		[DataRow("RefT_Ambig", "null")]
+		[DataRow("RefT_NonNull", "false")]
+		[DataRow("RefT_Nullable", "true")]
+		[DataRow("ValT_NonNull", "false")]
+		[DataRow("ValT_Nullable", "true")]
+		[TestMethod]
+		public void TestFieldIsNRT(string label, string expectedStr)
+		{
+			bool? expected = bool.TryParse(expectedStr, out var b) ? b : null;
+			Assert.AreEqual(expected, DummyClass.GetField($"Field_{label}").IsNRT());
+		}
+#endif
+
 		[TestMethod]
 		public void TestIsNullableT()
 		{
@@ -12,11 +66,38 @@ namespace BizHawk.Tests.Common.ReflectionExtensions
 			Assert.IsFalse(typeof(string).IsNullableT(), "string");
 			Assert.IsFalse(typeof(int[]).IsNullableT(), "int[]");
 			Assert.IsFalse(typeof(ReflectionExtensionTests).IsNullableT(), "class ReflectionExtensionTests");
+			Assert.IsFalse(DummyClass.IsNullableT(), "static class Dummy");
 			Assert.IsFalse(typeof(ValueType).IsNullableT(), "ValueType");
 			Assert.IsFalse(typeof(int).IsNullableT(), "int");
 			Assert.IsFalse(typeof(Nullable<>).IsNullableT(), "unparameterised Nullable<>");
 			Assert.IsTrue(typeof(int?).IsNullableT(), "Nullable<int>");
 			Assert.IsTrue(typeof(Guid?).IsNullableT(), "Nullable<Guid>");
 		}
+
+		[DataRow("RefT_Ambig", "null")]
+		[DataRow("RefT_NonNull", "false")]
+		[DataRow("RefT_Nullable", "true")]
+		[DataRow("ValT_NonNull", "false")]
+		[DataRow("ValT_Nullable", "true")]
+		[TestMethod]
+		public void TestParamIsNRT(string label, string expectedStr)
+		{
+			bool? expected = bool.TryParse(expectedStr, out var b) ? b : null;
+			Assert.AreEqual(expected, DummyClass.GetMethod($"Method_{label}").GetParameters()[0].IsNRTOrNullableT());
+		}
+
+#if false
+		[DataRow("RefT_Ambig", "null")]
+		[DataRow("RefT_NonNull", "false")]
+		[DataRow("RefT_Nullable", "true")]
+		[DataRow("ValT_NonNull", "false")]
+		[DataRow("ValT_Nullable", "true")]
+		[TestMethod]
+		public void TestReturnIsNRT(string label, string expectedStr)
+		{
+			bool? expected = bool.TryParse(expectedStr, out var b) ? b : null;
+			Assert.AreEqual(expected, DummyClass.GetMethod($"Method_{label}").ReturnTypeIsNRT());
+		}
+#endif
 	}
 }
