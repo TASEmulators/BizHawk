0e1d4d2e2b39b767f3270c4958b4167f5aecdb9a
YoshiRulz <OSSYoshiRulz+git@gmail.com>
2026-05-29 00:11:04 +1000

    Remove remaining warnings for `--mono-no-redirect`
    see fe2cd58be, b47b4d63b
    deprecated in 2.10 (outside of NixHawk, it was silently ignored)

--- Dist/launch-scripts.nix
+++ Dist/launch-scripts.nix
@@ -109,10 +109,6 @@
 		''}${lib.optionalString (!debugPInvokes) "# "}export MONO_LOG_LEVEL=debug MONO_LOG_MASK=dll # pass `--arg debugPInvokes true` to nix-build to enable
 		${lib.optionalString debugDotnetHostCrashes "# "}export MONO_CRASH_NOFILE=1 # pass `--arg debugDotnetHostCrashes true` to nix-build to disable
 		export MONO_WINFORMS_XIM_STYLE=disabled # see https://bugzilla.xamarin.com/show_bug.cgi?id=28047#c9; apparently this also affects NixOS under at least awesomewm, see https://github.com/TASEmulators/BizHawk/issues/1595
-		if [ "$1" = '--mono-no-redirect' ]; then
-			printf "(passing --mono-no-redirect is no longer necessary)\n" >&2
-			shift
-		fi
 		printf "(capturing output in %s/EmuHawkMono_last*.txt)\n" "$PWD" >&2
 		exec '${redirectOutputToFiles}' EmuHawkMono_laststdout.txt EmuHawkMono_laststderr.txt \
 			'${lib.getExe bizhawkAssemblies.mono}'${monoProfilerFlag} \
